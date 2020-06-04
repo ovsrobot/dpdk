11afe4026d467c82d3bfd07a05f660072993679d
David Coyle <david.coyle@intel.com>
2020-06-04 16:13:24 +0100

    crypto/aesni_mb: add support for DOCSIS protocol
    Add support to the AESNI-MB PMD for the DOCSIS protocol, through the
    rte_security API. This, therefore, includes adding support for the
    rte_security API to this PMD.
    
    Please note this is only a partial implementation, with just
    capabilities structures populated. Full implementation will follow in
    next version.
    
    Signed-off-by: David Coyle <david.coyle@intel.com>
    Signed-off-by: Mairtin o Loingsigh <mairtin.oloingsigh@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/crypto/aesni_mb/rte_aesni_mb_pmd_ops.c
+++ drivers/crypto/aesni_mb/rte_aesni_mb_pmd_ops.c
@@ -8,6 +8,9 @@
 #include <rte_common.h>
 #include <rte_malloc.h>
 #include <rte_cryptodev_pmd.h>
+#ifdef RTE_LIBRTE_SECURITY
+#include <rte_security_driver.h>
+#endif
 
 #include "aesni_mb_pmd_private.h"
 
@@ -499,12 +502,72 @@ static const struct rte_cryptodev_capabilities aesni_mb_pmd_capabilities[] = {
 	RTE_CRYPTODEV_END_OF_CAPABILITIES_LIST()
 };
 
+#ifdef RTE_LIBRTE_SECURITY
+static const struct rte_cryptodev_capabilities
+					aesni_mb_pmd_security_crypto_cap[] = {
+	{	/* AES DOCSIS BPI */
+		.op = RTE_CRYPTO_OP_TYPE_SYMMETRIC,
+		{.sym = {
+			.xform_type = RTE_CRYPTO_SYM_XFORM_CIPHER,
+			{.cipher = {
+				.algo = RTE_CRYPTO_CIPHER_AES_DOCSISBPI,
+				.block_size = 16,
+				.key_size = {
+					.min = 16,
+#if IMB_VERSION_NUM >= IMB_VERSION(0, 53, 3)
+					.max = 32,
+					.increment = 16
+#else
+					.max = 16,
+					.increment = 0
+#endif
+				},
+				.iv_size = {
+					.min = 16,
+					.max = 16,
+					.increment = 0
+				}
+			}, }
+		}, }
+	},
+
+	RTE_CRYPTODEV_END_OF_CAPABILITIES_LIST()
+};
+
+static const struct rte_security_capability aesni_mb_pmd_security_cap[] = {
+	{ /* DOCSIS Uplink */
+		.action = RTE_SECURITY_ACTION_TYPE_LOOKASIDE_PROTOCOL,
+		.protocol = RTE_SECURITY_PROTOCOL_DOCSIS,
+		.docsis = {
+			.direction = RTE_SECURITY_DOCSIS_UPLINK,
+			.crc_size = 4
+		},
+		.crypto_capabilities = aesni_mb_pmd_security_crypto_cap
+	},
+	{ /* DOCSIS downlink */
+		.action = RTE_SECURITY_ACTION_TYPE_LOOKASIDE_PROTOCOL,
+		.protocol = RTE_SECURITY_PROTOCOL_DOCSIS,
+		.docsis = {
+			.direction = RTE_SECURITY_DOCSIS_DOWNLINK,
+			.crc_size = 4
+		},
+		.crypto_capabilities = aesni_mb_pmd_security_crypto_cap
+	},
+	{
+		.action = RTE_SECURITY_ACTION_TYPE_NONE
+	}
+};
+#endif
 
 /** Configure device */
 static int
 aesni_mb_pmd_config(__rte_unused struct rte_cryptodev *dev,
 		__rte_unused struct rte_cryptodev_config *config)
 {
+#ifdef RTE_LIBRTE_SECURITY
+	/* Temporary to avoid compile issues */
+	(void)aesni_mb_pmd_security_cap;
+#endif
 	return 0;
 }
 
