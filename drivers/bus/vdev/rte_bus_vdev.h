ovsrobot/dpdk

commit d4a586d29e65c1c1069e52a43ddda3f03f1c7770

bus/vdev: move code from EAL into a new driver
Move the vdev bus from lib/librte_eal to drivers/bus.

As the crypto vdev helper function refers to data structure
in rte_vdev.h, so we move those helper function into drivers/bus
too.

Signed-off-by: Jianfeng Tan <jianfeng.tan@intel.com>

drivers/bus/vdev/rte_bus_vdev.h
@@ -124,6 +124,28 @@ RTE_PMD_EXPORT_NAME(nm, __COUNTER__)
 #define RTE_PMD_REGISTER_ALIAS(nm, alias)\
 static const char *vdrvinit_ ## nm ## _alias = RTE_STR(alias)
 
+/**
+ * Initialize a driver specified by name.
+ *
+ * @param name
+ *   The pointer to a driver name to be initialized.
+ * @param args
+ *   The pointer to arguments used by driver initialization.
+ * @return
+ *  0 on success, negative on error
+ */
+int rte_vdev_init(const char *name, const char *args);
+
+/**
+ * Uninitalize a driver specified by name.
+ *
+ * @param name
+ *   The pointer to a driver name to be initialized.
+ * @return
+ *  0 on success, negative on error
+ */
+int rte_vdev_uninit(const char *name);
+
 #ifdef __cplusplus
 }
 #endif

commit 3dd8ec4b5ae8af5753185c2766b3655bfc633fd9

drivers/bus/vdev: corrected a typing error in header file
Here I have corrected a potential typing mistake in header file.
while Uninitalizing the driver the comment was mentioning
initializing instead of Uninitalizing in despription of parameter

Fixes: d4a586d29e65 ("bus/vdev: move code from EAL into a new driver")
Cc: jianfeng.tan@intel.com

Signed-off-by: Muhammad Bilal <m.bilal@emumba.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/bus/vdev/rte_bus_vdev.h
@@ -155,7 +155,7 @@ int rte_vdev_init(const char *name, const char *args);
  * Uninitalize a driver specified by name.
  *
  * @param name
- *   The pointer to a driver name to be initialized.
+ *   The pointer to a driver name to be uninitialized.
  * @return
  *  0 on success, negative on error
  */