48c62f1521abd9c776b92a15b133314c20635576
Stephen Hemminger <stephen@networkplumber.org>
2020-07-01 13:23:44 -0700

    examples/l2fwd: replace references to master lcore
    Use term initial lcore instead.
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- examples/l2fwd-event/main.c
+++ examples/l2fwd-event/main.c
@@ -673,7 +673,7 @@ main(int argc, char **argv)
 
 	/* launch per-lcore init on every lcore */
 	rte_eal_mp_remote_launch(l2fwd_launch_one_lcore, rsrc,
-				 SKIP_MASTER);
+				 SKIP_INITIAL);
 	l2fwd_event_print_stats(rsrc);
 	if (rsrc->event_mode) {
 		struct l2fwd_event_resources *evt_rsrc =
