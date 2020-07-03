ovsrobot/dpdk

commit 51f3e107aca23a1cbc1a5ad9fdce7921340307b5

crypto/octeontx2: enable non-byte aligned data
The OCTEON TX2 crypto PMD supports non-byte aligned data as
input for SNOW and ZUC algos. Adding the same to the feature list.

Signed-off-by: Ankur Dwivedi <adwivedi@marvell.com>
Signed-off-by: Anoob Joseph <anoobj@marvell.com>

doc/guides/cryptodevs/features/octeontx2.ini
@@ -13,6 +13,7 @@ OOP SGL In LB  Out     = Y
 OOP SGL In SGL Out     = Y
 RSA PRIV OP KEY QT     = Y
 Symmetric sessionless  = Y
+Non-Byte aligned data  = Y
 
 ;
 ; Supported crypto algorithms of 'octeontx2' crypto driver.

drivers/crypto/octeontx2/otx2_cryptodev.c
@@ -104,6 +104,7 @@ otx2_cpt_pci_probe(struct rte_pci_driver *pci_drv __rte_unused,
 			     RTE_CRYPTODEV_FF_OOP_SGL_IN_SGL_OUT |
 			     RTE_CRYPTODEV_FF_ASYMMETRIC_CRYPTO |
 			     RTE_CRYPTODEV_FF_RSA_PRIV_OP_KEY_QT |
+			     RTE_CRYPTODEV_FF_NON_BYTE_ALIGNED_DATA |
 			     RTE_CRYPTODEV_FF_SYM_SESSIONLESS;
 
 	return 0;

commit cd7a7c2d690f9c3dddb3c012e4e2540423592ce6

crypto/octeontx2: revert non-byte aligned data feature
This reverts commit 51f3e107aca23a1cbc1a5ad9fdce7921340307b5.

For SNOW and ZUC algos the offset value for enryption and decryption
is converted to bytes. Hence RTE_CRYPTODEV_FF_NON_BYTE_ALIGNED_DATA
feature is not supported by the octeontx2 crypto pmd.

Fixes: 51f3e107aca2 ("crypto/octeontx2: enable non-byte aligned data")
Cc: stable@dpdk.org

Signed-off-by: Ankur Dwivedi <adwivedi@marvell.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

doc/guides/cryptodevs/features/octeontx2.ini
@@ -13,7 +13,6 @@ OOP SGL In LB  Out     = Y
 OOP SGL In SGL Out     = Y
 RSA PRIV OP KEY QT     = Y
 Symmetric sessionless  = Y
-Non-Byte aligned data  = Y
 
 ;
 ; Supported crypto algorithms of 'octeontx2' crypto driver.

drivers/crypto/octeontx2/otx2_cryptodev.c
@@ -104,7 +104,6 @@ otx2_cpt_pci_probe(struct rte_pci_driver *pci_drv __rte_unused,
 			     RTE_CRYPTODEV_FF_OOP_SGL_IN_SGL_OUT |
 			     RTE_CRYPTODEV_FF_ASYMMETRIC_CRYPTO |
 			     RTE_CRYPTODEV_FF_RSA_PRIV_OP_KEY_QT |
-			     RTE_CRYPTODEV_FF_NON_BYTE_ALIGNED_DATA |
 			     RTE_CRYPTODEV_FF_SYM_SESSIONLESS;
 
 	return 0;