ovsrobot/dpdk

commit 9c99878aa1b16de26fcce82c112b401766dd910e

log: introduce logtype register macro
Introduce the RTE_LOG_REGISTER macro to avoid the code duplication
in the logtype registration process.

It is a wrapper macro for declaring the logtype, registering it and
setting its level in the constructor context.

Signed-off-by: Jerin Jacob <jerinj@marvell.com>
Acked-by: Adam Dybkowski <adamx.dybkowski@intel.com>
Acked-by: Sachin Saxena <sachin.saxena@nxp.com>
Acked-by: Akhil Goyal <akhil.goyal@nxp.com>

drivers/vdpa/mlx5/mlx5_vdpa.c
@@ -839,15 +839,13 @@ static struct rte_pci_driver mlx5_vdpa_driver = {
 	.drv_flags = 0,
 };
 
+RTE_LOG_REGISTER(mlx5_vdpa_logtype, pmd.vdpa.mlx5, NOTICE)
+
 /**
  * Driver initialization routine.
  */
 RTE_INIT(rte_mlx5_vdpa_init)
 {
-	/* Initialize common log type. */
-	mlx5_vdpa_logtype = rte_log_register("pmd.vdpa.mlx5");
-	if (mlx5_vdpa_logtype >= 0)
-		rte_log_set_level(mlx5_vdpa_logtype, RTE_LOG_NOTICE);
 	if (mlx5_glue)
 		rte_pci_register(&mlx5_vdpa_driver);
 }

lib/librte_graph/graph.c
@@ -579,9 +579,4 @@ rte_graph_max_count(void)
 	return graph_id;
 }
 
-RTE_INIT(rte_graph_init_log)
-{
-	rte_graph_logtype = rte_log_register("lib.graph");
-	if (rte_graph_logtype >= 0)
-		rte_log_set_level(rte_graph_logtype, RTE_LOG_INFO);
-}
+RTE_LOG_REGISTER(rte_graph_logtype, lib.graph, INFO);

lib/librte_rawdev/rte_rawdev.c
@@ -613,11 +613,10 @@ handle_dev_xstats(const char *cmd __rte_unused,
 	return 0;
 }
 
-RTE_INIT(librawdev_init_log)
+RTE_LOG_REGISTER(librawdev_logtype, lib.rawdev, INFO);
+
+RTE_INIT(librawdev_init_telemetry)
 {
-	librawdev_logtype = rte_log_register("lib.rawdev");
-	if (librawdev_logtype >= 0)
-		rte_log_set_level(librawdev_logtype, RTE_LOG_INFO);
 	rte_telemetry_register_cmd("/rawdev/list", handle_dev_list,
 			"Returns list of available rawdev ports. Takes no parameters");
 	rte_telemetry_register_cmd("/rawdev/xstats", handle_dev_xstats,

lib/librte_stack/rte_stack.c
@@ -194,9 +194,4 @@ rte_stack_lookup(const char *name)
 	return r;
 }
 
-RTE_INIT(librte_stack_init_log)
-{
-	stack_logtype = rte_log_register("lib.stack");
-	if (stack_logtype >= 0)
-		rte_log_set_level(stack_logtype, RTE_LOG_NOTICE);
-}
+RTE_LOG_REGISTER(stack_logtype, lib.stack, NOTICE);

commit 2e9c89b426f305e3b8b4a0349eb88101f2bc9c44

log: remove unneeded log type declaration
RTE_LOG_REGISTER macro already declares the the log type.
Remove the unneeded log type declaration.

Fixes: 9c99878aa1b16de2 ("log: introduce logtype register macro")

Reported-by: David Marchand <david.marchand@redhat.com>
Signed-off-by: Jerin Jacob <jerinj@marvell.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/vdpa/mlx5/mlx5_vdpa.c
@@ -48,7 +48,6 @@
 TAILQ_HEAD(mlx5_vdpa_privs, mlx5_vdpa_priv) priv_list =
 					      TAILQ_HEAD_INITIALIZER(priv_list);
 static pthread_mutex_t priv_list_lock = PTHREAD_MUTEX_INITIALIZER;
-int mlx5_vdpa_logtype;
 
 static struct mlx5_vdpa_priv *
 mlx5_vdpa_find_priv_resource_by_vdev(struct rte_vdpa_device *vdev)

lib/librte_graph/graph.c
@@ -18,7 +18,6 @@
 static struct graph_head graph_list = STAILQ_HEAD_INITIALIZER(graph_list);
 static rte_spinlock_t graph_lock = RTE_SPINLOCK_INITIALIZER;
 static rte_graph_t graph_id;
-int rte_graph_logtype;
 
 #define GRAPH_ID_CHECK(id) ID_CHECK(id, graph_id)
 

lib/librte_rawdev/rte_rawdev.c
@@ -34,9 +34,6 @@
 #include "rte_rawdev.h"
 #include "rte_rawdev_pmd.h"
 
-/* dynamic log identifier */
-int librawdev_logtype;
-
 static struct rte_rawdev rte_rawdevices[RTE_RAWDEV_MAX_DEVS];
 
 struct rte_rawdev *rte_rawdevs = rte_rawdevices;

lib/librte_stack/rte_stack.c
@@ -17,8 +17,6 @@
 #include "rte_stack.h"
 #include "stack_pvt.h"
 
-int stack_logtype;
-
 TAILQ_HEAD(rte_stack_list, rte_tailq_entry);
 
 static struct rte_tailq_elem rte_stack_tailq = {