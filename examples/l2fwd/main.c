93976d56d81fef6f4a11a9b501cfe8339b9497a8
Ivan Dyukov <i.dyukov@samsung.com>
2020-07-06 23:37:31 +0300

    examples/l2fwd: new link status print format
    Add usage of rte_eth_link_strf function to example
    applications
    
    Signed-off-by: Ivan Dyukov <i.dyukov@samsung.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- examples/l2fwd/main.c
+++ examples/l2fwd/main.c
@@ -571,6 +571,7 @@ check_all_ports_link_status(uint32_t port_mask)
 	uint8_t count, all_ports_up, print_flag = 0;
 	struct rte_eth_link link;
 	int ret;
+	char link_status_text[60];
 
 	printf("\nChecking link status");
 	fflush(stdout);
@@ -594,14 +595,9 @@ check_all_ports_link_status(uint32_t port_mask)
 			}
 			/* print link status if flag set */
 			if (print_flag == 1) {
-				if (link.link_status)
-					printf(
-					"Port%d Link Up. Speed %u Mbps - %s\n",
-						portid, link.link_speed,
-				(link.link_duplex == ETH_LINK_FULL_DUPLEX) ?
-					("full-duplex") : ("half-duplex"));
-				else
-					printf("Port %d Link Down\n", portid);
+				rte_eth_link_strf(link_status_text, 60, NULL,
+						    &link);
+				printf("Port %d %s", portid, link_status_text);
 				continue;
 			}
 			/* clear all_ports_up flag if any link down */
