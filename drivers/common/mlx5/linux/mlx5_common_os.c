ovsrobot/dpdk

commit 420bbdae89f29744847a166b831c675ad5affd2a

net/mlx5: fix host physical function representor naming
The new kernel adds the names like "pf0" for Host PCI physical
function representor on Bluefield SmartNIC hosts. This patch
provides correct HPF representor recognition over the kernel
versions 5.7 and laters.

The following port naming formats are supported:

  - missing physical port name (no sysfs/netlink key) at all,
    master is assumed

  - decimal digits (for example "12"), representor is
    assumed, the value is the index of attached VF

  - "p" followed by decimal digits, for example "p2", master
    is assumed

  - "pf" followed by PF index, for example "pf0", Host PF
     representor is assumed on SmartNIC systems.

  - "pf" followed by PF index concatenated with "vf" followed by
     VF index, for example "pf0vf1", representor is assumed.
     If index of VF is "-1" it is a special case of Host PF
     representor, this representor must be indexed in devargs
     as 65535, for example representor=[0-3,65535] will
     allow representors for VF0, VF1, VF2, VF3 and for host PF.

Fixes: 79aa430721b1 ("common/mlx5: split common file under Linux directory")
Cc: stable@dpdk.org

Signed-off-by: Viacheslav Ovsiienko <viacheslavo@mellanox.com>
Acked-by: Matan Azrad <matan@mellanox.com>

drivers/common/mlx5/linux/mlx5_common_os.c
@@ -117,6 +117,17 @@ mlx5_translate_port_name(const char *port_name_in,
 		port_info_out->name_type = MLX5_PHYS_PORT_NAME_TYPE_UPLINK;
 		return;
 	}
+	/*
+	 * Check for port-name as a string of the form pf0
+	 * (support kernel ver >= 5.7 for HPF representor on BF).
+	 */
+	sc_items = sscanf(port_name_in, "%c%c%d",
+			  &pf_c1, &pf_c2, &port_info_out->pf_num);
+	if (sc_items == 3 && pf_c1 == 'p' && pf_c2 == 'f') {
+		port_info_out->port_name = -1;
+		port_info_out->name_type = MLX5_PHYS_PORT_NAME_TYPE_PFHPF;
+		return;
+	}
 	/* Check for port-name as a number (support kernel ver < 5.0 */
 	errno = 0;
 	port_info_out->port_name = strtol(port_name_in, &end, 0);

drivers/common/mlx5/linux/mlx5_nl.c
@@ -1145,6 +1145,8 @@ mlx5_nl_check_switch_info(bool num_vf_set,
 		/* Legacy representors naming schema. */
 		switch_info->representor = !num_vf_set;
 		break;
+	case MLX5_PHYS_PORT_NAME_TYPE_PFHPF:
+		/* Fallthrough */
 	case MLX5_PHYS_PORT_NAME_TYPE_PFVF:
 		/* New representors naming schema. */
 		switch_info->representor = 1;

drivers/common/mlx5/mlx5_common.h
@@ -146,6 +146,7 @@ enum mlx5_nl_phys_port_name_type {
 	MLX5_PHYS_PORT_NAME_TYPE_LEGACY, /* before kernel ver < 5.0 */
 	MLX5_PHYS_PORT_NAME_TYPE_UPLINK, /* p0, kernel ver >= 5.0 */
 	MLX5_PHYS_PORT_NAME_TYPE_PFVF, /* pf0vf0, kernel ver >= 5.0 */
+	MLX5_PHYS_PORT_NAME_TYPE_PFHPF, /* pf0, kernel ver >= 5.7, HPF rep */
 	MLX5_PHYS_PORT_NAME_TYPE_UNKNOWN, /* Unrecognized. */
 };
 

drivers/net/mlx5/linux/mlx5_ethdev_os.c
@@ -1091,6 +1091,8 @@ mlx5_sysfs_check_switch_info(bool device_dir,
 		/* Legacy representors naming schema. */
 		switch_info->representor = !device_dir;
 		break;
+	case MLX5_PHYS_PORT_NAME_TYPE_PFHPF:
+		/* Fallthrough */
 	case MLX5_PHYS_PORT_NAME_TYPE_PFVF:
 		/* New representors naming schema. */
 		switch_info->representor = 1;

drivers/net/mlx5/linux/mlx5_os.c
@@ -1491,6 +1491,8 @@ mlx5_os_pci_probe(struct rte_pci_driver *pci_drv __rte_unused,
 					if (list[ns].info.port_name == bd)
 						ns++;
 					break;
+				case MLX5_PHYS_PORT_NAME_TYPE_PFHPF:
+					/* Fallthrough */
 				case MLX5_PHYS_PORT_NAME_TYPE_PFVF:
 					if (list[ns].info.pf_num == bd)
 						ns++;

commit f0737861615ec7a8ae964dec685e2100fedd1c2f

common/mlx5: fix physical port name pattern recognition
This patch makes the Infiniband device physical port name
recognition more strict. Currently mlx5 PMD might recognize
the names like "pf0sf0" erroneously as "pf0" and the wrong
device type (host PF representor) is reported.

The names like "pf0sf0" belong to PCI subfunctions which
is currently not supported by mlx5 PMD and this false
recognition must be eliminated.

Fixes: 420bbdae89f2 ("net/mlx5: fix host physical function representor naming")
Cc: stable@dpdk.org

Signed-off-by: Viacheslav Ovsiienko <viacheslavo@mellanox.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/common/mlx5/linux/mlx5_common_os.c
@@ -89,7 +89,7 @@ void
 mlx5_translate_port_name(const char *port_name_in,
 			 struct mlx5_switch_info *port_info_out)
 {
-	char pf_c1, pf_c2, vf_c1, vf_c2;
+	char pf_c1, pf_c2, vf_c1, vf_c2, eol;
 	char *end;
 	int sc_items;
 
@@ -97,9 +97,9 @@ mlx5_translate_port_name(const char *port_name_in,
 	 * Check for port-name as a string of the form pf0vf0
 	 * (support kernel ver >= 5.0 or OFED ver >= 4.6).
 	 */
-	sc_items = sscanf(port_name_in, "%c%c%d%c%c%d",
+	sc_items = sscanf(port_name_in, "%c%c%d%c%c%d%c",
 			  &pf_c1, &pf_c2, &port_info_out->pf_num,
-			  &vf_c1, &vf_c2, &port_info_out->port_name);
+			  &vf_c1, &vf_c2, &port_info_out->port_name, &eol);
 	if (sc_items == 6 &&
 	    pf_c1 == 'p' && pf_c2 == 'f' &&
 	    vf_c1 == 'v' && vf_c2 == 'f') {
@@ -110,8 +110,8 @@ mlx5_translate_port_name(const char *port_name_in,
 	 * Check for port-name as a string of the form p0
 	 * (support kernel ver >= 5.0, or OFED ver >= 4.6).
 	 */
-	sc_items = sscanf(port_name_in, "%c%d",
-			  &pf_c1, &port_info_out->port_name);
+	sc_items = sscanf(port_name_in, "%c%d%c",
+			  &pf_c1, &port_info_out->port_name, &eol);
 	if (sc_items == 2 && pf_c1 == 'p') {
 		port_info_out->name_type = MLX5_PHYS_PORT_NAME_TYPE_UPLINK;
 		return;
@@ -120,8 +120,8 @@ mlx5_translate_port_name(const char *port_name_in,
 	 * Check for port-name as a string of the form pf0
 	 * (support kernel ver >= 5.7 for HPF representor on BF).
 	 */
-	sc_items = sscanf(port_name_in, "%c%c%d",
-			  &pf_c1, &pf_c2, &port_info_out->pf_num);
+	sc_items = sscanf(port_name_in, "%c%c%d%c",
+			  &pf_c1, &pf_c2, &port_info_out->pf_num, &eol);
 	if (sc_items == 3 && pf_c1 == 'p' && pf_c2 == 'f') {
 		port_info_out->port_name = -1;
 		port_info_out->name_type = MLX5_PHYS_PORT_NAME_TYPE_PFHPF;