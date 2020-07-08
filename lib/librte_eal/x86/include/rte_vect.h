b510b0589e404d43a14ee1d84fa2bb81f35a8d48
Vladimir Medvedkin <vladimir.medvedkin@intel.com>
2020-07-08 21:16:06 +0100

    eal: introduce zmm type for AVX 512-bit
    New data type to manipulate 512 bit AVX values.
    
    Signed-off-by: Vladimir Medvedkin <vladimir.medvedkin@intel.com>
    Acked-by: Konstantin Ananyev <konstantin.ananyev@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_eal/x86/include/rte_vect.h
+++ lib/librte_eal/x86/include/rte_vect.h
@@ -13,6 +13,7 @@
 
 #include <stdint.h>
 #include <rte_config.h>
+#include <rte_common.h>
 #include "generic/rte_vect.h"
 
 #if (defined(__ICC) || \
@@ -89,6 +90,26 @@ __extension__ ({                 \
 	(m.x);                   \
 })
 #endif /* (defined(__ICC) && __ICC < 1210) */
+
+#ifdef __AVX512F__
+
+typedef __m512i __x86_zmm_t;
+
+#define	ZMM_SIZE	(sizeof(__x86_zmm_t))
+#define	ZMM_MASK	(ZMM_SIZE - 1)
+
+typedef union __rte_x86_zmm  {
+	__x86_zmm_t	 z;
+	ymm_t    y[ZMM_SIZE / sizeof(ymm_t)];
+	xmm_t    x[ZMM_SIZE / sizeof(xmm_t)];
+	uint8_t  u8[ZMM_SIZE / sizeof(uint8_t)];
+	uint16_t u16[ZMM_SIZE / sizeof(uint16_t)];
+	uint32_t u32[ZMM_SIZE / sizeof(uint32_t)];
+	uint64_t u64[ZMM_SIZE / sizeof(uint64_t)];
+	double   pd[ZMM_SIZE / sizeof(double)];
+} __rte_aligned(ZMM_SIZE) __rte_x86_zmm_t;
+
+#endif /* __AVX512F__ */
 
 #ifdef __cplusplus
 }
