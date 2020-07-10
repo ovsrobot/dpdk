d6109dac2795fe53420d45a2709112cf217531a7
Joyce Kong <joyce.kong@arm.com>
2020-07-10 10:38:48 +0800

    lib/eal: add a common wrapper for restricted pointers
    The 'restrict' keyword is recognized in C99, while type qulifier
    '__restrict' compiles ok in C with all language levels. This patch
    is to replace the existing 'restrict' with '__rte_restrict' which
    is a common wrapper supported by all compilers.
    
    Signed-off-by: Joyce Kong <joyce.kong@arm.com>
    Reviewed-by: Phil Yang <phil.yang@arm.com>
    Reviewed-by: Ruifeng Wang <ruifeng.wang@arm.com>
    Acked-by: Jerin Jacob <jerinj@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_eal/include/rte_common.h
+++ lib/librte_eal/include/rte_common.h
@@ -104,6 +104,16 @@ typedef uint16_t unaligned_uint16_t;
 #define __rte_unused __attribute__((__unused__))
 
 /**
+ * Define a wrapper for restricted pointers which can be supported
+ * by all compilers.
+ */
+#if !defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L
+#define __rte_restrict __restrict
+#else
+#define __rte_restrict restrict
+#endif
+
+/**
  * definition to mark a variable or function parameter as used so
  * as to avoid a compiler warning
  */
