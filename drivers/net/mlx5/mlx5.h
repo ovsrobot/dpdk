18da8ac7cbee084d1cfa6449f74537781dd46769
Viacheslav Ovsiienko <viacheslavo@mellanox.com>
2020-07-15 06:21:51 +0000

    net/mlx5: add read device clock support
    If send schedule feature is engaged there is the Clock Queue
    created, that reports reliable the current device clock counter
    value. The device clock counter can be read directly from the
    Clock Queue CQE.
    
    Signed-off-by: Viacheslav Ovsiienko <viacheslavo@mellanox.com>
    Acked-by: Matan Azrad <matan@mellanox.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/mlx5/mlx5.h
+++ drivers/net/mlx5/mlx5.h
@@ -1010,5 +1010,6 @@ void mlx5_os_set_reg_mr_cb(mlx5_reg_mr_t *reg_mr_cb,
 
 int mlx5_txpp_start(struct rte_eth_dev *dev);
 void mlx5_txpp_stop(struct rte_eth_dev *dev);
+int mlx5_txpp_read_clock(struct rte_eth_dev *dev, uint64_t *timestamp);
 
 #endif /* RTE_PMD_MLX5_H_ */
