ovsrobot/dpdk

commit e480cf487a0d77ead8fa606532ead1e35eb2426f

net: add MPLS header structure
Add the Mpls header structure in librte_net. It will be used by next
patch that adds the support of Mpls L2 layer in the software packet
type parser.

Signed-off-by: Olivier Matz <olivier.matz@6wind.com>
Reviewed-by: Ferruh Yigit <ferruh.yigit@intel.com>

lib/librte_net/rte_mpls.h
@@ -1,0 +1,42 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2016 6WIND S.A.
+ */
+
+#ifndef _RTE_MPLS_H_
+#define _RTE_MPLS_H_
+
+/**
+ * @file
+ *
+ * MPLS-related defines
+ */
+
+#include <stdint.h>
+#include <rte_byteorder.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * MPLS header.
+ */
+struct mpls_hdr {
+	uint16_t tag_msb;   /**< Label(msb). */
+#if RTE_BYTE_ORDER == RTE_BIG_ENDIAN
+	uint8_t tag_lsb:4;  /**< Label(lsb). */
+	uint8_t tc:3;       /**< Traffic class. */
+	uint8_t bs:1;       /**< Bottom of stack. */
+#else
+	uint8_t bs:1;       /**< Bottom of stack. */
+	uint8_t tc:3;       /**< Traffic class. */
+	uint8_t tag_lsb:4;  /**< label(lsb) */
+#endif
+	uint8_t  ttl;       /**< Time to live. */
+} __attribute__((__packed__));
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* RTE_MPLS_H_ */

lib/librte_net/Makefile
@@ -20,6 +20,6 @@ SRCS-$(CONFIG_RTE_LIBRTE_NET) += rte_arp.c
 SYMLINK-$(CONFIG_RTE_LIBRTE_NET)-include := rte_ip.h rte_tcp.h rte_udp.h rte_esp.h
 SYMLINK-$(CONFIG_RTE_LIBRTE_NET)-include += rte_sctp.h rte_icmp.h rte_arp.h
 SYMLINK-$(CONFIG_RTE_LIBRTE_NET)-include += rte_ether.h rte_gre.h rte_net.h
-SYMLINK-$(CONFIG_RTE_LIBRTE_NET)-include += rte_net_crc.h
+SYMLINK-$(CONFIG_RTE_LIBRTE_NET)-include += rte_net_crc.h rte_mpls.h
 
 include $(RTE_SDK)/mk/rte.lib.mk

lib/librte_net/meson.build
@@ -13,7 +13,8 @@ headers = files('rte_ip.h',
 	'rte_ether.h',
 	'rte_gre.h',
 	'rte_net.h',
-	'rte_net_crc.h')
+	'rte_net_crc.h',
+	'rte_mpls.h')
 
 sources = files('rte_arp.c', 'rte_net.c', 'rte_net_crc.c')
 deps += ['mbuf']

commit e9f8e3094f1f4acf4b8a80bde0d867c3394ea7c3

net: fix compilation with pedantic enabled
when trying to compile rte_mpls with pedantic enabled,
it will complain about bit field defintion.
error: type of bit-field 'bs' is a GCC extension [-Werror=pedantic]
error: type of bit-field 'tc' is a GCC extension [-Werror=pedantic]
error: type of bit-field 'tag_lsb' is a GCC extension [-Werror=pedantic]

This fixes the compilation error.

Fixes: e480cf487a0d ("net: add MPLS header structure")
Cc: olivier.matz@6wind.com
Cc: stable@dpdk.org

Signed-off-by: Raslan Darawsheh <rasland@mellanox.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/librte_net/rte_mpls.h
@@ -24,13 +24,13 @@ extern "C" {
 struct rte_mpls_hdr {
 	uint16_t tag_msb;   /**< Label(msb). */
 #if RTE_BYTE_ORDER == RTE_BIG_ENDIAN
-	uint8_t tag_lsb:4;  /**< Label(lsb). */
-	uint8_t tc:3;       /**< Traffic class. */
-	uint8_t bs:1;       /**< Bottom of stack. */
+	uint32_t tag_lsb:4;  /**< Label(lsb). */
+	uint32_t tc:3;       /**< Traffic class. */
+	uint32_t bs:1;       /**< Bottom of stack. */
 #else
-	uint8_t bs:1;       /**< Bottom of stack. */
-	uint8_t tc:3;       /**< Traffic class. */
-	uint8_t tag_lsb:4;  /**< label(lsb) */
+	uint32_t bs:1;       /**< Bottom of stack. */
+	uint32_t tc:3;       /**< Traffic class. */
+	uint32_t tag_lsb:4;  /**< label(lsb) */
 #endif
 	uint8_t  ttl;       /**< Time to live. */
 } __rte_packed;