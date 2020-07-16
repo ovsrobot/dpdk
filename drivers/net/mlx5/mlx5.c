45a75b61f60b562697207f0390f95fdc805485f1
Bing Zhao <bingz@mellanox.com>
2020-07-16 21:49:58 +0800

    net/mlx5: add eCPRI flex parser capacity check
    If the NIC or the FW does not support the dynamic flex parser,
    it will return error when trying to create the parser for eCRPI.
    Then it is hard to know the detail error reason of the failure.
    Before creating the parser node and the following usage of the
    parser, the capacity bit saved in the HCA_CAP could be used to
    confirm if the dynamic flex parser is supported.
    If no, an error will be returned directly with ENOTSUP to prevent
    the following steps to be executed.
    
    Signed-off-by: Bing Zhao <bingz@mellanox.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/mlx5/mlx5.c
+++ drivers/net/mlx5/mlx5.c
@@ -636,6 +636,11 @@ mlx5_flex_parser_ecpri_alloc(struct rte_eth_dev *dev)
 	uint32_t ids[8];
 	int ret;
 
+	if (!priv->caps.parse_graph_flex_node) {
+		DRV_LOG(ERR, "Dynamic flex parser is not supported "
+			"for device %s.", priv->dev_data->name);
+		return -ENOTSUP;
+	}
 	node.header_length_mode = MLX5_GRAPH_NODE_LEN_FIXED;
 	/* 8 bytes now: 4B common header + 4B message body header. */
 	node.header_length_base_value = 0x8;
