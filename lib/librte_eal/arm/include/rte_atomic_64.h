1df25287e3cefc2665a58bb5f5e3bb943bfd0053
Phil Yang <phil.yang@arm.com>
2020-07-17 13:08:39 +0800

    eal/atomic: add wrapper for C11 atomic thread fence
    Provide a wrapper for __atomic_thread_fence builtins to support
    optimized code for __ATOMIC_SEQ_CST memory order for x86 platforms.
    
    Suggested-by: Honnappa Nagarahalli <honnappa.nagarahalli@arm.com>
    Signed-off-by: Phil Yang <phil.yang@arm.com>
    Reviewed-by: Ola Liljedahl <Ola.Liljedahl@arm.com>
    Acked-by: Konstantin Ananyev <konstantin.ananyev@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_eal/arm/include/rte_atomic_64.h
+++ lib/librte_eal/arm/include/rte_atomic_64.h
@@ -41,6 +41,12 @@ extern "C" {
 
 #define rte_cio_rmb() rte_rmb()
 
+static __rte_always_inline void
+rte_atomic_thread_fence(int mo)
+{
+	__atomic_thread_fence(mo);
+}
+
 /*------------------------ 128 bit atomic operations -------------------------*/
 
 #if defined(__ARM_FEATURE_ATOMICS) || defined(RTE_ARM_FEATURE_ATOMICS)
