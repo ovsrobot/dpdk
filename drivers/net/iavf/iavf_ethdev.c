ea417b9a4c6346505880c2cc3fec2acc12c5415e
SteveX Yang <stevex.yang@intel.com>
2020-08-11 06:29:12 +0000

    net/iavf: set speed to undefined for default case
    During PF/VF link update, a default speed value of 100M will be set
    if get_link_info has failed or speed is unknown.
    
    Consequently if PF is put in no-carrier state, VFs will switch to
    "in carrier" state due to a link up + a link speed set to 100M
    (default value if no speed detected).
    
    To be consistent with linux drivers on which PF and VFs are in
    same carrier state, sets default speed to undefined (instead of 100M)
    and updates a link status of VF only if link is up and speed is
    different from undefined.
    
    Fixes: 48de41ca11f0 ("net/avf: enable link status update")
    
    Signed-off-by: SteveX Yang <stevex.yang@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/iavf/iavf_ethdev.c
+++ drivers/net/iavf/iavf_ethdev.c
@@ -620,8 +620,9 @@ iavf_dev_link_update(struct rte_eth_dev *dev,
 	}
 
 	new_link.link_duplex = ETH_LINK_FULL_DUPLEX;
-	new_link.link_status = vf->link_up ? ETH_LINK_UP :
-					     ETH_LINK_DOWN;
+	new_link.link_status = (vf->link_up &&
+				new_link.link_speed != ETH_SPEED_NUM_NONE)
+				? ETH_LINK_UP : ETH_LINK_DOWN;
 	new_link.link_autoneg = !(dev->data->dev_conf.link_speeds &
 				ETH_LINK_SPEED_FIXED);
 
