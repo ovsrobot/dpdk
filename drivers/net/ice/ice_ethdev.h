762ad2ee1436ce5cadd3f22bf0c4a58fad8dbbbc
Guinan Sun <guinanx.sun@intel.com>
2020-08-26 07:54:59 +0000

    net/ice: add flow director enabled switch value
    The commit adds fdir_enabled flag into ice_adapter structure
    to identify if fdir id is active. Rx data path can be benefit if
    fdir id parsing is not needed, especially in vector path.
    
    Signed-off-by: Guinan Sun <guinanx.sun@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/ice/ice_ethdev.h
+++ drivers/net/ice/ice_ethdev.h
@@ -285,6 +285,7 @@ struct ice_fdir_filter_conf {
 	struct rte_flow_action_count act_count;
 
 	uint64_t input_set;
+	uint32_t mark_flag;
 };
 
 #define ICE_MAX_FDIR_FILTER_NUM		(1024 * 16)
@@ -463,6 +464,7 @@ struct ice_adapter {
 	bool is_safe_mode;
 	struct ice_devargs devargs;
 	enum ice_pkg_type active_pkg_type; /* loaded ddp package type */
+	uint16_t fdir_ref_cnt;
 };
 
 struct ice_vsi_vlan_pvid_info {
