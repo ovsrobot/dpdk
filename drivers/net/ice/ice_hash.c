ovsrobot/dpdk

commit e7cc68c70736d6f6c901ea1a64ff90e163b85286

net/ice: fix GTPU TEID hash
Refine gtpu teid hash rule mapping for GTPU_IP/GTPU_EH/GTPU_DWN/GTPU_UP.

Fixes: 37e444b77814 ("net/ice: support hash for GTPU protocols")

Signed-off-by: Jeff Guo <jia.guo@intel.com>
Acked-by: Qi Zhang <qi.z.zhang@intel.com>

drivers/net/ice/ice_hash.c
@@ -479,6 +479,8 @@ struct ice_hash_match_type ice_hash_type_list[] = {
 		BIT_ULL(ICE_FLOW_FIELD_IDX_L2TPV3_SESS_ID)},
 	{ETH_RSS_PFCP,
 		BIT_ULL(ICE_FLOW_FIELD_IDX_PFCP_SEID)},
+	{ETH_RSS_GTPU,
+		BIT_ULL(ICE_FLOW_FIELD_IDX_GTPU_IP_TEID)},
 	/* IPV4 */
 	{ETH_RSS_IPV4 | ETH_RSS_L3_SRC_ONLY,
 		BIT_ULL(ICE_FLOW_FIELD_IDX_IPV4_SA)},
@@ -1148,21 +1150,25 @@ ice_hash_parse_action(struct ice_pattern_match_item *pattern_match_item,
 				BIT_ULL(ICE_FLOW_FIELD_IDX_NAT_T_ESP_SPI);
 			}
 
-			/* update hash field for gtpu-ip and gtpu-eh. */
-			if (rss_type != ETH_RSS_GTPU)
-				break;
-			else if (hash_meta->pkt_hdr & ICE_FLOW_SEG_HDR_GTPU_IP)
-				hash_meta->hash_flds |=
-				BIT_ULL(ICE_FLOW_FIELD_IDX_GTPU_IP_TEID);
-			else if (hash_meta->pkt_hdr & ICE_FLOW_SEG_HDR_GTPU_EH)
+			/* update hash field for gtpu eh/gtpu dwn/gtpu up. */
+			if (hash_meta->pkt_hdr & ICE_FLOW_SEG_HDR_GTPU_EH) {
+				hash_meta->hash_flds &=
+				~(BIT_ULL(ICE_FLOW_FIELD_IDX_GTPU_IP_TEID));
 				hash_meta->hash_flds |=
 				BIT_ULL(ICE_FLOW_FIELD_IDX_GTPU_EH_TEID);
-			else if (hash_meta->pkt_hdr & ICE_FLOW_SEG_HDR_GTPU_DWN)
+			} else if (hash_meta->pkt_hdr &
+				   ICE_FLOW_SEG_HDR_GTPU_DWN) {
+				hash_meta->hash_flds &=
+				~(BIT_ULL(ICE_FLOW_FIELD_IDX_GTPU_IP_TEID));
 				hash_meta->hash_flds |=
 				BIT_ULL(ICE_FLOW_FIELD_IDX_GTPU_DWN_TEID);
-			else if (hash_meta->pkt_hdr & ICE_FLOW_SEG_HDR_GTPU_UP)
+			} else if (hash_meta->pkt_hdr &
+				   ICE_FLOW_SEG_HDR_GTPU_UP) {
+				hash_meta->hash_flds &=
+				~(BIT_ULL(ICE_FLOW_FIELD_IDX_GTPU_IP_TEID));
 				hash_meta->hash_flds |=
 				BIT_ULL(ICE_FLOW_FIELD_IDX_GTPU_UP_TEID);
+			}
 
 			break;
 

commit cc6540d225d0b6a1667508a0fbdd140b5e923c87

net/ice: fix the hash parser
GTPU TEID hash should only be enabled when ETH_RSS_GTPU is required.
And the hash parser should not restrict the combined usage of protocol.

Fixes: e7cc68c70736 ("net/ice: fix GTPU TEID hash")
Signed-off-by: Jeff Guo <jia.guo@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/ice/ice_hash.c
@@ -1141,7 +1141,7 @@ ice_hash_parse_action(struct ice_pattern_match_item *pattern_match_item,
 			}
 
 			/* update hash field for nat-t esp. */
-			if (rss_type == ETH_RSS_ESP &&
+			if (rss_type & ETH_RSS_ESP &&
 			    (m->eth_rss_hint & ETH_RSS_NONFRAG_IPV4_UDP ||
 			     m->eth_rss_hint & ETH_RSS_NONFRAG_IPV6_UDP)) {
 				hash_meta->hash_flds &=
@@ -1151,7 +1151,10 @@ ice_hash_parse_action(struct ice_pattern_match_item *pattern_match_item,
 			}
 
 			/* update hash field for gtpu eh/gtpu dwn/gtpu up. */
-			if (hash_meta->pkt_hdr & ICE_FLOW_SEG_HDR_GTPU_EH) {
+			if (!(rss_type & ETH_RSS_GTPU)) {
+				break;
+			} else if (hash_meta->pkt_hdr &
+				   ICE_FLOW_SEG_HDR_GTPU_EH) {
 				hash_meta->hash_flds &=
 				~(BIT_ULL(ICE_FLOW_FIELD_IDX_GTPU_IP_TEID));
 				hash_meta->hash_flds |=