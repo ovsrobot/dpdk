90bd089c0d453c0b0e924103516c57edc25311eb
Ruifeng Wang <ruifeng.wang@arm.com>
2020-09-16 10:21:08 +0200

    net/bnxt: add support for aarch32
    Expand vector PMD support to aarch32.
    
    Signed-off-by: Ruifeng Wang <ruifeng.wang@arm.com>
    Acked-by: Ajit Khaparde <ajit.khaparde@broadcom.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/bnxt/bnxt_txr.h
+++ drivers/net/bnxt/bnxt_txr.h
@@ -59,7 +59,7 @@ uint16_t bnxt_xmit_pkts(void *tx_queue, struct rte_mbuf **tx_pkts,
 			       uint16_t nb_pkts);
 uint16_t bnxt_dummy_xmit_pkts(void *tx_queue, struct rte_mbuf **tx_pkts,
 			      uint16_t nb_pkts);
-#if defined(RTE_ARCH_X86) || defined(RTE_ARCH_ARM64)
+#if defined(RTE_ARCH_X86) || defined(RTE_ARCH_ARM64) || defined(RTE_ARCH_ARM)
 uint16_t bnxt_xmit_pkts_vec(void *tx_queue, struct rte_mbuf **tx_pkts,
 			    uint16_t nb_pkts);
 #endif
