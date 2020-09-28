8dc27fb4ad4ab6cfe6e4ff025ba9c95fa5d1c265
Thomas Monjalon <thomas@monjalon.net>
2020-09-29 01:14:36 +0200

    ethdev: reset all when releasing a port
    The function rte_eth_dev_release_port() was resetting partially
    the struct rte_eth_dev. The drivers were completing it
    with more pointers set to NULL in the close or remove operations.
    
    A full memset is done so most of those assignments become useless.
    
    Signed-off-by: Thomas Monjalon <thomas@monjalon.net>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_ethdev/rte_ethdev.c
+++ lib/librte_ethdev/rte_ethdev.c
@@ -554,10 +554,6 @@ rte_eth_dev_release_port(struct rte_eth_dev *eth_dev)
 
 	rte_spinlock_lock(&rte_eth_dev_shared_data->ownership_lock);
 
-	eth_dev->state = RTE_ETH_DEV_UNUSED;
-	eth_dev->device = NULL;
-	eth_dev->intr_handle = NULL;
-
 	if (rte_eal_process_type() == RTE_PROC_PRIMARY) {
 		rte_free(eth_dev->data->rx_queues);
 		rte_free(eth_dev->data->tx_queues);
@@ -566,6 +562,7 @@ rte_eth_dev_release_port(struct rte_eth_dev *eth_dev)
 		rte_free(eth_dev->data->dev_private);
 		memset(eth_dev->data, 0, sizeof(struct rte_eth_dev_data));
 	}
+	memset(eth_dev, 0, sizeof(struct rte_eth_dev));
 
 	rte_spinlock_unlock(&rte_eth_dev_shared_data->ownership_lock);
 
