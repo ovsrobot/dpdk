c0b7202a824abc40e38a2519441fba782ea90161
jiawenwu <jiawenwu@trustnetic.com>
2020-10-05 20:08:53 +0800

    net/txgbe: add process mailbox operation
    Add check operation for vf function level reset, mailbox messages and ack from vf. Waiting to process the messages.
    
    Signed-off-by: jiawenwu <jiawenwu@trustnetic.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/txgbe/rte_pmd_txgbe.c
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2015-2020
+ */
+
+#include <rte_ethdev_driver.h>
+
+#include "base/txgbe.h"
+#include "txgbe_ethdev.h"
+#include "rte_pmd_txgbe.h"
+
+
