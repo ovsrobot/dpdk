6d93917019d40ca381c6ee7d6e96e1cf19fccc4d
Dekel Peled <dekelp@nvidia.com>
2020-10-07 14:37:40 +0300

    ethdev: support query of AGE action
    Existing API supports AGE action to monitor the aging of a flow.
    This patch implements RFC [1], introducing the response format for query
    of an AGE action.
    Application will be able to query the AGE action state.
    The response will be returned in the format implemented here.
    
    [1] https://mails.dpdk.org/archives/dev/2020-September/180061.html
    
    Signed-off-by: Dekel Peled <dekelp@nvidia.com>
    Acked-by: Matan Azrad <matan@nvidia.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_ethdev/rte_flow.h
+++ lib/librte_ethdev/rte_flow.h
@@ -2130,6 +2130,7 @@ enum rte_flow_action_type {
 	 * See struct rte_flow_action_age.
 	 * See function rte_flow_get_aged_flows
 	 * see enum RTE_ETH_EVENT_FLOW_AGED
+	 * See struct rte_flow_query_age
 	 */
 	RTE_FLOW_ACTION_TYPE_AGE,
 };
@@ -2191,6 +2192,19 @@ struct rte_flow_action_age {
 	uint32_t reserved:8; /**< Reserved, must be zero. */
 	void *context;
 		/**< The user flow context, NULL means the rte_flow pointer. */
+};
+
+/**
+ * RTE_FLOW_ACTION_TYPE_AGE (query)
+ *
+ * Query structure to retrieve the aging status information of a
+ * shared AGE action, or a flow rule using the AGE action.
+ */
+struct rte_flow_query_age {
+	uint32_t reserved:6; /**< Reserved, must be zero. */
+	uint32_t aged:1; /**< 1 if aging timeout expired, 0 otherwise. */
+	uint32_t last_hit_time_valid:1; /**< sec_since_last_hit value valid. */
+	uint32_t sec_since_last_hit:24; /**< Seconds since last traffic hit. */
 };
 
 /**
