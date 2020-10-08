a76b259dd1ae16f51c3ce659e915931041104773
Rohit Raj <rohit.raj@nxp.com>
2020-10-08 21:00:43 +0530

    eal: add API for bus close
    As per the current code we have API for bus probe, but the
    bus close API is missing. This breaks the multi process
    scenarios as objects are not cleaned while terminating the
    secondary processes.
    
    This patch adds a new API rte_bus_close() for cleanup of
    bus objects which were acquired during probe.
    
    Signed-off-by: Rohit Raj <rohit.raj@nxp.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_eal/linux/eal.c
+++ lib/librte_eal/linux/eal.c
@@ -1358,6 +1358,7 @@ rte_eal_cleanup(void)
 
 	if (rte_eal_process_type() == RTE_PROC_PRIMARY)
 		rte_memseg_walk(mark_freeable, NULL);
+	rte_bus_close();
 	rte_service_finalize();
 	rte_mp_channel_cleanup();
 	rte_trace_save();
