2e815e84d43040cdfde074dd8e85e97cee93217f
Ciara Power <ciara.power@intel.com>
2020-10-13 11:38:12 +0100

    net/virtio: add checks for max SIMD bitwidth
    When choosing a vector path to take, an extra condition must be
    satisfied to ensure the max SIMD bitwidth allows for the CPU enabled
    path.
    
    Cc: Maxime Coquelin <maxime.coquelin@redhat.com>
    Cc: Chenbo Xia <chenbo.xia@intel.com>
    Cc: Zhihong Wang <zhihong.wang@intel.com>
    
    Signed-off-by: Ciara Power <ciara.power@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/virtio/virtio_ethdev.c
+++ drivers/net/virtio/virtio_ethdev.c
@@ -2313,7 +2313,8 @@ virtio_dev_configure(struct rte_eth_dev *dev)
 		if ((hw->use_vec_rx || hw->use_vec_tx) &&
 		    (!rte_cpu_get_flag_enabled(RTE_CPUFLAG_AVX512F) ||
 		     !vtpci_with_feature(hw, VIRTIO_F_IN_ORDER) ||
-		     !vtpci_with_feature(hw, VIRTIO_F_VERSION_1))) {
+		     !vtpci_with_feature(hw, VIRTIO_F_VERSION_1) ||
+		     rte_get_max_simd_bitwidth() < RTE_SIMD_512)) {
 			PMD_DRV_LOG(INFO,
 				"disabled packed ring vectorized path for requirements not met");
 			hw->use_vec_rx = 0;
@@ -2364,6 +2365,12 @@ virtio_dev_configure(struct rte_eth_dev *dev)
 					   DEV_RX_OFFLOAD_VLAN_STRIP)) {
 				PMD_DRV_LOG(INFO,
 					"disabled split ring vectorized rx for offloading enabled");
+				hw->use_vec_rx = 0;
+			}
+
+			if (rte_get_max_simd_bitwidth() < RTE_SIMD_128) {
+				PMD_DRV_LOG(INFO,
+					"disabled split ring vectorized rx, max SIMD bitwidth too low");
 				hw->use_vec_rx = 0;
 			}
 		}
