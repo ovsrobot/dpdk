d2985d94c9f69635456adadc21aa0a90ef3388d7
SteveX Yang <stevex.yang@intel.com>
2020-10-14 09:19:45 +0000

    net/iavf: fix max mtu size packets with vlan tag cannot be received by default
    when application presets the max rx packet length and expected mtu at
    the same time, driver need identify if the preset max frame size can
    hold mtu data and Ether overhead completely.
    
    if not, adjust the max frame size via mtu_set ops within dev_configure.
    
    Fixes: 02d212ca3125 ("net/iavf: rename remaining avf strings")
    
    Signed-off-by: SteveX Yang <stevex.yang@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/iavf/iavf_ethdev.c
+++ drivers/net/iavf/iavf_ethdev.c
@@ -248,6 +248,8 @@ iavf_dev_configure(struct rte_eth_dev *dev)
 		IAVF_DEV_PRIVATE_TO_ADAPTER(dev->data->dev_private);
 	struct iavf_info *vf =  IAVF_DEV_PRIVATE_TO_VF(ad);
 	struct rte_eth_conf *dev_conf = &dev->data->dev_conf;
+	uint32_t frame_size = dev->data->mtu + IAVF_ETH_OVERHEAD;
+	int ret;
 
 	ad->rx_bulk_alloc_allowed = true;
 	/* Initialize to TRUE. If any of Rx queues doesn't meet the
@@ -258,6 +260,16 @@ iavf_dev_configure(struct rte_eth_dev *dev)
 
 	if (dev->data->dev_conf.rxmode.mq_mode & ETH_MQ_RX_RSS_FLAG)
 		dev->data->dev_conf.rxmode.offloads |= DEV_RX_OFFLOAD_RSS_HASH;
+
+	/**
+	 * Reset the max frame size via mtu_set ops if preset max frame
+	 * cannot hold MTU data and Ether overhead.
+	 */
+	if (frame_size > dev->data->dev_conf.rxmode.max_rx_pkt_len) {
+		ret = iavf_dev_mtu_set(dev, dev->data->mtu);
+		if (ret != 0)
+			return ret;
+	}
 
 	/* Vlan stripping setting */
 	if (vf->vf_res->vf_cap_flags & VIRTCHNL_VF_OFFLOAD_VLAN) {
