760ac6e84394b876ba38148cd1b784d8e0f16d3f
Honnappa Nagarahalli <honnappa.nagarahalli@arm.com>
2020-10-22 23:43:43 -0500

    test/ring: add stress tests for zero copy APIs
    Add stress tests for zero copy API.
    
    Signed-off-by: Honnappa Nagarahalli <honnappa.nagarahalli@arm.com>
    Reviewed-by: Dharmik Thakkar <dharmik.thakkar@arm.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ app/test/test_ring_st_peek_stress_zc.c
@@ -0,0 +1,65 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2020 Arm Limited
+ */
+
+#include "test_ring.h"
+#include "test_ring_stress_impl.h"
+#include <rte_ring_elem.h>
+
+static inline uint32_t
+_st_ring_dequeue_bulk(struct rte_ring *r, void **obj, uint32_t n,
+	uint32_t *avail)
+{
+	uint32_t m;
+	struct rte_ring_zc_data zcd;
+
+	static rte_spinlock_t lck = RTE_SPINLOCK_INITIALIZER;
+
+	rte_spinlock_lock(&lck);
+
+	m = rte_ring_dequeue_zc_bulk_start(r, n, &zcd, avail);
+	n = (m == n) ? n : 0;
+	if (n != 0) {
+		/* Copy the data from the ring */
+		test_ring_copy_from(&zcd, obj, -1, n);
+		rte_ring_dequeue_zc_finish(r, n);
+	}
+
+	rte_spinlock_unlock(&lck);
+	return n;
+}
+
+static inline uint32_t
+_st_ring_enqueue_bulk(struct rte_ring *r, void * const *obj, uint32_t n,
+	uint32_t *free)
+{
+	uint32_t m;
+	struct rte_ring_zc_data zcd;
+
+	static rte_spinlock_t lck = RTE_SPINLOCK_INITIALIZER;
+
+	rte_spinlock_lock(&lck);
+
+	m = rte_ring_enqueue_zc_bulk_start(r, n, &zcd, free);
+	n = (m == n) ? n : 0;
+	if (n != 0) {
+		/* Copy the data from the ring */
+		test_ring_copy_to(&zcd, obj, -1, n);
+		rte_ring_enqueue_zc_finish(r, n);
+	}
+
+	rte_spinlock_unlock(&lck);
+	return n;
+}
+
+static int
+_st_ring_init(struct rte_ring *r, const char *name, uint32_t num)
+{
+	return rte_ring_init(r, name, num, RING_F_SP_ENQ | RING_F_SC_DEQ);
+}
+
+const struct test test_ring_st_peek_stress_zc = {
+	.name = "ST_PEEK_ZC",
+	.nb_case = RTE_DIM(tests),
+	.cases = tests,
+};
