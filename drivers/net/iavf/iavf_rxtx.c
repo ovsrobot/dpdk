afa02e7e43485e5c42dc5aa157c6b624da7783d2
Wenzhuo Lu <wenzhuo.lu@intel.com>
2020-10-28 13:14:59 +0800

    net/iavf: enable AVX512 for flexible RX
    To enhance the per-core performance, this patch adds some AVX512
    instructions to the data path to handle the flexible RX descriptors.
    
    Signed-off-by: Wenzhuo Lu <wenzhuo.lu@intel.com>
    Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>
    Signed-off-by: Leyi Rong <leyi.rong@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/iavf/iavf_rxtx.c
+++ drivers/net/iavf/iavf_rxtx.c
@@ -2136,6 +2136,11 @@ iavf_set_rx_function(struct rte_eth_dev *dev)
 				dev->rx_pkt_burst = use_avx2 ?
 					iavf_recv_scattered_pkts_vec_avx2_flex_rxd :
 					iavf_recv_scattered_pkts_vec_flex_rxd;
+#ifdef CC_AVX512_SUPPORT
+				if (use_avx512)
+					dev->rx_pkt_burst =
+						iavf_recv_scattered_pkts_vec_avx512_flex_rxd;
+#endif
 			} else {
 				dev->rx_pkt_burst = use_avx2 ?
 					iavf_recv_scattered_pkts_vec_avx2 :
@@ -2155,6 +2160,11 @@ iavf_set_rx_function(struct rte_eth_dev *dev)
 				dev->rx_pkt_burst = use_avx2 ?
 					iavf_recv_pkts_vec_avx2_flex_rxd :
 					iavf_recv_pkts_vec_flex_rxd;
+#ifdef CC_AVX512_SUPPORT
+				if (use_avx512)
+					dev->rx_pkt_burst =
+						iavf_recv_pkts_vec_avx512_flex_rxd;
+#endif
 			} else {
 				dev->rx_pkt_burst = use_avx2 ?
 					iavf_recv_pkts_vec_avx2 :
