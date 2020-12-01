2960fad597a523b8e84b31f1be09f76ba77e3952
Nithin Dabilpuram <ndabilpuram@marvell.com>
2020-12-02 01:03:02 +0530

    test: change external memory test to use system page sz
    Currently external memory test uses 4K page size.
    VFIO DMA mapping works only with system page granularity.
    
    Earlier it was working because all the contiguous mappings
    were coalesced and mapped in one-go which ended up becoming
    a lot bigger page. Now that VFIO DMA mappings both in IOVA as VA
    and IOVA as PA mode, are being done at memseg list granularity,
    we need to use system page size.
    
    Signed-off-by: Nithin Dabilpuram <ndabilpuram@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- app/test/test_external_mem.c
+++ app/test/test_external_mem.c
@@ -532,8 +532,8 @@ fail:
 static int
 test_external_mem(void)
 {
+	size_t pgsz = rte_mem_page_size();
 	size_t len = EXTERNAL_MEM_SZ;
-	size_t pgsz = RTE_PGSIZE_4K;
 	rte_iova_t iova[len / pgsz];
 	void *addr;
 	int ret, n_pages;
