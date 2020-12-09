019d07df378b6323f49e3a124e7255aa18cc5301
Steve Yang <stevex.yang@intel.com>
2020-12-09 03:16:24 +0000

    net/octeontx: fix the jumbo frame flag condition for mtu set
    The jumbo frame uses the 'RTE_ETHER_MAX_LEN' as boundary condition, but
    the Ether overhead is larger than 18 when it supports dual VLAN tags.
    That will cause the jumbo flag rx offload is wrong when MTU size is
    'RTE_ETHER_MTU'.
    
    This fix will change the boundary condition with 'RTE_ETHER_MTU'.
    
    Fixes: 3151e6a687a3 ("net/octeontx: support MTU")
    
    Signed-off-by: Steve Yang <stevex.yang@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/octeontx/octeontx_ethdev.c
+++ drivers/net/octeontx/octeontx_ethdev.c
@@ -552,7 +552,7 @@ octeontx_dev_mtu_set(struct rte_eth_dev *eth_dev, uint16_t mtu)
 	if (rc)
 		return rc;
 
-	if (frame_size > RTE_ETHER_MAX_LEN)
+	if (mtu > RTE_ETHER_MTU)
 		nic->rx_offloads |= DEV_RX_OFFLOAD_JUMBO_FRAME;
 	else
 		nic->rx_offloads &= ~DEV_RX_OFFLOAD_JUMBO_FRAME;
