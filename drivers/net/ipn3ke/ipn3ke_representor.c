44fff91ff7b3f4b2cfec2ae03e1254f63f5a0d11
Steve Yang <stevex.yang@intel.com>
2020-12-09 03:16:23 +0000

    net/ipn3ke: fix the jumbo frame flag condition for mtu set
    The jumbo frame uses the 'RTE_ETHER_MAX_LEN' as boundary condition, but
    the Ether overhead is larger than 18 when it supports dual VLAN tags.
    That will cause the jumbo flag rx offload is wrong when MTU size is
    'RTE_ETHER_MTU'.
    
    This fix will change the boundary condition with 'RTE_ETHER_MTU'.
    
    Fixes: 70d6b7f550f4 ("net/ipn3ke: add representor")
    
    Signed-off-by: Steve Yang <stevex.yang@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/ipn3ke/ipn3ke_representor.c
+++ drivers/net/ipn3ke/ipn3ke_representor.c
@@ -2801,7 +2801,7 @@ ipn3ke_rpst_mtu_set(struct rte_eth_dev *ethdev, uint16_t mtu)
 		return -EBUSY;
 	}
 
-	if (frame_size > RTE_ETHER_MAX_LEN)
+	if (mtu > RTE_ETHER_MTU)
 		dev_data->dev_conf.rxmode.offloads |=
 			(uint64_t)(DEV_RX_OFFLOAD_JUMBO_FRAME);
 	else
