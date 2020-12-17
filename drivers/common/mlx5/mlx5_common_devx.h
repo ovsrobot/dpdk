5dd333c63e53bfc00c441afd057f8770e4e7243a
Michael Baum <michaelba@nvidia.com>
2020-12-17 11:44:28 +0000

    common/mlx5: share DevX SQ creation
    The SQ object in DevX is created in several places and in several
    different drivers.
    In all places almost all the details are the same, and in particular the
    allocations of the required resources.
    
    Add a structure that contains all the resources, and provide creation
    and release functions for it.
    
    Signed-off-by: Michael Baum <michaelba@nvidia.com>
    Acked-by: Matan Azrad <matan@nvidia.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/common/mlx5/mlx5_common_devx.h
+++ drivers/common/mlx5/mlx5_common_devx.h
@@ -18,11 +18,27 @@ struct mlx5_devx_cq {
 	volatile uint32_t *db_rec; /* The CQ doorbell record. */
 };
 
+/* DevX Send Queue structure. */
+struct mlx5_devx_sq {
+	struct mlx5_devx_obj *sq; /* The SQ DevX object. */
+	struct mlx5dv_devx_umem *umem_obj; /* The SQ umem object. */
+	union {
+		volatile void *umem_buf;
+		volatile struct mlx5_wqe *wqes; /* The SQ ring buffer. */
+	};
+	volatile uint32_t *db_rec; /* The SQ doorbell record. */
+};
+
+
 /* mlx5_common_devx.c */
 
 void mlx5_devx_cq_destroy(struct mlx5_devx_cq *cq);
 int mlx5_devx_cq_create(void *ctx, struct mlx5_devx_cq *cq_obj,
-			uint16_t log_desc_n, struct mlx5_devx_cq_attr *attr,
-			int socket);
+			uint16_t log_desc_n,
+			struct mlx5_devx_cq_attr *attr, int socket);
+void mlx5_devx_sq_destroy(struct mlx5_devx_sq *sq);
+int mlx5_devx_sq_create(void *ctx, struct mlx5_devx_sq *sq_obj,
+			uint16_t log_wqbb_n,
+			struct mlx5_devx_create_sq_attr *attr, int socket);
 
 #endif /* RTE_PMD_MLX5_COMMON_DEVX_H_ */
