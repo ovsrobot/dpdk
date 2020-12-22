e1f549f74e65caa8dd45d028d5997de8f77ece43
Maxime Coquelin <maxime.coquelin@redhat.com>
2020-12-22 14:56:57 +0100

    vhost: move dirty logging cache out of the virtqueue
    This patch moves the per-virtqueue's dirty logging cache
    out of the virtqueue struct, by allocating it dynamically
    only when live-migration is enabled.
    
    It saves 8 cachelines in vhost_virtqueue struct.
    
    Signed-off-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_vhost/vhost.c
+++ lib/librte_vhost/vhost.c
@@ -144,6 +144,10 @@ __vhost_log_cache_sync(struct virtio_net *dev, struct vhost_virtqueue *vq)
 	if (unlikely(!dev->log_base))
 		return;
 
+	/* No cache, nothing to sync */
+	if (unlikely(!vq->log_cache))
+		return;
+
 	rte_smp_wmb();
 
 	log_base = (unsigned long *)(uintptr_t)dev->log_base;
@@ -175,6 +179,14 @@ vhost_log_cache_page(struct virtio_net *dev, struct vhost_virtqueue *vq,
 	uint32_t bit_nr = page % (sizeof(unsigned long) << 3);
 	uint32_t offset = page / (sizeof(unsigned long) << 3);
 	int i;
+
+	if (unlikely(!vq->log_cache)) {
+		/* No logging cache allocated, write dirty log map directly */
+		rte_smp_wmb();
+		vhost_log_page((uint8_t *)(uintptr_t)dev->log_base, page);
+
+		return;
+	}
 
 	for (i = 0; i < vq->log_cache_nb_elem; i++) {
 		struct log_cache_entry *elem = vq->log_cache + i;
