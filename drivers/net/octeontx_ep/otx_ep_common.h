704a60fe72acafd092bf51bada3238dd648de1e3
Nalla Pradeep <pnalla@marvell.com>
2020-12-31 07:22:35 +0000

    net/octeontx_ep: add device init and uninit
    Add basic init and uninit function which includes
    initializing fields of ethdev private structure.
    
    Signed-off-by: Nalla Pradeep <pnalla@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/octeontx_ep/otx_ep_common.h
+++ drivers/net/octeontx_ep/otx_ep_common.h
@@ -4,11 +4,33 @@
 #ifndef _OTX_EP_COMMON_H_
 #define _OTX_EP_COMMON_H_
 
+#define otx_ep_printf(level, fmt, args...)		\
+	rte_log(RTE_LOG_ ## level, RTE_LOGTYPE_PMD,		\
+		 fmt, ##args)
+
+#define otx_ep_info(fmt, args...)				\
+	otx_ep_printf(INFO, fmt, ##args)
+
+#define otx_ep_err(fmt, args...)				\
+	otx_ep_printf(ERR, fmt, ##args)
+
+#define otx_ep_dbg(fmt, args...)				\
+	otx_ep_printf(DEBUG, fmt, ##args)
+
 /* OTX_EP EP VF device data structure */
 struct otx_ep_device {
 	/* PCI device pointer */
 	struct rte_pci_device *pdev;
+	uint16_t chip_id;
+	uint16_t vf_num;
 
 	struct rte_eth_dev *eth_dev;
+
+	int port_id;
+
+	/* Memory mapped h/w address */
+	uint8_t *hw_addr;
+
+	int port_configured;
 };
 #endif  /* _OTX_EP_COMMON_H_ */
