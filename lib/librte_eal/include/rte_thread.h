acb57cd53d5230e6f18a50c03c0563e0450ddeaa
Tal Shnaiderman <talshn@nvidia.com>
2021-01-05 19:06:35 +0200

    eal: add generic thread-local-storage functions
    Add support for TLS functionality in EAL.
    
    The following functions are added:
    rte_thread_tls_key_create - function to create a TLS data key.
    rte_thread_tls_key_delete - function to delete a TLS data key.
    rte_thread_tls_value_set - function to set value bound to the TLS key
    rte_thread_tls_value_get - function to get value bound to the TLS key
    
    TLS key will be defined by the new type rte_tls_key.
    
    The API Allocates the thread local storage (TLS) key.
    Any thread of the process can subsequently use this key
    to store and retrieve values that are local to the thread.
    
    Those functions are added in addition to TLS capability
    in rte_per_lcore.h to allow user to use the thread reasouce
    destruction capability in rte_thread_tls_key_create.
    
    Windows implementation is under librte_eal/windows and
    implemented using WIN32 API for Windows only.
    
    common implementation is under librte_eal/common and
    implemented using pthread for UNIX compilation.
    
    Signed-off-by: Tal Shnaiderman <talshn@nvidia.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_eal/include/rte_thread.h
+++ lib/librte_eal/include/rte_thread.h
@@ -16,6 +16,11 @@
  */
 
 /**
+ * Opaque pointer for TLS key.
+ */
+typedef struct eal_tls_key *rte_tls_key;
+
+/**
  * Set core affinity of the current thread.
  * Support both EAL and non-EAL thread and update TLS.
  *
@@ -35,5 +40,68 @@ int rte_thread_set_affinity(rte_cpuset_t *cpusetp);
  *
  */
 void rte_thread_get_affinity(rte_cpuset_t *cpusetp);
+
+/**
+ * Function to create a TLS data key visible to all threads in the process
+ * function need to be called once to create a key usable by all threads.
+ * rte_tls_key is an opaque pointer used to store the allocated key.
+ * which is later used to get/set a value.
+ * and optional destructor can be set to be called when a thread expires.
+ *
+ * @param key
+ *   Pointer to store the allocated rte_tls_key
+ * @param destructor
+ *   The function to be called when the thread expires.
+ *   Ignored on Windows OS.
+ *
+ * @return
+ *   On success, zero.
+ *   On failure, a negative number.
+ */
+
+__rte_experimental
+int rte_thread_tls_key_create(rte_tls_key *key, void (*destructor)(void *));
+
+/**
+ * Function to delete a TLS data key visible to all threads in the process
+ * rte_tls_key is the opaque pointer allocated by rte_thread_tls_key_create.
+ *
+ * @param key
+ *   The rte_tls_key will contain the allocated key
+ *
+ * @return
+ *   On success, zero.
+ *   On failure, a negative number.
+ */
+__rte_experimental
+int rte_thread_tls_key_delete(rte_tls_key key);
+
+/**
+ * Function to set value bound to the tls key on behalf of the calling thread
+ *
+ * @param key
+ *   The rte_tls_key key allocated by rte_thread_tls_key_create.
+ * @param value
+ *   The value bound to the rte_tls_key key for the calling thread.
+ *
+ * @return
+ *   On success, zero.
+ *   On failure, a negative number.
+ */
+__rte_experimental
+int rte_thread_tls_value_set(rte_tls_key key, const void *value);
+
+/**
+ * Function to get value bound to the tls key on behalf of the calling thread
+ *
+ * @param key
+ *   The rte_tls_key key allocated by rte_thread_tls_key_create.
+ *
+ * @return
+ *   On success, value data pointer (can also be NULL).
+ *   On failure, NULL and an error number is set in rte_errno.
+ */
+__rte_experimental
+void *rte_thread_tls_value_get(rte_tls_key key);
 
 #endif /* _RTE_THREAD_H_ */
