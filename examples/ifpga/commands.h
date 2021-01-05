189662cff81bd662516403d7be1b0ac0ce92eb42
Wei Huang <wei.huang@intel.com>
2021-01-05 02:09:29 -0500

    examples/ifpga: add example for opae ifpga API
    An example application shows how to use opae ifpga APIs.
    You can test each API by running corresponding command.
    
    Signed-off-by: Wei Huang <wei.huang@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ examples/ifpga/commands.h
@@ -0,0 +1,16 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2020-2021 Intel Corporation
+ */
+
+#ifndef _COMMANDS_H_
+#define _COMMANDS_H_
+
+#include "ifpga_opae_api.h"
+
+extern cmdline_parse_ctx_t main_ctx[];
+
+typedef struct {
+	char s[38];
+} uuid_str;
+
+#endif /* _COMMANDS_H_ */
