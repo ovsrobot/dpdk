07a37085a001fbbd0a6edc9bc0b37761bf849096
Tal Shnaiderman <talshn@nvidia.com>
2021-01-06 22:35:52 +0200

    eal: move thread affinity functions to new file
    Move the definition of the functions
    rte_thread_set_affinity and rte_thread_get_affinity
    to new file, rte_thread.h
    
    The file will implement generic threading functionality
    and will only host threading functions which do not reference
    pthread API.
    
    Signed-off-by: Tal Shnaiderman <talshn@nvidia.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ lib/librte_eal/include/rte_thread.h
@@ -0,0 +1,47 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2021 Mellanox Technologies, Ltd
+ */
+
+#include <rte_os.h>
+
+#ifndef _RTE_THREAD_H_
+#define _RTE_THREAD_H_
+
+/**
+ * @file
+ *
+ * Threading functions
+ *
+ * Simple threads functionality supplied by EAL.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * Set core affinity of the current thread.
+ * Support both EAL and non-EAL thread and update TLS.
+ *
+ * @param cpusetp
+ *   Point to cpu_set_t for setting current thread affinity.
+ * @return
+ *   On success, return 0; otherwise return -1;
+ */
+int rte_thread_set_affinity(rte_cpuset_t *cpusetp);
+
+/**
+ * Get core affinity of the current thread.
+ *
+ * @param cpusetp
+ *   Point to cpu_set_t for getting current thread cpu affinity.
+ *   It presumes input is not NULL, otherwise it causes panic.
+ *
+ */
+void rte_thread_get_affinity(rte_cpuset_t *cpusetp);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RTE_THREAD_H_ */
