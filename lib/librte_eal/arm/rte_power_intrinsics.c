5566a5aa0c429e0470f858651c37064d380786ab
Anatoly Burakov <anatoly.burakov@intel.com>
2021-01-12 17:37:13 +0000

    eal: add monitor wakeup function
    Now that we have everything in a C file, we can store the information
    about our sleep, and have a native mechanism to wake up the sleeping
    core. This mechanism would however only wake up a core that's sleeping
    while monitoring - waking up from `rte_power_pause` won't work.
    
    Signed-off-by: Anatoly Burakov <anatoly.burakov@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_eal/arm/rte_power_intrinsics.c
+++ lib/librte_eal/arm/rte_power_intrinsics.c
@@ -27,3 +27,12 @@ rte_power_pause(const uint64_t tsc_timestamp)
 
 	return -ENOTSUP;
 }
+
+/**
+ * This function is not supported on ARM.
+ */
+void
+rte_power_monitor_wakeup(const unsigned int lcore_id)
+{
+	RTE_SET_USED(lcore_id);
+}
