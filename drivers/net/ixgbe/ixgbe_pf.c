5b9a946008aa7ebbd88f5617ff4ebef545adcd70
Alvin Zhang <alvinx.zhang@intel.com>
2021-01-15 15:00:29 +0800

    net/ixgbe: fix configuration of max frame size
    For 82599 NIC, jumbo frame feature is not supported in IOV mode,
    but if a VF requests to configure the frame size to that not
    bigger than RTE_ETHER_MAX_LEN, the PMD should not return -1.
    This patch keeps ixgbe PMD's handling mode consistent with kernel
    driver in above situation.
    
    In addition, the value set by the command IXGBE_VF_SET_LPE
    represents the max frame size, not the mtu.
    
    Fixes: 1b9ea09c067b ("ixgbe: support X550")
    Fixes: 95a27b3ba5f5 ("net/ixgbe: enable jumbo frame for VF")
    Cc: stable@dpdk.org
    
    Signed-off-by: Alvin Zhang <alvinx.zhang@intel.com>
    Tested-by:  Xie,WeiX < weix.xie@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/ixgbe/ixgbe_pf.c
+++ drivers/net/ixgbe/ixgbe_pf.c
@@ -555,17 +555,20 @@ static int
 ixgbe_set_vf_lpe(struct rte_eth_dev *dev, __rte_unused uint32_t vf, uint32_t *msgbuf)
 {
 	struct ixgbe_hw *hw = IXGBE_DEV_PRIVATE_TO_HW(dev->data->dev_private);
-	uint32_t new_mtu = msgbuf[1];
+	uint32_t max_frame = msgbuf[1];
 	uint32_t max_frs;
 	uint32_t hlreg0;
-	int max_frame = new_mtu + RTE_ETHER_HDR_LEN + RTE_ETHER_CRC_LEN;
 
 	/* X540 and X550 support jumbo frames in IOV mode */
 	if (hw->mac.type != ixgbe_mac_X540 &&
 		hw->mac.type != ixgbe_mac_X550 &&
 		hw->mac.type != ixgbe_mac_X550EM_x &&
-		hw->mac.type != ixgbe_mac_X550EM_a)
-		return -1;
+		hw->mac.type != ixgbe_mac_X550EM_a) {
+		if (max_frame > dev->data->dev_conf.rxmode.max_rx_pkt_len)
+			return -1;
+
+		return 0;
+	}
 
 	if (max_frame < RTE_ETHER_MIN_LEN ||
 			max_frame > RTE_ETHER_MAX_JUMBO_FRAME_LEN)
@@ -573,9 +576,9 @@ ixgbe_set_vf_lpe(struct rte_eth_dev *dev, __rte_unused uint32_t vf, uint32_t *ms
 
 	max_frs = (IXGBE_READ_REG(hw, IXGBE_MAXFRS) &
 		   IXGBE_MHADD_MFS_MASK) >> IXGBE_MHADD_MFS_SHIFT;
-	if (max_frs < new_mtu) {
+	if (max_frs < max_frame) {
 		hlreg0 = IXGBE_READ_REG(hw, IXGBE_HLREG0);
-		if (new_mtu > RTE_ETHER_MAX_LEN) {
+		if (max_frame > RTE_ETHER_MAX_LEN) {
 			dev->data->dev_conf.rxmode.offloads |=
 				DEV_RX_OFFLOAD_JUMBO_FRAME;
 			hlreg0 |= IXGBE_HLREG0_JUMBOEN;
@@ -586,7 +589,7 @@ ixgbe_set_vf_lpe(struct rte_eth_dev *dev, __rte_unused uint32_t vf, uint32_t *ms
 		}
 		IXGBE_WRITE_REG(hw, IXGBE_HLREG0, hlreg0);
 
-		max_frs = new_mtu << IXGBE_MHADD_MFS_SHIFT;
+		max_frs = max_frame << IXGBE_MHADD_MFS_SHIFT;
 		IXGBE_WRITE_REG(hw, IXGBE_MAXFRS, max_frs);
 	}
 
