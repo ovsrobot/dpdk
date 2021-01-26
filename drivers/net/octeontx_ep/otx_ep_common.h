86ed88a8c5a36b4d481eba83a2c706a7823e1528
Nalla Pradeep <pnalla@marvell.com>
2021-01-26 13:30:49 -0800

    net/octeontx_ep: Added dev start and stop
    Dev start and stop operations are added. To accomplish this internal
    functions to enable or disable io queues are incorporated.
    
    Signed-off-by: Nalla Pradeep <pnalla@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/octeontx_ep/otx_ep_common.h
+++ drivers/net/octeontx_ep/otx_ep_common.h
@@ -19,6 +19,7 @@
 #define OTX_EP_PCI_RING_ALIGN   65536
 #define SDP_PKIND 40
 #define SDP_OTX2_PKIND 57
+#define OTX_EP_BUSY_LOOP_COUNT      (10000)
 #define OTX_EP_MAX_IOQS_PER_VF 8
 
 #define otx_ep_info(fmt, args...)				\
@@ -362,7 +363,14 @@ struct otx_ep_fn_list {
 
 	int (*setup_device_regs)(struct otx_ep_device *otx_ep);
 
+	int (*enable_io_queues)(struct otx_ep_device *otx_ep);
 	void (*disable_io_queues)(struct otx_ep_device *otx_ep);
+
+	int (*enable_iq)(struct otx_ep_device *otx_ep, uint32_t q_no);
+	void (*disable_iq)(struct otx_ep_device *otx_ep, uint32_t q_no);
+
+	int (*enable_oq)(struct otx_ep_device *otx_ep, uint32_t q_no);
+	void (*disable_oq)(struct otx_ep_device *otx_ep, uint32_t q_no);
 };
 
 /* SRIOV information */
@@ -416,6 +424,8 @@ struct otx_ep_device {
 
 	/* Device configuration */
 	const struct otx_ep_config *conf;
+
+	int started;
 
 	int port_configured;
 
