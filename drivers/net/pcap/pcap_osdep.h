e81d5a2ff2c20a49891fb0429ad59b3e2ab5156b
Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
2021-02-14 04:20:10 +0300

    net/pcap: move OS-dependent code to separate files
    PCAP PMD queries interface information differently for Linux and
    FreeBSD, OS-specific code is guarded by #ifdef. In preparation to add
    Windows-specific part and libpcap wrapper, extract OS-independent
    interface and move implementations to separate files. Rename
    rte_eth_pcap.c to pcap_ethdev.c for consistency with the rest of DPDK.
    
    Signed-off-by: Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/pcap/pcap_osdep.h
@@ -0,0 +1,17 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2021 Dmitry Kozlyuk
+ */
+
+#ifndef _RTE_PCAP_OSDEP_
+#define _RTE_PCAP_OSDEP_
+
+#include <rte_ether.h>
+
+/*
+ * Interface manipulation is always OS-specific.
+ */
+
+int osdep_iface_index_get(const char *name);
+int osdep_iface_mac_get(const char *name, struct rte_ether_addr *mac);
+
+#endif
