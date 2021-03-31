c261d6d772335f64a0f8b4cea42bddbbf1461235
Juraj Linkeš <juraj.linkes@pantheon.tech>
2021-03-31 11:06:42 +0200

    build: optional NUMA and cpu counts detection
    Add an option to automatically discover the host's numa and cpu counts
    and use those values for a non cross-build.
    Give users the option to override the per-arch default values or values
    from cross files by specifying them on the command line with -Dmax_lcores
    and -Dmax_numa_nodes.
    
    Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>
    Reviewed-by: Honnappa Nagarahalli <honnappa.nagarahalli@arm.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ buildtools/get-numa-count.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright (c) 2021 PANTHEON.tech s.r.o.
+
+import ctypes
+import glob
+import os
+import subprocess
+
+if os.name == 'posix':
+    if os.path.isdir('/sys/devices/system/node'):
+        print(len(glob.glob('/sys/devices/system/node/node*')))
+    else:
+        subprocess.run(['sysctl', '-n', 'vm.ndomains'])
+
+elif os.name == 'nt':
+    libkernel32 = ctypes.windll.kernel32
+
+    count = ctypes.c_ulong()
+
+    libkernel32.GetNumaHighestNodeNumber(ctypes.pointer(count))
+    print(count.value + 1)
