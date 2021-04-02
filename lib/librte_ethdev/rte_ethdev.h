22cde7d54c035c041df65863657b0b6de1d3210c
Chengchang Tang <tangchengchang@huawei.com>
2021-04-02 10:58:50 +0800

    ethdev: fix miss input validation when access EEPROM info
    This patch adds validity check of input pointer in EEPROM dump API.
    
    Fixes: 7a3f27cbf59b ("ethdev: add access to specific device info")
    Cc: stable@dpdk.org
    
    Signed-off-by: Chengchang Tang <tangchengchang@huawei.com>
    Signed-off-by: Min Hu (Connor) <humin29@huawei.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_ethdev/rte_ethdev.h
+++ lib/librte_ethdev/rte_ethdev.h
@@ -4429,6 +4429,7 @@ int rte_eth_dev_get_eeprom_length(uint16_t port_id);
  * @return
  *   - (0) if successful.
  *   - (-ENOTSUP) if hardware doesn't support.
+ *   - (-EINVAL) if bad parameter.
  *   - (-ENODEV) if *port_id* invalid.
  *   - (-EIO) if device is removed.
  *   - others depends on the specific operations implementation.
@@ -4447,6 +4448,7 @@ int rte_eth_dev_get_eeprom(uint16_t port_id, struct rte_dev_eeprom_info *info);
  *   - (0) if successful.
  *   - (-ENOTSUP) if hardware doesn't support.
  *   - (-ENODEV) if *port_id* invalid.
+ *   - (-EINVAL) if bad parameter.
  *   - (-EIO) if device is removed.
  *   - others depends on the specific operations implementation.
  */
