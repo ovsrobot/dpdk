49a22c3f6a78b8e6a282927b1b95e5d4b36a75cd
Narcisa Vasile <navasile@microsoft.com>
2021-04-02 18:39:06 -0700

    eal: implement functions for thread barrier management
    Add functions for barrier init, destroy, wait.
    
    Signed-off-by: Narcisa Vasile <navasile@microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/librte_eal/include/rte_thread_types.h
+++ lib/librte_eal/include/rte_thread_types.h
@@ -7,6 +7,7 @@
 
 #include <pthread.h>
 
+#define RTE_THREAD_BARRIER_SERIAL_THREAD PTHREAD_BARRIER_SERIAL_THREAD
 #define RTE_THREAD_MUTEX_INITIALIZER     PTHREAD_MUTEX_INITIALIZER
 
 #define EAL_THREAD_PRIORITY_NORMAL               0
@@ -14,5 +15,6 @@
 
 typedef pthread_t                       rte_thread_t;
 typedef pthread_mutex_t                 rte_thread_mutex_t;
+typedef pthread_barrier_t               rte_thread_barrier_t;
 
 #endif /* _RTE_THREAD_TYPES_H_ */
--- lib/librte_eal/windows/include/rte_windows_thread_types.h
+++ lib/librte_eal/windows/include/rte_windows_thread_types.h
@@ -7,10 +7,13 @@
 
 #include <rte_windows.h>
 
+#define RTE_THREAD_BARRIER_SERIAL_THREAD TRUE
+
 #define EAL_THREAD_PRIORITY_NORMAL             THREAD_PRIORITY_NORMAL
 #define EAL_THREAD_PRIORITY_REALTIME_CIRTICAL  THREAD_PRIORITY_TIME_CRITICAL
 
 typedef DWORD                       rte_thread_t;
 typedef CRITICAL_SECTION            rte_thread_mutex_t;
+typedef SYNCHRONIZATION_BARRIER     rte_thread_barrier_t;
 
 #endif /* _RTE_THREAD_TYPES_H_ */
