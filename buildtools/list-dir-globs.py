ovsrobot/dpdk

commit 2e33309ebe034092caaf70feb21167bc05983c1e

config: enable/disable drivers in Arm builds
Add support for enabling or disabling drivers for Arm cross build. Do
not implement any enable/disable lists yet.

Enabling drivers is useful when building for an SoC where we only want
to build a few drivers. That way the list won't be too long.

Similarly, disabling drivers is useful when we want to disable only a
few drivers.

Both of these are advantageous mainly in aarch64 -> aarch64 (or arch ->
same arch) builds, where the build machine may have the required driver
dependencies, yet we don't want to build drivers for a specific SoC.

If enable_drivers is a non-empty list, build only those drivers,
otherwise build all drivers and add them to enable_drivers.  If
disable_drivers is non-empty list, build all drivers specified in
enable_drivers except those in disable_drivers.

There are two drivers, bus/pci and bus/vdev, which break the build if
not enabled. Address this by always enabling these if the user disables
them or doesn't specify in their allowlist.

Also remove the old Makefile arm configuration options which don't do
anything in Meson.

Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>
Reviewed-by: Honnappa Nagarahalli <honnappa.nagarahalli@arm.com>
Reviewed-by: Ruifeng Wang <ruifeng.wang@arm.com>
Acked-by: Bruce Richardson <bruce.richardson@intel.com>

buildtools/list-dir-globs.py
@@ -14,6 +14,7 @@ root = os.path.join(os.getenv('MESON_SOURCE_ROOT', '.'),
                     os.getenv('MESON_SUBDIR', '.'))
 
 for path in sys.argv[1].split(','):
-    for p in iglob(os.path.join(root, path)):
-        if os.path.isdir(p):
-            print(os.path.relpath(p))
+    if path:
+        for p in iglob(os.path.join(root, path)):
+            if os.path.isdir(p):
+                print(os.path.relpath(p))

config/arm/meson.build
@@ -17,9 +17,6 @@ flags_common = [
 	#	['RTE_ARM64_MEMCPY_ALIGN_MASK', 0xF],
 	#	['RTE_ARM64_MEMCPY_STRICT_ALIGN', false],
 
-	['RTE_NET_FM10K', false],
-	['RTE_NET_AVP', false],
-
 	['RTE_SCHED_VECTOR', false],
 	['RTE_ARM_USE_WFE', false],
 	['RTE_ARCH_ARM64', true],
@@ -126,7 +123,6 @@ implementer_cavium = {
 				['RTE_MACHINE', '"octeontx2"'],
 				['RTE_ARM_FEATURE_ATOMICS', true],
 				['RTE_USE_C11_MEM_MODEL', true],
-				['RTE_EAL_IGB_UIO', false],
 				['RTE_MAX_LCORE', 36],
 				['RTE_MAX_NUMA_NODES', 1]
 			]

config/meson.build
@@ -63,6 +63,10 @@ if not is_windows
 			pmd_subdir_opt)
 endif
 
+# init disable/enable driver lists that will be populated in different places
+disable_drivers = ''
+enable_drivers = ''
+
 # set the machine type and cflags for it
 if meson.is_cross_build()
 	machine = host_machine.cpu()

drivers/common/qat/meson.build
@@ -14,13 +14,13 @@ qat_compress = true
 qat_compress_path = 'compress/qat'
 qat_compress_relpath = '../../' + qat_compress_path
 
-if disabled_drivers.contains(qat_crypto_path)
+if disable_drivers.contains(qat_crypto_path)
 	qat_crypto = false
 	dpdk_drvs_disabled += qat_crypto_path
 	set_variable(qat_crypto_path.underscorify() + '_disable_reason',
 			'Explicitly disabled via build config')
 endif
-if disabled_drivers.contains(qat_compress_path)
+if disable_drivers.contains(qat_compress_path)
 	qat_compress = false
 	dpdk_drvs_disabled += qat_compress_path
 	set_variable(qat_compress_path.underscorify() + '_disable_reason',

drivers/meson.build
@@ -20,8 +20,25 @@ subdirs = [
 	'baseband', # depends on common and bus.
 ]
 
-disabled_drivers = run_command(list_dir_globs, get_option('disable_drivers'),
-		).stdout().split()
+if meson.is_cross_build()
+	disable_drivers += ',' + meson.get_cross_property('disable_drivers', '')
+	enable_drivers += ',' + meson.get_cross_property('enable_drivers', '')
+endif
+
+# add cmdline disabled drivers and meson disabled drivers together
+disable_drivers += ',' + get_option('disable_drivers')
+disable_drivers = run_command(list_dir_globs, disable_drivers).stdout().split()
+
+# add cmdline enabled drivers and meson enabled drivers together
+enable_drivers = ',' + get_option('enable_drivers')
+enable_drivers = run_command(list_dir_globs, enable_drivers).stdout().split()
+if enable_drivers.length() == 0
+	enable_drivers = run_command(list_dir_globs, '*/*').stdout().split()
+endif
+
+# these drivers must always be enabled, otherwise the build breaks
+always_enable = ['bus/pci', 'bus/vdev']
+enable_drivers += always_enable
 
 default_cflags = machine_args
 default_cflags += ['-DALLOW_EXPERIMENTAL_API']
@@ -75,9 +92,16 @@ foreach subpath:subdirs
 		ext_deps = []
 		pkgconfig_extra_libs = []
 
-		if disabled_drivers.contains(drv_path)
+		if not enable_drivers.contains(drv_path)
 			build = false
-			reason = 'explicitly disabled via build config'
+			reason = 'not in enabled drivers build config'
+		elif disable_drivers.contains(drv_path)
+			if always_enable.contains(drv_path)
+				message('Driver @0@ cannot be disabled, not disabling.'.format(drv_path))
+			else
+				build = false
+				reason = 'explicitly disabled via build config'
+			endif
 		else
 			# pull in driver directory which should update all the local variables
 			subdir(drv_path)

commit e8a72e74ac80e175e04c179308a81d94891655c8

buildtools: fix all drivers disabled on Windows
buildtools/list-dir-globs.py printed paths with OS directory separator,
which is "/" on Unices and "\" on Windows, while Meson code always
expected "/". This resulted it all drivers being disabled on Windows.

Replace "\" with "/" in script output. Forward slash is a valid,
although non-default, separator on Windows, so no paths can be broken
by this substitution.

Fixes: 2e33309ebe03 ("config: enable/disable drivers in Arm builds")
Cc: Juraj Linkeš <juraj.linkes@pantheon.tech>

Signed-off-by: Dmitry Kozlyuk <dmitry.kozliuk@gmail.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

buildtools/list-dir-globs.py
@@ -17,4 +17,4 @@ for path in sys.argv[1].split(','):
     if path:
         for p in iglob(os.path.join(root, path)):
             if os.path.isdir(p):
-                print(os.path.relpath(p))
+                print(os.path.relpath(p).replace('\\', '/'))