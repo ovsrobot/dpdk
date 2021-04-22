ovsrobot/dpdk

commit 62024eb8275696bead35b38a6062a2513f1f7c58

ethdev: change stop operation callback to return int
Change eth_dev_stop_t return value from void to int.
Make eth_dev_stop_t implementations across all drivers to return
negative errno values if case of error conditions.

Signed-off-by: Ivan Ilchenko <ivan.ilchenko@oktetlabs.ru>
Signed-off-by: Andrew Rybchenko <arybchenko@solarflare.com>
Reviewed-by: Ferruh Yigit <ferruh.yigit@intel.com>

drivers/net/kni/rte_eth_kni.c
@@ -177,7 +177,7 @@ eth_kni_dev_start(struct rte_eth_dev *dev)
 	return 0;
 }
 
-static void
+static int
 eth_kni_dev_stop(struct rte_eth_dev *dev)
 {
 	struct pmd_internals *internals = dev->data->dev_private;
@@ -197,6 +197,8 @@ eth_kni_dev_stop(struct rte_eth_dev *dev)
 
 	dev->data->dev_link.link_status = 0;
 	dev->data->dev_started = 0;
+
+	return 0;
 }
 
 static int
@@ -208,7 +210,7 @@ eth_kni_close(struct rte_eth_dev *eth_dev)
 	if (rte_eal_process_type() != RTE_PROC_PRIMARY)
 		return 0;
 
-	eth_kni_dev_stop(eth_dev);
+	ret = eth_kni_dev_stop(eth_dev);
 
 	/* mac_addrs must not be freed alone because part of dev_private */
 	eth_dev->data->mac_addrs = NULL;
@@ -219,7 +221,7 @@ eth_kni_close(struct rte_eth_dev *eth_dev)
 		PMD_LOG(WARNING, "Not able to release kni for %s",
 			eth_dev->data->name);
 
-	return 0;
+	return ret;
 }
 
 static int
@@ -486,6 +488,7 @@ eth_kni_remove(struct rte_vdev_device *vdev)
 {
 	struct rte_eth_dev *eth_dev;
 	const char *name;
+	int ret;
 
 	name = rte_vdev_device_name(vdev);
 	PMD_LOG(INFO, "Un-Initializing eth_kni for %s", name);
@@ -494,7 +497,9 @@ eth_kni_remove(struct rte_vdev_device *vdev)
 	eth_dev = rte_eth_dev_allocated(name);
 	if (eth_dev != NULL) {
 		if (rte_eal_process_type() != RTE_PROC_PRIMARY) {
-			eth_kni_dev_stop(eth_dev);
+			ret = eth_kni_dev_stop(eth_dev);
+			if (ret != 0)
+				return ret;
 			return rte_eth_dev_release_port(eth_dev);
 		}
 		eth_kni_close(eth_dev);

commit fa02b98356881742338f2c7daffb4e08f1fae833

net/kni: fix rewritten return value
Return value of function 'eth_kni_dev_stop' passed to 'ret' is
rewritten later, and this is unreasonable.

This patch fixes it.

Fixes: 62024eb82756 ("ethdev: change stop operation callback to return int")
Cc: stable@dpdk.org

Signed-off-by: Min Hu (Connor) <humin29@huawei.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/kni/rte_eth_kni.c
@@ -211,6 +211,11 @@ eth_kni_close(struct rte_eth_dev *eth_dev)
 		return 0;
 
 	ret = eth_kni_dev_stop(eth_dev);
+	if (ret) {
+		PMD_LOG(WARNING, "Not able to stop kni for %s",
+			eth_dev->data->name);
+		return ret;
+	}
 
 	/* mac_addrs must not be freed alone because part of dev_private */
 	eth_dev->data->mac_addrs = NULL;