ovsrobot/dpdk

commit 146ffa81d05ec331b46a3980fe23b7a28cac142e

net/i40e: add Tx preparation for simple Tx datapath
Introduce i40e_simple_prep_pkts() as the preparation function for
simple Tx data path, as it's for sanity check for simple Tx.

Suggested-by: Konstantin Ananyev <konstantin.ananyev@intel.com>
Signed-off-by: Leyi Rong <leyi.rong@intel.com>
Acked-by: Konstantin Ananyev <konstantin.ananyev@intel.com>

drivers/net/i40e/i40e_rxtx.c
@@ -1481,6 +1481,43 @@ i40e_xmit_pkts_vec(void *tx_queue, struct rte_mbuf **tx_pkts,
 
 /*********************************************************************
  *
+ *  TX simple prep functions
+ *
+ **********************************************************************/
+uint16_t
+i40e_simple_prep_pkts(__rte_unused void *tx_queue, struct rte_mbuf **tx_pkts,
+		      uint16_t nb_pkts)
+{
+	int i;
+	uint64_t ol_flags;
+	struct rte_mbuf *m;
+
+	for (i = 0; i < nb_pkts; i++) {
+		m = tx_pkts[i];
+		ol_flags = m->ol_flags;
+
+		if (m->nb_segs != 1) {
+			rte_errno = EINVAL;
+			return i;
+		}
+
+		if (ol_flags & PKT_TX_OFFLOAD_MASK) {
+			rte_errno = ENOTSUP;
+			return i;
+		}
+
+		/* check the size of packet */
+		if (m->pkt_len < I40E_TX_MIN_PKT_LEN ||
+		    m->pkt_len > I40E_FRAME_SIZE_MAX) {
+			rte_errno = EINVAL;
+			return i;
+		}
+	}
+	return i;
+}
+
+/*********************************************************************
+ *
  *  TX prep functions
  *
  **********************************************************************/
@@ -3412,7 +3449,7 @@ i40e_set_tx_function(struct rte_eth_dev *dev)
 			PMD_INIT_LOG(DEBUG, "Simple tx finally be used.");
 			dev->tx_pkt_burst = i40e_xmit_pkts_simple;
 		}
-		dev->tx_pkt_prepare = NULL;
+		dev->tx_pkt_prepare = i40e_simple_prep_pkts;
 	} else {
 		PMD_INIT_LOG(DEBUG, "Xmit tx finally be used.");
 		dev->tx_pkt_burst = i40e_xmit_pkts;

drivers/net/i40e/i40e_rxtx.h
@@ -208,6 +208,8 @@ uint16_t i40e_recv_scattered_pkts(void *rx_queue,
 uint16_t i40e_xmit_pkts(void *tx_queue,
 			struct rte_mbuf **tx_pkts,
 			uint16_t nb_pkts);
+uint16_t i40e_simple_prep_pkts(void *tx_queue, struct rte_mbuf **tx_pkts,
+			       uint16_t nb_pkts);
 uint16_t i40e_prep_pkts(void *tx_queue, struct rte_mbuf **tx_pkts,
 		uint16_t nb_pkts);
 int i40e_tx_queue_init(struct i40e_tx_queue *txq);

commit 1118f18809473aa7cc4827ad16ab038c2f0c17a0

net/i40e: fix offload flag checking in simple Tx datapath
For Tx packets, if no other offload flag except
'PKT_TX_OUTER_IPV6, PKT_TX_OUTER_IPV4, PKT_TX_IPV6, PKT_TX_IPV4' is
set, the hardware doesn't need to do offloads for these packets.

This patch removes those previously indicated flags from the offload
flag checking in simple Tx datapath.

Fixes: 146ffa81d05e ("net/i40e: add Tx preparation for simple Tx datapath")
Cc: stable@dpdk.org

Signed-off-by: Alvin Zhang <alvinx.zhang@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/i40e/i40e_rxtx.c
@@ -1501,7 +1501,9 @@ i40e_simple_prep_pkts(__rte_unused void *tx_queue, struct rte_mbuf **tx_pkts,
 			return i;
 		}
 
-		if (ol_flags & PKT_TX_OFFLOAD_MASK) {
+		if (ol_flags & PKT_TX_OFFLOAD_MASK &
+		    ~(PKT_TX_OUTER_IPV6 | PKT_TX_OUTER_IPV4 |
+		      PKT_TX_IPV6 | PKT_TX_IPV4)) {
 			rte_errno = ENOTSUP;
 			return i;
 		}