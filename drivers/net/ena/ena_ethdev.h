119ef20603a59f7a18656b95f42e04515bcc08d3
Stanislaw Kardach <kda@semihalf.com>
2021-04-30 14:57:22 +0200

    net/ena: make ethdev references smp safe
    rte_pci_device and rte_eth_dev are process-local structures. Therefore
    ena_adapter::pdev and ena_adapter::rte_dev cannot be used universally.
    Switch this to extracting those structures via rte_eth_devices indexing
    and remove pdev since it's not used outside of init.
    
    Signed-off-by: Stanislaw Kardach <kda@semihalf.com>
    Reviewed-by: Michal Krawczyk <mk@semihalf.com>
    Reviewed-by: Igor Chauskin <igorch@amazon.com>
    Reviewed-by: Shay Agroskin <shayagr@amazon.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/ena/ena_ethdev.h
+++ drivers/net/ena/ena_ethdev.h
@@ -219,9 +219,8 @@ struct ena_shared_data {
 /* board specific private data structure */
 struct ena_adapter {
 	/* OS defined structs */
-	struct rte_pci_device *pdev;
-	struct rte_eth_dev_data *rte_eth_dev_data;
-	struct rte_eth_dev *rte_dev;
+	struct rte_eth_dev_data *edev_data;
+	int port_id;
 
 	struct ena_com_dev ena_dev __rte_cache_aligned;
 
