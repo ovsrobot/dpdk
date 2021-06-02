bad23e70ff67bcdf127c026d59579018d679bf7a
Jiawen Wu <jiawenwu@trustnetic.com>
2021-06-02 17:40:48 +0800

    net/ngbe: add device init and uninit
    Add basic init and uninit function.
    Map device IDs and subsystem IDs to single ID for easy operation.
    
    Signed-off-by: Jiawen Wu <jiawenwu@trustnetic.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/ngbe/base/ngbe_hw.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2018-2020 Beijing WangXun Technology Co., Ltd.
+ * Copyright(c) 2010-2017 Intel Corporation
+ */
+
+#ifndef _NGBE_HW_H_
+#define _NGBE_HW_H_
+
+#include "ngbe_type.h"
+
+void ngbe_map_device_id(struct ngbe_hw *hw);
+
+#endif /* _NGBE_HW_H_ */
