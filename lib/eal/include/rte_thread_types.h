32ff7b43899cba021253ebee37c46bf42cef7010
Narcisa Vasile <navasile@microsoft.com>
2021-06-04 16:38:19 -0700

    eal: add thread id and simple thread functions
    Use a portable, type-safe representation for the thread identifier.
    Add functions for comparing thread ids and obtaining the thread id
    for the current thread.
    
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ lib/eal/include/rte_thread_types.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2021 Microsoft Corporation
+ */
+
+#ifndef _RTE_THREAD_TYPES_H_
+#define _RTE_THREAD_TYPES_H_
+
+#include <pthread.h>
+
+#endif /* _RTE_THREAD_TYPES_H_ */
