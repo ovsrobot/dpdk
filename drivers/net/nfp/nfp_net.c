ovsrobot/dpdk

commit 5e15e799d697f8332d2a00fc1532be923db72f30

net/nfp: create separate entity for PF device
Before this change memory for the private data of all physical ports
where allocated with single rte_zmalloc() call. Specific port private
data was accessed by means of an offset into this memory. This scheme is
problematic when attempting to free only one port's private data at a
time.

To address this, a new entity is created called struct nfp_pf_dev. This
struct represents the PF device. It has a number of PF specific members.
Notably it has a pointer of type rte_eth_dev that points to the eth_dev
associated with the first physical port of the device. It also has an
array of nfp_net_hw's containing pointers to all the physical ports
under the PF.

Memory is first allocated for the PF and PF specific initialization is
attempted. Next, all the physical ports under the PF is iterated and
memory is allocated separately for the private data of each port. Port 0
is skipped during this phase because memory has already been allocated
and an eth_dev already exits for the 0th port.

Signed-off-by: Heinrich Kuhn <heinrich.kuhn@netronome.com>
Signed-off-by: Simon Horman <simon.horman@netronome.com>
Reviewed-by: Louis Peens <louis.peens@netronome.com>

drivers/net/nfp/nfp_net_pmd.h
@@ -102,6 +102,9 @@ struct nfp_net_adapter;
 #define NFD_CFG_MINOR_VERSION(x)    (((x) & 0xff) << 0)
 #define NFD_CFG_MINOR_VERSION_of(x) (((x) >> 0) & 0xff)
 
+/* Number of supported physical ports */
+#define NFP_MAX_PHYPORTS	12
+
 #include <linux/types.h>
 #include <rte_io.h>
 
@@ -382,7 +385,60 @@ struct nfp_net_rxq {
 	int rx_qcidx;
 } __rte_aligned(64);
 
+struct nfp_pf_dev {
+	/* Backpointer to associated pci device */
+	struct rte_pci_device *pci_dev;
+
+	/* First physical port's eth device */
+	struct rte_eth_dev *eth_dev;
+
+	/* Array of physical ports belonging to this PF */
+	struct nfp_net_hw *ports[NFP_MAX_PHYPORTS];
+
+	/* Current values for control */
+	uint32_t ctrl;
+
+	uint8_t *ctrl_bar;
+	uint8_t *tx_bar;
+	uint8_t *rx_bar;
+
+	uint8_t *qcp_cfg;
+	rte_spinlock_t reconfig_lock;
+
+	uint16_t flbufsz;
+	uint16_t device_id;
+	uint16_t vendor_id;
+	uint16_t subsystem_device_id;
+	uint16_t subsystem_vendor_id;
+#if defined(DSTQ_SELECTION)
+#if DSTQ_SELECTION
+	uint16_t device_function;
+#endif
+#endif
+
+	struct nfp_cpp *cpp;
+	struct nfp_cpp_area *ctrl_area;
+	struct nfp_cpp_area *hwqueues_area;
+	struct nfp_cpp_area *msix_area;
+
+	uint8_t *hw_queues;
+	uint8_t total_phyports;
+	bool	multiport;
+
+	union eth_table_entry *eth_table;
+
+	struct nfp_hwinfo *hwinfo;
+	struct nfp_rtsym_table *sym_tbl;
+	uint32_t nfp_cpp_service_id;
+};
+
 struct nfp_net_hw {
+	/* Backpointer to the PF this port belongs to */
+	struct nfp_pf_dev *pf_dev;
+
+	/* Backpointer to the eth_dev of this port*/
+	struct rte_eth_dev *eth_dev;
+
 	/* Info from the firmware */
 	uint32_t ver;
 	uint32_t cap;
@@ -427,15 +483,11 @@ struct nfp_net_hw {
 	struct nfp_cpp_area *msix_area;
 
 	uint8_t *hw_queues;
-	uint8_t is_pf;
-	uint8_t pf_port_idx;
-	uint8_t pf_multiport_enabled;
-	uint8_t total_ports;
+	uint8_t idx;
+	bool	is_phyport;
 
 	union eth_table_entry *eth_table;
 
-	struct nfp_hwinfo *hwinfo;
-	struct nfp_rtsym_table *sym_tbl;
 	uint32_t nfp_cpp_service_id;
 };
 
@@ -445,6 +497,9 @@ struct nfp_net_adapter {
 
 #define NFP_NET_DEV_PRIVATE_TO_HW(adapter)\
 	(&((struct nfp_net_adapter *)adapter)->hw)
+
+#define NFP_NET_DEV_PRIVATE_TO_PF(dev_priv)\
+	(((struct nfp_net_hw *)dev_priv)->pf_dev)
 
 #endif /* _NFP_NET_PMD_H_ */
 /*

commit 19de9727d6ff29cb765ba226e064b292de515123

net/nfp: improve PF probing logic
When using rte_eth_dev_pci_generic_probe() during probing a
rte_eth_dev will be created with the name field corresponding to the PCI
address of the device. NFP4000/6000 devices only have a single PF (but
potentially multiple physical ports). This means that in a simple two
port example the rte_eth_devices[] array will be populated with two
devices: 0000:02:00.0 and 0000:02:00.0_port1. This is inconsistent and
not ideal. It will also cause issues when a secondary process tries to
attach to these ports.

This patch removes the use of rte_eth_dev_pci_generic_probe() and
allocates eth_dev's for each physical port during PF initialization,
giving them more consistent names.

Fixes: 5e15e799d697 ("net/nfp: create separate entity for PF device")

Signed-off-by: Heinrich Kuhn <heinrich.kuhn@netronome.com>
Signed-off-by: Simon Horman <simon.horman@corigine.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/nfp/nfp_net.c
@@ -58,7 +58,7 @@ static int nfp_net_dev_mtu_set(struct rte_eth_dev *dev, uint16_t mtu);
 static int nfp_net_infos_get(struct rte_eth_dev *dev,
 			     struct rte_eth_dev_info *dev_info);
 static int nfp_net_init(struct rte_eth_dev *eth_dev);
-static int nfp_pf_init(struct rte_eth_dev *eth_dev);
+static int nfp_pf_init(struct rte_pci_device *pci_dev);
 static int nfp_pci_uninit(struct rte_eth_dev *eth_dev);
 static int nfp_init_phyports(struct nfp_pf_dev *pf_dev);
 static int nfp_net_link_update(struct rte_eth_dev *dev, int wait_to_complete);
@@ -3530,20 +3530,14 @@ static int nfp_init_phyports(struct nfp_pf_dev *pf_dev)
 			goto nfp_net_init;
 		}
 
-		/* First port has already been initialized */
-		if (i == 0) {
-			eth_dev = pf_dev->eth_dev;
-			goto skip_dev_alloc;
-		}
-
-		/* Allocate a eth_dev for remaining ports */
+		/* Allocate a eth_dev for this phyport */
 		eth_dev = rte_eth_dev_allocate(port_name);
 		if (!eth_dev) {
 			ret = -ENODEV;
 			goto port_cleanup;
 		}
 
-		/* Allocate memory for remaining ports */
+		/* Allocate memory for this phyport */
 		eth_dev->data->dev_private =
 			rte_zmalloc_socket(port_name, sizeof(struct nfp_net_hw),
 					   RTE_CACHE_LINE_SIZE, numa_node);
@@ -3553,7 +3547,6 @@ static int nfp_init_phyports(struct nfp_pf_dev *pf_dev)
 			goto port_cleanup;
 		}
 
-skip_dev_alloc:
 		hw = NFP_NET_DEV_PRIVATE_TO_HW(eth_dev->data->dev_private);
 
 		/* Add this device to the PF's array of physical ports */
@@ -3600,23 +3593,19 @@ error:
 	return ret;
 }
 
-static int nfp_pf_init(struct rte_eth_dev *eth_dev)
+static int nfp_pf_init(struct rte_pci_device *pci_dev)
 {
-	struct rte_pci_device *pci_dev;
-	struct nfp_net_hw *hw = NULL;
 	struct nfp_pf_dev *pf_dev = NULL;
 	struct nfp_cpp *cpp;
 	struct nfp_hwinfo *hwinfo;
 	struct nfp_rtsym_table *sym_tbl;
 	struct nfp_eth_table *nfp_eth_table = NULL;
 	struct rte_service_spec service;
+	uint32_t *nfp_cpp_service_id = NULL;
 	char name[RTE_ETH_NAME_MAX_LEN];
 	int total_ports;
 	int ret = -ENODEV;
 	int err;
-
-	pci_dev = RTE_ETH_DEV_TO_PCI(eth_dev);
-	hw = NFP_NET_DEV_PRIVATE_TO_HW(eth_dev);
 
 	if (!pci_dev)
 		return ret;
@@ -3685,7 +3674,7 @@ static int nfp_pf_init(struct rte_eth_dev *eth_dev)
 		goto sym_tbl_cleanup;
 	}
 	/* Allocate memory for the PF "device" */
-	snprintf(name, sizeof(name), "nfp_pf%d", eth_dev->data->port_id);
+	snprintf(name, sizeof(name), "nfp_pf%d", 0);
 	pf_dev = rte_zmalloc(name, sizeof(*pf_dev), 0);
 	if (!pf_dev) {
 		ret = -ENOMEM;
@@ -3702,9 +3691,6 @@ static int nfp_pf_init(struct rte_eth_dev *eth_dev)
 		pf_dev->multiport = true;
 
 	pf_dev->pci_dev = pci_dev;
-
-	/* The first eth_dev is part of the PF struct */
-	pf_dev->eth_dev = eth_dev;
 
 	/* Map the symbol table */
 	pf_dev->ctrl_bar = nfp_rtsym_map(pf_dev->sym_tbl, "_pf0_net_bar0",
@@ -3754,7 +3740,7 @@ static int nfp_pf_init(struct rte_eth_dev *eth_dev)
 	service.callback_userdata = (void *)cpp;
 
 	if (rte_service_component_register(&service,
-					   &hw->nfp_cpp_service_id))
+					   nfp_cpp_service_id))
 		RTE_LOG(ERR, PMD, "NFP CPP bridge service register() failed");
 	else
 		RTE_LOG(DEBUG, PMD, "NFP CPP bridge service registered");
@@ -3780,8 +3766,7 @@ error:
 static int nfp_pf_pci_probe(struct rte_pci_driver *pci_drv __rte_unused,
 			    struct rte_pci_device *dev)
 {
-	return rte_eth_dev_pci_generic_probe(dev,
-		sizeof(struct nfp_net_hw), nfp_pf_init);
+	return nfp_pf_init(dev);
 }
 
 static const struct rte_pci_id pci_id_nfp_pf_net_map[] = {

drivers/net/nfp/nfp_net_pmd.h
@@ -389,9 +389,6 @@ struct nfp_pf_dev {
 	/* Backpointer to associated pci device */
 	struct rte_pci_device *pci_dev;
 
-	/* First physical port's eth device */
-	struct rte_eth_dev *eth_dev;
-
 	/* Array of physical ports belonging to this PF */
 	struct nfp_net_hw *ports[NFP_MAX_PHYPORTS];
 