a130d11110ddfe7081af3a0c4d4b07a53bf5ac78
Suanming Mou <suanmingm@nvidia.com>
2021-07-13 11:44:56 +0300

    net/mlx5: enable index pool per-core cache
    This commit enables the tag and header modify action index pool
    per-core cache in non-reclaim memory mode.
    
    Signed-off-by: Suanming Mou <suanmingm@nvidia.com>
    Acked-by: Matan Azrad <matan@nvidia.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/mlx5/mlx5.h
+++ drivers/net/mlx5/mlx5.h
@@ -1116,6 +1116,7 @@ struct mlx5_dev_ctx_shared {
 	uint32_t qp_ts_format:2; /* QP timestamp formats supported. */
 	uint32_t meter_aso_en:1; /* Flow Meter ASO is supported. */
 	uint32_t ct_aso_en:1; /* Connection Tracking ASO is supported. */
+	uint32_t reclaim_mode:1; /* Reclaim memory. */
 	uint32_t max_port; /* Maximal IB device port index. */
 	struct mlx5_bond_info bond; /* Bonding information. */
 	void *ctx; /* Verbs/DV/DevX context. */
