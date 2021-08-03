23ca6e287fd30ab5ea60047a72750e2c72ccae94
Narcisa Vasile <navasile@microsoft.com>
2021-08-03 12:01:23 -0700

    eal: add thread attributes
    Implement thread attributes for:
    * thread affinity
    * thread priority
    Implement functions for managing thread attributes.
    
    Priority is represented through an enum that allows for two levels:
    	- RTE_THREAD_PRIORITY_NORMAL
    	- RTE_THREAD_PRIORITY_REALTIME_CRITICAL
    
    Affinity is described by the rte_cpuset_t type.
    
    An rte_thread_attr_t object can be set to the default values
    by calling rte_thread_attr_init().
    
    Signed-off-by: Narcisa Vasile <navasile@microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/common/rte_thread.c
+++ lib/eal/common/rte_thread.c
@@ -9,6 +9,7 @@
 #include <string.h>
 
 #include <rte_common.h>
+#include <rte_debug.h>
 #include <rte_errno.h>
 #include <rte_log.h>
 #include <rte_thread.h>
@@ -31,6 +32,51 @@ int
 rte_thread_equal(rte_thread_t t1, rte_thread_t t2)
 {
 	return pthread_equal((pthread_t)t1.opaque_id, (pthread_t)t2.opaque_id);
+}
+
+int
+rte_thread_attr_init(rte_thread_attr_t *attr)
+{
+	RTE_VERIFY(attr != NULL);
+
+	CPU_ZERO(&attr->cpuset);
+	attr->priority = RTE_THREAD_PRIORITY_NORMAL;
+
+	return 0;
+}
+
+int
+rte_thread_attr_set_affinity(rte_thread_attr_t *thread_attr,
+			     rte_cpuset_t *cpuset)
+{
+	RTE_VERIFY(thread_attr != NULL);
+	RTE_VERIFY(cpuset != NULL);
+
+	thread_attr->cpuset = *cpuset;
+
+	return 0;
+}
+
+int
+rte_thread_attr_get_affinity(rte_thread_attr_t *thread_attr,
+			     rte_cpuset_t *cpuset)
+{
+	RTE_VERIFY(thread_attr != NULL);
+	RTE_VERIFY(cpuset != NULL);
+
+	*cpuset = thread_attr->cpuset;
+
+	return 0;
+}
+
+int
+rte_thread_attr_set_priority(rte_thread_attr_t *thread_attr,
+			     enum rte_thread_priority priority)
+{
+	RTE_VERIFY(thread_attr != NULL);
+
+	thread_attr->priority = priority;
+	return 0;
 }
 
 int
