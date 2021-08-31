ovsrobot/dpdk

commit abf7275bbaa2918a387e1f28f2c352053279c879

ixgbe: move to drivers/net/
move ixgbe PMD to drivers/net directory.
As part of the move, we rename the ixgbe directory, containing the
ixgbe "base driver" code, from "ixgbe" to "base".

Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>
Acked-by: John McNamara <john.mcnamara@intel.com>
Acked-by: Thomas Monjalon <thomas.monjalon@6wind.com>

drivers/net/ixgbe/ixgbe_ethdev.c
@@ -1,7 +1,7 @@
 /*-
  *   BSD LICENSE
  *
- *   Copyright(c) 2010-2014 Intel Corporation. All rights reserved.
+ *   Copyright(c) 2010-2015 Intel Corporation. All rights reserved.
  *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
@@ -62,9 +62,9 @@
 #include <rte_dev.h>
 
 #include "ixgbe_logs.h"
-#include "ixgbe/ixgbe_api.h"
-#include "ixgbe/ixgbe_vf.h"
-#include "ixgbe/ixgbe_common.h"
+#include "base/ixgbe_api.h"
+#include "base/ixgbe_vf.h"
+#include "base/ixgbe_common.h"
 #include "ixgbe_ethdev.h"
 #include "ixgbe_bypass.h"
 #include "ixgbe_rxtx.h"
@@ -461,7 +461,7 @@ rte_ixgbe_dev_atomic_write_link_status(struct rte_eth_dev *dev,
 }
 
 /*
- * This function is the same as ixgbe_is_sfp() in ixgbe/ixgbe.h.
+ * This function is the same as ixgbe_is_sfp() in base/ixgbe.h.
  */
 static inline int
 ixgbe_is_sfp(struct ixgbe_hw *hw)
@@ -509,7 +509,7 @@ ixgbe_enable_intr(struct rte_eth_dev *dev)
 }
 
 /*
- * This function is based on ixgbe_disable_intr() in ixgbe/ixgbe.h.
+ * This function is based on ixgbe_disable_intr() in base/ixgbe.h.
  */
 static void
 ixgbe_disable_intr(struct ixgbe_hw *hw)
@@ -710,7 +710,7 @@ ixgbe_swfw_lock_reset(struct ixgbe_hw *hw)
 }
 
 /*
- * This function is based on code in ixgbe_attach() in ixgbe/ixgbe.c.
+ * This function is based on code in ixgbe_attach() in base/ixgbe.c.
  * It returns 0 on success.
  */
 static int
@@ -1740,7 +1740,7 @@ ixgbe_dev_close(struct rte_eth_dev *dev)
 }
 
 /*
- * This function is based on ixgbe_update_stats_counters() in ixgbe/ixgbe.c
+ * This function is based on ixgbe_update_stats_counters() in base/ixgbe.c
  */
 static void
 ixgbe_dev_stats_get(struct rte_eth_dev *dev, struct rte_eth_stats *stats)

MAINTAINERS
@@ -221,7 +221,7 @@ F: doc/guides/nics/intel_vf.rst
 Intel ixgbe
 M: Helin Zhang <helin.zhang@intel.com>
 M: Konstantin Ananyev <konstantin.ananyev@intel.com>
-F: lib/librte_pmd_ixgbe/
+F: drivers/net/ixgbe/
 F: doc/guides/nics/ixgbe.rst
 F: doc/guides/nics/intel_vf.rst
 

drivers/net/Makefile
@@ -37,6 +37,7 @@ DIRS-$(CONFIG_RTE_LIBRTE_E1000_PMD) += e1000
 DIRS-$(CONFIG_RTE_LIBRTE_ENIC_PMD) += enic
 DIRS-$(CONFIG_RTE_LIBRTE_FM10K_PMD) += fm10k
 DIRS-$(CONFIG_RTE_LIBRTE_I40E_PMD) += i40e
+DIRS-$(CONFIG_RTE_LIBRTE_IXGBE_PMD) += ixgbe
 
 include $(RTE_SDK)/mk/rte.sharelib.mk
 include $(RTE_SDK)/mk/rte.subdir.mk

drivers/net/ixgbe/Makefile
@@ -1,6 +1,6 @@
 #   BSD LICENSE
 #
-#   Copyright(c) 2010-2014 Intel Corporation. All rights reserved.
+#   Copyright(c) 2010-2015 Intel Corporation. All rights reserved.
 #   All rights reserved.
 #
 #   Redistribution and use in source and binary forms, with or without
@@ -82,10 +82,10 @@ endif
 # Add extra flags for base driver files (also known as shared code)
 # to disable warnings in them
 #
-BASE_DRIVER_OBJS=$(patsubst %.c,%.o,$(notdir $(wildcard $(SRCDIR)/ixgbe/*.c)))
+BASE_DRIVER_OBJS=$(patsubst %.c,%.o,$(notdir $(wildcard $(SRCDIR)/base/*.c)))
 $(foreach obj, $(BASE_DRIVER_OBJS), $(eval CFLAGS_$(obj)+=$(CFLAGS_BASE_DRIVER)))
 
-VPATH += $(SRCDIR)/ixgbe
+VPATH += $(SRCDIR)/base
 
 #
 # all source are stored in SRCS-y

commit 54ff8afce6b5535bee17ffcceef2fea7b7cf8534

net/ixgbe: fix vf mac remains
In the following two scenarios, the mac address residual problem
will occur:
1) In the VF hard pass-through environment, after adding the mac
address, the process restarts, and the previously added mac is
still valid;
2) In the vf hard pass-through environment, after the mac address
is issued, the port will start/stop, and the previously added mac
is still valid;

The patch clears the mac address at the start and stop of the vf
port. After the start is cleared, the rte framework will restore
the mac addition to solve the problem of residual mac addresses.

Fixes: abf7275bbaa2 ("ixgbe: move to drivers/net/")
Cc: stable@dpdk.org

Signed-off-by: Qiming Chen <chenqiming_huawei@163.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/ixgbe/ixgbe_ethdev.c
@@ -5400,6 +5400,9 @@ ixgbevf_dev_start(struct rte_eth_dev *dev)
 	 */
 	ixgbevf_dev_link_update(dev, 0);
 
+	/* Clear the mac address, the rte frame will be restored */
+	ixgbevf_set_uc_addr_vf(hw, 0, NULL);
+
 	hw->adapter_stopped = false;
 
 	return 0;
@@ -5443,6 +5446,9 @@ ixgbevf_dev_stop(struct rte_eth_dev *dev)
 		rte_free(intr_handle->intr_vec);
 		intr_handle->intr_vec = NULL;
 	}
+
+	/* Clear the mac address */
+	ixgbevf_set_uc_addr_vf(hw, 0, NULL);
 
 	adapter->rss_reta_updated = 0;
 