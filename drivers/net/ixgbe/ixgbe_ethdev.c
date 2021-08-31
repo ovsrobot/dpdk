ovsrobot/dpdk

commit abf7275bbaa2918a387e1f28f2c352053279c879

ixgbe: move to drivers/net/
move ixgbe PMD to drivers/net directory.
As part of the move, we rename the ixgbe directory, containing the
ixgbe "base driver" code, from "ixgbe" to "base".

Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>
Acked-by: John McNamara <john.mcnamara@intel.com>
Acked-by: Thomas Monjalon <thomas.monjalon@6wind.com>

drivers/net/ixgbe/ixgbe_ethdev.c
@@ -1,7 +1,7 @@
 /*-
  *   BSD LICENSE
  *
- *   Copyright(c) 2010-2014 Intel Corporation. All rights reserved.
+ *   Copyright(c) 2010-2015 Intel Corporation. All rights reserved.
  *   All rights reserved.
  *
  *   Redistribution and use in source and binary forms, with or without
@@ -62,9 +62,9 @@
 #include <rte_dev.h>
 
 #include "ixgbe_logs.h"
-#include "ixgbe/ixgbe_api.h"
-#include "ixgbe/ixgbe_vf.h"
-#include "ixgbe/ixgbe_common.h"
+#include "base/ixgbe_api.h"
+#include "base/ixgbe_vf.h"
+#include "base/ixgbe_common.h"
 #include "ixgbe_ethdev.h"
 #include "ixgbe_bypass.h"
 #include "ixgbe_rxtx.h"
@@ -461,7 +461,7 @@ rte_ixgbe_dev_atomic_write_link_status(struct rte_eth_dev *dev,
 }
 
 /*
- * This function is the same as ixgbe_is_sfp() in ixgbe/ixgbe.h.
+ * This function is the same as ixgbe_is_sfp() in base/ixgbe.h.
  */
 static inline int
 ixgbe_is_sfp(struct ixgbe_hw *hw)
@@ -509,7 +509,7 @@ ixgbe_enable_intr(struct rte_eth_dev *dev)
 }
 
 /*
- * This function is based on ixgbe_disable_intr() in ixgbe/ixgbe.h.
+ * This function is based on ixgbe_disable_intr() in base/ixgbe.h.
  */
 static void
 ixgbe_disable_intr(struct ixgbe_hw *hw)
@@ -710,7 +710,7 @@ ixgbe_swfw_lock_reset(struct ixgbe_hw *hw)
 }
 
 /*
- * This function is based on code in ixgbe_attach() in ixgbe/ixgbe.c.
+ * This function is based on code in ixgbe_attach() in base/ixgbe.c.
  * It returns 0 on success.
  */
 static int
@@ -1740,7 +1740,7 @@ ixgbe_dev_close(struct rte_eth_dev *dev)
 }
 
 /*
- * This function is based on ixgbe_update_stats_counters() in ixgbe/ixgbe.c
+ * This function is based on ixgbe_update_stats_counters() in base/ixgbe.c
  */
 static void
 ixgbe_dev_stats_get(struct rte_eth_dev *dev, struct rte_eth_stats *stats)

MAINTAINERS
@@ -221,7 +221,7 @@ F: doc/guides/nics/intel_vf.rst
 Intel ixgbe
 M: Helin Zhang <helin.zhang@intel.com>
 M: Konstantin Ananyev <konstantin.ananyev@intel.com>
-F: lib/librte_pmd_ixgbe/
+F: drivers/net/ixgbe/
 F: doc/guides/nics/ixgbe.rst
 F: doc/guides/nics/intel_vf.rst
 

drivers/net/Makefile
@@ -37,6 +37,7 @@ DIRS-$(CONFIG_RTE_LIBRTE_E1000_PMD) += e1000
 DIRS-$(CONFIG_RTE_LIBRTE_ENIC_PMD) += enic
 DIRS-$(CONFIG_RTE_LIBRTE_FM10K_PMD) += fm10k
 DIRS-$(CONFIG_RTE_LIBRTE_I40E_PMD) += i40e
+DIRS-$(CONFIG_RTE_LIBRTE_IXGBE_PMD) += ixgbe
 
 include $(RTE_SDK)/mk/rte.sharelib.mk
 include $(RTE_SDK)/mk/rte.subdir.mk

drivers/net/ixgbe/Makefile
@@ -1,6 +1,6 @@
 #   BSD LICENSE
 #
-#   Copyright(c) 2010-2014 Intel Corporation. All rights reserved.
+#   Copyright(c) 2010-2015 Intel Corporation. All rights reserved.
 #   All rights reserved.
 #
 #   Redistribution and use in source and binary forms, with or without
@@ -82,10 +82,10 @@ endif
 # Add extra flags for base driver files (also known as shared code)
 # to disable warnings in them
 #
-BASE_DRIVER_OBJS=$(patsubst %.c,%.o,$(notdir $(wildcard $(SRCDIR)/ixgbe/*.c)))
+BASE_DRIVER_OBJS=$(patsubst %.c,%.o,$(notdir $(wildcard $(SRCDIR)/base/*.c)))
 $(foreach obj, $(BASE_DRIVER_OBJS), $(eval CFLAGS_$(obj)+=$(CFLAGS_BASE_DRIVER)))
 
-VPATH += $(SRCDIR)/ixgbe
+VPATH += $(SRCDIR)/base
 
 #
 # all source are stored in SRCS-y

commit b644de979d6c2c4b3abacdd22b01f3599ae80572

net/ixgbe: fix mac resourece leak
In the eth_ixgbevf_dev_init and eth_ixgbe_dev_init functions, memory is
alloced for the MAC address, and the address is stored in the
eth_dev->data->mac_addrs member variable. If the subsequent function is
abnormal, you need to use the rte_free function to release the MAC
address memory.

Fixes: abf7275bbaa2 ("ixgbe: move to drivers/net/")
Cc: stable@dpdk.org

Signed-off-by: Qiming Chen <chenqiming_huawei@163.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/ixgbe/ixgbe_ethdev.c
@@ -1218,6 +1218,8 @@ eth_ixgbe_dev_init(struct rte_eth_dev *eth_dev, void *init_params __rte_unused)
 		PMD_INIT_LOG(ERR,
 			     "Failed to allocate %d bytes needed to store MAC addresses",
 			     RTE_ETHER_ADDR_LEN * IXGBE_VMDQ_NUM_UC_MAC);
+		rte_free(eth_dev->data->mac_addrs);
+		eth_dev->data->mac_addrs = NULL;
 		return -ENOMEM;
 	}
 
@@ -1670,6 +1672,8 @@ eth_ixgbevf_dev_init(struct rte_eth_dev *eth_dev)
 
 	default:
 		PMD_INIT_LOG(ERR, "VF Initialization Failure: %d", diag);
+		rte_free(eth_dev->data->mac_addrs);
+		eth_dev->data->mac_addrs = NULL;
 		return -EIO;
 	}
 