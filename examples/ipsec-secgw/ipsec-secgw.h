bed6a0f3024906a37be990d759e88a8e67a5f35f
Radu Nicolau <radu.nicolau@intel.com>
2021-09-15 14:45:16 +0100

    examples/ipsec-secgw: add support for telemetry
    Add telemetry support to the IPsec GW sample app
    
    Signed-off-by: Declan Doherty <declan.doherty@intel.com>
    Signed-off-by: Radu Nicolau <radu.nicolau@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- examples/ipsec-secgw/ipsec-secgw.h
+++ examples/ipsec-secgw/ipsec-secgw.h
@@ -83,7 +83,17 @@ struct ethaddr_info {
 	uint64_t src, dst;
 };
 
-#if (STATS_INTERVAL > 0)
+struct ipsec_spd_stats {
+	uint64_t protect;
+	uint64_t bypass;
+	uint64_t discard;
+};
+
+struct ipsec_sa_stats {
+	uint64_t hit;
+	uint64_t miss;
+};
+
 struct ipsec_core_statistics {
 	uint64_t tx;
 	uint64_t rx;
@@ -91,10 +101,29 @@ struct ipsec_core_statistics {
 	uint64_t tx_call;
 	uint64_t dropped;
 	uint64_t burst_rx;
+
+	struct {
+		struct ipsec_spd_stats spd4;
+		struct ipsec_spd_stats spd6;
+		struct ipsec_sa_stats sad;
+	} outbound;
+
+	struct {
+		struct ipsec_spd_stats spd4;
+		struct ipsec_spd_stats spd6;
+		struct ipsec_sa_stats sad;
+	} inbound;
+
+	struct {
+		uint64_t miss;
+	} lpm4;
+
+	struct {
+		uint64_t miss;
+	} lpm6;
 } __rte_cache_aligned;
 
 struct ipsec_core_statistics core_statistics[RTE_MAX_LCORE];
-#endif /* STATS_INTERVAL */
 
 extern struct ethaddr_info ethaddr_tbl[RTE_MAX_ETHPORTS];
 
