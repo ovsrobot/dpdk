161c58c2f0bf09b1b3b16383b313b39fe510a5aa
Bernard Iremonger <bernard.iremonger@intel.com>
2021-09-22 17:48:15 +0100

    flow_classify: remove eperimental tag from the API
    This API was introduced in 17.11, removing experimental tag
    to promote to stable state.
    
    Signed-off-by: Bernard Iremonger <bernard.iremonger@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/flow_classify/rte_flow_classify.h
+++ lib/flow_classify/rte_flow_classify.h
@@ -157,7 +157,6 @@ struct rte_flow_classify_ipv4_5tuple_stats {
  * @return
  *   Handle to flow classifier instance on success or NULL otherwise
  */
-__rte_experimental
 struct rte_flow_classifier *
 rte_flow_classifier_create(struct rte_flow_classifier_params *params);
 
@@ -169,7 +168,6 @@ rte_flow_classifier_create(struct rte_flow_classifier_params *params);
  * @return
  *   0 on success, error code otherwise
  */
-__rte_experimental
 int
 rte_flow_classifier_free(struct rte_flow_classifier *cls);
 
@@ -183,7 +181,6 @@ rte_flow_classifier_free(struct rte_flow_classifier *cls);
  * @return
  *   0 on success, error code otherwise
  */
-__rte_experimental
 int
 rte_flow_classify_table_create(struct rte_flow_classifier *cls,
 		struct rte_flow_classify_table_params *params);
@@ -205,7 +202,6 @@ rte_flow_classify_table_create(struct rte_flow_classifier *cls,
  * @return
  *   0 on success, error code otherwise
  */
-__rte_experimental
 int
 rte_flow_classify_validate(struct rte_flow_classifier *cls,
 		const struct rte_flow_attr *attr,
@@ -232,7 +228,6 @@ rte_flow_classify_validate(struct rte_flow_classifier *cls,
  * @return
  *   A valid handle in case of success, NULL otherwise.
  */
-__rte_experimental
 struct rte_flow_classify_rule *
 rte_flow_classify_table_entry_add(struct rte_flow_classifier *cls,
 		const struct rte_flow_attr *attr,
@@ -251,7 +246,6 @@ rte_flow_classify_table_entry_add(struct rte_flow_classifier *cls,
  * @return
  *   0 on success, error code otherwise.
  */
-__rte_experimental
 int
 rte_flow_classify_table_entry_delete(struct rte_flow_classifier *cls,
 		struct rte_flow_classify_rule *rule);
@@ -273,7 +267,6 @@ rte_flow_classify_table_entry_delete(struct rte_flow_classifier *cls,
  * @return
  *   0 on success, error code otherwise.
  */
-__rte_experimental
 int
 rte_flow_classifier_query(struct rte_flow_classifier *cls,
 		struct rte_mbuf **pkts,
