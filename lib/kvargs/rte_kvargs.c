208af7d3581e14af361fc03c69c2d4f8a62f7fbd
Olivier Matz <olivier.matz@6wind.com>
2021-09-29 23:39:41 +0200

    kvargs: new function to get from key and value
    A quite common scenario with kvargs is to lookup for a <key>=<value> in
    a kvlist. For instance, check if name=foo is present in
    name=toto,name=foo,name=bar. This is currently done in drivers/bus with
    rte_kvargs_process() + the rte_kvargs_strcmp() handler.
    
    This approach is not straightforward, and can be replaced by this new
    function.
    
    Signed-off-by: Olivier Matz <olivier.matz@6wind.com>
    Reviewed-by: Xueming Li <xuemingl@nvidia.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/kvargs/rte_kvargs.c
+++ lib/kvargs/rte_kvargs.c
@@ -204,19 +204,32 @@ rte_kvargs_free(struct rte_kvargs *kvlist)
 	free(kvlist);
 }
 
+/* Lookup a value in an rte_kvargs list by its key and value. */
+const char *
+rte_kvargs_get_with_value(const struct rte_kvargs *kvlist, const char *key,
+			  const char *value)
+{
+	unsigned int i;
+
+	if (kvlist == NULL)
+		return NULL;
+	for (i = 0; i < kvlist->count; ++i) {
+		if (key != NULL && strcmp(kvlist->pairs[i].key, key) != 0)
+			continue;
+		if (value != NULL && strcmp(kvlist->pairs[i].value, value) != 0)
+			continue;
+		return kvlist->pairs[i].value;
+	}
+	return NULL;
+}
+
 /* Lookup a value in an rte_kvargs list by its key. */
 const char *
 rte_kvargs_get(const struct rte_kvargs *kvlist, const char *key)
 {
-	unsigned int i;
-
 	if (kvlist == NULL || key == NULL)
 		return NULL;
-	for (i = 0; i < kvlist->count; ++i) {
-		if (strcmp(kvlist->pairs[i].key, key) == 0)
-			return kvlist->pairs[i].value;
-	}
-	return NULL;
+	return rte_kvargs_get_with_value(kvlist, key, NULL);
 }
 
 /*
