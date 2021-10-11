c4b241ff354f09aac9711ec1336c759a95c57e1d
Dmitry Kozlyuk <dkozlyuk@oss.nvidia.com>
2021-10-11 11:56:42 +0300

    eal/linux: make hugetlbfs analysis reusable
    get_hugepage_dir() searched for a hugetlbfs mount with a given page size
    using handcraft parsing of /proc/mounts and mixing traversal logic with
    selecting the needed entry. Separate code to enumerate hugetlbfs mounts
    to eal_hugepage_mount_walk() taking a callback that can inspect already
    parsed entries. Use mntent(3) API for parsing. This allows to reuse
    enumeration logic in subsequent patches.
    
    Signed-off-by: Dmitry Kozlyuk <dkozlyuk@nvidia.com>
    Reviewed-by: Viacheslav Ovsiienko <viacheslavo@nvidia.com>
    Reviewed-by: John Levon <john.levon@nutanix.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ lib/eal/linux/eal_hugepage_info.h
@@ -0,0 +1,39 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2021 NVIDIA Corporation & Affiliates
+ */
+
+#ifndef _EAL_HUGEPAGE_INFO_
+#define _EAL_HUGEPAGE_INFO_
+
+#include <stdint.h>
+
+/**
+ * Function called for each hugetlbfs mount point.
+ *
+ * @param path
+ *  Mount point directory.
+ * @param hugepage_sz
+ *  Hugepage size for the mount or default system hugepage size.
+ * @param arg
+ *  User data.
+ *
+ * @return
+ *  0 to continue walking, 1 to stop.
+ */
+typedef int (eal_hugepage_mount_walk_cb)(const char *path, uint64_t hugepage_sz,
+					 void *arg);
+
+/**
+ * Enumerate hugetlbfs mount points.
+ *
+ * @param cb
+ *  Function called for each mount point.
+ * @param cb_arg
+ *  User data passed to the callback.
+ *
+ * @return
+ *  0 on success, negative on failure.
+ */
+int eal_hugepage_mount_walk(eal_hugepage_mount_walk_cb *cb, void *cb_arg);
+
+#endif /* _EAL_HUGEPAGE_INFO_ */
