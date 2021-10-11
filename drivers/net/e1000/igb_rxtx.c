dfde98adc47865520cfea33dd32f463e7dfd1616
Ferruh Yigit <ferruh.yigit@intel.com>
2021-10-12 00:53:39 +0100

    ethdev: fix max Rx packet length
    There is a confusion on setting max Rx packet length, this patch aims to
    clarify it.
    
    'rte_eth_dev_configure()' API accepts max Rx packet size via
    'uint32_t max_rx_pkt_len' field of the config struct 'struct
    rte_eth_conf'.
    
    Also 'rte_eth_dev_set_mtu()' API can be used to set the MTU, and result
    stored into '(struct rte_eth_dev)->data->mtu'.
    
    These two APIs are related but they work in a disconnected way, they
    store the set values in different variables which makes hard to figure
    out which one to use, also having two different method for a related
    functionality is confusing for the users.
    
    Other issues causing confusion is:
    * maximum transmission unit (MTU) is payload of the Ethernet frame. And
      'max_rx_pkt_len' is the size of the Ethernet frame. Difference is
      Ethernet frame overhead, and this overhead may be different from
      device to device based on what device supports, like VLAN and QinQ.
    * 'max_rx_pkt_len' is only valid when application requested jumbo frame,
      which adds additional confusion and some APIs and PMDs already
      discards this documented behavior.
    * For the jumbo frame enabled case, 'max_rx_pkt_len' is an mandatory
      field, this adds configuration complexity for application.
    
    As solution, both APIs gets MTU as parameter, and both saves the result
    in same variable '(struct rte_eth_dev)->data->mtu'. For this
    'max_rx_pkt_len' updated as 'mtu', and it is always valid independent
    from jumbo frame.
    
    For 'rte_eth_dev_configure()', 'dev->data->dev_conf.rxmode.mtu' is user
    request and it should be used only within configure function and result
    should be stored to '(struct rte_eth_dev)->data->mtu'. After that point
    both application and PMD uses MTU from this variable.
    
    When application doesn't provide an MTU during 'rte_eth_dev_configure()'
    default 'RTE_ETHER_MTU' value is used.
    
    Additional clarification done on scattered Rx configuration, in
    relation to MTU and Rx buffer size.
    MTU is used to configure the device for physical Rx/Tx size limitation,
    Rx buffer is where to store Rx packets, many PMDs use mbuf data buffer
    size as Rx buffer size.
    PMDs compare MTU against Rx buffer size to decide enabling scattered Rx
    or not. If scattered Rx is not supported by device, MTU bigger than Rx
    buffer size should fail.
    
    Signed-off-by: Ferruh Yigit <ferruh.yigit@intel.com>
    Acked-by: Ajit Khaparde <ajit.khaparde@broadcom.com>
    Acked-by: Somnath Kotur <somnath.kotur@broadcom.com>
    Acked-by: Huisong Li <lihuisong@huawei.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/e1000/igb_rxtx.c
+++ drivers/net/e1000/igb_rxtx.c
@@ -2329,6 +2329,7 @@ eth_igb_rx_init(struct rte_eth_dev *dev)
 	uint32_t srrctl;
 	uint16_t buf_size;
 	uint16_t rctl_bsize;
+	uint32_t max_len;
 	uint16_t i;
 	int ret;
 
@@ -2347,9 +2348,8 @@ eth_igb_rx_init(struct rte_eth_dev *dev)
 	/*
 	 * Configure support of jumbo frames, if any.
 	 */
+	max_len = dev->data->mtu + E1000_ETH_OVERHEAD;
 	if (dev->data->dev_conf.rxmode.offloads & DEV_RX_OFFLOAD_JUMBO_FRAME) {
-		uint32_t max_len = dev->data->dev_conf.rxmode.max_rx_pkt_len;
-
 		rctl |= E1000_RCTL_LPE;
 
 		/*
@@ -2427,8 +2427,7 @@ eth_igb_rx_init(struct rte_eth_dev *dev)
 					       E1000_SRRCTL_BSIZEPKT_SHIFT);
 
 			/* It adds dual VLAN length for supporting dual VLAN */
-			if ((dev->data->dev_conf.rxmode.max_rx_pkt_len +
-						2 * VLAN_TAG_SIZE) > buf_size){
+			if ((max_len + 2 * VLAN_TAG_SIZE) > buf_size) {
 				if (!dev->data->scattered_rx)
 					PMD_INIT_LOG(DEBUG,
 						     "forcing scatter mode");
@@ -2652,15 +2651,15 @@ eth_igbvf_rx_init(struct rte_eth_dev *dev)
 	uint32_t srrctl;
 	uint16_t buf_size;
 	uint16_t rctl_bsize;
+	uint32_t max_len;
 	uint16_t i;
 	int ret;
 
 	hw = E1000_DEV_PRIVATE_TO_HW(dev->data->dev_private);
 
 	/* setup MTU */
-	e1000_rlpml_set_vf(hw,
-		(uint16_t)(dev->data->dev_conf.rxmode.max_rx_pkt_len +
-		VLAN_TAG_SIZE));
+	max_len = dev->data->mtu + E1000_ETH_OVERHEAD;
+	e1000_rlpml_set_vf(hw, (uint16_t)(max_len + VLAN_TAG_SIZE));
 
 	/* Configure and enable each RX queue. */
 	rctl_bsize = 0;
@@ -2717,8 +2716,7 @@ eth_igbvf_rx_init(struct rte_eth_dev *dev)
 					       E1000_SRRCTL_BSIZEPKT_SHIFT);
 
 			/* It adds dual VLAN length for supporting dual VLAN */
-			if ((dev->data->dev_conf.rxmode.max_rx_pkt_len +
-						2 * VLAN_TAG_SIZE) > buf_size){
+			if ((max_len + 2 * VLAN_TAG_SIZE) > buf_size) {
 				if (!dev->data->scattered_rx)
 					PMD_INIT_LOG(DEBUG,
 						     "forcing scatter mode");
