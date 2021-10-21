6e602db0550c70242ea79f20d13ad3c8f062157d
Rongwei Liu <rongweil@nvidia.com>
2021-10-21 11:56:36 +0300

    net/mlx5: set txq affinity in round-robin
    Previously, we set txq affinity to 0 and let firmware
    to perform round-robin when bonding. Firmware uses a
    global counter to assign txq affinity to different
    physical ports accord to remainder after division.
    
    There are three dis-advantages:
    1. The global counter is shared between kernel and dpdk.
    2. After restarting pmd or port, the previous counter value
    is reused, so the new affinity is unpredictable.
    3. There is no way to get what affinity is set by firmware.
    
    In this update, we will create several TISs up to the
    number of bonding ports and bind each TIS to one PF port.
    
    For each port, it will start to pick up TIS using its port
    index. Upper layer application can quickly calculate each txq's
    affinity without querying.
    
    At DPDK layer, when creating txq with 2 bonding ports, the
    affinity is set like:
    port 0: 1-->2-->1-->2
    port 1: 2-->1-->2-->1
    port 2: 1-->2-->1-->2
    
    Note: Only applicable to DevX api.
    This affinity subjects to HW hash.
    
    Signed-off-by: Rongwei Liu <rongweil@nvidia.com>
    Acked-by: Matan Azrad <matan@nvidia.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/mlx5/linux/mlx5_os.c
+++ drivers/net/mlx5/linux/mlx5_os.c
@@ -928,7 +928,6 @@ mlx5_representor_match(struct mlx5_dev_spawn_data *spawn,
 	return false;
 }
 
-
 /**
  * Spawn an Ethernet device from Verbs information.
  *
@@ -1706,6 +1705,7 @@ err_secondary:
 	 */
 	MLX5_ASSERT(spawn->ifindex);
 	priv->if_index = spawn->ifindex;
+	priv->lag_affinity_idx = sh->refcnt - 1;
 	eth_dev->data->dev_private = priv;
 	priv->dev_data = eth_dev->data;
 	eth_dev->data->mac_addrs = priv->mac;
