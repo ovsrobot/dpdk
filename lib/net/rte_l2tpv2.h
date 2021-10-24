ovsrobot/dpdk

commit 3a929df1f28684c6ba2dd2151a39eb232f95f4ed

ethdev: support L2TPv2 and PPP procotol
Added flow pattern items and header formats of L2TPv2 and PPP.

Signed-off-by: Wenjun Wu <wenjun1.wu@intel.com>
Signed-off-by: Jie Wang <jie1x.wang@intel.com>
Acked-by: Ori Kam <orika@nvidia.com>
Acked-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Reviewed-by: Ferruh Yigit <ferruh.yigit@intel.com>

lib/net/rte_l2tpv2.h
@@ -1,0 +1,234 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2021 Intel Corporation.
+ */
+
+#ifndef _RTE_L2TPV2_H_
+#define _RTE_L2TPV2_H_
+
+/**
+ * @file
+ *
+ * L2TP header:
+ *
+ * `-0--------------------1----------------2-------------------3`
+ *
+ * `-0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1`
+ *
+ * `+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+`
+ *
+ * `|T|L|x|x|S|x|O|P|x|x|x|x|--Ver--|-----------Length (opt)--------|`
+ *
+ * `+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+`
+ *
+ * `|-----------Tunnel ID-----------|-----------Session ID----------|`
+ *
+ * `+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+`
+ *
+ * `|-----------Ns (opt)------------|-----------Nr (opt)------------|`
+ *
+ * `+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+`
+ *
+ * `|---------Offset Size (opt)-----|---------Offset pad... (opt)`
+ *
+ * `+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+`
+ *
+ * The Type (T) bit indicates the type of message. It is set to 0 for a data
+ * message and 1 for a control message.
+ *
+ * If the Length (L) bit is 1, the Length field is present. This bit MUST be
+ * set to 1 for control messages.
+ *
+ * The x bits are reserved for future extensions. All reserved bits MUST
+ * be set to 0 on outgoing messages and ignored on incoming messages.
+ *
+ * If the Sequence (S) bit is set to 1 the Ns and Nr fields are present.
+ * The S bit MUST be set to 1 for control messages.
+ *
+ * If the Offset (O) bit is 1, the Offset Size field is present. The O
+ * bit MUST be set to 0 for control messages.
+ *
+ * If the Priority (P) bit is 1, this data message should receive
+ * preferential treatment in its local queuing and transmission.
+ * The P bit MUST be set to 0 for control messages.
+ *
+ * Ver MUST be 2, indicating the version of the L2TP data message header.
+ *
+ * The Length field indicates the total length of the message in octets.
+ *
+ * Tunnel ID indicates the identifier for the control connection.
+ *
+ * Session ID indicates the identifier for a session within a tunnel.
+ *
+ * Ns indicates the sequence number for this data or control message.
+ *
+ * Nr indicates the sequence number expected in the next control message
+ * to be received.
+ *
+ * The Offset Size field, if present, specifies the number of octets
+ * past the L2TP header at which the payload data is expected to start.
+ * Actual data within the offset padding is undefined. If the offset
+ * field is present, the L2TP header ends after the last octet of the
+ * offset padding.
+ */
+
+#include <stdint.h>
+#include <rte_byteorder.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * L2TPv2 Common Header
+ */
+RTE_STD_C11
+struct rte_l2tpv2_common_hdr {
+	union {
+		/** header flags and protocol version */
+		rte_be16_t flags_version;
+		struct {
+#if RTE_BYTE_ORDER == RTE_LITTLE_ENDIAN
+			rte_be16_t t:1;		/**< message Type */
+			rte_be16_t l:1;		/**< length option bit */
+			rte_be16_t res1:2;	/**< reserved */
+			rte_be16_t s:1;		/**< ns/nr option bit */
+			rte_be16_t res2:1;	/**< reserved */
+			rte_be16_t o:1;		/**< offset option bit */
+			rte_be16_t p:1;		/**< priority option bit */
+			rte_be16_t res3:4;	/**< reserved */
+			rte_be16_t ver:4;	/**< protocol version */
+#elif RTE_BYTE_ORDER == RTE_BIG_ENDIAN
+			rte_be16_t ver:4;	/**< protocol version */
+			rte_be16_t res3:4;	/**< reserved */
+			rte_be16_t p:1;		/**< priority option bit */
+			rte_be16_t o:1;		/**< offset option bit */
+			rte_be16_t res2:1;	/**< reserved */
+			rte_be16_t s:1;		/**< ns/nr option bit */
+			rte_be16_t res1:2;	/**< reserved */
+			rte_be16_t l:1;		/**< length option bit */
+			rte_be16_t t:1;		/**< message Type */
+#endif
+		};
+	};
+};
+
+/**
+ * L2TPv2 message Header contains all options(length, ns, nr,
+ * offset size, offset padding).
+ */
+struct rte_l2tpv2_msg_with_all_options {
+	rte_be16_t length;		/**< length(16) */
+	rte_be16_t tunnel_id;		/**< tunnel ID(16) */
+	rte_be16_t session_id;		/**< session ID(16) */
+	rte_be16_t ns;			/**< Ns(16) */
+	rte_be16_t nr;			/**< Nr(16) */
+	rte_be16_t offset_size;		/**< offset size(16) */
+	uint8_t   *offset_padding;	/**< offset padding(variable length) */
+} __rte_packed;
+
+/**
+ * L2TPv2 message Header contains all options except length(ns, nr,
+ * offset size, offset padding).
+ */
+struct rte_l2tpv2_msg_without_length {
+	rte_be16_t tunnel_id;		/**< tunnel ID(16) */
+	rte_be16_t session_id;		/**< session ID(16) */
+	rte_be16_t ns;			/**< Ns(16) */
+	rte_be16_t nr;			/**< Nr(16) */
+	rte_be16_t offset_size;		/**< offset size(16) */
+	uint8_t   *offset_padding;	/**< offset padding(variable length) */
+} __rte_packed;
+
+/**
+ * L2TPv2 message Header contains all options except ns_nr(length,
+ * offset size, offset padding).
+ * Ns and Nr MUST be toghter.
+ */
+struct rte_l2tpv2_msg_without_ns_nr {
+	rte_be16_t length;		/**< length(16) */
+	rte_be16_t tunnel_id;		/**< tunnel ID(16) */
+	rte_be16_t session_id;		/**< session ID(16) */
+	rte_be16_t offset_size;		/**< offset size(16) */
+	uint8_t   *offset_padding;	/**< offset padding(variable length) */
+};
+
+/**
+ * L2TPv2 message Header contains all options except ns_nr(length, ns, nr).
+ * offset size and offset padding MUST be toghter.
+ */
+struct rte_l2tpv2_msg_without_offset {
+	rte_be16_t length;		/**< length(16) */
+	rte_be16_t tunnel_id;		/**< tunnel ID(16) */
+	rte_be16_t session_id;		/**< session ID(16) */
+	rte_be16_t ns;			/**< Ns(16) */
+	rte_be16_t nr;			/**< Nr(16) */
+};
+
+/**
+ * L2TPv2 message Header contains options offset size and offset padding.
+ */
+struct rte_l2tpv2_msg_with_offset {
+	rte_be16_t tunnel_id;		/**< tunnel ID(16) */
+	rte_be16_t session_id;		/**< session ID(16) */
+	rte_be16_t offset_size;		/**< offset size(16) */
+	uint8_t   *offset_padding;	/**< offset padding(variable length) */
+} __rte_packed;
+
+/**
+ * L2TPv2 message Header contains options ns and nr.
+ */
+struct rte_l2tpv2_msg_with_ns_nr {
+	rte_be16_t tunnel_id;		/**< tunnel ID(16) */
+	rte_be16_t session_id;		/**< session ID(16) */
+	rte_be16_t ns;			/**< Ns(16) */
+	rte_be16_t nr;			/**< Nr(16) */
+};
+
+/**
+ * L2TPv2 message Header contains option length.
+ */
+struct rte_l2tpv2_msg_with_length {
+	rte_be16_t length;		/**< length(16) */
+	rte_be16_t tunnel_id;		/**< tunnel ID(16) */
+	rte_be16_t session_id;		/**< session ID(16) */
+};
+
+/**
+ * L2TPv2 message Header without all options.
+ */
+struct rte_l2tpv2_msg_without_all_options {
+	rte_be16_t tunnel_id;		/**< tunnel ID(16) */
+	rte_be16_t session_id;		/**< session ID(16) */
+};
+
+/**
+ * L2TPv2 Combined Message Header Format: Common Header + Options
+ */
+RTE_STD_C11
+struct rte_l2tpv2_combined_msg_hdr {
+	struct rte_l2tpv2_common_hdr common; /**< common header */
+	union {
+		/** header with all options */
+		struct rte_l2tpv2_msg_with_all_options type0;
+		/** header with all options except length */
+		struct rte_l2tpv2_msg_without_length type1;
+		/** header with all options except ns/nr */
+		struct rte_l2tpv2_msg_without_ns_nr type2;
+		/** header with all options except offset */
+		struct rte_l2tpv2_msg_without_offset type3;
+		/** header with offset options */
+		struct rte_l2tpv2_msg_with_offset type4;
+		/** header with ns/nr options */
+		struct rte_l2tpv2_msg_with_ns_nr type5;
+		/** header with length option */
+		struct rte_l2tpv2_msg_with_length type6;
+		/** header without all options */
+		struct rte_l2tpv2_msg_without_all_options type7;
+	};
+} __rte_packed;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RTE_L2TPV2_H_ */

doc/guides/nics/features/default.ini
@@ -110,6 +110,7 @@ ipv4                 =
 ipv6                 =
 ipv6_ext             =
 ipv6_frag_ext        =
+l2tpv2               =
 l2tpv3oip            =
 mark                 =
 meta                 =
@@ -121,6 +122,7 @@ pfcp                 =
 phy_port             =
 port_id              =
 port_representor     =
+ppp                  =
 pppoed               =
 pppoes               =
 pppoe_proto_id       =

lib/ethdev/rte_flow.c
@@ -156,6 +156,8 @@ static const struct rte_flow_desc_data rte_flow_desc_item[] = {
 	MK_FLOW_ITEM(REPRESENTED_PORT, sizeof(struct rte_flow_item_ethdev)),
 	MK_FLOW_ITEM_FN(FLEX, sizeof(struct rte_flow_item_flex),
 			rte_flow_item_flex_conv),
+	MK_FLOW_ITEM(L2TPV2, sizeof(struct rte_flow_item_l2tpv2)),
+	MK_FLOW_ITEM(PPP, sizeof(struct rte_flow_item_ppp)),
 };
 
 /** Generate flow_action[] entry. */

lib/ethdev/rte_flow.h
@@ -35,6 +35,8 @@
 #include <rte_mbuf_dyn.h>
 #include <rte_meter.h>
 #include <rte_gtp.h>
+#include <rte_l2tpv2.h>
+#include <rte_ppp.h>
 
 #ifdef __cplusplus
 extern "C" {
@@ -644,6 +646,20 @@ enum rte_flow_item_type {
 	 * @see struct rte_flow_item_flex.
 	 */
 	RTE_FLOW_ITEM_TYPE_FLEX,
+
+	/**
+	 * Matches L2TPv2 Header.
+	 *
+	 * See struct rte_flow_item_l2tpv2.
+	 */
+	RTE_FLOW_ITEM_TYPE_L2TPV2,
+
+	/**
+	 * Matches PPP Header.
+	 *
+	 * See struct rte_flow_item_ppp.
+	 */
+	RTE_FLOW_ITEM_TYPE_PPP,
 };
 
 /**
@@ -1897,6 +1913,57 @@ struct rte_flow_item_ethdev {
 #ifndef __cplusplus
 static const struct rte_flow_item_ethdev rte_flow_item_ethdev_mask = {
 	.port_id = 0xffff,
+};
+#endif
+
+/**
+ * @warning
+ * @b EXPERIMENTAL: this structure may change without prior notice
+ *
+ * RTE_FLOW_ITEM_TYPE_L2TPV2
+ *
+ * Matches L2TPv2 Header
+ */
+struct rte_flow_item_l2tpv2 {
+	struct rte_l2tpv2_combined_msg_hdr hdr;
+};
+
+/** Default mask for RTE_FLOW_ITEM_TYPE_L2TPV2. */
+#ifndef __cplusplus
+static const struct rte_flow_item_l2tpv2 rte_flow_item_l2tpv2_mask = {
+	/*
+	 * flags and version bit mask
+	 * 7 6 5 4 3 2 1 0 7 6 5 4 3 2 1 0
+	 * T L x x S x O P x x x x V V V V
+	 */
+	.hdr = {
+		.common = {
+			.flags_version = RTE_BE16(0xcb0f),
+		},
+	},
+};
+#endif
+
+/**
+ * @warning
+ * @b EXPERIMENTAL: this structure may change without prior notice
+ *
+ * RTE_FLOW_ITEM_TYPE_PPP
+ *
+ * Matches PPP Header
+ */
+struct rte_flow_item_ppp {
+	struct rte_ppp_hdr hdr;
+};
+
+/** Default mask for RTE_FLOW_ITEM_TYPE_PPP. */
+#ifndef __cplusplus
+static const struct rte_flow_item_ppp rte_flow_item_ppp_mask = {
+	.hdr = {
+		.addr = 0xff,
+		.ctrl = 0xff,
+		.proto_id = RTE_BE16(0xffff),
+	}
 };
 #endif
 

lib/net/meson.build
@@ -19,6 +19,8 @@ headers = files(
         'rte_higig.h',
         'rte_ecpri.h',
         'rte_geneve.h',
+        'rte_l2tpv2.h',
+        'rte_ppp.h',
 )
 
 sources = files(

lib/net/rte_ppp.h
@@ -1,0 +1,34 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2021 Intel Corporation.
+ */
+
+#ifndef _RTE_PPP_H_
+#define _RTE_PPP_H_
+
+/**
+ * @file
+ *
+ * PPP headers definition.
+ */
+
+#include <stdint.h>
+#include <rte_byteorder.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * PPP Header
+ */
+struct rte_ppp_hdr {
+	uint8_t addr; /**< PPP address(8) */
+	uint8_t ctrl; /**< PPP control(8) */
+	rte_be16_t proto_id; /**< PPP protocol identifier(16) */
+} __rte_packed;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RTE_PPP_H_ */

commit 2a81e381734cbeea75aa7920dfd15b14111a909b

net: fix pedantic build for L2TPv2 definitions
Build is broken on RHEL7 following introduction of this new protocol.

Fixes: 3a929df1f286 ("ethdev: support L2TPv2 and PPP procotol")

Signed-off-by: David Marchand <david.marchand@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/net/rte_l2tpv2.h
@@ -86,6 +86,7 @@ struct rte_l2tpv2_common_hdr {
 	union {
 		/** header flags and protocol version */
 		rte_be16_t flags_version;
+		__extension__
 		struct {
 #if RTE_BYTE_ORDER == RTE_LITTLE_ENDIAN
 			rte_be16_t t:1;		/**< message Type */