7a198c9808c8548b5f7f8f4b98aa5f748aea28ad
Radha Mohan Chintakuntla <radhac@marvell.com>
2021-10-25 21:12:58 -0700

    dma/cnxk: create and initialize dmadev on pci probe
    This patch creates and initializes a dmadev device on pci probe.
    
    Signed-off-by: Radha Mohan Chintakuntla <radhac@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- doc/guides/dmadevs/index.rst
+++ doc/guides/dmadevs/index.rst
@@ -11,5 +11,6 @@ an application through DMA API.
    :maxdepth: 2
    :numbered:
 
+   cnxk
    idxd
    ioat
---
+++ drivers/dma/cnxk/cnxk_dmadev.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(C) 2021 Marvell International Ltd.
+ */
+#ifndef _CNXK_DMADEV_H_
+#define _CNXK_DMADEV_H_
+
+struct cnxk_dpi_vf_s {
+	struct roc_dpi rdpi;
+};
+
+#endif
