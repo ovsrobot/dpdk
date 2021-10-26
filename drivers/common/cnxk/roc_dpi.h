7b42797dfeb03f1ede55e59295596c4eb83e2f82
Radha Mohan Chintakuntla <radhac@marvell.com>
2021-10-25 21:12:57 -0700

    common/cnxk: add DPI DMA support
    Add base support as ROC(Rest of Chip) API which will be used by PMD
    dmadev driver.
    
    This patch adds routines to init, fini, configure the DPI DMA device
    found in Marvell's CN9k or CN10k SoC familes.
    
    Signed-off-by: Radha Mohan Chintakuntla <radhac@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/common/cnxk/roc_dpi.h
@@ -0,0 +1,44 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(C) 2021 Marvell.
+ */
+
+#ifndef _ROC_DPI_H_
+#define _ROC_DPI_H_
+
+struct roc_dpi_args {
+	uint8_t num_ssegs;
+	uint8_t num_dsegs;
+	uint8_t comp_type;
+	uint8_t direction;
+	uint8_t sdevice;
+	uint8_t ddevice;
+	uint8_t swap;
+	uint8_t use_lock : 1;
+	uint8_t tt : 7;
+	uint16_t func;
+	uint16_t grp;
+	uint32_t tag;
+	uint64_t comp_ptr;
+};
+
+struct roc_dpi {
+	struct plt_pci_device *pci_dev;
+	const struct plt_memzone *mz;
+	uint8_t *rbase;
+	uint16_t vfid;
+	uint16_t pool_size_m1;
+	uint16_t chunk_head;
+	uint64_t *chunk_base;
+	uint64_t *chunk_next;
+	uint64_t aura_handle;
+	plt_spinlock_t chunk_lock;
+} __plt_cache_aligned;
+
+int __roc_api roc_dpi_dev_init(struct roc_dpi *roc_dpi);
+int __roc_api roc_dpi_dev_fini(struct roc_dpi *roc_dpi);
+
+int __roc_api roc_dpi_queue_configure(struct roc_dpi *dpi);
+int __roc_api roc_dpi_queue_start(struct roc_dpi *dpi);
+int __roc_api roc_dpi_queue_stop(struct roc_dpi *dpi);
+
+#endif
