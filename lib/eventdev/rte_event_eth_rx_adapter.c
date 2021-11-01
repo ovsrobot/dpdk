2e2954295f53413b87e88aa870b04268086393a1
Mattias Rönnblom <mattias.ronnblom@ericsson.com>
2021-11-01 19:40:16 +0100

    eventdev: have adapters support device maintenance
    Introduce support for event devices requiring calls to
    rte_event_maintain() in the Ethernet RX, Timer and Crypto Eventdev
    adapters.
    
    PATCH v3:
      - Set the new 'op' rte_event_maintain() parameter to zero.
    RFC v2:
      - For simplicity, the timer adapter now unconditionally calls
        rte_event_maintain().
      - The RX adapter now only calls rte_event_maintain() when it has not
        enqueued any events.
    
    Signed-off-by: Mattias Rönnblom <mattias.ronnblom@ericsson.com>
    Tested-by: Richard Eklycke <richard.eklycke@ericsson.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eventdev/rte_event_eth_rx_adapter.c
+++ lib/eventdev/rte_event_eth_rx_adapter.c
@@ -992,6 +992,7 @@ rxa_eth_rx(struct event_eth_rx_adapter *rx_adapter, uint16_t port_id,
 					&rx_adapter->stats;
 	uint16_t n;
 	uint32_t nb_rx = 0;
+	uint32_t nb_flushed = 0;
 
 	if (rxq_empty)
 		*rxq_empty = 0;
@@ -1000,7 +1001,7 @@ rxa_eth_rx(struct event_eth_rx_adapter *rx_adapter, uint16_t port_id,
 	 */
 	while (rxa_pkt_buf_available(buf)) {
 		if (buf->count >= BATCH_SIZE)
-			rxa_flush_event_buffer(rx_adapter, buf);
+			nb_flushed += rxa_flush_event_buffer(rx_adapter, buf);
 
 		stats->rx_poll_count++;
 		n = rte_eth_rx_burst(port_id, queue_id, mbufs, BATCH_SIZE);
@@ -1016,7 +1017,11 @@ rxa_eth_rx(struct event_eth_rx_adapter *rx_adapter, uint16_t port_id,
 	}
 
 	if (buf->count > 0)
-		rxa_flush_event_buffer(rx_adapter, buf);
+		nb_flushed += rxa_flush_event_buffer(rx_adapter, buf);
+
+	if (nb_flushed == 0)
+		rte_event_maintain(rx_adapter->eventdev_id,
+				   rx_adapter->event_port_id, 0);
 
 	return nb_rx;
 }
