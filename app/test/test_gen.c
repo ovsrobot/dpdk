957b83a52686f07fece2044e1fe53d7e3f19bdb7
Harry van Haaren <harry.van.haaren@intel.com>
2021-12-14 14:12:35 +0000

    gen: add raw packet data API and tests
    This commit adds a new API to gen, allowing the caller to set
    raw packet data with a size. Tests are added to test the new
    API with randomized packet data.
    
    Signed-off-by: Harry van Haaren <harry.van.haaren@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- app/test/test_gen.c
+++ app/test/test_gen.c
@@ -5,6 +5,7 @@
 #include <rte_common.h>
 #include <rte_gen.h>
 #include <rte_mbuf.h>
+#include <rte_random.h>
 
 #include "test.h"
 
@@ -75,6 +76,37 @@ test_gen_loop_rxtx(void)
 
 		total_sent += nb_tx;
 	}
+	rte_gen_destroy(gen);
+	return 0;
+}
+
+static int
+test_gen_packet_set_raw(void)
+{
+	struct rte_gen *gen = rte_gen_create(mp);
+	TEST_ASSERT_FAIL(gen, "Expected valid pointer after create()");
+
+	/* Set a raw packet payload, and ensure the next received packet has
+	 * that packet data as contents and size.
+	 */
+	uint64_t pkt_data[8];
+	uint32_t i;
+	for (i = 0; i < 8; i++)
+		pkt_data[i] = rte_rand();
+
+	int32_t err = rte_gen_packet_set_raw(gen, (void *)pkt_data, 64);
+	TEST_ASSERT_EQUAL(err, 0, "Expected set raw() to return success.");
+
+	struct rte_mbuf *bufs[BURST_MAX];
+	uint16_t nb_rx = rte_gen_rx_burst(gen, bufs, 1);
+	TEST_ASSERT_EQUAL(nb_rx, 1, "Expected rx packet burst.");
+
+	void *mbuf_data = rte_pktmbuf_mtod(bufs[0], void *);
+	int32_t data_equal = memcmp(pkt_data, mbuf_data, 64) == 0;
+	TEST_ASSERT_EQUAL(data_equal, 1,
+		"Expected packet data equal to input data.");
+
+	rte_pktmbuf_free(bufs[0]);
 
 	rte_gen_destroy(gen);
 	return 0;
@@ -88,6 +120,7 @@ static struct unit_test_suite gen_suite  = {
 		TEST_CASE_ST(NULL, NULL, test_gen_create),
 		TEST_CASE_ST(NULL, NULL, test_gen_basic_rxtx),
 		TEST_CASE_ST(NULL, NULL, test_gen_loop_rxtx),
+		TEST_CASE_ST(NULL, NULL, test_gen_packet_set_raw),
 		TEST_CASES_END() /**< NULL terminate unit test array */
 	}
 };
