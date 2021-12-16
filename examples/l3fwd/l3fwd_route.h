b512df5feb41bfd6a4de66e8e306532df1a0e2e3
Sean Morrissey <sean.morrissey@intel.com>
2021-12-16 15:01:04 +0000

    examples/l3fwd: add config file support for EM
    Add support to define ipv4 and ipv6 forwarding tables
    from reading from a config file for EM with a format
    similar to l3fwd-acl one.
    
    With the removal of the hardcoded route tables for IPv4
    and IPv6 from 'l3fwd_em', these routes have been moved
    to a separate default config file for use with EM.
    
    Related l3fwd docs have been updated to relfect these
    changes.
    
    Signed-off-by: Sean Morrissey <sean.morrissey@intel.com>
    Signed-off-by: Ravi Kerur <rkerur@gmail.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- examples/l3fwd/l3fwd_route.h
+++ examples/l3fwd/l3fwd_route.h
@@ -2,17 +2,27 @@
  * Copyright(c) 2021 Intel Corporation
  */
 
-struct ipv4_l3fwd_route {
-	uint32_t ip;
-	uint8_t  depth;
-	uint8_t  if_out;
-};
+struct ipv4_5tuple {
+	uint32_t ip_dst;
+	uint32_t ip_src;
+	uint16_t port_dst;
+	uint16_t port_src;
+	uint8_t  proto;
+} __rte_packed;
 
-struct ipv6_l3fwd_route {
-	uint8_t ip[16];
-	uint8_t depth;
-	uint8_t if_out;
-};
+struct ipv6_5tuple {
+	union {
+		uint8_t  ip_dst[IPV6_ADDR_LEN];
+		uint32_t ip32_dst[4];
+	};
+	union {
+		uint8_t  ip_src[IPV6_ADDR_LEN];
+		uint32_t ip32_src[4];
+	};
+	uint16_t port_dst;
+	uint16_t port_src;
+	uint8_t  proto;
+} __rte_packed;
 
 struct lpm_route_rule {
 	union {
@@ -23,6 +33,14 @@ struct lpm_route_rule {
 		};
 	};
 	uint8_t depth;
+	uint8_t if_out;
+};
+
+struct em_rule {
+		union {
+		struct ipv4_5tuple v4_key;
+		struct ipv6_5tuple v6_key;
+	};
 	uint8_t if_out;
 };
 
