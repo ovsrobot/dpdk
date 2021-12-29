47a61fa9d04419dbd8505e6642656417dbfd3467
Yanling Song <songyl@ramaxel.com>
2021-12-29 21:37:33 +0800

    net/spnic: add mgmt module
    Mgmt module manage the message gerenated from the hardware.
    This patch implements mgmt module initialization, related event
    processing and message command definition.
    
    Signed-off-by: Yanling Song <songyl@ramaxel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/spnic/base/spnic_mgmt.h
+++ drivers/net/spnic/base/spnic_mgmt.h
@@ -7,6 +7,13 @@
 
 #define SPNIC_MSG_HANDLER_RES	(-1)
 
+/* Structures for l2nic and mag msg to mgmt sync interface */
+struct mgmt_msg_head {
+	u8 status;
+	u8 version;
+	u8 rsvd0[6];
+};
+
 /* Cmdq module type */
 enum spnic_mod_type {
 	SPNIC_MOD_COMM = 0, /* HW communication module */
@@ -32,5 +39,72 @@ enum spnic_mod_type {
 	SPNIC_MOD_IOE = 18,
 	SPNIC_MOD_MAX
 };
+
+typedef enum {
+	RES_TYPE_FLUSH_BIT = 0,
+	RES_TYPE_MQM,
+	RES_TYPE_SMF,
+
+	RES_TYPE_COMM = 10,
+	/* clear mbox and aeq, The RES_TYPE_COMM bit must be set */
+	RES_TYPE_COMM_MGMT_CH,
+	/* clear cmdq, The RES_TYPE_COMM bit must be set */
+	RES_TYPE_COMM_CMD_CH,
+	RES_TYPE_NIC,
+	RES_TYPE_OVS,
+	RES_TYPE_MAX = 20,
+} func_reset_flag_e;
+
+#define SPNIC_COMM_RES		((1 << RES_TYPE_COMM) | \
+				(1 << RES_TYPE_FLUSH_BIT) | \
+				(1 << RES_TYPE_MQM) | \
+				(1 << RES_TYPE_SMF) | \
+				(1 << RES_TYPE_COMM_CMD_CH))
+#define SPNIC_NIC_RES		(1 << RES_TYPE_NIC)
+#define SPNIC_OVS_RES		(1 << RES_TYPE_OVS)
+
+struct spnic_recv_msg {
+	void *msg;
+
+	u16 msg_len;
+	enum spnic_mod_type mod;
+	u16 cmd;
+	u8 seq_id;
+	u16 msg_id;
+	int async_mgmt_to_pf;
+};
+
+enum comm_pf_to_mgmt_event_state {
+	SEND_EVENT_UNINIT = 0,
+	SEND_EVENT_START,
+	SEND_EVENT_SUCCESS,
+	SEND_EVENT_FAIL,
+	SEND_EVENT_TIMEOUT,
+	SEND_EVENT_END
+};
+
+struct spnic_msg_pf_to_mgmt {
+	struct spnic_hwdev *hwdev;
+
+	/* Mutex for sync message */
+	pthread_mutex_t sync_msg_mutex;
+
+	void *mgmt_ack_buf;
+
+	struct spnic_recv_msg recv_msg_from_mgmt;
+	struct spnic_recv_msg recv_resp_msg_from_mgmt;
+
+	u16 sync_msg_id;
+};
+
+int spnic_mgmt_msg_aeqe_handler(void *hwdev, u8 *header, u8 size, void *param);
+
+int spnic_pf_to_mgmt_init(struct spnic_hwdev *hwdev);
+
+void spnic_pf_to_mgmt_free(struct spnic_hwdev *hwdev);
+
+int spnic_msg_to_mgmt_sync(void *hwdev, enum spnic_mod_type mod, u16 cmd,
+			   void *buf_in, u16 in_size, void *buf_out,
+			   u16 *out_size, u32 timeout);
 
 #endif /* _SPNIC_MGMT_H_ */
