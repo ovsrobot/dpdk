840b65f24aa50e98adfcb81192114c19392f0fda
Alexander Kozyrev <akozyrev@nvidia.com>
2022-01-18 07:07:58 +0200

    app/testpmd: implement rte flow table
    Add testpmd support for the rte_flow_table API.
    Provide the command line interface for the flow
    table creation/destruction. Usage example:
      testpmd> flow table 0 create table_id 6
        group 9 priority 4 ingress mode 1
        rules_number 64 item_template 2 action_template 4
      testpmd> flow table 0 destroy table 6
    
    Signed-off-by: Alexander Kozyrev <akozyrev@nvidia.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- app/test-pmd/testpmd.h
+++ app/test-pmd/testpmd.h
@@ -176,6 +176,14 @@ struct port_template {
 	} template; /**< PMD opaque template object */
 };
 
+/** Descriptor for a flow table. */
+struct port_table {
+	struct port_table *next; /**< Next table in list. */
+	struct port_table *tmp; /**< Temporary linking. */
+	uint32_t id; /**< Table ID. */
+	struct rte_flow_table *table; /**< PMD opaque template object */
+};
+
 /** Descriptor for a single flow. */
 struct port_flow {
 	struct port_flow *next; /**< Next flow in list. */
@@ -258,6 +266,7 @@ struct rte_port {
 	uint8_t                 slave_flag; /**< bonding slave port */
 	struct port_template    *item_templ_list; /**< Item templates. */
 	struct port_template    *action_templ_list; /**< Action templates. */
+	struct port_table       *table_list; /**< Flow tables. */
 	struct port_flow        *flow_list; /**< Associated flows. */
 	struct port_indirect_action *actions_list;
 	/**< Associated indirect actions. */
@@ -911,6 +920,12 @@ int port_flow_action_template_create(portid_t port_id, uint32_t id,
 				     const struct rte_flow_action *masks);
 int port_flow_action_template_destroy(portid_t port_id, uint32_t n,
 				      const uint32_t *template);
+int port_flow_table_create(portid_t port_id, uint32_t id,
+		   const struct rte_flow_table_attr *table_attr,
+		   uint32_t nb_item_templates, uint32_t *item_templates,
+		   uint32_t nb_action_templates, uint32_t *action_templates);
+int port_flow_table_destroy(portid_t port_id,
+			    uint32_t n, const uint32_t *table);
 int port_flow_validate(portid_t port_id,
 		       const struct rte_flow_attr *attr,
 		       const struct rte_flow_item *pattern,
