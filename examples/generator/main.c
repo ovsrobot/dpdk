b0c701c09ed2ec1548cd2809e122d9ef6d0a719e
Ronan Randles <ronan.randles@intel.com>
2022-01-21 10:31:16 +0000

    examples/generator: enable gen library for traffic gen
    This commit shows the steps necessary to enable traffic generation
    using the gen library
    
    Signed-off-by: Ronan Randles <ronan.randles@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- examples/generator/main.c
+++ examples/generator/main.c
@@ -1,14 +1,18 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright(c) 2010-2015 Intel Corporation
+ * Copyright(c) 2021 Intel Corporation
  */
 
 #include <stdint.h>
 #include <inttypes.h>
+#include <unistd.h>
+#include <signal.h>
+
 #include <rte_eal.h>
 #include <rte_ethdev.h>
 #include <rte_cycles.h>
 #include <rte_lcore.h>
 #include <rte_mbuf.h>
+#include <rte_gen.h>
 
 #define RX_RING_SIZE 1024
 #define TX_RING_SIZE 1024
@@ -16,23 +20,23 @@
 #define NUM_MBUFS 8191
 #define MBUF_CACHE_SIZE 256
 #define BURST_SIZE 32
+#define MIN_THREADS 3
 
-/* Configuration of ethernet ports. 8<  */
 static const struct rte_eth_conf port_conf_default = {
 	.rxmode = {
 		.max_lro_pkt_size = RTE_ETHER_MAX_LEN,
 	},
 };
-/* >8 End of configuration of ethernet ports. */
 
-/* basicfwd.c: Basic DPDK skeleton forwarding example. */
+static volatile int done;
+static struct rte_mempool *mbuf_pool;
+struct rte_gen *gen;
 
-/*
- * Initializes a given port using global settings and with the RX buffers
+static void handle_sigint(int sig);
+
+/* Initializes a given port using global settings and with the RX buffers
  * coming from the mbuf_pool passed as a parameter.
  */
-
-/* Main functional part of port initialization. 8< */
 static inline int
 port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 {
@@ -68,6 +72,12 @@ port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 	if (retval != 0)
 		return retval;
 
+	int lcore_available_count = rte_lcore_count();
+	if (lcore_available_count < MIN_THREADS) {
+		printf("Not enough threads available\n");
+		return -1;
+	}
+
 	/* Allocate and set up 1 RX queue per Ethernet port. */
 	for (q = 0; q < rx_rings; q++) {
 		retval = rte_eth_rx_queue_setup(port, q, nb_rxd,
@@ -86,9 +96,8 @@ port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 			return retval;
 	}
 
-	/* Starting Ethernet port. 8< */
+	/* Start the Ethernet port. */
 	retval = rte_eth_dev_start(port);
-	/* >8 End of starting of ethernet port. */
 	if (retval < 0)
 		return retval;
 
@@ -104,27 +113,21 @@ port_init(uint16_t port, struct rte_mempool *mbuf_pool)
 
 	/* Enable RX in promiscuous mode for the Ethernet device. */
 	retval = rte_eth_promiscuous_enable(port);
-	/* End of setting RX port in promiscuous mode. */
 	if (retval != 0)
 		return retval;
 
 	return 0;
 }
-/* >8 End of main functional part of port initialization. */
 
-/*
- * The lcore main. This is the main thread that does the work, reading from
+/* The lcore main. This is the main thread that does the work, reading from
  * an input port and writing to an output port.
  */
-
- /* Basic forwarding application lcore. 8< */
-static __rte_noreturn void
-lcore_main(void)
+static int
+lcore_producer(__rte_unused void *arg)
 {
 	uint16_t port;
 
-	/*
-	 * Check that the port is on the same NUMA node as the polling thread
+	/* Check that the port is on the same NUMA node as the polling thread
 	 * for best performance.
 	 */
 	RTE_ETH_FOREACH_DEV(port)
@@ -135,57 +138,99 @@ lcore_main(void)
 					"polling thread.\n\tPerformance will "
 					"not be optimal.\n", port);
 
-	printf("\nCore %u forwarding packets. [Ctrl+C to quit]\n",
-			rte_lcore_id());
+	/* Run until the application is quit or killed. */
+	while (!done) {
+		struct rte_mbuf *bufs[BURST_SIZE];
+		int i;
+		/* Receive packets from gen and then tx them over port */
+		RTE_ETH_FOREACH_DEV(port) {
+			int nb_recieved = rte_gen_rx_burst(gen, bufs,
+							BURST_SIZE);
+			for (i = 0; i < nb_recieved; i++) {
+				bufs[i]->pkt_len = 64;
+				bufs[i]->data_len = 64;
+			}
 
-	/* Main work of application loop. 8< */
-	for (;;) {
-		/*
-		 * Receive packets on a port and forward them on the paired
-		 * port. The mapping is 0 -> 1, 1 -> 0, 2 -> 3, 3 -> 2, etc.
+			uint16_t nb_tx = rte_eth_tx_burst(port, 0, bufs,
+							nb_recieved);
+			if (nb_tx != nb_recieved)
+				rte_pktmbuf_free_bulk(&bufs[nb_tx],
+							(nb_recieved - nb_tx));
+
+			if (unlikely(nb_tx == 0))
+				continue;
+
+		}
+	}
+	return 0;
+}
+
+/* The lcore main. This is the main thread that does the work, reading from
+ * an input port and writing to an output port.
+ */
+static int
+lcore_consumer(__rte_unused void *arg)
+{
+	uint16_t port;
+
+	/* Check that the port is on the same NUMA node as the polling thread
+	 * for best performance.
+	 */
+	RTE_ETH_FOREACH_DEV(port)
+		if (rte_eth_dev_socket_id(port) >= 0 &&
+				rte_eth_dev_socket_id(port) !=
+						(int)rte_socket_id())
+			printf("WARNING, port %u is on remote NUMA node to "
+					"polling thread.\n\tPerformance will "
+					"not be optimal.\n", port);
+
+	/* Run until the application is quit or killed. */
+	while (!done) {
+		struct rte_mbuf *bufs[BURST_SIZE];
+
+		/* Receive packets over port and then tx them to gen library
+		 * for stats
 		 */
 		RTE_ETH_FOREACH_DEV(port) {
+			uint64_t latency[BURST_SIZE];
+			uint16_t nb_rx = rte_eth_rx_burst(port, 0, bufs,
+							BURST_SIZE);
+			rte_gen_tx_burst(gen, bufs, latency, nb_rx);
 
-			/* Get burst of RX packets, from first port of pair. */
-			struct rte_mbuf *bufs[BURST_SIZE];
-			const uint16_t nb_rx = rte_eth_rx_burst(port, 0,
-					bufs, BURST_SIZE);
+			int nb_sent = rte_gen_tx_burst(gen, bufs,
+							latency, nb_rx);
+			if (nb_sent != nb_rx)
+				rte_panic("invalid tx quantity\n");
 
 			if (unlikely(nb_rx == 0))
 				continue;
 
-			/* Send burst of TX packets, to second port of pair. */
-			const uint16_t nb_tx = rte_eth_tx_burst(port ^ 1, 0,
-					bufs, nb_rx);
-
-			/* Free any unsent packets. */
-			if (unlikely(nb_tx < nb_rx)) {
-				uint16_t buf;
-				for (buf = nb_tx; buf < nb_rx; buf++)
-					rte_pktmbuf_free(bufs[buf]);
-			}
 		}
 	}
-	/* >8 End of loop. */
+	return 0;
 }
-/* >8 End Basic forwarding application lcore. */
 
-/*
- * The main function, which does initialization and calls the per-lcore
+void handle_sigint(int sig)
+{
+	RTE_SET_USED(sig);
+	printf("\nExiting...\n");
+	done = 1;
+}
+
+/* The main function, which does initialization and calls the per-lcore
  * functions.
  */
 int
 main(int argc, char *argv[])
 {
-	struct rte_mempool *mbuf_pool;
+	signal(SIGINT, handle_sigint);
 	unsigned int nb_ports;
 	uint16_t portid;
 
-	/* Initializion the Environment Abstraction Layer (EAL). 8< */
+	/* Initialize the Environment Abstraction Layer (EAL). */
 	int ret = rte_eal_init(argc, argv);
 	if (ret < 0)
 		rte_exit(EXIT_FAILURE, "Error with EAL initialization\n");
-	/* >8 End of initializion the Environment Abstraction Layer (EAL). */
 
 	argc -= ret;
 	argv += ret;
@@ -196,28 +241,44 @@ main(int argc, char *argv[])
 		rte_exit(EXIT_FAILURE, "Error: number of ports must be even\n");
 
 	/* Creates a new mempool in memory to hold the mbufs. */
-
-	/* Allocates mempool to hold the mbufs. 8< */
 	mbuf_pool = rte_pktmbuf_pool_create("MBUF_POOL", NUM_MBUFS * nb_ports,
 		MBUF_CACHE_SIZE, 0, RTE_MBUF_DEFAULT_BUF_SIZE, rte_socket_id());
-	/* >8 End of allocating mempool to hold mbuf. */
 
 	if (mbuf_pool == NULL)
 		rte_exit(EXIT_FAILURE, "Cannot create mbuf pool\n");
 
-	/* Initializing all ports. 8< */
+	/* Initialize all ports. */
 	RTE_ETH_FOREACH_DEV(portid)
 		if (port_init(portid, mbuf_pool) != 0)
 			rte_exit(EXIT_FAILURE, "Cannot init port %"PRIu16 "\n",
 					portid);
-	/* >8 End of initializing all ports. */
 
-	if (rte_lcore_count() > 1)
-		printf("\nWARNING: Too many lcores enabled. Only 1 used.\n");
+	gen = rte_gen_create(mbuf_pool);
+	if (!gen)
+		rte_panic("Gen failed to initialize\n");
 
-	/* Call lcore_main on the main core only. Called on single lcore. 8< */
-	lcore_main();
-	/* >8 End of called on single lcore. */
+	int err = rte_gen_packet_parse_string(gen, "Ether()/IP()", NULL);
+	if (err)
+		rte_panic("Failed to parse input args");
+
+	/* launch lcore functions */
+	uint32_t lcore_count = 0;
+	uint32_t lcore_id = 0;
+	RTE_LCORE_FOREACH_WORKER(lcore_id) {
+		if (lcore_count == 0)
+			rte_eal_remote_launch(lcore_producer, NULL, lcore_id);
+		else if (lcore_count == 1)
+			rte_eal_remote_launch(lcore_consumer, NULL, lcore_id);
+		else
+			break;
+
+		lcore_count++;
+	}
+	/* Stall the main thread until all other threads have returned. */
+	rte_eal_mp_wait_lcore();
+
+	/* All threads returned, safe to destroy gen instance */
+	rte_gen_destroy(gen);
 
 	/* clean up the EAL */
 	rte_eal_cleanup();
