5adf7fb6c4e79531c8c2a22b8973dd2036059e14
Weiguo Li <liwg06@foxmail.com>
2022-02-06 11:58:38 +0800

    net/tap: add define guards to avoid multi-inclusion
    Supplement define guards to prevent multiple inclusion.
    
    Signed-off-by: Weiguo Li <liwg06@foxmail.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/tap/tap_log.h
+++ drivers/net/tap/tap_log.h
@@ -3,8 +3,13 @@
  * Copyright 2017 Mellanox Technologies, Ltd
  */
 
+#ifndef _TAP_LOG_H_
+#define _TAP_LOG_H_
+
 extern int tap_logtype;
 
 #define TAP_LOG(level, fmt, args...) \
 	rte_log(RTE_LOG_ ## level, tap_logtype, "%s(): " fmt "\n", \
 		__func__, ## args)
+
+#endif /* _TAP_LOG_H_ */
