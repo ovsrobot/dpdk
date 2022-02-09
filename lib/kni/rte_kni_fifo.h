69a06b9bc1bc99bdb5780a457b14094eec0e4506
Weiguo Li <liwg06@foxmail.com>
2022-02-09 15:24:05 +0800

    kni: add define guards to avoid multi-inclusion
    Supplement define guards to prevent multiple inclusion.
    
    Signed-off-by: Weiguo Li <liwg06@foxmail.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/kni/rte_kni_fifo.h
+++ lib/kni/rte_kni_fifo.h
@@ -2,7 +2,8 @@
  * Copyright(c) 2010-2014 Intel Corporation
  */
 
-
+#ifndef RTE_KNI_FIFO_H
+#define RTE_KNI_FIFO_H
 
 /**
  * @internal when c11 memory model enabled use c11 atomic memory barrier.
@@ -115,3 +116,5 @@ kni_fifo_free_count(struct rte_kni_fifo *fifo)
 	uint32_t fifo_read = __KNI_LOAD_ACQUIRE(&fifo->read);
 	return (fifo_read - fifo_write - 1) & (fifo->len - 1);
 }
+
+#endif /* RTE_KNI_FIFO_H */
