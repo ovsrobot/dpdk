705c4da11635f676fcf178841aedc443ead2098a
Suanming Mou <suanmingm@nvidia.com>
2022-02-10 18:29:14 +0200

    net/mlx5: introduce hardware steering operation
    A new hardware based steering operation is going to be introduced
    for high insertion rate.
    
    This commit adds the basic driver operation.
    
    Signed-off-by: Suanming Mou <suanmingm@nvidia.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/mlx5/mlx5_flow_hw.c
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2020 Mellanox Technologies, Ltd
+ */
+
+#include <rte_flow.h>
+
+#include "mlx5_flow.h"
+
+#ifdef HAVE_IBV_FLOW_DV_SUPPORT
+
+const struct mlx5_flow_driver_ops mlx5_flow_hw_drv_ops;
+
+#endif
