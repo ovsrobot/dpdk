ovsrobot/dpdk

commit 7a335720575507f55b723b1e10bfea7daeba1386

lib: remove C++ include guard from private headers
The private headers are compiled internally with a C compiler.
Thus extern "C" declaration is useless in such files.

Signed-off-by: Thomas Monjalon <thomas@monjalon.net>

lib/cryptodev/cryptodev_pmd.h
@@ -13,10 +13,6 @@
  * them directly.
  */
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <string.h>
 
 #include <rte_config.h>
@@ -560,9 +556,5 @@ set_asym_session_private_data(struct rte_cryptodev_asym_session *sess,
 {
 	sess->sess_private_data[driver_id] = private_data;
 }
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* _CRYPTODEV_PMD_H_ */

lib/acl/acl_vect.h
@@ -11,10 +11,6 @@
  * RTE ACL SSE/AVX related header.
  */
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 
 /*
  * Takes 2 SIMD registers containing N transitions each (tr0, tr1).
@@ -79,9 +75,5 @@ extern "C" {
 	addr = _##P##_add_epi32(addr, t);			\
 } while (0)
 
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* _RTE_ACL_VECT_H_ */

lib/acl/tb_mem.h
@@ -14,10 +14,6 @@
  * destructed (finish of build phase).
  */
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #include <rte_acl_osdep.h>
 #include <setjmp.h>
 
@@ -39,9 +35,5 @@ struct tb_mem_pool {
 
 void *tb_alloc(struct tb_mem_pool *pool, size_t size);
 void tb_free_pool(struct tb_mem_pool *pool);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* _TB_MEM_H_ */

lib/bpf/bpf_def.h
@@ -22,10 +22,6 @@
 #include <stdint.h>
 
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /*
  * The instruction encodings.
  */
@@ -143,9 +139,5 @@ struct ebpf_insn {
  * eBPF allows functions with R1-R5 as arguments.
  */
 #define	EBPF_FUNC_MAX_ARGS	(EBPF_REG_6 - EBPF_REG_1)
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* RTE_BPF_DEF_H_ */

lib/bpf/bpf_impl.h
@@ -8,10 +8,6 @@
 #include <rte_bpf.h>
 #include <sys/mman.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #define MAX_BPF_STACK_SIZE	0x200
 
 struct rte_bpf {
@@ -46,9 +42,5 @@ bpf_size(uint32_t bpf_op_sz)
 		return sizeof(uint64_t);
 	return 0;
 }
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* _BPF_H_ */

lib/distributor/distributor_private.h
@@ -13,10 +13,6 @@
  * one-at-a-time to workers, with dynamic load balancing.
  */
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 #define NO_FLAGS 0
 #define RTE_DISTRIB_PREFIX "DT_"
 
@@ -169,9 +165,5 @@ void
 find_match_vec(struct rte_distributor *d,
 			uint16_t *data_ptr,
 			uint16_t *output_ptr);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* _DIST_PRIV_H_ */

lib/eal/common/malloc_heap.h
@@ -33,10 +33,6 @@ struct malloc_heap {
 	char name[RTE_HEAP_NAME_MAX_LEN];
 } __rte_cache_aligned;
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 static inline unsigned
 malloc_get_numa_socket(void)
 {
@@ -99,9 +95,5 @@ malloc_socket_to_heap_id(unsigned int socket_id);
 
 int
 rte_eal_malloc_heap_init(void);
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* MALLOC_HEAP_H_ */

lib/ethdev/ethdev_driver.h
@@ -17,10 +17,6 @@
 
 #include <rte_ethdev.h>
 
-#ifdef __cplusplus
-extern "C" {
-#endif
-
 /**< @internal Declaration of the hairpin peer queue information structure. */
 struct rte_hairpin_peer_info;
 
@@ -1512,9 +1508,5 @@ struct rte_eth_tunnel_filter_conf {
 	uint32_t tenant_id;     /**< Tenant ID to match. VNI, GRE key... */
 	uint16_t queue_id;      /**< Queue assigned to if match. */
 };
-
-#ifdef __cplusplus
-}
-#endif
 
 #endif /* _RTE_ETHDEV_DRIVER_H_ */

commit bd8c069ee9a7c249f161b48c59820fc19d913bde

cryptodev: fix missing C++ guards
Some public header files were missing 'extern "C"' C++ guards,
and couldnt be used by C++ applications. Add the missing guards.

Fixes: 7a3357205755 ("lib: remove C++ include guard from private headers")
Cc: thomas@monjalon.net
Cc: stable@dpdk.org

Signed-off-by: Brian Dooley <brian.dooley@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/cryptodev/cryptodev_pmd.h
@@ -5,6 +5,10 @@
 #ifndef _CRYPTODEV_PMD_H_
 #define _CRYPTODEV_PMD_H_
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 /** @file
  * RTE Crypto PMD APIs
  *
@@ -639,5 +643,9 @@ RTE_STD_C11 struct rte_cryptodev_asym_session {
 	uint8_t padding[3];
 	uint8_t sess_private_data[0];
 };
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* _CRYPTODEV_PMD_H_ */