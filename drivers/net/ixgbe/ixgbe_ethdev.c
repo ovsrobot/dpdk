e2c407e2b6e8892546a7b26a172e4e888342ab87
Jeff Daly <jeffd@silicom-usa.com>
2022-02-24 10:23:56 -0500

    net/ixgbe: Limit SDP3 check of TX_DISABLE to appropriate devices
    1ca05831b9b added a check that SDP3 (used as a TX_DISABLE output to the
    SFP cage on these cards) is not asserted to avoid incorrectly reporting
    link up when the SFP's laser is turned off.
    
    ff8162cb957 limited this workaround to fiber ports
    
    This patch:
    * Adds additional check for 82599 type, not all fiber ixgbe devs use
      SDP3 as TX_DISABLE
    
    Fixes: 1ca05831b9b ("net/ixgbe: fix link status")
    Fixes: ff8162cb957 ("net/ixgbe: fix link status")
    Cc: stable@dpdk.org
    
    Signed-off-by: Jeff Daly <jeffd@silicom-usa.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/ixgbe/ixgbe_ethdev.c
+++ drivers/net/ixgbe/ixgbe_ethdev.c
@@ -4260,7 +4260,8 @@ ixgbe_dev_link_update_share(struct rte_eth_dev *dev,
 		return rte_eth_linkstatus_set(dev, &link);
 	}
 
-	if (ixgbe_get_media_type(hw) == ixgbe_media_type_fiber) {
+	if (hw->mac.type == ixgbe_mac_82599EB &&
+		ixgbe_get_media_type(hw) == ixgbe_media_type_fiber) {
 		esdp_reg = IXGBE_READ_REG(hw, IXGBE_ESDP);
 		if ((esdp_reg & IXGBE_ESDP_SDP3))
 			link_up = 0;
