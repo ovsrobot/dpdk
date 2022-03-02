55cc8f37c374d8c9169244464d9460d99ce6f7c8
Rongwei Liu <rongweil@nvidia.com>
2022-03-02 13:19:38 +0200

    net/mlx5: fix shared RSS destroy failure
    When both shared and non-shared RSS actions are present in single
    flow rule shared RSS index is unset by mistake.
    
    For example:
    1. flow indirect_action 0 create action_id 3 ingress action RSS ...
    2. set sample_actions 0 mark id 43690 / queue index 0 / end
    3. flow create 0 ingress group 107 pattern eth / sample ratio 2
       index 0  / indirect 3 / end
    
    PMD translates the indirect action to a shared RSS description at first.
    In the split prefix flow, RSS->shared_RSS is unset when translating sample
    queue action, the subfix flow will treat the RSS as non-shared.
    
    Fixes: 8e61555657b2 ("net/mlx5: fix shared RSS and mark actions combination")
    Cc: stable@dpdk.org
    Signed-off-by: Rongwei Liu <rongweil@nvidia.com>
    Acked-by: Viacheslav Ovsiienko <viacheslavo@nvidia.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/mlx5/mlx5_flow_dv.c
+++ drivers/net/mlx5/mlx5_flow_dv.c
@@ -11183,6 +11183,7 @@ flow_dv_hrxq_prepare(struct rte_eth_dev *dev,
 		     uint32_t *hrxq_idx)
 {
 	struct mlx5_flow_handle *dh = dev_flow->handle;
+	uint32_t shared_rss = rss_desc->shared_rss;
 	struct mlx5_hrxq *hrxq;
 
 	MLX5_ASSERT(rss_desc->queue_num);
@@ -11194,6 +11195,7 @@ flow_dv_hrxq_prepare(struct rte_eth_dev *dev,
 		rss_desc->queue_num = 1;
 	hrxq = mlx5_hrxq_get(dev, rss_desc);
 	*hrxq_idx = hrxq ? hrxq->idx : 0;
+	rss_desc->shared_rss = shared_rss;
 	return hrxq;
 }
 
