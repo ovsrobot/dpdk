ed7cba396642d27e9a0efc611dc7e40445fd93ef
Vamsi Attunuru <vattunuru@marvell.com>
2022-03-17 13:27:34 +0530

    ethdev: add packet expiry event subtype
    Patch adds a new event subtype for notifying expiry
    event upon soft packet limit expiry.
    
    Signed-off-by: Vamsi Attunuru <vattunuru@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/ethdev/rte_ethdev.h
+++ lib/ethdev/rte_ethdev.h
@@ -3828,6 +3828,8 @@ enum rte_eth_event_ipsec_subtype {
 	RTE_ETH_EVENT_IPSEC_SA_TIME_EXPIRY,
 	/** Soft byte expiry of SA */
 	RTE_ETH_EVENT_IPSEC_SA_BYTE_EXPIRY,
+	/** Soft packet expiry of SA */
+	RTE_ETH_EVENT_IPSEC_SA_PKT_EXPIRY,
 	/** Max value of this enum */
 	RTE_ETH_EVENT_IPSEC_MAX
 };
@@ -3849,6 +3851,7 @@ struct rte_eth_event_ipsec_desc {
 	 * - @ref RTE_ETH_EVENT_IPSEC_ESN_OVERFLOW
 	 * - @ref RTE_ETH_EVENT_IPSEC_SA_TIME_EXPIRY
 	 * - @ref RTE_ETH_EVENT_IPSEC_SA_BYTE_EXPIRY
+	 * - @ref RTE_ETH_EVENT_IPSEC_SA_PKT_EXPIRY
 	 *
 	 * @see struct rte_security_session_conf
 	 *
