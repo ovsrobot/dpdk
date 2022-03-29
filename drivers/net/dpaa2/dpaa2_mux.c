ovsrobot/dpdk

commit 1def64c2d79e07eb3ec73f5d1ca797e512a3e4d5

net/dpaa2: add dpdmux initialization and configuration
This patch introduces an rte pmd API to configure dpdmux from
the application.
dpdmux can work in association with dpni as an additional
distribution capability on the NIC.

Signed-off-by: Nipun Gupta <nipun.gupta@nxp.com>
Acked-by: Shreyansh Jain <shreyansh.jain@nxp.com>

drivers/net/dpaa2/dpaa2_mux.c
@@ -1,0 +1,222 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2018 NXP
+ */
+
+#include <sys/queue.h>
+#include <stdio.h>
+#include <errno.h>
+#include <stdint.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+
+#include <rte_ethdev.h>
+#include <rte_log.h>
+#include <rte_eth_ctrl.h>
+#include <rte_malloc.h>
+#include <rte_flow_driver.h>
+#include <rte_tailq.h>
+
+#include <rte_fslmc.h>
+#include <fsl_dpdmux.h>
+#include <fsl_dpkg.h>
+
+#include <dpaa2_ethdev.h>
+#include <dpaa2_pmd_logs.h>
+
+struct dpaa2_dpdmux_dev {
+	TAILQ_ENTRY(dpaa2_dpdmux_dev) next;
+		/**< Pointer to Next device instance */
+	struct fsl_mc_io dpdmux;  /** handle to DPDMUX portal object */
+	uint16_t token;
+	uint32_t dpdmux_id; /*HW ID for DPDMUX object */
+	uint8_t num_ifs;   /* Number of interfaces in DPDMUX */
+};
+
+struct rte_flow {
+	struct dpdmux_rule_cfg rule;
+};
+
+TAILQ_HEAD(dpdmux_dev_list, dpaa2_dpdmux_dev);
+static struct dpdmux_dev_list dpdmux_dev_list =
+	TAILQ_HEAD_INITIALIZER(dpdmux_dev_list); /*!< DPDMUX device list */
+
+static struct dpaa2_dpdmux_dev *get_dpdmux_from_id(uint32_t dpdmux_id)
+{
+	struct dpaa2_dpdmux_dev *dpdmux_dev = NULL;
+
+	/* Get DPBP dev handle from list using index */
+	TAILQ_FOREACH(dpdmux_dev, &dpdmux_dev_list, next) {
+		if (dpdmux_dev->dpdmux_id == dpdmux_id)
+			break;
+	}
+
+	return dpdmux_dev;
+}
+
+struct rte_flow *
+rte_pmd_dpaa2_mux_flow_create(uint32_t dpdmux_id,
+			      struct rte_flow_item *pattern[],
+			      struct rte_flow_action *actions[])
+{
+	struct dpaa2_dpdmux_dev *dpdmux_dev;
+	struct dpkg_profile_cfg kg_cfg;
+	const struct rte_flow_item_ipv4 *spec;
+	const struct rte_flow_action_vf *vf_conf;
+	struct dpdmux_cls_action dpdmux_action;
+	struct rte_flow *flow = NULL;
+	void *key_iova, *mask_iova, *key_cfg_iova = NULL;
+	int ret;
+
+	if (pattern[0]->type != RTE_FLOW_ITEM_TYPE_IPV4) {
+		DPAA2_PMD_ERR("Not supported pattern type: %d",
+			      pattern[0]->type);
+		return NULL;
+	}
+
+	/* Find the DPDMUX from dpdmux_id in our list */
+	dpdmux_dev = get_dpdmux_from_id(dpdmux_id);
+	if (!dpdmux_dev) {
+		DPAA2_PMD_ERR("Invalid dpdmux_id: %d", dpdmux_id);
+		return NULL;
+	}
+
+	key_cfg_iova = rte_zmalloc(NULL, DIST_PARAM_IOVA_SIZE,
+				   RTE_CACHE_LINE_SIZE);
+	if (!key_cfg_iova) {
+		DPAA2_PMD_ERR("Unable to allocate flow-dist parameters");
+		return NULL;
+	}
+
+	/* Currently taking only IP protocol as an extract type.
+	 * This can be exended to other fields using pattern->type.
+	 */
+	memset(&kg_cfg, 0, sizeof(struct dpkg_profile_cfg));
+	kg_cfg.extracts[0].extract.from_hdr.prot = NET_PROT_IP;
+	kg_cfg.extracts[0].extract.from_hdr.field = NH_FLD_IP_PROTO;
+	kg_cfg.extracts[0].type = DPKG_EXTRACT_FROM_HDR;
+	kg_cfg.extracts[0].extract.from_hdr.type = DPKG_FULL_FIELD;
+	kg_cfg.num_extracts = 1;
+
+	ret = dpkg_prepare_key_cfg(&kg_cfg, key_cfg_iova);
+	if (ret) {
+		DPAA2_PMD_ERR("dpkg_prepare_key_cfg failed: err(%d)", ret);
+		goto creation_error;
+	}
+
+	ret = dpdmux_set_custom_key(&dpdmux_dev->dpdmux, CMD_PRI_LOW,
+				    dpdmux_dev->token,
+			(uint64_t)(DPAA2_VADDR_TO_IOVA(key_cfg_iova)));
+	if (ret) {
+		DPAA2_PMD_ERR("dpdmux_set_custom_key failed: err(%d)", ret);
+		goto creation_error;
+	}
+
+	/* As now our key extract parameters are set, let us configure
+	 * the rule.
+	 */
+	flow = rte_zmalloc(NULL, sizeof(struct rte_flow) +
+			   (2 * DIST_PARAM_IOVA_SIZE), RTE_CACHE_LINE_SIZE);
+	if (!flow) {
+		DPAA2_PMD_ERR(
+			"Memory allocation failure for rule configration\n");
+		goto creation_error;
+	}
+	key_iova = (void *)((size_t)flow + sizeof(struct rte_flow));
+	mask_iova = (void *)((size_t)key_iova + DIST_PARAM_IOVA_SIZE);
+
+	spec = (const struct rte_flow_item_ipv4 *)pattern[0]->spec;
+	memcpy(key_iova, (const void *)&spec->hdr.next_proto_id,
+	       sizeof(uint8_t));
+	memcpy(mask_iova, pattern[0]->mask, sizeof(uint8_t));
+
+	flow->rule.key_iova = (uint64_t)(DPAA2_VADDR_TO_IOVA(key_iova));
+	flow->rule.mask_iova = (uint64_t)(DPAA2_VADDR_TO_IOVA(mask_iova));
+	flow->rule.key_size = sizeof(uint8_t);
+
+	vf_conf = (const struct rte_flow_action_vf *)(actions[0]->conf);
+	if (vf_conf->id == 0 || vf_conf->id > dpdmux_dev->num_ifs) {
+		DPAA2_PMD_ERR("Invalid destination id\n");
+		goto creation_error;
+	}
+	dpdmux_action.dest_if = vf_conf->id;
+
+	ret = dpdmux_add_custom_cls_entry(&dpdmux_dev->dpdmux, CMD_PRI_LOW,
+					  dpdmux_dev->token, &flow->rule,
+					  &dpdmux_action);
+	if (ret) {
+		DPAA2_PMD_ERR("dpdmux_add_custom_cls_entry failed: err(%d)",
+			      ret);
+		goto creation_error;
+	}
+
+	return flow;
+
+creation_error:
+	rte_free((void *)key_cfg_iova);
+	rte_free((void *)flow);
+	return NULL;
+}
+
+static int
+dpaa2_create_dpdmux_device(int vdev_fd __rte_unused,
+			   struct vfio_device_info *obj_info __rte_unused,
+			   int dpdmux_id)
+{
+	struct dpaa2_dpdmux_dev *dpdmux_dev;
+	struct dpdmux_attr attr;
+	int ret;
+
+	PMD_INIT_FUNC_TRACE();
+
+	/* Allocate DPAA2 dpdmux handle */
+	dpdmux_dev = rte_malloc(NULL, sizeof(struct dpaa2_dpdmux_dev), 0);
+	if (!dpdmux_dev) {
+		DPAA2_PMD_ERR("Memory allocation failed for DPDMUX Device");
+		return -1;
+	}
+
+	/* Open the dpdmux object */
+	dpdmux_dev->dpdmux.regs = rte_mcp_ptr_list[MC_PORTAL_INDEX];
+	ret = dpdmux_open(&dpdmux_dev->dpdmux, CMD_PRI_LOW, dpdmux_id,
+			  &dpdmux_dev->token);
+	if (ret) {
+		DPAA2_PMD_ERR("Unable to open dpdmux object: err(%d)", ret);
+		goto init_err;
+	}
+
+	ret = dpdmux_get_attributes(&dpdmux_dev->dpdmux, CMD_PRI_LOW,
+				    dpdmux_dev->token, &attr);
+	if (ret) {
+		DPAA2_PMD_ERR("Unable to get dpdmux attr: err(%d)", ret);
+		goto init_err;
+	}
+
+	ret = dpdmux_if_set_default(&dpdmux_dev->dpdmux, CMD_PRI_LOW,
+				    dpdmux_dev->token, 1);
+	if (ret) {
+		DPAA2_PMD_ERR("setting default interface failed in %s",
+			      __func__);
+		goto init_err;
+	}
+
+	dpdmux_dev->dpdmux_id = dpdmux_id;
+	dpdmux_dev->num_ifs = attr.num_ifs;
+
+	TAILQ_INSERT_TAIL(&dpdmux_dev_list, dpdmux_dev, next);
+
+	return 0;
+
+init_err:
+	if (dpdmux_dev)
+		rte_free(dpdmux_dev);
+
+	return -1;
+}
+
+static struct rte_dpaa2_object rte_dpaa2_dpdmux_obj = {
+	.dev_type = DPAA2_MUX,
+	.create = dpaa2_create_dpdmux_device,
+};
+
+RTE_PMD_REGISTER_DPAA2_OBJECT(dpdmux, rte_dpaa2_dpdmux_obj);

drivers/net/dpaa2/Makefile
@@ -33,6 +33,7 @@ CFLAGS += -DALLOW_EXPERIMENTAL_API
 SRCS-$(CONFIG_RTE_LIBRTE_DPAA2_PMD) += base/dpaa2_hw_dpni.c
 SRCS-$(CONFIG_RTE_LIBRTE_DPAA2_PMD) += dpaa2_rxtx.c
 SRCS-$(CONFIG_RTE_LIBRTE_DPAA2_PMD) += dpaa2_ethdev.c
+SRCS-$(CONFIG_RTE_LIBRTE_DPAA2_PMD) += dpaa2_mux.c
 SRCS-$(CONFIG_RTE_LIBRTE_DPAA2_PMD) += mc/dpni.c
 SRCS-$(CONFIG_RTE_LIBRTE_DPAA2_PMD) += mc/dpkg.c
 SRCS-$(CONFIG_RTE_LIBRTE_DPAA2_PMD) += mc/dpdmux.c

drivers/net/dpaa2/dpaa2_ethdev.h
@@ -11,6 +11,8 @@
 #include <rte_event_eth_rx_adapter.h>
 #include <rte_pmd_dpaa2.h>
 
+#include <dpaa2_hw_pvt.h>
+
 #include <mc/fsl_dpni.h>
 #include <mc/fsl_mc_sys.h>
 

drivers/net/dpaa2/meson.build
@@ -9,6 +9,7 @@ endif
 
 deps += ['mempool_dpaa2']
 sources = files('base/dpaa2_hw_dpni.c',
+		'dpaa2_mux.c',
 		'dpaa2_ethdev.c',
 		'dpaa2_rxtx.c',
 		'mc/dpkg.c',

drivers/net/dpaa2/rte_pmd_dpaa2.h
@@ -36,4 +36,27 @@ enum pmd_dpaa2_ts {
 __rte_experimental
 void rte_pmd_dpaa2_set_timestamp(enum pmd_dpaa2_ts);
 
+/**
+ * @warning
+ * @b EXPERIMENTAL: this API may change, or be removed, without prior notice
+ *
+ * Create a flow rule to demultiplex ethernet traffic to separate network
+ * interfaces.
+ *
+ * @param dpdmux_id
+ *    ID of the DPDMUX MC object.
+ * @param[in] pattern
+ *    Pattern specification.
+ * @param[in] actions
+ *    Associated actions.
+ *
+ * @return
+ *    A valid handle in case of success, NULL otherwise.
+ */
+__rte_experimental
+struct rte_flow *
+rte_pmd_dpaa2_mux_flow_create(uint32_t dpdmux_id,
+			      struct rte_flow_item *pattern[],
+			      struct rte_flow_action *actions[]);
+
 #endif /* _RTE_PMD_DPAA2_H */

drivers/net/dpaa2/rte_pmd_dpaa2_version.map
@@ -14,5 +14,6 @@ DPDK_17.11 {
 EXPERIMENTAL {
 	global:
 
+	rte_pmd_dpaa2_mux_flow_create;
 	rte_pmd_dpaa2_set_timestamp;
 } DPDK_17.11;

commit 309ff08e99314ab3216712875d487e1b02b60c0a

net/dpaa2: fix dpdmux default interface
if dpdmux objects created by restool tools with
the argument "--default-if=<if-id-number>", this
function would change it to 1

Fixes: 1def64c2d79e ("net/dpaa2: add dpdmux initialization and configuration")
Cc: nipun.gupta@nxp.com

Signed-off-by: Tianli Lai <laitianli@tom.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/dpaa2/dpaa2_mux.c
@@ -296,7 +296,7 @@ dpaa2_create_dpdmux_device(int vdev_fd __rte_unused,
 	}
 
 	ret = dpdmux_if_set_default(&dpdmux_dev->dpdmux, CMD_PRI_LOW,
-				    dpdmux_dev->token, 1);
+				    dpdmux_dev->token, attr.default_if);
 	if (ret) {
 		DPAA2_PMD_ERR("setting default interface failed in %s",
 			      __func__);