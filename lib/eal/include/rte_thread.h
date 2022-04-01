7b358dd9321dbb815a7a5f6618ce0e2f798d9355
Tyler Retzlaff <roretzla@linux.microsoft.com>
2022-04-01 06:29:52 -0700

    eal: implement functions for get/set thread affinity
    Implement functions for getting/setting thread affinity.
    Threads can be pinned to specific cores by setting their
    affinity attribute.
    
    Signed-off-by: Narcisa Vasile <navasile@microsoft.com>
    Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/include/rte_thread.h
+++ lib/eal/include/rte_thread.h
@@ -2,6 +2,8 @@
  * Copyright(c) 2021 Mellanox Technologies, Ltd
  */
 
+#include <stdint.h>
+
 #include <rte_os.h>
 #include <rte_compat.h>
 
@@ -21,11 +23,54 @@ extern "C" {
 #endif
 
 /**
+ * Thread id descriptor.
+ */
+typedef struct rte_thread_tag {
+	uintptr_t opaque_id; /**< thread identifier */
+} rte_thread_t;
+
+/**
  * TLS key type, an opaque pointer.
  */
 typedef struct eal_tls_key *rte_thread_key;
 
 #ifdef RTE_HAS_CPUSET
+
+/**
+ * Set the affinity of thread 'thread_id' to the cpu set
+ * specified by 'cpuset'.
+ *
+ * @param thread_id
+ *    Id of the thread for which to set the affinity.
+ *
+ * @param cpuset
+ *   Pointer to CPU affinity to set.
+ *
+ * @return
+ *   On success, return 0.
+ *   On failure, return a positive errno-style error number.
+ */
+__rte_experimental
+int rte_thread_set_affinity_by_id(rte_thread_t thread_id,
+		const rte_cpuset_t *cpuset);
+
+/**
+ * Get the affinity of thread 'thread_id' and store it
+ * in 'cpuset'.
+ *
+ * @param thread_id
+ *    Id of the thread for which to get the affinity.
+ *
+ * @param cpuset
+ *   Pointer for storing the affinity value.
+ *
+ * @return
+ *   On success, return 0.
+ *   On failure, return a positive errno-style error number.
+ */
+__rte_experimental
+int rte_thread_get_affinity_by_id(rte_thread_t thread_id,
+		rte_cpuset_t *cpuset);
 
 /**
  * Set core affinity of the current thread.
