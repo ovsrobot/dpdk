2edc21f6155206744cc116b9738024909013dff1
Kumara Parameshwaran <kumaraparamesh92@gmail.com>
2022-04-03 17:20:31 +0530

    gro: bug fix in identifying 0 length tcp packets
    As the minimum Ethernet frame size is 64 bytes, a 0 length
    tcp payload without tcp options would be 54 bytes and hence
    there would be padding. So it would be incorrect to use the
    packet length to determine the tcp data length.
    
    Fixes: 1e4cf4d6d4fb ("gro: cleanup")
    Cc: stable@dpdk.org
    
    Signed-off-by: Kumara Parameshwaran <kparameshwar@vmware.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/gro/gro_tcp4.c
+++ lib/gro/gro_tcp4.c
@@ -198,7 +198,7 @@ gro_tcp4_reassemble(struct rte_mbuf *pkt,
 	struct rte_tcp_hdr *tcp_hdr;
 	uint32_t sent_seq;
 	int32_t tcp_dl;
-	uint16_t ip_id, hdr_len, frag_off;
+	uint16_t ip_id, frag_off;
 	uint8_t is_atomic;
 
 	struct tcp4_flow_key key;
@@ -217,7 +217,6 @@ gro_tcp4_reassemble(struct rte_mbuf *pkt,
 	eth_hdr = rte_pktmbuf_mtod(pkt, struct rte_ether_hdr *);
 	ipv4_hdr = (struct rte_ipv4_hdr *)((char *)eth_hdr + pkt->l2_len);
 	tcp_hdr = (struct rte_tcp_hdr *)((char *)ipv4_hdr + pkt->l3_len);
-	hdr_len = pkt->l2_len + pkt->l3_len + pkt->l4_len;
 
 	/*
 	 * Don't process the packet which has FIN, SYN, RST, PSH, URG, ECE
@@ -229,7 +228,7 @@ gro_tcp4_reassemble(struct rte_mbuf *pkt,
 	 * Don't process the packet whose payload length is less than or
 	 * equal to 0.
 	 */
-	tcp_dl = pkt->pkt_len - hdr_len;
+	tcp_dl = rte_be_to_cpu_16(ipv4_hdr->total_length) - (pkt->l3_len + pkt->l4_len);
 	if (tcp_dl <= 0)
 		return -1;
 
