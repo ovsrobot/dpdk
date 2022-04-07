3db8980f684a893492a5b3825df2748b03d704ab
Owen Hilyard <ohilyard@iol.unh.edu>
2022-04-07 17:47:08 -0400

    app/test-pmd-api: Implementation files for the API
    As of right now, this is a fairly direct port. As such, most of the main
    file from test-acl is present in api_impl.cc. If this proof of concept
    is going to expand into a usable application, the acl test helper can be
    moved to another file to help keep the service definition file clean.
    The header file must remain a C header file in order to be able to be
    included in the main file. At this point, the main file is just a stub
    that starts the RPC server, but I have left it so that any extensions
    can be written in C and the C++ parts of this app can be easily
    encapsulated.
    
    Signed-off-by: Owen Hilyard <ohilyard@iol.unh.edu>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ app/test-pmd-api/api_impl.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2022 University of New Hampshire
+ */
+
+/*
+Dispite this file being attached to a C++ file, all definitions must remain ISO C. In additon,
+any exported members from C++ must be in an "extern C" block.
+*/
+
+void rte_testpmd_run();
---
+++ app/test-pmd-api/main.c
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2022 University of New Hampshire
+ */
+#include "api_impl.h"
+
+int
+main()
+{
+	rte_testpmd_run();
+	return 0;
+}
