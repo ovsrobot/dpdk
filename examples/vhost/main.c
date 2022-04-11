92dd8b48f19fa7288e8a886da4e695fafa55fb85
Yuan Wang <yuanx.wang@intel.com>
2022-04-12 00:34:51 +0800

    examples/vhost: fix floating point exception when there is no VMDQ
    If the VMDQ limits is 0, a divide-by-zero error occurs.
    This patch replaces throwing a floating point exception with
    a normal error message.
    
    Signed-off-by: Yuan Wang <yuanx.wang@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- examples/vhost/main.c
+++ examples/vhost/main.c
@@ -400,6 +400,10 @@ port_init(uint16_t port)
 
 		return retval;
 	}
+	if (dev_info.max_vmdq_pools == 0) {
+		RTE_LOG(ERR, VHOST_PORT, "Failed to get VMDQ info.\n");
+		return -1;
+	}
 
 	rxconf = &dev_info.default_rxconf;
 	txconf = &dev_info.default_txconf;
