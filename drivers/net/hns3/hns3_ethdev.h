ovsrobot/dpdk

commit 14ea9f0a62c60a86503304f626e663293ed8b09c

net/hns3: dump device feature capability
Kunpeng 920 and Kunpeng 930 support different feature capability.
This patch dumps feature capability Current device supports.

Signed-off-by: Min Hu (Connor) <humin29@huawei.com>

drivers/net/hns3/hns3_ethdev_dump.c
@@ -55,6 +55,32 @@ get_io_func_hint_name(uint32_t hint)
 }
 
 static void
+get_dev_feature_capability(FILE *file, struct hns3_hw *hw)
+{
+	const char * const caps_name[] = {
+		"DCB",
+		"COPPER",
+		"FD QUEUE REGION",
+		"PTP",
+		"TX PUSH",
+		"INDEP TXRX",
+		"STASH",
+		"SIMPLE BD",
+		"RXD Advanced Layout",
+		"OUTER UDP CKSUM",
+		"RAS IMP",
+		"TM",
+		"VF VLAN FILTER MOD",
+	};
+	uint32_t i;
+
+	fprintf(file, "  - Dev Capability:\n");
+	for (i = 0; i < RTE_DIM(caps_name); i++)
+		fprintf(file, "\t  -- support %s: %s\n", caps_name[i],
+			hw->capability & BIT(i) ? "yes" : "no");
+}
+
+static void
 get_device_basic_info(FILE *file, struct rte_eth_dev *dev)
 {
 	struct hns3_adapter *hns = dev->data->dev_private;
@@ -93,7 +119,11 @@ get_device_basic_info(FILE *file, struct rte_eth_dev *dev)
 int
 hns3_eth_dev_priv_dump(struct rte_eth_dev *dev, FILE *file)
 {
+	struct hns3_adapter *hns = dev->data->dev_private;
+	struct hns3_hw *hw = &hns->hw;
+
 	get_device_basic_info(file, dev);
+	get_dev_feature_capability(file, hw);
 
 	return 0;
 }

commit 807d0ecaae2f82a0cadf079346810118675a6dfa

net/hns3: refactor feature capability dump
This patch refactor feature capability dump.

Fixes: 14ea9f0a62c6 ("net/hns3: dump device feature capability")

Signed-off-by: Min Hu (Connor) <humin29@huawei.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/hns3/hns3_ethdev.h
@@ -874,7 +874,7 @@ struct hns3_adapter {
 
 #define HNS3_DEVARG_MBX_TIME_LIMIT_MS	"mbx_time_limit_ms"
 
-enum {
+enum hns3_dev_cap {
 	HNS3_DEV_SUPPORT_DCB_B,
 	HNS3_DEV_SUPPORT_COPPER_B,
 	HNS3_DEV_SUPPORT_FD_QUEUE_REGION_B,

drivers/net/hns3/hns3_ethdev_dump.c
@@ -2,15 +2,11 @@
  * Copyright(C) 2022 HiSilicon Limited
  */
 
-#include <rte_kvargs.h>
-#include <rte_bus_pci.h>
-#include <ethdev_pci.h>
-#include <rte_pci.h>
-
 #include "hns3_common.h"
 #include "hns3_logs.h"
 #include "hns3_regs.h"
 #include "hns3_rxtx.h"
+#include "hns3_ethdev.h"
 
 static const char *
 get_adapter_state_name(enum hns3_adapter_state state)
@@ -84,27 +80,29 @@ get_dev_mac_info(FILE *file, struct hns3_adapter *hns)
 static void
 get_dev_feature_capability(FILE *file, struct hns3_hw *hw)
 {
-	const char * const caps_name[] = {
-		"DCB",
-		"COPPER",
-		"FD QUEUE REGION",
-		"PTP",
-		"TX PUSH",
-		"INDEP TXRX",
-		"STASH",
-		"SIMPLE BD",
-		"RXD Advanced Layout",
-		"OUTER UDP CKSUM",
-		"RAS IMP",
-		"TM",
-		"VF VLAN FILTER MOD",
+	const struct {
+		enum hns3_dev_cap cap;
+		const char *name;
+	} caps_name[] = {
+		{HNS3_DEV_SUPPORT_DCB_B, "DCB"},
+		{HNS3_DEV_SUPPORT_COPPER_B, "COPPER"},
+		{HNS3_DEV_SUPPORT_FD_QUEUE_REGION_B, "FD QUEUE REGION"},
+		{HNS3_DEV_SUPPORT_PTP_B, "PTP"},
+		{HNS3_DEV_SUPPORT_TX_PUSH_B, "TX PUSH"},
+		{HNS3_DEV_SUPPORT_INDEP_TXRX_B, "INDEP TXRX"},
+		{HNS3_DEV_SUPPORT_STASH_B, "STASH"},
+		{HNS3_DEV_SUPPORT_RXD_ADV_LAYOUT_B, "RXD Advanced Layout"},
+		{HNS3_DEV_SUPPORT_OUTER_UDP_CKSUM_B, "OUTER UDP CKSUM"},
+		{HNS3_DEV_SUPPORT_RAS_IMP_B, "RAS IMP"},
+		{HNS3_DEV_SUPPORT_TM_B, "TM"},
 	};
 	uint32_t i;
 
 	fprintf(file, "  - Dev Capability:\n");
 	for (i = 0; i < RTE_DIM(caps_name); i++)
-		fprintf(file, "\t  -- support %s: %s\n", caps_name[i],
-			hw->capability & BIT(i) ? "yes" : "no");
+		fprintf(file, "\t  -- support %s: %s\n", caps_name[i].name,
+			hns3_get_bit(hw->capability, caps_name[i].cap) ? "Yes" :
+									 "No");
 }
 
 static const char *