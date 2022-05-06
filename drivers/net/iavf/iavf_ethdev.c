1780a3770fe401ff095ec86a3c34b12b7fb5163c
Radu Nicolau <radu.nicolau@intel.com>
2022-05-06 10:45:31 +0100

    net/iavf: fix device stop
    Move security context destroy from device stop to device close function.
    Deleting the context on device stop can prevent the application from
    properly cleaning and releasing resources.
    
    Fixes: 6bc987ecb860 ("net/iavf: support IPsec inline crypto")
    Cc: stable@dpdk.org
    
    Signed-off-by: Radu Nicolau <radu.nicolau@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/iavf/iavf_ethdev.c
+++ drivers/net/iavf/iavf_ethdev.c
@@ -1035,9 +1035,6 @@ iavf_dev_stop(struct rte_eth_dev *dev)
 	iavf_add_del_mc_addr_list(adapter, vf->mc_addrs, vf->mc_addrs_num,
 				  false);
 
-	/* free iAVF security device context all related resources */
-	iavf_security_ctx_destroy(adapter);
-
 	adapter->stopped = 1;
 	dev->data->dev_started = 0;
 
@@ -2624,6 +2621,9 @@ iavf_dev_close(struct rte_eth_dev *dev)
 		return 0;
 
 	ret = iavf_dev_stop(dev);
+
+	/* free iAVF security device context all related resources */
+	iavf_security_ctx_destroy(adapter);
 
 	iavf_flow_flush(dev, NULL);
 	iavf_flow_uninit(adapter);
