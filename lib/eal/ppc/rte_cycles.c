0a8b6062e1ada04d23cf0f4262d76720c362cee2
Duncan Bellamy <dunk@denkimushi.com>
2022-05-07 22:15:46 +0100

    lib/eal/ppc fix compilation for musl
    musl lacks __ppc_get_timebase() but has __builtin_ppc_get_timebase()
    
    the __ppc_get_timebase_freq() is taken from:
    https://git.alpinelinux.org/aports/commit/?id=06b03f70fb94972286c0c9f6278df89e53903833
    
    Signed-off-by: Duncan Bellamy <dunk@denkimushi.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/ppc/rte_cycles.c
+++ lib/eal/ppc/rte_cycles.c
@@ -2,12 +2,22 @@
  * Copyright (C) IBM Corporation 2019.
  */
 
+#include <features.h>
+#ifdef __GLIBC__
 #include <sys/platform/ppc.h>
+#else
+#include <string.h>
+#include <stdio.h>
+#endif
 
 #include "eal_private.h"
 
 uint64_t
 get_tsc_freq_arch(void)
 {
+#ifdef __GLIBC__
 	return __ppc_get_timebase_freq();
+#else
+	return no_ppc_get_timebase_freq();
+#endif
 }
