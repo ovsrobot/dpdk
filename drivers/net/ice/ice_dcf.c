ovsrobot/dpdk

commit daa714d55c722b6184e1bf612a74153337f0df5e

net/ice: handle AdminQ command by DCF
The DCF (Device Config Function) splits the AdminQ command into two
parts: one is the descriptor of AdminQ command, the other is the buffer
of AdminQ command (the descriptor has BUF flag set). When both of them
are received by the PF, the PF will handle them as one command.

And also, the filled descriptor and buffer of the response will be sent
back to DCF one by one through the virtchnl from PF.

Signed-off-by: Haiyue Wang <haiyue.wang@intel.com>
Acked-by: Qi Zhang <qi.z.zhang@intel.com>

drivers/net/ice/ice_dcf.c
@@ -397,6 +397,71 @@ ret:
 }
 
 int
+ice_dcf_send_aq_cmd(void *dcf_hw, struct ice_aq_desc *desc,
+		    void *buf, uint16_t buf_size)
+{
+	struct dcf_virtchnl_cmd desc_cmd, buff_cmd;
+	struct ice_dcf_hw *hw = dcf_hw;
+	int err = 0;
+	int i = 0;
+
+	if ((buf && !buf_size) || (!buf && buf_size) ||
+	    buf_size > ICE_DCF_AQ_BUF_SZ)
+		return -EINVAL;
+
+	desc_cmd.v_op = VIRTCHNL_OP_DCF_CMD_DESC;
+	desc_cmd.req_msglen = sizeof(*desc);
+	desc_cmd.req_msg = (uint8_t *)desc;
+	desc_cmd.rsp_buflen = sizeof(*desc);
+	desc_cmd.rsp_msgbuf = (uint8_t *)desc;
+
+	if (buf == NULL)
+		return ice_dcf_execute_virtchnl_cmd(hw, &desc_cmd);
+
+	desc->flags |= rte_cpu_to_le_16(ICE_AQ_FLAG_BUF);
+
+	buff_cmd.v_op = VIRTCHNL_OP_DCF_CMD_BUFF;
+	buff_cmd.req_msglen = buf_size;
+	buff_cmd.req_msg = buf;
+	buff_cmd.rsp_buflen = buf_size;
+	buff_cmd.rsp_msgbuf = buf;
+
+	rte_spinlock_lock(&hw->vc_cmd_send_lock);
+	ice_dcf_vc_cmd_set(hw, &desc_cmd);
+	ice_dcf_vc_cmd_set(hw, &buff_cmd);
+
+	if (ice_dcf_vc_cmd_send(hw, &desc_cmd) ||
+	    ice_dcf_vc_cmd_send(hw, &buff_cmd)) {
+		err = -1;
+		PMD_DRV_LOG(ERR, "fail to send OP_DCF_CMD_DESC/BUFF");
+		goto ret;
+	}
+
+	do {
+		if ((!desc_cmd.pending && !buff_cmd.pending) ||
+		    (!desc_cmd.pending && desc_cmd.v_ret != IAVF_SUCCESS) ||
+		    (!buff_cmd.pending && buff_cmd.v_ret != IAVF_SUCCESS))
+			break;
+
+		rte_delay_ms(ICE_DCF_ARQ_CHECK_TIME);
+	} while (i++ < ICE_DCF_ARQ_MAX_RETRIES);
+
+	if (desc_cmd.v_ret != IAVF_SUCCESS || buff_cmd.v_ret != IAVF_SUCCESS) {
+		err = -1;
+		PMD_DRV_LOG(ERR,
+			    "No response (%d times) or return failure (desc: %d / buff: %d)",
+			    i, desc_cmd.v_ret, buff_cmd.v_ret);
+	}
+
+ret:
+	ice_dcf_aq_cmd_clear(hw, &desc_cmd);
+	ice_dcf_aq_cmd_clear(hw, &buff_cmd);
+	rte_spinlock_unlock(&hw->vc_cmd_send_lock);
+
+	return err;
+}
+
+int
 ice_dcf_init_hw(struct rte_eth_dev *eth_dev, struct ice_dcf_hw *hw)
 {
 	struct rte_pci_device *pci_dev = RTE_ETH_DEV_TO_PCI(eth_dev);

drivers/net/ice/ice_dcf.h
@@ -11,6 +11,7 @@
 #include <iavf_adminq_cmd.h>
 #include <iavf_type.h>
 
+#include "base/ice_type.h"
 #include "ice_logs.h"
 
 struct dcf_virtchnl_cmd {
@@ -45,7 +46,8 @@ struct ice_dcf_hw {
 
 int ice_dcf_execute_virtchnl_cmd(struct ice_dcf_hw *hw,
 				 struct dcf_virtchnl_cmd *cmd);
-
+int ice_dcf_send_aq_cmd(void *dcf_hw, struct ice_aq_desc *desc,
+			void *buf, uint16_t buf_size);
 int ice_dcf_init_hw(struct rte_eth_dev *eth_dev, struct ice_dcf_hw *hw);
 void ice_dcf_uninit_hw(struct rte_eth_dev *eth_dev, struct ice_dcf_hw *hw);
 

commit 531230920a6d90ae7f58d072434844edae120e27

net/ice: add retry mechanism for DCF after failure
The origin design is if error happen during the step 3 of given situation,
it will return error directly without retry. While in current patch, it
will retry at every interval time during certain time. If retry succeed,
rule can be continuously created.

The given situation as following steps show:
step 1. Kernel PF and DPDK DCF are ready at the beginning.
step 2. A VF reset happen, kernel send an event to DCF and set STATE
to pause.
step 3. Before DCF receive the event, it is possible a rule creation is
ongoing and virtual channel command from DCF to kernel PF is executing.
step 4. Then result of command is failure, it will lead to error return
to DCF. Error code will be set as EINVAL, not EAGAIN.

Fixes: daa714d55c72 ("net/ice: handle AdminQ command by DCF")
Cc: stable@dpdk.org

Signed-off-by: Peng Zhang <peng1x.zhang@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/ice/ice_dcf.c
@@ -474,7 +474,7 @@ ice_dcf_send_aq_cmd(void *dcf_hw, struct ice_aq_desc *desc,
 	struct dcf_virtchnl_cmd desc_cmd, buff_cmd;
 	struct ice_dcf_hw *hw = dcf_hw;
 	int err = 0;
-	int i = 0;
+	int i, j = 0;
 
 	if ((buf && !buf_size) || (!buf && buf_size) ||
 	    buf_size > ICE_DCF_AQ_BUF_SZ)
@@ -501,25 +501,33 @@ ice_dcf_send_aq_cmd(void *dcf_hw, struct ice_aq_desc *desc,
 	ice_dcf_vc_cmd_set(hw, &desc_cmd);
 	ice_dcf_vc_cmd_set(hw, &buff_cmd);
 
-	if (ice_dcf_vc_cmd_send(hw, &desc_cmd) ||
-	    ice_dcf_vc_cmd_send(hw, &buff_cmd)) {
-		err = -1;
-		PMD_DRV_LOG(ERR, "fail to send OP_DCF_CMD_DESC/BUFF");
-		goto ret;
-	}
-
 	do {
-		if (!desc_cmd.pending && !buff_cmd.pending)
+		if (ice_dcf_vc_cmd_send(hw, &desc_cmd) ||
+		    ice_dcf_vc_cmd_send(hw, &buff_cmd)) {
+			err = -1;
+			PMD_DRV_LOG(ERR, "fail to send OP_DCF_CMD_DESC/BUFF");
+			goto ret;
+		}
+
+		i = 0;
+		do {
+			if (!desc_cmd.pending && !buff_cmd.pending)
+				break;
+
+			rte_delay_ms(ICE_DCF_ARQ_CHECK_TIME);
+		} while (i++ < ICE_DCF_ARQ_MAX_RETRIES);
+
+		if (desc_cmd.v_ret == IAVF_SUCCESS && buff_cmd.v_ret == IAVF_SUCCESS)
 			break;
 
 		rte_delay_ms(ICE_DCF_ARQ_CHECK_TIME);
-	} while (i++ < ICE_DCF_ARQ_MAX_RETRIES);
+	} while (j++ < ICE_DCF_ARQ_MAX_RETRIES);
 
 	if (desc_cmd.v_ret != IAVF_SUCCESS || buff_cmd.v_ret != IAVF_SUCCESS) {
 		err = -1;
 		PMD_DRV_LOG(ERR,
-			    "No response (%d times) or return failure (desc: %d / buff: %d)",
-			    i, desc_cmd.v_ret, buff_cmd.v_ret);
+			    "No response (%d times) or return failure (desc: %d / buff: %d)"
+			    " after retry %d times cmd", i, desc_cmd.v_ret, buff_cmd.v_ret, j);
 	}
 
 ret: