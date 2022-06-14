d5daed14228be4a696336ab311ec309103d5e1de
Tyler Retzlaff <roretzla@linux.microsoft.com>
2022-06-14 16:47:14 -0700

    eal: add thread lifetime management
    The *rte_thread_create()* function can optionally receive an
    rte_thread_attr_t object that will cause the thread to be created with
    the affinity and priority described by the attributes object. If
    no rte_thread_attr_t is passed (parameter is NULL), the default
    affinity and priority are used.
    
    On Windows, the function executed by a thread when the thread starts is
    represeneted by a function pointer of type DWORD (*func) (void*).
    On other platforms, the function pointer is a void* (*func) (void*).
    
    Performing a cast between these two types of function pointers to
    uniformize the API on all platforms may result in undefined behavior.
    TO fix this issue, a wrapper that respects the signature required by
    CreateThread() has been created on Windows.
    
    Signed-off-by: Narcisa Vasile <navasile@microsoft.com>
    Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/include/rte_thread.h
+++ lib/eal/include/rte_thread.h
@@ -30,6 +30,8 @@ typedef struct {
 	uintptr_t opaque_id; /**< thread identifier */
 } rte_thread_t;
 
+typedef void * (*rte_thread_func) (void *);
+
 /**
  * Thread priority values.
  */
@@ -56,6 +58,69 @@ typedef struct {
  * TLS key type, an opaque pointer.
  */
 typedef struct eal_tls_key *rte_thread_key;
+
+/**
+ * @warning
+ * @b EXPERIMENTAL: this API may change without prior notice.
+ *
+ * Create a new thread that will invoke the 'thread_func' routine.
+ *
+ * @param thread_id
+ *    A pointer that will store the id of the newly created thread.
+ *
+ * @param thread_attr
+ *    Attributes that are used at the creation of the new thread.
+ *
+ * @param thread_func
+ *    The routine that the new thread will invoke when starting execution.
+ *
+ * @param arg
+ *    Argument to be passed to the 'thread_func' routine.
+ *
+ * @return
+ *   On success, return 0.
+ *   On failure, return a positive errno-style error number.
+ */
+__rte_experimental
+int rte_thread_create(rte_thread_t *thread_id,
+		const rte_thread_attr_t *thread_attr,
+		rte_thread_func thread_func, void *arg);
+
+/**
+ * @warning
+ * @b EXPERIMENTAL: this API may change without prior notice.
+ *
+ * Waits for the thread identified by 'thread_id' to terminate
+ *
+ * @param thread_id
+ *    The identifier of the thread.
+ *
+ * @param value_ptr
+ *    Stores the exit status of the thread.
+ *
+ * @return
+ *   On success, return 0.
+ *   On failure, return a positive errno-style error number.
+ */
+__rte_experimental
+int rte_thread_join(rte_thread_t thread_id, unsigned long *value_ptr);
+
+/**
+ * @warning
+ * @b EXPERIMENTAL: this API may change without prior notice.
+ *
+ * Indicate that the return value of the thread is not needed and
+ * all thread resources should be release when the thread terminates.
+ *
+ * @param thread_id
+ *    The id of the thread to be detached.
+ *
+ * @return
+ *   On success, return 0.
+ *   On failure, return a positive errno-style error number.
+ */
+__rte_experimental
+int rte_thread_detach(rte_thread_t thread_id);
 
 /**
  * @warning
