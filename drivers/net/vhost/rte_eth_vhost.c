ovsrobot/dpdk

commit 3a6ee8dafb21d7a55af59b573195a9dc18732476

net/vhost: enable compliant offloading mode
This patch enables the compliant offloading flags mode by
default, which prevents the Rx path to set Tx offload flags,
which is illegal. A new legacy-ol-flags devarg is introduced
to enable the legacy behaviour.

Signed-off-by: Maxime Coquelin <maxime.coquelin@redhat.com>
Reviewed-by: Chenbo Xia <chenbo.xia@intel.com>

drivers/net/vhost/rte_eth_vhost.c
@@ -31,9 +31,10 @@ enum {VIRTIO_RXQ, VIRTIO_TXQ, VIRTIO_QNUM};
 #define ETH_VHOST_CLIENT_ARG		"client"
 #define ETH_VHOST_IOMMU_SUPPORT		"iommu-support"
 #define ETH_VHOST_POSTCOPY_SUPPORT	"postcopy-support"
-#define ETH_VHOST_VIRTIO_NET_F_HOST_TSO "tso"
-#define ETH_VHOST_LINEAR_BUF  "linear-buffer"
-#define ETH_VHOST_EXT_BUF  "ext-buffer"
+#define ETH_VHOST_VIRTIO_NET_F_HOST_TSO	"tso"
+#define ETH_VHOST_LINEAR_BUF		"linear-buffer"
+#define ETH_VHOST_EXT_BUF		"ext-buffer"
+#define ETH_VHOST_LEGACY_OL_FLAGS	"legacy-ol-flags"
 #define VHOST_MAX_PKT_BURST 32
 
 static const char *valid_arguments[] = {
@@ -45,6 +46,7 @@ static const char *valid_arguments[] = {
 	ETH_VHOST_VIRTIO_NET_F_HOST_TSO,
 	ETH_VHOST_LINEAR_BUF,
 	ETH_VHOST_EXT_BUF,
+	ETH_VHOST_LEGACY_OL_FLAGS,
 	NULL
 };
 
@@ -1470,6 +1472,7 @@ rte_pmd_vhost_probe(struct rte_vdev_device *dev)
 	int tso = 0;
 	int linear_buf = 0;
 	int ext_buf = 0;
+	int legacy_ol_flags = 0;
 	struct rte_eth_dev *eth_dev;
 	const char *name = rte_vdev_device_name(dev);
 
@@ -1578,6 +1581,17 @@ rte_pmd_vhost_probe(struct rte_vdev_device *dev)
 		if (ext_buf == 1)
 			flags |= RTE_VHOST_USER_EXTBUF_SUPPORT;
 	}
+
+	if (rte_kvargs_count(kvlist, ETH_VHOST_LEGACY_OL_FLAGS) == 1) {
+		ret = rte_kvargs_process(kvlist,
+				ETH_VHOST_LEGACY_OL_FLAGS,
+				&open_int, &legacy_ol_flags);
+		if (ret < 0)
+			goto out_free;
+	}
+
+	if (legacy_ol_flags == 0)
+		flags |= RTE_VHOST_USER_NET_COMPLIANT_OL_FLAGS;
 
 	if (dev->device.numa_node == SOCKET_ID_ANY)
 		dev->device.numa_node = rte_socket_id();

commit 07fbf22a1ab6bc338811f19fb780a06f40c08d38

net/vhost: fix compliant offloading flag
This patch fixes the check to set compliant offloading flag.
Compliant offloading flag should be set when the
'legacy-ol-flags' is true.

Fixes: 3a6ee8dafb21("net/vhost: enable compliant offloading mode")

Signed-off-by: Xuan Ding <xuan.ding@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/vhost/rte_eth_vhost.c
@@ -1735,7 +1735,7 @@ rte_pmd_vhost_probe(struct rte_vdev_device *dev)
 			goto out_free;
 	}
 
-	if (legacy_ol_flags == 0)
+	if (legacy_ol_flags == 1)
 		flags |= RTE_VHOST_USER_NET_COMPLIANT_OL_FLAGS;
 
 	if (dev->device.numa_node == SOCKET_ID_ANY)