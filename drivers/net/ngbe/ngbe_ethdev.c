ovsrobot/dpdk

commit 240422edbf8488e6d204f571080e5c271c90b182

net/ngbe: support OEM subsystem vendor ID
Add support for OEM subsystem vendor ID.

Signed-off-by: Jiawen Wu <jiawenwu@trustnetic.com>

drivers/net/ngbe/ngbe_ethdev.c
@@ -359,7 +359,19 @@ eth_ngbe_dev_init(struct rte_eth_dev *eth_dev, void *init_params __rte_unused)
 	hw->back = pci_dev;
 	hw->device_id = pci_dev->id.device_id;
 	hw->vendor_id = pci_dev->id.vendor_id;
-	hw->sub_system_id = pci_dev->id.subsystem_device_id;
+	if (pci_dev->id.subsystem_vendor_id == PCI_VENDOR_ID_WANGXUN) {
+		hw->sub_system_id = pci_dev->id.subsystem_device_id;
+	} else {
+		u32 ssid;
+
+		ssid = ngbe_flash_read_dword(hw, 0xFFFDC);
+		if (ssid == 0x1) {
+			PMD_INIT_LOG(ERR,
+				"Read of internal subsystem device id failed\n");
+			return -ENODEV;
+		}
+		hw->sub_system_id = (u16)ssid >> 8 | (u16)ssid << 8;
+	}
 	ngbe_map_device_id(hw);
 	hw->hw_addr = (void *)pci_dev->mem_resource[0].addr;
 

drivers/net/ngbe/base/ngbe_hw.c
@@ -1822,22 +1822,23 @@ s32 ngbe_enable_rx_dma(struct ngbe_hw *hw, u32 regval)
 /* cmd_addr is used for some special command:
  * 1. to be sector address, when implemented erase sector command
  * 2. to be flash address when implemented read, write flash address
+ *
+ * Return 0 on success, return 1 on failure.
  */
 u32 ngbe_fmgr_cmd_op(struct ngbe_hw *hw, u32 cmd, u32 cmd_addr)
 {
-	u32 cmd_val = 0;
-	u32 i = 0;
+	u32 cmd_val, i;
 
 	cmd_val = NGBE_SPICMD_CMD(cmd) | NGBE_SPICMD_CLK(3) | cmd_addr;
 	wr32(hw, NGBE_SPICMD, cmd_val);
 
-	for (i = 0; i < 10000; i++) {
+	for (i = 0; i < NGBE_SPI_TIMEOUT; i++) {
 		if (rd32(hw, NGBE_SPISTAT) & NGBE_SPISTAT_OPDONE)
 			break;
 
 		usec_delay(10);
 	}
-	if (i == 10000)
+	if (i == NGBE_SPI_TIMEOUT)
 		return 1;
 
 	return 0;
@@ -1845,10 +1846,10 @@ u32 ngbe_fmgr_cmd_op(struct ngbe_hw *hw, u32 cmd, u32 cmd_addr)
 
 u32 ngbe_flash_read_dword(struct ngbe_hw *hw, u32 addr)
 {
-	u32 status = 0;
+	u32 status;
 
 	status = ngbe_fmgr_cmd_op(hw, 1, addr);
-	if (status) {
+	if (status == 0x1) {
 		DEBUGOUT("Read flash timeout.");
 		return status;
 	}

drivers/net/ngbe/base/ngbe_type.h
@@ -18,7 +18,7 @@
 #define NGBE_MAX_UTA              128
 
 #define NGBE_PCI_MASTER_DISABLE_TIMEOUT	800
-
+#define NGBE_SPI_TIMEOUT	10000
 
 #define NGBE_ALIGN		128 /* as intel did */
 #define NGBE_ISB_SIZE		16

commit d2a12078d3753605e762584ef53f141607a7c614

net/ngbe: fix customized devices probe failure
The devices with OEM subsystem vendor ID failed to be initialized,
because flash was read before memory address was set.

Fixes: 240422edbf84 ("net/ngbe: support OEM subsystem vendor ID")

Signed-off-by: Jiawen Wu <jiawenwu@trustnetic.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/ngbe/ngbe_ethdev.c
@@ -363,6 +363,8 @@ eth_ngbe_dev_init(struct rte_eth_dev *eth_dev, void *init_params __rte_unused)
 	rte_eth_copy_pci_info(eth_dev, pci_dev);
 	eth_dev->data->dev_flags |= RTE_ETH_DEV_AUTOFILL_QUEUE_XSTATS;
 
+	hw->hw_addr = (void *)pci_dev->mem_resource[0].addr;
+
 	/* Vendor and Device ID need to be set before init of shared code */
 	hw->back = pci_dev;
 	hw->device_id = pci_dev->id.device_id;
@@ -381,7 +383,6 @@ eth_ngbe_dev_init(struct rte_eth_dev *eth_dev, void *init_params __rte_unused)
 		hw->sub_system_id = (u16)ssid >> 8 | (u16)ssid << 8;
 	}
 	ngbe_map_device_id(hw);
-	hw->hw_addr = (void *)pci_dev->mem_resource[0].addr;
 
 	/* Reserve memory for interrupt status block */
 	mz = rte_eth_dma_zone_reserve(eth_dev, "ngbe_driver", -1,