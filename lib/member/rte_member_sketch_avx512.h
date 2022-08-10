6ac65ead046a707a64790a53f5140c832e30b93e
Leyi Rong <leyi.rong@intel.com>
2022-08-10 15:45:17 +0800

    member: implement NitroSketch mode
    Sketching algorithm provide high-fidelity approximate measurements and
    appears as a promising alternative to traditional approches such as
    packet sampling.
    
    NitroSketch [1] is a software sketching framework that optimizes
    performance, provides accuracy guarantees, and supports a variety of
    sketches.
    
    This commit adds a new data structure called sketch into
    membership library. This new data structure is an efficient
    way to profile the traffic for heavy hitters. Also use min-heap
    structure to maintain the top-k flow keys.
    
    [1] Zaoxing Liu, Ran Ben-Basat, Gil Einziger, Yaron Kassner, Vladimir
    Braverman, Roy Friedman, Vyas Sekar, "NitroSketch: Robust and General
    Sketch-based Monitoring in Software Switches", in ACM SIGCOMM 2019.
    
    Signed-off-by: Alan Liu <zaoxingliu@gmail.com>
    Signed-off-by: Yipeng Wang <yipeng1.wang@intel.com>
    Signed-off-by: Leyi Rong <leyi.rong@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ lib/member/rte_member_sketch_avx512.h
@@ -0,0 +1,36 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2020 Intel Corporation
+ */
+
+#ifndef _RTE_MEMBER_SKETCH_AVX512_H_
+#define _RTE_MEMBER_SKETCH_AVX512_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <rte_vect.h>
+#include "rte_member.h"
+#include "rte_member_sketch.h"
+#include "rte_xxh64_avx512.h"
+
+#define NUM_ROW_VEC 8
+
+void
+sketch_update_avx512(const struct rte_member_setsum *ss,
+		     const void *key,
+		     uint32_t count);
+
+uint64_t
+sketch_lookup_avx512(const struct rte_member_setsum *ss,
+		     const void *key);
+
+void
+sketch_delete_avx512(const struct rte_member_setsum *ss,
+		     const void *key);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RTE_MEMBER_SKETCH_AVX512_H_ */
