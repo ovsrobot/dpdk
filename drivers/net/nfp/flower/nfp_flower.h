a6977a2cdc933402997b8550156b12a468780331
Chaoyong He <chaoyong.he@corigine.com>
2022-08-12 18:22:27 +0800

    net/nfp: add flower ctrl VNIC rxtx logic
    Adds a Rx and Tx function for the ctrl VNIC. The logic is mostly
    identical to the normal Rx and Tx functionality of the NFP PMD.
    
    Make use of the ctrl VNIC service logic to service the ctrl VNIC Rx
    path.
    
    Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
    Signed-off-by: Heinrich Kuhn <heinrich.kuhn@corigine.com>
    Reviewed-by: Niklas Söderlund <niklas.soderlund@corigine.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/nfp/flower/nfp_flower.h
+++ drivers/net/nfp/flower/nfp_flower.h
@@ -7,8 +7,17 @@
 #define _NFP_FLOWER_H_
 
 enum nfp_flower_service {
+	NFP_FLOWER_SERVICE_CTRL,
 	NFP_FLOWER_SERVICE_MAX
 };
+
+/*
+ * Flower fallback and ctrl path always adds and removes
+ * 8 bytes of prepended data. Tx descriptors must point
+ * to the correct packet data offset after metadata has
+ * been added
+ */
+#define FLOWER_PKT_DATA_OFFSET 8
 
 /* The flower application's private structure */
 struct nfp_app_flower {
@@ -29,6 +38,12 @@ struct nfp_app_flower {
 
 	/* the eth table as reported by firmware */
 	struct nfp_eth_table *nfp_eth_table;
+
+	/* Ctrl vNIC Rx counter */
+	uint64_t ctrl_vnic_rx_count;
+
+	/* Ctrl vNIC Tx counter */
+	uint64_t ctrl_vnic_tx_count;
 };
 
 int nfp_init_app_flower(struct nfp_pf_dev *pf_dev);
