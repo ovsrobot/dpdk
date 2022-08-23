15b88e623f06dfaef3ed5d50a7e533ac96711520
Yuan Wang <yuanx.wang@intel.com>
2022-08-24 00:35:34 +0800

    net/vhost: support asynchronous data path
    Vhost asynchronous data-path offloads packet copy from the CPU
    to the DMA engine. As a result, large packet copy can be accelerated
    by the DMA engine, and vhost can free CPU cycles for higher level
    functions.
    
    In this patch, we enable asynchronous data-path for vhostpmd.
    Asynchronous data path is enabled per tx/rx queue, and users need
    to specify the DMA device used by the tx/rx queue. Each tx/rx queue
    only supports to use one DMA device, but one DMA device can be shared
    among multiple tx/rx queues of different vhostpmd ports.
    
    Two PMD parameters are added:
    - dmas:	specify the used DMA device for a tx/rx queue.
    	(Default: no queues enable asynchronous data path)
    - dma-ring-size: DMA ring size.
    	(Default: 4096).
    
    Here is an example:
    --vdev 'eth_vhost0,iface=./s0,dmas=[txq0@0000:00.01.0;rxq0@0000:00.01.1],dma-ring-size=4096'
    
    Signed-off-by: Jiayu Hu <jiayu.hu@intel.com>
    Signed-off-by: Yuan Wang <yuanx.wang@intel.com>
    Signed-off-by: Wenwu Ma <wenwux.ma@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/vhost/vhost_testpmd.c
@@ -0,0 +1,65 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2022 Intel Corporation.
+ */
+#include <rte_eth_vhost.h>
+#include <cmdline_parse_num.h>
+#include <cmdline_parse_string.h>
+
+#include "testpmd.h"
+
+struct cmd_tx_poll_result {
+	cmdline_fixed_string_t async_vhost;
+	cmdline_fixed_string_t tx;
+	cmdline_fixed_string_t poll;
+	cmdline_fixed_string_t completed;
+	cmdline_fixed_string_t what;
+};
+
+static cmdline_parse_token_string_t cmd_tx_async_vhost =
+	TOKEN_STRING_INITIALIZER(struct cmd_tx_poll_result, async_vhost, "async_vhost");
+static cmdline_parse_token_string_t cmd_tx_tx =
+	TOKEN_STRING_INITIALIZER(struct cmd_tx_poll_result, tx, "tx");
+static cmdline_parse_token_string_t cmd_tx_poll =
+	TOKEN_STRING_INITIALIZER(struct cmd_tx_poll_result, poll, "poll");
+static cmdline_parse_token_string_t cmd_tx_completed =
+	TOKEN_STRING_INITIALIZER(struct cmd_tx_poll_result, completed, "completed");
+static cmdline_parse_token_string_t cmd_tx_what =
+	TOKEN_STRING_INITIALIZER(struct cmd_tx_poll_result, what, "on#off");
+
+static void
+cmd_tx_poll_parsed(void *parsed_result, __rte_unused struct cmdline *cl, __rte_unused void *data)
+{
+	struct cmd_tx_poll_result *res = parsed_result;
+
+	if (!strcmp(res->what, "on"))
+		rte_eth_vhost_async_tx_poll_completed(true);
+	else if (!strcmp(res->what, "off"))
+		rte_eth_vhost_async_tx_poll_completed(false);
+}
+
+static cmdline_parse_inst_t async_vhost_cmd_tx_poll = {
+	.f = cmd_tx_poll_parsed,
+	.data = NULL,
+	.help_str = "async-vhost tx poll completed on|off",
+	.tokens = {
+		(void *)&cmd_tx_async_vhost,
+		(void *)&cmd_tx_tx,
+		(void *)&cmd_tx_poll,
+		(void *)&cmd_tx_completed,
+		(void *)&cmd_tx_what,
+		NULL,
+	},
+};
+
+static struct testpmd_driver_commands async_vhost_cmds = {
+	.commands = {
+	{
+		&async_vhost_cmd_tx_poll,
+		"async_vhost tx poll completed (on|off)\n"
+		"    Poll and free DMA completed packets in Tx path.\n",
+	},
+	{ NULL, NULL },
+	},
+};
+
+TESTPMD_ADD_DRIVER_COMMANDS(async_vhost_cmds)
