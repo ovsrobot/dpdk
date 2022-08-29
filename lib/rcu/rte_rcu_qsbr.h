ovsrobot/dpdk

commit 30a1de105a5f40d77b344a891c4a68f79e815c43

lib: remove unneeded header includes
These header includes have been flagged by the iwyu_tool
and removed.

Signed-off-by: Sean Morrissey <sean.morrissey@intel.com>

lib/rcu/rte_rcu_qsbr.h
@@ -32,11 +32,7 @@ extern "C" {
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdint.h>
-#include <inttypes.h>
-#include <errno.h>
 #include <rte_common.h>
-#include <rte_memory.h>
-#include <rte_lcore.h>
 #include <rte_debug.h>
 #include <rte_atomic.h>
 #include <rte_ring.h>

commit ac135c761f980ce220f1f34588f78771bb2ec407

rcu: fix build failure with debug dp log level
Build fails if RTE_LOG_DP_LEVEL is set to RTE_LOG_DEBUG. Fix the
same by including the required header when RTE_LOG_DP_LEVEL is
set to RTE_LOG_DEBUG.

../lib/rcu/rte_rcu_qsbr.h:678:40: error: expected ‘)’ before ‘PRIu64’
  678 |    "%s: status: least acked token = %" PRIu64,
      |                                        ^~~~~~

Fixes: 30a1de105a5f ("lib: remove unneeded header includes")
Cc: sean.morrissey@intel.com

Signed-off-by: Anoob Joseph <anoobj@marvell.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/rcu/rte_rcu_qsbr.h
@@ -37,6 +37,10 @@ extern "C" {
 #include <rte_atomic.h>
 #include <rte_ring.h>
 
+#if RTE_LOG_DP_LEVEL >= RTE_LOG_DEBUG
+#include <inttypes.h>
+#endif
+
 extern int rte_rcu_log_type;
 
 #if RTE_LOG_DP_LEVEL >= RTE_LOG_DEBUG