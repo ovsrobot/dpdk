3727ccb342d6ec1752a38c22540b66b5f9371324
Yuan Wang <yuanx.wang@intel.com>
2022-09-03 03:10:26 +0800

    app/testpmd: add rxhdrs commands and parameters
    Add command line parameter:
    --rxhdrs=mac,[ipv4,udp]
    
    Set the protocol_hdr of segments to scatter packets on receiving if
    split feature is engaged. And the queues with BUFFER_SPLIT flag.
    
    Add interactive mode command:
    testpmd>set rxhdrs mac,ipv4,tcp,udp,sctp
    (protocol sequence should be valid)
    
    The protocol split feature is off by default. To enable protocol split,
    you need:
    1. Start testpmd with multiple mempools. E.g. --mbuf-size=2048,2048
    2. Configure Rx queue with rx_offload buffer split on.
    3. Set the protocol type of buffer split. E.g. set rxhdrs mac,ipv4
            (default protocols of testpmd : mac|icmp|ipv4|ipv6|tcp|udp|
                                  sctp|inner_mac|inner_ipv4|inner_ipv6|
                                  inner_tcp|inner_udp|inner_sctp)
    Above protocols can be configured in testpmd. But the configuration can
    only be applied when it is supported by specific pmd.
    
    Signed-off-by: Yuan Wang <yuanx.wang@intel.com>
    Signed-off-by: Xuan Ding <xuan.ding@intel.com>
    Signed-off-by: Wenxuan Wu <wenxuanx.wu@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- app/test-pmd/testpmd.c
+++ app/test-pmd/testpmd.c
@@ -247,6 +247,7 @@ uint16_t rx_pkt_seg_lengths[MAX_SEGS_BUFFER_SPLIT];
 uint8_t  rx_pkt_nb_segs; /**< Number of segments to split */
 uint16_t rx_pkt_seg_offsets[MAX_SEGS_BUFFER_SPLIT];
 uint8_t  rx_pkt_nb_offs; /**< Number of specified offsets */
+uint32_t rx_pkt_hdr_protos[MAX_SEGS_BUFFER_SPLIT];
 
 /*
  * Configuration of packet segments used by the "txonly" processing engine.
@@ -2685,6 +2686,7 @@ rx_queue_setup(uint16_t port_id, uint16_t rx_queue_id,
 		rx_seg->offset = i < rx_pkt_nb_offs ?
 				   rx_pkt_seg_offsets[i] : 0;
 		rx_seg->mp = mpx ? mpx : mp;
+		rx_seg->proto_hdr = rx_pkt_hdr_protos[i];
 	}
 	rx_conf->rx_nseg = rx_pkt_nb_segs;
 	rx_conf->rx_seg = rx_useg;
