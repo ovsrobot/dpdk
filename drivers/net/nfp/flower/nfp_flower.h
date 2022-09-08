50edf36a7f151062065e099eab23fdbc691dce5e
Chaoyong He <chaoyong.he@corigine.com>
2022-09-08 16:44:55 +0800

    net/nfp: add flower PF setup logic
    Adds the vNIC initialization logic for the flower PF vNIC. The flower
    firmware exposes this vNIC for the purposes of fallback traffic in the
    switchdev use-case.
    
    Adds minimal dev_ops for this PF device. Because the device is being
    exposed externally to DPDK it should also be configured using DPDK
    helpers like rte_eth_configure(). For these helpers to work the flower
    logic needs to implements a minimal set of dev_ops.
    
    Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
    Reviewed-by: Niklas Söderlund <niklas.soderlund@corigine.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/nfp/flower/nfp_flower.h
+++ drivers/net/nfp/flower/nfp_flower.h
@@ -8,8 +8,14 @@
 
 /* The flower application's private structure */
 struct nfp_app_fw_flower {
+	/* Pointer to a mempool for the PF vNIC */
+	struct rte_mempool *pf_pktmbuf_pool;
+
 	/* Pointer to the PF vNIC */
 	struct nfp_net_hw *pf_hw;
+
+	/* the eth table as reported by firmware */
+	struct nfp_eth_table *nfp_eth_table;
 };
 
 int nfp_init_app_fw_flower(struct nfp_pf_dev *pf_dev);
