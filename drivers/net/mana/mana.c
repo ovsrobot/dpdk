9ed74532cd237f107bd986ff935b3b92db5d3e38
Long Li <longli@microsoft.com>
2022-09-23 19:45:43 -0700

    net/mana: send packets
    With all the TX queues created, MANA can send packets over those queues.
    
    Signed-off-by: Long Li <longli@microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/mana/mana.c
+++ drivers/net/mana/mana.c
@@ -991,6 +991,7 @@ mana_pci_probe_mac(struct rte_pci_device *pci_dev,
 				/* fd is no not used after mapping doorbell */
 				close(fd);
 
+				eth_dev->tx_pkt_burst = mana_tx_burst;
 				eth_dev->rx_pkt_burst = mana_rx_burst;
 
 				rte_spinlock_lock(&mana_shared_data->lock);
