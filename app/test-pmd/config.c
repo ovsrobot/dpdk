1d60dc20bb85fdaf7c4c06363892beba08fc28b9
Yuan Wang <yuanx.wang@intel.com>
2022-10-06 07:18:35 +0800

    app/testpmd: add rxhdrs commands and parameters
    Add command line parameter:
    --rxhdrs=eth[,ipv4,udp]
    
    Set the protocol_hdr of segments to scatter packets on receiving if
    split feature is engaged. And the queues with BUFFER_SPLIT flag.
    
    Add interactive mode command:
    testpmd>set rxhdrs eth,ipv4,udp
    (protocol sequence should be valid)
    
    The protocol split feature is off by default. To enable protocol split,
    you need:
    1. Start testpmd with multiple mempools. E.g. --mbuf-size=2048,2048
    2. Configure Rx queue with rx_offload buffer split on.
    3. Set the protocol type of buffer split. E.g. set rxhdrs eth,eth-ipv4
            (default protocols of testpmd : eth|ipv4|ipv6|ipv4-tcp|ipv6-tcp|
             ipv4-udp|ipv6-udp|ipv4-sctp|ipv6-sctp|grenat|inner-eth|
             inner-ipv4|inner-ipv6|inner-ipv4-tcp|inner-ipv6-tcp|
             inner-ipv4-udp|inner-ipv6-udp|inner-ipv4-sctp|inner-ipv6-sctp)
    Above protocols can be configured in testpmd. But the configuration can
    only be applied when it is supported by specific pmd.
    
    Signed-off-by: Yuan Wang <yuanx.wang@intel.com>
    Signed-off-by: Xuan Ding <xuan.ding@intel.com>
    Signed-off-by: Wenxuan Wu <wenxuanx.wu@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- app/test-pmd/config.c
+++ app/test-pmd/config.c
@@ -4844,6 +4844,101 @@ show_rx_pkt_segments(void)
 	}
 }
 
+static const char *get_ptype_str(uint32_t ptype)
+{
+	switch (ptype) {
+	case RTE_PTYPE_L2_ETHER:
+		return "eth";
+	case RTE_PTYPE_L3_IPV4_EXT_UNKNOWN:
+		return "ipv4";
+	case RTE_PTYPE_L3_IPV6_EXT_UNKNOWN:
+		return "ipv6";
+	case RTE_PTYPE_L3_IPV4_EXT_UNKNOWN | RTE_PTYPE_L4_TCP:
+		return "ipv4-tcp";
+	case RTE_PTYPE_L3_IPV6_EXT_UNKNOWN | RTE_PTYPE_L4_TCP:
+		return "ipv6-tcp";
+	case RTE_PTYPE_L3_IPV4_EXT_UNKNOWN | RTE_PTYPE_L4_UDP:
+		return "ipv4-udp";
+	case RTE_PTYPE_L3_IPV6_EXT_UNKNOWN | RTE_PTYPE_L4_UDP:
+		return "ipv6-udp";
+	case RTE_PTYPE_L3_IPV4_EXT_UNKNOWN | RTE_PTYPE_L4_SCTP:
+		return "ipv4-sctp";
+	case RTE_PTYPE_L3_IPV6_EXT_UNKNOWN | RTE_PTYPE_L4_SCTP:
+		return "ipv6-sctp";
+	case RTE_PTYPE_L4_TCP:
+		return "tcp";
+	case RTE_PTYPE_L4_UDP:
+		return "tcp";
+	case RTE_PTYPE_L4_SCTP:
+		return "sctp";
+	case RTE_PTYPE_TUNNEL_GRENAT:
+		return "grenat";
+	case RTE_PTYPE_INNER_L2_ETHER:
+		return "inner-eth";
+	case RTE_PTYPE_INNER_L3_IPV4_EXT_UNKNOWN:
+		return "inner-ipv4";
+	case RTE_PTYPE_INNER_L3_IPV6_EXT_UNKNOWN:
+		return "inner-ipv6";
+	case RTE_PTYPE_INNER_L3_IPV4_EXT_UNKNOWN | RTE_PTYPE_INNER_L4_TCP:
+		return "inner-ipv4-tcp";
+	case RTE_PTYPE_INNER_L3_IPV6_EXT_UNKNOWN | RTE_PTYPE_INNER_L4_TCP:
+		return "inner-ipv6-tcp";
+	case RTE_PTYPE_INNER_L3_IPV4_EXT_UNKNOWN | RTE_PTYPE_INNER_L4_UDP:
+		return "inner-ipv4-udp";
+	case RTE_PTYPE_INNER_L3_IPV6_EXT_UNKNOWN | RTE_PTYPE_INNER_L4_UDP:
+		return "inner-ipv6-udp";
+	case RTE_PTYPE_INNER_L3_IPV4_EXT_UNKNOWN | RTE_PTYPE_INNER_L4_SCTP:
+		return "inner-ipv4-sctp";
+	case RTE_PTYPE_INNER_L3_IPV6_EXT_UNKNOWN | RTE_PTYPE_INNER_L4_SCTP:
+		return "inner-ipv6-sctp";
+	case RTE_PTYPE_INNER_L4_TCP:
+		return "inner-tcp";
+	case RTE_PTYPE_INNER_L4_UDP:
+		return "inner-udp";
+	case RTE_PTYPE_INNER_L4_SCTP:
+		return "inner-sctp";
+	default:
+		return "unsupported";
+	}
+}
+
+void
+show_rx_pkt_hdrs(void)
+{
+	uint32_t i, n;
+
+	n = rx_pkt_nb_segs;
+	printf("Number of segments: %u\n", n);
+	if (n) {
+		printf("Packet segs: ");
+		for (i = 0; i < n - 1; i++)
+			printf("%s, ", get_ptype_str(rx_pkt_hdr_protos[i]));
+		printf("payload\n");
+	}
+}
+
+void
+set_rx_pkt_hdrs(unsigned int *seg_hdrs, unsigned int nb_segs)
+{
+	unsigned int i;
+
+	if (nb_segs + 1 > MAX_SEGS_BUFFER_SPLIT) {
+		printf("nb segments per RX packets=%u > "
+		       "MAX_SEGS_BUFFER_SPLIT - ignored\n", nb_segs + 1);
+		return;
+	}
+
+	memset(rx_pkt_hdr_protos, 0, sizeof(rx_pkt_hdr_protos));
+
+	for (i = 0; i < nb_segs; i++)
+		rx_pkt_hdr_protos[i] = (uint32_t)seg_hdrs[i];
+	/*
+	 * We calculate the number of hdrs, but payload is not included,
+	 * so rx_pkt_nb_segs would increase 1.
+	 */
+	rx_pkt_nb_segs = nb_segs + 1;
+}
+
 void
 set_rx_pkt_segments(unsigned int *seg_lengths, unsigned int nb_segs)
 {
