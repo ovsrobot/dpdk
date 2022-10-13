d36beff0e17baaed988aedade23e6bead40caa03
Owen Hilyard <ohilyard@iol.unh.edu>
2022-10-13 10:35:10 +0000

    dts: add config parser module
    The configuration is split into two parts, one defining the parameters
    of the test run and the other defining the topology to be used.
    
    The format of the configuration is YAML. It is validated according to a
    json schema which also server as detailed documentation of the various
    configuration fields. This means that the complete set of allowed values
    are tied to the schema as a source of truth. This enables making changes
    to parts of DTS that interface with config files without a high risk of
    breaking someone's configuration.
    
    This configuration system uses immutable objects to represent the
    configuration, making IDE/LSP autocomplete work properly.
    
    There are two ways to specify the configuration file path, an
    environment variable or a command line argument, applied in that order.
    
    Signed-off-by: Owen Hilyard <ohilyard@iol.unh.edu>
    Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ dts/framework/settings.py
@@ -0,0 +1,84 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2010-2021 Intel Corporation
+# Copyright(c) 2022 PANTHEON.tech s.r.o.
+# Copyright(c) 2022 University of New Hampshire
+#
+
+import argparse
+import os
+from dataclasses import dataclass
+from typing import Any, Callable, Iterable, Optional, Sequence, TypeVar
+
+_T = TypeVar("_T")
+
+
+def _env_arg(env_var: str) -> Any:
+    class _EnvironmentArgument(argparse.Action):
+        def __init__(
+            self,
+            option_strings: Sequence[str],
+            dest: str,
+            nargs: Optional[str | int] = None,
+            const: Optional[str] = None,
+            default: str = None,
+            type: Callable[[str], Optional[_T | argparse.FileType]] = None,
+            choices: Optional[Iterable[_T]] = None,
+            required: bool = True,
+            help: Optional[str] = None,
+            metavar: Optional[str | tuple[str, ...]] = None,
+        ) -> None:
+            env_var_value = os.environ.get(env_var)
+            default = env_var_value or default
+            super(_EnvironmentArgument, self).__init__(
+                option_strings,
+                dest,
+                nargs=nargs,
+                const=const,
+                default=default,
+                type=type,
+                choices=choices,
+                required=required,
+                help=help,
+                metavar=metavar,
+            )
+
+        def __call__(
+            self,
+            parser: argparse.ArgumentParser,
+            namespace: argparse.Namespace,
+            values: Any,
+            option_string: str = None,
+        ) -> None:
+            setattr(namespace, self.dest, values)
+
+    return _EnvironmentArgument
+
+
+@dataclass(slots=True, frozen=True)
+class _Settings:
+    config_file_path: str
+
+
+def _get_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser(description="DPDK test framework.")
+
+    parser.add_argument(
+        "--config-file",
+        action=_env_arg("DTS_CFG_FILE"),
+        default="conf.yaml",
+        required=False,
+        help="[DTS_CFG_FILE] configuration file that describes the test cases, SUTs "
+        "and targets.",
+    )
+
+    return parser
+
+
+def _get_settings() -> _Settings:
+    parsed_args = _get_parser().parse_args()
+    return _Settings(
+        config_file_path=parsed_args.config_file,
+    )
+
+
+SETTINGS: _Settings = _get_settings()
