7a841574fce5f2d595069b8ff63108b14adc5cdc
Chengwen Feng <fengchengwen@huawei.com>
2022-10-17 03:40:52 +0000

    memarea: introduce memarea library
    The memarea library is an allocator of variable-size object which based
    on a memory region.
    
    This patch provides rte_memarea_create() and rte_memarea_destroy() API.
    
    Signed-off-by: Chengwen Feng <fengchengwen@huawei.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/include/rte_log.h
+++ lib/eal/include/rte_log.h
@@ -48,6 +48,7 @@ extern "C" {
 #define RTE_LOGTYPE_EFD       18 /**< Log related to EFD. */
 #define RTE_LOGTYPE_EVENTDEV  19 /**< Log related to eventdev. */
 #define RTE_LOGTYPE_GSO       20 /**< Log related to GSO. */
+#define RTE_LOGTYPE_MEMAREA   21 /**< Log related to memarea. */
 
 /* these log types can be used in an application */
 #define RTE_LOGTYPE_USER1     24 /**< User-defined log type 1. */
