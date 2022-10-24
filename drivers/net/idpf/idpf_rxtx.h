86691cea89c8bdc01009872166c57a65d705131c
Junfeng Guo <junfeng.guo@intel.com>
2022-10-24 21:12:20 +0800

    net/idpf: add support for basic Rx datapath
    Add basic Rx support in split queue mode and single queue mode.
    
    Signed-off-by: Beilei Xing <beilei.xing@intel.com>
    Signed-off-by: Xiaoyun Li <xiaoyun.li@intel.com>
    Signed-off-by: Junfeng Guo <junfeng.guo@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/idpf/idpf_rxtx.h
+++ drivers/net/idpf/idpf_rxtx.h
@@ -140,8 +140,13 @@ int idpf_tx_queue_start(struct rte_eth_dev *dev, uint16_t tx_queue_id);
 int idpf_tx_queue_stop(struct rte_eth_dev *dev, uint16_t tx_queue_id);
 void idpf_dev_tx_queue_release(struct rte_eth_dev *dev, uint16_t qid);
 
+uint16_t idpf_singleq_recv_pkts(void *rx_queue, struct rte_mbuf **rx_pkts,
+				uint16_t nb_pkts);
+uint16_t idpf_splitq_recv_pkts(void *rx_queue, struct rte_mbuf **rx_pkts,
+			       uint16_t nb_pkts);
 void idpf_stop_queues(struct rte_eth_dev *dev);
 
+void idpf_set_rx_function(struct rte_eth_dev *dev);
 const uint32_t *idpf_dev_supported_ptypes_get(struct rte_eth_dev *dev);
 
 #endif /* _IDPF_RXTX_H_ */
