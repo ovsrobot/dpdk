ovsrobot/dpdk

commit 339f1ba5135367e566c3ca9db68910fd8c7a6448

net/bonding: make configure method re-entrant
According to the documentation, rte_eth_dev_configure()
can be invoked repeatedly while in stopped state.
The current implementation in the bonding driver
allows for that (technically), but the user sees
warnings which say that back-end devices have
already been harnessed. Re-factor the code
to have cleanup before each (re-)configure.

Signed-off-by: Ivan Malov <ivan.malov@oktetlabs.ru>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>
Acked-by: Chas Williams <3chas3@gmail.com>

drivers/net/bonding/rte_eth_bond_pmd.c
@@ -2148,18 +2148,14 @@ bond_ethdev_stop(struct rte_eth_dev *eth_dev)
 	return 0;
 }
 
-int
-bond_ethdev_close(struct rte_eth_dev *dev)
+static void
+bond_ethdev_cfg_cleanup(struct rte_eth_dev *dev)
 {
 	struct bond_dev_private *internals = dev->data->dev_private;
 	uint16_t bond_port_id = internals->port_id;
 	int skipped = 0;
 	struct rte_flow_error ferror;
 
-	if (rte_eal_process_type() != RTE_PROC_PRIMARY)
-		return 0;
-
-	RTE_BOND_LOG(INFO, "Closing bonded device %s", dev->device->name);
 	while (internals->slave_count != skipped) {
 		uint16_t port_id = internals->slaves[skipped].port_id;
 
@@ -2178,6 +2174,20 @@ bond_ethdev_close(struct rte_eth_dev *dev)
 		}
 	}
 	bond_flow_ops.flush(dev, &ferror);
+}
+
+int
+bond_ethdev_close(struct rte_eth_dev *dev)
+{
+	struct bond_dev_private *internals = dev->data->dev_private;
+
+	if (rte_eal_process_type() != RTE_PROC_PRIMARY)
+		return 0;
+
+	RTE_BOND_LOG(INFO, "Closing bonded device %s", dev->device->name);
+
+	bond_ethdev_cfg_cleanup(dev);
+
 	bond_ethdev_free_queues(dev);
 	rte_bitmap_reset(internals->vlan_filter_bmp);
 	rte_bitmap_free(internals->vlan_filter_bmp);
@@ -3609,6 +3619,9 @@ bond_ethdev_configure(struct rte_eth_dev *dev)
 	};
 
 	unsigned i, j;
+
+
+	bond_ethdev_cfg_cleanup(dev);
 
 	/*
 	 * If RSS is enabled, fill table with default values and

commit f6af87c1ebfdc489ddfee3710b5a516146d48e35

net/bonding: fix device configure reentrancy
As per report [1], the previous patch for device
configure code apparently overlooks the corner
case of manually adding back-end devices to
the bond using testpmd CLI. The problem is
in removing back-end ports on re-configure
instead of just stopping them. Fix that.

Fixes: 339f1ba51353 ("net/bonding: make configure method re-entrant")
Cc: stable@dpdk.org

[1] https://bugs.dpdk.org/show_bug.cgi?id=1119

Signed-off-by: Ivan Malov <ivan.malov@oktetlabs.ru>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/bonding/eth_bond_private.h
@@ -184,6 +184,8 @@ struct bond_dev_private {
 
 	void *vlan_filter_bmpmem;		/* enabled vlan filter bitmap */
 	struct rte_bitmap *vlan_filter_bmp;
+
+	bool kvargs_processing_is_done;
 };
 
 extern const struct eth_dev_ops default_dev_ops;

drivers/net/bonding/rte_eth_bond_pmd.c
@@ -2170,7 +2170,7 @@ bond_ethdev_stop(struct rte_eth_dev *eth_dev)
 }
 
 static void
-bond_ethdev_cfg_cleanup(struct rte_eth_dev *dev)
+bond_ethdev_cfg_cleanup(struct rte_eth_dev *dev, bool remove)
 {
 	struct bond_dev_private *internals = dev->data->dev_private;
 	uint16_t bond_port_id = internals->port_id;
@@ -2182,10 +2182,15 @@ bond_ethdev_cfg_cleanup(struct rte_eth_dev *dev)
 
 	while (internals->slave_count != skipped) {
 		uint16_t port_id = internals->slaves[skipped].port_id;
+		int ret;
 
-		if (rte_eth_dev_stop(port_id) != 0) {
+		ret = rte_eth_dev_stop(port_id);
+		if (ret != 0) {
 			RTE_BOND_LOG(ERR, "Failed to stop device on port %u",
 				     port_id);
+		}
+
+		if (ret != 0 || !remove) {
 			skipped++;
 			continue;
 		}
@@ -2209,7 +2214,7 @@ bond_ethdev_close(struct rte_eth_dev *dev)
 
 	RTE_BOND_LOG(INFO, "Closing bonded device %s", dev->device->name);
 
-	bond_ethdev_cfg_cleanup(dev);
+	bond_ethdev_cfg_cleanup(dev, true);
 
 	bond_ethdev_free_queues(dev);
 	rte_bitmap_reset(internals->vlan_filter_bmp);
@@ -3644,7 +3649,7 @@ bond_ethdev_configure(struct rte_eth_dev *dev)
 	unsigned i, j;
 
 
-	bond_ethdev_cfg_cleanup(dev);
+	bond_ethdev_cfg_cleanup(dev, false);
 
 	/*
 	 * If RSS is enabled, fill table with default values and
@@ -3733,8 +3738,10 @@ bond_ethdev_configure(struct rte_eth_dev *dev)
 	 * if no kvlist, it means that this bonded device has been created
 	 * through the bonding api.
 	 */
-	if (!kvlist)
+	if (!kvlist || internals->kvargs_processing_is_done)
 		return 0;
+
+	internals->kvargs_processing_is_done = true;
 
 	/* Parse MAC address for bonded device */
 	arg_count = rte_kvargs_count(kvlist, PMD_BOND_MAC_ADDR_KVARG);