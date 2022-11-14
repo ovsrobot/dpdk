870ed0ab65853c7b98c5aeea1f2cf5e856c0efee
Rongwei Liu <rongweil@nvidia.com>
2022-11-14 10:47:55 +0200

    ethdev: add special flags when creating async transfer table
    In case flow rules match only one kind of traffic in a flow table,
    then optimization can be done via allocation of this table.
    Such optimization is possible only if the application gives a hint
    about its usage of the table during initial configuration.
    
    The transfer domain rules may process traffic from wire or vport,
    which may correspond to two kinds of underlayer resources.
    That's why the first two hints introduced in this patch are about
    wire and vport traffic specialization.
    Wire means traffic arrives from the uplink port while vport means
    traffic initiated from VF/SF.
    
    There are two possible approaches for providing the hints.
    Using IPv4 as an example:
    1. Use pattern item in both template table and flow rules.
    
       pattern_template: pattern ANY_VPORT / eth / ipv4 is 1.1.1.1 / end
       async flow create: pattern ANY_VPORT / eth / ipv4 is 1.1.1.2 / end
    
       "ANY_VPORT" needs to be present in each flow rule even if it's
       just a hint. No value to match because matching is already done by
       IPv4 item.
    
    2. Add special flags into table_attr.
    
       template_table 0 create table_id 0 group 1 transfer vport_orig
    
    Approach 1 needs to specify the pattern in each flow rule which wastes
    memory and is not user friendly.
    This patch takes the 2nd approach and introduces one new member
    "specialize" into rte_flow_table_attr to indicate possible flow table
    optimization.
    
    By default, there is no hint, so the behavior of the transfer domain
    doesn't change.
    There is no guarantee that the hint will be used by the PMD.
    
    Signed-off-by: Rongwei Liu <rongweil@nvidia.com>
    Acked-by: Ori Kam <orika@nvidia.com>
    
    v2: Move the new field to template table attribute.
    v4: Mark it as optional and clear the concept.
    v5: Change specialize type to uint32_t.
    v6: Change the flags to macros and re-construct the commit log.
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/ethdev/rte_flow.h
+++ lib/ethdev/rte_flow.h
@@ -5186,6 +5186,29 @@ rte_flow_actions_template_destroy(uint16_t port_id,
  */
 struct rte_flow_template_table;
 
+/**@{@name Special optional flags for template table attribute
+ * Each bit is a hint for table specialization,
+ * offering a potential optimization at PMD layer.
+ * PMD can ignore the unsupported bits silently.
+ */
+/**
+ * Specialize table for transfer flows which come only from wire.
+ * It allows PMD not to allocate resources for non-wire originated traffic.
+ * This bit is not a matching criteria, just an optimization hint.
+ * Flow rules which match non-wire originated traffic will be missed
+ * if the hint is supported.
+ */
+#define RTE_FLOW_TABLE_SPECIALIZE_TRANSFER_WIRE_ORIG RTE_BIT32(0)
+/**
+ * Specialize table for transfer flows which come only from vport (e.g. VF, SF).
+ * It allows PMD not to allocate resources for non-vport originated traffic.
+ * This bit is not a matching criteria, just an optimization hint.
+ * Flow rules which match non-vport originated traffic will be missed
+ * if the hint is supported.
+ */
+#define RTE_FLOW_TABLE_SPECIALIZE_TRANSFER_VPORT_ORIG RTE_BIT32(1)
+/**@}*/
+
 /**
  * @warning
  * @b EXPERIMENTAL: this API may change without prior notice.
@@ -5201,6 +5224,11 @@ struct rte_flow_template_table_attr {
 	 * Maximum number of flow rules that this table holds.
 	 */
 	uint32_t nb_flows;
+	/**
+	 * Optional hint flags for PMD optimization.
+	 * Value is composed with RTE_FLOW_TABLE_SPECIALIZE_*.
+	 */
+	uint32_t specialize;
 };
 
 /**
