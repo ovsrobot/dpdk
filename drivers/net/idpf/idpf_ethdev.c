ovsrobot/dpdk

commit e9ff6df15b9aa0149d29bbd4ccac30a29ec85502

net/idpf: stop before closing device
This patch stops device at the beginning of idpf_dev_close.

Fixes: 14aa6ed8f2ec ("net/idpf: support device start and stop")

Signed-off-by: Beilei Xing <beilei.xing@intel.com>
Acked-by: Qi Zhang <qi.z.zhang@intel.com>

drivers/net/idpf/idpf_ethdev.c
@@ -609,6 +609,9 @@ idpf_dev_stop(struct rte_eth_dev *dev)
 {
 	struct idpf_vport *vport = dev->data->dev_private;
 
+	if (vport->stopped == 1)
+		return 0;
+
 	idpf_vc_ena_dis_vport(vport, false);
 
 	idpf_stop_queues(dev);
@@ -616,6 +619,8 @@ idpf_dev_stop(struct rte_eth_dev *dev)
 	idpf_vc_config_irq_map_unmap(vport, false);
 
 	idpf_vc_dealloc_vectors(vport);
+
+	vport->stopped = 1;
 
 	return 0;
 }
@@ -625,6 +630,8 @@ idpf_dev_close(struct rte_eth_dev *dev)
 {
 	struct idpf_vport *vport = dev->data->dev_private;
 	struct idpf_adapter *adapter = vport->adapter;
+
+	idpf_dev_stop(dev);
 
 	idpf_vc_destroy_vport(vport);
 

drivers/net/idpf/idpf_ethdev.h
@@ -127,6 +127,8 @@ struct idpf_vport {
 	struct idpf_chunks_info chunks_info;
 
 	uint16_t devarg_id;
+
+	bool stopped;
 };
 
 struct idpf_adapter {

commit e62565ef28db79f32584a6b08a8f806a27185ac4

net/idpf: fix port start
Port can't start successfully if stopping port and starting port
again.
This patch fixes port start by initialization.

Fixes: e9ff6df15b9a ("net/idpf: stop before closing device")

Signed-off-by: Beilei Xing <beilei.xing@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/idpf/idpf_ethdev.c
@@ -552,6 +552,8 @@ idpf_dev_start(struct rte_eth_dev *dev)
 	uint16_t req_vecs_num;
 	int ret;
 
+	vport->stopped = 0;
+
 	if (dev->data->mtu > vport->max_mtu) {
 		PMD_DRV_LOG(ERR, "MTU should be less than %d", vport->max_mtu);
 		ret = -EINVAL;