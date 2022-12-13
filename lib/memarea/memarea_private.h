db4e3ba9af9ea37fd9b44cc441d750ffb138abc1
Chengwen Feng <fengchengwen@huawei.com>
2022-12-13 09:13:55 +0000

    memarea: support alloc/free/refcnt-update API
    This patch supports rte_memarea_alloc()/rte_memarea_free()/
    rte_memarea_refcnt_update() API.
    
    Signed-off-by: Chengwen Feng <fengchengwen@huawei.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/memarea/memarea_private.h
+++ lib/memarea/memarea_private.h
@@ -28,6 +28,9 @@ struct rte_memarea {
 	void                    *area_addr;
 	struct memarea_elem_list elem_list;
 	struct memarea_elem_list free_list;
+
+	uint64_t alloc_fails;
+	uint64_t refcnt_check_fails;
 } __rte_cache_aligned;
 
 #endif /* MEMAREA_PRIVATE_H */
