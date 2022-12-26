9ff6242cf5954b40d7c37e1acb8c8af6e8640b7a
Alvaro Karsz <alvaro.karsz@solid-run.com>
2022-12-26 09:47:09 +0200

    bus/fslmc: unlock spinlock in case of timeout
    Bug in function mc_send_command,
    A spinlock is not unlocked in case of timeouts.
    
    Fixes: 79711846f676 ("bus/fslmc: add timeout in MC send command")
    Signed-off-by: Alvaro Karsz <alvaro.karsz@solid-run.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/bus/fslmc/mc/mc_sys.c
+++ drivers/bus/fslmc/mc/mc_sys.c
@@ -77,8 +77,12 @@ int mc_send_command(struct fsl_mc_io *mc_io, struct mc_command *cmd)
 		total_time = rte_get_timer_cycles() - start_time;
 	} while (status == MC_CMD_STATUS_READY && total_time <= time_to_wait);
 
-	if (status == MC_CMD_STATUS_READY)
+	if (status == MC_CMD_STATUS_READY) {
+		/* --- Call unlock function here in case portal is shared --- */
+		rte_spinlock_unlock(&mc_portal_lock);
+
 		return mc_status_to_error(MC_CMD_STATUS_TIMEOUT);
+	}
 
 	/* Read the response back into the command buffer */
 	mc_read_response(mc_io->regs, cmd);
