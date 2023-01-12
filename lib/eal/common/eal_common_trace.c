648c4c60329ec0f852639363f06e0740174b62ac
Tyler Retzlaff <roretzla@linux.microsoft.com>
2023-01-12 13:55:34 -0800

    eal: remove thread getname API
    Remove the rte_thread_getname API.  The API is __rte_experimental and
    requires no deprecation notice.
    
    Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/common/eal_common_trace.c
+++ lib/eal/common/eal_common_trace.c
@@ -356,8 +356,6 @@ found:
 	/* Store the thread name */
 	char *name = header->stream_header.thread_name;
 	memset(name, 0, __RTE_TRACE_EMIT_STRING_LEN_MAX);
-	rte_thread_getname(pthread_self(), name,
-		__RTE_TRACE_EMIT_STRING_LEN_MAX);
 
 	trace->lcore_meta[count].mem = header;
 	trace->nb_trace_mem_list++;
