5783391bc07d6a0d3cd0acff01cbff7a61a152a0
Morten Brørup <mb@smartsharesystems.com>
2023-01-16 13:44:11 +0100

    net/bnx2x: fix warnings about rte_memcpy lengths
    Bugfix: The vlan in the bulletin does not contain a VLAN header, only the
    VLAN ID, so only copy 2 byte, not 4. The target structure has padding
    after the field, so copying 2 byte too many is effectively harmless.
    There is no need to backport this patch.
    
    Use RTE_PTR_ADD where copying arrays to the offset of a first field in a
    structure holding multiple fields, to avoid compiler warnings with
    decorated rte_memcpy.
    
    Bugzilla ID: 1146
    
    Fixes: 540a211084a7695a1c7bc43068934c140d6989be ("bnx2x: driver core")
    Cc: stephen@networkplumber.org
    Cc: rmody@marvell.com
    Cc: shshaikh@marvell.com
    
    Signed-off-by: Morten Brørup <mb@smartsharesystems.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/bnx2x/bnx2x_stats.c
+++ drivers/net/bnx2x/bnx2x_stats.c
@@ -819,8 +819,9 @@ bnx2x_hw_stats_update(struct bnx2x_softc *sc)
 
     rte_memcpy(old, new, sizeof(struct nig_stats));
 
-    rte_memcpy(&(estats->rx_stat_ifhcinbadoctets_hi), &(pstats->mac_stx[1]),
-	   sizeof(struct mac_stx));
+	rte_memcpy(RTE_PTR_ADD(estats,
+			offsetof(struct bnx2x_eth_stats, rx_stat_ifhcinbadoctets_hi)),
+			&pstats->mac_stx[1], sizeof(struct mac_stx));
     estats->brb_drop_hi = pstats->brb_drop_hi;
     estats->brb_drop_lo = pstats->brb_drop_lo;
 
@@ -1492,9 +1493,11 @@ bnx2x_stats_init(struct bnx2x_softc *sc)
 		REG_RD(sc, NIG_REG_STAT0_BRB_TRUNCATE + port*0x38);
 	if (!CHIP_IS_E3(sc)) {
 		REG_RD_DMAE(sc, NIG_REG_STAT0_EGRESS_MAC_PKT0 + port*0x50,
-				&(sc->port.old_nig_stats.egress_mac_pkt0_lo), 2);
+				RTE_PTR_ADD(&sc->port.old_nig_stats,
+				offsetof(struct nig_stats, egress_mac_pkt0_lo)), 2);
 		REG_RD_DMAE(sc, NIG_REG_STAT0_EGRESS_MAC_PKT1 + port*0x50,
-				&(sc->port.old_nig_stats.egress_mac_pkt1_lo), 2);
+				RTE_PTR_ADD(&sc->port.old_nig_stats,
+				offsetof(struct nig_stats, egress_mac_pkt1_lo)), 2);
 	}
 
 	/* function stats */
