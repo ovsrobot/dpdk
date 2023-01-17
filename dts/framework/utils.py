b8868630b658729f55d482f01f66a19a7d075163
Juraj Linkeš <juraj.linkes@pantheon.tech>
2023-01-17 15:49:00 +0000

    dts: add dpdk execution handling
    Add methods for setting up and shutting down DPDK apps and for
    constructing EAL parameters.
    
    Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- dts/framework/utils.py
+++ dts/framework/utils.py
@@ -32,6 +32,26 @@ def skip_setup(func) -> Callable[..., None]:
         return func
 
 
+def expand_range(range_str: str) -> list[int]:
+    """
+    Process range string into a list of integers. There are two possible formats:
+    n - a single integer
+    n-m - a range of integers
+
+    The returned range includes both n and m. Empty string returns an empty list.
+    """
+    expanded_range: list[int] = []
+    if range_str:
+        range_boundaries = range_str.split("-")
+        # will throw an exception when items in range_boundaries can't be converted,
+        # serving as type check
+        expanded_range.extend(
+            range(int(range_boundaries[0]), int(range_boundaries[-1]) + 1)
+        )
+
+    return expanded_range
+
+
 def GREEN(text: str) -> str:
     return f"\u001B[32;1m{str(text)}\u001B[0m"
 
