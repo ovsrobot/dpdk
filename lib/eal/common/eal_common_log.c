89a91d5a049843dcfc2a6dbbdc9160e7103fcf90
Stephen Hemminger <stephen@networkplumber.org>
2023-02-09 17:07:11 -0800

    efd: replace RTE_LOGTYPE_EFD with local type
    Replace all uses of global logtype with a local log type.
    Do not break message formats across source lines.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/common/eal_common_log.c
+++ lib/eal/common/eal_common_log.c
@@ -365,7 +365,6 @@ static const struct logtype logtype_strings[] = {
 	{RTE_LOGTYPE_PIPELINE,   "lib.pipeline"},
 	{RTE_LOGTYPE_MBUF,       "lib.mbuf"},
 	{RTE_LOGTYPE_CRYPTODEV,  "lib.cryptodev"},
-	{RTE_LOGTYPE_EFD,        "lib.efd"},
 	{RTE_LOGTYPE_EVENTDEV,   "lib.eventdev"},
 
 	{RTE_LOGTYPE_USER1,      "user1"},
