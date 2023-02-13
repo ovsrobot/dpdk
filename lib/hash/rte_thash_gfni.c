5c780a3fbdbfce4c8aecb40eed3ffe3f625a6606
Stephen Hemminger <stephen@networkplumber.org>
2023-02-13 11:55:51 -0800

    hash: convert RTE_LOGTYPE_HASH to dynamic type
    Use dynamic type for hash and add subtypes for crc and gfni.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/hash/rte_thash_gfni.c
+++ lib/hash/rte_thash_gfni.c
@@ -8,6 +8,9 @@
 #include <rte_log.h>
 #include <rte_thash_gfni.h>
 
+RTE_LOG_REGISTER_SUFFIX(hash_gfni_logtype, gfni, INFO);
+#define RTE_LOGTYPE_HASH hash_gfni_logtype
+
 uint32_t
 rte_thash_gfni(const uint64_t *mtrx __rte_unused,
 	       const uint8_t *key __rte_unused, int len __rte_unused)
