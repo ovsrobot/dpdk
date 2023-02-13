d576ab886ca8b4265444e486d0be3c1e3910f011
Juraj Linkeš <juraj.linkes@pantheon.tech>
2023-02-13 16:28:39 +0100

    dts: add dpdk build on sut
    Add the ability to build DPDK and apps on the SUT, using a configured
    target.
    
    Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- dts/framework/utils.py
+++ dts/framework/utils.py
@@ -1,9 +1,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright(c) 2010-2014 Intel Corporation
-# Copyright(c) 2022 PANTHEON.tech s.r.o.
-# Copyright(c) 2022 University of New Hampshire
+# Copyright(c) 2022-2023 PANTHEON.tech s.r.o.
+# Copyright(c) 2022-2023 University of New Hampshire
 
 import sys
+from typing import Callable
+
+from .settings import SETTINGS
 
 
 def check_dts_python_version() -> None:
@@ -22,9 +25,21 @@ def check_dts_python_version() -> None:
         print(RED("Please use Python >= 3.10 instead"), file=sys.stderr)
 
 
+def skip_setup(func) -> Callable[..., None]:
+    if SETTINGS.skip_setup:
+        return lambda *args: None
+    else:
+        return func
+
+
 def GREEN(text: str) -> str:
     return f"\u001B[32;1m{str(text)}\u001B[0m"
 
 
 def RED(text: str) -> str:
     return f"\u001B[31;1m{str(text)}\u001B[0m"
+
+
+class EnvVarsDict(dict):
+    def __str__(self) -> str:
+        return " ".join(["=".join(item) for item in self.items()])
