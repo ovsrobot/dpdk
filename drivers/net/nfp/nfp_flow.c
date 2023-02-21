ovsrobot/dpdk

commit ac12e126c482af21230de393d03f1e7a68624d34

net/nfp: support TTL flow action
Add the corresponding data structure and logics, to support
the offload of set TTL action.

Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@corigine.com>

drivers/net/nfp/nfp_flow.c
@@ -561,9 +561,11 @@ nfp_flow_key_layers_calculate_actions(const struct rte_flow_action actions[],
 		struct nfp_fl_key_ls *key_ls)
 {
 	int ret = 0;
+	bool tc_hl_flag = false;
 	bool mac_set_flag = false;
 	bool ip_set_flag = false;
 	bool tp_set_flag = false;
+	bool ttl_tos_flag = false;
 	const struct rte_flow_action *action;
 
 	for (action = actions; action->type != RTE_FLOW_ACTION_TYPE_END; ++action) {
@@ -652,6 +654,22 @@ nfp_flow_key_layers_calculate_actions(const struct rte_flow_action actions[],
 			if (!tp_set_flag) {
 				key_ls->act_size += sizeof(struct nfp_fl_act_set_tport);
 				tp_set_flag = true;
+			}
+			break;
+		case RTE_FLOW_ACTION_TYPE_SET_TTL:
+			PMD_DRV_LOG(DEBUG, "RTE_FLOW_ACTION_TYPE_SET_TTL detected");
+			if (key_ls->key_layer & NFP_FLOWER_LAYER_IPV4) {
+				if (!ttl_tos_flag) {
+					key_ls->act_size +=
+						sizeof(struct nfp_fl_act_set_ip4_ttl_tos);
+					ttl_tos_flag = true;
+				}
+			} else {
+				if (!tc_hl_flag) {
+					key_ls->act_size +=
+						sizeof(struct nfp_fl_act_set_ipv6_tc_hl_fl);
+					tc_hl_flag = true;
+				}
 			}
 			break;
 		default:
@@ -1431,6 +1449,52 @@ nfp_flow_action_push_vlan(char *act_data,
 	return 0;
 }
 
+static void
+nfp_flow_action_set_ttl(char *act_data,
+		const struct rte_flow_action *action,
+		bool ttl_tos_flag)
+{
+	size_t act_size;
+	struct nfp_fl_act_set_ip4_ttl_tos *ttl_tos;
+	const struct rte_flow_action_set_ttl *ttl_conf;
+
+	if (ttl_tos_flag)
+		ttl_tos = (struct nfp_fl_act_set_ip4_ttl_tos *)act_data - 1;
+	else
+		ttl_tos = (struct nfp_fl_act_set_ip4_ttl_tos *)act_data;
+
+	act_size = sizeof(struct nfp_fl_act_set_ip4_ttl_tos);
+	ttl_tos->head.jump_id = NFP_FL_ACTION_OPCODE_SET_IPV4_TTL_TOS;
+	ttl_tos->head.len_lw = act_size >> NFP_FL_LW_SIZ;
+
+	ttl_conf = (const struct rte_flow_action_set_ttl *)action->conf;
+	ttl_tos->ipv4_ttl = ttl_conf->ttl_value;
+	ttl_tos->reserved = 0;
+}
+
+static void
+nfp_flow_action_set_hl(char *act_data,
+		const struct rte_flow_action *action,
+		bool tc_hl_flag)
+{
+	size_t act_size;
+	struct nfp_fl_act_set_ipv6_tc_hl_fl *tc_hl;
+	const struct rte_flow_action_set_ttl *ttl_conf;
+
+	if (tc_hl_flag)
+		tc_hl = (struct nfp_fl_act_set_ipv6_tc_hl_fl *)act_data - 1;
+	else
+		tc_hl = (struct nfp_fl_act_set_ipv6_tc_hl_fl *)act_data;
+
+	act_size = sizeof(struct nfp_fl_act_set_ipv6_tc_hl_fl);
+	tc_hl->head.jump_id = NFP_FL_ACTION_OPCODE_SET_IPV6_TC_HL_FL;
+	tc_hl->head.len_lw = act_size >> NFP_FL_LW_SIZ;
+
+	ttl_conf = (const struct rte_flow_action_set_ttl *)action->conf;
+	tc_hl->ipv6_hop_limit = ttl_conf->ttl_value;
+	tc_hl->reserved = 0;
+}
+
 static int
 nfp_flow_compile_action(__rte_unused struct nfp_flower_representor *representor,
 		const struct rte_flow_action actions[],
@@ -1439,17 +1503,21 @@ nfp_flow_compile_action(__rte_unused struct nfp_flower_representor *representor,
 	int ret = 0;
 	char *position;
 	char *action_data;
+	bool ttl_tos_flag = false;
+	bool tc_hl_flag = false;
 	bool drop_flag = false;
 	bool ip_set_flag = false;
 	bool tp_set_flag = false;
 	bool mac_set_flag = false;
 	uint32_t total_actions = 0;
 	const struct rte_flow_action *action;
+	struct nfp_flower_meta_tci *meta_tci;
 	struct nfp_fl_rule_metadata *nfp_flow_meta;
 
 	nfp_flow_meta = nfp_flow->payload.meta;
 	action_data   = nfp_flow->payload.action_data;
 	position      = action_data;
+	meta_tci = (struct nfp_flower_meta_tci *)nfp_flow->payload.unmasked_data;
 
 	for (action = actions; action->type != RTE_FLOW_ACTION_TYPE_END; ++action) {
 		switch (action->type) {
@@ -1551,6 +1619,22 @@ nfp_flow_compile_action(__rte_unused struct nfp_flower_representor *representor,
 			if (!tp_set_flag) {
 				position += sizeof(struct nfp_fl_act_set_tport);
 				tp_set_flag = true;
+			}
+			break;
+		case RTE_FLOW_ACTION_TYPE_SET_TTL:
+			PMD_DRV_LOG(DEBUG, "Process RTE_FLOW_ACTION_TYPE_SET_TTL");
+			if (meta_tci->nfp_flow_key_layer & NFP_FLOWER_LAYER_IPV4) {
+				nfp_flow_action_set_ttl(position, action, ttl_tos_flag);
+				if (!ttl_tos_flag) {
+					position += sizeof(struct nfp_fl_act_set_ip4_ttl_tos);
+					ttl_tos_flag = true;
+				}
+			} else {
+				nfp_flow_action_set_hl(position, action, ttl_tos_flag);
+				if (!tc_hl_flag) {
+					position += sizeof(struct nfp_fl_act_set_ipv6_tc_hl_fl);
+					tc_hl_flag = true;
+				}
 			}
 			break;
 		default:

doc/guides/nics/features/nfp.ini
@@ -52,3 +52,4 @@ set_mac_dst          = Y
 set_mac_src          = Y
 set_tp_dst           = Y
 set_tp_src           = Y
+set_ttl              = Y

drivers/net/nfp/flower/nfp_flower_cmsg.h
@@ -400,6 +400,25 @@ struct nfp_fl_act_set_ip4_addrs {
 };
 
 /*
+ * IPv4 ttl tos
+ *    3                   2                   1
+ *  1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |   -   |opcode |       |jump_id|    ttl_mask   |   tos_mask    |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |       ttl     |      tos      |               0               |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ */
+struct nfp_fl_act_set_ip4_ttl_tos {
+	struct nfp_fl_act_head head;
+	uint8_t ipv4_ttl_mask;
+	uint8_t ipv4_tos_mask;
+	uint8_t ipv4_ttl;
+	uint8_t ipv4_tos;
+	rte_be16_t reserved;
+};
+
+/*
  * IPv6 addr
  *    3                   2                   1
  *  1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0
@@ -430,6 +449,31 @@ struct nfp_fl_act_set_ipv6_addr {
 		rte_be32_t mask;
 		rte_be32_t exact;
 	} ipv6[4];
+};
+
+/*
+ * ipv6 tc hl fl
+ *    3                   2                   1
+ *  1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0 9 8 7 6 5 4 3 2 1 0
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |   -   |opcode |       |jump_id|  tclass_mask  |  hlimit_mask  |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |               0               |  tclass       |  hlimit       |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |           0           |             flabel_mask               |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ * |           0           |             flabel                    |
+ * +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+ */
+struct nfp_fl_act_set_ipv6_tc_hl_fl {
+	struct nfp_fl_act_head head;
+	uint8_t ipv6_tc_mask;
+	uint8_t ipv6_hop_limit_mask;
+	rte_be16_t reserved;
+	uint8_t ipv6_tc;
+	uint8_t ipv6_hop_limit;
+	rte_be32_t ipv6_label_mask;
+	rte_be32_t ipv6_label;
 };
 
 /*

commit fc5bc7f1994442fb5beb98d4a07bec108411bc5f

net/nfp: fix the set TTL flow action
The former logic of set TTL flow action don't consider the mask
filed of control message passed to the firmware.
This caused the firmware skip the set action logic, and the offloaded
packets don't have the right TTL as expected.

Fixes: ac12e126c482 ("net/nfp: support TTL flow action")
Cc: stable@dpdk.org

Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@corigine.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/nfp/nfp_flow.c
@@ -2252,6 +2252,7 @@ nfp_flow_action_set_ttl(char *act_data,
 
 	ttl_conf = (const struct rte_flow_action_set_ttl *)action->conf;
 	ttl_tos->ipv4_ttl = ttl_conf->ttl_value;
+	ttl_tos->ipv4_ttl_mask = 0xff;
 	ttl_tos->reserved = 0;
 }
 
@@ -2275,6 +2276,7 @@ nfp_flow_action_set_hl(char *act_data,
 
 	ttl_conf = (const struct rte_flow_action_set_ttl *)action->conf;
 	tc_hl->ipv6_hop_limit = ttl_conf->ttl_value;
+	tc_hl->ipv6_hop_limit_mask = 0xff;
 	tc_hl->reserved = 0;
 }
 