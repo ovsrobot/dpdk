e6bfd0902252b8c7f68e2c1d2e21031c311f46d2
Harman Kalra <hkalra@marvell.com>
2023-03-03 13:40:10 +0530

    common/cnxk: enable one to one SQ QINT mapping
    Enabling one to one mapping between SQ to 64 QINTs per LF. So in case of
    SQ interrupt event NIX can deliver associated QINT MSI-X interrupt to
    software and increment respective QINT count CSR.
    
    While for some cn10k chip models keeping the workaround (i.e. all SQs
    assigned to same QINT index), for errata where NIX may use an incorrect
    QINT_IDX for SQ interrupts.
    
    Signed-off-by: Harman Kalra <hkalra@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/common/cnxk/roc_errata.h
+++ drivers/common/cnxk/roc_errata.h
@@ -98,4 +98,12 @@ roc_errata_nix_sdp_send_has_mtu_size_16k(void)
 		roc_model_is_cn96_a0() || roc_model_is_cn96_b0());
 }
 
+/* Errata IPBUNIXTX-39300 */
+static inline bool
+roc_errata_nix_assign_incorrect_qint(void)
+{
+	return (roc_model_is_cn10ka_a0() || roc_model_is_cnf10ka_a0() ||
+		roc_model_is_cnf10kb_a0() || roc_model_is_cn10ka_a1());
+}
+
 #endif /* _ROC_ERRATA_H_ */
