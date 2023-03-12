b99635626a87d1207e168ee4ba8ef1302b7b4602
J.J. Martzki <mars14850@gmail.com>
2023-03-12 14:20:20 +0800

    lib/bpf: Rename bpf function names to avoid potential conflict with libpcap
    The library libpcap has their function 'bpf_validate' either so there would
    be a multiple definition issue when linking with librte_bpf.a and libpcap.a
    statically (Same as http://dpdk.org/patch/52631). So just rename the
    function names to avoid such issue.
    
    Signed-off-by: J.J. Martzki <mars14850@gmail.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/bpf/bpf_impl.h
+++ lib/bpf/bpf_impl.h
@@ -17,12 +17,10 @@ struct rte_bpf {
 	uint32_t stack_sz;
 };
 
-extern int bpf_validate(struct rte_bpf *bpf);
-
-extern int bpf_jit(struct rte_bpf *bpf);
-
-extern int bpf_jit_x86(struct rte_bpf *);
-extern int bpf_jit_arm64(struct rte_bpf *);
+extern int rte_bpf_validate(struct rte_bpf *bpf);
+extern int rte_bpf_jit(struct rte_bpf *bpf);
+extern int rte_bpf_jit_x86(struct rte_bpf *bpf);
+extern int rte_bpf_jit_arm64(struct rte_bpf *bpf);
 
 extern int rte_bpf_logtype;
 
