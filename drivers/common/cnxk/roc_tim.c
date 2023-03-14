ovsrobot/dpdk

commit 44a9307c0908ae28989f5ab9b90ddcbc5484592f

common/cnxk: support mailbox locking
Support mailbox locking using spinlock to have different subsystems
sharing same mailbox object.

Signed-off-by: Rakesh Kudurumalla <rkudurumalla@marvell.com>

drivers/common/cnxk/roc_tim.c
@@ -12,17 +12,23 @@ tim_fill_msix(struct roc_tim *roc_tim, uint16_t nb_ring)
 	struct tim *tim = roc_tim_to_tim_priv(roc_tim);
 	struct dev *dev = &sso->dev;
 	struct msix_offset_rsp *rsp;
+	struct mbox *mbox = mbox_get(dev->mbox);
 	int i, rc;
 
-	mbox_alloc_msg_msix_offset(dev->mbox);
-	rc = mbox_process_msg(dev->mbox, (void **)&rsp);
-	if (rc)
-		return -EIO;
+	mbox_alloc_msg_msix_offset(mbox);
+	rc = mbox_process_msg(mbox, (void **)&rsp);
+	if (rc) {
+		rc = -EIO;
+		goto exit;
+	}
 
 	for (i = 0; i < nb_ring; i++)
 		tim->tim_msix_offsets[i] = rsp->timlf_msixoff[i];
 
-	return 0;
+	rc = 0;
+exit:
+	mbox_put(mbox);
+	return rc;
 }
 
 static void
@@ -91,12 +97,12 @@ roc_tim_lf_enable(struct roc_tim *roc_tim, uint8_t ring_id, uint64_t *start_tsc,
 {
 	struct sso *sso = roc_sso_to_sso_priv(roc_tim->roc_sso);
 	struct dev *dev = &sso->dev;
+	struct mbox *mbox = mbox_get(dev->mbox);
 	struct tim_enable_rsp *rsp;
 	struct tim_ring_req *req;
 	int rc = -ENOSPC;
 
-	plt_spinlock_lock(&sso->mbox_lock);
-	req = mbox_alloc_msg_tim_enable_ring(dev->mbox);
+	req = mbox_alloc_msg_tim_enable_ring(mbox);
 	if (req == NULL)
 		goto fail;
 	req->ring = ring_id;
@@ -114,7 +120,7 @@ roc_tim_lf_enable(struct roc_tim *roc_tim, uint8_t ring_id, uint64_t *start_tsc,
 		*start_tsc = rsp->timestarted;
 
 fail:
-	plt_spinlock_unlock(&sso->mbox_lock);
+	mbox_put(mbox);
 	return rc;
 }
 
@@ -123,11 +129,11 @@ roc_tim_lf_disable(struct roc_tim *roc_tim, uint8_t ring_id)
 {
 	struct sso *sso = roc_sso_to_sso_priv(roc_tim->roc_sso);
 	struct dev *dev = &sso->dev;
+	struct mbox *mbox = mbox_get(dev->mbox);
 	struct tim_ring_req *req;
 	int rc = -ENOSPC;
 
-	plt_spinlock_lock(&sso->mbox_lock);
-	req = mbox_alloc_msg_tim_disable_ring(dev->mbox);
+	req = mbox_alloc_msg_tim_disable_ring(mbox);
 	if (req == NULL)
 		goto fail;
 	req->ring = ring_id;
@@ -139,7 +145,7 @@ roc_tim_lf_disable(struct roc_tim *roc_tim, uint8_t ring_id)
 	}
 
 fail:
-	plt_spinlock_unlock(&sso->mbox_lock);
+	mbox_put(mbox);
 	return rc;
 }
 
@@ -159,11 +165,11 @@ roc_tim_lf_config(struct roc_tim *roc_tim, uint8_t ring_id,
 {
 	struct sso *sso = roc_sso_to_sso_priv(roc_tim->roc_sso);
 	struct dev *dev = &sso->dev;
+	struct mbox *mbox = mbox_get(dev->mbox);
 	struct tim_config_req *req;
 	int rc = -ENOSPC;
 
-	plt_spinlock_lock(&sso->mbox_lock);
-	req = mbox_alloc_msg_tim_config_ring(dev->mbox);
+	req = mbox_alloc_msg_tim_config_ring(mbox);
 	if (req == NULL)
 		goto fail;
 	req->ring = ring_id;
@@ -178,14 +184,14 @@ roc_tim_lf_config(struct roc_tim *roc_tim, uint8_t ring_id,
 	req->clockfreq = clockfreq;
 	req->gpioedge = TIM_GPIO_LTOH_TRANS;
 
-	rc = mbox_process(dev->mbox);
+	rc = mbox_process(mbox);
 	if (rc) {
 		tim_err_desc(rc);
 		rc = -EIO;
 	}
 
 fail:
-	plt_spinlock_unlock(&sso->mbox_lock);
+	mbox_put(mbox);
 	return rc;
 }
 
@@ -196,12 +202,12 @@ roc_tim_lf_interval(struct roc_tim *roc_tim, enum roc_tim_clk_src clk_src,
 {
 	struct sso *sso = roc_sso_to_sso_priv(roc_tim->roc_sso);
 	struct dev *dev = &sso->dev;
+	struct mbox *mbox = mbox_get(dev->mbox);
 	struct tim_intvl_req *req;
 	struct tim_intvl_rsp *rsp;
 	int rc = -ENOSPC;
 
-	plt_spinlock_lock(&sso->mbox_lock);
-	req = mbox_alloc_msg_tim_get_min_intvl(dev->mbox);
+	req = mbox_alloc_msg_tim_get_min_intvl(mbox);
 	if (req == NULL)
 		goto fail;
 
@@ -218,7 +224,7 @@ roc_tim_lf_interval(struct roc_tim *roc_tim, enum roc_tim_clk_src clk_src,
 	*interval = rsp->intvl_cyc;
 
 fail:
-	plt_spinlock_unlock(&sso->mbox_lock);
+	mbox_put(mbox);
 	return rc;
 }
 
@@ -231,17 +237,17 @@ roc_tim_lf_alloc(struct roc_tim *roc_tim, uint8_t ring_id, uint64_t *clk)
 	struct tim_lf_alloc_req *req;
 	struct tim_lf_alloc_rsp *rsp;
 	struct dev *dev = &sso->dev;
+	struct mbox *mbox = mbox_get(dev->mbox);
 	int rc = -ENOSPC;
 
-	plt_spinlock_lock(&sso->mbox_lock);
-	req = mbox_alloc_msg_tim_lf_alloc(dev->mbox);
+	req = mbox_alloc_msg_tim_lf_alloc(mbox);
 	if (req == NULL)
 		goto fail;
 	req->npa_pf_func = idev_npa_pffunc_get();
 	req->sso_pf_func = idev_sso_pffunc_get();
 	req->ring = ring_id;
 
-	rc = mbox_process_msg(dev->mbox, (void **)&rsp);
+	rc = mbox_process_msg(mbox, (void **)&rsp);
 	if (rc) {
 		tim_err_desc(rc);
 		rc = -EIO;
@@ -255,19 +261,19 @@ roc_tim_lf_alloc(struct roc_tim *roc_tim, uint8_t ring_id, uint64_t *clk)
 				   tim->tim_msix_offsets[ring_id]);
 	if (rc < 0) {
 		plt_tim_dbg("Failed to register Ring[%d] IRQ", ring_id);
-		free_req = mbox_alloc_msg_tim_lf_free(dev->mbox);
+		free_req = mbox_alloc_msg_tim_lf_free(mbox);
 		if (free_req == NULL) {
 			rc = -ENOSPC;
 			goto fail;
 		}
 		free_req->ring = ring_id;
-		rc = mbox_process(dev->mbox);
+		rc = mbox_process(mbox);
 		if (rc)
 			rc = -EIO;
 	}
 
 fail:
-	plt_spinlock_unlock(&sso->mbox_lock);
+	mbox_put(dev->mbox);
 	return rc;
 }
 
@@ -283,8 +289,7 @@ roc_tim_lf_free(struct roc_tim *roc_tim, uint8_t ring_id)
 	tim_unregister_irq_priv(roc_tim, sso->pci_dev->intr_handle, ring_id,
 				tim->tim_msix_offsets[ring_id]);
 
-	plt_spinlock_lock(&sso->mbox_lock);
-	req = mbox_alloc_msg_tim_lf_free(dev->mbox);
+	req = mbox_alloc_msg_tim_lf_free(mbox_get(dev->mbox));
 	if (req == NULL)
 		goto fail;
 	req->ring = ring_id;
@@ -293,27 +298,32 @@ roc_tim_lf_free(struct roc_tim *roc_tim, uint8_t ring_id)
 	if (rc < 0) {
 		tim_err_desc(rc);
 		rc = -EIO;
+		goto fail;
 	}
+	rc = 0;
 
 fail:
-	plt_spinlock_unlock(&sso->mbox_lock);
-	return 0;
+	mbox_put(dev->mbox);
+	return rc;
 }
 
 int
 tim_free_lf_count_get(struct dev *dev, uint16_t *nb_lfs)
 {
+	struct mbox *mbox = mbox_get(dev->mbox);
 	struct free_rsrcs_rsp *rsrc_cnt;
 	int rc;
 
-	mbox_alloc_msg_free_rsrc_cnt(dev->mbox);
-	rc = mbox_process_msg(dev->mbox, (void **)&rsrc_cnt);
+	mbox_alloc_msg_free_rsrc_cnt(mbox);
+	rc = mbox_process_msg(mbox, (void **)&rsrc_cnt);
 	if (rc) {
 		plt_err("Failed to get free resource count\n");
+		mbox_put(mbox);
 		return -EIO;
 	}
 
 	*nb_lfs = rsrc_cnt->tim;
+	mbox_put(mbox);
 
 	return 0;
 }
@@ -336,21 +346,21 @@ roc_tim_init(struct roc_tim *roc_tim)
 	dev->roc_tim = roc_tim;
 	PLT_STATIC_ASSERT(sizeof(struct tim) <= TIM_MEM_SZ);
 	nb_lfs = roc_tim->nb_lfs;
-	plt_spinlock_lock(&sso->mbox_lock);
 
 	rc = tim_free_lf_count_get(dev, &nb_free_lfs);
 	if (rc) {
 		plt_tim_dbg("Failed to get TIM resource count");
 		nb_lfs = 0;
-		goto fail;
+		return nb_lfs;
 	}
 
 	if (nb_lfs && (nb_free_lfs < nb_lfs)) {
 		plt_tim_dbg("Requested LFs : %d Available LFs : %d", nb_lfs, nb_free_lfs);
 		nb_lfs = 0;
-		goto fail;
+		return nb_lfs;
 	}
 
+	mbox_get(dev->mbox);
 	attach_req = mbox_alloc_msg_attach_resources(dev->mbox);
 	if (attach_req == NULL) {
 		nb_lfs = 0;
@@ -367,11 +377,12 @@ roc_tim_init(struct roc_tim *roc_tim)
 		goto fail;
 	}
 
+	mbox_put(dev->mbox);
 	rc = tim_fill_msix(roc_tim, nb_lfs);
 	if (rc < 0) {
 		plt_err("Unable to get TIM MSIX vectors");
 
-		detach_req = mbox_alloc_msg_detach_resources(dev->mbox);
+		detach_req = mbox_alloc_msg_detach_resources(mbox_get(dev->mbox));
 		if (detach_req == NULL) {
 			nb_lfs = 0;
 			goto fail;
@@ -380,11 +391,14 @@ roc_tim_init(struct roc_tim *roc_tim)
 		detach_req->timlfs = true;
 		mbox_process(dev->mbox);
 		nb_lfs = 0;
+	} else {
+		goto done;
 	}
 	roc_tim->nb_lfs = nb_lfs;
 
 fail:
-	plt_spinlock_unlock(&sso->mbox_lock);
+	mbox_put(dev->mbox);
+done:
 	return nb_lfs;
 }
 
@@ -394,13 +408,13 @@ roc_tim_fini(struct roc_tim *roc_tim)
 	struct sso *sso = roc_sso_to_sso_priv(roc_tim->roc_sso);
 	struct rsrc_detach_req *detach_req;
 	struct dev *dev = &sso->dev;
+	struct mbox *mbox = mbox_get(dev->mbox);
 
-	plt_spinlock_lock(&sso->mbox_lock);
-	detach_req = mbox_alloc_msg_detach_resources(dev->mbox);
+	detach_req = mbox_alloc_msg_detach_resources(mbox);
 	PLT_ASSERT(detach_req);
 	detach_req->partial = true;
 	detach_req->timlfs = true;
 
-	mbox_process(dev->mbox);
-	plt_spinlock_unlock(&sso->mbox_lock);
+	mbox_process(mbox);
+	mbox_put(mbox);
 }

drivers/common/cnxk/cnxk_telemetry_npa.c
@@ -62,10 +62,11 @@ cnxk_tel_npa_aura(int aura_id, struct plt_tel_data *d)
 	if (plt_bitmap_get(lf->npa_bmp, aura_id))
 		return -1;
 
-	req = mbox_alloc_msg_npa_aq_enq(lf->mbox);
+	req = mbox_alloc_msg_npa_aq_enq(mbox_get(lf->mbox));
 	if (!req) {
 		plt_err("Failed to alloc aq enq for npa");
-		return -1;
+		rc = -1;
+		goto exit;
 	}
 
 	req->aura_id = aura_id;
@@ -75,7 +76,7 @@ cnxk_tel_npa_aura(int aura_id, struct plt_tel_data *d)
 	rc = mbox_process_msg(lf->mbox, (void *)&rsp);
 	if (rc) {
 		plt_err("Failed to get pool(%d) context", aura_id);
-		return rc;
+		goto exit;
 	}
 
 	aura = &rsp->aura;
@@ -110,7 +111,10 @@ cnxk_tel_npa_aura(int aura_id, struct plt_tel_data *d)
 	CNXK_TEL_DICT_INT(d, aura, err_qint_idx, w5_);
 	CNXK_TEL_DICT_U64(d, aura, thresh, w6_);
 
-	return 0;
+	rc = 0;
+exit:
+	mbox_put(lf->mbox);
+	return rc;
 }
 
 static int
@@ -129,10 +133,11 @@ cnxk_tel_npa_pool(int pool_id, struct plt_tel_data *d)
 	if (plt_bitmap_get(lf->npa_bmp, pool_id))
 		return -1;
 
-	req = mbox_alloc_msg_npa_aq_enq(lf->mbox);
+	req = mbox_alloc_msg_npa_aq_enq(mbox_get(lf->mbox));
 	if (!req) {
 		plt_err("Failed to alloc aq enq for npa");
-		return -1;
+		rc = -1;
+		goto exit;
 	}
 
 	req->aura_id = pool_id;
@@ -142,7 +147,7 @@ cnxk_tel_npa_pool(int pool_id, struct plt_tel_data *d)
 	rc = mbox_process_msg(lf->mbox, (void *)&rsp);
 	if (rc) {
 		plt_err("Failed to get pool(%d) context", pool_id);
-		return rc;
+		goto exit;
 	}
 
 	pool = &rsp->pool;
@@ -176,7 +181,10 @@ cnxk_tel_npa_pool(int pool_id, struct plt_tel_data *d)
 	CNXK_TEL_DICT_INT(d, pool, thresh_qint_idx, w8_);
 	CNXK_TEL_DICT_INT(d, pool, err_qint_idx, w8_);
 
-	return 0;
+	rc = 0;
+exit:
+	mbox_put(lf->mbox);
+	return rc;
 }
 
 static int

drivers/common/cnxk/roc_cpt.c
@@ -235,11 +235,14 @@ cpt_lf_outb_cfg(struct dev *dev, uint16_t sso_pf_func, uint16_t nix_pf_func,
 		uint8_t lf_id, bool ena)
 {
 	struct cpt_inline_ipsec_cfg_msg *req;
-	struct mbox *mbox = dev->mbox;
+	struct mbox *mbox = mbox_get(dev->mbox);
+	int rc;
 
 	req = mbox_alloc_msg_cpt_inline_ipsec_cfg(mbox);
-	if (req == NULL)
-		return -ENOSPC;
+	if (req == NULL) {
+		rc = -ENOSPC;
+		goto exit;
+	}
 
 	req->dir = CPT_INLINE_OUTBOUND;
 	req->slot = lf_id;
@@ -251,7 +254,10 @@ cpt_lf_outb_cfg(struct dev *dev, uint16_t sso_pf_func, uint16_t nix_pf_func,
 		req->enable = 0;
 	}
 
-	return mbox_process(mbox);
+	rc = mbox_process(mbox);
+exit:
+	mbox_put(mbox);
+	return rc;
 }
 
 int
@@ -276,13 +282,20 @@ roc_cpt_inline_ipsec_inb_cfg_read(struct roc_cpt *roc_cpt,
 {
 	struct cpt *cpt = roc_cpt_to_cpt_priv(roc_cpt);
 	struct dev *dev = &cpt->dev;
+	struct mbox *mbox = mbox_get(dev->mbox);
 	struct msg_req *req;
+	int rc;
 
-	req = mbox_alloc_msg_nix_read_inline_ipsec_cfg(dev->mbox);
-	if (req == NULL)
-		return -EIO;
+	req = mbox_alloc_msg_nix_read_inline_ipsec_cfg(mbox);
+	if (req == NULL) {
+		rc = -EIO;
+		goto exit;
+	}
 
-	return mbox_process_msg(dev->mbox, (void *)&inb_cfg);
+	rc = mbox_process_msg(mbox, (void *)&inb_cfg);
+exit:
+	mbox_put(mbox);
+	return rc;
 }
 
 int
@@ -292,19 +305,25 @@ roc_cpt_inline_ipsec_inb_cfg(struct roc_cpt *roc_cpt, uint16_t param1,
 	struct cpt *cpt = roc_cpt_to_cpt_priv(roc_cpt);
 	struct cpt_rx_inline_lf_cfg_msg *req;
 	struct mbox *mbox;
+	int rc;
 
-	mbox = cpt->dev.mbox;
+	mbox = mbox_get(cpt->dev.mbox);
 
 	req = mbox_alloc_msg_cpt_rx_inline_lf_cfg(mbox);
-	if (req == NULL)
-		return -ENOSPC;
+	if (req == NULL) {
+		rc = -ENOSPC;
+		goto exit;
+	}
 
 	req->sso_pf_func = idev_sso_pffunc_get();
 	req->param1 = param1;
 	req->param2 = param2;
 	req->opcode = opcode;
 
-	return mbox_process(mbox);
+	rc = mbox_process(mbox);
+exit:
+	mbox_put(mbox);
+	return rc;
 }
 
 int
@@ -313,10 +332,14 @@ roc_cpt_rxc_time_cfg(struct roc_cpt *roc_cpt, struct roc_cpt_rxc_time_cfg *cfg)
 	struct cpt *cpt = roc_cpt_to_cpt_priv(roc_cpt);
 	struct cpt_rxc_time_cfg_req *req;
 	struct dev *dev = &cpt->dev;
+	struct mbox *mbox = mbox_get(dev->mbox);
+	int rc;
 
-	req = mbox_alloc_msg_cpt_rxc_time_cfg(dev->mbox);
-	if (req == NULL)
-		return -ENOSPC;
+	req = mbox_alloc_msg_cpt_rxc_time_cfg(mbox);
+	if (req == NULL) {
+		rc = -ENOSPC;
+		goto exit;
+	}
 
 	req->blkaddr = 0;
 
@@ -331,18 +354,22 @@ roc_cpt_rxc_time_cfg(struct roc_cpt *roc_cpt, struct roc_cpt_rxc_time_cfg *cfg)
 	req->active_limit = cfg->active_limit;
 	req->active_thres = cfg->active_thres;
 
-	return mbox_process(dev->mbox);
+	rc = mbox_process(mbox);
+exit:
+	mbox_put(mbox);
+	return rc;
 }
 
 int
 cpt_get_msix_offset(struct dev *dev, struct msix_offset_rsp **msix_rsp)
 {
-	struct mbox *mbox = dev->mbox;
+	struct mbox *mbox = mbox_get(dev->mbox);
 	int rc;
 
 	/* Get MSIX vector offsets */
 	mbox_alloc_msg_msix_offset(mbox);
 	rc = mbox_process_msg(mbox, (void *)msix_rsp);
+	mbox_put(mbox);
 
 	return rc;
 }
@@ -350,55 +377,74 @@ cpt_get_msix_offset(struct dev *dev, struct msix_offset_rsp **msix_rsp)
 int
 cpt_lfs_attach(struct dev *dev, uint8_t blkaddr, bool modify, uint16_t nb_lf)
 {
-	struct mbox *mbox = dev->mbox;
+	struct mbox *mbox = mbox_get(dev->mbox);
 	struct rsrc_attach_req *req;
+	int rc;
 
-	if (blkaddr != RVU_BLOCK_ADDR_CPT0 && blkaddr != RVU_BLOCK_ADDR_CPT1)
-		return -EINVAL;
+	if (blkaddr != RVU_BLOCK_ADDR_CPT0 && blkaddr != RVU_BLOCK_ADDR_CPT1) {
+		rc = -EINVAL;
+		goto exit;
+	}
 
 	/* Attach CPT(lf) */
 	req = mbox_alloc_msg_attach_resources(mbox);
-	if (req == NULL)
-		return -ENOSPC;
+	if (req == NULL) {
+		rc = -ENOSPC;
+		goto exit;
+	}
 
 	req->cptlfs = nb_lf;
 	req->modify = modify;
 	req->cpt_blkaddr = blkaddr;
 
-	return mbox_process(mbox);
+	rc = mbox_process(mbox);
+exit:
+	mbox_put(mbox);
+	return rc;
 }
 
 int
 cpt_lfs_detach(struct dev *dev)
 {
-	struct mbox *mbox = dev->mbox;
+	struct mbox *mbox = mbox_get(dev->mbox);
 	struct rsrc_detach_req *req;
+	int rc;
 
 	req = mbox_alloc_msg_detach_resources(mbox);
-	if (req == NULL)
-		return -ENOSPC;
+	if (req == NULL) {
+		rc =  -ENOSPC;
+		goto exit;
+	}
 
 	req->cptlfs = 1;
 	req->partial = 1;
 
-	return mbox_process(mbox);
+	rc = mbox_process(mbox);
+exit:
+	mbox_put(mbox);
+	return rc;
 }
 
 static int
 cpt_available_lfs_get(struct dev *dev, uint16_t *nb_lf)
 {
-	struct mbox *mbox = dev->mbox;
+	struct mbox *mbox = mbox_get(dev->mbox);
 	struct free_rsrcs_rsp *rsp;
 	int rc;
 
 	mbox_alloc_msg_free_rsrc_cnt(mbox);
 
 	rc = mbox_process_msg(mbox, (void *)&rsp);
-	if (rc)
-		return -EIO;
+	if (rc) {
+		rc = -EIO;
+		goto exit;
+	}
 
 	*nb_lf = PLT_MAX((uint16_t)rsp->cpt, (uint16_t)rsp->cpt1);
-	return 0;
+	rc = 0;
+exit:
+	mbox_put(mbox);
+	return rc;
 }
 
 int
@@ -406,14 +452,19 @@ cpt_lfs_alloc(struct dev *dev, uint8_t eng_grpmsk, uint8_t blkaddr,
 	      bool inl_dev_sso)
 {
 	struct cpt_lf_alloc_req_msg *req;
-	struct mbox *mbox = dev->mbox;
+	struct mbox *mbox = mbox_get(dev->mbox);
+	int rc;
 
-	if (blkaddr != RVU_BLOCK_ADDR_CPT0 && blkaddr != RVU_BLOCK_ADDR_CPT1)
-		return -EINVAL;
+	if (blkaddr != RVU_BLOCK_ADDR_CPT0 && blkaddr != RVU_BLOCK_ADDR_CPT1) {
+		rc = -EINVAL;
+		goto exit;
+	}
 
 	req = mbox_alloc_msg_cpt_lf_alloc(mbox);
-	if (!req)
-		return -ENOSPC;
+	if (!req) {
+		rc = -ENOSPC;
+		goto exit;
+	}
 
 	req->nix_pf_func = 0;
 	if (inl_dev_sso && nix_inl_dev_pffunc_get())
@@ -423,34 +474,49 @@ cpt_lfs_alloc(struct dev *dev, uint8_t eng_grpmsk, uint8_t blkaddr,
 	req->eng_grpmsk = eng_grpmsk;
 	req->blkaddr = blkaddr;
 
-	return mbox_process(mbox);
+	rc = mbox_process(mbox);
+exit:
+	mbox_put(mbox);
+	return rc;
 }
 
 int
 cpt_lfs_free(struct dev *dev)
 {
-	mbox_alloc_msg_cpt_lf_free(dev->mbox);
+	struct mbox *mbox = mbox_get(dev->mbox);
+	int rc;
 
-	return mbox_process(dev->mbox);
+	mbox_alloc_msg_cpt_lf_free(mbox);
+
+	rc = mbox_process(mbox);
+	mbox_put(mbox);
+	return rc;
 }
 
 static int
 cpt_hardware_caps_get(struct dev *dev, struct roc_cpt *roc_cpt)
 {
 	struct cpt_caps_rsp_msg *rsp;
+	struct mbox *mbox = mbox_get(dev->mbox);
 	int ret;
 
-	mbox_alloc_msg_cpt_caps_get(dev->mbox);
+	mbox_alloc_msg_cpt_caps_get(mbox);
 
-	ret = mbox_process_msg(dev->mbox, (void *)&rsp);
-	if (ret)
-		return -EIO;
+	ret = mbox_process_msg(mbox, (void *)&rsp);
+	if (ret) {
+		ret = -EIO;
+		goto exit;
+	}
 
 	roc_cpt->cpt_revision = rsp->cpt_revision;
 	mbox_memcpy(roc_cpt->hw_caps, rsp->eng_caps,
 		    sizeof(union cpt_eng_caps) * CPT_MAX_ENG_TYPES);
 
-	return 0;
+	ret = 0;
+
+exit:
+	mbox_put(mbox);
+	return ret;
 }
 
 static uint32_t
@@ -743,14 +809,21 @@ cpt_lf_reset(struct roc_cpt_lf *lf)
 {
 	struct cpt_lf_rst_req *req;
 	struct dev *dev = lf->dev;
+	struct mbox *mbox = mbox_get(dev->mbox);
+	int rc;
 
-	req = mbox_alloc_msg_cpt_lf_reset(dev->mbox);
-	if (req == NULL)
-		return -EIO;
+	req = mbox_alloc_msg_cpt_lf_reset(mbox);
+	if (req == NULL) {
+		rc = -EIO;
+		goto exit;
+	}
 
 	req->slot = lf->lf_id;
 
-	return mbox_process(dev->mbox);
+	rc = mbox_process(mbox);
+exit:
+	mbox_put(mbox);
+	return rc;
 }
 
 static void
@@ -891,11 +964,14 @@ roc_cpt_eng_grp_add(struct roc_cpt *roc_cpt, enum cpt_eng_type eng_type)
 	struct dev *dev = &cpt->dev;
 	struct cpt_eng_grp_req *req;
 	struct cpt_eng_grp_rsp *rsp;
+	struct mbox *mbox = mbox_get(dev->mbox);
 	int ret;
 
-	req = mbox_alloc_msg_cpt_eng_grp_get(dev->mbox);
-	if (req == NULL)
-		return -EIO;
+	req = mbox_alloc_msg_cpt_eng_grp_get(mbox);
+	if (req == NULL) {
+		ret = -EIO;
+		goto exit;
+	}
 
 	switch (eng_type) {
 	case CPT_ENG_TYPE_AE:
@@ -903,22 +979,29 @@ roc_cpt_eng_grp_add(struct roc_cpt *roc_cpt, enum cpt_eng_type eng_type)
 	case CPT_ENG_TYPE_IE:
 		break;
 	default:
-		return -EINVAL;
+		ret = -EINVAL;
+		goto exit;
 	}
 
 	req->eng_type = eng_type;
 	ret = mbox_process_msg(dev->mbox, (void *)&rsp);
-	if (ret)
-		return -EIO;
+	if (ret) {
+		ret = -EIO;
+		goto exit;
+	}
 
 	if (rsp->eng_grp_num > 8) {
 		plt_err("Invalid CPT engine group");
-		return -ENOTSUP;
+		ret = -ENOTSUP;
+		goto exit;
 	}
 
 	roc_cpt->eng_grp[eng_type] = rsp->eng_grp_num;
 
-	return rsp->eng_grp_num;
+	ret = rsp->eng_grp_num;
+exit:
+	mbox_put(mbox);
+	return ret;
 }
 
 void

drivers/common/cnxk/roc_cpt_debug.c
@@ -71,11 +71,14 @@ cpt_af_reg_read(struct roc_cpt *roc_cpt, uint64_t reg, uint64_t *val)
 	struct cpt *cpt = roc_cpt_to_cpt_priv(roc_cpt);
 	struct cpt_rd_wr_reg_msg *msg;
 	struct dev *dev = &cpt->dev;
+	struct mbox *mbox = mbox_get(dev->mbox);
 	int ret;
 
-	msg = mbox_alloc_msg_cpt_rd_wr_register(dev->mbox);
-	if (msg == NULL)
-		return -EIO;
+	msg = mbox_alloc_msg_cpt_rd_wr_register(mbox);
+	if (msg == NULL) {
+		ret = -EIO;
+		goto exit;
+	}
 
 	msg->hdr.pcifunc = dev->pf_func;
 
@@ -84,12 +87,17 @@ cpt_af_reg_read(struct roc_cpt *roc_cpt, uint64_t reg, uint64_t *val)
 	msg->ret_val = val;
 
 	ret = mbox_process_msg(dev->mbox, (void *)&msg);
-	if (ret)
-		return -EIO;
+	if (ret) {
+		ret =  -EIO;
+		goto exit;
+	}
 
 	*val = msg->val;
 
-	return 0;
+	ret = 0;
+exit:
+	mbox_put(mbox);
+	return ret;
 }
 
 static int
@@ -99,16 +107,21 @@ cpt_sts_print(struct roc_cpt *roc_cpt)
 	struct dev *dev = &cpt->dev;
 	struct cpt_sts_req *req;
 	struct cpt_sts_rsp *rsp;
+	struct mbox *mbox = mbox_get(dev->mbox);
 	int ret;
 
-	req = mbox_alloc_msg_cpt_sts_get(dev->mbox);
-	if (req == NULL)
-		return -EIO;
+	req = mbox_alloc_msg_cpt_sts_get(mbox);
+	if (req == NULL) {
+		ret = -EIO;
+		goto exit;
+	}
 
 	req->blkaddr = 0;
 	ret = mbox_process_msg(dev->mbox, (void *)&rsp);
-	if (ret)
-		return -EIO;
+	if (ret) {
+		ret = -EIO;
+		goto exit;
+	}
 
 	plt_print("    %s:\t0x%016" PRIx64, "inst_req_pc", rsp->inst_req_pc);
 	plt_print("    %s:\t0x%016" PRIx64, "inst_lat_pc", rsp->inst_lat_pc);
@@ -161,7 +174,10 @@ cpt_sts_print(struct roc_cpt *roc_cpt)
 	plt_print("    %s:\t\t0x%016" PRIx64, "cptclk_cnt", rsp->cptclk_cnt);
 	plt_print("    %s:\t\t0x%016" PRIx64, "diag", rsp->diag);
 
-	return 0;
+	ret = 0;
+exit:
+	mbox_put(mbox);
+	return ret;
 }
 
 int

drivers/common/cnxk/roc_dev.c
@@ -135,8 +135,6 @@ af_pf_wait_msg(struct dev *dev, uint16_t vf, int num_msg)
 	/* Enable interrupts */
 	plt_write64(~0ull, dev->bar2 + RVU_PF_INT_ENA_W1S);
 
-	plt_spinlock_lock(&mdev->mbox_lock);
-
 	req_hdr = (struct mbox_hdr *)((uintptr_t)mdev->mbase + mbox->rx_start);
 	if (req_hdr->num_msgs != num_msg)
 		plt_err("Routed messages: %d received: %d", num_msg,
@@ -203,7 +201,6 @@ af_pf_wait_msg(struct dev *dev, uint16_t vf, int num_msg)
 
 		offset = mbox->rx_start + msg->next_msgoff;
 	}
-	plt_spinlock_unlock(&mdev->mbox_lock);
 
 	return req_hdr->num_msgs;
 }
@@ -225,6 +222,7 @@ vf_pf_process_msgs(struct dev *dev, uint16_t vf)
 
 	offset = mbox->rx_start + PLT_ALIGN(sizeof(*req_hdr), MBOX_MSG_ALIGN);
 
+	mbox_get(dev->mbox);
 	for (i = 0; i < req_hdr->num_msgs; i++) {
 		msg = (struct mbox_msghdr *)((uintptr_t)mdev->mbase + offset);
 		size = mbox->rx_start + msg->next_msgoff - offset;
@@ -278,6 +276,7 @@ vf_pf_process_msgs(struct dev *dev, uint16_t vf)
 		af_pf_wait_msg(dev, vf, routed);
 		mbox_reset(dev->mbox, 0);
 	}
+	mbox_put(dev->mbox);
 
 	/* Send mbox responses to VF */
 	if (mdev->num_msgs) {
@@ -1015,10 +1014,13 @@ static int
 dev_setup_shared_lmt_region(struct mbox *mbox, bool valid_iova, uint64_t iova)
 {
 	struct lmtst_tbl_setup_req *req;
+	int rc;
 
-	req = mbox_alloc_msg_lmtst_tbl_setup(mbox);
-	if (!req)
-		return -ENOSPC;
+	req = mbox_alloc_msg_lmtst_tbl_setup(mbox_get(mbox));
+	if (!req) {
+		rc = -ENOSPC;
+		goto exit;
+	}
 
 	/* This pcifunc is defined with primary pcifunc whose LMT address
 	 * will be shared. If call contains valid IOVA, following pcifunc
@@ -1028,7 +1030,10 @@ dev_setup_shared_lmt_region(struct mbox *mbox, bool valid_iova, uint64_t iova)
 	req->use_local_lmt_region = valid_iova;
 	req->lmt_iova = iova;
 
-	return mbox_process(mbox);
+	rc = mbox_process(mbox);
+exit:
+	mbox_put(mbox);
+	return rc;
 }
 
 /* Total no of lines * size of each lmtline */

drivers/common/cnxk/roc_idev.c
@@ -40,6 +40,7 @@ idev_set_defaults(struct idev_cfg *idev)
 	idev->cpt = NULL;
 	idev->nix_inl_dev = NULL;
 	plt_spinlock_init(&idev->nix_inl_dev_lock);
+	plt_spinlock_init(&idev->npa_dev_lock);
 	__atomic_store_n(&idev->npa_refcnt, 0, __ATOMIC_RELEASE);
 }
 

drivers/common/cnxk/roc_idev_priv.h
@@ -33,6 +33,7 @@ struct idev_cfg {
 	struct nix_inl_dev *nix_inl_dev;
 	struct idev_nix_inl_cfg inl_cfg;
 	plt_spinlock_t nix_inl_dev_lock;
+	plt_spinlock_t npa_dev_lock;
 };
 
 /* Generic */

drivers/common/cnxk/roc_mbox.c
@@ -50,14 +50,12 @@ mbox_reset(struct mbox *mbox, int devid)
 	struct mbox_hdr *rx_hdr =
 		(struct mbox_hdr *)((uintptr_t)mdev->mbase + mbox->rx_start);
 
-	plt_spinlock_lock(&mdev->mbox_lock);
 	mdev->msg_size = 0;
 	mdev->rsp_size = 0;
 	tx_hdr->msg_size = 0;
 	tx_hdr->num_msgs = 0;
 	rx_hdr->msg_size = 0;
 	rx_hdr->num_msgs = 0;
-	plt_spinlock_unlock(&mdev->mbox_lock);
 }
 
 int
@@ -167,7 +165,6 @@ mbox_alloc_msg_rsp(struct mbox *mbox, int devid, int size, int size_rsp)
 	struct mbox_dev *mdev = &mbox->dev[devid];
 	struct mbox_msghdr *msghdr = NULL;
 
-	plt_spinlock_lock(&mdev->mbox_lock);
 	size = PLT_ALIGN(size, MBOX_MSG_ALIGN);
 	size_rsp = PLT_ALIGN(size_rsp, MBOX_MSG_ALIGN);
 	/* Check if there is space in mailbox */
@@ -191,7 +188,6 @@ mbox_alloc_msg_rsp(struct mbox *mbox, int devid, int size, int size_rsp)
 	mdev->rsp_size += size_rsp;
 	msghdr->next_msgoff = mdev->msg_size + msgs_offset();
 exit:
-	plt_spinlock_unlock(&mdev->mbox_lock);
 
 	return msghdr;
 }
@@ -409,11 +405,14 @@ send_ready_msg(struct mbox *mbox, uint16_t *pcifunc)
 	struct ready_msg_rsp *rsp;
 	int rc;
 
-	mbox_alloc_msg_ready(mbox);
+	mbox_alloc_msg_ready(mbox_get(mbox));
 
 	rc = mbox_process_msg(mbox, (void *)&rsp);
-	if (rc)
+	if (rc) {
+		mbox_put(mbox);
 		return rc;
+	}
+	mbox_put(mbox);
 
 	if (rsp->hdr.ver != MBOX_VERSION) {
 		plt_err("Incompatible MBox versions(AF: 0x%04x Client: 0x%04x)",

commit ffd38012c79177f2d645b080a2c24e8e75926369

common/cnxk: fix initialization of timer LF count
Assignment of nb_lfs variable with the available number of timer LFs was
not happening in the success case after the addition of the new label
"done". This patch fixes the same by moving the assignment under the
label.

Fixes: 44a9307c0908 ("common/cnxk: support mailbox locking")

Signed-off-by: Shijith Thotton <sthotton@marvell.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/common/cnxk/roc_tim.c
@@ -394,11 +394,11 @@ roc_tim_init(struct roc_tim *roc_tim)
 	} else {
 		goto done;
 	}
-	roc_tim->nb_lfs = nb_lfs;
 
 fail:
 	mbox_put(dev->mbox);
 done:
+	roc_tim->nb_lfs = nb_lfs;
 	return nb_lfs;
 }
 