1f5c489b362b50aa3a6a604a79e4234c06fa859a
Chengwen Feng <fengchengwen@huawei.com>
2023-03-20 09:21:02 +0000

    dma/dpaa2: fix segment fault when parse devargs
    The rte_kvargs_process() was used to parse KV pairs, it also supports
    to parse 'only keys' (e.g. socket_id) type. And the callback function
    parameter 'value' is NULL when parsed 'only keys'.
    
    This patch fixes segment fault when parse input args with 'only keys'.
    
    Fixes: 9e2f8ca6c224 ("dma/dpaa2: support DMA operations")
    Cc: stable@dpdk.org
    
    Signed-off-by: Chengwen Feng <fengchengwen@huawei.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/dma/dpaa2/dpaa2_qdma.c
+++ drivers/dma/dpaa2/dpaa2_qdma.c
@@ -1169,6 +1169,9 @@ check_devargs_handler(__rte_unused const char *key,
 		      const char *value,
 		      __rte_unused void *opaque)
 {
+	if (value == NULL)
+		return -EINVAL;
+
 	if (strcmp(value, "1"))
 		return -1;
 
