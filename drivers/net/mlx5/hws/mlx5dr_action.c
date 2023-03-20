ovsrobot/dpdk

commit ce946c7d3999793b61c3fed016417d27c333c736

net/mlx5/hws: support ibv context shared with local one
The idea is to have a shared ibv_context that all the resources
allocated on it (FT + TIR are exceptions)
Whenever a resource is created locally an alias object to that resource
allocated and used in the other context.
The connections between the resources are done according to each type of
the resource, to the original resource, or its alias resource.

Signed-off-by: Erez Shitrit <erezsh@nvidia.com>
Reviewed-by: Alex Vesker <valex@nvidia.com>
Acked-by: Viacheslav Ovsiienko <viacheslavo@nvidia.com>

drivers/net/mlx5/hws/mlx5dr_action.c
@@ -1147,6 +1147,7 @@ mlx5dr_action_create_reformat_root(struct mlx5dr_action *action,
 {
 	enum mlx5dv_flow_table_type ft_type = 0; /*fix compilation warn*/
 	uint32_t verb_reformat_type = 0;
+	struct ibv_context *ibv_ctx;
 	int ret;
 
 	/* Convert action to FT type and verbs reformat type */
@@ -1157,8 +1158,9 @@ mlx5dr_action_create_reformat_root(struct mlx5dr_action *action,
 	mlx5dr_action_conv_reformat_to_verbs(action->type, &verb_reformat_type);
 
 	/* Create the reformat type for root table */
+	ibv_ctx = mlx5dr_context_get_local_ibv(action->ctx);
 	action->flow_action =
-		mlx5_glue->dv_create_flow_action_packet_reformat_root(action->ctx->ibv_ctx,
+		mlx5_glue->dv_create_flow_action_packet_reformat_root(ibv_ctx,
 								      data_sz,
 								      data,
 								      verb_reformat_type,
@@ -1496,14 +1498,17 @@ mlx5dr_action_create_modify_header_root(struct mlx5dr_action *action,
 					__be64 *actions)
 {
 	enum mlx5dv_flow_table_type ft_type = 0;
+	struct ibv_context *local_ibv_ctx;
 	int ret;
 
 	ret = mlx5dr_action_conv_flags_to_ft_type(action->flags, &ft_type);
 	if (ret)
 		return rte_errno;
 
+	local_ibv_ctx = mlx5dr_context_get_local_ibv(action->ctx);
+
 	action->flow_action =
-		mlx5_glue->dv_create_flow_action_modify_header_root(action->ctx->ibv_ctx,
+		mlx5_glue->dv_create_flow_action_modify_header_root(local_ibv_ctx,
 								    actions_sz,
 								    (uint64_t *)actions,
 								    ft_type);

drivers/net/mlx5/hws/mlx5dr_table.c
@@ -40,7 +40,8 @@ mlx5dr_table_up_default_fdb_miss_tbl(struct mlx5dr_table *tbl)
 	assert(ctx->caps->eswitch_manager);
 	vport = ctx->caps->eswitch_manager_vport_number;
 
-	default_miss = mlx5dr_cmd_miss_ft_create(ctx->ibv_ctx, &ft_attr, vport);
+	default_miss = mlx5dr_cmd_miss_ft_create(mlx5dr_context_get_local_ibv(ctx),
+						 &ft_attr, vport);
 	if (!default_miss) {
 		DR_LOG(ERR, "Failed to default miss table type: 0x%x", tbl_type);
 		return rte_errno;
@@ -96,7 +97,8 @@ mlx5dr_table_connect_to_default_miss_tbl(struct mlx5dr_table *tbl,
 }
 
 struct mlx5dr_devx_obj *
-mlx5dr_table_create_default_ft(struct mlx5dr_table *tbl)
+mlx5dr_table_create_default_ft(struct ibv_context *ibv,
+			       struct mlx5dr_table *tbl)
 {
 	struct mlx5dr_cmd_ft_create_attr ft_attr = {0};
 	struct mlx5dr_devx_obj *ft_obj;
@@ -104,7 +106,7 @@ mlx5dr_table_create_default_ft(struct mlx5dr_table *tbl)
 
 	mlx5dr_table_init_next_ft_attr(tbl, &ft_attr);
 
-	ft_obj = mlx5dr_cmd_flow_table_create(tbl->ctx->ibv_ctx, &ft_attr);
+	ft_obj = mlx5dr_cmd_flow_table_create(ibv, &ft_attr);
 	if (ft_obj && tbl->type == MLX5DR_TABLE_TYPE_FDB) {
 		/* Take/create ref over the default miss */
 		ret = mlx5dr_table_up_default_fdb_miss_tbl(tbl);
@@ -128,6 +130,171 @@ free_ft_obj:
 	return NULL;
 }
 
+static int
+mlx5dr_table_init_check_hws_support(struct mlx5dr_context *ctx,
+				    struct mlx5dr_table *tbl)
+{
+	if (!(ctx->flags & MLX5DR_CONTEXT_FLAG_HWS_SUPPORT)) {
+		DR_LOG(ERR, "HWS not supported, cannot create mlx5dr_table");
+		rte_errno = EOPNOTSUPP;
+		return rte_errno;
+	}
+
+	if (mlx5dr_context_shared_gvmi_used(ctx) && tbl->type == MLX5DR_TABLE_TYPE_FDB) {
+		DR_LOG(ERR, "FDB with shared port resources is not supported");
+		rte_errno = EOPNOTSUPP;
+		return rte_errno;
+	}
+
+	return 0;
+}
+
+static int
+mlx5dr_table_shared_gvmi_resource_create(struct mlx5dr_context *ctx,
+					 enum mlx5dr_table_type type,
+					 struct mlx5dr_context_shared_gvmi_res *gvmi_res)
+{
+	struct mlx5dr_cmd_ft_create_attr ft_attr = {0};
+	uint32_t calculated_ft_id;
+	int ret;
+
+	if (!mlx5dr_context_shared_gvmi_used(ctx))
+		return 0;
+
+	ft_attr.type = mlx5dr_table_get_res_fw_ft_type(type, false);
+	ft_attr.level = ctx->caps->nic_ft.max_level - 1;
+	ft_attr.rtc_valid = true;
+
+	gvmi_res->end_ft =
+		mlx5dr_cmd_flow_table_create(mlx5dr_context_get_local_ibv(ctx),
+					     &ft_attr);
+	if (!gvmi_res->end_ft) {
+		DR_LOG(ERR, "Failed to create end-ft");
+		return rte_errno;
+	}
+
+	calculated_ft_id =
+		mlx5dr_table_get_res_fw_ft_type(type, false) << FT_ID_FT_TYPE_OFFSET;
+	calculated_ft_id |= gvmi_res->end_ft->id;
+
+	/* create alias to that FT */
+	ret = mlx5dr_matcher_create_aliased_obj(ctx,
+						ctx->local_ibv_ctx,
+						ctx->ibv_ctx,
+						ctx->caps->vhca_id,
+						calculated_ft_id,
+						MLX5_GENERAL_OBJ_TYPE_FT_ALIAS,
+						&gvmi_res->aliased_end_ft);
+	if (ret) {
+		DR_LOG(ERR, "Failed to create alias end-ft");
+		goto free_end_ft;
+	}
+
+	return 0;
+
+free_end_ft:
+	mlx5dr_cmd_destroy_obj(gvmi_res->end_ft);
+
+	return rte_errno;
+}
+
+static void
+mlx5dr_table_shared_gvmi_resourse_destroy(struct mlx5dr_context *ctx,
+					  struct mlx5dr_context_shared_gvmi_res *gvmi_res)
+{
+	if (!mlx5dr_context_shared_gvmi_used(ctx))
+		return;
+
+	if (gvmi_res->aliased_end_ft) {
+		mlx5dr_cmd_destroy_obj(gvmi_res->aliased_end_ft);
+		gvmi_res->aliased_end_ft = NULL;
+	}
+	if (gvmi_res->end_ft) {
+		mlx5dr_cmd_destroy_obj(gvmi_res->end_ft);
+		gvmi_res->end_ft = NULL;
+	}
+}
+
+/* called under spinlock ctx->ctrl_lock */
+static struct mlx5dr_context_shared_gvmi_res *
+mlx5dr_table_get_shared_gvmi_res(struct mlx5dr_context *ctx, enum mlx5dr_table_type type)
+{
+	int ret;
+
+	if (!mlx5dr_context_shared_gvmi_used(ctx))
+		return NULL;
+
+	if (ctx->gvmi_res[type].aliased_end_ft) {
+		ctx->gvmi_res[type].refcount++;
+		return &ctx->gvmi_res[type];
+	}
+
+	ret = mlx5dr_table_shared_gvmi_resource_create(ctx, type, &ctx->gvmi_res[type]);
+	if (ret) {
+		DR_LOG(ERR, "Failed to create shared gvmi res for type: %d", type);
+		goto out;
+	}
+
+	ctx->gvmi_res[type].refcount = 1;
+
+	return &ctx->gvmi_res[type];
+
+out:
+	return NULL;
+}
+
+/* called under spinlock ctx->ctrl_lock */
+static void mlx5dr_table_put_shared_gvmi_res(struct mlx5dr_table *tbl)
+{
+	struct mlx5dr_context *ctx = tbl->ctx;
+
+	if (!mlx5dr_context_shared_gvmi_used(ctx))
+		return;
+
+	if (--ctx->gvmi_res[tbl->type].refcount)
+		return;
+
+	mlx5dr_table_shared_gvmi_resourse_destroy(ctx, &ctx->gvmi_res[tbl->type]);
+}
+
+static void mlx5dr_table_uninit_shared_ctx_res(struct mlx5dr_table *tbl)
+{
+	struct mlx5dr_context *ctx = tbl->ctx;
+
+	if (!mlx5dr_context_shared_gvmi_used(ctx))
+		return;
+
+	mlx5dr_cmd_destroy_obj(tbl->local_ft);
+
+	mlx5dr_table_put_shared_gvmi_res(tbl);
+}
+
+/* called under spin_lock ctx->ctrl_lock */
+static int mlx5dr_table_init_shared_ctx_res(struct mlx5dr_context *ctx, struct mlx5dr_table *tbl)
+{
+	if (!mlx5dr_context_shared_gvmi_used(ctx))
+		return 0;
+
+	/* create local-ft for root access */
+	tbl->local_ft =
+		mlx5dr_table_create_default_ft(mlx5dr_context_get_local_ibv(ctx), tbl);
+	if (!tbl->local_ft) {
+		DR_LOG(ERR, "Failed to create local-ft");
+		return rte_errno;
+	}
+
+	if (!mlx5dr_table_get_shared_gvmi_res(tbl->ctx, tbl->type)) {
+		DR_LOG(ERR, "Failed to shared gvmi resources");
+		goto clean_local_ft;
+	}
+
+	return 0;
+
+clean_local_ft:
+	mlx5dr_table_destroy_default_ft(tbl, tbl->local_ft);
+	return rte_errno;
+}
+
 void mlx5dr_table_destroy_default_ft(struct mlx5dr_table *tbl,
 				     struct mlx5dr_devx_obj *ft_obj)
 {
@@ -143,11 +310,9 @@ static int mlx5dr_table_init(struct mlx5dr_table *tbl)
 	if (mlx5dr_table_is_root(tbl))
 		return 0;
 
-	if (!(tbl->ctx->flags & MLX5DR_CONTEXT_FLAG_HWS_SUPPORT)) {
-		DR_LOG(ERR, "HWS not supported, cannot create mlx5dr_table");
-		rte_errno = EOPNOTSUPP;
-		return rte_errno;
-	}
+	ret = mlx5dr_table_init_check_hws_support(ctx, tbl);
+	if (ret)
+		return ret;
 
 	switch (tbl->type) {
 	case MLX5DR_TABLE_TYPE_NIC_RX:
@@ -165,7 +330,7 @@ static int mlx5dr_table_init(struct mlx5dr_table *tbl)
 	}
 
 	pthread_spin_lock(&ctx->ctrl_lock);
-	tbl->ft = mlx5dr_table_create_default_ft(tbl);
+	tbl->ft = mlx5dr_table_create_default_ft(tbl->ctx->ibv_ctx, tbl);
 	if (!tbl->ft) {
 		DR_LOG(ERR, "Failed to create flow table devx object");
 		pthread_spin_unlock(&ctx->ctrl_lock);
@@ -175,10 +340,17 @@ static int mlx5dr_table_init(struct mlx5dr_table *tbl)
 	ret = mlx5dr_action_get_default_stc(ctx, tbl->type);
 	if (ret)
 		goto tbl_destroy;
+
+	ret = mlx5dr_table_init_shared_ctx_res(ctx, tbl);
+	if (ret)
+		goto put_stc;
+
 	pthread_spin_unlock(&ctx->ctrl_lock);
 
 	return 0;
 
+put_stc:
+	mlx5dr_action_put_default_stc(ctx, tbl->type);
 tbl_destroy:
 	mlx5dr_table_destroy_default_ft(tbl, tbl->ft);
 	pthread_spin_unlock(&ctx->ctrl_lock);
@@ -192,6 +364,7 @@ static void mlx5dr_table_uninit(struct mlx5dr_table *tbl)
 	pthread_spin_lock(&tbl->ctx->ctrl_lock);
 	mlx5dr_action_put_default_stc(tbl->ctx, tbl->type);
 	mlx5dr_table_destroy_default_ft(tbl, tbl->ft);
+	mlx5dr_table_uninit_shared_ctx_res(tbl);
 	pthread_spin_unlock(&tbl->ctx->ctrl_lock);
 }
 

drivers/common/mlx5/mlx5_prm.h
@@ -291,6 +291,12 @@
 
 #define MAX_ACTIONS_DATA_IN_HEADER_MODIFY 512
 
+/* Alias FT id passed to the ALLOW_OTHER_VHCA_ACCESS & CREATE_GENERAL_OBJECT
+ * commands should have the following format:
+ * {table_type: 8bits, table_id: 24bits}.
+ */
+#define FT_ID_FT_TYPE_OFFSET 24
+
 /* Completion mode. */
 enum mlx5_completion_mode {
 	MLX5_COMP_ONLY_ERR = 0x0,

drivers/net/mlx5/hws/mlx5dr.h
@@ -96,6 +96,8 @@ struct mlx5dr_context_attr {
 	size_t initial_log_ste_memory; /* Currently not in use */
 	/* Optional PD used for allocating res ources */
 	struct ibv_pd *pd;
+	/* Optional other ctx for resources allocation, all objects will be created on it */
+	struct ibv_context *shared_ibv_ctx;
 };
 
 struct mlx5dr_table_attr {

drivers/net/mlx5/hws/mlx5dr_cmd.c
@@ -217,18 +217,23 @@ void mlx5dr_cmd_set_attr_connect_miss_tbl(struct mlx5dr_context *ctx,
 {
 	struct mlx5dr_devx_obj *default_miss_tbl;
 
-	if (type != MLX5DR_TABLE_TYPE_FDB)
+	if (type != MLX5DR_TABLE_TYPE_FDB && !mlx5dr_context_shared_gvmi_used(ctx))
 		return;
 
-	default_miss_tbl = ctx->common_res[type].default_miss->ft;
-	if (!default_miss_tbl) {
-		assert(false);
-		return;
-	}
 	ft_attr->modify_fs = MLX5_IFC_MODIFY_FLOW_TABLE_MISS_ACTION;
 	ft_attr->type = fw_ft_type;
 	ft_attr->table_miss_action = MLX5_IFC_MODIFY_FLOW_TABLE_MISS_ACTION_GOTO_TBL;
-	ft_attr->table_miss_id = default_miss_tbl->id;
+
+	if (type == MLX5DR_TABLE_TYPE_FDB) {
+		default_miss_tbl = ctx->common_res[type].default_miss->ft;
+		if (!default_miss_tbl) {
+			assert(false);
+			return;
+		}
+		ft_attr->table_miss_id = default_miss_tbl->id;
+	} else {
+		ft_attr->table_miss_id = ctx->gvmi_res[type].aliased_end_ft->id;
+	}
 }
 
 struct mlx5dr_devx_obj *

drivers/net/mlx5/hws/mlx5dr_cmd.h
@@ -11,6 +11,8 @@ struct mlx5dr_cmd_ft_create_attr {
 	bool rtc_valid;
 };
 
+#define ACCESS_KEY_LEN	32
+
 struct mlx5dr_cmd_ft_modify_attr {
 	uint8_t type;
 	uint32_t rtc_id_0;
@@ -50,7 +52,7 @@ struct mlx5dr_cmd_alias_obj_create_attr {
 	uint32_t obj_id;
 	uint16_t vhca_id;
 	uint16_t obj_type;
-	uint8_t access_key[32];
+	uint8_t access_key[ACCESS_KEY_LEN];
 };
 
 struct mlx5dr_cmd_stc_create_attr {
@@ -134,7 +136,7 @@ struct mlx5dr_cmd_sq_create_attr {
 struct mlx5dr_cmd_allow_other_vhca_access_attr {
 	uint16_t obj_type;
 	uint32_t obj_id;
-	uint8_t access_key[32];
+	uint8_t access_key[ACCESS_KEY_LEN];
 };
 
 struct mlx5dr_cmd_query_ft_caps {
@@ -178,6 +180,7 @@ struct mlx5dr_cmd_query_caps {
 	uint32_t trivial_match_definer;
 	uint32_t vhca_id;
 	bool cross_vhca_resources;
+	uint32_t shared_vhca_id;
 	char fw_ver[64];
 };
 

drivers/net/mlx5/hws/mlx5dr_context.c
@@ -178,6 +178,36 @@ static void mlx5dr_context_uninit_hws(struct mlx5dr_context *ctx)
 	mlx5dr_context_uninit_pd(ctx);
 }
 
+static int mlx5dr_context_init_shared_ctx(struct mlx5dr_context *ctx,
+					  struct ibv_context *ibv_ctx,
+					  struct mlx5dr_context_attr *attr)
+{
+	struct mlx5dr_cmd_query_caps shared_caps = {0};
+	int ret;
+
+	if (!attr->shared_ibv_ctx) {
+		ctx->ibv_ctx = ibv_ctx;
+	} else {
+		ctx->ibv_ctx = attr->shared_ibv_ctx;
+		ctx->local_ibv_ctx = ibv_ctx;
+		ret = mlx5dr_cmd_query_caps(attr->shared_ibv_ctx, &shared_caps);
+		if (ret || !shared_caps.cross_vhca_resources) {
+			DR_LOG(INFO, "No cross_vhca_resources cap for shared ibv");
+			rte_errno = ENOTSUP;
+			return rte_errno;
+		}
+		ctx->caps->shared_vhca_id = shared_caps.vhca_id;
+	}
+
+	if (ctx->local_ibv_ctx && !ctx->caps->cross_vhca_resources) {
+		DR_LOG(INFO, "No cross_vhca_resources cap for local ibv");
+		rte_errno = ENOTSUP;
+		return rte_errno;
+	}
+
+	return 0;
+}
+
 struct mlx5dr_context *mlx5dr_context_open(struct ibv_context *ibv_ctx,
 					   struct mlx5dr_context_attr *attr)
 {
@@ -190,7 +220,6 @@ struct mlx5dr_context *mlx5dr_context_open(struct ibv_context *ibv_ctx,
 		return NULL;
 	}
 
-	ctx->ibv_ctx = ibv_ctx;
 	pthread_spin_init(&ctx->ctrl_lock, PTHREAD_PROCESS_PRIVATE);
 
 	ctx->caps = simple_calloc(1, sizeof(*ctx->caps));
@@ -199,6 +228,9 @@ struct mlx5dr_context *mlx5dr_context_open(struct ibv_context *ibv_ctx,
 
 	ret = mlx5dr_cmd_query_caps(ibv_ctx, ctx->caps);
 	if (ret)
+		goto free_caps;
+
+	if (mlx5dr_context_init_shared_ctx(ctx, ibv_ctx, attr))
 		goto free_caps;
 
 	ret = mlx5dr_context_init_hws(ctx, attr);

drivers/net/mlx5/hws/mlx5dr_context.h
@@ -22,13 +22,22 @@ struct mlx5dr_context_common_res {
 	struct mlx5dr_cmd_forward_tbl *default_miss;
 };
 
+struct mlx5dr_context_shared_gvmi_res {
+	struct mlx5dr_devx_obj *end_ft;
+	struct mlx5dr_devx_obj *aliased_end_ft;
+	uint32_t refcount;
+};
+
 struct mlx5dr_context {
 	struct ibv_context *ibv_ctx;
+	/* When local_ibv_ctx is not NULL means we are using shared_ibv for resources */
+	struct ibv_context *local_ibv_ctx;
 	struct mlx5dr_cmd_query_caps *caps;
 	struct ibv_pd *pd;
 	uint32_t pd_num;
 	struct mlx5dr_pool *stc_pool[MLX5DR_TABLE_TYPE_MAX];
 	struct mlx5dr_context_common_res common_res[MLX5DR_TABLE_TYPE_MAX];
+	struct mlx5dr_context_shared_gvmi_res gvmi_res[MLX5DR_TABLE_TYPE_MAX];
 	struct mlx5dr_pattern_cache *pattern_cache;
 	pthread_spinlock_t ctrl_lock;
 	enum mlx5dr_context_flags flags;
@@ -37,4 +46,17 @@ struct mlx5dr_context {
 	LIST_HEAD(table_head, mlx5dr_table) head;
 };
 
+static inline bool mlx5dr_context_shared_gvmi_used(struct mlx5dr_context *ctx)
+{
+	return ctx->local_ibv_ctx ? true : false;
+}
+
+static inline struct ibv_context *
+mlx5dr_context_get_local_ibv(struct mlx5dr_context *ctx)
+{
+	if (mlx5dr_context_shared_gvmi_used(ctx))
+		return ctx->local_ibv_ctx;
+
+	return ctx->ibv_ctx;
+}
 #endif /* MLX5DR_CONTEXT_H_ */

commit eedbe3c88d03dfe344306d6bec141870212f4700

net/mlx5/hws: shared context uses defaults from local context
Fix default miss behavior for shared resources, the problem
could happen in two cases:
When the table created, the default miss should go to the alias ft
that will direct it back to local context.

When the rule is without specific hit address and we are in a shared
RTC from here it should be redirected back to the local context.

Fixes: ce946c7d3999 ("net/mlx5/hws: support ibv context shared with local one")
Reviewed-by: Alex Vesker <valex@nvidia.com>
Signed-off-by: Erez Shitrit <erezsh@nvidia.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/mlx5/hws/mlx5dr_action.c
@@ -1751,8 +1751,15 @@ int mlx5dr_action_get_default_stc(struct mlx5dr_context *ctx,
 		goto free_nop_dw6;
 	}
 
-	stc_attr.action_type = MLX5_IFC_STC_ACTION_TYPE_ALLOW;
 	stc_attr.action_offset = MLX5DR_ACTION_OFFSET_HIT;
+	if (!mlx5dr_context_shared_gvmi_used(ctx)) {
+		stc_attr.action_type = MLX5_IFC_STC_ACTION_TYPE_ALLOW;
+	} else {
+		/* On shared gvmi the default hit behavior is jump to alias end ft */
+		stc_attr.action_type = MLX5_IFC_STC_ACTION_TYPE_JUMP_TO_FT;
+		stc_attr.dest_table_id = ctx->gvmi_res[tbl_type].aliased_end_ft->id;
+	}
+
 	ret = mlx5dr_action_alloc_single_stc(ctx, &stc_attr, tbl_type,
 					     &default_stc->default_hit);
 	if (ret) {

drivers/net/mlx5/hws/mlx5dr_table.c
@@ -272,6 +272,9 @@ static void mlx5dr_table_uninit_shared_ctx_res(struct mlx5dr_table *tbl)
 /* called under spin_lock ctx->ctrl_lock */
 static int mlx5dr_table_init_shared_ctx_res(struct mlx5dr_context *ctx, struct mlx5dr_table *tbl)
 {
+	struct mlx5dr_cmd_ft_modify_attr ft_attr = {0};
+	int ret;
+
 	if (!mlx5dr_context_shared_gvmi_used(ctx))
 		return 0;
 
@@ -288,8 +291,22 @@ static int mlx5dr_table_init_shared_ctx_res(struct mlx5dr_context *ctx, struct m
 		goto clean_local_ft;
 	}
 
+	/* On shared gvmi the default behavior is jump to alias end ft */
+	mlx5dr_cmd_set_attr_connect_miss_tbl(tbl->ctx,
+					     tbl->fw_ft_type,
+					     tbl->type,
+					     &ft_attr);
+
+	ret = mlx5dr_cmd_flow_table_modify(tbl->ft, &ft_attr);
+	if (ret) {
+		DR_LOG(ERR, "Failed to point table to its default miss");
+		goto clean_shared_res;
+	}
+
 	return 0;
 
+clean_shared_res:
+	mlx5dr_table_put_shared_gvmi_res(tbl);
 clean_local_ft:
 	mlx5dr_table_destroy_default_ft(tbl, tbl->local_ft);
 	return rte_errno;
@@ -337,20 +354,20 @@ static int mlx5dr_table_init(struct mlx5dr_table *tbl)
 		return rte_errno;
 	}
 
-	ret = mlx5dr_action_get_default_stc(ctx, tbl->type);
+	ret = mlx5dr_table_init_shared_ctx_res(ctx, tbl);
 	if (ret)
 		goto tbl_destroy;
 
-	ret = mlx5dr_table_init_shared_ctx_res(ctx, tbl);
+	ret = mlx5dr_action_get_default_stc(ctx, tbl->type);
 	if (ret)
-		goto put_stc;
+		goto free_shared_ctx;
 
 	pthread_spin_unlock(&ctx->ctrl_lock);
 
 	return 0;
 
-put_stc:
-	mlx5dr_action_put_default_stc(ctx, tbl->type);
+free_shared_ctx:
+	mlx5dr_table_uninit_shared_ctx_res(tbl);
 tbl_destroy:
 	mlx5dr_table_destroy_default_ft(tbl, tbl->ft);
 	pthread_spin_unlock(&ctx->ctrl_lock);
@@ -363,8 +380,8 @@ static void mlx5dr_table_uninit(struct mlx5dr_table *tbl)
 		return;
 	pthread_spin_lock(&tbl->ctx->ctrl_lock);
 	mlx5dr_action_put_default_stc(tbl->ctx, tbl->type);
-	mlx5dr_table_destroy_default_ft(tbl, tbl->ft);
 	mlx5dr_table_uninit_shared_ctx_res(tbl);
+	mlx5dr_table_destroy_default_ft(tbl, tbl->ft);
 	pthread_spin_unlock(&tbl->ctx->ctrl_lock);
 }
 