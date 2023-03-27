ovsrobot/dpdk

commit e83d41f0694d69475bbf10f3296fa832585a2531

mailmap: add list of contributors
Since a number of contributors to DPDK have submitted patches to DPDK
under more than one email address, we should maintain a mailmap file
to properly track their commits using "shortlog",
and to do accurate automatic Cc with "get_maintainer.pl".

It also helps fix up any mangled names, for example, with
surname/firstname reversed, or with incorrect capitalization.
By keeping this file in the DPDK repository,
rather than committers maintaining their own copies,
it allows individual contributors to edit it
to update their own email address preferences if so desired.

While at it, update our checkpatches.sh script
and add some documentation to help new contributors.

Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>
Signed-off-by: David Marchand <david.marchand@redhat.com>
Acked-by: Thomas Monjalon <thomas@monjalon.net>

devtools/checkpatches.sh
@@ -248,6 +248,28 @@ check_release_notes() { # <patch>
 		grep -v $current_rel_notes
 }
 
+check_names() { # <patch>
+	res=0
+
+	old_IFS=$IFS
+	IFS='
+'
+	for contributor in $(sed -rn '1,/^--- / {s/.*: (.*<.*@.*>)/\1/p}' $1); do
+		! grep -qE "^$contributor($| <)" .mailmap || continue
+		name=${contributor%% <*}
+		if grep -q "^$name <" .mailmap; then
+			reason="$name mail differs from primary mail"
+		else
+			reason="$contributor is unknown"
+		fi
+		echo "$reason, please fix the commit message or update .mailmap."
+		res=1
+	done
+	IFS=$old_IFS
+
+	return $res
+}
+
 number=0
 range='origin/main..'
 quiet=false
@@ -350,6 +372,14 @@ check () { # <patch-file> <commit>
 
 	! $verbose || printf '\nChecking release notes updates:\n'
 	report=$(check_release_notes "$tmpinput")
+	if [ $? -ne 0 ] ; then
+		$headline_printed || print_headline "$subject"
+		printf '%s\n' "$report"
+		ret=1
+	fi
+
+	! $verbose || printf '\nChecking names in commit log:\n'
+	report=$(check_names "$tmpinput")
 	if [ $? -ne 0 ] ; then
 		$headline_printed || print_headline "$subject"
 		printf '%s\n' "$report"

MAINTAINERS
@@ -103,6 +103,7 @@ F: devtools/libabigail.abignore
 F: devtools/words-case.txt
 F: license/
 F: .editorconfig
+F: .mailmap
 
 Build System
 M: Bruce Richardson <bruce.richardson@intel.com>

devtools/check-spdx-tag.sh
@@ -20,7 +20,7 @@ check_spdx() {
 	echo "--------------------------"
     fi
     git grep -L SPDX-License-Identifier -- \
-	':^.git*' ':^.ci/*' ':^.travis.yml' \
+	':^.git*' ':^.mailmap' ':^.ci/*' ':^.travis.yml' \
 	':^README' ':^MAINTAINERS' ':^VERSION' ':^ABI_VERSION' \
 	':^*/Kbuild' ':^*/README' \
 	':^license/' ':^config/' ':^buildtools/' ':^*/poetry.lock' \

commit af43f7db383fe25965b3693033e3057c4f441056

devtools: move mailmap check after patch applied
The names in a patch were possibly checked with checkpatches.sh
before applying the patch, so before .mailmap file was updated.

The check is moved and translated in check-git-log.sh,
which is run only on a repository, not a detached patch file.

Fixes: e83d41f0694d ("mailmap: add list of contributors")
Cc: stable@dpdk.org

Signed-off-by: Thomas Monjalon <thomas@monjalon.net>
Signed-off-by: 0-day Robot <robot@bytheb.org>

devtools/check-git-log.sh
@@ -259,6 +259,21 @@ done)
 [ -z "$bad" ] || { printf "Missing 'Signed-off-by:' tag: \n$bad\n"\
 	&& failure=true;}
 
+# check names
+names=$(git log --format='From: %an <%ae>%n%b' --reverse $range |
+	sed -rn 's,.*: (.*<.*@.*>),\1,p' |
+	sort -u)
+bad=$(for contributor in $names ; do
+	! grep -qE "^$contributor($| <)" $selfdir/../.mailmap || continue
+	if grep -q "^${contributor%% <*} <" .mailmap ; then
+		printf "\t$contributor is not the primary email address\n"
+	else
+		printf "\t$contributor is unknown in .mailmap\n"
+	fi
+done)
+[ -z "$bad" ] || { printf "Contributor name/email mismatch with .mailmap: \n$bad\n"\
+	&& failure=true;}
+
 total=$(echo "$commits" | wc -l)
 if $failure ; then
 	printf "\nInvalid patch(es) found - checked $total patch"

devtools/checkpatches.sh
@@ -248,28 +248,6 @@ check_release_notes() { # <patch>
 		grep -v $current_rel_notes
 }
 
-check_names() { # <patch>
-	res=0
-
-	old_IFS=$IFS
-	IFS='
-'
-	for contributor in $(sed -rn '/^$/,/^--- / {s/.*: (.*<.*@.*>)/\1/p}' $1); do
-		! grep -qE "^$contributor($| <)" .mailmap || continue
-		name=${contributor%% <*}
-		if grep -q "^$name <" .mailmap; then
-			reason="$name mail differs from primary mail"
-		else
-			reason="$contributor is unknown"
-		fi
-		echo "$reason, please fix the commit message or update .mailmap."
-		res=1
-	done
-	IFS=$old_IFS
-
-	return $res
-}
-
 number=0
 range='origin/main..'
 quiet=false
@@ -372,14 +350,6 @@ check () { # <patch-file> <commit>
 
 	! $verbose || printf '\nChecking release notes updates:\n'
 	report=$(check_release_notes "$tmpinput")
-	if [ $? -ne 0 ] ; then
-		$headline_printed || print_headline "$subject"
-		printf '%s\n' "$report"
-		ret=1
-	fi
-
-	! $verbose || printf '\nChecking names in commit log:\n'
-	report=$(check_names "$tmpinput")
 	if [ $? -ne 0 ] ; then
 		$headline_printed || print_headline "$subject"
 		printf '%s\n' "$report"