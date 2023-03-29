8a0b7abb9b1dee630d8e4232606f5e4b037767de
Zhirun Yan <zhirun.yan@intel.com>
2023-03-29 15:43:32 +0900

    graph: introduce graph clone API for other worker core
    This patch adds graph API for supporting to clone the graph object for
    a specified worker core. The new graph will also clone all nodes.
    
    Signed-off-by: Haiyue Wang <haiyue.wang@intel.com>
    Signed-off-by: Cunming Liang <cunming.liang@intel.com>
    Signed-off-by: Zhirun Yan <zhirun.yan@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/graph/graph_private.h
+++ lib/graph/graph_private.h
@@ -98,6 +98,8 @@ struct graph {
 	/**< Circular buffer mask for wrap around. */
 	rte_graph_t id;
 	/**< Graph identifier. */
+	rte_graph_t parent_id;
+	/**< Parent graph identifier. */
 	unsigned int lcore_id;
 	/**< Lcore identifier where the graph prefer to run on. */
 	size_t mem_sz;
