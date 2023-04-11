aa3ad64228ad2a9f1ad3d0195d0c7149d1ac51bb
Tyler Retzlaff <roretzla@linux.microsoft.com>
2023-04-11 14:12:26 -0700

    eal: exclude exposure of rte atomic APIs for MSVC builds
    It's discouraged to use rte_atomics APIs instead standard APIs should be
    used from C11. Since MSVC is a new toolchain/platform combination block
    visibility of the rte_atomic APIs from day 1.
    
    Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/x86/include/rte_atomic.h
+++ lib/eal/x86/include/rte_atomic.h
@@ -82,6 +82,8 @@ rte_smp_mb(void)
 
 #define rte_io_rmb() rte_compiler_barrier()
 
+#ifndef RTE_TOOLCHAIN_MSVC
+
 /**
  * Synchronization fence between threads based on the specified memory order.
  *
@@ -276,6 +278,8 @@ static inline int rte_atomic32_dec_and_test(rte_atomic32_t *v)
 #include "rte_atomic_32.h"
 #else
 #include "rte_atomic_64.h"
+#endif
+
 #endif
 
 #ifdef __cplusplus
