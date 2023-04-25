ddb167246e1842ac2f9f499da6a9221e657ab7e1
Kaisen You <kaisenx.you@intel.com>
2023-04-25 13:16:49 +0800

    enhance NUMA affinity heuristic
    Trying to allocate memory on the first detected numa node,it has less
    chance to find some memory actually available rather than on the main
    lcore numa node (especially when the DPDK application is started only
    on one numa node).
    
    Fixes: 8b0a1b8cb481 ("eal: stop using pthread for lcores and control threads")
    Fixes: 770d41bf3309 ("malloc: fix allocation with unknown socket ID")
    Cc: stable@dpdk.org
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Signed-off-by: Kaisen You <kaisenx.you@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/common/eal_common_thread.c
+++ lib/eal/common/eal_common_thread.c
@@ -252,6 +252,10 @@ static int ctrl_thread_init(void *arg)
 	struct rte_thread_ctrl_params *params = arg;
 
 	__rte_thread_init(rte_lcore_id(), cpuset);
+	/* set the value of the per-core variable _socket_id.
+	 * Convenient for threads to find memory.
+	 */
+	RTE_PER_LCORE(_socket_id) = SOCKET_ID_ANY;
 	params->ret = rte_thread_set_affinity_by_id(rte_thread_self(), cpuset);
 	if (params->ret != 0) {
 		__atomic_store_n(&params->ctrl_thread_status,
--- lib/eal/common/malloc_heap.c
+++ lib/eal/common/malloc_heap.c
@@ -716,6 +716,12 @@ malloc_get_numa_socket(void)
 		if (conf->socket_mem[socket_id] != 0)
 			return socket_id;
 	}
+	/* Trying to allocate memory on the main lcore numa node.
+	 * especially when the DPDK application is started only on one numa node.
+	 */
+	socket_id = rte_lcore_to_socket_id(rte_get_main_lcore());
+	if (socket_id != (unsigned int)SOCKET_ID_ANY)
+		return socket_id;
 
 	return rte_socket_id_by_idx(0);
 }
