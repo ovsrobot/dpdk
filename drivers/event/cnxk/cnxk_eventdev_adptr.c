ovsrobot/dpdk

commit 6a24c7c4bcd12a85095fe25bd92083c247cc4a9e

event/cnxk: add Tx adapter freeing
Tx adapter allocate data during eth_tx_adapter_queue_add() call and
it's only cleaned but not freed during eth_tx_adapter_queue_del().
Implemented eth_tx_adapter_free() callback to free adapter data.

Signed-off-by: Volodymyr Fialko <vfialko@marvell.com>

drivers/event/cnxk/cnxk_eventdev_adptr.c
@@ -589,3 +589,37 @@ cnxk_sso_tx_adapter_queue_del(const struct rte_eventdev *event_dev,
 
 	return 0;
 }
+
+int
+cnxk_sso_tx_adapter_start(uint8_t id, const struct rte_eventdev *event_dev)
+{
+	struct cnxk_sso_evdev *dev = cnxk_sso_pmd_priv(event_dev);
+
+	dev->tx_adptr_active_mask |= (1 << id);
+
+	return 0;
+}
+
+int
+cnxk_sso_tx_adapter_stop(uint8_t id, const struct rte_eventdev *event_dev)
+{
+	struct cnxk_sso_evdev *dev = cnxk_sso_pmd_priv(event_dev);
+
+	dev->tx_adptr_active_mask &= ~(1 << id);
+
+	return 0;
+}
+
+int
+cnxk_sso_tx_adapter_free(uint8_t id __rte_unused,
+			 const struct rte_eventdev *event_dev)
+{
+	struct cnxk_sso_evdev *dev = cnxk_sso_pmd_priv(event_dev);
+
+	if (dev->tx_adptr_data_sz && dev->tx_adptr_active_mask == 0) {
+		dev->tx_adptr_data_sz = 0;
+		free(dev->tx_adptr_data);
+	}
+
+	return 0;
+}

drivers/event/cnxk/cn10k_eventdev.c
@@ -943,6 +943,9 @@ static struct eventdev_ops cn10k_sso_dev_ops = {
 	.eth_tx_adapter_caps_get = cn10k_sso_tx_adapter_caps_get,
 	.eth_tx_adapter_queue_add = cn10k_sso_tx_adapter_queue_add,
 	.eth_tx_adapter_queue_del = cn10k_sso_tx_adapter_queue_del,
+	.eth_tx_adapter_start = cnxk_sso_tx_adapter_start,
+	.eth_tx_adapter_stop = cnxk_sso_tx_adapter_stop,
+	.eth_tx_adapter_free = cnxk_sso_tx_adapter_free,
 
 	.timer_adapter_caps_get = cnxk_tim_caps_get,
 

drivers/event/cnxk/cn9k_eventdev.c
@@ -1173,6 +1173,9 @@ static struct eventdev_ops cn9k_sso_dev_ops = {
 	.eth_tx_adapter_caps_get = cn9k_sso_tx_adapter_caps_get,
 	.eth_tx_adapter_queue_add = cn9k_sso_tx_adapter_queue_add,
 	.eth_tx_adapter_queue_del = cn9k_sso_tx_adapter_queue_del,
+	.eth_tx_adapter_start = cnxk_sso_tx_adapter_start,
+	.eth_tx_adapter_stop = cnxk_sso_tx_adapter_stop,
+	.eth_tx_adapter_free = cnxk_sso_tx_adapter_free,
 
 	.timer_adapter_caps_get = cnxk_tim_caps_get,
 

drivers/event/cnxk/cnxk_eventdev.h
@@ -117,6 +117,7 @@ struct cnxk_sso_evdev {
 	uint16_t max_port_id;
 	uint16_t max_queue_id[RTE_MAX_ETHPORTS];
 	uint8_t tx_adptr_configured;
+	uint32_t tx_adptr_active_mask;
 	uint16_t tim_adptr_ring_cnt;
 	uint16_t *timer_adptr_rings;
 	uint64_t *timer_adptr_sz;
@@ -314,5 +315,8 @@ int cnxk_sso_tx_adapter_queue_add(const struct rte_eventdev *event_dev,
 int cnxk_sso_tx_adapter_queue_del(const struct rte_eventdev *event_dev,
 				  const struct rte_eth_dev *eth_dev,
 				  int32_t tx_queue_id);
+int cnxk_sso_tx_adapter_start(uint8_t id, const struct rte_eventdev *event_dev);
+int cnxk_sso_tx_adapter_stop(uint8_t id, const struct rte_eventdev *event_dev);
+int cnxk_sso_tx_adapter_free(uint8_t id, const struct rte_eventdev *event_dev);
 
 #endif /* __CNXK_EVENTDEV_H__ */

commit ef8c531f6d00fcaeffa8e50d089b249d4856f21b

event/cnxk: fix Tx adapter data pointer
Dpdk test application crashes when event inline IPsec test ran for
second time onwards.
In case of event device cleanup, Tx adapter data pointer is free
but not set back to NULL, which causes incomplete
initialization on next run.

Fixes: 6a24c7c4bcd ("event/cnxk: add Tx adapter freeing")

Signed-off-by: Rahul Bhansali <rbhansali@marvell.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/event/cnxk/cnxk_eventdev_adptr.c
@@ -635,6 +635,7 @@ cnxk_sso_tx_adapter_free(uint8_t id __rte_unused,
 	if (dev->tx_adptr_data_sz && dev->tx_adptr_active_mask == 0) {
 		dev->tx_adptr_data_sz = 0;
 		free(dev->tx_adptr_data);
+		dev->tx_adptr_data = NULL;
 	}
 
 	return 0;