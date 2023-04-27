27808f64f66d0286d702fc28cdd1c3b5fdd9c4d9
Qiming Yang <qiming.yang@intel.com>
2023-04-27 06:20:00 +0000

    net/ice/base: introduce a new ID for E810 NIC
    Signed-off-by: Milena Olech <milena.olech@intel.com>
    Signed-off-by: Qiming Yang <qiming.yang@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/ice/base/ice_devids.h
+++ drivers/net/ice/base/ice_devids.h
@@ -6,6 +6,8 @@
 #define _ICE_DEVIDS_H_
 
 /* Device IDs */
+#define ICE_DEV_ID_E822_SI_DFLT		0x1888
+/* Intel(R) Ethernet Connection E823-L for backplane */
 #define ICE_DEV_ID_E823L_BACKPLANE	0x124C
 /* Intel(R) Ethernet Connection E823-L for SFP */
 #define ICE_DEV_ID_E823L_SFP		0x124D
@@ -35,6 +37,7 @@
 #define ICE_SUBDEV_ID_E810T4		0x02EA
 #define ICE_SUBDEV_ID_E810T5		0x0010
 #define ICE_SUBDEV_ID_E810T6		0x0012
+#define ICE_SUBDEV_ID_E810T7		0x0011
 /* Intel(R) Ethernet Controller E810-XXV for backplane */
 #define ICE_DEV_ID_E810_XXV_BACKPLANE	0x1599
 /* Intel(R) Ethernet Controller E810-XXV for QSFP */
@@ -42,8 +45,6 @@
 /* Intel(R) Ethernet Controller E810-XXV for SFP */
 #define ICE_DEV_ID_E810_XXV_SFP		0x159B
 /* Intel(R) Ethernet Connection E823-C for backplane */
-#define ICE_DEV_ID_E822_SI_DFLT		0x1888
-/* Intel(R) Ethernet Connection E823-L for backplane */
 #define ICE_DEV_ID_E823C_BACKPLANE	0x188A
 /* Intel(R) Ethernet Connection E823-C for QSFP */
 #define ICE_DEV_ID_E823C_QSFP		0x188B
