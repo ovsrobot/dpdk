f0a8bd5f0a20580352b7c27eba255f34cb74203a
Volodymyr Fialko <vfialko@marvell.com>
2023-05-24 21:31:13 +0530

    pdcp: add support for status report
    Implement status report generation for PDCP entity.
    
    Signed-off-by: Anoob Joseph <anoobj@marvell.com>
    Signed-off-by: Volodymyr Fialko <vfialko@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/pdcp/pdcp_process.h
+++ lib/pdcp/pdcp_process.h
@@ -7,7 +7,20 @@
 
 #include <rte_pdcp.h>
 
+#include <pdcp_entity.h>
+#include <pdcp_cnt.h>
+
 int
 pdcp_process_func_set(struct rte_pdcp_entity *entity, const struct rte_pdcp_entity_conf *conf);
+
+static inline void
+pdcp_rx_deliv_set(const struct rte_pdcp_entity *entity, uint32_t rx_deliv)
+{
+	struct entity_priv_dl_part *dl = entity_dl_part_get(entity);
+	struct entity_priv *en_priv = entity_priv_get(entity);
+
+	pdcp_cnt_bitmap_range_clear(dl->bitmap, en_priv->state.rx_deliv, rx_deliv);
+	en_priv->state.rx_deliv = rx_deliv;
+}
 
 #endif /* PDCP_PROCESS_H */
