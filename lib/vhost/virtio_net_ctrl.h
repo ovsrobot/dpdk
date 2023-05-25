fd78ca21f6d3ab7ee75a1365e042d4f5d45b5ca3
Maxime Coquelin <maxime.coquelin@redhat.com>
2023-05-25 18:25:40 +0200

    vhost: add control virtqueue support
    In order to support multi-queue with VDUSE, having
    control queue support is required.
    
    This patch adds control queue implementation, it will be
    used later when adding VDUSE support. Only split ring
    layout is supported for now, packed ring support will be
    added later.
    
    Signed-off-by: Maxime Coquelin <maxime.coquelin@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ lib/vhost/virtio_net_ctrl.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2023 Red Hat, Inc.
+ */
+
+#ifndef _VIRTIO_NET_CTRL_H
+#define _VIRTIO_NET_CTRL_H
+
+int virtio_net_ctrl_handle(struct virtio_net *dev);
+
+#endif
