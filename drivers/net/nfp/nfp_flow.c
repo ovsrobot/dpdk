ovsrobot/dpdk

commit fff680eef7f980755f47c4cec9dd3e44cd52be80

net/nfp: support IPv6 NVGRE encap flow action
Add the offload support of encap action of IPv6 NVGRE tunnel.

Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@corigine.com>

drivers/net/nfp/nfp_flow.c
@@ -51,6 +51,10 @@ struct vxlan_data {
 				sizeof(struct rte_ipv4_hdr) + \
 				sizeof(struct rte_flow_item_gre) + \
 				sizeof(rte_be32_t))    /* gre key */
+#define NVGRE_V6_LEN     (sizeof(struct rte_ether_hdr) + \
+				sizeof(struct rte_ipv6_hdr) + \
+				sizeof(struct rte_flow_item_gre) + \
+				sizeof(rte_be32_t))    /* gre key */
 
 /* Process structure associated with a flow item */
 struct nfp_flow_item_proc {
@@ -2867,6 +2871,43 @@ nfp_flow_action_nvgre_encap_v4(struct nfp_app_fw_flower *app_fw_flower,
 }
 
 static int
+nfp_flow_action_nvgre_encap_v6(struct nfp_app_fw_flower *app_fw_flower,
+		char *act_data,
+		char *actions,
+		const struct rte_flow_action_raw_encap *raw_encap,
+		struct nfp_fl_rule_metadata *nfp_flow_meta,
+		struct nfp_fl_tun *tun)
+{
+	uint8_t tos;
+	const struct rte_ether_hdr *eth;
+	const struct rte_flow_item_ipv6 *ipv6;
+	const struct rte_flow_item_gre *gre;
+	struct nfp_fl_act_pre_tun *pre_tun;
+	struct nfp_fl_act_set_tun *set_tun;
+	size_t act_pre_size = sizeof(struct nfp_fl_act_pre_tun);
+	size_t act_set_size = sizeof(struct nfp_fl_act_set_tun);
+
+	eth    = (const struct rte_ether_hdr *)raw_encap->data;
+	ipv6   = (const struct rte_flow_item_ipv6 *)(eth + 1);
+	gre    = (const struct rte_flow_item_gre *)(ipv6 + 1);
+
+	pre_tun = (struct nfp_fl_act_pre_tun *)actions;
+	memset(pre_tun, 0, act_pre_size);
+	nfp_flow_pre_tun_v6_process(pre_tun, ipv6->hdr.dst_addr);
+
+	set_tun = (struct nfp_fl_act_set_tun *)(act_data + act_pre_size);
+	memset(set_tun, 0, act_set_size);
+	tos = (ipv6->hdr.vtc_flow >> RTE_IPV6_HDR_TC_SHIFT) & 0xff;
+	nfp_flow_set_tun_process(set_tun, NFP_FL_TUN_GRE, 0,
+			ipv6->hdr.hop_limits, tos);
+	set_tun->tun_proto = gre->protocol;
+
+	/* Send the tunnel neighbor cmsg to fw */
+	return nfp_flower_add_tun_neigh_v6_encap(app_fw_flower, nfp_flow_meta,
+			tun, eth, ipv6);
+}
+
+static int
 nfp_flow_action_raw_encap(struct nfp_app_fw_flower *app_fw_flower,
 		char *act_data,
 		char *actions,
@@ -2906,6 +2947,10 @@ nfp_flow_action_raw_encap(struct nfp_app_fw_flower *app_fw_flower,
 		break;
 	case NVGRE_V4_LEN:
 		ret = nfp_flow_action_nvgre_encap_v4(app_fw_flower, act_data,
+				actions, raw_encap, nfp_flow_meta, tun);
+		break;
+	case NVGRE_V6_LEN:
+		ret = nfp_flow_action_nvgre_encap_v6(app_fw_flower, act_data,
 				actions, raw_encap, nfp_flow_meta, tun);
 		break;
 	default:

commit b63d62529f2815e91708aa196eb120d3434d2aff

net/nfp: fix VNI of IPv6 NVGRE encap action
Complete the missing logic of parse the tunnel id of IPv6 NVGRE tunnel
and send it to the firmware by control message.

Fixes: fff680eef7f9 ("net/nfp: support IPv6 NVGRE encap flow action")
Cc: stable@dpdk.org

Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
Reviewed-by: Niklas Söderlund <niklas.soderlund@corigine.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/nfp/nfp_flow.c
@@ -3207,6 +3207,7 @@ nfp_flow_action_nvgre_encap_v6(struct nfp_app_fw_flower *app_fw_flower,
 		struct nfp_fl_tun *tun)
 {
 	uint8_t tos;
+	uint64_t tun_id;
 	const struct rte_ether_hdr *eth;
 	const struct rte_flow_item_ipv6 *ipv6;
 	const struct rte_flow_item_gre *gre;
@@ -3218,6 +3219,7 @@ nfp_flow_action_nvgre_encap_v6(struct nfp_app_fw_flower *app_fw_flower,
 	eth    = (const struct rte_ether_hdr *)raw_encap->data;
 	ipv6   = (const struct rte_flow_item_ipv6 *)(eth + 1);
 	gre    = (const struct rte_flow_item_gre *)(ipv6 + 1);
+	tun_id = rte_be_to_cpu_32(*(const rte_be32_t *)(gre + 1));
 
 	pre_tun = (struct nfp_fl_act_pre_tun *)actions;
 	memset(pre_tun, 0, act_pre_size);
@@ -3226,7 +3228,7 @@ nfp_flow_action_nvgre_encap_v6(struct nfp_app_fw_flower *app_fw_flower,
 	set_tun = (struct nfp_fl_act_set_tun *)(act_data + act_pre_size);
 	memset(set_tun, 0, act_set_size);
 	tos = rte_be_to_cpu_32(ipv6->hdr.vtc_flow) >> RTE_IPV6_HDR_TC_SHIFT;
-	nfp_flow_set_tun_process(set_tun, NFP_FL_TUN_GRE, 0,
+	nfp_flow_set_tun_process(set_tun, NFP_FL_TUN_GRE, tun_id,
 			ipv6->hdr.hop_limits, tos);
 	set_tun->tun_proto = gre->protocol;
 