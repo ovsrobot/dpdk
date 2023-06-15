5e94377c1a4e7aa57bf895f058b4d348e8b76d70
Fengnan Chang <changfengnan@bytedance.com>
2023-06-15 15:07:44 +0800

    eal: fix prompt info when remap_segment failed
    When there is not enough space to memsegs, we should prompt
    which configuration should be modified instead of printing
    some numbers.
    
    Signed-off-by: Fengnan Chang <changfengnan@bytedance.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/freebsd/eal_memory.c
+++ lib/eal/freebsd/eal_memory.c
@@ -172,9 +172,8 @@ rte_eal_hugepage_init(void)
 				break;
 			}
 			if (msl_idx == RTE_MAX_MEMSEG_LISTS) {
-				RTE_LOG(ERR, EAL, "Could not find space for memseg. Please increase %s and/or %s in configuration.\n",
-					RTE_STR(RTE_MAX_MEMSEG_PER_TYPE),
-					RTE_STR(RTE_MAX_MEM_MB_PER_TYPE));
+				RTE_LOG(ERR, EAL, "Could not find space for memseg. Please increase RTE_MAX_MEMSEG_PER_LIST "
+					"RTE_MAX_MEMSEG_PER_TYPE and/or RTE_MAX_MEM_MB_PER_TYPE in configuration.\n");
 				return -1;
 			}
 			arr = &msl->memseg_arr;
