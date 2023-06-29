b7111c99a6390aa7ab7f8404e9281e1327d97718
Stephen Hemminger <stephen@networkplumber.org>
2023-06-29 08:58:43 -0700

    eal: add option to put timestamp on console output
    When debugging driver or startup issues, it is useful to have
    a timestamp on each message printed. The messages in syslog
    already have a timestamp, but often syslog is not available
    during testing. The timestamp format is chosen to look
    like the default Linux dmesg timestamp.
    
    The first few lines are not timestamped because the flag is stored
    in internal configuration which is stored in shared memory
    which is not setup up until a little later in startup process.
    
    Example:
    $ dpdk-testpmd --log-timestamp -- -i
    EAL: Detected CPU lcores: 16
    EAL: Detected NUMA nodes: 1
    EAL: Detected static linkage of DPDK
    EAL: Multi-process socket /var/run/dpdk/rte/mp_socket
    EAL: Selected IOVA mode 'VA'
    [       0.112264] testpmd: No probed ethernet devices
    Interactive-mode selected
    [       0.184573] testpmd: create a new mbuf pool <mb_pool_0>: n=163456, size=2176, socket=0
    [       0.184612] testpmd: preferred mempool ops selected: ring_mp_mc
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/unix/eal_log.c
+++ lib/eal/unix/eal_log.c
@@ -4,7 +4,9 @@
 
 #include <stdio.h>
 #include <sys/types.h>
+#include <sys/uio.h>
 #include <syslog.h>
+#include <time.h>
 #include <unistd.h>
 
 #include <rte_log.h>
@@ -17,12 +19,38 @@
  * default log function
  */
 static ssize_t
-console_log_write(__rte_unused void *c, const char *buf, size_t size)
+console_log_write(void *c, const char *buf, size_t size)
 {
+	const struct internal_config *internal_conf = c;
 	ssize_t ret;
 
-	/* write on stderr */
-	ret = write(STDERR_FILENO, buf, size);
+	/* add optional timestamp for stderr */
+	if (internal_conf->log_timestamp) {
+		struct iovec iov[2];
+		struct timespec ts;
+		char tbuf[64];
+
+		/* format up monotonic timestamp */
+		clock_gettime(CLOCK_MONOTONIC, &ts);
+		ts.tv_sec -= internal_conf->log_start_time.tv_sec;
+		ts.tv_nsec -= internal_conf->log_start_time.tv_nsec;
+		if (ts.tv_nsec < 0) {
+			--ts.tv_sec;
+			ts.tv_nsec += 1000000000ul;
+		}
+
+		/* use writev to put timestamp and buf in single operation */
+		iov[0].iov_base = tbuf;
+		iov[0].iov_len  = snprintf(tbuf, sizeof(tbuf), "[%8lu.%06lu] ",
+					   ts.tv_sec, ts.tv_nsec / 1000u);
+
+		/* casts are to unconstify the buf */
+		iov[1].iov_base = (void *)(uintptr_t)buf;
+		iov[1].iov_len = size;
+		ret = writev(STDERR_FILENO, iov, 2);
+	} else {
+		ret = write(STDERR_FILENO, buf, size);
+	}
 
 	/* Syslog error levels are from 0 to 7, so subtract 1 to convert */
 	syslog(rte_log_cur_msg_loglevel() - 1, "%.*s", (int)size, buf);
@@ -58,13 +86,20 @@ static cookie_io_functions_t console_log_func = {
 int
 eal_log_init(const char *id, int facility)
 {
+	struct internal_config *internal_conf = eal_get_internal_configuration();
 	FILE *log_stream;
 
 	/* has user has already setup a log stream */
 	if (eal_log_get_default())
 		return 0;
 
-	log_stream = fopencookie(NULL, "w+", console_log_func);
+	if (internal_conf->process_type == RTE_PROC_PRIMARY &&
+	    internal_conf->log_timestamp) {
+		if (clock_gettime(CLOCK_MONOTONIC, &internal_conf->log_start_time) < 0)
+			return -1;
+	}
+
+	log_stream = fopencookie(internal_conf, "w+", console_log_func);
 	if (log_stream == NULL)
 		return -1;
 
