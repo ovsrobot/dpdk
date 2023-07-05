ovsrobot/dpdk

commit ecb22a294980c86ee3b5be95732e50dac801e79c

graph: introduce graph bind unbind API
Add lcore_id for graph to hold affinity core id where graph would run on.
Add bind/unbind API to set/unset graph affinity attribute. lcore_id will
be set as MAX by default, it means not enable this attribute.

Signed-off-by: Haiyue Wang <haiyue.wang@intel.com>
Signed-off-by: Cunming Liang <cunming.liang@intel.com>
Signed-off-by: Zhirun Yan <zhirun.yan@intel.com>
Acked-by: Jerin Jacob <jerinj@marvell.com>

lib/graph/graph.c
@@ -260,6 +260,65 @@ graph_mem_fixup_secondary(struct rte_graph *graph)
 	return graph_mem_fixup_node_ctx(graph);
 }
 
+static bool
+graph_src_node_avail(struct graph *graph)
+{
+	struct graph_node *graph_node;
+
+	STAILQ_FOREACH(graph_node, &graph->node_list, next)
+		if ((graph_node->node->flags & RTE_NODE_SOURCE_F) &&
+		    (graph_node->node->lcore_id == RTE_MAX_LCORE ||
+		     graph->lcore_id == graph_node->node->lcore_id))
+			return true;
+
+	return false;
+}
+
+int
+rte_graph_model_mcore_dispatch_core_bind(rte_graph_t id, int lcore)
+{
+	struct graph *graph;
+
+	GRAPH_ID_CHECK(id);
+	if (!rte_lcore_is_enabled(lcore))
+		SET_ERR_JMP(ENOLINK, fail, "lcore %d not enabled", lcore);
+
+	STAILQ_FOREACH(graph, &graph_list, next)
+		if (graph->id == id)
+			break;
+
+	if (graph->graph->model == RTE_GRAPH_MODEL_MCORE_DISPATCH)
+		goto fail;
+
+	graph->lcore_id = lcore;
+	graph->socket = rte_lcore_to_socket_id(lcore);
+
+	/* check the availability of source node */
+	if (!graph_src_node_avail(graph))
+		graph->graph->head = 0;
+
+	return 0;
+
+fail:
+	return -rte_errno;
+}
+
+void
+rte_graph_model_mcore_dispatch_core_unbind(rte_graph_t id)
+{
+	struct graph *graph;
+
+	GRAPH_ID_CHECK(id);
+	STAILQ_FOREACH(graph, &graph_list, next)
+		if (graph->id == id)
+			break;
+
+	graph->lcore_id = RTE_MAX_LCORE;
+
+fail:
+	return;
+}
+
 struct rte_graph *
 rte_graph_lookup(const char *name)
 {
@@ -346,6 +405,7 @@ rte_graph_create(const char *name, struct rte_graph_param *prm)
 	graph->src_node_count = src_node_count;
 	graph->node_count = graph_nodes_count(graph);
 	graph->id = graph_id;
+	graph->lcore_id = RTE_MAX_LCORE;
 	graph->num_pkt_to_capture = prm->num_pkt_to_capture;
 	if (prm->pcap_filename)
 		rte_strscpy(graph->pcap_filename, prm->pcap_filename, RTE_GRAPH_PCAP_FILE_SZ);

lib/graph/graph_private.h
@@ -100,6 +100,8 @@ struct graph {
 	/**< Circular buffer mask for wrap around. */
 	rte_graph_t id;
 	/**< Graph identifier. */
+	unsigned int lcore_id;
+	/**< Lcore identifier where the graph prefer to run on. Used for mcore dispatch model. */
 	size_t mem_sz;
 	/**< Memory size of the graph. */
 	int socket;

lib/graph/rte_graph.h
@@ -286,6 +286,28 @@ __rte_experimental
 int rte_graph_export(const char *name, FILE *f);
 
 /**
+ * Bind graph with specific lcore for mcore dispatch model.
+ *
+ * @param id
+ *   Graph id to get the pointer of graph object
+ * @param lcore
+ * The lcore where the graph will run on
+ * @return
+ *   0 on success, error otherwise.
+ */
+__rte_experimental
+int rte_graph_model_mcore_dispatch_core_bind(rte_graph_t id, int lcore);
+
+/**
+ * Unbind graph with lcore for mcore dispatch model
+ *
+ * @param id
+ * Graph id to get the pointer of graph object
+ */
+__rte_experimental
+void rte_graph_model_mcore_dispatch_core_unbind(rte_graph_t id);
+
+/**
  * Get graph object from its name.
  *
  * Typical usage of this API to get graph objects in the worker thread and

lib/graph/version.map
@@ -15,6 +15,8 @@ EXPERIMENTAL {
 	rte_graph_list_dump;
 	rte_graph_max_count;
 	rte_graph_model_is_valid;
+	rte_graph_model_mcore_dispatch_core_bind;
+	rte_graph_model_mcore_dispatch_core_unbind;
 	rte_graph_model_mcore_dispatch_node_lcore_affinity_set;
 	rte_graph_node_get;
 	rte_graph_node_get_by_name;

commit 79532aeaf3b047211751ffb71be68c5855a83c1c

graph: fix graph model check in core binding
Fix graph model check in core binding with graph. And rte_graph_clone()
need to use valid params rather than NULL pointer. Update release notes
for new mcore dispatch model.

Fixes: ecb22a294980 ("graph: introduce graph bind unbind API")
Fixes: 67e2303cd823 ("test/graph: add functional tests for mcore dispatch model")

Signed-off-by: Zhirun Yan <zhirun.yan@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/graph/graph.c
@@ -287,7 +287,7 @@ rte_graph_model_mcore_dispatch_core_bind(rte_graph_t id, int lcore)
 		if (graph->id == id)
 			break;
 
-	if (graph->graph->model == RTE_GRAPH_MODEL_MCORE_DISPATCH)
+	if (graph->graph->model != RTE_GRAPH_MODEL_MCORE_DISPATCH)
 		goto fail;
 
 	graph->lcore_id = lcore;

app/test/test_graph.c
@@ -702,6 +702,7 @@ test_graph_model_mcore_dispatch_node_lcore_affinity_set(void)
 	unsigned int worker_lcore = RTE_MAX_LCORE;
 	rte_node_t nid = RTE_NODE_ID_INVALID;
 	char node_name[64] = "test_node00";
+	struct rte_graph_param graph_conf;
 	struct rte_node *node;
 	int ret = 0;
 
@@ -711,7 +712,7 @@ test_graph_model_mcore_dispatch_node_lcore_affinity_set(void)
 		printf("Set node %s affinity to lcore %u\n", node_name, worker_lcore);
 
 	nid = rte_node_from_name(node_name);
-	cloned_graph_id = rte_graph_clone(graph_id, "cloned-test1", NULL);
+	cloned_graph_id = rte_graph_clone(graph_id, "cloned-test1", &graph_conf);
 	node = rte_graph_node_get(cloned_graph_id, nid);
 
 	if (node->dispatch.lcore_id != worker_lcore) {
@@ -729,11 +730,18 @@ test_graph_model_mcore_dispatch_core_bind_unbind(void)
 {
 	rte_graph_t cloned_graph_id = RTE_GRAPH_ID_INVALID;
 	unsigned int worker_lcore = RTE_MAX_LCORE;
+	struct rte_graph_param graph_conf;
 	struct rte_graph *graph;
 	int ret = 0;
 
 	worker_lcore = rte_get_next_lcore(worker_lcore, true, 1);
-	cloned_graph_id = rte_graph_clone(graph_id, "cloned-test2", NULL);
+	cloned_graph_id = rte_graph_clone(graph_id, "cloned-test2", &graph_conf);
+
+	ret = rte_graph_worker_model_set(RTE_GRAPH_MODEL_MCORE_DISPATCH);
+	if (ret != 0) {
+		printf("Set graph mcore dispatch model failed\n");
+		ret = -1;
+	}
 
 	ret = rte_graph_model_mcore_dispatch_core_bind(cloned_graph_id, worker_lcore);
 	if (ret != 0) {
@@ -765,10 +773,11 @@ static int
 test_graph_worker_model_set_get(void)
 {
 	rte_graph_t cloned_graph_id = RTE_GRAPH_ID_INVALID;
+	struct rte_graph_param graph_conf;
 	struct rte_graph *graph;
 	int ret = 0;
 
-	cloned_graph_id = rte_graph_clone(graph_id, "cloned-test3", NULL);
+	cloned_graph_id = rte_graph_clone(graph_id, "cloned-test3", &graph_conf);
 	ret = rte_graph_worker_model_set(RTE_GRAPH_MODEL_MCORE_DISPATCH);
 	if (ret != 0) {
 		printf("Set graph mcore dispatch model failed\n");