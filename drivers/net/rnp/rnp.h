f59bbb04fcf3652a916312e81ce0ea8026842c59
Wenbo Cao <caowenbo@mucse.com>
2023-08-01 07:21:47 +0000

    2/8] net/rnp: add ethdev probe and remove
    Add basic PCIe ethdev probe and remove.
    
    Signed-off-by: Wenbo Cao <caowenbo@mucse.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/rnp/rnp.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(C) 2023 Mucse IC Design Ltd.
+ */
+#ifndef __RNP_H__
+#define __RNP_H__
+
+#define PCI_VENDOR_ID_MUCSE	(0x8848)
+#define RNP_DEV_ID_N10G		(0x1000)
+
+struct rnp_eth_port {
+} __rte_cache_aligned;
+
+#endif /* __RNP_H__ */
