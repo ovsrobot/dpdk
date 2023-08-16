e2b9db47e6df1219f9a8e3cd83760cb071872cda
Beilei Xing <beilei.xing@intel.com>
2023-08-16 15:05:40 +0000

    net/cpfl: support link update for representor
    Add link update ops for representor.
    
    Signed-off-by: Jingjing Wu <jingjing.wu@intel.com>
    Signed-off-by: Beilei Xing <beilei.xing@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/cpfl/cpfl_representor.c
+++ drivers/net/cpfl/cpfl_representor.c
@@ -285,12 +285,31 @@ cpfl_repr_dev_stop(struct rte_eth_dev *dev)
 	return 0;
 }
 
+static int
+cpfl_repr_link_update(struct rte_eth_dev *ethdev,
+		      __rte_unused int wait_to_complete)
+{
+	struct cpfl_repr *repr = CPFL_DEV_TO_REPR(ethdev);
+	struct rte_eth_link *dev_link = &ethdev->data->dev_link;
+
+	if (!(ethdev->data->dev_flags & RTE_ETH_DEV_REPRESENTOR)) {
+		PMD_INIT_LOG(ERR, "This ethdev is not representor.");
+		return -EINVAL;
+	}
+	dev_link->link_status = repr->func_up ?
+			RTE_ETH_LINK_UP : RTE_ETH_LINK_DOWN;
+
+	return 0;
+}
+
 static const struct eth_dev_ops cpfl_repr_dev_ops = {
 	.dev_start		= cpfl_repr_dev_start,
 	.dev_stop		= cpfl_repr_dev_stop,
 	.dev_configure		= cpfl_repr_dev_configure,
 	.dev_close		= cpfl_repr_dev_close,
 	.dev_infos_get		= cpfl_repr_dev_info_get,
+
+	.link_update		= cpfl_repr_link_update,
 };
 
 static int
@@ -305,6 +324,8 @@ cpfl_repr_init(struct rte_eth_dev *eth_dev, void *init_param)
 	repr->itf.type = CPFL_ITF_TYPE_REPRESENTOR;
 	repr->itf.adapter = adapter;
 	repr->itf.data = eth_dev->data;
+	if (repr->vport_info->vport_info.vport_status == CPCHNL2_VPORT_STATUS_ENABLED)
+		repr->func_up = true;
 
 	eth_dev->dev_ops = &cpfl_repr_dev_ops;
 
