a7f3e089c4d05f6c7338f0c6e4aeeca9ab6de9f7
Stephen Hemminger <stephen@networkplumber.org>
2023-08-17 00:28:12 -0400

    eal: cleanup plugins data
    When rte_eal_cleanup is called walk through the list of shared
    objects loaded, and close them and free the data structure.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: Sinan Kaya <okaya@kernel.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/linux/eal.c
+++ lib/eal/linux/eal.c
@@ -1375,6 +1375,8 @@ rte_eal_cleanup(void)
 	eal_trace_fini();
 	eal_mp_dev_hotplug_cleanup();
 	rte_eal_alarm_cleanup();
+	eal_plugins_cleanup();
+
 	/* after this point, any DPDK pointers will become dangling */
 	rte_eal_memory_detach();
 	rte_eal_malloc_heap_cleanup();
