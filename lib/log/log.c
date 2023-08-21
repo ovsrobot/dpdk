e68ec2294c407090fd47f9c2b26b69d42ea13742
Stephen Hemminger <stephen@networkplumber.org>
2023-08-21 09:09:09 -0700

    power: convert RTE_LOGTYPE_POWER to dynamic type
    Use dynamic log type for power library.
    Also replace use of RTE_LOGTYPE_USER1 with lib.power.guest.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Acked-by: David Hunt <david.hunt@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/log/log.c
+++ lib/log/log.c
@@ -357,7 +357,6 @@ static const struct logtype logtype_strings[] = {
 	{RTE_LOGTYPE_PMD,        "pmd"},
 	{RTE_LOGTYPE_HASH,       "lib.hash"},
 	{RTE_LOGTYPE_LPM,        "lib.lpm"},
-	{RTE_LOGTYPE_POWER,      "lib.power"},
 	{RTE_LOGTYPE_METER,      "lib.meter"},
 	{RTE_LOGTYPE_SCHED,      "lib.sched"},
 	{RTE_LOGTYPE_PORT,       "lib.port"},
