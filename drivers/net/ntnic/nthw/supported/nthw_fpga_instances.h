f5684688bd81b1b0fb26819e38337b248f58129b
Christian Koue Muf <ckm@napatech.com>
2023-08-21 15:54:19 +0200

    net/ntnic: initial commit which adds register defines
    The NTNIC PMD does not rely on a kernel space Napatech driver,
    thus all defines related to the register layout is part of the PMD
    code, which will be added in later commits.
    
    Signed-off-by: Christian Koue Muf <ckm@napatech.com>
    Reviewed-by: Mykola Kostenok <mko-plv@napatech.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/ntnic/nthw/supported/nthw_fpga_instances.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2023 Napatech A/S
+ */
+
+#ifndef NTHW_FPGA_INSTANCES_H
+#define NTHW_FPGA_INSTANCES_H
+
+#include "fpga_model.h"
+
+extern nt_fpga_prod_init_t *nthw_fpga_instances[];
+
+extern nt_fpga_prod_init_t nthw_fpga_9563_055_024_0000;
+
+#endif /* NTHW_FPGA_INSTANCES_H */
