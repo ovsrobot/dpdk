64ff58ef77c8fb3704fb8863d90efdf93b0a0664
Renyong Wan <wanry@3snic.com>
2023-08-29 15:58:00 +0800

    net/sssnic: support probe and remove
    Register device ID for 3SNIC ethernet adapter to support
    PCI ethdev probe and remove.
    
    Signed-off-by: Steven Song <steven.song@3snic.com>
    Signed-off-by: Renyong Wan <wanry@3snic.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/sssnic/base/sssnic_hw.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2018-2022 Shenzhen 3SNIC Information Technology Co., Ltd.
+ */
+
+#ifndef _SSSNIC_HW_H_
+#define _SSSNIC_HW_H_
+
+#define SSSNIC_PCI_VENDOR_ID 0x1F3F
+#define SSSNIC_DEVICE_ID_STD 0x9020
+
+#endif /* _SSSNIC_HW_H_ */
--- drivers/net/sssnic/sssnic_ethdev.c
+++ drivers/net/sssnic/sssnic_ethdev.c
@@ -7,25 +7,54 @@
 #include <ethdev_pci.h>
 
 #include "sssnic_log.h"
+#include "base/sssnic_hw.h"
+
+static int
+sssnic_ethdev_init(struct rte_eth_dev *ethdev)
+{
+	RTE_SET_USED(ethdev);
+	PMD_INIT_FUNC_TRACE();
+
+	return -EINVAL;
+}
+
+static int
+sssnic_ethdev_uninit(struct rte_eth_dev *ethdev)
+{
+	RTE_SET_USED(ethdev);
+	PMD_INIT_FUNC_TRACE();
+
+	if (rte_eal_process_type() != RTE_PROC_PRIMARY)
+		return 0;
+
+	return -EINVAL;
+}
 
 static int
 sssnic_pci_probe(struct rte_pci_driver *pci_drv, struct rte_pci_device *pci_dev)
 {
 	RTE_SET_USED(pci_drv);
-	RTE_SET_USED(pci_dev);
 	PMD_INIT_FUNC_TRACE();
-	return -EINVAL;
+
+	return rte_eth_dev_pci_generic_probe(pci_dev, 0, sssnic_ethdev_init);
 }
 
 static int
 sssnic_pci_remove(struct rte_pci_device *pci_dev)
 {
-	RTE_SET_USED(pci_dev);
 	PMD_INIT_FUNC_TRACE();
-	return -EINVAL;
+
+	return rte_eth_dev_pci_generic_remove(pci_dev, sssnic_ethdev_uninit);
 }
 
+static const struct rte_pci_id sssnic_pci_id_map[] = {
+	{ RTE_PCI_DEVICE(SSSNIC_PCI_VENDOR_ID, SSSNIC_DEVICE_ID_STD) },
+	{ .vendor_id = 0 },
+};
+
 static struct rte_pci_driver sssnic_pmd = {
+	.id_table = sssnic_pci_id_map,
+	.drv_flags = RTE_PCI_DRV_NEED_MAPPING,
 	.probe = sssnic_pci_probe,
 	.remove = sssnic_pci_remove,
 };
