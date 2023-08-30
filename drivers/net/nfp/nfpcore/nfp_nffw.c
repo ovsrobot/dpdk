5e7c7d52a64276a78b7b7236f582ab7ec1926679
Chaoyong He <chaoyong.he@corigine.com>
2023-08-30 10:14:36 +0800

    net/nfp: adjust the log statement
    Add log statement to the important control logic, which means
    more strict check of the return value of function call.
    Also remove some verbose info log statement.
    
    Signed-off-by: Chaoyong He <chaoyong.he@corigine.com>
    Reviewed-by: Niklas Söderlund <niklas.soderlund@corigine.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/nfp/nfpcore/nfp_nffw.c
+++ drivers/net/nfp/nfpcore/nfp_nffw.c
@@ -3,6 +3,7 @@
  * All rights reserved.
  */
 
+#include "../nfp_logs.h"
 #include "nfp_cpp.h"
 #include "nfp_nffw.h"
 #include "nfp_mip.h"
@@ -131,8 +132,10 @@ nfp_nffw_info_open(struct nfp_cpp *cpp)
 	memset(state, 0, sizeof(*state));
 
 	state->res = nfp_resource_acquire(cpp, NFP_RESOURCE_NFP_NFFW);
-	if (state->res == NULL)
+	if (state->res == NULL) {
+		PMD_DRV_LOG(ERR, "NFFW - acquire resource failed");
 		goto err_free;
+	}
 
 	fwinf = &state->fwinf;
 
@@ -142,8 +145,10 @@ nfp_nffw_info_open(struct nfp_cpp *cpp)
 	err = nfp_cpp_read(cpp, nfp_resource_cpp_id(state->res),
 			nfp_resource_address(state->res),
 			fwinf, sizeof(*fwinf));
-	if (err < (int)sizeof(*fwinf))
+	if (err < (int)sizeof(*fwinf)) {
+		PMD_DRV_LOG(ERR, "NFFW - CPP read error %d", err);
 		goto err_release;
+	}
 
 	if (nffw_res_flg_init_get(fwinf) == 0)
 		goto err_release;
