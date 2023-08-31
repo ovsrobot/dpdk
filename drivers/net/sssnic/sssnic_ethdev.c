8d5f07b9d37a1fd522596a4ee1e811286756ad8d
Renyong Wan <wanry@3snic.com>
2023-08-31 17:56:19 +0800

    net/sssnic: add build and doc infrastructure
    Adding minimum PMD code, doc and build infrastructure for sssnic.
    
    Signed-off-by: Steven Song <steven.song@3snic.com>
    Signed-off-by: Renyong Wan <wanry@3snic.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/sssnic/sssnic_ethdev.c
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2018-2022 Shenzhen 3SNIC Information Technology Co., Ltd.
+ */
+
+#include <rte_common.h>
+#include <ethdev_pci.h>
+
+static int
+sssnic_pci_probe(struct rte_pci_driver *pci_drv, struct rte_pci_device *pci_dev)
+{
+	RTE_SET_USED(pci_drv);
+	RTE_SET_USED(pci_dev);
+	return -EINVAL;
+}
+
+static int
+sssnic_pci_remove(struct rte_pci_device *pci_dev)
+{
+	RTE_SET_USED(pci_dev);
+	return -EINVAL;
+}
+
+static struct rte_pci_driver sssnic_pmd = {
+	.probe = sssnic_pci_probe,
+	.remove = sssnic_pci_remove,
+};
+
+RTE_PMD_REGISTER_PCI(net_sssnic, sssnic_pmd);
