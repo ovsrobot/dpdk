2726419f40eaa09e92d30f2e27bbe6350298ff53
Renyong Wan <wanry@3snic.com>
2023-09-01 11:28:12 +0800

    net/sssnic: add log type and log macros
    Adding log macros to print runtime messages and trace functions.
    
    Signed-off-by: Steven Song <steven.song@3snic.com>
    Signed-off-by: Renyong Wan <wanry@3snic.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/sssnic/sssnic_ethdev.c
+++ drivers/net/sssnic/sssnic_ethdev.c
@@ -5,11 +5,14 @@
 #include <rte_common.h>
 #include <ethdev_pci.h>
 
+#include "sssnic_log.h"
+
 static int
 sssnic_pci_probe(struct rte_pci_driver *pci_drv, struct rte_pci_device *pci_dev)
 {
 	RTE_SET_USED(pci_drv);
 	RTE_SET_USED(pci_dev);
+	PMD_INIT_FUNC_TRACE();
 	return -EINVAL;
 }
 
@@ -17,6 +20,7 @@ static int
 sssnic_pci_remove(struct rte_pci_device *pci_dev)
 {
 	RTE_SET_USED(pci_dev);
+	PMD_INIT_FUNC_TRACE();
 	return -EINVAL;
 }
 
@@ -26,3 +30,12 @@ static struct rte_pci_driver sssnic_pmd = {
 };
 
 RTE_PMD_REGISTER_PCI(net_sssnic, sssnic_pmd);
+
+RTE_LOG_REGISTER_SUFFIX(sssnic_logtype_driver, driver, INFO);
+RTE_LOG_REGISTER_SUFFIX(sssnic_logtype_init, init, NOTICE);
+#ifdef RTE_ETHDEV_DEBUG_RX
+RTE_LOG_REGISTER_SUFFIX(sssnic_logtype_rx, rx, DEBUG);
+#endif /*RTE_ETHDEV_DEBUG_RX*/
+#ifdef RTE_ETHDEV_DEBUG_TX
+RTE_LOG_REGISTER_SUFFIX(sssnic_logtype_tx, tx, DEBUG);
+#endif /*RTE_ETHDEV_DEBUG_TX*/
