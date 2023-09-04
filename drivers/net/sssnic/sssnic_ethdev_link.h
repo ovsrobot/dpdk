125bc158f796971ca032f290dc29cec6704c3f66
Renyong Wan <wanry@3snic.com>
2023-09-04 12:56:38 +0800

    net/sssnic: support dev link status
    Signed-off-by: Steven Song <steven.song@3snic.com>
    Signed-off-by: Renyong Wan <wanry@3snic.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/sssnic/sssnic_ethdev_link.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2018-2022 Shenzhen 3SNIC Information Technology Co., Ltd.
+ */
+
+#ifndef _SSSNIC_ETHDEV_LINK_H_
+#define _SSSNIC_ETHDEV_LINK_H_
+
+int sssnic_ethdev_set_link_up(struct rte_eth_dev *ethdev);
+int sssnic_ethdev_set_link_down(struct rte_eth_dev *ethdev);
+int sssnic_ethdev_link_update(struct rte_eth_dev *ethdev, int wait_to_complete);
+
+#endif /* _SSSNIC_ETHDEV_LINK_H_ */
