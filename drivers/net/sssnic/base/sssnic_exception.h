27601cb3c87d6badcf0cc0ddc44c8bc7107a403d
Renyong Wan <wanry@3snic.com>
2023-09-04 12:56:39 +0800

    net/sssnic: support link status event
    Signed-off-by: Steven Song <steven.song@3snic.com>
    Signed-off-by: Renyong Wan <wanry@3snic.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/sssnic/base/sssnic_exception.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2018-2022 Shenzhen 3SNIC Information Technology Co., Ltd.
+ */
+
+#ifndef _SSSNIC_EXCEPTION_H_
+#define _SSSNIC_EXCEPTION_H_
+
+int sssnic_exception_process_init(struct sssnic_hw *hw);
+
+#endif /* _SSSNIC_EXCEPTION_H_ */
