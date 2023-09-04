6b82856466b79559081e82ed58dd6cafcaa75e7a
Morten Brørup <mb@smartsharesystems.com>
2023-09-04 10:43:49 +0200

    clarify purpose of empty cache lines
    This patch introduces the generic RTE_CACHE_GUARD macro into the EAL, and
    replaces vaguely described empty cache lines in the rte_ring structure
    with this macro.
    
    Although the implementation of the rte_ring structure assumes that the
    hardware speculatively prefetches 1 cache line, this number can be changed
    at build time by modifying RTE_CACHE_GUARD_LINES in rte_config.h.
    
    The background and the RFC was discussed in this thread:
    http://inbox.dpdk.org/dev/98CBD80474FA8B44BF855DF32C47DC35D87B39@smartserver.smartshare.dk/
    
    Signed-off-by: Morten Brørup <mb@smartsharesystems.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- config/rte_config.h
+++ config/rte_config.h
@@ -37,6 +37,7 @@
 #define RTE_MAX_TAILQ 32
 #define RTE_LOG_DP_LEVEL RTE_LOG_INFO
 #define RTE_MAX_VFIO_CONTAINERS 64
+#define RTE_CACHE_GUARD_LINES 1
 
 /* bsd module defines */
 #define RTE_CONTIGMEM_MAX_NUM_BUFS 64
