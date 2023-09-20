7bc24982c223d8f125820936f4ae505f0d41d2d9
Srikanth Yalavarthi <syalavarthi@marvell.com>
2023-09-20 00:25:05 -0700

    ml/cnxk: update device debug functions
    Added cnxk wrapper for device dump and selftest debug
    functions.
    
    Signed-off-by: Srikanth Yalavarthi <syalavarthi@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/ml/cnxk/cn10k_ml_ocm.c
+++ drivers/ml/cnxk/cn10k_ml_ocm.c
@@ -483,19 +483,15 @@ cn10k_ml_ocm_pagemask_to_str(struct cn10k_ml_ocm_tile_info *tile_info, uint16_t 
 }
 
 void
-cn10k_ml_ocm_print(struct rte_ml_dev *dev, FILE *fp)
+cn10k_ml_ocm_print(struct cnxk_ml_dev *cnxk_mldev, FILE *fp)
 {
-	struct cn10k_ml_dev *cn10k_mldev;
-	struct cnxk_ml_dev *cnxk_mldev;
 	struct cn10k_ml_ocm *ocm;
 	uint8_t tile_id;
 	uint8_t word_id;
 	int wb_pages;
 	char *str;
 
-	cnxk_mldev = dev->data->dev_private;
-	cn10k_mldev = &cnxk_mldev->cn10k_mldev;
-	ocm = &cn10k_mldev->ocm;
+	ocm = &cnxk_mldev->cn10k_mldev.ocm;
 
 	/* Nibbles + prefix '0x' */
 	str = rte_zmalloc("ocm_mask_str", ocm->num_pages / 4 + 2, RTE_CACHE_LINE_SIZE);
@@ -510,8 +506,7 @@ cn10k_ml_ocm_print(struct rte_ml_dev *dev, FILE *fp)
 
 		wb_pages = 0 - ocm->tile_ocm_info[tile_id].scratch_pages;
 		for (word_id = 0; word_id < ocm->mask_words; word_id++)
-			wb_pages +=
-				rte_popcount32(ocm->tile_ocm_info[tile_id].ocm_mask[word_id]);
+			wb_pages += rte_popcount32(ocm->tile_ocm_info[tile_id].ocm_mask[word_id]);
 
 		fprintf(fp,
 			"tile = %2u, scratch_pages = %4u,"
