ff77ad85d89c8965136bddae8f18c237e84facff
Prince Takkar <ptakkar@marvell.com>
2023-09-20 00:25:23 -0700

    ml/cnxk: support quantize and dequantize callback
    Added support for quantize and dequantize callback
    functions for TVM models.
    
    Signed-off-by: Prince Takkar <ptakkar@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/ml/cnxk/mvtvm_ml_ops.h
+++ drivers/ml/cnxk/mvtvm_ml_ops.h
@@ -21,5 +21,9 @@ int mvtvm_ml_model_load(struct cnxk_ml_dev *cnxk_mldev, struct rte_ml_model_para
 int mvtvm_ml_model_unload(struct cnxk_ml_dev *cnxk_mldev, struct cnxk_ml_model *model);
 int mvtvm_ml_model_start(struct cnxk_ml_dev *cnxk_mldev, struct cnxk_ml_model *model);
 int mvtvm_ml_model_stop(struct cnxk_ml_dev *cnxk_mldev, struct cnxk_ml_model *model);
+int mvtvm_ml_io_quantize(void *device, uint16_t model_id, const char *layer_name,
+			 const DLTensor **deq_tensor, void *qbuffer);
+int mvtvm_ml_io_dequantize(void *device, uint16_t model_id, const char *layer_name, void *qbuffer,
+			   const DLTensor **deq_tensor);
 
 #endif /* _MVTVM_ML_OPS_H_ */
