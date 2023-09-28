ovsrobot/dpdk

commit 7514d76d407beafef3b70997fcf2a8e8720ab235

net/idpf: add basic statistics
This patch add hardware packets/bytes statistics.

Signed-off-by: Mingxia Liu <mingxia.liu@intel.com>
Reviewed-by: Jingjing Wu <jingjing.wu@intel.com>

drivers/net/idpf/idpf_ethdev.c
@@ -140,6 +140,87 @@ idpf_dev_supported_ptypes_get(struct rte_eth_dev *dev __rte_unused)
 	return ptypes;
 }
 
+static uint64_t
+idpf_get_mbuf_alloc_failed_stats(struct rte_eth_dev *dev)
+{
+	uint64_t mbuf_alloc_failed = 0;
+	struct idpf_rx_queue *rxq;
+	int i = 0;
+
+	for (i = 0; i < dev->data->nb_rx_queues; i++) {
+		rxq = dev->data->rx_queues[i];
+		mbuf_alloc_failed += __atomic_load_n(&rxq->rx_stats.mbuf_alloc_failed,
+						     __ATOMIC_RELAXED);
+	}
+
+	return mbuf_alloc_failed;
+}
+
+static int
+idpf_dev_stats_get(struct rte_eth_dev *dev, struct rte_eth_stats *stats)
+{
+	struct idpf_vport *vport =
+		(struct idpf_vport *)dev->data->dev_private;
+	struct virtchnl2_vport_stats *pstats = NULL;
+	int ret;
+
+	ret = idpf_vc_stats_query(vport, &pstats);
+	if (ret == 0) {
+		uint8_t crc_stats_len = (dev->data->dev_conf.rxmode.offloads &
+					 RTE_ETH_RX_OFFLOAD_KEEP_CRC) ? 0 :
+					 RTE_ETHER_CRC_LEN;
+
+		idpf_vport_stats_update(&vport->eth_stats_offset, pstats);
+		stats->ipackets = pstats->rx_unicast + pstats->rx_multicast +
+				pstats->rx_broadcast - pstats->rx_discards;
+		stats->opackets = pstats->tx_broadcast + pstats->tx_multicast +
+						pstats->tx_unicast;
+		stats->imissed = pstats->rx_discards;
+		stats->oerrors = pstats->tx_errors + pstats->tx_discards;
+		stats->ibytes = pstats->rx_bytes;
+		stats->ibytes -= stats->ipackets * crc_stats_len;
+		stats->obytes = pstats->tx_bytes;
+
+		dev->data->rx_mbuf_alloc_failed = idpf_get_mbuf_alloc_failed_stats(dev);
+		stats->rx_nombuf = dev->data->rx_mbuf_alloc_failed;
+	} else {
+		PMD_DRV_LOG(ERR, "Get statistics failed");
+	}
+	return ret;
+}
+
+static void
+idpf_reset_mbuf_alloc_failed_stats(struct rte_eth_dev *dev)
+{
+	struct idpf_rx_queue *rxq;
+	int i;
+
+	for (i = 0; i < dev->data->nb_rx_queues; i++) {
+		rxq = dev->data->rx_queues[i];
+		__atomic_store_n(&rxq->rx_stats.mbuf_alloc_failed, 0, __ATOMIC_RELAXED);
+	}
+}
+
+static int
+idpf_dev_stats_reset(struct rte_eth_dev *dev)
+{
+	struct idpf_vport *vport =
+		(struct idpf_vport *)dev->data->dev_private;
+	struct virtchnl2_vport_stats *pstats = NULL;
+	int ret;
+
+	ret = idpf_vc_stats_query(vport, &pstats);
+	if (ret != 0)
+		return ret;
+
+	/* set stats offset base on current values */
+	vport->eth_stats_offset = *pstats;
+
+	idpf_reset_mbuf_alloc_failed_stats(dev);
+
+	return 0;
+}
+
 static int
 idpf_init_rss(struct idpf_vport *vport)
 {
@@ -326,6 +407,9 @@ idpf_dev_start(struct rte_eth_dev *dev)
 		PMD_DRV_LOG(ERR, "Failed to enable vport");
 		goto err_vport;
 	}
+
+	if (idpf_dev_stats_reset(dev))
+		PMD_DRV_LOG(ERR, "Failed to reset stats");
 
 	vport->stopped = 0;
 
@@ -606,6 +690,8 @@ static const struct eth_dev_ops idpf_eth_dev_ops = {
 	.tx_queue_release		= idpf_dev_tx_queue_release,
 	.mtu_set			= idpf_dev_mtu_set,
 	.dev_supported_ptypes_get	= idpf_dev_supported_ptypes_get,
+	.stats_get			= idpf_dev_stats_get,
+	.stats_reset			= idpf_dev_stats_reset,
 };
 
 static uint16_t

drivers/common/idpf/idpf_common_device.c
@@ -652,4 +652,21 @@ idpf_vport_info_init(struct idpf_vport *vport,
 	return 0;
 }
 
+void
+idpf_vport_stats_update(struct virtchnl2_vport_stats *oes, struct virtchnl2_vport_stats *nes)
+{
+	nes->rx_bytes = nes->rx_bytes - oes->rx_bytes;
+	nes->rx_unicast = nes->rx_unicast - oes->rx_unicast;
+	nes->rx_multicast = nes->rx_multicast - oes->rx_multicast;
+	nes->rx_broadcast = nes->rx_broadcast - oes->rx_broadcast;
+	nes->rx_errors = nes->rx_errors - oes->rx_errors;
+	nes->rx_discards = nes->rx_discards - oes->rx_discards;
+	nes->tx_bytes = nes->tx_bytes - oes->tx_bytes;
+	nes->tx_unicast = nes->tx_unicast - oes->tx_unicast;
+	nes->tx_multicast = nes->tx_multicast - oes->tx_multicast;
+	nes->tx_broadcast = nes->tx_broadcast - oes->tx_broadcast;
+	nes->tx_errors = nes->tx_errors - oes->tx_errors;
+	nes->tx_discards = nes->tx_discards - oes->tx_discards;
+}
+
 RTE_LOG_REGISTER_SUFFIX(idpf_common_logtype, common, NOTICE);

drivers/common/idpf/idpf_common_device.h
@@ -115,6 +115,8 @@ struct idpf_vport {
 	bool tx_vec_allowed;
 	bool rx_use_avx512;
 	bool tx_use_avx512;
+
+	struct virtchnl2_vport_stats eth_stats_offset;
 };
 
 /* Message type read in virtual channel from PF */
@@ -191,5 +193,7 @@ int idpf_vport_irq_unmap_config(struct idpf_vport *vport, uint16_t nb_rx_queues)
 __rte_internal
 int idpf_vport_info_init(struct idpf_vport *vport,
 			 struct virtchnl2_create_vport *vport_info);
+__rte_internal
+void idpf_vport_stats_update(struct virtchnl2_vport_stats *oes, struct virtchnl2_vport_stats *nes);
 
 #endif /* _IDPF_COMMON_DEVICE_H_ */

drivers/common/idpf/idpf_common_virtchnl.c
@@ -217,6 +217,7 @@ idpf_vc_cmd_execute(struct idpf_adapter *adapter, struct idpf_cmd_info *args)
 	case VIRTCHNL2_OP_UNMAP_QUEUE_VECTOR:
 	case VIRTCHNL2_OP_ALLOC_VECTORS:
 	case VIRTCHNL2_OP_DEALLOC_VECTORS:
+	case VIRTCHNL2_OP_GET_STATS:
 		/* for init virtchnl ops, need to poll the response */
 		err = idpf_vc_one_msg_read(adapter, args->ops, args->out_size, args->out_buffer);
 		clear_cmd(adapter);
@@ -804,6 +805,32 @@ idpf_vc_ptype_info_query(struct idpf_adapter *adapter)
 
 	rte_free(ptype_info);
 	return err;
+}
+
+int
+idpf_vc_stats_query(struct idpf_vport *vport,
+		struct virtchnl2_vport_stats **pstats)
+{
+	struct idpf_adapter *adapter = vport->adapter;
+	struct virtchnl2_vport_stats vport_stats;
+	struct idpf_cmd_info args;
+	int err;
+
+	vport_stats.vport_id = vport->vport_id;
+	args.ops = VIRTCHNL2_OP_GET_STATS;
+	args.in_args = (u8 *)&vport_stats;
+	args.in_args_size = sizeof(vport_stats);
+	args.out_buffer = adapter->mbx_resp;
+	args.out_size = IDPF_DFLT_MBX_BUF_SIZE;
+
+	err = idpf_vc_cmd_execute(adapter, &args);
+	if (err) {
+		DRV_LOG(ERR, "Failed to execute command of VIRTCHNL2_OP_GET_STATS");
+		*pstats = NULL;
+		return err;
+	}
+	*pstats = (struct virtchnl2_vport_stats *)args.out_buffer;
+	return 0;
 }
 
 #define IDPF_RX_BUF_STRIDE		64

drivers/common/idpf/idpf_common_virtchnl.h
@@ -49,4 +49,7 @@ __rte_internal
 int idpf_vc_rxq_config(struct idpf_vport *vport, struct idpf_rx_queue *rxq);
 __rte_internal
 int idpf_vc_txq_config(struct idpf_vport *vport, struct idpf_tx_queue *txq);
+__rte_internal
+int idpf_vc_stats_query(struct idpf_vport *vport,
+			struct virtchnl2_vport_stats **pstats);
 #endif /* _IDPF_COMMON_VIRTCHNL_H_ */

drivers/common/idpf/version.map
@@ -46,6 +46,7 @@ INTERNAL {
 	idpf_vc_rss_key_set;
 	idpf_vc_rss_lut_set;
 	idpf_vc_rxq_config;
+	idpf_vc_stats_query;
 	idpf_vc_txq_config;
 	idpf_vc_vectors_alloc;
 	idpf_vc_vectors_dealloc;
@@ -59,6 +60,7 @@ INTERNAL {
 	idpf_vport_irq_map_config;
 	idpf_vport_irq_unmap_config;
 	idpf_vport_rss_config;
+	idpf_vport_stats_update;
 
 	local: *;
 };

commit a3b793e4adc0e6d218b355dc3473c38a22e24377

net/idpf: fix incorrect status calculation
Fix the incorrect ingress packet number calculation.

Fixes: 7514d76d407b ("net/idpf: add basic statistics")
Cc: stable@dpdk.org

Signed-off-by: Yuying Zhang <yuying.zhang@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/idpf/idpf_ethdev.c
@@ -281,7 +281,7 @@ idpf_dev_stats_get(struct rte_eth_dev *dev, struct rte_eth_stats *stats)
 
 		idpf_vport_stats_update(&vport->eth_stats_offset, pstats);
 		stats->ipackets = pstats->rx_unicast + pstats->rx_multicast +
-				pstats->rx_broadcast - pstats->rx_discards;
+				pstats->rx_broadcast;
 		stats->opackets = pstats->tx_broadcast + pstats->tx_multicast +
 						pstats->tx_unicast;
 		stats->ierrors = pstats->rx_errors;