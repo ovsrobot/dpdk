ovsrobot/dpdk

commit 14a4aa9eae71de092bcc93be3be998979421f6c3

event/cnxk: support get remaining ticks
Add support to get the remaining ticks to expire for a
given event timer.

Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>

drivers/event/cnxk/cnxk_tim_worker.c
@@ -171,3 +171,38 @@ cnxk_tim_timer_cancel_burst(const struct rte_event_timer_adapter *adptr,
 
 	return index;
 }
+
+int
+cnxk_tim_remaining_ticks_get(const struct rte_event_timer_adapter *adapter,
+			     const struct rte_event_timer *evtim, uint64_t *ticks_remaining)
+{
+	struct cnxk_tim_ring *tim_ring = adapter->data->adapter_priv;
+	struct cnxk_tim_bkt *bkt, *current_bkt;
+	struct cnxk_tim_ent *entry;
+	uint64_t bkt_cyc, bucket;
+	uint64_t sema;
+
+	if (evtim->impl_opaque[1] == 0 || evtim->impl_opaque[0] == 0)
+		return -ENOENT;
+
+	entry = (struct cnxk_tim_ent *)(uintptr_t)evtim->impl_opaque[0];
+	if (entry->wqe != evtim->ev.u64)
+		return -ENOENT;
+
+	if (evtim->state != RTE_EVENT_TIMER_ARMED)
+		return -ENOENT;
+
+	bkt = (struct cnxk_tim_bkt *)evtim->impl_opaque[1];
+	sema = rte_atomic_load_explicit(&bkt->w1, rte_memory_order_acquire);
+	if (cnxk_tim_bkt_get_hbt(sema) || !cnxk_tim_bkt_get_nent(sema))
+		return -ENOENT;
+
+	bkt_cyc = tim_ring->tick_fn(tim_ring->tbase) - tim_ring->ring_start_cyc;
+	bucket = rte_reciprocal_divide_u64(bkt_cyc, &tim_ring->fast_div);
+	current_bkt = &tim_ring->bkt[bucket];
+
+	*ticks_remaining = RTE_MAX(bkt, current_bkt) - RTE_MIN(bkt, current_bkt);
+	/* Assume that the current bucket is yet to expire */
+	*ticks_remaining += 1;
+	return 0;
+}

drivers/event/cnxk/cnxk_tim_evdev.c
@@ -392,6 +392,7 @@ cnxk_tim_caps_get(const struct rte_eventdev *evdev, uint64_t flags,
 	cnxk_tim_ops.start = cnxk_tim_ring_start;
 	cnxk_tim_ops.stop = cnxk_tim_ring_stop;
 	cnxk_tim_ops.get_info = cnxk_tim_ring_info_get;
+	cnxk_tim_ops.remaining_ticks_get = cnxk_tim_remaining_ticks_get;
 	sso_set_priv_mem_fn = priv_mem_fn;
 
 	if (dev->enable_stats) {

drivers/event/cnxk/cnxk_tim_evdev.h
@@ -320,6 +320,9 @@ cnxk_tim_timer_cancel_burst(const struct rte_event_timer_adapter *adptr,
 			    struct rte_event_timer **tim,
 			    const uint16_t nb_timers);
 
+int cnxk_tim_remaining_ticks_get(const struct rte_event_timer_adapter *adapter,
+				 const struct rte_event_timer *evtim, uint64_t *ticks_remaining);
+
 int cnxk_tim_caps_get(const struct rte_eventdev *dev, uint64_t flags,
 		      uint32_t *caps,
 		      const struct event_timer_adapter_ops **ops,

commit 920322c74b7eb8ef8a14434363677478f7ae2bef

event/cnxk: remove single use of rte stdatomic
The variable operated on by the single use of rte stdatomic was not
RTE_ATOMIC(T) specified. Remove the use of stdatomic for now to fix
LLVM build with enable_stdatomic=true. event/cnxk will be converted
to rte stdatomic in a later series.

Fixes: 14a4aa9eae71 ("event/cnxk: support get remaining ticks")
Cc: pbhagavatula@marvell.com

Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/event/cnxk/cnxk_tim_worker.c
@@ -193,7 +193,7 @@ cnxk_tim_remaining_ticks_get(const struct rte_event_timer_adapter *adapter,
 		return -ENOENT;
 
 	bkt = (struct cnxk_tim_bkt *)evtim->impl_opaque[1];
-	sema = rte_atomic_load_explicit(&bkt->w1, rte_memory_order_acquire);
+	sema = __atomic_load_n(&bkt->w1, rte_memory_order_acquire);
 	if (cnxk_tim_bkt_get_hbt(sema) || !cnxk_tim_bkt_get_nent(sema))
 		return -ENOENT;
 