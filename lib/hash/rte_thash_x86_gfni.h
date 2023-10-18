ovsrobot/dpdk

commit 3d4e27fd7ff050d565c7450930c92fb945706518

use abstracted bit count functions
Now that DPDK provides such bit count functions, make use of them.

This patch was prepared with a "brutal" commandline:

$ old=__builtin_clzll; new=rte_clz64;
  git grep -lw $old :^lib/eal/include/rte_bitops.h |
  xargs sed -i -e "s#\<$old\>#$new#g"
$ old=__builtin_clz; new=rte_clz32;
  git grep -lw $old :^lib/eal/include/rte_bitops.h |
  xargs sed -i -e "s#\<$old\>#$new#g"

$ old=__builtin_ctzll; new=rte_ctz64;
  git grep -lw $old :^lib/eal/include/rte_bitops.h |
  xargs sed -i -e "s#\<$old\>#$new#g"
$ old=__builtin_ctz; new=rte_ctz32;
  git grep -lw $old :^lib/eal/include/rte_bitops.h |
  xargs sed -i -e "s#\<$old\>#$new#g"

$ old=__builtin_popcountll; new=rte_popcount64;
  git grep -lw $old :^lib/eal/include/rte_bitops.h |
  xargs sed -i -e "s#\<$old\>#$new#g"
$ old=__builtin_popcount; new=rte_popcount32;
  git grep -lw $old :^lib/eal/include/rte_bitops.h |
  xargs sed -i -e "s#\<$old\>#$new#g"

Then inclusion of rte_bitops.h was added were necessary.

Signed-off-by: David Marchand <david.marchand@redhat.com>
Acked-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
Reviewed-by: Long Li <longli@microsoft.com>

lib/hash/rte_thash_x86_gfni.h
@@ -110,7 +110,7 @@ __rte_thash_gfni(const uint64_t *mtrx, const uint8_t *tuple,
 				secondary_tuple);
 		}
 
-		chunk_len = __builtin_popcountll(load_mask);
+		chunk_len = rte_popcount64(load_mask);
 		for (i = 0; i < ((chunk_len + prepend) / 8); i++, mtrx += 8) {
 			perm_bytes = _mm512_mask_permutexvar_epi8(perm_bytes,
 				permute_mask, permute_idx, tuple_bytes);

commit 56417d82e04dbc46b00042feb29b468861666fce

hash: fix build with GFNI
As an external header, rte_thash_x86_gfni.h should be self sufficient.

Fixes: 3d4e27fd7ff0 ("use abstracted bit count functions")

Signed-off-by: David Marchand <david.marchand@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/hash/rte_thash_x86_gfni.h
@@ -12,6 +12,7 @@
  * using Galois Fields New Instructions.
  */
 
+#include <rte_bitops.h>
 #include <rte_compat.h>
 #include <rte_vect.h>
 