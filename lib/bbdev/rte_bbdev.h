97880aca3fab98ba5fc8c2be10292bdcbeab2ba1
Stephen Hemminger <stephen@networkplumber.org>
2023-10-19 12:10:10 -0700

    bbdev: remove experimental tag
    The API's for bbdev were last added to in 22.11.
    Remove experimental flag now.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/bbdev/rte_bbdev.h
+++ lib/bbdev/rte_bbdev.h
@@ -675,7 +675,6 @@ rte_bbdev_enqueue_ldpc_dec_ops(uint16_t dev_id, uint16_t queue_id,
  *   The number of operations actually enqueued.
  *   (This is the number of processed entries in the @p ops array.)
  */
-__rte_experimental
 static inline uint16_t
 rte_bbdev_enqueue_fft_ops(uint16_t dev_id, uint16_t queue_id,
 		struct rte_bbdev_fft_op **ops, uint16_t num_ops)
@@ -860,7 +859,6 @@ rte_bbdev_dequeue_ldpc_dec_ops(uint16_t dev_id, uint16_t queue_id,
  *   The number of operations actually dequeued (this is the number of entries
  *   copied into the @p ops array).
  */
-__rte_experimental
 static inline uint16_t
 rte_bbdev_dequeue_fft_ops(uint16_t dev_id, uint16_t queue_id,
 		struct rte_bbdev_fft_op **ops, uint16_t num_ops)
@@ -1042,7 +1040,6 @@ rte_bbdev_queue_intr_ctl(uint16_t dev_id, uint16_t queue_id, int epfd, int op,
  * @returns
  *   Device status as string or NULL if invalid.
  */
-__rte_experimental
 const char*
 rte_bbdev_device_status_str(enum rte_bbdev_device_status status);
 
@@ -1055,7 +1052,6 @@ rte_bbdev_device_status_str(enum rte_bbdev_device_status status);
  * @returns
  *   Queue status as string or NULL if op_type is invalid.
  */
-__rte_experimental
 const char*
 rte_bbdev_enqueue_status_str(enum rte_bbdev_enqueue_status status);
 
