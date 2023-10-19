f3bce654ec06dba80392ec9ad022060a62a2739e
Stephen Hemminger <stephen@networkplumber.org>
2023-10-19 12:10:04 -0700

    power: remove experimental from API's
    The power management API's were last changed in 22.11 release.
    Therefore remove experimental for 23.11 release.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/power/rte_power.h
+++ lib/power/rte_power.h
@@ -25,9 +25,6 @@ enum power_management_env {PM_ENV_NOT_SET, PM_ENV_ACPI_CPUFREQ, PM_ENV_KVM_VM,
 		PM_ENV_AMD_PSTATE_CPUFREQ};
 
 /**
- * @warning
- * @b EXPERIMENTAL: this API may change, or be removed, without prior notice
- *
  * Check if a specific power management environment type is supported on a
  * currently running system.
  *
@@ -39,7 +36,6 @@ enum power_management_env {PM_ENV_NOT_SET, PM_ENV_ACPI_CPUFREQ, PM_ENV_KVM_VM,
  *   - 0 if unsupported
  *   - -1 if error, with rte_errno indicating reason for error.
  */
-__rte_experimental
 int rte_power_check_env_supported(enum power_management_env env);
 
 /**
