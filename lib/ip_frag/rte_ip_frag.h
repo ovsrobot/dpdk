e173953869c0d7201555bd965129a243643ddbde
Stephen Hemminger <stephen@networkplumber.org>
2023-10-19 12:10:06 -0700

    ip_frag: mark a couple of functions stable
    There were two functions added in 22.11 which were
    marked as experimental. Remove the experimental tag.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Acked-by: Konstantin Ananyev <konstantin.v.ananyev@yandex.ru>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/ip_frag/rte_ip_frag.h
+++ lib/ip_frag/rte_ip_frag.h
@@ -205,7 +205,6 @@ int32_t rte_ipv4_fragment_packet(struct rte_mbuf *pkt_in,
  *   in the pkts_out array.
  *   Otherwise - (-1) * errno.
  */
-__rte_experimental
 int32_t
 rte_ipv4_fragment_copy_nonseg_packet(struct rte_mbuf *pkt_in,
 	struct rte_mbuf **pkts_out,
@@ -289,7 +288,6 @@ rte_ip_frag_table_statistics_dump(FILE * f, const struct rte_ip_frag_tbl *tbl);
  * @param tms
  *   Current timestamp
  */
-__rte_experimental
 void
 rte_ip_frag_table_del_expired_entries(struct rte_ip_frag_tbl *tbl,
 	struct rte_ip_frag_death_row *dr, uint64_t tms);
