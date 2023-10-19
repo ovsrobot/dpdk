4720fdc501660bb8146436b36d4a9ab67be932cc
Stephen Hemminger <stephen@networkplumber.org>
2023-10-19 12:10:09 -0700

    vhost: remove experimental from some API's
    All API's that before 22.11 release should have experimental
    tag removed.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/vhost/rte_vhost_async.h
+++ lib/vhost/rte_vhost_async.h
@@ -24,7 +24,6 @@ extern "C" {
  * @return
  *  0 on success, -1 on failures
  */
-__rte_experimental
 int rte_vhost_async_channel_register(int vid, uint16_t queue_id);
 
 /**
@@ -37,7 +36,6 @@ int rte_vhost_async_channel_register(int vid, uint16_t queue_id);
  * @return
  *  0 on success, -1 on failures
  */
-__rte_experimental
 int rte_vhost_async_channel_unregister(int vid, uint16_t queue_id);
 
 /**
@@ -54,7 +52,6 @@ int rte_vhost_async_channel_unregister(int vid, uint16_t queue_id);
  * @return
  *  0 on success, -1 on failures
  */
-__rte_experimental
 int rte_vhost_async_channel_register_thread_unsafe(int vid, uint16_t queue_id);
 
 /**
@@ -71,7 +68,6 @@ int rte_vhost_async_channel_register_thread_unsafe(int vid, uint16_t queue_id);
  * @return
  *  0 on success, -1 on failures
  */
-__rte_experimental
 int rte_vhost_async_channel_unregister_thread_unsafe(int vid,
 		uint16_t queue_id);
 
@@ -95,7 +91,6 @@ int rte_vhost_async_channel_unregister_thread_unsafe(int vid,
  * @return
  *  num of packets enqueued
  */
-__rte_experimental
 uint16_t rte_vhost_submit_enqueue_burst(int vid, uint16_t queue_id,
 		struct rte_mbuf **pkts, uint16_t count, int16_t dma_id,
 		uint16_t vchan_id);
@@ -120,7 +115,6 @@ uint16_t rte_vhost_submit_enqueue_burst(int vid, uint16_t queue_id,
  * @return
  *  num of packets returned
  */
-__rte_experimental
 uint16_t rte_vhost_poll_enqueue_completed(int vid, uint16_t queue_id,
 		struct rte_mbuf **pkts, uint16_t count, int16_t dma_id,
 		uint16_t vchan_id);
@@ -136,7 +130,6 @@ uint16_t rte_vhost_poll_enqueue_completed(int vid, uint16_t queue_id,
  * @return
  *  the amount of in-flight packets on success; -1 on failure
  */
-__rte_experimental
 int rte_vhost_async_get_inflight(int vid, uint16_t queue_id);
 
 /**
@@ -153,7 +146,6 @@ int rte_vhost_async_get_inflight(int vid, uint16_t queue_id);
  * @return
  * the amount of in-flight packets on success; -1 on failure
  */
-__rte_experimental
 int rte_vhost_async_get_inflight_thread_unsafe(int vid, uint16_t queue_id);
 
 /**
@@ -178,7 +170,6 @@ int rte_vhost_async_get_inflight_thread_unsafe(int vid, uint16_t queue_id);
  * @return
  *  Number of packets returned
  */
-__rte_experimental
 uint16_t rte_vhost_clear_queue_thread_unsafe(int vid, uint16_t queue_id,
 		struct rte_mbuf **pkts, uint16_t count, int16_t dma_id,
 		uint16_t vchan_id);
@@ -203,7 +194,6 @@ uint16_t rte_vhost_clear_queue_thread_unsafe(int vid, uint16_t queue_id,
  * @return
  *  Number of packets returned
  */
-__rte_experimental
 uint16_t rte_vhost_clear_queue(int vid, uint16_t queue_id,
 		struct rte_mbuf **pkts, uint16_t count, int16_t dma_id,
 		uint16_t vchan_id);
@@ -226,13 +216,9 @@ uint16_t rte_vhost_clear_queue(int vid, uint16_t queue_id,
  * @return
  *  0 on success, and -1 on failure
  */
-__rte_experimental
 int rte_vhost_async_dma_configure(int16_t dma_id, uint16_t vchan_id);
 
 /**
- * @warning
- * @b EXPERIMENTAL: this API may change, or be removed, without prior notice
- *
  * This function tries to receive packets from the guest with offloading
  * copies to the DMA vChannels. Successfully dequeued packets are returned
  * in "pkts". The other packets that their copies are submitted to
@@ -260,16 +246,12 @@ int rte_vhost_async_dma_configure(int16_t dma_id, uint16_t vchan_id);
  * @return
  *  Number of successfully dequeued packets
  */
-__rte_experimental
 uint16_t
 rte_vhost_async_try_dequeue_burst(int vid, uint16_t queue_id,
 	struct rte_mempool *mbuf_pool, struct rte_mbuf **pkts, uint16_t count,
 	int *nr_inflight, int16_t dma_id, uint16_t vchan_id);
 
 /**
- * @warning
- * @b EXPERIMENTAL: this API may change, or be removed, without prior notice.
- *
  * Unconfigure DMA vChannel in Vhost asynchronous data path.
  * This function should be called when the specified DMA vChannel is no longer
  * used by the Vhost library. Before this function is called, make sure there
@@ -282,7 +264,6 @@ rte_vhost_async_try_dequeue_burst(int vid, uint16_t queue_id,
  * @return
  *  0 on success, and -1 on failure
  */
-__rte_experimental
 int
 rte_vhost_async_dma_unconfigure(int16_t dma_id, uint16_t vchan_id);
 
