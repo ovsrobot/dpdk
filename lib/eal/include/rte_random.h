d26212373785a422655aa1fa3fcdc24b0e6bec88
Stephen Hemminger <stephen@networkplumber.org>
2023-10-20 14:41:09 -0700

    eal: make rte_drand a stable API
    This API was added in 2020.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/include/rte_random.h
+++ lib/eal/include/rte_random.h
@@ -70,9 +70,6 @@ uint64_t
 rte_rand_max(uint64_t upper_bound);
 
 /**
- * @warning
- * @b EXPERIMENTAL: this API may change without prior notice
- *
  * Generates a pseudo-random floating point number.
  *
  * This function returns a non-negative double-precision floating random
@@ -87,7 +84,6 @@ rte_rand_max(uint64_t upper_bound);
  * @return
  *   A pseudo-random value between 0 and 1.0.
  */
-__rte_experimental
 double rte_drand(void);
 
 #ifdef __cplusplus
