ovsrobot/dpdk

commit 36c46e738120c381cf663c96692454c5aa75e203

ethdev: cleanup shared data with the last port
If no port is allocated and no port owner is still registered,
ethdev from a primary process may release the memzone used to store
port data.
This makes it possible for the DPDK memory allocator to release
associated resources back to the OS.

Signed-off-by: David Marchand <david.marchand@redhat.com>
Acked-by: Morten Brørup <mb@smartsharesystems.com>

lib/ethdev/ethdev_private.c
@@ -11,6 +11,7 @@
 
 static const char *MZ_RTE_ETH_DEV_DATA = "rte_eth_dev_data";
 
+static const struct rte_memzone *eth_dev_shared_mz;
 struct eth_dev_shared *eth_dev_shared_data;
 
 /* spinlock for eth device callbacks */
@@ -326,7 +327,7 @@ eth_dev_shared_data_prepare(void)
 	const unsigned int flags = 0;
 	const struct rte_memzone *mz;
 
-	if (eth_dev_shared_data == NULL) {
+	if (eth_dev_shared_mz == NULL) {
 		if (rte_eal_process_type() == RTE_PROC_PRIMARY) {
 			/* Allocate port data and ownership shared memory. */
 			mz = rte_memzone_reserve(MZ_RTE_ETH_DEV_DATA,
@@ -339,16 +340,34 @@ eth_dev_shared_data_prepare(void)
 			goto out;
 		}
 
+		eth_dev_shared_mz = mz;
 		eth_dev_shared_data = mz->addr;
 		if (rte_eal_process_type() == RTE_PROC_PRIMARY) {
+			eth_dev_shared_data->allocated_owners = 0;
 			eth_dev_shared_data->next_owner_id =
 					RTE_ETH_DEV_NO_OWNER + 1;
+			eth_dev_shared_data->allocated_ports = 0;
 			memset(eth_dev_shared_data->data, 0,
 			       sizeof(eth_dev_shared_data->data));
 		}
 	}
 out:
 	return eth_dev_shared_data;
+}
+
+void
+eth_dev_shared_data_release(void)
+{
+	RTE_ASSERT(rte_eal_process_type() == RTE_PROC_PRIMARY);
+
+	if (eth_dev_shared_data->allocated_ports != 0)
+		return;
+	if (eth_dev_shared_data->allocated_owners != 0)
+		return;
+
+	rte_memzone_free(eth_dev_shared_mz);
+	eth_dev_shared_mz = NULL;
+	eth_dev_shared_data = NULL;
 }
 
 void

lib/ethdev/ethdev_driver.c
@@ -115,6 +115,8 @@ rte_eth_dev_allocate(const char *name)
 	eth_dev->data->backer_port_id = RTE_MAX_ETHPORTS;
 	eth_dev->data->mtu = RTE_ETHER_MTU;
 	pthread_mutex_init(&eth_dev->data->flow_ops_mutex, NULL);
+	RTE_ASSERT(rte_eal_process_type() == RTE_PROC_PRIMARY);
+	eth_dev_shared_data->allocated_ports++;
 
 unlock:
 	rte_spinlock_unlock(rte_mcfg_ethdev_get_lock());
@@ -265,6 +267,10 @@ rte_eth_dev_release_port(struct rte_eth_dev *eth_dev)
 		rte_free(eth_dev->data->dev_private);
 		pthread_mutex_destroy(&eth_dev->data->flow_ops_mutex);
 		memset(eth_dev->data, 0, sizeof(struct rte_eth_dev_data));
+		eth_dev->data = NULL;
+
+		eth_dev_shared_data->allocated_ports--;
+		eth_dev_shared_data_release();
 	}
 
 	rte_spinlock_unlock(rte_mcfg_ethdev_get_lock());

lib/ethdev/ethdev_private.h
@@ -14,7 +14,9 @@
 #include "rte_ethdev.h"
 
 struct eth_dev_shared {
+	uint64_t allocated_owners;
 	uint64_t next_owner_id;
+	uint64_t allocated_ports;
 	struct rte_eth_dev_data data[RTE_MAX_ETHPORTS];
 };
 
@@ -68,6 +70,8 @@ void eth_dev_fp_ops_setup(struct rte_eth_fp_ops *fpo,
 
 
 void *eth_dev_shared_data_prepare(void)
+	__rte_exclusive_locks_required(rte_mcfg_ethdev_get_lock());
+void eth_dev_shared_data_release(void)
 	__rte_exclusive_locks_required(rte_mcfg_ethdev_get_lock());
 
 void eth_dev_rxq_release(struct rte_eth_dev *dev, uint16_t qid);

lib/ethdev/rte_ethdev.c
@@ -444,6 +444,7 @@ rte_eth_dev_owner_new(uint64_t *owner_id)
 
 	if (eth_dev_shared_data_prepare() != NULL) {
 		*owner_id = eth_dev_shared_data->next_owner_id++;
+		eth_dev_shared_data->allocated_owners++;
 		ret = 0;
 	} else {
 		ret = -ENOMEM;
@@ -566,6 +567,8 @@ rte_eth_dev_owner_delete(const uint64_t owner_id)
 		RTE_ETHDEV_LOG(NOTICE,
 			"All port owners owned by %016"PRIx64" identifier have removed\n",
 			owner_id);
+		eth_dev_shared_data->allocated_owners--;
+		eth_dev_shared_data_release();
 	} else {
 		RTE_ETHDEV_LOG(ERR,
 			       "Invalid owner ID=%016"PRIx64"\n",

commit 89725ba5a4f154e812ffed7f71d52df7fb1c0520

ethdev: refresh shared memory reference in secondary process
In the process of releasing ethdev port in the primary process,
secondary processes are asked to stop referencing such a port.
Doing so, those processes can't predict whether the primary process
will later invalidate/free the shared memory. So they may live with a
reference to an old shared memory memzone and location in memory.

Refresh the shared memory pointer in secondary process when requesting
access to the shared memory: this is the best moment ethdev can check
it because the primary process will (re-)create a shared memory only if
no secondary process is referencing a ethdev port.

Bugzilla ID: 1303
Fixes: 36c46e738120 ("ethdev: cleanup shared data with the last port")

Signed-off-by: David Marchand <david.marchand@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/ethdev/ethdev_private.c
@@ -324,17 +324,18 @@ rte_eth_call_tx_callbacks(uint16_t port_id, uint16_t queue_id,
 void *
 eth_dev_shared_data_prepare(void)
 {
-	const unsigned int flags = 0;
 	const struct rte_memzone *mz;
 
-	if (eth_dev_shared_mz == NULL) {
-		if (rte_eal_process_type() == RTE_PROC_PRIMARY) {
-			/* Allocate port data and ownership shared memory. */
-			mz = rte_memzone_reserve(MZ_RTE_ETH_DEV_DATA,
-					sizeof(*eth_dev_shared_data),
-					rte_socket_id(), flags);
-		} else
-			mz = rte_memzone_lookup(MZ_RTE_ETH_DEV_DATA);
+	if (rte_eal_process_type() == RTE_PROC_PRIMARY) {
+		const unsigned int flags = 0;
+
+		if (eth_dev_shared_mz != NULL)
+			goto out;
+
+		/* Allocate port data and ownership shared memory. */
+		mz = rte_memzone_reserve(MZ_RTE_ETH_DEV_DATA,
+				sizeof(*eth_dev_shared_data),
+				rte_socket_id(), flags);
 		if (mz == NULL) {
 			RTE_ETHDEV_LOG(ERR, "Cannot allocate ethdev shared data\n");
 			goto out;
@@ -342,14 +343,27 @@ eth_dev_shared_data_prepare(void)
 
 		eth_dev_shared_mz = mz;
 		eth_dev_shared_data = mz->addr;
-		if (rte_eal_process_type() == RTE_PROC_PRIMARY) {
-			eth_dev_shared_data->allocated_owners = 0;
-			eth_dev_shared_data->next_owner_id =
-					RTE_ETH_DEV_NO_OWNER + 1;
-			eth_dev_shared_data->allocated_ports = 0;
-			memset(eth_dev_shared_data->data, 0,
-			       sizeof(eth_dev_shared_data->data));
+		eth_dev_shared_data->allocated_owners = 0;
+		eth_dev_shared_data->next_owner_id =
+			RTE_ETH_DEV_NO_OWNER + 1;
+		eth_dev_shared_data->allocated_ports = 0;
+		memset(eth_dev_shared_data->data, 0,
+		       sizeof(eth_dev_shared_data->data));
+	} else {
+		mz = rte_memzone_lookup(MZ_RTE_ETH_DEV_DATA);
+		if (mz == NULL) {
+			/* Clean remaining any traces of a previous shared mem */
+			eth_dev_shared_mz = NULL;
+			eth_dev_shared_data = NULL;
+			RTE_ETHDEV_LOG(ERR, "Cannot lookup ethdev shared data\n");
+			goto out;
 		}
+		if (mz == eth_dev_shared_mz && mz->addr == eth_dev_shared_data)
+			goto out;
+
+		/* Shared mem changed in primary process, refresh pointers */
+		eth_dev_shared_mz = mz;
+		eth_dev_shared_data = mz->addr;
 	}
 out:
 	return eth_dev_shared_data;