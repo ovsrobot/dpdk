ovsrobot/dpdk

commit 5433956d51859fa8a9d4d76e2877ee20c8fc6002

event/dlb2: add eventdev probe
Add the eventdev portion of probe, and parse command line
options, but do not initialize hardware.

Signed-off-by: Timothy McDaniel <timothy.mcdaniel@intel.com>
Reviewed-by: Gage Eads <gage.eads@intel.com>

drivers/event/dlb2/pf/dlb2_main.c
@@ -1,0 +1,621 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2016-2020 Intel Corporation
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <errno.h>
+#include <assert.h>
+#include <unistd.h>
+#include <string.h>
+
+#include <rte_malloc.h>
+#include <rte_errno.h>
+
+#include "base/dlb2_resource.h"
+#include "base/dlb2_osdep.h"
+#include "base/dlb2_regs.h"
+#include "dlb2_main.h"
+#include "../dlb2_user.h"
+#include "../dlb2_priv.h"
+#include "../dlb2_inline_fns.h"
+
+#define PF_ID_ZERO 0	/* PF ONLY! */
+#define NO_OWNER_VF 0	/* PF ONLY! */
+#define NOT_VF_REQ false /* PF ONLY! */
+
+#define DLB2_PCI_CFG_SPACE_SIZE 256
+#define DLB2_PCI_CAP_POINTER 0x34
+#define DLB2_PCI_CAP_NEXT(hdr) (((hdr) >> 8) & 0xFC)
+#define DLB2_PCI_CAP_ID(hdr) ((hdr) & 0xFF)
+#define DLB2_PCI_EXT_CAP_NEXT(hdr) (((hdr) >> 20) & 0xFFC)
+#define DLB2_PCI_EXT_CAP_ID(hdr) ((hdr) & 0xFFFF)
+#define DLB2_PCI_EXT_CAP_ID_ERR 1
+#define DLB2_PCI_ERR_UNCOR_MASK 8
+#define DLB2_PCI_ERR_UNC_UNSUP  0x00100000
+
+#define DLB2_PCI_EXP_DEVCTL 8
+#define DLB2_PCI_LNKCTL 16
+#define DLB2_PCI_SLTCTL 24
+#define DLB2_PCI_RTCTL 28
+#define DLB2_PCI_EXP_DEVCTL2 40
+#define DLB2_PCI_LNKCTL2 48
+#define DLB2_PCI_SLTCTL2 56
+#define DLB2_PCI_CMD 4
+#define DLB2_PCI_X_CMD 2
+#define DLB2_PCI_EXP_DEVSTA 10
+#define DLB2_PCI_EXP_DEVSTA_TRPND 0x20
+#define DLB2_PCI_EXP_DEVCTL_BCR_FLR 0x8000
+
+#define DLB2_PCI_CAP_ID_EXP       0x10
+#define DLB2_PCI_CAP_ID_MSIX      0x11
+#define DLB2_PCI_EXT_CAP_ID_PAS   0x1B
+#define DLB2_PCI_EXT_CAP_ID_PRI   0x13
+#define DLB2_PCI_EXT_CAP_ID_ACS   0xD
+
+#define DLB2_PCI_PRI_CTRL_ENABLE         0x1
+#define DLB2_PCI_PRI_ALLOC_REQ           0xC
+#define DLB2_PCI_PRI_CTRL                0x4
+#define DLB2_PCI_MSIX_FLAGS              0x2
+#define DLB2_PCI_MSIX_FLAGS_ENABLE       0x8000
+#define DLB2_PCI_MSIX_FLAGS_MASKALL      0x4000
+#define DLB2_PCI_ERR_ROOT_STATUS         0x30
+#define DLB2_PCI_ERR_COR_STATUS          0x10
+#define DLB2_PCI_ERR_UNCOR_STATUS        0x4
+#define DLB2_PCI_COMMAND_INTX_DISABLE    0x400
+#define DLB2_PCI_ACS_CAP                 0x4
+#define DLB2_PCI_ACS_CTRL                0x6
+#define DLB2_PCI_ACS_SV                  0x1
+#define DLB2_PCI_ACS_RR                  0x4
+#define DLB2_PCI_ACS_CR                  0x8
+#define DLB2_PCI_ACS_UF                  0x10
+#define DLB2_PCI_ACS_EC                  0x20
+
+/* Stubs: Allow building partial probe patch */
+void dlb2_resource_free(struct dlb2_hw *hw)
+{
+	RTE_SET_USED(hw);
+}
+
+int dlb2_resource_init(struct dlb2_hw *hw)
+{
+	int ret = 0;
+	RTE_SET_USED(hw);
+
+	return ret;
+}
+
+void dlb2_clr_pmcsr_disable(struct dlb2_hw *hw)
+{
+	RTE_SET_USED(hw);
+}
+
+/* End stubs */
+
+static int
+dlb2_pci_find_ext_capability(struct rte_pci_device *pdev, uint32_t id)
+{
+	uint32_t hdr;
+	size_t sz;
+	int pos;
+
+	pos = DLB2_PCI_CFG_SPACE_SIZE;
+	sz = sizeof(hdr);
+
+	while (pos > 0xFF) {
+		if (rte_pci_read_config(pdev, &hdr, sz, pos) != (int)sz)
+			return -1;
+
+		if (DLB2_PCI_EXT_CAP_ID(hdr) == id)
+			return pos;
+
+		pos = DLB2_PCI_EXT_CAP_NEXT(hdr);
+	}
+
+	return -1;
+}
+
+static int dlb2_pci_find_capability(struct rte_pci_device *pdev, uint32_t id)
+{
+	uint8_t pos;
+	int ret;
+	uint16_t hdr;
+
+	ret = rte_pci_read_config(pdev, &pos, 1, DLB2_PCI_CAP_POINTER);
+	pos &= 0xFC;
+
+	if (ret != 1)
+		return -1;
+
+	while (pos > 0x3F) {
+		ret = rte_pci_read_config(pdev, &hdr, 2, pos);
+		if (ret != 2)
+			return -1;
+
+		if (DLB2_PCI_CAP_ID(hdr) == id)
+			return pos;
+
+		if (DLB2_PCI_CAP_ID(hdr) == 0xFF)
+			return -1;
+
+		pos = DLB2_PCI_CAP_NEXT(hdr);
+	}
+
+	return -1;
+}
+
+static int
+dlb2_pf_init_driver_state(struct dlb2_dev *dlb2_dev)
+{
+	rte_spinlock_init(&dlb2_dev->resource_mutex);
+	rte_spinlock_init(&dlb2_dev->measurement_lock);
+
+	return 0;
+}
+
+static void dlb2_pf_enable_pm(struct dlb2_dev *dlb2_dev)
+{
+	dlb2_clr_pmcsr_disable(&dlb2_dev->hw);
+}
+
+#define DLB2_READY_RETRY_LIMIT 1000
+static int dlb2_pf_wait_for_device_ready(struct dlb2_dev *dlb2_dev)
+{
+	u32 retries = 0;
+
+	/* Allow at least 1s for the device to become active after power-on */
+	for (retries = 0; retries < DLB2_READY_RETRY_LIMIT; retries++) {
+		union dlb2_cfg_mstr_cfg_diagnostic_idle_status idle;
+		union dlb2_cfg_mstr_cfg_pm_status pm_st;
+		u32 addr;
+
+		addr = DLB2_CFG_MSTR_CFG_PM_STATUS;
+		pm_st.val = DLB2_CSR_RD(&dlb2_dev->hw, addr);
+		addr = DLB2_CFG_MSTR_CFG_DIAGNOSTIC_IDLE_STATUS;
+		idle.val = DLB2_CSR_RD(&dlb2_dev->hw, addr);
+		if (pm_st.field.pmsm == 1 && idle.field.dlb_func_idle == 1)
+			break;
+
+		rte_delay_ms(1);
+	};
+
+	if (retries == DLB2_READY_RETRY_LIMIT) {
+		DLB2_LOG_ERR("[%s()] wait for device ready timed out\n",
+		       __func__);
+		return -1;
+	}
+
+	return 0;
+}
+
+struct dlb2_dev *
+dlb2_probe(struct rte_pci_device *pdev)
+{
+	struct dlb2_dev *dlb2_dev;
+	int ret = 0;
+
+	DLB2_INFO(dlb2_dev, "probe\n");
+
+	dlb2_dev = rte_malloc("DLB2_PF", sizeof(struct dlb2_dev),
+			      RTE_CACHE_LINE_SIZE);
+
+	if (dlb2_dev == NULL) {
+		ret = -ENOMEM;
+		goto dlb2_dev_malloc_fail;
+	}
+
+	/* PCI Bus driver has already mapped bar space into process.
+	 * Save off our IO register and FUNC addresses.
+	 */
+
+	/* BAR 0 */
+	if (pdev->mem_resource[0].addr == NULL) {
+		DLB2_ERR(dlb2_dev, "probe: BAR 0 addr (csr_kva) is NULL\n");
+		ret = -EINVAL;
+		goto pci_mmap_bad_addr;
+	}
+	dlb2_dev->hw.func_kva = (void *)(uintptr_t)pdev->mem_resource[0].addr;
+	dlb2_dev->hw.func_phys_addr = pdev->mem_resource[0].phys_addr;
+
+	DLB2_INFO(dlb2_dev, "DLB2 FUNC VA=%p, PA=%p, len=%p\n",
+		  (void *)dlb2_dev->hw.func_kva,
+		  (void *)dlb2_dev->hw.func_phys_addr,
+		  (void *)(pdev->mem_resource[0].len));
+
+	/* BAR 2 */
+	if (pdev->mem_resource[2].addr == NULL) {
+		DLB2_ERR(dlb2_dev, "probe: BAR 2 addr (func_kva) is NULL\n");
+		ret = -EINVAL;
+		goto pci_mmap_bad_addr;
+	}
+	dlb2_dev->hw.csr_kva = (void *)(uintptr_t)pdev->mem_resource[2].addr;
+	dlb2_dev->hw.csr_phys_addr = pdev->mem_resource[2].phys_addr;
+
+	DLB2_INFO(dlb2_dev, "DLB2 CSR VA=%p, PA=%p, len=%p\n",
+		  (void *)dlb2_dev->hw.csr_kva,
+		  (void *)dlb2_dev->hw.csr_phys_addr,
+		  (void *)(pdev->mem_resource[2].len));
+
+	dlb2_dev->pdev = pdev;
+
+	/* PM enable must be done before any other MMIO accesses, and this
+	 * setting is persistent across device reset.
+	 */
+	dlb2_pf_enable_pm(dlb2_dev);
+
+	ret = dlb2_pf_wait_for_device_ready(dlb2_dev);
+	if (ret)
+		goto wait_for_device_ready_fail;
+
+	ret = dlb2_pf_reset(dlb2_dev);
+	if (ret)
+		goto dlb2_reset_fail;
+
+	ret = dlb2_pf_init_driver_state(dlb2_dev);
+	if (ret)
+		goto init_driver_state_fail;
+
+	ret = dlb2_resource_init(&dlb2_dev->hw);
+	if (ret)
+		goto resource_init_fail;
+
+	return dlb2_dev;
+
+resource_init_fail:
+	dlb2_resource_free(&dlb2_dev->hw);
+init_driver_state_fail:
+dlb2_reset_fail:
+pci_mmap_bad_addr:
+wait_for_device_ready_fail:
+	rte_free(dlb2_dev);
+dlb2_dev_malloc_fail:
+	rte_errno = ret;
+	return NULL;
+}
+
+int
+dlb2_pf_reset(struct dlb2_dev *dlb2_dev)
+{
+	int ret = 0;
+	int i = 0;
+	uint32_t dword[16];
+	uint16_t cmd;
+	off_t off;
+
+	uint16_t dev_ctl_word;
+	uint16_t dev_ctl2_word;
+	uint16_t lnk_word;
+	uint16_t lnk_word2;
+	uint16_t slt_word;
+	uint16_t slt_word2;
+	uint16_t rt_ctl_word;
+	uint32_t pri_reqs_dword;
+	uint16_t pri_ctrl_word;
+
+	int pcie_cap_offset;
+	int pri_cap_offset;
+	int msix_cap_offset;
+	int err_cap_offset;
+	int acs_cap_offset;
+	int wait_count;
+
+	uint16_t devsta_busy_word;
+	uint16_t devctl_word;
+
+	struct rte_pci_device *pdev = dlb2_dev->pdev;
+
+	/* Save PCI config state */
+
+	for (i = 0; i < 16; i++) {
+		if (rte_pci_read_config(pdev, &dword[i], 4, i * 4) != 4)
+			return ret;
+	}
+
+	pcie_cap_offset = dlb2_pci_find_capability(pdev, DLB2_PCI_CAP_ID_EXP);
+
+	if (pcie_cap_offset < 0) {
+		DLB2_LOG_ERR("[%s()] failed to find the pcie capability\n",
+		       __func__);
+		return pcie_cap_offset;
+	}
+
+	off = pcie_cap_offset + DLB2_PCI_EXP_DEVCTL;
+	if (rte_pci_read_config(pdev, &dev_ctl_word, 2, off) != 2)
+		dev_ctl_word = 0;
+
+	off = pcie_cap_offset + DLB2_PCI_LNKCTL;
+	if (rte_pci_read_config(pdev, &lnk_word, 2, off) != 2)
+		lnk_word = 0;
+
+	off = pcie_cap_offset + DLB2_PCI_SLTCTL;
+	if (rte_pci_read_config(pdev, &slt_word, 2, off) != 2)
+		slt_word = 0;
+
+	off = pcie_cap_offset + DLB2_PCI_RTCTL;
+	if (rte_pci_read_config(pdev, &rt_ctl_word, 2, off) != 2)
+		rt_ctl_word = 0;
+
+	off = pcie_cap_offset + DLB2_PCI_EXP_DEVCTL2;
+	if (rte_pci_read_config(pdev, &dev_ctl2_word, 2, off) != 2)
+		dev_ctl2_word = 0;
+
+	off = pcie_cap_offset + DLB2_PCI_LNKCTL2;
+	if (rte_pci_read_config(pdev, &lnk_word2, 2, off) != 2)
+		lnk_word2 = 0;
+
+	off = pcie_cap_offset + DLB2_PCI_SLTCTL2;
+	if (rte_pci_read_config(pdev, &slt_word2, 2, off) != 2)
+		slt_word2 = 0;
+
+	off = DLB2_PCI_EXT_CAP_ID_PRI;
+	pri_cap_offset = dlb2_pci_find_ext_capability(pdev, off);
+
+	if (pri_cap_offset >= 0) {
+		off = pri_cap_offset + DLB2_PCI_PRI_ALLOC_REQ;
+		if (rte_pci_read_config(pdev, &pri_reqs_dword, 4, off) != 4)
+			pri_reqs_dword = 0;
+	}
+
+	/* clear the PCI command register before issuing the FLR */
+
+	off = DLB2_PCI_CMD;
+	cmd = 0;
+	if (rte_pci_write_config(pdev, &cmd, 2, off) != 2) {
+		DLB2_LOG_ERR("[%s()] failed to write the pci command\n",
+		       __func__);
+		return ret;
+	}
+
+	/* issue the FLR */
+	for (wait_count = 0; wait_count < 4; wait_count++) {
+		int sleep_time;
+
+		off = pcie_cap_offset + DLB2_PCI_EXP_DEVSTA;
+		ret = rte_pci_read_config(pdev, &devsta_busy_word, 2, off);
+		if (ret != 2) {
+			DLB2_LOG_ERR("[%s()] failed to read the pci device status\n",
+			       __func__);
+			return ret;
+		}
+
+		if (!(devsta_busy_word & DLB2_PCI_EXP_DEVSTA_TRPND))
+			break;
+
+		sleep_time = (1 << (wait_count)) * 100;
+		rte_delay_ms(sleep_time);
+	}
+
+	if (wait_count == 4) {
+		DLB2_LOG_ERR("[%s()] wait for pci pending transactions timed out\n",
+		       __func__);
+		return -1;
+	}
+
+	off = pcie_cap_offset + DLB2_PCI_EXP_DEVCTL;
+	ret = rte_pci_read_config(pdev, &devctl_word, 2, off);
+	if (ret != 2) {
+		DLB2_LOG_ERR("[%s()] failed to read the pcie device control\n",
+		       __func__);
+		return ret;
+	}
+
+	devctl_word |= DLB2_PCI_EXP_DEVCTL_BCR_FLR;
+
+	ret = rte_pci_write_config(pdev, &devctl_word, 2, off);
+	if (ret != 2) {
+		DLB2_LOG_ERR("[%s()] failed to write the pcie device control\n",
+		       __func__);
+		return ret;
+	}
+
+	rte_delay_ms(100);
+
+	/* Restore PCI config state */
+
+	if (pcie_cap_offset >= 0) {
+		off = pcie_cap_offset + DLB2_PCI_EXP_DEVCTL;
+		ret = rte_pci_write_config(pdev, &dev_ctl_word, 2, off);
+		if (ret != 2) {
+			DLB2_LOG_ERR("[%s()] failed to write the pcie device control at offset %d\n",
+				__func__, (int)off);
+			return ret;
+		}
+
+		off = pcie_cap_offset + DLB2_PCI_LNKCTL;
+		ret = rte_pci_write_config(pdev, &lnk_word, 2, off);
+		if (ret != 2) {
+			DLB2_LOG_ERR("[%s()] failed to write the pcie config space at offset %d\n",
+				__func__, (int)off);
+			return ret;
+		}
+
+		off = pcie_cap_offset + DLB2_PCI_SLTCTL;
+		ret = rte_pci_write_config(pdev, &slt_word, 2, off);
+		if (ret != 2) {
+			DLB2_LOG_ERR("[%s()] failed to write the pcie config space at offset %d\n",
+				__func__, (int)off);
+			return ret;
+		}
+
+		off = pcie_cap_offset + DLB2_PCI_RTCTL;
+		ret = rte_pci_write_config(pdev, &rt_ctl_word, 2, off);
+		if (ret != 2) {
+			DLB2_LOG_ERR("[%s()] failed to write the pcie config space at offset %d\n",
+				__func__, (int)off);
+			return ret;
+		}
+
+		off = pcie_cap_offset + DLB2_PCI_EXP_DEVCTL2;
+		ret = rte_pci_write_config(pdev, &dev_ctl2_word, 2, off);
+		if (ret != 2) {
+			DLB2_LOG_ERR("[%s()] failed to write the pcie config space at offset %d\n",
+				__func__, (int)off);
+			return ret;
+		}
+
+		off = pcie_cap_offset + DLB2_PCI_LNKCTL2;
+		ret = rte_pci_write_config(pdev, &lnk_word2, 2, off);
+		if (ret != 2) {
+			DLB2_LOG_ERR("[%s()] failed to write the pcie config space at offset %d\n",
+				__func__, (int)off);
+			return ret;
+		}
+
+		off = pcie_cap_offset + DLB2_PCI_SLTCTL2;
+		ret = rte_pci_write_config(pdev, &slt_word2, 2, off);
+		if (ret != 2) {
+			DLB2_LOG_ERR("[%s()] failed to write the pcie config space at offset %d\n",
+				__func__, (int)off);
+			return ret;
+		}
+	}
+
+	if (pri_cap_offset >= 0) {
+		pri_ctrl_word = DLB2_PCI_PRI_CTRL_ENABLE;
+
+		off = pri_cap_offset + DLB2_PCI_PRI_ALLOC_REQ;
+		ret = rte_pci_write_config(pdev, &pri_reqs_dword, 4, off);
+		if (ret != 4) {
+			DLB2_LOG_ERR("[%s()] failed to write the pcie config space at offset %d\n",
+				__func__, (int)off);
+			return ret;
+		}
+
+		off = pri_cap_offset + DLB2_PCI_PRI_CTRL;
+		ret = rte_pci_write_config(pdev, &pri_ctrl_word, 2, off);
+		if (ret != 2) {
+			DLB2_LOG_ERR("[%s()] failed to write the pcie config space at offset %d\n",
+				__func__, (int)off);
+			return ret;
+		}
+	}
+
+	off = DLB2_PCI_EXT_CAP_ID_ERR;
+	err_cap_offset = dlb2_pci_find_ext_capability(pdev, off);
+
+	if (err_cap_offset >= 0) {
+		uint32_t tmp;
+
+		off = err_cap_offset + DLB2_PCI_ERR_ROOT_STATUS;
+		if (rte_pci_read_config(pdev, &tmp, 4, off) != 4)
+			tmp = 0;
+
+		ret = rte_pci_write_config(pdev, &tmp, 4, off);
+		if (ret != 4) {
+			DLB2_LOG_ERR("[%s()] failed to write the pcie config space at offset %d\n",
+				__func__, (int)off);
+			return ret;
+		}
+
+		off = err_cap_offset + DLB2_PCI_ERR_COR_STATUS;
+		if (rte_pci_read_config(pdev, &tmp, 4, off) != 4)
+			tmp = 0;
+
+		ret = rte_pci_write_config(pdev, &tmp, 4, off);
+		if (ret != 4) {
+			DLB2_LOG_ERR("[%s()] failed to write the pcie config space at offset %d\n",
+				__func__, (int)off);
+			return ret;
+		}
+
+		off = err_cap_offset + DLB2_PCI_ERR_UNCOR_STATUS;
+		if (rte_pci_read_config(pdev, &tmp, 4, off) != 4)
+			tmp = 0;
+
+		ret = rte_pci_write_config(pdev, &tmp, 4, off);
+		if (ret != 4) {
+			DLB2_LOG_ERR("[%s()] failed to write the pcie config space at offset %d\n",
+				__func__, (int)off);
+			return ret;
+		}
+	}
+
+	for (i = 16; i > 0; i--) {
+		off = (i - 1) * 4;
+		ret = rte_pci_write_config(pdev, &dword[i - 1], 4, off);
+		if (ret != 4) {
+			DLB2_LOG_ERR("[%s()] failed to write the pcie config space at offset %d\n",
+				__func__, (int)off);
+			return ret;
+		}
+	}
+
+	off = DLB2_PCI_CMD;
+	if (rte_pci_read_config(pdev, &cmd, 2, off) == 2) {
+		cmd &= ~DLB2_PCI_COMMAND_INTX_DISABLE;
+		if (rte_pci_write_config(pdev, &cmd, 2, off) != 2) {
+			DLB2_LOG_ERR("[%s()] failed to write the pci command\n",
+			       __func__);
+			return ret;
+		}
+	}
+
+	msix_cap_offset = dlb2_pci_find_capability(pdev,
+						   DLB2_PCI_CAP_ID_MSIX);
+	if (msix_cap_offset >= 0) {
+		off = msix_cap_offset + DLB2_PCI_MSIX_FLAGS;
+		if (rte_pci_read_config(pdev, &cmd, 2, off) == 2) {
+			cmd |= DLB2_PCI_MSIX_FLAGS_ENABLE;
+			cmd |= DLB2_PCI_MSIX_FLAGS_MASKALL;
+			if (rte_pci_write_config(pdev, &cmd, 2, off) != 2) {
+				DLB2_LOG_ERR("[%s()] failed to write msix flags\n",
+				       __func__);
+				return ret;
+			}
+		}
+
+		off = msix_cap_offset + DLB2_PCI_MSIX_FLAGS;
+		if (rte_pci_read_config(pdev, &cmd, 2, off) == 2) {
+			cmd &= ~DLB2_PCI_MSIX_FLAGS_MASKALL;
+			if (rte_pci_write_config(pdev, &cmd, 2, off) != 2) {
+				DLB2_LOG_ERR("[%s()] failed to write msix flags\n",
+				       __func__);
+				return ret;
+			}
+		}
+	}
+
+	off = DLB2_PCI_EXT_CAP_ID_ACS;
+	acs_cap_offset = dlb2_pci_find_ext_capability(pdev, off);
+
+	if (acs_cap_offset >= 0) {
+		uint16_t acs_cap, acs_ctrl, acs_mask;
+		off = acs_cap_offset + DLB2_PCI_ACS_CAP;
+		if (rte_pci_read_config(pdev, &acs_cap, 2, off) != 2)
+			acs_cap = 0;
+
+		off = acs_cap_offset + DLB2_PCI_ACS_CTRL;
+		if (rte_pci_read_config(pdev, &acs_ctrl, 2, off) != 2)
+			acs_ctrl = 0;
+
+		acs_mask = DLB2_PCI_ACS_SV | DLB2_PCI_ACS_RR;
+		acs_mask |= (DLB2_PCI_ACS_CR | DLB2_PCI_ACS_UF);
+		acs_ctrl |= (acs_cap & acs_mask);
+
+		ret = rte_pci_write_config(pdev, &acs_ctrl, 2, off);
+		if (ret != 2) {
+			DLB2_LOG_ERR("[%s()] failed to write the pcie config space at offset %d\n",
+				__func__, (int)off);
+			return ret;
+		}
+
+		off = acs_cap_offset + DLB2_PCI_ACS_CTRL;
+		if (rte_pci_read_config(pdev, &acs_ctrl, 2, off) != 2)
+			acs_ctrl = 0;
+
+		acs_mask = DLB2_PCI_ACS_RR | DLB2_PCI_ACS_CR;
+		acs_mask |= DLB2_PCI_ACS_EC;
+		acs_ctrl &= ~acs_mask;
+
+		off = acs_cap_offset + DLB2_PCI_ACS_CTRL;
+		ret = rte_pci_write_config(pdev, &acs_ctrl, 2, off);
+		if (ret != 2) {
+			DLB2_LOG_ERR("[%s()] failed to write the pcie config space at offset %d\n",
+				__func__, (int)off);
+			return ret;
+		}
+	}
+
+	return 0;
+}

drivers/event/dlb2/dlb2.c
@@ -2,6 +2,349 @@
  * Copyright(c) 2016-2020 Intel Corporation
  */
 
-#include <rte_log.h>
+#include <assert.h>
+#include <errno.h>
+#include <nmmintrin.h>
+#include <pthread.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/mman.h>
+#include <sys/fcntl.h>
 
+#include <rte_common.h>
+#include <rte_config.h>
+#include <rte_cycles.h>
+#include <rte_debug.h>
+#include <rte_dev.h>
+#include <rte_errno.h>
+#include <rte_eventdev.h>
+#include <rte_eventdev_pmd.h>
+#include <rte_io.h>
+#include <rte_kvargs.h>
+#include <rte_log.h>
+#include <rte_malloc.h>
+#include <rte_mbuf.h>
+#include <rte_prefetch.h>
+#include <rte_ring.h>
+#include <rte_string_fns.h>
+
+#include "dlb2_priv.h"
+#include "dlb2_inline_fns.h"
+
+/*
+ * Resources exposed to eventdev. Some values overridden at runtime using
+ * values returned by the DLB kernel driver.
+ */
+#if (RTE_EVENT_MAX_QUEUES_PER_DEV > UINT8_MAX)
+#error "RTE_EVENT_MAX_QUEUES_PER_DEV cannot fit in member max_event_queues"
+#endif
+
+struct process_local_port_data
+dlb2_port[DLB2_MAX_NUM_PORTS][DLB2_NUM_PORT_TYPES];
+
+#define DLB2_BASE_10 10
+
+static int
+dlb2_string_to_int(int *result, const char *str)
+{
+	long ret;
+	char *endptr;
+
+	if (str == NULL || result == NULL)
+		return -EINVAL;
+
+	errno = 0;
+	ret = strtol(str, &endptr, DLB2_BASE_10);
+	if (errno)
+		return -errno;
+
+	/* long int and int may be different width for some architectures */
+	if (ret < INT_MIN || ret > INT_MAX || endptr == str)
+		return -EINVAL;
+
+	*result = ret;
+	return 0;
+}
+
+static int
+set_numa_node(const char *key __rte_unused, const char *value, void *opaque)
+{
+	int *socket_id = opaque;
+	int ret;
+
+	ret = dlb2_string_to_int(socket_id, value);
+	if (ret < 0)
+		return ret;
+
+	if (*socket_id > RTE_MAX_NUMA_NODES)
+		return -EINVAL;
+	return 0;
+}
+
+static int
+set_max_num_events(const char *key __rte_unused,
+		   const char *value,
+		   void *opaque)
+{
+	int *max_num_events = opaque;
+	int ret;
+
+	if (value == NULL || opaque == NULL) {
+		DLB2_LOG_ERR("NULL pointer\n");
+		return -EINVAL;
+	}
+
+	ret = dlb2_string_to_int(max_num_events, value);
+	if (ret < 0)
+		return ret;
+
+	if (*max_num_events < 0 || *max_num_events >
+			DLB2_MAX_NUM_LDB_CREDITS) {
+		DLB2_LOG_ERR("dlb2: max_num_events must be between 0 and %d\n",
+			     DLB2_MAX_NUM_LDB_CREDITS);
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static int
+set_num_dir_credits(const char *key __rte_unused,
+		    const char *value,
+		    void *opaque)
+{
+	int *num_dir_credits = opaque;
+	int ret;
+
+	if (value == NULL || opaque == NULL) {
+		DLB2_LOG_ERR("NULL pointer\n");
+		return -EINVAL;
+	}
+
+	ret = dlb2_string_to_int(num_dir_credits, value);
+	if (ret < 0)
+		return ret;
+
+	if (*num_dir_credits < 0 ||
+	    *num_dir_credits > DLB2_MAX_NUM_DIR_CREDITS) {
+		DLB2_LOG_ERR("dlb2: num_dir_credits must be between 0 and %d\n",
+			     DLB2_MAX_NUM_DIR_CREDITS);
+		return -EINVAL;
+	}
+
+	return 0;
+}
+
+static int
+set_dev_id(const char *key __rte_unused,
+	   const char *value,
+	   void *opaque)
+{
+	int *dev_id = opaque;
+	int ret;
+
+	if (value == NULL || opaque == NULL) {
+		DLB2_LOG_ERR("NULL pointer\n");
+		return -EINVAL;
+	}
+
+	ret = dlb2_string_to_int(dev_id, value);
+	if (ret < 0)
+		return ret;
+
+	return 0;
+}
+
+static int
+set_cos(const char *key __rte_unused,
+	const char *value,
+	void *opaque)
+{
+	enum dlb2_cos *cos_id = opaque;
+	int x = 0;
+	int ret;
+
+	if (value == NULL || opaque == NULL) {
+		DLB2_LOG_ERR("NULL pointer\n");
+		return -EINVAL;
+	}
+
+	ret = dlb2_string_to_int(&x, value);
+	if (ret < 0)
+		return ret;
+
+	if (x != DLB2_COS_DEFAULT && (x < DLB2_COS_0 || x > DLB2_COS_3)) {
+		DLB2_LOG_ERR(
+			"COS %d out of range, must be DLB2_COS_DEFAULT or 0-3\n",
+			x);
+		return -EINVAL;
+	}
+
+	*cos_id = x;
+
+	return 0;
+}
+
+
+static int
+set_qid_depth_thresh(const char *key __rte_unused,
+		     const char *value,
+		     void *opaque)
+{
+	struct dlb2_qid_depth_thresholds *qid_thresh = opaque;
+	int first, last, thresh, i;
+
+	if (value == NULL || opaque == NULL) {
+		DLB2_LOG_ERR("NULL pointer\n");
+		return -EINVAL;
+	}
+
+	/* command line override may take one of the following 3 forms:
+	 * qid_depth_thresh=all:<threshold_value> ... all queues
+	 * qid_depth_thresh=qidA-qidB:<threshold_value> ... a range of queues
+	 * qid_depth_thresh=qid:<threshold_value> ... just one queue
+	 */
+	if (sscanf(value, "all:%d", &thresh) == 1) {
+		first = 0;
+		last = DLB2_MAX_NUM_QUEUES - 1;
+	} else if (sscanf(value, "%d-%d:%d", &first, &last, &thresh) == 3) {
+		/* we have everything we need */
+	} else if (sscanf(value, "%d:%d", &first, &thresh) == 2) {
+		last = first;
+	} else {
+		DLB2_LOG_ERR("Error parsing qid depth devarg. Should be all:val, qid-qid:val, or qid:val\n");
+		return -EINVAL;
+	}
+
+	if (first > last || first < 0 || last >= DLB2_MAX_NUM_QUEUES) {
+		DLB2_LOG_ERR("Error parsing qid depth devarg, invalid qid value\n");
+		return -EINVAL;
+	}
+
+	if (thresh < 0 || thresh > DLB2_MAX_QUEUE_DEPTH_THRESHOLD) {
+		DLB2_LOG_ERR("Error parsing qid depth devarg, threshold > %d\n",
+			     DLB2_MAX_QUEUE_DEPTH_THRESHOLD);
+		return -EINVAL;
+	}
+
+	for (i = first; i <= last; i++)
+		qid_thresh->val[i] = thresh; /* indexed by qid */
+
+	return 0;
+}
+
+int
+dlb2_primary_eventdev_probe(struct rte_eventdev *dev,
+			    const char *name,
+			    struct dlb2_devargs *dlb2_args)
+{
+	RTE_SET_USED(dev);
+	RTE_SET_USED(name);
+	RTE_SET_USED(dlb2_args);
+
+	return 0;
+}
+
+int
+dlb2_secondary_eventdev_probe(struct rte_eventdev *dev,
+			      const char *name)
+{
+	RTE_SET_USED(dev);
+	RTE_SET_USED(name);
+
+	return 0;
+}
+
+int
+dlb2_parse_params(const char *params,
+		  const char *name,
+		  struct dlb2_devargs *dlb2_args)
+{
+	int ret = 0;
+	static const char * const args[] = { NUMA_NODE_ARG,
+					     DLB2_MAX_NUM_EVENTS,
+					     DLB2_NUM_DIR_CREDITS,
+					     DEV_ID_ARG,
+					     DLB2_QID_DEPTH_THRESH_ARG,
+					     DLB2_COS_ARG,
+					     NULL };
+
+	if (params != NULL && params[0] != '\0') {
+		struct rte_kvargs *kvlist = rte_kvargs_parse(params, args);
+
+		if (kvlist == NULL) {
+			RTE_LOG(INFO, PMD,
+				"Ignoring unsupported parameters when creating device '%s'\n",
+				name);
+		} else {
+			int ret = rte_kvargs_process(kvlist, NUMA_NODE_ARG,
+						     set_numa_node,
+						     &dlb2_args->socket_id);
+			if (ret != 0) {
+				DLB2_LOG_ERR("%s: Error parsing numa node parameter",
+					     name);
+				rte_kvargs_free(kvlist);
+				return ret;
+			}
+
+			ret = rte_kvargs_process(kvlist, DLB2_MAX_NUM_EVENTS,
+						 set_max_num_events,
+						 &dlb2_args->max_num_events);
+			if (ret != 0) {
+				DLB2_LOG_ERR("%s: Error parsing max_num_events parameter",
+					     name);
+				rte_kvargs_free(kvlist);
+				return ret;
+			}
+
+			ret = rte_kvargs_process(kvlist,
+					DLB2_NUM_DIR_CREDITS,
+					set_num_dir_credits,
+					&dlb2_args->num_dir_credits_override);
+			if (ret != 0) {
+				DLB2_LOG_ERR("%s: Error parsing num_dir_credits parameter",
+					     name);
+				rte_kvargs_free(kvlist);
+				return ret;
+			}
+
+			ret = rte_kvargs_process(kvlist, DEV_ID_ARG,
+						 set_dev_id,
+						 &dlb2_args->dev_id);
+			if (ret != 0) {
+				DLB2_LOG_ERR("%s: Error parsing dev_id parameter",
+					     name);
+				rte_kvargs_free(kvlist);
+				return ret;
+			}
+
+			ret = rte_kvargs_process(
+					kvlist,
+					DLB2_QID_DEPTH_THRESH_ARG,
+					set_qid_depth_thresh,
+					&dlb2_args->qid_depth_thresholds);
+			if (ret != 0) {
+				DLB2_LOG_ERR("%s: Error parsing qid_depth_thresh parameter",
+					     name);
+				rte_kvargs_free(kvlist);
+				return ret;
+			}
+
+			ret = rte_kvargs_process(kvlist, DLB2_COS_ARG,
+						 set_cos,
+						 &dlb2_args->cos_id);
+			if (ret != 0) {
+				DLB2_LOG_ERR("%s: Error parsing cos parameter",
+					     name);
+				rte_kvargs_free(kvlist);
+				return ret;
+			}
+
+			rte_kvargs_free(kvlist);
+		}
+	}
+	return ret;
+}
 RTE_LOG_REGISTER(eventdev_dlb2_log_level, pmd.event.dlb2, NOTICE);

drivers/event/dlb2/meson.build
@@ -7,7 +7,10 @@ if not is_linux or not dpdk_conf.has('RTE_ARCH_X86_64')
         subdir_done()
 endif
 
-sources = files('dlb2.c')
+sources = files('dlb2.c',
+		'pf/dlb2_main.c',
+		'pf/dlb2_pf.c'
+)
 
 headers = files()
 

commit 366570d4172085e8d9f75b60bdc7def45df7258e

event/dlb2: fix disable PASID
In vfio-pci driver when PASID is enabled by default
DLB hardware puts DLB in SIOV mode. This breaks
DLB PF-PMD mode. For DLB PF-PMD mode to function properly
PASID needs to be disabled.

In this commit this issue is addressed and PASID is disabled
by writing a zero to PASID control register.

Fixes: 5433956d5185 ("event/dlb2: add eventdev probe")
Cc: stable@dpdk.org

Signed-off-by: Abdullah Sevincer <abdullah.sevincer@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/event/dlb2/pf/dlb2_main.c
@@ -514,6 +514,16 @@ dlb2_pf_reset(struct dlb2_dev *dlb2_dev)
 		}
 	}
 
+	/* Disable PASID incase it is enabled by default, which
+	 * breaks the DLB if enabled.
+	 */
+	off = RTE_PCI_PASID_CAP_OFFSET + RTE_PCI_PASID_CTRL;
+	if (rte_pci_set_pasid(pdev, off, false)) {
+		DLB2_LOG_ERR("[%s()] failed to write the pcie config space at offset %d\n",
+				__func__, (int)off);
+		return -1;
+	}
+
 	return 0;
 }
 