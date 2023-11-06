015548af9e244c8aadbfe75554a60241ff8f66da
Juraj Linkeš <juraj.linkes@pantheon.tech>
2023-11-06 18:15:51 +0100

    dts: port and virtual device docstring update
    Format according to the Google format and PEP257, with slight
    deviations.
    
    Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- dts/framework/testbed_model/__init__.py
+++ dts/framework/testbed_model/__init__.py
@@ -2,9 +2,19 @@
 # Copyright(c) 2022-2023 University of New Hampshire
 # Copyright(c) 2023 PANTHEON.tech s.r.o.
 
-"""
-This package contains the classes used to model the physical traffic generator,
-system under test and any other components that need to be interacted with.
+"""Testbed modelling.
+
+This package defines the testbed elements DTS works with:
+
+    * A system under test node: :class:`SutNode`,
+    * A traffic generator node: :class:`TGNode`,
+    * The ports of network interface cards (NICs) present on nodes: :class:`Port`,
+    * The logical cores of CPUs present on nodes: :class:`LogicalCore`,
+    * The virtual devices that can be created on nodes: :class:`VirtualDevice`,
+    * The operating systems running on nodes: :class:`LinuxSession` and :class:`PosixSession`.
+
+DTS needs to be able to connect to nodes and understand some of the hardware present on these nodes
+to properly build and test DPDK.
 """
 
 # pylama:ignore=W0611
