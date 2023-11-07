ovsrobot/dpdk

commit 27da6a1234148fef43687d324ab2221df0631b5c

eal: hide legacy atomics API for MSVC
It's discouraged to use rte_atomics APIs instead standard APIs should be
used from C11. Since MSVC is a new toolchain/platform combination block
visibility of the rte_atomic APIs from day 1.

Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
Acked-by: Morten Brørup <mb@smartsharesystems.com>

lib/eal/x86/include/rte_atomic.h
@@ -83,6 +83,8 @@ rte_smp_mb(void)
 
 #define rte_io_rmb() rte_compiler_barrier()
 
+#ifndef RTE_TOOLCHAIN_MSVC
+
 /**
  * Synchronization fence between threads based on the specified memory order.
  *
@@ -277,6 +279,8 @@ static inline int rte_atomic32_dec_and_test(rte_atomic32_t *v)
 #include "rte_atomic_32.h"
 #else
 #include "rte_atomic_64.h"
+#endif
+
 #endif
 
 #ifdef __cplusplus

lib/eal/include/generic/rte_atomic.h
@@ -161,6 +161,8 @@ static inline void rte_atomic_thread_fence(int memorder);
 
 /*------------------------- 16 bit atomic operations -------------------------*/
 
+#ifndef RTE_TOOLCHAIN_MSVC
+
 /**
  * Atomic compare and set.
  *
@@ -1068,6 +1070,8 @@ static inline void rte_atomic64_clear(rte_atomic64_t *v)
 }
 #endif
 
+#endif
+
 /*------------------------ 128 bit atomic operations -------------------------*/
 
 /**
@@ -1077,7 +1081,9 @@ typedef struct {
 	union {
 		uint64_t val[2];
 #ifdef RTE_ARCH_64
+#ifndef RTE_TOOLCHAIN_MSVC
 		__extension__ __int128 int128;
+#endif
 #endif
 	};
 } __rte_aligned(16) rte_int128_t;

commit 6517420273c87adb1bad5a484fd20015052b8bc7

eal: fix msvc conditional compile of rte atomic thread fence
MSVC does not expose legacy atomics. When the legacy atomics were
excluded rte_atomic_thread_fence was also unintentionally excluded

Adjust the conditional compilation check to expose
rte_atomic_thread_fence.

Fixes: 27da6a123414 ("eal: hide legacy atomics API for MSVC")

Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/eal/x86/include/rte_atomic.h
@@ -83,8 +83,6 @@ rte_smp_mb(void)
 
 #define rte_io_rmb() rte_compiler_barrier()
 
-#ifndef RTE_TOOLCHAIN_MSVC
-
 /**
  * Synchronization fence between threads based on the specified memory order.
  *
@@ -100,6 +98,8 @@ rte_atomic_thread_fence(rte_memory_order memorder)
 	else
 		__rte_atomic_thread_fence(memorder);
 }
+
+#ifndef RTE_TOOLCHAIN_MSVC
 
 /*------------------------- 16 bit atomic operations -------------------------*/
 