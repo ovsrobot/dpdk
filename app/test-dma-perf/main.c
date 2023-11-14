bbfbcf645b0dbaa07a0912c68c86df18502d6bdb
Jie Hai <haijie1@huawei.com>
2023-11-14 18:59:49 +0800

    app/dma-perf: replace strtok with reentrant version
    Multiple threads calling the same function may cause condition
    race issues, which often leads to abnormal behavior and can cause
    more serious vulnerabilities such as abnormal termination, denial
    of service, and compromised data integrity.
    
    The strtok() is non-reentrant, it is better to replace it with a
    reentrant version.
    
    Fixes: 623dc9364dc6 ("app/dma-perf: introduce DMA performance test")
    Cc: stable@dpdk.org
    
    Signed-off-by: Jie Hai <haijie1@huawei.com>
    Acked-by: Chengwen Feng <fengchengwen@huawei.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- app/test-dma-perf/main.c
+++ app/test-dma-perf/main.c
@@ -164,6 +164,7 @@ parse_lcore(struct test_configure *test_case, const char *value)
 	uint16_t len;
 	char *input;
 	struct lcore_dma_map_t *lcore_dma_map;
+	char *sp = NULL;
 
 	if (test_case == NULL || value == NULL)
 		return -1;
@@ -175,7 +176,7 @@ parse_lcore(struct test_configure *test_case, const char *value)
 
 	memset(lcore_dma_map, 0, sizeof(struct lcore_dma_map_t));
 
-	char *token = strtok(input, ", ");
+	char *token = strtok_r(input, ", ", &sp);
 	while (token != NULL) {
 		if (lcore_dma_map->cnt >= MAX_LCORE_NB) {
 			free(input);
@@ -185,7 +186,7 @@ parse_lcore(struct test_configure *test_case, const char *value)
 		uint16_t lcore_id = atoi(token);
 		lcore_dma_map->lcores[lcore_dma_map->cnt++] = lcore_id;
 
-		token = strtok(NULL, ", ");
+		token = strtok_r(NULL, ", ", &sp);
 	}
 
 	free(input);
@@ -201,6 +202,7 @@ parse_lcore_dma(struct test_configure *test_case, const char *value)
 	char *start, *end, *substr;
 	uint16_t lcore_id;
 	int ret = 0;
+	char *sp = NULL;
 
 	if (test_case == NULL || value == NULL)
 		return -1;
@@ -216,7 +218,7 @@ parse_lcore_dma(struct test_configure *test_case, const char *value)
 		goto out;
 	}
 
-	substr = strtok(addrs, ",");
+	substr = strtok_r(addrs, ",", &sp);
 	if (substr == NULL) {
 		fprintf(stderr, "No input DMA address\n");
 		ret = -1;
@@ -258,7 +260,7 @@ parse_lcore_dma(struct test_configure *test_case, const char *value)
 		strlcpy(lcore_dma_map->dma_names[lcore_dma_map->cnt], ptrs[1],
 				RTE_DEV_NAME_MAX_LEN);
 		lcore_dma_map->cnt++;
-		substr = strtok(NULL, ",");
+		substr = strtok_r(NULL, ",", &sp);
 	} while (substr != NULL);
 
 out:
@@ -486,6 +488,7 @@ main(int argc, char *argv[])
 	char *rst_path_ptr = NULL;
 	char rst_path[PATH_MAX];
 	int new_argc;
+	char *sp = NULL;
 
 	memset(args, 0, sizeof(args));
 
@@ -504,7 +507,7 @@ main(int argc, char *argv[])
 	}
 	if (rst_path_ptr == NULL) {
 		strlcpy(rst_path, cfg_path_ptr, PATH_MAX);
-		char *token = strtok(basename(rst_path), ".");
+		char *token = strtok_r(basename(rst_path), ".", &sp);
 		if (token == NULL) {
 			printf("Config file error.\n");
 			return -1;
