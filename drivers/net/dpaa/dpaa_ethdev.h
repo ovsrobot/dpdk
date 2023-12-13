2aef601fd4705ae2ed45e448bc2da048ddd59a57
Stephen Hemminger <stephen@networkplumber.org>
2023-12-13 08:35:40 -0800

    net/dpaa: replace RTE_LOG_DP with rte_log_dp
    Want datapath logs to use own logtype.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/dpaa/dpaa_ethdev.h
+++ drivers/net/dpaa/dpaa_ethdev.h
@@ -232,6 +232,6 @@ extern int dpaa_logtype_pmd;
 
 /* DP Logs, toggled out at compile time if level lower than current level */
 #define DPAA_DP_LOG(level, fmt, args...) \
-	RTE_LOG_DP(level, PMD, fmt, ## args)
+	rte_log_dp(RTE_LOG_ ## level, dpaa_logtype_pmd, fmt, ## args)
 
 #endif
