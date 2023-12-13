38f9f9568ca450d68b3d7f65c8b5f3f83f29c2db
Stephen Hemminger <stephen@networkplumber.org>
2023-12-12 20:16:39 -0800

    net/octeon_ep: replace RTE_LOG_DP with rte_log_dp
    Want datapath logs to use own logtype.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/octeon_ep/otx_ep_rxtx.c
+++ drivers/net/octeon_ep/otx_ep_rxtx.c
@@ -884,9 +884,8 @@ otx_ep_recv_pkts(void *rx_queue, struct rte_mbuf **rx_pkts, uint16_t nb_pkts)
 		next_fetch = (pkts == new_pkts - 1) ? 0 : 1;
 		oq_pkt = otx_ep_droq_read_packet(otx_ep, droq, next_fetch);
 		if (!oq_pkt) {
-			RTE_LOG_DP(ERR, PMD,
-				   "DROQ read pkt failed pending %" PRIu64
-				    "last_pkt_count %" PRIu64 "new_pkts %d.\n",
+			otx_ep_log_dp(ERR, "DROQ read pkt failed pending %" PRIu64
+				    "last_pkt_count %" PRIu64 "new_pkts %d.",
 				   droq->pkts_pending, droq->last_pkt_count,
 				   new_pkts);
 			droq->stats.rx_err++;
