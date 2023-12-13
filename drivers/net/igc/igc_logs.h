600cfdd29b5e07ba72e99e22ec5363df5f3a7f05
Stephen Hemminger <stephen@networkplumber.org>
2023-12-12 20:16:36 -0800

    net/igc: replace RTE_LOG_DP with rte_log_dp
    Want datapath logs to use own logtype.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Acked-by: Junfeng Guo <junfeng.guo@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/igc/igc_logs.h
+++ drivers/net/igc/igc_logs.h
@@ -21,15 +21,17 @@ extern int igc_logtype_driver;
 #define PMD_INIT_FUNC_TRACE() PMD_INIT_LOG(DEBUG, " >>")
 
 #ifdef RTE_ETHDEV_DEBUG_RX
-#define PMD_RX_LOG(level, fmt, args...) \
-	RTE_LOG(level, PMD, "%s(): " fmt "\n", __func__, ## args)
+#define PMD_RX_LOG(level, fmt, args...)			\
+	rte_log(RTE_LOG_ ## level, igc_logtype_driver,	\
+		"%s(): " fmt "\n", __func__, ## args)
 #else
 #define PMD_RX_LOG(level, fmt, args...) do { } while (0)
 #endif
 
 #ifdef RTE_ETHDEV_DEBUG_TX
-#define PMD_TX_LOG(level, fmt, args...) \
-	RTE_LOG(level, PMD, "%s(): " fmt "\n", __func__, ## args)
+#define PMD_TX_LOG(level, fmt, args...)			\
+	rte_log(RTE_LOG_ ## level, igc_logtype_driver,	\
+		"%s(): " fmt "\n", __func__, ## args)
 #else
 #define PMD_TX_LOG(level, fmt, args...) do { } while (0)
 #endif
