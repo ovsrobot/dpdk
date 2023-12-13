fd759b731436f91f3eb42eab69353c9793422e8e
Stephen Hemminger <stephen@networkplumber.org>
2023-12-13 08:35:37 -0800

    net/atlantic: replace RTE_LOG_DP with rte_log_dp
    Want datapath logs to use own logtype.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/atlantic/atl_logs.h
+++ drivers/net/atlantic/atl_logs.h
@@ -14,18 +14,19 @@ extern int atl_logtype_init;
 
 #define PMD_INIT_FUNC_TRACE() PMD_INIT_LOG(DEBUG, " >>")
 
-#define PMD_RX_LOG(level, fmt, args...) \
-	RTE_LOG_DP(level, PMD, "%s(): " fmt "\n", __func__, ## args)
-
-#define PMD_TX_LOG(level, fmt, args...) \
-	RTE_LOG_DP(level, PMD, "%s(): " fmt "\n", __func__, ## args)
-
 extern int atl_logtype_driver;
 #define PMD_DRV_LOG_RAW(level, fmt, args...) \
 	rte_log(RTE_LOG_ ## level, atl_logtype_driver, "%s(): " fmt, \
 		__func__, ## args)
 
 #define PMD_DRV_LOG(level, fmt, args...) \
-	PMD_DRV_LOG_RAW(level, fmt "\n", ## args)
+	rte_log_dp(RTE_LOG_ ## level, atl_logtype_driver, fmt "\n", \
+		   ## args)
+
+#define PMD_RX_LOG(level, fmt, args...) \
+	PMD_DRV_LOG(level, fmt, ## args)
+
+#define PMD_TX_LOG(level, fmt, args...) \
+	PMD_DRV_LOG(level, fmt, ## args)
 
 #endif
