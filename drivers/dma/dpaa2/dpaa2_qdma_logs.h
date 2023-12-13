3e273019b17c6c9f6beb8bdc1f815a373e684fb8
Stephen Hemminger <stephen@networkplumber.org>
2023-12-12 17:42:41 -0800

    dma/dpaa, dma/dpaa2: replace RTE_LOG_DP with rte_log_dp
    Want datapath logs to use own logtype.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/dma/dpaa2/dpaa2_qdma_logs.h
+++ drivers/dma/dpaa2/dpaa2_qdma_logs.h
@@ -30,7 +30,8 @@ extern int dpaa2_qdma_logtype;
 
 /* DP Logs, toggled out at compile time if level lower than current level */
 #define DPAA2_QDMA_DP_LOG(level, fmt, args...) \
-	RTE_LOG_DP(level, PMD, "dpaa2_qdma: " fmt "\n", ## args)
+	rte_log_dp(RTE_LOG_ ## level, dpaa2_qdma_logtype, \
+		   "dpaa2_qdma: " fmt "\n", ## args)
 
 #define DPAA2_QDMA_DP_DEBUG(fmt, args...) \
 	DPAA2_QDMA_DP_LOG(DEBUG, fmt, ## args)
