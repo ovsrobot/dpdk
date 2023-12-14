3ded35308a8bc47bed1c57da7a05257d8e373b0d
Brian Dooley <brian.dooley@intel.com>
2023-12-14 15:15:44 +0000

    crypto/ipsec_mb: unified IPsec MB interface
    Currently IPsec MB provides both the JOB API and direct API.
    AESNI_MB PMD is using the JOB API codepath while ZUC, KASUMI, SNOW3G,
    CHACHA20_POLY1305 and AESNI_GCM are using the direct API.
    Instead of using the direct API for these PMDs, they should now make
    use of the JOB API codepath. This would remove all use of the IPsec MB
    direct API for these PMDs.
    
    Signed-off-by: Brian Dooley <brian.dooley@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/crypto/ipsec_mb/pmd_aesni_mb.c
+++ drivers/crypto/ipsec_mb/pmd_aesni_mb.c
@@ -761,7 +761,7 @@ aesni_mb_set_session_aead_parameters(const IMB_MGR *mb_mgr,
 }
 
 /** Configure a aesni multi-buffer session from a crypto xform chain */
-static int
+int
 aesni_mb_session_configure(IMB_MGR *mb_mgr,
 		void *priv_sess,
 		const struct rte_crypto_sym_xform *xform)
@@ -2131,7 +2131,7 @@ set_job_null_op(IMB_JOB *job, struct rte_crypto_op *op)
 }
 
 #if IMB_VERSION(1, 2, 0) < IMB_VERSION_NUM
-static uint16_t
+uint16_t
 aesni_mb_dequeue_burst(void *queue_pair, struct rte_crypto_op **ops,
 		uint16_t nb_ops)
 {
@@ -2321,7 +2321,7 @@ flush_mb_mgr(struct ipsec_mb_qp *qp, IMB_MGR *mb_mgr,
 	return processed_ops;
 }
 
-static uint16_t
+uint16_t
 aesni_mb_dequeue_burst(void *queue_pair, struct rte_crypto_op **ops,
 		uint16_t nb_ops)
 {
@@ -2456,7 +2456,7 @@ verify_sync_dgst(struct rte_crypto_sym_vec *vec,
 	return k;
 }
 
-static uint32_t
+uint32_t
 aesni_mb_process_bulk(struct rte_cryptodev *dev __rte_unused,
 	struct rte_cryptodev_sym_session *sess, union rte_crypto_sym_ofs sofs,
 	struct rte_crypto_sym_vec *vec)
