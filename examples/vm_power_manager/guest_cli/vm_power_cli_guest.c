ovsrobot/dpdk

commit 0e8f47491f090f44a4956429cb27f6942b6618b0

examples/vm_power: add command to query CPU frequency
Add command and related logic to query CPU frequencies
either for specified CPU or all cores.

Signed-off-by: Marcin Hajkowski <marcinx.hajkowski@intel.com>
Tested-by: David Hunt <david.hunt@intel.com>
Acked-by: Lee Daly <lee.daly@intel.com>

examples/vm_power_manager/guest_cli/vm_power_cli_guest.c
@@ -107,14 +107,14 @@ set_policy_defaults(struct channel_packet *pkt)
 	pkt->workload = LOW;
 	pkt->policy_to_use = TIME;
 	pkt->command = PKT_POLICY;
-	strcpy(pkt->vm_name, "ubuntu2");
+	strlcpy(pkt->vm_name, "ubuntu2", sizeof(pkt->vm_name));
 
 	return 0;
 }
 
-static void cmd_quit_parsed(__attribute__((unused)) void *parsed_result,
-				__attribute__((unused)) struct cmdline *cl,
-			    __attribute__((unused)) void *data)
+static void cmd_quit_parsed(__rte_unused void *parsed_result,
+		__rte_unused struct cmdline *cl,
+		__rte_unused void *data)
 {
 	unsigned lcore_id;
 
@@ -139,10 +139,126 @@ cmdline_parse_inst_t cmd_quit = {
 
 /* *** VM operations *** */
 
-struct cmd_set_cpu_freq_result {
-	cmdline_fixed_string_t set_cpu_freq;
-	uint8_t lcore_id;
-	cmdline_fixed_string_t cmd;
+struct cmd_freq_list_result {
+	cmdline_fixed_string_t query_freq;
+	cmdline_fixed_string_t cpu_num;
+};
+
+static int
+query_freq_list(struct channel_packet *pkt, unsigned int lcore_id)
+{
+	int ret;
+	ret = rte_power_guest_channel_send_msg(pkt, lcore_id);
+	if (ret < 0) {
+		RTE_LOG(ERR, GUEST_CLI, "Error sending message.\n");
+		return -1;
+	}
+	return 0;
+}
+
+static int
+receive_freq_list(struct channel_packet_freq_list *pkt_freq_list,
+		unsigned int lcore_id)
+{
+	int ret;
+
+	ret = rte_power_guest_channel_receive_msg(pkt_freq_list,
+			sizeof(struct channel_packet_freq_list),
+			lcore_id);
+	if (ret < 0) {
+		RTE_LOG(ERR, GUEST_CLI, "Error receiving message.\n");
+		return -1;
+	}
+	if (pkt_freq_list->command != CPU_POWER_FREQ_LIST) {
+		RTE_LOG(ERR, GUEST_CLI, "Unexpected message received.\n");
+		return -1;
+	}
+	return 0;
+}
+
+static void
+cmd_query_freq_list_parsed(void *parsed_result,
+		__rte_unused struct cmdline *cl,
+		__rte_unused void *data)
+{
+	struct cmd_freq_list_result *res = parsed_result;
+	unsigned int lcore_id;
+	struct channel_packet_freq_list pkt_freq_list;
+	struct channel_packet pkt;
+	bool query_list = false;
+	int ret;
+	char *ep;
+
+	memset(&pkt, 0, sizeof(struct channel_packet));
+	memset(&pkt_freq_list, 0, sizeof(struct channel_packet_freq_list));
+
+	if (!strcmp(res->cpu_num, "all")) {
+
+		/* Get first enabled lcore. */
+		lcore_id = rte_get_next_lcore(-1,
+				0,
+				0);
+		if (lcore_id == RTE_MAX_LCORE) {
+			cmdline_printf(cl, "Enabled core not found.\n");
+			return;
+		}
+
+		pkt.command = CPU_POWER_QUERY_FREQ_LIST;
+		strlcpy(pkt.vm_name, policy.vm_name, sizeof(pkt.vm_name));
+		query_list = true;
+	} else {
+		errno = 0;
+		lcore_id = (unsigned int)strtol(res->cpu_num, &ep, 10);
+		if (errno != 0 || lcore_id >= MAX_VCPU_PER_VM ||
+			ep == res->cpu_num) {
+			cmdline_printf(cl, "Invalid parameter provided.\n");
+			return;
+		}
+		pkt.command = CPU_POWER_QUERY_FREQ;
+		strlcpy(pkt.vm_name, policy.vm_name, sizeof(pkt.vm_name));
+		pkt.resource_id = lcore_id;
+	}
+
+	ret = query_freq_list(&pkt, lcore_id);
+	if (ret < 0) {
+		cmdline_printf(cl, "Error during sending frequency list query.\n");
+		return;
+	}
+
+	ret = receive_freq_list(&pkt_freq_list, lcore_id);
+	if (ret < 0) {
+		cmdline_printf(cl, "Error during frequency list reception.\n");
+		return;
+	}
+	if (query_list) {
+		unsigned int i;
+		for (i = 0; i < pkt_freq_list.num_vcpu; ++i)
+			cmdline_printf(cl, "Frequency of [%d] vcore is %d.\n",
+					i,
+					pkt_freq_list.freq_list[i]);
+	} else {
+		cmdline_printf(cl, "Frequency of [%d] vcore is %d.\n",
+				lcore_id,
+				pkt_freq_list.freq_list[lcore_id]);
+	}
+}
+
+cmdline_parse_token_string_t cmd_query_freq_token =
+	TOKEN_STRING_INITIALIZER(struct cmd_freq_list_result, query_freq, "query_cpu_freq");
+cmdline_parse_token_string_t cmd_query_freq_cpu_num_token =
+	TOKEN_STRING_INITIALIZER(struct cmd_freq_list_result, cpu_num, NULL);
+
+cmdline_parse_inst_t cmd_query_freq_list = {
+	.f = cmd_query_freq_list_parsed,  /* function to call */
+	.data = NULL,      /* 2nd arg of func */
+	.help_str = "query_cpu_freq <core_num>|all, request"
+				" information regarding virtual core frequencies."
+				" The keyword 'all' will query list of all vcores for the VM",
+	.tokens = {        /* token list, NULL terminated */
+		(void *)&cmd_query_freq_token,
+		(void *)&cmd_query_freq_cpu_num_token,
+		NULL,
+	},
 };
 
 static int
@@ -171,9 +287,15 @@ check_response_cmd(unsigned int lcore_id, int *result)
 	return 0;
 }
 
+struct cmd_set_cpu_freq_result {
+	cmdline_fixed_string_t set_cpu_freq;
+	uint8_t lcore_id;
+	cmdline_fixed_string_t cmd;
+};
+
 static void
 cmd_set_cpu_freq_parsed(void *parsed_result, struct cmdline *cl,
-		       __attribute__((unused)) void *data)
+	       __rte_unused void *data)
 {
 	int ret = -1;
 	struct cmd_set_cpu_freq_result *res = parsed_result;
@@ -259,7 +381,7 @@ send_policy(struct channel_packet *pkt, struct cmdline *cl)
 
 static void
 cmd_send_policy_parsed(void *parsed_result, struct cmdline *cl,
-		       __attribute__((unused)) void *data)
+		__rte_unused void *data)
 {
 	int ret = -1;
 	struct cmd_send_policy_result *res = parsed_result;
@@ -295,11 +417,12 @@ cmdline_parse_ctx_t main_ctx[] = {
 		(cmdline_parse_inst_t *)&cmd_quit,
 		(cmdline_parse_inst_t *)&cmd_send_policy_set,
 		(cmdline_parse_inst_t *)&cmd_set_cpu_freq_set,
+		(cmdline_parse_inst_t *)&cmd_query_freq_list,
 		NULL,
 };
 
 void
-run_cli(__attribute__((unused)) void *arg)
+run_cli(__rte_unused void *arg)
 {
 	struct cmdline *cl;
 

commit 9cff5bb2e8f64b5ed9a5cdfa8962688ba1826827

examples/vm_power_manager: fix lcore ID restriction
Currently the config option allows lcore IDs up to 255,
irrespective of RTE_MAX_LCORES and needs to be fixed.

The patch allows config options based on DPDK config.

Fixes: 0e8f47491f09 ("examples/vm_power: add command to query CPU frequency")
Cc: marcinx.hajkowski@intel.com
Cc: stable@dpdk.org

Signed-off-by: Sivaprasad Tummala <sivaprasad.tummala@amd.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

examples/vm_power_manager/guest_cli/vm_power_cli_guest.c
@@ -401,7 +401,7 @@ check_response_cmd(unsigned int lcore_id, int *result)
 
 struct cmd_set_cpu_freq_result {
 	cmdline_fixed_string_t set_cpu_freq;
-	uint8_t lcore_id;
+	uint16_t lcore_id;
 	cmdline_fixed_string_t cmd;
 };
 
@@ -444,7 +444,7 @@ cmdline_parse_token_string_t cmd_set_cpu_freq =
 			set_cpu_freq, "set_cpu_freq");
 cmdline_parse_token_num_t cmd_set_cpu_freq_core_num =
 	TOKEN_NUM_INITIALIZER(struct cmd_set_cpu_freq_result,
-			lcore_id, RTE_UINT8);
+			lcore_id, RTE_UINT16);
 cmdline_parse_token_string_t cmd_set_cpu_freq_cmd_cmd =
 	TOKEN_STRING_INITIALIZER(struct cmd_set_cpu_freq_result,
 			cmd, "up#down#min#max#enable_turbo#disable_turbo");