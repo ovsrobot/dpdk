c03561a8a575b29a8c3632e39afcf6e9d91ec796
Amit Prakash Shukla <amitprakashs@marvell.com>
2024-02-10 01:13:45 +0530

    common/cnxk: dma result to an offset of the event
    Adds support to configure writing result to offset of the DMA
    response event.
    
    Signed-off-by: Amit Prakash Shukla <amitprakashs@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/common/cnxk/roc_idev_priv.h
+++ drivers/common/cnxk/roc_idev_priv.h
@@ -37,6 +37,7 @@ struct idev_cfg {
 	struct roc_nix_list roc_nix_list;
 	plt_spinlock_t nix_inl_dev_lock;
 	plt_spinlock_t npa_dev_lock;
+	uint8_t dma_cs_offset;
 };
 
 /* Generic */
@@ -55,6 +56,8 @@ void idev_sso_pffunc_set(uint16_t sso_pf_func);
 uint16_t idev_sso_pffunc_get(void);
 struct roc_sso *idev_sso_get(void);
 void idev_sso_set(struct roc_sso *sso);
+void idev_dma_cs_offset_set(uint8_t offset);
+uint8_t idev_dma_cs_offset_get(void);
 
 /* idev lmt */
 uint16_t idev_lmt_pffunc_get(void);
