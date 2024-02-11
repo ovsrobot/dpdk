91b7f0e8fa1dedfe33d49b0988f98c2c9dfa618a
Kumara Parameshwaran <kumaraparamesh92@gmail.com>
2024-02-11 10:25:11 +0530

    gro : packets not getting flushed in heavy-weight mode API
    In heavy-weight mode GRO which is based on timer, the GRO packets
    will not be flushed in spite of timer expiry if there is no packet
    in the current poll. If timer mode GRO is enabled the
    rte_gro_timeout_flush API should be invoked.
    
    Signed-off-by: Kumara Parameshwaran <kumaraparamesh92@gmail.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- app/test-pmd/csumonly.c
+++ app/test-pmd/csumonly.c
@@ -870,16 +870,22 @@ pkt_burst_checksum_forward(struct fwd_stream *fs)
 
 	/* receive a burst of packet */
 	nb_rx = common_fwd_stream_receive(fs, pkts_burst, nb_pkt_per_burst);
+#ifndef RTE_LIB_GRO
 	if (unlikely(nb_rx == 0))
 		return false;
-
+#else
+	gro_enable = gro_ports[fs->rx_port].enable;
+	if (unlikely(nb_rx == 0)) {
+		if (!gro_enable || (gro_flush_cycles == GRO_DEFAULT_FLUSH_CYCLES))
+			return false;
+		if ((rte_gro_get_pkt_count(current_fwd_lcore()->gro_ctx) == 0))
+			return false;
+	}
+#endif
 	rx_bad_ip_csum = 0;
 	rx_bad_l4_csum = 0;
 	rx_bad_outer_l4_csum = 0;
 	rx_bad_outer_ip_csum = 0;
-#ifdef RTE_LIB_GRO
-	gro_enable = gro_ports[fs->rx_port].enable;
-#endif
 
 	txp = &ports[fs->tx_port];
 	tx_offloads = txp->dev_conf.txmode.offloads;
