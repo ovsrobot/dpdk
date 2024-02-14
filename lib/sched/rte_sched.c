fc5c37ef6b6f04cf72b71b59ead7486496cb9f8d
Tyler Retzlaff <roretzla@linux.microsoft.com>
2024-02-13 20:17:39 -0800

    sched: use C11 alignas
    Replace use of __rte_aligned_16 with C11 alignas(16) and garbage collect
    the __rte_aligned_16 macro which was only used once.
    
    Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/sched/rte_sched.c
+++ lib/sched/rte_sched.c
@@ -2,6 +2,7 @@
  * Copyright(c) 2010-2014 Intel Corporation
  */
 
+#include <stdalign.h>
 #include <stdio.h>
 #include <string.h>
 
@@ -193,7 +194,7 @@ struct rte_sched_subport {
 
 	/* Bitmap */
 	struct rte_bitmap *bmp;
-	uint32_t grinder_base_bmp_pos[RTE_SCHED_PORT_N_GRINDERS] __rte_aligned_16;
+	alignas(16) uint32_t grinder_base_bmp_pos[RTE_SCHED_PORT_N_GRINDERS];
 
 	/* Grinders */
 	struct rte_sched_grinder grinder[RTE_SCHED_PORT_N_GRINDERS];
