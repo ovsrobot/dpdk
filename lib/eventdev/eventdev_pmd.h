91eeed8540d82c2e676236e4be7a62a515077c20
Tyler Retzlaff <roretzla@linux.microsoft.com>
2024-02-23 11:03:45 -0800

    eventdev: use C11 alignas
    The current location used for __rte_aligned(a) for alignment of types
    and variables is not compatible with MSVC. There is only a single
    location accepted by both toolchains.
    
    For variables standard C11 offers alignas(a) supported by conformant
    compilers i.e. both MSVC and GCC.
    
    For types the standard offers no alignment facility that compatibly
    interoperates with C and C++ but may be achieved by relocating the
    placement of __rte_aligned(a) to the aforementioned location accepted
    by all currently supported toolchains.
    
    To allow alignment for both compilers do the following:
    
    * Move __rte_aligned from the end of {struct,union} definitions to
      be between {struct,union} and tag.
    
      The placement between {struct,union} and the tag allows the desired
      alignment to be imparted on the type regardless of the toolchain being
      used for all of GCC, LLVM, MSVC compilers building both C and C++.
    
    * Replace use of __rte_aligned(a) on variables/fields with alignas(a).
    
    Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
    Acked-by: Morten Brørup <mb@smartsharesystems.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eventdev/eventdev_pmd.h
+++ lib/eventdev/eventdev_pmd.h
@@ -107,7 +107,7 @@ struct rte_eventdev_global {
  * This structure is safe to place in shared memory to be common among
  * different processes in a multi-process configuration.
  */
-struct rte_eventdev_data {
+struct __rte_cache_aligned rte_eventdev_data {
 	int socket_id;
 	/**< Socket ID where memory is allocated */
 	uint8_t dev_id;
@@ -146,10 +146,10 @@ struct rte_eventdev_data {
 
 	uint64_t reserved_64s[4]; /**< Reserved for future fields */
 	void *reserved_ptrs[4];	  /**< Reserved for future fields */
-} __rte_cache_aligned;
+};
 
 /** @internal The data structure associated with each event device. */
-struct rte_eventdev {
+struct __rte_cache_aligned rte_eventdev {
 	struct rte_eventdev_data *data;
 	/**< Pointer to device data */
 	struct eventdev_ops *dev_ops;
@@ -189,7 +189,7 @@ struct rte_eventdev {
 
 	uint64_t reserved_64s[3]; /**< Reserved for future fields */
 	void *reserved_ptrs[3];	  /**< Reserved for future fields */
-} __rte_cache_aligned;
+};
 
 extern struct rte_eventdev *rte_eventdevs;
 /** @internal The pool of rte_eventdev structures. */
