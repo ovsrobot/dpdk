fe716d4983afe69b4f9ebb4e658cd86387751066
Tyler Retzlaff <roretzla@linux.microsoft.com>
2024-02-24 00:21:49 -0800

    cryptodev: expand rte markers empty when building with MSVC
    RTE_MARKER typedefs are a GCC extension unsupported by MSVC. Use
    __rte_marker to expand RTE_MARKER fields empty in rte_security_session
    struct.
    
    Make slight adjustment to cacheline1 __rte_cache_min_aligned as to
    continue maintaining correct alignment with or without the marker
    expanding empty.
    
    Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/cryptodev/cryptodev_pmd.h
+++ lib/cryptodev/cryptodev_pmd.h
@@ -139,7 +139,7 @@ struct cryptodev_driver {
  * has a fixed algo, key, op-type, digest_len etc.
  */
 struct rte_cryptodev_sym_session {
-	RTE_MARKER cacheline0;
+	__rte_marker(RTE_MARKER, cacheline0)
 	uint64_t opaque_data;
 	/**< Can be used for external metadata */
 	uint32_t sess_data_sz;
@@ -151,7 +151,8 @@ struct rte_cryptodev_sym_session {
 	rte_iova_t driver_priv_data_iova;
 	/**< Session driver data IOVA address */
 
-	RTE_MARKER cacheline1 __rte_cache_min_aligned;
+	__rte_cache_min_aligned
+	__rte_marker(RTE_MARKER, cacheline1)
 	/**< Second cache line - start of the driver session data */
 	uint8_t driver_priv_data[];
 	/**< Driver specific session data, variable size */
