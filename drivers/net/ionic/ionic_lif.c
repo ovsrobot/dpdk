9ccf02d64f14efd29e71fa5ee69790fc0408c663
Tyler Retzlaff <roretzla@linux.microsoft.com>
2024-02-26 21:41:28 -0800

    net/ionic: use mbuf descriptor accessors
    RTE_MARKER typedefs are a GCC extension unsupported by MSVC. Use
    new rte_mbuf_rearm_data and rte_mbuf_rx_descriptor_fields1 accessors
    that provide a compatible type pointer without using the marker fields.
    
    Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/ionic/ionic_lif.c
+++ drivers/net/ionic/ionic_lif.c
@@ -744,8 +744,7 @@ ionic_rx_rearm_data(struct ionic_lif *lif)
 
 	rte_compiler_barrier();
 
-	RTE_BUILD_BUG_ON(sizeof(rxm.rearm_data[0]) != sizeof(uint64_t));
-	return rxm.rearm_data[0];
+	return *rte_mbuf_rearm_data(&rxm);
 }
 
 static uint64_t
@@ -762,8 +761,7 @@ ionic_rx_seg_rearm_data(struct ionic_lif *lif)
 
 	rte_compiler_barrier();
 
-	RTE_BUILD_BUG_ON(sizeof(rxm.rearm_data[0]) != sizeof(uint64_t));
-	return rxm.rearm_data[0];
+	return *rte_mbuf_rearm_data(&rxm);
 }
 
 int
