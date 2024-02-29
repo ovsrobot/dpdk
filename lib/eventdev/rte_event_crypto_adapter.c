84d6ff28259b13b8f696546eb6c10eebde19c657
Stephen Hemminger <stephen@networkplumber.org>
2024-02-29 14:57:51 -0800

    eventdev: replace use of fixed size rte_memcpy
    Automatically generated by devtools/cocci/rte_memcpy.cocci
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eventdev/rte_event_crypto_adapter.c
+++ lib/eventdev/rte_event_crypto_adapter.c
@@ -647,7 +647,7 @@ eca_ops_enqueue_burst(struct event_crypto_adapter *adapter,
 			continue;
 		}
 
-		rte_memcpy(ev, &m_data->response_info, sizeof(*ev));
+		memcpy(ev, &m_data->response_info, sizeof(*ev));
 		ev->event_ptr = ops[i];
 		ev->event_type = RTE_EVENT_TYPE_CRYPTODEV;
 		if (adapter->implicit_release_disabled)
