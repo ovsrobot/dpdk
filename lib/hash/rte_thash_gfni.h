ovsrobot/dpdk

commit 944a03a5cfc132d6ac4932669cfb8444de73cd79

hash: fix MSVC link on GFNI stubs
Make the GFNI stub functions always built.
This solves the conditional linking problem.
If GFNI is available, they will never get used.

Fixes: 07d836e5929d ("hash: uninline GFNI stubs")

Reported-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
Tested-by: Tyler Retzlaff <roretzla@linux.microsoft.com>

lib/hash/rte_thash_gfni.h
@@ -9,6 +9,7 @@
 extern "C" {
 #endif
 
+#include <rte_compat.h>
 #include <rte_log.h>
 
 #ifdef RTE_ARCH_X86
@@ -17,11 +18,22 @@ extern "C" {
 
 #endif
 
-#ifndef RTE_THASH_GFNI_DEFINED
+/**
+ * @internal
+ * Stubs only used when GFNI is not available.
+ */
+__rte_internal
+uint32_t
+rte_thash_gfni_stub(const uint64_t *mtrx, const uint8_t *key, int len);
 
+__rte_internal
+void
+rte_thash_gfni_bulk_stub(const uint64_t *mtrx, int len, uint8_t *tuple[],
+	uint32_t val[], uint32_t num);
+
+#ifndef RTE_THASH_GFNI_DEFINED
 /**
  * Calculate Toeplitz hash.
- * Dummy implementation.
  *
  * @param m
  *  Pointer to the matrices generated from the corresponding
@@ -33,12 +45,14 @@ extern "C" {
  * @return
  *  Calculated Toeplitz hash value.
  */
-uint32_t
-rte_thash_gfni(const uint64_t *mtrx, const uint8_t *key, int len);
+static inline uint32_t
+rte_thash_gfni(const uint64_t *mtrx, const uint8_t *key, int len)
+{
+	return rte_thash_gfni_stub(mtrx, key, len);
+}
 
 /**
  * Bulk implementation for Toeplitz hash.
- * Dummy implementation.
  *
  * @param m
  *  Pointer to the matrices generated from the corresponding
@@ -53,9 +67,12 @@ rte_thash_gfni(const uint64_t *mtrx, const uint8_t *key, int len);
  * @param num
  *  Number of tuples to hash.
  */
-void
+static inline void
 rte_thash_gfni_bulk(const uint64_t *mtrx, int len, uint8_t *tuple[],
-	uint32_t val[], uint32_t num);
+	uint32_t val[], uint32_t num)
+{
+	return rte_thash_gfni_bulk_stub(mtrx, len, tuple, val, num);
+}
 
 #endif /* RTE_THASH_GFNI_DEFINED */
 

lib/hash/rte_thash_gfni.c
@@ -7,15 +7,13 @@
 #include <rte_log.h>
 #include <rte_thash_gfni.h>
 
-#ifndef RTE_THASH_GFNI_DEFINED
-
 RTE_LOG_REGISTER_SUFFIX(hash_gfni_logtype, gfni, INFO);
 #define RTE_LOGTYPE_HASH hash_gfni_logtype
 #define HASH_LOG(level, ...) \
 	RTE_LOG_LINE(level, HASH, "" __VA_ARGS__)
 
 uint32_t
-rte_thash_gfni(const uint64_t *mtrx __rte_unused,
+rte_thash_gfni_stub(const uint64_t *mtrx __rte_unused,
 	const uint8_t *key __rte_unused, int len __rte_unused)
 {
 	static bool warned;
@@ -30,7 +28,7 @@ rte_thash_gfni(const uint64_t *mtrx __rte_unused,
 }
 
 void
-rte_thash_gfni_bulk(const uint64_t *mtrx __rte_unused,
+rte_thash_gfni_bulk_stub(const uint64_t *mtrx __rte_unused,
 	int len __rte_unused, uint8_t *tuple[] __rte_unused,
 	uint32_t val[], uint32_t num)
 {
@@ -47,5 +45,3 @@ rte_thash_gfni_bulk(const uint64_t *mtrx __rte_unused,
 	for (i = 0; i < num; i++)
 		val[i] = 0;
 }
-
-#endif

lib/hash/version.map
@@ -41,10 +41,15 @@ DPDK_24 {
 	rte_thash_get_gfni_matrices;
 	rte_thash_get_helper;
 	rte_thash_get_key;
-	rte_thash_gfni;
-	rte_thash_gfni_bulk;
 	rte_thash_gfni_supported;
 	rte_thash_init_ctx;
 
 	local: *;
+};
+
+INTERNAL {
+	global:
+
+	rte_thash_gfni_stub;
+	rte_thash_gfni_bulk_stub;
 };

commit a5ff88d079d2a33084b6525489574040e9816aec

hash: remove return statement from function returning void
rte_thash_gfni_bulk and rte_thash_gfni_bulk_stub both return void.
Remove superfluous return statement from rte_thash_gfni_bulk.

Fixes: 944a03a5cfc1 ("hash: fix MSVC link on GFNI stubs")
Cc: stephen@networkplumber.org

Signed-off-by: Tyler Retzlaff <roretzla@linux.microsoft.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/hash/rte_thash_gfni.h
@@ -71,7 +71,7 @@ static inline void
 rte_thash_gfni_bulk(const uint64_t *mtrx, int len, uint8_t *tuple[],
 	uint32_t val[], uint32_t num)
 {
-	return rte_thash_gfni_bulk_stub(mtrx, len, tuple, val, num);
+	rte_thash_gfni_bulk_stub(mtrx, len, tuple, val, num);
 }
 
 #endif /* RTE_THASH_GFNI_DEFINED */