b49a81fdeeb9ae8dad5e4b4e5a0be46566583cab
Sivaprasad Tummala <sivaprasad.tummala@amd.com>
2024-03-21 19:47:17 +0100

    examples/l3fwd: fix port ID restriction
    Currently application supports port IDs up to 255
    irrespective of RTE_MAX_ETHPORTS.
    
    The patch fixes these constraints by allowing port
    IDs up to RTE_MAX_ETHPORTS.
    
    Fixes: af75078fece3 ("first public release")
    Cc: stable@dpdk.org
    
    Signed-off-by: Sivaprasad Tummala <sivaprasad.tummala@amd.com>
    Acked-by: Konstantin Ananyev <konstantin.ananyev@huawei.com>
    Acked-by: Morten Brørup <mb@smartsharesystems.com>
    Acked-by: Ferruh Yigit <ferruh.yigit@amd.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- examples/l3fwd/main.c
+++ examples/l3fwd/main.c
@@ -500,7 +500,7 @@ parse_config(const char *q_arg)
 	char *str_fld[_NUM_FLD];
 	int i;
 	unsigned size;
-	uint32_t max_fld[_NUM_FLD] = {USHRT_MAX,
+	uint32_t max_fld[_NUM_FLD] = {RTE_MAX_ETHPORTS,
 				USHRT_MAX, RTE_MAX_LCORE};
 
 	nb_lcore_params = 0;
@@ -530,7 +530,7 @@ parse_config(const char *q_arg)
 			return -1;
 		}
 		lcore_params_array[nb_lcore_params].port_id =
-			(uint8_t)int_fld[FLD_PORT];
+			(uint16_t)int_fld[FLD_PORT];
 		lcore_params_array[nb_lcore_params].queue_id =
 			(uint16_t)int_fld[FLD_QUEUE];
 		lcore_params_array[nb_lcore_params].lcore_id =
