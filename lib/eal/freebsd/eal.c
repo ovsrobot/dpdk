606791bb76f6b4d5659aa528c2525c13d23b0651
Stephen Hemminger <stephen@networkplumber.org>
2024-03-25 18:57:05 -0700

    log: add timestamp option
    When debugging driver or startup issues, it is useful to have
    a timestamp on each message printed. The messages in syslog
    already have a timestamp, but often syslog is not available
    during testing.
    
    There are multiple timestamp formats similar to Linux dmesg.
    The default is time relative since startup (when first
    step of logging initialization is done by constructor).
    Other alternative formats are delta, ctime, reltime and iso formats.
    
    Example:
    $ dpdk-testpmd --log-timestamp -- -i
    [     0.008610] EAL: Detected CPU lcores: 8
    [     0.008634] EAL: Detected NUMA nodes: 1
    [     0.008792] EAL: Detected static linkage of DPDK
    [     0.010620] EAL: Multi-process socket /var/run/dpdk/rte/mp_socket
    [     0.012618] EAL: Selected IOVA mode 'VA'
    [     0.016675] testpmd: No probed ethernet devices
    Interactive-mode selected
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/freebsd/eal.c
+++ lib/eal/freebsd/eal.c
@@ -392,8 +392,10 @@ eal_parse_args(int argc, char **argv)
 			goto out;
 		}
 
-		/* eal_log_level_parse() already handled these */
-		if (opt == OPT_LOG_LEVEL_NUM || opt == OPT_LOG_SYSLOG_NUM)
+		/* eal_log_level_parse() already handled these options */
+		if (opt == OPT_LOG_LEVEL_NUM ||
+		    opt == OPT_SYSLOG_NUM ||
+		    opt == OPT_LOG_TIMESTAMP_NUM)
 			continue;
 
 		ret = eal_parse_common_option(opt, optarg, internal_conf);
