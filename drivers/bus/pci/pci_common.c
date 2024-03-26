ovsrobot/dpdk

commit 8f4de2dba9b90e97537e3bcc2db539c85210d854

bus/pci: fill bus specific information
For diagnostic, it may be useful to provide the PCI vendor and device
id.

Signed-off-by: David Marchand <david.marchand@redhat.com>
Acked-by: Bruce Richardson <bruce.richardson@intel.com>

drivers/bus/pci/pci_common.c
@@ -44,6 +44,38 @@ const char *rte_pci_get_sysfs_path(void)
 	return path;
 }
 
+#ifdef RTE_EXEC_ENV_WINDOWS
+#define asprintf pci_asprintf
+
+static int
+__rte_format_printf(2, 3)
+pci_asprintf(char **buffer, const char *format, ...)
+{
+	int size, ret;
+	va_list arg;
+
+	va_start(arg, format);
+	size = vsnprintf(NULL, 0, format, arg);
+	va_end(arg);
+	if (size < 0)
+		return -1;
+	size++;
+
+	*buffer = malloc(size);
+	if (*buffer == NULL)
+		return -1;
+
+	va_start(arg, format);
+	ret = vsnprintf(*buffer, size, format, arg);
+	va_end(arg);
+	if (ret != size - 1) {
+		free(*buffer);
+		return -1;
+	}
+	return ret;
+}
+#endif /* RTE_EXEC_ENV_WINDOWS */
+
 static struct rte_devargs *
 pci_devargs_lookup(const struct rte_pci_addr *pci_addr)
 {
@@ -59,7 +91,7 @@ pci_devargs_lookup(const struct rte_pci_addr *pci_addr)
 }
 
 void
-pci_name_set(struct rte_pci_device *dev)
+pci_common_set(struct rte_pci_device *dev)
 {
 	struct rte_devargs *devargs;
 
@@ -80,6 +112,19 @@ pci_name_set(struct rte_pci_device *dev)
 	else
 		/* Otherwise, it uses the internal, canonical form. */
 		dev->device.name = dev->name;
+
+	if (asprintf(&dev->bus_info, "vendor_id=%"PRIx16", device_id=%"PRIx16,
+			dev->id.vendor_id, dev->id.device_id) != -1)
+		dev->device.bus_info = dev->bus_info;
+}
+
+void
+pci_free(struct rte_pci_device *dev)
+{
+	if (dev == NULL)
+		return;
+	free(dev->bus_info);
+	free(dev);
 }
 
 /* map a particular resource from a file */
@@ -604,7 +649,7 @@ pci_unplug(struct rte_device *dev)
 	if (ret == 0) {
 		rte_pci_remove_device(pdev);
 		rte_devargs_remove(dev->devargs);
-		free(pdev);
+		pci_free(pdev);
 	}
 	return ret;
 }

drivers/bus/pci/bsd/pci.c
@@ -248,7 +248,7 @@ pci_scan_one(int dev_pci_fd, struct pci_conf *conf)
 	/* FreeBSD has no NUMA support (yet) */
 	dev->device.numa_node = 0;
 
-	pci_name_set(dev);
+	pci_common_set(dev);
 
 	/* FreeBSD has only one pass through driver */
 	dev->kdrv = RTE_PCI_KDRV_NIC_UIO;
@@ -299,11 +299,11 @@ pci_scan_one(int dev_pci_fd, struct pci_conf *conf)
 			} else { /* already registered */
 				dev2->kdrv = dev->kdrv;
 				dev2->max_vfs = dev->max_vfs;
-				pci_name_set(dev2);
+				pci_common_set(dev2);
 				memmove(dev2->mem_resource,
 					dev->mem_resource,
 					sizeof(dev->mem_resource));
-				free(dev);
+				pci_free(dev);
 			}
 			return 0;
 		}
@@ -313,7 +313,7 @@ pci_scan_one(int dev_pci_fd, struct pci_conf *conf)
 	return 0;
 
 skipdev:
-	free(dev);
+	pci_free(dev);
 	return 0;
 }
 

drivers/bus/pci/bus_pci_driver.h
@@ -43,6 +43,7 @@ struct rte_pci_device {
 	uint16_t max_vfs;                   /**< sriov enable if not zero */
 	enum rte_pci_kernel_driver kdrv;    /**< Kernel driver passthrough */
 	char name[PCI_PRI_STR_SIZE+1];      /**< PCI location (ASCII) */
+	char *bus_info;                     /**< PCI bus specific info */
 	struct rte_intr_handle *vfio_req_intr_handle;
 				/**< Handler of VFIO request interrupt */
 };

drivers/bus/pci/linux/pci.c
@@ -226,7 +226,7 @@ pci_scan_one(const char *dirname, const struct rte_pci_addr *addr)
 	/* get vendor id */
 	snprintf(filename, sizeof(filename), "%s/vendor", dirname);
 	if (eal_parse_sysfs_value(filename, &tmp) < 0) {
-		free(dev);
+		pci_free(dev);
 		return -1;
 	}
 	dev->id.vendor_id = (uint16_t)tmp;
@@ -234,7 +234,7 @@ pci_scan_one(const char *dirname, const struct rte_pci_addr *addr)
 	/* get device id */
 	snprintf(filename, sizeof(filename), "%s/device", dirname);
 	if (eal_parse_sysfs_value(filename, &tmp) < 0) {
-		free(dev);
+		pci_free(dev);
 		return -1;
 	}
 	dev->id.device_id = (uint16_t)tmp;
@@ -243,7 +243,7 @@ pci_scan_one(const char *dirname, const struct rte_pci_addr *addr)
 	snprintf(filename, sizeof(filename), "%s/subsystem_vendor",
 		 dirname);
 	if (eal_parse_sysfs_value(filename, &tmp) < 0) {
-		free(dev);
+		pci_free(dev);
 		return -1;
 	}
 	dev->id.subsystem_vendor_id = (uint16_t)tmp;
@@ -252,7 +252,7 @@ pci_scan_one(const char *dirname, const struct rte_pci_addr *addr)
 	snprintf(filename, sizeof(filename), "%s/subsystem_device",
 		 dirname);
 	if (eal_parse_sysfs_value(filename, &tmp) < 0) {
-		free(dev);
+		pci_free(dev);
 		return -1;
 	}
 	dev->id.subsystem_device_id = (uint16_t)tmp;
@@ -261,7 +261,7 @@ pci_scan_one(const char *dirname, const struct rte_pci_addr *addr)
 	snprintf(filename, sizeof(filename), "%s/class",
 		 dirname);
 	if (eal_parse_sysfs_value(filename, &tmp) < 0) {
-		free(dev);
+		pci_free(dev);
 		return -1;
 	}
 	/* the least 24 bits are valid: class, subclass, program interface */
@@ -295,13 +295,13 @@ pci_scan_one(const char *dirname, const struct rte_pci_addr *addr)
 		dev->device.numa_node = 0;
 	}
 
-	pci_name_set(dev);
+	pci_common_set(dev);
 
 	/* parse resources */
 	snprintf(filename, sizeof(filename), "%s/resource", dirname);
 	if (pci_parse_sysfs_resource(filename, dev) < 0) {
 		RTE_LOG(ERR, EAL, "%s(): cannot parse resource\n", __func__);
-		free(dev);
+		pci_free(dev);
 		return -1;
 	}
 
@@ -310,7 +310,7 @@ pci_scan_one(const char *dirname, const struct rte_pci_addr *addr)
 	ret = pci_get_kernel_driver_by_path(filename, driver, sizeof(driver));
 	if (ret < 0) {
 		RTE_LOG(ERR, EAL, "Fail to get kernel driver\n");
-		free(dev);
+		pci_free(dev);
 		return -1;
 	}
 
@@ -324,7 +324,7 @@ pci_scan_one(const char *dirname, const struct rte_pci_addr *addr)
 		else
 			dev->kdrv = RTE_PCI_KDRV_UNKNOWN;
 	} else {
-		free(dev);
+		pci_free(dev);
 		return 0;
 	}
 	/* device is valid, add in list (sorted) */
@@ -346,7 +346,7 @@ pci_scan_one(const char *dirname, const struct rte_pci_addr *addr)
 					dev2->kdrv = dev->kdrv;
 					dev2->max_vfs = dev->max_vfs;
 					dev2->id = dev->id;
-					pci_name_set(dev2);
+					pci_common_set(dev2);
 					memmove(dev2->mem_resource,
 						dev->mem_resource,
 						sizeof(dev->mem_resource));
@@ -376,10 +376,10 @@ pci_scan_one(const char *dirname, const struct rte_pci_addr *addr)
 					else if (dev2->device.devargs !=
 						 dev->device.devargs) {
 						rte_devargs_remove(dev2->device.devargs);
-						pci_name_set(dev2);
+						pci_common_set(dev2);
 					}
 				}
-				free(dev);
+				pci_free(dev);
 			}
 			return 0;
 		}

drivers/bus/pci/private.h
@@ -44,10 +44,16 @@ struct rte_pci_device;
 int rte_pci_scan(void);
 
 /**
- * Find the name of a PCI device.
+ * Set common internal information for a PCI device.
  */
 void
-pci_name_set(struct rte_pci_device *dev);
+pci_common_set(struct rte_pci_device *dev);
+
+/**
+ * Free a PCI device.
+ */
+void
+pci_free(struct rte_pci_device *dev);
 
 /**
  * Validate whether a device with given PCI address should be ignored or not.

drivers/bus/pci/windows/pci.c
@@ -382,7 +382,7 @@ pci_scan_one(HDEVINFO dev_info, PSP_DEVINFO_DATA device_info_data)
 	dev->id = pci_id;
 	dev->max_vfs = 0; /* TODO: get max_vfs */
 
-	pci_name_set(dev);
+	pci_common_set(dev);
 
 	set_kernel_driver_type(device_info_data, dev);
 
@@ -410,7 +410,7 @@ pci_scan_one(HDEVINFO dev_info, PSP_DEVINFO_DATA device_info_data)
 				dev2->max_vfs = dev->max_vfs;
 				memmove(dev2->mem_resource, dev->mem_resource,
 					sizeof(dev->mem_resource));
-				free(dev);
+				pci_free(dev);
 			}
 			return 0;
 		}
@@ -419,7 +419,7 @@ pci_scan_one(HDEVINFO dev_info, PSP_DEVINFO_DATA device_info_data)
 
 	return 0;
 end:
-	free(dev);
+	pci_free(dev);
 	return ret;
 }
 

commit 92e5fb7576b57f31bb1c987a0c76c59bae05d7f7

windows: common wrapper for vasprintf and asprintf
Replace the windows version of asprintf() that was only usable
in eal. With a more generic one that supports both vasprintf()
and asprintf().  This also eliminates duplicate code.

Fixes: 8f4de2dba9b9 ("bus/pci: fill bus specific information")
Fixes: 9ec521006db0 ("eal/windows: hide asprintf shim")

Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/bus/pci/pci_common.c
@@ -45,38 +45,6 @@ const char *rte_pci_get_sysfs_path(void)
 	return path;
 }
 
-#ifdef RTE_EXEC_ENV_WINDOWS
-#define asprintf pci_asprintf
-
-static int
-__rte_format_printf(2, 3)
-pci_asprintf(char **buffer, const char *format, ...)
-{
-	int size, ret;
-	va_list arg;
-
-	va_start(arg, format);
-	size = vsnprintf(NULL, 0, format, arg);
-	va_end(arg);
-	if (size < 0)
-		return -1;
-	size++;
-
-	*buffer = malloc(size);
-	if (*buffer == NULL)
-		return -1;
-
-	va_start(arg, format);
-	ret = vsnprintf(*buffer, size, format, arg);
-	va_end(arg);
-	if (ret != size - 1) {
-		free(*buffer);
-		return -1;
-	}
-	return ret;
-}
-#endif /* RTE_EXEC_ENV_WINDOWS */
-
 static struct rte_devargs *
 pci_devargs_lookup(const struct rte_pci_addr *pci_addr)
 {

lib/eal/common/eal_private.h
@@ -737,16 +737,6 @@ void __rte_thread_init(unsigned int lcore_id, rte_cpuset_t *cpuset);
  */
 void __rte_thread_uninit(void);
 
-/**
- * asprintf(3) replacement for Windows.
- */
-#ifdef RTE_EXEC_ENV_WINDOWS
-__rte_format_printf(2, 3)
-int eal_asprintf(char **buffer, const char *format, ...);
-
-#define asprintf(buffer, format, ...) \
-		eal_asprintf(buffer, format, ##__VA_ARGS__)
-#endif
 
 #define EAL_LOG(level, ...) \
 	RTE_LOG_LINE(level, EAL, "" __VA_ARGS__)

lib/eal/windows/eal.c
@@ -503,34 +503,6 @@ rte_eal_init(int argc, char **argv)
 	return fctret;
 }
 
-/* Don't use MinGW asprintf() to have identical code with all toolchains. */
-int
-eal_asprintf(char **buffer, const char *format, ...)
-{
-	int size, ret;
-	va_list arg;
-
-	va_start(arg, format);
-	size = vsnprintf(NULL, 0, format, arg);
-	va_end(arg);
-	if (size < 0)
-		return -1;
-	size++;
-
-	*buffer = malloc(size);
-	if (*buffer == NULL)
-		return -1;
-
-	va_start(arg, format);
-	ret = vsnprintf(*buffer, size, format, arg);
-	va_end(arg);
-	if (ret != size - 1) {
-		free(*buffer);
-		return -1;
-	}
-	return ret;
-}
-
 int
 rte_vfio_container_dma_map(__rte_unused int container_fd,
 			__rte_unused uint64_t vaddr,

lib/eal/windows/include/rte_os_shim.h
@@ -3,6 +3,7 @@
 #ifndef _RTE_OS_SHIM_
 #define _RTE_OS_SHIM_
 
+#include <stdio.h>
 #include <time.h>
 
 #include <rte_os.h>
@@ -120,4 +121,49 @@ rte_localtime_r(const time_t *timer, struct tm *buf)
 }
 #define localtime_r(timer, buf) rte_localtime_r(timer, buf)
 
+/* print to allocated string */
+static inline int
+rte_vasprintf(char **strp, const char *fmt, va_list ap)
+{
+	char *str;
+	int len, ret;
+
+	*strp = NULL;
+
+	/* determine size of buffer needed */
+	len = _vscprintf(fmt, ap);
+	if (len < 0)
+		return -1;
+
+	len += 1;	/* for nul termination */
+	str = malloc(len);
+	if (str == NULL)
+		return -1;
+
+	ret = vsnprintf(str, len, fmt, ap);
+	if (ret < 0) {
+		free(str);
+		return -1;
+	} else {
+		*strp = str;
+		return ret;
+	}
+}
+#define vasprintf(strp, fmt, ap) rte_vasprintf(strp, fmt, ap)
+
+static inline int
+rte_asprintf(char **strp, const char *fmt, ...)
+{
+	int ret;
+
+	va_list ap;
+
+	va_start(ap, fmt);
+	ret = rte_vasprintf(strp, fmt, ap);
+	va_end(ap);
+
+	return ret;
+}
+
+#define asprintf(strp, fmt, ...) rte_asprintf(strp, fmt, __VA_ARGS__)
 #endif /* _RTE_OS_SHIM_ */