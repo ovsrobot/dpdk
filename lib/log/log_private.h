991d0b43d35fc10028bba6cbf3b1369924680807
Stephen Hemminger <stephen@networkplumber.org>
2024-03-29 20:00:55 -0700

    log: add timestamp option
    When debugging driver or startup issues, it is useful to have
    a timestamp on each message printed. The messages in syslog
    already have a timestamp, but often syslog is not available
    during testing.
    
    There are multiple timestamp formats similar to Linux dmesg.
    The default is time relative since startup (when first
    step of logging initialization is done by constructor).
    Other alternative formats are delta, ctime, reltime and iso formats.
    
    Example:
    $ dpdk-testpmd --log-timestamp -- -i
    [     0.008610] EAL: Detected CPU lcores: 8
    [     0.008634] EAL: Detected NUMA nodes: 1
    [     0.008792] EAL: Detected static linkage of DPDK
    [     0.010620] EAL: Multi-process socket /var/run/dpdk/rte/mp_socket
    [     0.012618] EAL: Selected IOVA mode 'VA'
    [     0.016675] testpmd: No probed ethernet devices
    Interactive-mode selected
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/log/log_private.h
+++ lib/log/log_private.h
@@ -8,4 +8,11 @@ typedef int (*log_print_t)(FILE *f, uint32_t level, const char *fmt, va_list ap)
 __rte_format_printf(3, 0)
 int log_print(FILE *f, uint32_t level, const char *format, va_list ap);
 
+bool log_timestamp_enabled(void);
+
+ssize_t log_timestamp(char *tsbuf, size_t tsbuflen);
+
+__rte_format_printf(3, 0)
+int log_print_with_timestamp(FILE *f, uint32_t level, const char *format, va_list ap);
+
 #endif /* LOG_PRIVATE_H */
