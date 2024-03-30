cbb7fb8d2ab145172140018a430e11d20456ba8b
Stephen Hemminger <stephen@networkplumber.org>
2024-03-29 20:00:56 -0700

    log: add optional support of syslog
    Log to syslog only if option is specified. And if syslog is used
    then normally only log to syslog, don't duplicate output.
    Also enables syslog support on FreeBSD.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/log/log_private.h
+++ lib/log/log_private.h
@@ -15,4 +15,8 @@ ssize_t log_timestamp(char *tsbuf, size_t tsbuflen);
 __rte_format_printf(3, 0)
 int log_print_with_timestamp(FILE *f, uint32_t level, const char *format, va_list ap);
 
+bool log_syslog_enabled(bool is_tty);
+void log_syslog_open(const char *id, bool is_terminal);
+
+
 #endif /* LOG_PRIVATE_H */
