14883be424255117f2b677c8fa7b1efc33561568
Gongming Chen <chengm11@chinatelecom.cn>
2024-04-03 11:25:28 +0800

    vhost: fix crash caused by accessing a freed vsocket
    When a vhost user message handling error in the event dispatch thread,
    vsocket reconn is added to the reconnection list of the reconnection
    thread.
    Since the reconnection, event dispatching and app configuration thread
    do not have common thread protection restrictions, the app config
    thread freed vsocket in the rte_vhost_driver_unregister process,
    but vsocket reconn can still exist in the reconn_list through this
    mechanism.
    Then in the reconnection thread, the vsocket is connected again and
    conn is added to the dispatch thread.
    Finally, the vsocket is accessed again in the event dispatch thread,
    resulting in a use-after-free error.
    
    This patch adds a vhost threads read-write lock to restrict
    reconnection, event dispatching and app configuration threads.
    When the vhost driver unregisters, it exclusively holds the lock to
    safely free the vsocket.
    
    #0  0x0000000000000025 in ?? ()
    #1  0x0000000003ed7ca0 in vhost_user_read_cb at lib/vhost/socket.c:330
    #2  0x0000000003ed625f in fdset_event_dispatch at lib/vhost/fd_man.c:283
    
    Fixes: e623e0c6d8a5 ("vhost: add vhost-user client mode")
    Cc: stable@dpdk.org
    
    Signed-off-by: Gongming Chen <chengm11@chinatelecom.cn>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ lib/vhost/vhost_thread.c
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2024 China Telecom Cloud Technology Co., Ltd
+ */
+
+#include <rte_rwlock.h>
+
+#include "vhost_thread.h"
+
+static rte_rwlock_t vhost_thread_lock = RTE_RWLOCK_INITIALIZER;
+
+void
+vhost_thread_read_lock(void)
+{
+	rte_rwlock_read_lock(&vhost_thread_lock);
+}
+
+void
+vhost_thread_read_unlock(void)
+{
+	rte_rwlock_read_unlock(&vhost_thread_lock);
+}
+
+void
+vhost_thread_write_lock(void)
+{
+	rte_rwlock_write_lock(&vhost_thread_lock);
+}
+
+void
+vhost_thread_write_unlock(void)
+{
+	rte_rwlock_write_unlock(&vhost_thread_lock);
+}
