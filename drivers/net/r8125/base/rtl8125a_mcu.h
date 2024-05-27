19dcfab76b4db9b86c01f91220d1bd79ddfa6260
Howard Wang <howard_wang@realsil.com.cn>
2024-05-27 14:15:28 +0800

    r8125: add r8125 ethernet poll mode driver
    r8125 is for Realtek 2.5 Gigabit Ethernet NICs.
    
    Signed-off-by: Howard Wang <pro_nic_dpdk@realtek.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/r8125/base/rtl8125a_mcu.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2024 Realtek Corporation. All rights reserved
+ */
+
+#ifndef _RTL8125A_MCU_H_
+#define _RTL8125A_MCU_H_
+
+void rtl8125_set_mac_mcu_8125a_1(struct rtl8125_hw *hw);
+void rtl8125_set_mac_mcu_8125a_2(struct rtl8125_hw *hw);
+
+void rtl8125_set_phy_mcu_8125a_1(struct rtl8125_hw *hw);
+void rtl8125_set_phy_mcu_8125a_2(struct rtl8125_hw *hw);
+
+#endif
