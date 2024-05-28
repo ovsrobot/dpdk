2cf79f95d06138f8f78c927b608b04c6c2666735
Soumyadeep Hore <soumyadeep.hore@intel.com>
2024-05-28 07:28:32 +0000

    common/idpf: updated IDPF VF device ID
    Update IDPF VF device id to 145C removing the support for legacy AVF of
    0x1889.
    
    In accordance to DCR-3788 added device ID for S-IOV device.
    
    Signed-off-by: Soumyadeep Hore <soumyadeep.hore@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/common/idpf/base/idpf_devids.h
+++ drivers/common/idpf/base/idpf_devids.h
@@ -1,18 +1,20 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright(c) 2001-2023 Intel Corporation
+ * Copyright(c) 2001-2024 Intel Corporation
  */
 
 #ifndef _IDPF_DEVIDS_H_
 #define _IDPF_DEVIDS_H_
 
+#ifndef LINUX_SUPPORT
 /* Vendor ID */
 #define IDPF_INTEL_VENDOR_ID		0x8086
+#endif /* LINUX_SUPPORT */
 
 /* Device IDs */
 #define IDPF_DEV_ID_PF			0x1452
-#define IDPF_DEV_ID_VF			0x1889
-
-
-
+#define IDPF_DEV_ID_VF			0x145C
+#ifdef SIOV_SUPPORT
+#define IDPF_DEV_ID_VF_SIOV		0x0DD5
+#endif /* SIOV_SUPPORT */
 
 #endif /* _IDPF_DEVIDS_H_ */
