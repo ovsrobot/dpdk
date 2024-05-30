c0c036d1feb36acc323488ce5da04433f6989035
Daniel Gregory <daniel.gregory@bytedance.com>
2024-05-30 18:19:48 +0100

    eal/riscv: add support for zicbop extension
    The zicbop extension adds instructions for prefetching data into cache.
    Use them to implement RISCV-specific versions of the rte_prefetch* and
    rte_prefetch*_write functions.
    
    - prefetch.r indicates to hardware that the cache block will be accessed
      by a data read soon
    - prefetch.w indicates to hardware that the cache block will be accessed
      by a data write soon
    
    These instructions are emitted by __builtin_prefetch on modern versions
    of Clang (17.0.1+) and GCC (13.1.0+). For earlier versions, we may not
    have support for assembling Zicbop instructions, so emit the word
    that encodes a 'prefetch.[rw] 0(a0)' instruction.
    
    This new functionality is controlled by a Meson flag that is disabled by
    default. Whilst it's a hint, like rte_pause(), and so has no effect if
    the target doesn't support the extension, it requires the address
    prefetched to be loaded into a0, which may be costly.
    
    Signed-off-by: Daniel Gregory <daniel.gregory@bytedance.com>
    Suggested-by: Punit Agrawal <punit.agrawal@bytedance.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/riscv/include/rte_prefetch.h
+++ lib/eal/riscv/include/rte_prefetch.h
@@ -14,21 +14,42 @@ extern "C" {
 
 #include <rte_compat.h>
 #include <rte_common.h>
+
+#ifdef RTE_RISCV_ZICBOP
+#define RTE_PREFETCH_WRITE_ARCH_DEFINED
+#endif
+
 #include "generic/rte_prefetch.h"
 
+/*
+ * Modern versions of GCC & Clang will emit prefetch instructions for
+ * __builtin_prefetch when the Zicbop extension is present.
+ * The RTE_RISCV_ZICBOP option controls whether we emit them manually for older
+ * compilers that may not have the support to assemble them.
+ */
 static inline void rte_prefetch0(const volatile void *p)
 {
-	RTE_SET_USED(p);
+#ifndef RTE_RISCV_ZICBOP
+	/* by default __builtin_prefetch prepares for a read */
+	__builtin_prefetch((const void *)p);
+#else
+	/* prefetch.r 0(a0) */
+	register const volatile void *a0 asm("a0") = p;
+	asm volatile (".int 0x00156013" : : "r" (a0));
+#endif
 }
 
+/*
+ * The RISC-V Zicbop extension doesn't have instructions to prefetch to only a
+ * subset of cache levels, so fallback to rte_prefetch0
+ */
 static inline void rte_prefetch1(const volatile void *p)
 {
-	RTE_SET_USED(p);
+	rte_prefetch0(p);
 }
-
 static inline void rte_prefetch2(const volatile void *p)
 {
-	RTE_SET_USED(p);
+	rte_prefetch0(p);
 }
 
 static inline void rte_prefetch_non_temporal(const volatile void *p)
@@ -43,6 +64,34 @@ rte_cldemote(const volatile void *p)
 {
 	RTE_SET_USED(p);
 }
+
+#ifdef RTE_RISCV_ZICBOP
+__rte_experimental
+static inline void
+rte_prefetch0_write(const void *p)
+{
+	/* prefetch.w 0(a0) */
+	register const void *a0 asm("a0") = p;
+	asm volatile (".int 0x00356013" : : "r" (a0));
+}
+
+/*
+ * The RISC-V Zicbop extension doesn't have instructions to prefetch to only a
+ * subset of cache levels, so fallback to rte_prefetch0_write
+ */
+__rte_experimental
+static inline void
+rte_prefetch1_write(const void *p)
+{
+	rte_prefetch0_write(p);
+}
+__rte_experimental
+static inline void
+rte_prefetch2_write(const void *p)
+{
+	rte_prefetch0_write(p);
+}
+#endif /* RTE_RISCV_ZICBOP */
 
 #ifdef __cplusplus
 }
