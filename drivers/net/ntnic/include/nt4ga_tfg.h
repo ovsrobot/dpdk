0af37a402d642ba0058a6419516d001b1e9f1eec
Serhii Iliushyk <sil-plv@napatech.com>
2024-06-03 18:18:02 +0200

    net/ntnic: add NTNIC adapter interfaces
    Add ntnic adapter interfaces structures.
    
    Signed-off-by: Serhii Iliushyk <sil-plv@napatech.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/ntnic/include/nt4ga_tfg.h
@@ -0,0 +1,18 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2023 Napatech A/S
+ */
+
+#ifndef NT4GA_TFG_H_
+#define NT4GA_TFG_H_
+
+typedef struct nt4ga_tfg_s {
+	nthw_mac_tfg_t *mp_nthw_mac_tfg;
+} nt4ga_tfg_t;
+
+int nt4ga_tfg_init(struct adapter_info_s *p_adapter_info);
+int nt4ga_tfg_setup(struct adapter_info_s *p_adapter_info, const int n_intf_no,
+	const int n_cmd_start_stop, const int n_frame_count, const int n_frame_size,
+	const int n_frame_fill_mode, const int n_frame_stream_id);
+
+#endif	/* NT4GA_TFG_H_ */
