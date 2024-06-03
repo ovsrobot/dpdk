4fa7f22fc61c226bf5704a6af67e9fb2dda38ab2
Serhii Iliushyk <sil-plv@napatech.com>
2024-06-03 18:18:09 +0200

    net/ntnic: add support of the NT200A0X smartNIC
    Add ntnic support for NT200A0X NIC
    
    * Reset
    
    * Clock profiles
    
    * FPGA Registers
    
    * API for FPGA registers
    
    Signed-off-by: Serhii Iliushyk <sil-plv@napatech.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/ntnic/nthw/core/nt200a0x/clock_profiles/nthw_fpga_clk9563.h
@@ -0,0 +1,9 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2023 Napatech A/S
+ */
+
+#ifndef _NTHW_FPGA_CLK_9563_H_
+#define _NTHW_FPGA_CLK_9563_H_
+
+#endif	/* _NTHW_FPGA_CLK_9563_H_ */
---
+++ drivers/net/ntnic/nthw/core/nt200a0x/reset/nthw_fpga_rst9563.h
@@ -0,0 +1,9 @@
+/*
+ * SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2023 Napatech A/S
+ */
+
+#ifndef __NTHW_FPGA_RST9563_H__
+#define __NTHW_FPGA_RST9563_H__
+
+#endif	/* __NTHW_FPGA_RST9563_H__ */
