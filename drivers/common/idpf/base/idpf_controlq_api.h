685a695431a42308394df529a972464d51f4cb82
Soumyadeep Hore <soumyadeep.hore@intel.com>
2024-06-04 08:05:51 +0000

    common/idpf: added NVME CPF specific code with defines
    Removes NVME dependency on memory allocations and
    uses a prepared buffer instead.
    
    The changes do not affect other components.
    
    Signed-off-by: Soumyadeep Hore <soumyadeep.hore@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/common/idpf/base/idpf_controlq_api.h
+++ drivers/common/idpf/base/idpf_controlq_api.h
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: BSD-3-Clause
- * Copyright(c) 2001-2023 Intel Corporation
+ * Copyright(c) 2001-2024 Intel Corporation
  */
 
 #ifndef _IDPF_CONTROLQ_API_H_
@@ -158,8 +158,13 @@ enum idpf_mbx_opc {
 /* Will init all required q including default mb.  "q_info" is an array of
  * create_info structs equal to the number of control queues to be created.
  */
+#ifdef NVME_CPF
+int idpf_ctlq_init(struct idpf_hw *hw, u8 num_q,
+			struct idpf_ctlq_create_info *q_info, struct idpf_ctlq_info **ctlq);
+#else
 int idpf_ctlq_init(struct idpf_hw *hw, u8 num_q,
 		   struct idpf_ctlq_create_info *q_info);
+#endif
 
 /* Allocate and initialize a single control queue, which will be added to the
  * control queue list; returns a handle to the created control queue
