6234b2cc93b73d9b65ebb06145db3846f89730e0
Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
2024-06-16 10:12:20 +0530

    test/crypto: add modex tests for zero padded operands
    Add modex tests for zero padded operands, as in ASN encoding.
    
    Signed-off-by: Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- app/test/test_cryptodev_asym.c
+++ app/test/test_cryptodev_asym.c
@@ -3294,6 +3294,14 @@ static struct unit_test_suite cryptodev_openssl_asym_testsuite  = {
 			ut_setup_asym, ut_teardown_asym,
 			modular_exponentiation, &modex_test_cases[0]),
 		TEST_CASE_NAMED_WITH_DATA(
+			"Modex test for zero padding",
+			ut_setup_asym, ut_teardown_asym,
+			modular_exponentiation, &modex_test_cases[1]),
+		TEST_CASE_NAMED_WITH_DATA(
+			"Modex test for zero padding (2)",
+			ut_setup_asym, ut_teardown_asym,
+			modular_exponentiation, &modex_test_cases[2]),
+		TEST_CASE_NAMED_WITH_DATA(
 			"Modex Group 5 test",
 			ut_setup_asym, ut_teardown_asym,
 			modular_exponentiation, &modex_group_test_cases[0]),
@@ -3371,6 +3379,14 @@ static struct unit_test_suite cryptodev_octeontx_asym_testsuite  = {
 			"Modex test",
 			ut_setup_asym, ut_teardown_asym,
 			modular_exponentiation, &modex_test_cases[0]),
+		TEST_CASE_NAMED_WITH_DATA(
+			"Modex test for zero padding",
+			ut_setup_asym, ut_teardown_asym,
+			modular_exponentiation, &modex_test_cases[1]),
+		TEST_CASE_NAMED_WITH_DATA(
+			"Modex test for zero padding (2)",
+			ut_setup_asym, ut_teardown_asym,
+			modular_exponentiation, &modex_test_cases[2]),
 		TEST_CASE_NAMED_WITH_DATA(
 			"Modex Group 5 test",
 			ut_setup_asym, ut_teardown_asym,
