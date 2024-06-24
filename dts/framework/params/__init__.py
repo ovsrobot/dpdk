ovsrobot/dpdk

commit 87ba4cdc0dbbb5b74556da6a9175804f2f7d74a5

dts: use Unpack for type checking and hinting
Interactive shells that inherit DPDKShell initialise their params
classes from a kwargs dict. Therefore, static type checking is
disabled. This change uses the functionality of Unpack added in
PEP 692 to re-enable it. The disadvantage is that this functionality has
been implemented only with TypedDict, forcing the creation of TypedDict
mirrors of the Params classes.

Signed-off-by: Luca Vizzarro <luca.vizzarro@arm.com>
Reviewed-by: Paul Szczepanek <paul.szczepanek@arm.com>
Reviewed-by: Juraj Linkeš <juraj.linkes@pantheon.tech>
Reviewed-by: Jeremy Spewock <jspewock@iol.unh.edu>
Reviewed-by: Nicholas Pratte <npratte@iol.unh.edu>

dts/framework/params/types.py
@@ -1,0 +1,133 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2024 Arm Limited
+
+"""Module containing TypeDict-equivalents of Params classes for static typing and hinting.
+
+TypedDicts can be used in conjunction with Unpack and kwargs for type hinting on function calls.
+
+Example:
+    ..code:: python
+        def create_testpmd(**kwargs: Unpack[TestPmdParamsDict]):
+            params = TestPmdParams(**kwargs)
+"""
+
+from pathlib import PurePath
+from typing import TypedDict
+
+from framework.params import Switch, YesNoSwitch
+from framework.params.testpmd import (
+    AnonMempoolAllocationMode,
+    EthPeer,
+    Event,
+    FlowGenForwardingMode,
+    HairpinMode,
+    NoisyForwardingMode,
+    Params,
+    PortNUMAConfig,
+    PortTopology,
+    RingNUMAConfig,
+    RSSSetting,
+    RXMultiQueueMode,
+    RXRingParams,
+    SimpleForwardingModes,
+    SimpleMempoolAllocationMode,
+    TxIPAddrPair,
+    TXOnlyForwardingMode,
+    TXRingParams,
+    TxUDPPortPair,
+)
+from framework.testbed_model.cpu import LogicalCoreList
+from framework.testbed_model.port import Port
+from framework.testbed_model.virtual_device import VirtualDevice
+
+
+class EalParamsDict(TypedDict, total=False):
+    """:class:`TypedDict` equivalent of :class:`~.eal.EalParams`."""
+
+    lcore_list: LogicalCoreList | None
+    memory_channels: int | None
+    prefix: str
+    no_pci: Switch
+    vdevs: list[VirtualDevice] | None
+    ports: list[Port] | None
+    other_eal_param: Params | None
+
+
+class TestPmdParamsDict(EalParamsDict, total=False):
+    """:class:`TypedDict` equivalent of :class:`~.testpmd.TestPmdParams`."""
+
+    interactive_mode: Switch
+    auto_start: Switch
+    tx_first: Switch
+    stats_period: int | None
+    display_xstats: list[str] | None
+    nb_cores: int | None
+    coremask: int | None
+    nb_ports: int | None
+    port_topology: PortTopology | None
+    portmask: int | None
+    portlist: str | None
+    numa: YesNoSwitch
+    socket_num: int | None
+    port_numa_config: list[PortNUMAConfig] | None
+    ring_numa_config: list[RingNUMAConfig] | None
+    total_num_mbufs: int | None
+    mbuf_size: list[int] | None
+    mbcache: int | None
+    max_pkt_len: int | None
+    eth_peers_configfile: PurePath | None
+    eth_peer: list[EthPeer] | None
+    tx_ip: TxIPAddrPair | None
+    tx_udp: TxUDPPortPair | None
+    enable_lro: Switch
+    max_lro_pkt_size: int | None
+    disable_crc_strip: Switch
+    enable_scatter: Switch
+    enable_hw_vlan: Switch
+    enable_hw_vlan_filter: Switch
+    enable_hw_vlan_strip: Switch
+    enable_hw_vlan_extend: Switch
+    enable_hw_qinq_strip: Switch
+    pkt_drop_enabled: Switch
+    rss: RSSSetting | None
+    forward_mode: (
+        SimpleForwardingModes
+        | FlowGenForwardingMode
+        | TXOnlyForwardingMode
+        | NoisyForwardingMode
+        | None
+    )
+    hairpin_mode: HairpinMode | None
+    hairpin_queues: int | None
+    burst: int | None
+    enable_rx_cksum: Switch
+    rx_queues: int | None
+    rx_ring: RXRingParams | None
+    no_flush_rx: Switch
+    rx_segments_offsets: list[int] | None
+    rx_segments_length: list[int] | None
+    multi_rx_mempool: Switch
+    rx_shared_queue: Switch | int
+    rx_offloads: int | None
+    rx_mq_mode: RXMultiQueueMode | None
+    tx_queues: int | None
+    tx_ring: TXRingParams | None
+    tx_offloads: int | None
+    eth_link_speed: int | None
+    disable_link_check: Switch
+    disable_device_start: Switch
+    no_lsc_interrupt: Switch
+    no_rmv_interrupt: Switch
+    bitrate_stats: int | None
+    latencystats: int | None
+    print_events: list[Event] | None
+    mask_events: list[Event] | None
+    flow_isolate_all: Switch
+    disable_flow_flush: Switch
+    hot_plug: Switch
+    vxlan_gpe_port: int | None
+    geneve_parsed_port: int | None
+    lock_all_memory: YesNoSwitch
+    mempool_allocation_mode: SimpleMempoolAllocationMode | AnonMempoolAllocationMode | None
+    record_core_cycles: Switch
+    record_burst_status: Switch

dts/framework/remote_session/testpmd_shell.py
@@ -21,10 +21,11 @@ from enum import Flag, auto
 from pathlib import PurePath
 from typing import ClassVar
 
-from typing_extensions import Self
+from typing_extensions import Self, Unpack
 
 from framework.exception import InteractiveCommandExecutionError
 from framework.params.testpmd import SimpleForwardingModes, TestPmdParams
+from framework.params.types import TestPmdParamsDict
 from framework.parser import ParserFn, TextParser
 from framework.remote_session.dpdk_shell import DPDKShell
 from framework.settings import SETTINGS
@@ -604,7 +605,7 @@ class TestPmdShell(DPDKShell):
         ascending_cores: bool = True,
         append_prefix_timestamp: bool = True,
         start_on_init: bool = True,
-        **app_params,
+        **app_params: Unpack[TestPmdParamsDict],
     ) -> None:
         """Overrides :meth:`~.dpdk_shell.DPDKShell.__init__`. Changes app_params to kwargs."""
         super().__init__(

commit 1062e59fce8042e7de04bb3205a1ce1a74122de3

dts: update params and parser docstrings
Address a few errors reported by Sphinx when generating documentation:
framework/params/__init__.py:docstring of framework.params.modify_str:3:
    WARNING: Inline interpreted text or phrase reference start-string
    without end-string.
framework/params/eal.py:docstring of framework.params.eal.EalParams:35:
    WARNING: Definition list ends without a blank line; unexpected
    unindent.
framework/params/types.py:docstring of framework.params.types:8:
    WARNING: Inline strong start-string without end-string.
framework/params/types.py:docstring of framework.params.types:9:
    WARNING: Inline strong start-string without end-string.
framework/parser.py:docstring of framework.parser.TextParser:33: ERROR:
    Unexpected indentation.
framework/parser.py:docstring of framework.parser.TextParser:43: ERROR:
    Unexpected indentation.
framework/parser.py:docstring of framework.parser.TextParser:49: ERROR:
    Unexpected indentation.
framework/parser.py:docstring of framework.parser.TextParser.wrap:8:
    ERROR: Unexpected indentation.
framework/parser.py:docstring of framework.parser.TextParser.wrap:9:
    WARNING: Block quote ends without a blank line; unexpected unindent.

Fixes: 87ba4cdc0dbb ("dts: use Unpack for type checking and hinting")
Fixes: d70159cb62f5 ("dts: add params manipulation module")
Fixes: 967fc62b0a43 ("dts: refactor EAL parameters class")
Fixes: 818fe14e3422 ("dts: add parsing utility module")
Cc: luca.vizzarro@arm.com

Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>
Signed-off-by: 0-day Robot <robot@bytheb.org>

dts/framework/params/__init__.py
@@ -53,9 +53,9 @@ def _reduce_functions(funcs: Iterable[FnPtr]) -> FnPtr:
 
 
 def modify_str(*funcs: FnPtr) -> Callable[[T], T]:
-    """Class decorator modifying the ``__str__`` method with a function created from its arguments.
+    r"""Class decorator modifying the ``__str__`` method with a function created from its arguments.
 
-    The :attr:`FnPtr`s fed to the decorator are executed from left to right in the arguments list
+    The :attr:`FnPtr`\s fed to the decorator are executed from left to right in the arguments list
     order.
 
     Args:

dts/framework/params/eal.py
@@ -26,13 +26,16 @@ class EalParams(Params):
         prefix: Set the file prefix string with which to start DPDK, e.g.: ``prefix="vf"``.
         no_pci: Switch to disable PCI bus, e.g.: ``no_pci=True``.
         vdevs: Virtual devices, e.g.::
+
             vdevs=[
                 VirtualDevice('net_ring0'),
                 VirtualDevice('net_ring1')
             ]
+
         ports: The list of ports to allow.
-        other_eal_param: user defined DPDK EAL parameters, e.g.:
-                ``other_eal_param='--single-file-segments'``
+        other_eal_param: user defined DPDK EAL parameters, e.g.::
+
+            ``other_eal_param='--single-file-segments'``
     """
 
     lcore_list: LogicalCoreList | None = field(default=None, metadata=Params.short("l"))

dts/framework/params/types.py
@@ -6,7 +6,8 @@
 TypedDicts can be used in conjunction with Unpack and kwargs for type hinting on function calls.
 
 Example:
-    ..code:: python
+    .. code:: python
+
         def create_testpmd(**kwargs: Unpack[TestPmdParamsDict]):
             params = TestPmdParams(**kwargs)
 """

dts/framework/parser.py
@@ -46,7 +46,7 @@ class TextParser(ABC):
     Example:
         The following example makes use of and demonstrates every parser function available:
 
-        ..code:: python
+        .. code:: python
 
             from dataclasses import dataclass, field
             from enum import Enum
@@ -90,7 +90,7 @@ class TextParser(ABC):
         """Makes a wrapped parser function.
 
         `parser_fn` is called and if a non-None value is returned, `wrapper_function` is called with
-        it. Otherwise the function returns early with None. In pseudo-code:
+        it. Otherwise the function returns early with None. In pseudo-code::
 
             intermediate_value := parser_fn(input)
             if intermediary_value is None then