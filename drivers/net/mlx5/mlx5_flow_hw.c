ovsrobot/dpdk

commit 89578504edd9f13da5359d8cb9b943d5cb8ba67a

net/mlx5: add ipool resize function
Before this patch, ipool size could be fixed by
setting max_idx in mlx5_indexed_pool_config upon
ipool creation. Or it can be auto resized to the
maximum limit by setting max_idx to zero upon
ipool creation and the saved value is the maximum
index possible.
This patch adds ipool_resize API that enables to
update the value of max_idx in case it is not set to
maximum, meaning not in auto resize mode. It
enables the allocation of new trunk when using
malloc/zmalloc up to the max_idx limit. Please
notice the resize number of entries should be divisible by trunk_size.

Signed-off-by: Maayan Kashani <mkashani@nvidia.com>
Acked-by: Dariusz Sosnowski <dsosnowski@nvidia.com>

drivers/net/mlx5/mlx5_utils.c
@@ -809,6 +809,35 @@ mlx5_ipool_get_next(struct mlx5_indexed_pool *pool, uint32_t *pos)
 	return NULL;
 }
 
+int
+mlx5_ipool_resize(struct mlx5_indexed_pool *pool, uint32_t num_entries)
+{
+	uint32_t cur_max_idx;
+	uint32_t max_index = mlx5_trunk_idx_offset_get(pool, TRUNK_MAX_IDX + 1);
+
+	if (num_entries % pool->cfg.trunk_size) {
+		DRV_LOG(ERR, "num_entries param should be trunk_size(=%u) multiplication\n",
+			pool->cfg.trunk_size);
+		return -EINVAL;
+	}
+
+	mlx5_ipool_lock(pool);
+	cur_max_idx = pool->cfg.max_idx + num_entries;
+	/* If the ipool max idx is above maximum or uint overflow occurred. */
+	if (cur_max_idx > max_index || cur_max_idx < num_entries) {
+		DRV_LOG(ERR, "Ipool resize failed\n");
+		DRV_LOG(ERR, "Adding %u entries to existing %u entries, will cross max limit(=%u)\n",
+		num_entries, cur_max_idx, max_index);
+		mlx5_ipool_unlock(pool);
+		return -EINVAL;
+	}
+
+	/* Update maximum entries number. */
+	pool->cfg.max_idx = cur_max_idx;
+	mlx5_ipool_unlock(pool);
+	return 0;
+}
+
 void
 mlx5_ipool_dump(struct mlx5_indexed_pool *pool)
 {

drivers/net/mlx5/mlx5_utils.h
@@ -428,6 +428,22 @@ void mlx5_ipool_flush_cache(struct mlx5_indexed_pool *pool);
 void *mlx5_ipool_get_next(struct mlx5_indexed_pool *pool, uint32_t *pos);
 
 /**
+ * This function resize the ipool.
+ *
+ * @param pool
+ *   Pointer to the index memory pool handler.
+ * @param num_entries
+ *   Number of entries to be added to the pool.
+ *   This number should be divisible by trunk_size.
+ *
+ * @return
+ *   - non-zero value on error.
+ *   - 0 on success.
+ *
+ */
+int mlx5_ipool_resize(struct mlx5_indexed_pool *pool, uint32_t num_entries);
+
+/**
  * This function allocates new empty Three-level table.
  *
  * @param type

commit b871a1c588884940599c434a748f1c0194e3475f

net/mlx5: fix indexed pool resize
On success, indexed pool resize sets maximal pool entries number to
the `num_entries` parameter value.

The patch fixes maximal pool entries assignment.

The patch also adds `error` parameter to log error types.

Fixes: 89578504edd9 ("net/mlx5: add ipool resize function")

Signed-off-by: Gregory Etelson <getelson@nvidia.com>
Acked-by: Dariusz Sosnowski <dsosnowski@nvidia.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/mlx5/mlx5_flow_hw.c
@@ -14634,11 +14634,9 @@ flow_hw_table_resize(struct rte_eth_dev *dev,
 		return rte_flow_error_set(error, EINVAL,
 					  RTE_FLOW_ERROR_TYPE_UNSPECIFIED,
 					  table, "shrinking table is not supported");
-	ret = mlx5_ipool_resize(table->flow, nb_flows);
+	ret = mlx5_ipool_resize(table->flow, nb_flows, error);
 	if (ret)
-		return rte_flow_error_set(error, EINVAL,
-					  RTE_FLOW_ERROR_TYPE_UNSPECIFIED,
-					  table, "cannot resize flows pool");
+		return ret;
 	/*
 	 * A resizable matcher doesn't support rule update. In this case, the ipool
 	 * for the resource is not created and there is no need to resize it.

drivers/net/mlx5/mlx5_utils.c
@@ -811,30 +811,25 @@ mlx5_ipool_get_next(struct mlx5_indexed_pool *pool, uint32_t *pos)
 }
 
 int
-mlx5_ipool_resize(struct mlx5_indexed_pool *pool, uint32_t num_entries)
+mlx5_ipool_resize(struct mlx5_indexed_pool *pool, uint32_t num_entries,
+	struct rte_flow_error *error)
 {
-	uint32_t cur_max_idx;
-	uint32_t max_index = mlx5_trunk_idx_offset_get(pool, TRUNK_MAX_IDX + 1);
-
-	if (num_entries % pool->cfg.trunk_size) {
-		DRV_LOG(ERR, "num_entries param should be trunk_size(=%u) multiplication\n",
-			pool->cfg.trunk_size);
-		return -EINVAL;
-	}
-
+	if (num_entries == pool->cfg.max_idx)
+		return 0;
+	else if (num_entries < pool->cfg.max_idx)
+		return rte_flow_error_set(error, EINVAL,
+					  RTE_FLOW_ERROR_TYPE_UNSPECIFIED,
+					  NULL, "cannot decrease pool size");
+	if (num_entries % pool->cfg.trunk_size)
+		return rte_flow_error_set(error, EINVAL,
+					  RTE_FLOW_ERROR_TYPE_UNSPECIFIED,
+					  NULL, "number of entries in pool must be trunk size multiplication");
+	if (num_entries >= mlx5_trunk_idx_offset_get(pool, TRUNK_MAX_IDX + 1))
+		return rte_flow_error_set(error, EINVAL,
+					  RTE_FLOW_ERROR_TYPE_UNSPECIFIED,
+					  NULL, "requested number of entries exceeds pool limit");
 	mlx5_ipool_lock(pool);
-	cur_max_idx = pool->cfg.max_idx + num_entries;
-	/* If the ipool max idx is above maximum or uint overflow occurred. */
-	if (cur_max_idx > max_index || cur_max_idx < num_entries) {
-		DRV_LOG(ERR, "Ipool resize failed\n");
-		DRV_LOG(ERR, "Adding %u entries to existing %u entries, will cross max limit(=%u)\n",
-		num_entries, cur_max_idx, max_index);
-		mlx5_ipool_unlock(pool);
-		return -EINVAL;
-	}
-
-	/* Update maximum entries number. */
-	pool->cfg.max_idx = cur_max_idx;
+	pool->cfg.max_idx = num_entries;
 	mlx5_ipool_unlock(pool);
 	return 0;
 }

drivers/net/mlx5/mlx5_utils.h
@@ -438,7 +438,8 @@ void *mlx5_ipool_get_next(struct mlx5_indexed_pool *pool, uint32_t *pos);
  *   - 0 on success.
  *
  */
-int mlx5_ipool_resize(struct mlx5_indexed_pool *pool, uint32_t num_entries);
+int mlx5_ipool_resize(struct mlx5_indexed_pool *pool, uint32_t num_entries,
+		      struct rte_flow_error *error);
 
 /**
  * This function allocates new empty Three-level table.