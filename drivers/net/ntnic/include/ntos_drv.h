3f3093a81812af067dfe5f97fd3293a87fccd021
Serhii Iliushyk <sil-plv@napatech.com>
2024-07-11 14:07:26 +0200

    net/ntnic: add link management skeleton
    Add functionality to read and control the link-state of the ntnic.
    Note that must functions are not implemented yet.
    
    Adds the following eth_dev_ops:
    - dev_set_link_up
    - dev_set_link_down
    - link_update
    - mac_addr_add
    - mac_addr_set
    - set_mc_addr_list
    - promiscuous_enable
    
    Signed-off-by: Serhii Iliushyk <sil-plv@napatech.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/ntnic/include/ntos_drv.h
+++ drivers/net/ntnic/include/ntos_drv.h
@@ -25,9 +25,24 @@ struct pmd_internals {
 	const struct rte_pci_device *pci_dev;
 	char name[20];
 	int n_intf_no;
+	int lpbk_mode;
+	uint8_t ts_multiplier;
+	uint16_t min_tx_pkt_size;
+	uint16_t max_tx_pkt_size;
+	unsigned int nb_rx_queues;
+	unsigned int nb_tx_queues;
 	uint32_t port;
 	uint32_t port_id;
+	/* Offset of the VF from the PF */
+	uint8_t vf_offset;
+	nt_meta_port_type_t type;
+	/* if a virtual port type - the vhid */
+	int vhid;
 	struct drv_s *p_drv;
+	/* Ethernet (MAC) addresses. Element number zero denotes default address. */
+	struct rte_ether_addr eth_addrs[NUM_MAC_ADDRS_PER_PORT];
+	/* Multicast ethernet (MAC) addresses. */
+	struct rte_ether_addr mc_addrs[NUM_MULTICAST_ADDRS_PER_PORT];
 	struct pmd_internals *next;
 };
 
