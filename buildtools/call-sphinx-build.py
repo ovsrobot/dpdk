01f3afb0defbd040794882e383e67437259ebf72
Juraj Linkeš <juraj.linkes@pantheon.tech>
2024-07-12 10:57:23 +0200

    doc: guides and API meson improvements
    The Sphinx script argument parsing improvement gives us more
    flexibility going forward, such as the ability to add non-positional
    arguments.
    
    Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>
    Reviewed-by: Luca Vizzarro <luca.vizzarro@arm.com>
    Reviewed-by: Jeremy Spewock <jspewock@iol.unh.edu>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>
    Tested-by: Luca Vizzarro <luca.vizzarro@arm.com>
    Tested-by: Nicholas Pratte <npratte@iol.unh.edu>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- buildtools/call-sphinx-build.py
+++ buildtools/call-sphinx-build.py
@@ -3,31 +3,41 @@
 # Copyright(c) 2019 Intel Corporation
 #
 
+import argparse
 import sys
 import os
 from os.path import join
 from subprocess import run
 
-# assign parameters to variables
-(sphinx, version, src, dst, *extra_args) = sys.argv[1:]
+parser = argparse.ArgumentParser()
+parser.add_argument('sphinx')
+parser.add_argument('version')
+parser.add_argument('src')
+parser.add_argument('dst')
+args, extra_args = parser.parse_known_args()
 
 # set the version in environment for sphinx to pick up
-os.environ['DPDK_VERSION'] = version
+os.environ['DPDK_VERSION'] = args.version
 
-sphinx_cmd = [sphinx] + extra_args
+sphinx_cmd = [args.sphinx] + extra_args
 
 # find all the files sphinx will process so we can write them as dependencies
 srcfiles = []
-for root, dirs, files in os.walk(src):
+for root, dirs, files in os.walk(args.src):
     srcfiles.extend([join(root, f) for f in files])
 
+if not os.path.exists(args.dst):
+    os.makedirs(args.dst)
+
 # run sphinx, putting the html output in a "html" directory
-with open(join(dst, 'sphinx_html.out'), 'w') as out:
-    process = run(sphinx_cmd + ['-b', 'html', src, join(dst, 'html')],
-                  stdout=out)
+with open(join(args.dst, 'sphinx_html.out'), 'w') as out:
+    process = run(
+        sphinx_cmd + ['-b', 'html', args.src, join(args.dst, 'html')],
+        stdout=out
+    )
 
 # create a gcc format .d file giving all the dependencies of this doc build
-with open(join(dst, '.html.d'), 'w') as d:
+with open(join(args.dst, '.html.d'), 'w') as d:
     d.write('html: ' + ' '.join(srcfiles) + '\n')
 
 sys.exit(process.returncode)
