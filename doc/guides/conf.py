d14ac01ca92c18e08b08bceab437c6bc6d9b575d
Juraj Linkeš <juraj.linkes@pantheon.tech>
2024-07-12 10:57:24 +0200

    dts: add API doc generation
    The tool used to generate DTS API docs is Sphinx, which is already in
    use in DPDK. The same configuration is used to preserve style with one
    DTS-specific configuration (so that the DPDK docs are unchanged) that
    modifies how the sidebar displays the content.
    
    Sphinx generates the documentation from Python docstrings. The docstring
    format is the Google format [0] which requires the sphinx.ext.napoleon
    extension. The other extension, sphinx.ext.intersphinx, enables linking
    to objects in external documentations, such as the Python documentation.
    
    There are two requirements for building DTS docs:
    * The same Python version as DTS or higher, because Sphinx imports the
      code.
    * Also the same Python packages as DTS, for the same reason.
    
    [0] https://google.github.io/styleguide/pyguide.html#38-comments-and-docstrings
    
    Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>
    Reviewed-by: Luca Vizzarro <luca.vizzarro@arm.com>
    Reviewed-by: Jeremy Spewock <jspewock@iol.unh.edu>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>
    Tested-by: Luca Vizzarro <luca.vizzarro@arm.com>
    Tested-by: Nicholas Pratte <npratte@iol.unh.edu>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- doc/guides/conf.py
+++ doc/guides/conf.py
@@ -9,7 +9,7 @@ from os import listdir
 from os import environ
 from os.path import basename, dirname
 from os.path import join as path_join
-from sys import argv, stderr
+from sys import argv, stderr, path
 
 import configparser
 
@@ -22,6 +22,37 @@ except:
           'layout: https://sphinx-rtd-theme.readthedocs.io/',
           file=stderr)
     pass
+
+# Napoleon enables the Google format of Python doscstrings, used in DTS
+# Intersphinx allows linking to external projects, such as Python docs, also used in DTS
+extensions = ['sphinx.ext.napoleon', 'sphinx.ext.intersphinx']
+
+# DTS Python docstring options
+autodoc_default_options = {
+    'members': True,
+    'member-order': 'bysource',
+    'show-inheritance': True,
+}
+autodoc_class_signature = 'separated'
+autodoc_typehints = 'both'
+autodoc_typehints_format = 'short'
+autodoc_typehints_description_target = 'documented'
+napoleon_numpy_docstring = False
+napoleon_attr_annotations = True
+napoleon_preprocess_types = True
+add_module_names = False
+toc_object_entries = True
+toc_object_entries_show_parents = 'hide'
+intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
+
+dts_root = environ.get('DTS_ROOT')
+if dts_root:
+    path.append(dts_root)
+    # DTS Sidebar config
+    html_theme_options = {
+        'collapse_navigation': False,
+        'navigation_depth': -1,
+    }
 
 stop_on_error = ('-W' in argv)
 
