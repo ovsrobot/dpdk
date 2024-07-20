3d879a6a54c1da439c9dd80e29c2c762ce98af8d
Sivaprasad Tummala <sivaprasad.tummala@amd.com>
2024-07-20 16:50:27 +0000

    power: refactor uncore power management library
    This patch refactors the power management library, addressing uncore
    power management. The primary changes involve the creation of dedicated
    directories for each driver within 'drivers/power/uncore/*'. The
    adjustment of meson.build files enables the selective activation
    of individual drivers.
    
    This refactor significantly improves code organization, enhances
    clarity and boosts maintainability. It lays the foundation for more
    focused development on individual drivers and facilitates seamless
    integration of future enhancements, particularly the AMD uncore driver.
    
    Signed-off-by: Sivaprasad Tummala <sivaprasad.tummala@amd.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/power/intel_uncore/intel_uncore.h
+++ drivers/power/intel_uncore/intel_uncore.h
@@ -1,60 +1,21 @@
 /* SPDX-License-Identifier: BSD-3-Clause
  * Copyright(c) 2022 Intel Corporation
- * Copyright(c) 2024 Advanced Micro Devices, Inc.
  */
 
-#ifndef RTE_POWER_UNCORE_H
-#define RTE_POWER_UNCORE_H
+#ifndef INTEL_UNCORE_H
+#define INTEL_UNCORE_H
 
 /**
  * @file
- * RTE Uncore Frequency Management
+ * RTE Intel Uncore Frequency Management
  */
+
+#include "rte_power.h"
 #include "rte_power_uncore_ops.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
-
-/* Uncore Power Management Environment */
-enum rte_uncore_power_mgmt_env {
-	RTE_UNCORE_PM_ENV_NOT_SET,
-	RTE_UNCORE_PM_ENV_AUTO_DETECT,
-	RTE_UNCORE_PM_ENV_INTEL_UNCORE,
-	RTE_UNCORE_PM_ENV_AMD_HSMP
-};
-
-/**
- * Set the default uncore power management implementation.
- * This has to be called prior to calling any other rte_power_uncore_*() API.
- * It is thread safe. New env can be set only in uninitialized state.
- * rte_power_unset_uncore_env must be called if different env was already set.
- *
- * @param env
- *  env. The environment in which to initialise Uncore Power Management for.
- *
- * @return
- *  - 0 on success.
- *  - Negative on error.
- */
-__rte_experimental
-int rte_power_set_uncore_env(enum rte_uncore_power_mgmt_env env);
-
-/**
- * Unset the global uncore environment configuration.
- * This can only be called after all threads have completed.
- */
-__rte_experimental
-void rte_power_unset_uncore_env(void);
-
-/**
- * Get the default uncore power management implementation.
- *
- * @return
- *  power_management_env The configured environment.
- */
-__rte_experimental
-enum rte_uncore_power_mgmt_env rte_power_get_uncore_env(void);
 
 /**
  * Initialize uncore frequency management for specific die on a package.
@@ -74,7 +35,7 @@ enum rte_uncore_power_mgmt_env rte_power_get_uncore_env(void);
  *  - Negative on error.
  */
 int
-rte_power_uncore_init(unsigned int pkg, unsigned int die);
+power_intel_uncore_init(unsigned int pkg, unsigned int die);
 
 /**
  * Exit uncore frequency management on a specific die on a package.
@@ -95,7 +56,7 @@ rte_power_uncore_init(unsigned int pkg, unsigned int die);
  *  - Negative on error.
  */
 int
-rte_power_uncore_exit(unsigned int pkg, unsigned int die);
+power_intel_uncore_exit(unsigned int pkg, unsigned int die);
 
 /**
  * Return the current index of available frequencies of a specific die on a package.
@@ -114,13 +75,8 @@ rte_power_uncore_exit(unsigned int pkg, unsigned int die);
  *  The current index of available frequencies.
  *  If error, it will return 'RTE_POWER_INVALID_FREQ_INDEX = (~0)'.
  */
-static inline uint32_t
-rte_power_get_uncore_freq(unsigned int pkg, unsigned int die)
-{
-	struct rte_power_uncore_ops *ops = rte_power_get_uncore_ops();
-
-	return ops->get_freq(pkg, die);
-}
+uint32_t
+power_get_intel_uncore_freq(unsigned int pkg, unsigned int die);
 
 /**
  * Set minimum and maximum uncore frequency for specified die on a package
@@ -143,13 +99,8 @@ rte_power_get_uncore_freq(unsigned int pkg, unsigned int die)
  *  - 0 on success without frequency changed.
  *  - Negative on error.
  */
-static inline uint32_t
-rte_power_set_uncore_freq(unsigned int pkg, unsigned int die, uint32_t index)
-{
-	struct rte_power_uncore_ops *ops = rte_power_get_uncore_ops();
-
-	return ops->set_freq(pkg, die, index);
-}
+int
+power_set_intel_uncore_freq(unsigned int pkg, unsigned int die, uint32_t index);
 
 /**
  * Set minimum and maximum uncore frequency for specified die on a package
@@ -157,14 +108,21 @@ rte_power_set_uncore_freq(unsigned int pkg, unsigned int die, uint32_t index)
  * It should be protected outside of this function for threadsafe.
  *
  * This function should NOT be called in the fast path.
+ *
+ * @param pkg
+ *  Package number.
+ *  Each physical CPU in a system is referred to as a package.
+ * @param die
+ *  Die number.
+ *  Each package can have several dies connected together via the uncore mesh.
+ *
+ * @return
+ *  - 1 on success with frequency changed.
+ *  - 0 on success without frequency changed.
+ *  - Negative on error.
  */
-static inline uint32_t
-rte_power_uncore_freq_max(unsigned int pkg, unsigned int die)
-{
-	struct rte_power_uncore_ops *ops = rte_power_get_uncore_ops();
-
-	return ops->freq_max(pkg, die);
-}
+int
+power_intel_uncore_freq_max(unsigned int pkg, unsigned int die);
 
 /**
  * Set minimum and maximum uncore frequency for specified die on a package
@@ -172,14 +130,21 @@ rte_power_uncore_freq_max(unsigned int pkg, unsigned int die)
  * It should be protected outside of this function for threadsafe.
  *
  * This function should NOT be called in the fast path.
+ *
+ * @param pkg
+ *  Package number.
+ *  Each physical CPU in a system is referred to as a package.
+ * @param die
+ *  Die number.
+ *  Each package can have several dies connected together via the uncore mesh.
+ *
+ * @return
+ *  - 1 on success with frequency changed.
+ *  - 0 on success without frequency changed.
+ *  - Negative on error.
  */
-static inline uint32_t
-rte_power_uncore_freq_min(unsigned int pkg, unsigned int die)
-{
-	struct rte_power_uncore_ops *ops = rte_power_get_uncore_ops();
-
-	return ops->freq_min(pkg, die);
-}
+int
+power_intel_uncore_freq_min(unsigned int pkg, unsigned int die);
 
 /**
  * Return the list of available frequencies in the index array.
@@ -201,14 +166,9 @@ rte_power_uncore_freq_min(unsigned int pkg, unsigned int die)
  *  - The number of available index's in frequency array.
  *  - Negative on error.
  */
-static inline uint32_t
-rte_power_uncore_freqs(unsigned int pkg, unsigned int die,
-			uint32_t *freqs, uint32_t num)
-{
-	struct rte_power_uncore_ops *ops = rte_power_get_uncore_ops();
-
-	return ops->get_avail_freqs(pkg, die, freqs, num);
-}
+int
+power_intel_uncore_freqs(unsigned int pkg, unsigned int die,
+		unsigned int *freqs, unsigned int num);
 
 /**
  * Return the list length of available frequencies in the index array.
@@ -226,13 +186,9 @@ rte_power_uncore_freqs(unsigned int pkg, unsigned int die,
  *  - The number of available index's in frequency array.
  *  - Negative on error.
  */
-static inline int
-rte_power_uncore_get_num_freqs(unsigned int pkg, unsigned int die)
-{
-	struct rte_power_uncore_ops *ops = rte_power_get_uncore_ops();
+int
+power_intel_uncore_get_num_freqs(unsigned int pkg, unsigned int die);
 
-	return ops->get_num_freqs(pkg, die);
-}
 /**
  * Return the number of packages (CPUs) on a system
  * by parsing the uncore sysfs directory.
@@ -243,13 +199,9 @@ rte_power_uncore_get_num_freqs(unsigned int pkg, unsigned int die)
  *  - Zero on error.
  *  - Number of package on system on success.
  */
-static inline unsigned int
-rte_power_uncore_get_num_pkgs(void)
-{
-	struct rte_power_uncore_ops *ops = rte_power_get_uncore_ops();
+unsigned int
+power_intel_uncore_get_num_pkgs(void);
 
-	return ops->get_num_pkgs();
-}
 /**
  * Return the number of dies for pakckages (CPUs) specified
  * from parsing the uncore sysfs directory.
@@ -264,16 +216,11 @@ rte_power_uncore_get_num_pkgs(void)
  *  - Zero on error.
  *  - Number of dies for package on sucecss.
  */
-static inline unsigned int
-rte_power_uncore_get_num_dies(unsigned int pkg)
-{
-	struct rte_power_uncore_ops *ops = rte_power_get_uncore_ops();
-
-	return ops->get_num_dies(pkg);
-}
+unsigned int
+power_intel_uncore_get_num_dies(unsigned int pkg);
 
 #ifdef __cplusplus
 }
 #endif
 
-#endif /* RTE_POWER_UNCORE_H */
+#endif /* INTEL_UNCORE_H */
