e1802e03b4f751bcb7083b519ebcc64d0bc00d3b
Juraj Linkeš <juraj.linkes@pantheon.tech>
2024-08-06 10:46:22 +0200

    dts: add API doc generation
    The tool used to generate DTS API docs is Sphinx, which is already in
    use in DPDK. The same configuration is used to preserve style with one
    DTS-specific configuration (so that the DPDK docs are unchanged) that
    modifies how the sidebar displays the content.
    
    Sphinx generates the documentation from Python docstrings. The docstring
    format is the Google format [0] which requires the sphinx.ext.napoleon
    extension. The other extension, sphinx.ext.intersphinx, enables linking
    to objects in external documentations, such as the Python documentation.
    
    There is one requirement for building DTS docs - the same Python version
    as DTS or higher, because Sphinx's autodoc extension imports the code.
    
    The dependencies needed to import the code don't have to be satisfied,
    as the autodoc extension allows us to mock the imports. The missing
    packages are taken from the DTS pyproject.toml file.
    
    [0] https://google.github.io/styleguide/pyguide.html#38-comments-and-docstrings
    
    Signed-off-by: Juraj Linkeš <juraj.linkes@pantheon.tech>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ buildtools/get-dts-deps.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2024 PANTHEON.tech s.r.o.
+#
+
+"""Utilities for DTS dependencies.
+
+The module can be used as an executable script,
+which verifies that the running Python version meets the version requirement of DTS.
+The script returns the standard exit codes in this mode (0 is success, 1 is failure).
+
+The module also contains a function, get_missing_imports,
+which looks for runtime and doc generation dependencies in the DTS pyproject.toml file
+a returns a list of module names used in an import statement that are missing.
+"""
+
+import configparser
+import importlib.metadata
+import importlib.util
+import os.path
+import platform
+
+_VERSION_COMPARISON_CHARS = '^<>='
+_EXTRA_DEPS = {'invoke': '>=1.3', 'paramiko': '>=2.4'}
+_DPDK_ROOT = os.path.dirname(os.path.dirname(__file__))
+_DTS_DEP_FILE_PATH = os.path.join(_DPDK_ROOT, 'dts', 'pyproject.toml')
+
+
+def _get_version_tuple(version_str):
+    return tuple(map(int, version_str.split(".")))
+
+
+def _get_dependencies(cfg_file_path):
+    cfg = configparser.ConfigParser()
+    with open(cfg_file_path) as f:
+        dts_deps_file_str = f.read()
+        dts_deps_file_str = dts_deps_file_str.replace("\n]", "]")
+        cfg.read_string(dts_deps_file_str)
+
+    deps_section = cfg['tool.poetry.dependencies']
+    deps = {dep: deps_section[dep].strip('"\'') for dep in deps_section}
+    doc_deps_section = cfg['tool.poetry.group.docs.dependencies']
+    doc_deps = {dep: doc_deps_section[dep].strip("\"'") for dep in doc_deps_section}
+
+    return deps | doc_deps
+
+
+def get_missing_imports():
+    missing_imports = []
+    req_deps = _get_dependencies(_DTS_DEP_FILE_PATH)
+    req_deps.pop('python')
+
+    for req_dep, req_ver in (req_deps | _EXTRA_DEPS).items():
+        try:
+            req_ver = _get_version_tuple(req_ver.strip(_VERSION_COMPARISON_CHARS))
+            found_dep_ver = _get_version_tuple(importlib.metadata.version(req_dep))
+            if found_dep_ver < req_ver:
+                print(
+                    f'The version "{found_dep_ver}" of package "{req_dep}" '
+                    f'is lower than required "{req_ver}".'
+                )
+        except importlib.metadata.PackageNotFoundError:
+            print(f'Package "{req_dep}" not found.')
+            missing_imports.append(req_dep.lower().replace('-', '_'))
+
+    return missing_imports
+
+
+if __name__ == '__main__':
+    python_version = _get_dependencies(_DTS_DEP_FILE_PATH).pop('python')
+    if python_version:
+        sys_ver = _get_version_tuple(platform.python_version())
+        req_ver = _get_version_tuple(python_version.strip(_VERSION_COMPARISON_CHARS))
+        if sys_ver < req_ver:
+            print(
+                f'The available Python version "{sys_ver}" is lower than required "{req_ver}".'
+            )
+            exit(1)
