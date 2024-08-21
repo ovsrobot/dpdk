5256d3ea403fab05bc4d54621486418db8363239
Robin Jarry <rjarry@redhat.com>
2024-08-21 18:25:32 +0200

    net: add utilities for well known ipv6 address types
    Add more utilities to work with IPv6 addresses. These functions will be
    required in order to help building IPv6 routing applications.
    
    Signed-off-by: Robin Jarry <rjarry@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/net/rte_ip6.h
+++ lib/net/rte_ip6.h
@@ -28,6 +28,7 @@
 #include <netinet/ip6.h>
 #endif
 
+#include <rte_ether.h>
 #include <rte_byteorder.h>
 #include <rte_mbuf.h>
 #include <rte_memcpy.h>
@@ -156,6 +157,103 @@ rte_ipv6_addr_is_unspec(const struct rte_ipv6_addr *ip)
 	static const struct rte_ipv6_addr unspec = {0};
 	return rte_ipv6_addr_eq(ip, &unspec);
 }
+
+/**
+ * Check if an IPv6 address is the loopback address as defined in RFC 4291, section 2.5.3.
+ */
+static inline bool
+rte_ipv6_addr_is_loopback(const struct rte_ipv6_addr *ip)
+{
+	static const struct rte_ipv6_addr loopback = {
+		.a = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0x01}
+	};
+	return rte_ipv6_addr_eq(ip, &loopback);
+}
+
+/**
+ * Check if an IPv6 address is link-local as defined in RFC 4291, section 2.5.6.
+ */
+static inline bool
+rte_ipv6_addr_is_linklocal(const struct rte_ipv6_addr *ip)
+{
+	static const struct rte_ipv6_addr local = {
+		.a = {0xfe, 0x80, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}
+	};
+	return rte_ipv6_addr_eq_prefix(ip, &local, 64);
+}
+
+/**
+ * Check if an IPv6 address is multicast as defined in RFC 4291, section 2.7.
+ */
+static inline bool
+rte_ipv6_addr_is_mcast(const struct rte_ipv6_addr *ip)
+{
+	return ip->a[0] == 0xff;
+}
+
+/**
+ * Generate a link-local IPv6 address from an ethernet address as specified in
+ * RFC 2464, section 5.
+ */
+static inline void
+rte_ipv6_llocal_from_ethernet(struct rte_ipv6_addr *ip, const struct rte_ether_addr *mac)
+{
+	ip->a[0] = 0xfe;
+	ip->a[1] = 0x80;
+	memset(&ip->a[2], 0, 6);
+	ip->a[8] = mac->addr_bytes[0];
+	ip->a[9] = mac->addr_bytes[1];
+	ip->a[10] = mac->addr_bytes[2];
+	ip->a[11] = 0xff;
+	ip->a[12] = 0xfe;
+	ip->a[13] = mac->addr_bytes[3];
+	ip->a[14] = mac->addr_bytes[4];
+	ip->a[15] = mac->addr_bytes[5];
+}
+
+/**
+ * Convert a unicast or anycast IPv6 address to a solicited-node multicast
+ * address as defined in RFC 4291, section 2.7.1.
+ */
+static inline void
+rte_ipv6_solnode_from_addr(struct rte_ipv6_addr *sol, const struct rte_ipv6_addr *ip)
+{
+	sol->a[0] = 0xff;
+	sol->a[1] = 0x02;
+	memset(&sol->a[2], 0, 9);
+	sol->a[11] = 0x01;
+	sol->a[12] = 0xff;
+	sol->a[13] = ip->a[13];
+	sol->a[14] = ip->a[14];
+	sol->a[15] = ip->a[15];
+}
+
+/**
+ * Generate a multicast ethernet address from a multicast IPv6 address as defined
+ * in RFC 2464, section 7.
+ */
+static inline void
+rte_ether_mcast_from_ipv6(struct rte_ether_addr *mac, const struct rte_ipv6_addr *ip)
+{
+	mac->addr_bytes[0] = 0x33;
+	mac->addr_bytes[1] = 0x33;
+	mac->addr_bytes[2] = ip->a[12];
+	mac->addr_bytes[3] = ip->a[13];
+	mac->addr_bytes[4] = ip->a[14];
+	mac->addr_bytes[5] = ip->a[15];
+}
+
+/** Well known multicast addresses as defined in RFC 4291, section 2.7.1. */
+#define RTE_IPV6_ADDR_ALLNODES_IFACE_LOCAL \
+	((struct rte_ipv6_addr){.a = {0xff, 0x01, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}})
+#define RTE_IPV6_ADDR_ALLNODES_LINK_LOCAL \
+	((struct rte_ipv6_addr){.a = {0xff, 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}})
+#define RTE_IPV6_ADDR_ALLROUTERS_IFACE_LOCAL \
+	((struct rte_ipv6_addr){.a = {0xff, 0x01, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}})
+#define RTE_IPV6_ADDR_ALLROUTERS_LINK_LOCAL \
+	((struct rte_ipv6_addr){.a = {0xff, 0x02, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}})
+#define RTE_IPV6_ADDR_ALLROUTERS_SITE_LOCAL \
+	((struct rte_ipv6_addr){.a = {0xff, 0x05, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1}})
 
 /**
  * IPv6 Header
