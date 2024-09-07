ovsrobot/dpdk

commit 0f39f32482a1fe71df41113e5ccf8d73a423c9b2

net/sfc: remove use of EAL logtype
When registering a common logtype for this driver, instead of
explicitly fall back to EAL, rely on the log registration macro to
achieve the same.

Signed-off-by: David Marchand <david.marchand@redhat.com>
Reviewed-by: Andrew Rybchenko <andrew.rybchenko@oktetlabs.ru>

drivers/net/sfc/sfc_ethdev.c
@@ -37,8 +37,6 @@
 #define SFC_XSTAT_ID_INVALID_VAL  UINT64_MAX
 #define SFC_XSTAT_ID_INVALID_NAME '\0'
 
-uint32_t sfc_logtype_driver;
-
 static struct sfc_dp_list sfc_dp_head =
 	TAILQ_HEAD_INITIALIZER(sfc_dp_head);
 
@@ -3618,12 +3616,4 @@ RTE_PMD_REGISTER_PARAM_STRING(net_sfc_efx,
 	SFC_KVARG_FW_VARIANT "=" SFC_KVARG_VALUES_FW_VARIANT " "
 	SFC_KVARG_RXD_WAIT_TIMEOUT_NS "=<long> "
 	SFC_KVARG_STATS_UPDATE_PERIOD_MS "=<long>");
-
-RTE_INIT(sfc_driver_register_logtype)
-{
-	int ret;
-
-	ret = rte_log_register_type_and_pick_level(SFC_LOGTYPE_PREFIX "driver",
-						   RTE_LOG_NOTICE);
-	sfc_logtype_driver = (ret < 0) ? RTE_LOGTYPE_EAL : ret;
-}
+RTE_LOG_REGISTER_SUFFIX(sfc_logtype_driver, "driver", NOTICE);

drivers/net/sfc/sfc_log.h
@@ -11,7 +11,7 @@
 #define _SFC_LOG_H_
 
 /** Generic driver log type */
-extern uint32_t sfc_logtype_driver;
+extern int sfc_logtype_driver;
 
 /** Common log type name prefix */
 #define SFC_LOGTYPE_PREFIX	"pmd.net.sfc."

commit 4b5c4d11b6a1f46d60e1c38512ea97f4409003f0

net/sfc: fix driver logtype token
The net/sfc driver logtype was registered under pmd.net.sfc."driver".

Fixes: 0f39f32482a1 ("net/sfc: remove use of EAL logtype")

Signed-off-by: David Marchand <david.marchand@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/sfc/sfc_ethdev.c
@@ -3616,4 +3616,4 @@ RTE_PMD_REGISTER_PARAM_STRING(net_sfc_efx,
 	SFC_KVARG_FW_VARIANT "=" SFC_KVARG_VALUES_FW_VARIANT " "
 	SFC_KVARG_RXD_WAIT_TIMEOUT_NS "=<long> "
 	SFC_KVARG_STATS_UPDATE_PERIOD_MS "=<long>");
-RTE_LOG_REGISTER_SUFFIX(sfc_logtype_driver, "driver", NOTICE);
+RTE_LOG_REGISTER_SUFFIX(sfc_logtype_driver, driver, NOTICE);