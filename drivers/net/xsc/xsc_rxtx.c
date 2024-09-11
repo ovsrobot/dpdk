192012e80c4682fc717e45b6384059991e1464a6
WanRenyong <wanry@yunsilicon.com>
2024-09-11 10:07:34 +0800

    net/xsc: add ethdev start and stop ops
    Implement xsc ethdev start and stop function.
    
    Signed-off-by: WanRenyong <wanry@yunsilicon.com>
    Signed-off-by: Rong Qian <qianr@yunsilicon.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/xsc/xsc_rxtx.c
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2024 Yunsilicon Technology Co., Ltd.
+ */
+
+#include "xsc_log.h"
+#include "xsc_defs.h"
+#include "xsc_dev.h"
+#include "xsc_ethdev.h"
+#include "xsc_rxtx.h"
+
+uint16_t
+xsc_rx_burst(void *dpdk_rxq, struct rte_mbuf **pkts, uint16_t pkts_n)
+{
+	return 0;
+}
+
+uint16_t
+xsc_tx_burst(void *dpdk_txq, struct rte_mbuf **pkts, uint16_t pkts_n)
+{
+	return 0;
+}
+
