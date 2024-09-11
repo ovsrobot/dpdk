f4d4a525e8f947611a2881849a9f35c073cc6e20
WanRenyong <wanry@yunsilicon.com>
2024-09-11 10:07:24 +0800

    net/xsc: add PCI device probe and remove
    Support the following Yunsilicon NICs to be probed:
    
    - metaScale-200
    - metaScale-200S
    - metaScale-50
    - metaScale-100Q
    
    Signed-off-by: WanRenyong <wanry@yunsilicon.com>
    Signed-off-by: Na Na <nana@yunsilicon.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/xsc/xsc_defs.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright 2024 Yunsilicon Technology Co., Ltd.
+ */
+
+#ifndef XSC_DEFS_H_
+#define XSC_DEFS_H_
+
+#define XSC_PCI_VENDOR_ID		0x1f67
+#define XSC_PCI_DEV_ID_MS		0x1111
+
+#endif /* XSC_DEFS_H_ */
+
