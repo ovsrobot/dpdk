82b8fb5bb5ea6cc241721e4b114a6894bd79499b
Isaac Boukris <iboukris@gmail.com>
2024-09-21 17:00:21 +0300

    timer/linux: lower rounding of tsc estimation to 10KHz
    In practice, the estimation result is just a couple of KHz
    away from the kernel's tsc_khz value, so it should suffice.
    
    Roundin to 10MHz can cause a significant drift from real time,
    up to a second per 10 minutes.
    
    See also bugzilla: 959
    
    Signed-off-by: Isaac Boukris <iboukris@gmail.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/linux/eal_timer.c
+++ lib/eal/linux/eal_timer.c
@@ -192,7 +192,7 @@ get_tsc_freq(void)
 {
 #ifdef CLOCK_MONOTONIC_RAW
 #define NS_PER_SEC 1E9
-#define CYC_PER_10MHZ 1E7
+#define CYC_PER_10KHZ 1E4
 
 	struct timespec sleeptime = {.tv_nsec = NS_PER_SEC / 10 }; /* 1/10 second */
 
@@ -209,8 +209,8 @@ get_tsc_freq(void)
 
 		double secs = (double)ns/NS_PER_SEC;
 		tsc_hz = (uint64_t)((end - start)/secs);
-		/* Round up to 10Mhz. 1E7 ~ 10Mhz */
-		return RTE_ALIGN_MUL_NEAR(tsc_hz, CYC_PER_10MHZ);
+		/* Round up to 10Khz. 1E4 ~ 10Khz */
+		return RTE_ALIGN_MUL_NEAR(tsc_hz, CYC_PER_10KHZ);
 	}
 #endif
 	return 0;
