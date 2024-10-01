a117110dcc9570102bd09d01c81ede119df89ef0
Morten Brørup <mb@smartsharesystems.com>
2024-10-01 13:41:31 +0000

    mempool: fix mempool cache size
    This patch refactors the mempool cache to fix two bugs:
    1. When a mempool is created with a per-lcore cache size of N objects, the
    per-lcore caches were actually created with a size of 1.5 * N objects.
    2. The mempool cache field names did not reflect their purpose;
    the "flushthresh" field held the size, and the "size" field held the
    number of objects remaining in the cache when returning from a get
    operation refilling it from the backend.
    
    Especially the first item could be fatal:
    When more objects than a mempool's configured cache size is held in the
    mempool's caches associated with other lcores, a rightsized mempool may
    unexpectedly run out of objects, causing the application to fail.
    
    Furthermore, this patch introduces some optimizations.
    (Work in progress. Details to follow later. Submitting to get CI
    performance data.)
    
    Various drivers accessing the mempool directly have been updated
    accordingly.
    These drivers did not update mempool statistics when accessing the mempool
    directly, so that is fixed too.
    
    Note: Performance not yet benchmarked.
    
    Signed-off-by: Morten Brørup <mb@smartsharesystems.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/common/idpf/idpf_common_rxtx_avx512.c
+++ drivers/common/idpf/idpf_common_rxtx_avx512.c
@@ -1034,11 +1034,12 @@ idpf_tx_singleq_free_bufs_avx512(struct idpf_tx_queue *txq)
 			goto done;
 		}
 
-		/* The cache follows the following algorithm
-		 *   1. Add the objects to the cache
-		 *   2. Anything greater than the cache min value (if it crosses the
-		 *   cache flush threshold) is flushed to the ring.
-		 */
+		/* Insufficient free space in the cache? */
+		if (unlikely(n + cache->len > cache->size)) {
+			rte_mempool_generic_put(mp, (void *)txep, n, cache);
+			goto done;
+		}
+
 		/* Add elements back into the cache */
 		uint32_t copied = 0;
 		/* n is multiple of 32 */
@@ -1056,12 +1057,10 @@ idpf_tx_singleq_free_bufs_avx512(struct idpf_tx_queue *txq)
 		}
 		cache->len += n;
 
-		if (cache->len >= cache->flushthresh) {
-			rte_mempool_ops_enqueue_bulk(mp,
-						     &cache->objs[cache->size],
-						     cache->len - cache->size);
-			cache->len = cache->size;
-		}
+		/* Increment stat. */
+		RTE_MEMPOOL_CACHE_STAT_ADD(cache, put_bulk, 1);
+		RTE_MEMPOOL_CACHE_STAT_ADD(cache, put_objs, n);
+
 		goto done;
 	}
 
@@ -1345,11 +1344,12 @@ idpf_tx_splitq_free_bufs_avx512(struct idpf_tx_queue *txq)
 			goto done;
 		}
 
-		/* The cache follows the following algorithm
-		 *   1. Add the objects to the cache
-		 *   2. Anything greater than the cache min value (if it crosses the
-		 *   cache flush threshold) is flushed to the ring.
-		 */
+		/* Insufficient free space in the cache? */
+		if (unlikely(n + cache->len > cache->size)) {
+			rte_mempool_generic_put(mp, (void *)txep, n, cache);
+			goto done;
+		}
+
 		/* Add elements back into the cache */
 		uint32_t copied = 0;
 		/* n is multiple of 32 */
@@ -1367,12 +1367,10 @@ idpf_tx_splitq_free_bufs_avx512(struct idpf_tx_queue *txq)
 		}
 		cache->len += n;
 
-		if (cache->len >= cache->flushthresh) {
-			rte_mempool_ops_enqueue_bulk(mp,
-						     &cache->objs[cache->size],
-						     cache->len - cache->size);
-			cache->len = cache->size;
-		}
+		/* Increment stat. */
+		RTE_MEMPOOL_CACHE_STAT_ADD(cache, put_bulk, 1);
+		RTE_MEMPOOL_CACHE_STAT_ADD(cache, put_objs, n);
+
 		goto done;
 	}
 
