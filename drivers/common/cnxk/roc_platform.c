ovsrobot/dpdk

commit 455a771fd6f1a9cb6edc8711ff278ad31709cf7c

common/cnxk: fix segregation of logs based on module
Originally the logs were segregated under various labels which could be
selectively enabled. It was changed to use 'pmd.common.cnxk' while
changing the macro used for registering logging. Address the same by
restoring the segregation.

Current logs:
        ...
        logtype3
        pmd.common.cnxk
        pmd.common.iavf
        ...

Changed to:
        ...
        logtype3
        pmd.common.cnxk.base
        pmd.common.cnxk.crypto
        pmd.common.cnxk.dpi
        pmd.common.cnxk.esw
        pmd.common.cnxk.event
        pmd.common.cnxk.flow
        pmd.common.cnxk.mbox
        pmd.common.cnxk.mempool
        pmd.common.cnxk.ml
        pmd.common.cnxk.nix
        pmd.common.cnxk.ree
        pmd.common.cnxk.rep
        pmd.common.cnxk.timer
        pmd.common.cnxk.tm
        pmd.common.iavf
        ...

Updated documentation also to reflect the same.

Fixes: 233692f550a1 ("dma/cnxk: rework DMA driver")
Cc: stable@dpdk.org

Signed-off-by: Anoob Joseph <anoobj@marvell.com>

drivers/common/cnxk/roc_platform.c
@@ -85,17 +85,17 @@ roc_plt_init(void)
 	return 0;
 }
 
-RTE_LOG_REGISTER_DEFAULT(cnxk_logtype_base, NOTICE);
-RTE_LOG_REGISTER_DEFAULT(cnxk_logtype_mbox, NOTICE);
-RTE_LOG_REGISTER_DEFAULT(cnxk_logtype_cpt, NOTICE);
-RTE_LOG_REGISTER_DEFAULT(cnxk_logtype_ml, NOTICE);
-RTE_LOG_REGISTER_DEFAULT(cnxk_logtype_npa, NOTICE);
-RTE_LOG_REGISTER_DEFAULT(cnxk_logtype_nix, NOTICE);
-RTE_LOG_REGISTER_DEFAULT(cnxk_logtype_npc, NOTICE);
-RTE_LOG_REGISTER_DEFAULT(cnxk_logtype_sso, NOTICE);
-RTE_LOG_REGISTER_DEFAULT(cnxk_logtype_tim, NOTICE);
-RTE_LOG_REGISTER_DEFAULT(cnxk_logtype_tm, NOTICE);
-RTE_LOG_REGISTER_DEFAULT(cnxk_logtype_dpi, NOTICE);
-RTE_LOG_REGISTER_DEFAULT(cnxk_logtype_rep, NOTICE);
-RTE_LOG_REGISTER_DEFAULT(cnxk_logtype_esw, NOTICE);
-RTE_LOG_REGISTER_DEFAULT(cnxk_logtype_ree, NOTICE);
+RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_base, base, NOTICE);
+RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_mbox, mbox, NOTICE);
+RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_cpt, crypto, NOTICE);
+RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_ml, ml, NOTICE);
+RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_npa, mempool, NOTICE);
+RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_nix, nix, NOTICE);
+RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_npc, flow, NOTICE);
+RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_sso, event, NOTICE);
+RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_tim, timer, NOTICE);
+RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_tm, tm, NOTICE);
+RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_dpi, dpi, NOTICE);
+RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_rep, rep, NOTICE);
+RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_esw, esw, NOTICE);
+RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_ree, ree, NOTICE);

commit da75ac2f101db4426e7c9a778ffa51e40939e9e4

common/cnxk: fix updated loglevel from NOTICE to INFO
In latest dpdk PMD log type is removed and driver
specific log type is added for CNXK. This patch
changes loglevel of CNXK from NOTICE to INFO
to display logs while running applications

Fixes: 455a771fd6f1 ("common/cnxk: fix segregation of logs based on module")

Signed-off-by: Rakesh Kudurumalla <rkudurumalla@marvell.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/common/cnxk/roc_platform.c
@@ -85,7 +85,7 @@ roc_plt_init(void)
 	return 0;
 }
 
-RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_base, base, NOTICE);
+RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_base, base, INFO);
 RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_mbox, mbox, NOTICE);
 RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_cpt, crypto, NOTICE);
 RTE_LOG_REGISTER_SUFFIX(cnxk_logtype_ml, ml, NOTICE);