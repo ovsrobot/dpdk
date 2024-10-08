4bdddd481064fc6f09708334c88fa2aee80e5d12
Jun Yang <jun.yang@nxp.com>
2024-10-08 12:53:02 +0530

    dma/dpaa: add workaround for ERR050757
    ERR050757 on LS104x indicates:
    
    For outbound PCIe read transactions, a completion buffer is used
    to store the PCIe completions till the data is passed back to the
    initiator. At most 16 outstanding transactions are allowed and
    maximum read request is 256 bytes. The completion buffer size
    inside the controller needs to be at least 4KB, but the PCIe
    controller has 3 KB of buffer. In case the size of pending
    outbound read transactions of more than 3KB, the PCIe controller
    may drop the incoming completions without notifying the initiator
    of the transaction, leaving transactions unfinished. All
    subsequent outbound reads to PCIe are blocked permanently.
    To avoid qDMA hang as it keeps waiting for data that was silently
    dropped, set stride mode for qDMA.
    
    Signed-off-by: Jun Yang <jun.yang@nxp.com>
    Signed-off-by: Gagandeep Singh <g.singh@nxp.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/dma/dpaa/dpaa_qdma.c
+++ drivers/dma/dpaa/dpaa_qdma.c
@@ -167,7 +167,6 @@ fsl_qdma_pre_comp_sd_desc(struct fsl_qdma_queue *queue)
 
 		/* Descriptor Buffer */
 		sdf->srttype = FSL_QDMA_CMD_RWTTYPE;
-
 		ddf->dwttype = FSL_QDMA_CMD_RWTTYPE;
 		ddf->lwc = FSL_QDMA_CMD_LWC;
 
@@ -449,8 +448,9 @@ fsl_qdma_reg_init(struct fsl_qdma_engine *fsl_qdma)
 
 			/* Initialize the queue mode. */
 			reg = FSL_QDMA_BCQMR_EN;
-			reg |= FSL_QDMA_BCQMR_CD_THLD(ilog2(temp->n_cq) - 4);
-			reg |= FSL_QDMA_BCQMR_CQ_SIZE(ilog2(temp->n_cq) - 6);
+			reg |= FSL_QDMA_BCQMR_CD_THLD(ilog2_qthld(temp->n_cq));
+			reg |= FSL_QDMA_BCQMR_CQ_SIZE(ilog2_qsize(temp->n_cq));
+			temp->le_cqmr = reg;
 			qdma_writel(reg, block + FSL_QDMA_BCQMR(i));
 		}
 
@@ -694,6 +694,9 @@ fsl_qdma_enqueue_desc_single(struct fsl_qdma_queue *fsl_queue,
 	struct fsl_qdma_comp_sg_desc *csgf_src, *csgf_dest;
 	struct fsl_qdma_cmpd_ft *ft;
 	int ret;
+#ifdef RTE_DMA_DPAA_ERRATA_ERR050757
+	struct fsl_qdma_sdf *sdf;
+#endif
 
 	ret = fsl_qdma_enqueue_overflow(fsl_queue);
 	if (unlikely(ret))
@@ -701,6 +704,19 @@ fsl_qdma_enqueue_desc_single(struct fsl_qdma_queue *fsl_queue,
 
 	ft = fsl_queue->ft[fsl_queue->ci];
 
+#ifdef RTE_DMA_DPAA_ERRATA_ERR050757
+	sdf = &ft->df.sdf;
+	sdf->srttype = FSL_QDMA_CMD_RWTTYPE;
+	if (len > FSL_QDMA_CMD_SS_ERR050757_LEN) {
+		sdf->ssen = 1;
+		sdf->sss = FSL_QDMA_CMD_SS_ERR050757_LEN;
+		sdf->ssd = FSL_QDMA_CMD_SS_ERR050757_LEN;
+	} else {
+		sdf->ssen = 0;
+		sdf->sss = 0;
+		sdf->ssd = 0;
+	}
+#endif
 	csgf_src = &ft->desc_sbuf;
 	csgf_dest = &ft->desc_dbuf;
 	qdma_desc_sge_addr_set64(csgf_src, src);
@@ -733,6 +749,9 @@ fsl_qdma_enqueue_desc_sg(struct fsl_qdma_queue *fsl_queue)
 	uint32_t total_len;
 	uint16_t start, idx, num, i, next_idx;
 	int ret;
+#ifdef RTE_DMA_DPAA_ERRATA_ERR050757
+	struct fsl_qdma_sdf *sdf;
+#endif
 
 eq_sg:
 	total_len = 0;
@@ -798,6 +817,19 @@ eq_sg:
 	ft->desc_dsge[num - 1].final = 1;
 	csgf_src->length = total_len;
 	csgf_dest->length = total_len;
+#ifdef RTE_DMA_DPAA_ERRATA_ERR050757
+	sdf = &ft->df.sdf;
+	sdf->srttype = FSL_QDMA_CMD_RWTTYPE;
+	if (total_len > FSL_QDMA_CMD_SS_ERR050757_LEN) {
+		sdf->ssen = 1;
+		sdf->sss = FSL_QDMA_CMD_SS_ERR050757_LEN;
+		sdf->ssd = FSL_QDMA_CMD_SS_ERR050757_LEN;
+	} else {
+		sdf->ssen = 0;
+		sdf->sss = 0;
+		sdf->ssd = 0;
+	}
+#endif
 	ret = fsl_qdma_enqueue_desc_to_ring(fsl_queue, num);
 	if (ret)
 		return ret;
