5c39ba9aea584d258203f46a5cdbffe977918f02
Howard Wang <howard_wang@realsil.com.cn>
2024-10-15 11:09:27 +0800

    net/r8169: add driver_start and driver_stop
    rtl8125ap and rtl8125bp need driver start and stop whether
    dash is enabled or not.
    
    Signed-off-by: Howard Wang <howard_wang@realsil.com.cn>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/r8169/base/rtl8126a_mcu.h
+++ drivers/net/r8169/base/rtl8126a_mcu.h
@@ -12,5 +12,6 @@ void rtl_set_mac_mcu_8126a_3(struct rtl_hw *hw);
 void rtl_set_phy_mcu_8126a_1(struct rtl_hw *hw);
 void rtl_set_phy_mcu_8126a_2(struct rtl_hw *hw);
 void rtl_set_phy_mcu_8126a_3(struct rtl_hw *hw);
+
 #endif
 
