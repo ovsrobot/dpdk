b6763cf07604c2af178312c1ab509423fa173d23
Hanxiao Li <li.hanxiao@zte.com.cn>
2024-10-25 22:10:37 +0800

    crypto/zsda: introduce key expansion code
    Introduce key expansion code for zsda sym drivers.
    
    Signed-off-by: Hanxiao Li <li.hanxiao@zte.com.cn>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/crypto/zsda/zsda_sym_session.h
@@ -0,0 +1,9 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2024 ZTE Corporation
+ */
+
+#ifndef _ZSDA_SYM_SESSION_H_
+#define _ZSDA_SYM_SESSION_H_
+
+
+#endif /* _ZSDA_SYM_SESSION_H_ */
