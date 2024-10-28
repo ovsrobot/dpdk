40d26c7320e0f8766a3abf24cc679770cae88c39
Howard Wang <howard_wang@realsil.com.cn>
2024-10-28 15:31:01 +0800

    net/r8169: add support for hardware operations
    rtl_hw_ops contains multiple function pointers, each pointing to a
    specific hardware operation function.
    
    Signed-off-by: Howard Wang <howard_wang@realsil.com.cn>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/r8169/base/rtl8125b_mcu.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2024 Realtek Corporation. All rights reserved
+ */
+
+#ifndef _RTL8125B_MCU_H_
+#define _RTL8125B_MCU_H_
+
+void rtl_set_mac_mcu_8125b_1(struct rtl_hw *hw);
+void rtl_set_mac_mcu_8125b_2(struct rtl_hw *hw);
+
+void rtl_set_phy_mcu_8125b_1(struct rtl_hw *hw);
+void rtl_set_phy_mcu_8125b_2(struct rtl_hw *hw);
+
+#endif
