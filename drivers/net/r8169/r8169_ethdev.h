97d21db4a9d69a0bbe62462dad1e54de83ff6650
Howard Wang <howard_wang@realsil.com.cn>
2024-11-08 20:11:11 +0800

    net/r8169: add support for hw config
    Implement the rtl_hw_config function to configure the hardware.
    
    Signed-off-by: Howard Wang <howard_wang@realsil.com.cn>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/r8169/r8169_ethdev.h
+++ drivers/net/r8169/r8169_ethdev.h
@@ -13,7 +13,18 @@
 #include "r8169_compat.h"
 
 struct rtl_hw {
-	u8 *mmio_addr;
+	u8  *mmio_addr;
+	u32 mcfg;
+	u8  HwSuppIntMitiVer;
+
+	/* Enable Tx No Close */
+	u8 EnableTxNoClose;
+
+	/* Dash */
+	u8 HwSuppDashVer;
+	u8 DASH;
+	u8 HwSuppOcpChannelVer;
+	u8 AllowAccessDashOcp;
 };
 
 struct rtl_sw_stats {
