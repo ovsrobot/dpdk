ovsrobot/dpdk

commit 0513f0af034df5dc543bb6eb6b17661839491a89

net/bnxt/tf_ulp: add stats cache for Thor2
This change adds a stats cache for Thor2 flows using counters.
Flow stats will be harvested periodically in the background
and stats reads by the application will be returned stats from
the cache and not by initiating a read from HW.

This change also adds read-clear functionality for counter resets
and restructures the stats collection while loop to
guarantee full coverage of entries added or removed during the
collection period.

Signed-off-by: Peter Spreadborough <peter.spreadborough@broadcom.com>
Signed-off-by: Sriharsha Basavapatna <sriharsha.basavapatna@broadcom.com>
Reviewed-by: Jay Ding <jay.ding@broadcom.com>
Reviewed-by: Shahaji Bhosle <sbhosle@broadcom.com>
Reviewed-by: Ajit Khaparde <ajit.khaparde@broadcom.com>

drivers/net/bnxt/tf_ulp/bnxt_ulp.h
@@ -162,6 +162,8 @@ struct bnxt_ulp_data {
 	uint64_t			feature_bits;
 	uint64_t			default_class_bits;
 	uint64_t			default_act_bits;
+	struct ulp_fc_tfc_stats_cache_entry *stats_cache;
+	struct bnxt_ulp_sc_info		*sc_info;
 };
 
 enum bnxt_ulp_tfo_type {

drivers/net/bnxt/tf_ulp/ulp_sc_mgr.c
@@ -1,0 +1,526 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2014-2021 Broadcom
+ * All rights reserved.
+ */
+
+#include <rte_common.h>
+#include <rte_cycles.h>
+#include <rte_malloc.h>
+#include <rte_log.h>
+#include "bnxt.h"
+#include "bnxt_ulp.h"
+#include "bnxt_ulp_utils.h"
+#include "bnxt_ulp_tfc.h"
+#include "bnxt_tf_common.h"
+#include "ulp_sc_mgr.h"
+#include "ulp_flow_db.h"
+#include "ulp_template_db_enum.h"
+#include "ulp_template_struct.h"
+#include "tfc.h"
+#include "tfc_debug.h"
+#include "tfc_action_handle.h"
+
+#define ULP_TFC_CNTR_READ_BYTES 32
+#define ULP_TFC_CNTR_ALIGN 32
+#define ULP_TFC_ACT_WORD_SZ 32
+
+static const struct bnxt_ulp_sc_core_ops *
+bnxt_ulp_sc_ops_get(struct bnxt_ulp_context *ctxt)
+{
+	int32_t rc;
+	enum bnxt_ulp_device_id  dev_id;
+	const struct bnxt_ulp_sc_core_ops *func_ops;
+
+	rc = bnxt_ulp_cntxt_dev_id_get(ctxt, &dev_id);
+	if (rc)
+		return NULL;
+
+	switch (dev_id) {
+	case BNXT_ULP_DEVICE_ID_THOR2:
+		func_ops = &ulp_sc_tfc_core_ops;
+		break;
+	case BNXT_ULP_DEVICE_ID_THOR:
+	case BNXT_ULP_DEVICE_ID_STINGRAY:
+	case BNXT_ULP_DEVICE_ID_WH_PLUS:
+	default:
+		func_ops = NULL;
+		break;
+	}
+	return func_ops;
+}
+
+int32_t ulp_sc_mgr_init(struct bnxt_ulp_context *ctxt)
+{
+	const struct bnxt_ulp_sc_core_ops *sc_ops;
+	struct bnxt_ulp_device_params *dparms;
+	struct bnxt_ulp_sc_info *ulp_sc_info;
+	uint32_t stats_cache_tbl_sz;
+	uint32_t dev_id;
+	int rc;
+
+	if (!ctxt) {
+		BNXT_DRV_DBG(DEBUG, "Invalid ULP CTXT\n");
+		return -EINVAL;
+	}
+
+	if (bnxt_ulp_cntxt_dev_id_get(ctxt, &dev_id)) {
+		BNXT_DRV_DBG(DEBUG, "Failed to get device id\n");
+		return -EINVAL;
+	}
+
+	dparms = bnxt_ulp_device_params_get(dev_id);
+	if (!dparms) {
+		BNXT_DRV_DBG(DEBUG, "Failed to device parms\n");
+		return -EINVAL;
+	}
+
+	sc_ops = bnxt_ulp_sc_ops_get(ctxt);
+	if (sc_ops == NULL) {
+		BNXT_DRV_DBG(DEBUG, "Failed to get the counter ops\n");
+		return -EINVAL;
+	}
+
+	ulp_sc_info = rte_zmalloc("ulp_sc_info", sizeof(*ulp_sc_info), 0);
+	if (!ulp_sc_info) {
+		rc = -ENOMEM;
+		goto error;
+	}
+
+	ulp_sc_info->sc_ops = sc_ops;
+	ulp_sc_info->flags = 0;
+
+	rc = pthread_mutex_init(&ulp_sc_info->sc_lock, NULL);
+	if (rc) {
+		BNXT_DRV_DBG(ERR, "Failed to initialize sc mutex\n");
+		goto error;
+	}
+
+	/* Add the SC info tbl to the ulp context. */
+	bnxt_ulp_cntxt_ptr2_sc_info_set(ctxt, ulp_sc_info);
+
+	ulp_sc_info->num_counters = dparms->ext_flow_db_num_entries;
+	if (!ulp_sc_info->num_counters) {
+		/* No need for software counters, call fw directly */
+		BNXT_DRV_DBG(DEBUG, "Sw flow counter support not enabled\n");
+		return 0;
+	}
+
+	/*
+	 * Size is determined by the number of flows + 10% to cover IDs
+	 * used for resources.
+	 */
+	stats_cache_tbl_sz = sizeof(struct ulp_sc_tfc_stats_cache_entry) *
+		(ulp_sc_info->num_counters +
+		 (ulp_sc_info->num_counters / 10));
+
+	ulp_sc_info->stats_cache_tbl = rte_zmalloc("ulp_stats_cache_tbl",
+						   stats_cache_tbl_sz, 0);
+	if (!ulp_sc_info->stats_cache_tbl) {
+		rc = -ENOMEM;
+		goto error;
+	}
+
+	ulp_sc_info->read_data = rte_zmalloc("ulp_stats_cache_read_data",
+					     ULP_SC_BATCH_SIZE * ULP_SC_PAGE_SIZE,
+					     ULP_SC_PAGE_SIZE);
+	if (!ulp_sc_info->read_data) {
+		rte_free(ulp_sc_info->stats_cache_tbl);
+		rc = -ENOMEM;
+		goto error;
+	}
+
+	rc = ulp_sc_mgr_thread_start(ctxt);
+	if (rc)
+		BNXT_DRV_DBG(DEBUG, "Stats counter thread start failed\n");
+
+ error:
+	return rc;
+}
+
+/*
+ * Release all resources in the Flow Counter Manager for this ulp context
+ *
+ * ctxt [in] The ulp context for the Flow Counter manager
+ *
+ */
+int32_t
+ulp_sc_mgr_deinit(struct bnxt_ulp_context *ctxt)
+{
+	struct bnxt_ulp_sc_info *ulp_sc_info;
+
+	ulp_sc_info = bnxt_ulp_cntxt_ptr2_sc_info_get(ctxt);
+
+	if (!ulp_sc_info)
+		return -EINVAL;
+
+	pthread_mutex_lock(&ulp_sc_info->sc_lock);
+
+	ulp_sc_mgr_thread_cancel(ctxt);
+
+	pthread_mutex_destroy(&ulp_sc_info->sc_lock);
+
+	if (ulp_sc_info->stats_cache_tbl)
+		rte_free(ulp_sc_info->stats_cache_tbl);
+
+	if (ulp_sc_info->read_data)
+		rte_free(ulp_sc_info->read_data);
+
+	rte_free(ulp_sc_info);
+
+	/* Safe to ignore on deinit */
+	(void)bnxt_ulp_cntxt_ptr2_sc_info_set(ctxt, NULL);
+
+	return 0;
+}
+
+#define ULP_SC_PERIOD_S 1
+#define ULP_SC_PERIOD_MS (ULP_SC_PERIOD_S * 1000)
+
+static uint32_t ulp_stats_cache_main_loop(void *arg)
+{
+	struct ulp_sc_tfc_stats_cache_entry *count;
+	const struct bnxt_ulp_sc_core_ops *sc_ops;
+	struct ulp_sc_tfc_stats_cache_entry *sce;
+	struct ulp_sc_tfc_stats_cache_entry *sce_end;
+	struct tfc_mpc_batch_info_t batch_info;
+	struct bnxt_ulp_sc_info *ulp_sc_info;
+	struct bnxt_ulp_context *ctxt = NULL;
+	uint16_t words = (ULP_TFC_CNTR_READ_BYTES + ULP_TFC_ACT_WORD_SZ - 1) / ULP_TFC_ACT_WORD_SZ;
+	uint32_t batch_size;
+	struct tfc *tfcp = NULL;
+	uint32_t batch;
+	uint32_t delay = ULP_SC_PERIOD_MS;
+	uint64_t start;
+	uint64_t stop;
+	uint64_t hz;
+	uint8_t *data;
+	int rc;
+	static uint32_t loop;
+	uint64_t cycles = 0;
+	uint64_t cpms = 0;
+
+	while (!ctxt) {
+		ctxt = bnxt_ulp_cntxt_entry_acquire(arg);
+
+		if (ctxt)
+			break;
+
+		BNXT_DRV_DBG(INFO, "could not get the ulp context lock\n");
+		rte_delay_us_block(1000);
+	}
+
+
+	ulp_sc_info = bnxt_ulp_cntxt_ptr2_sc_info_get(ctxt);
+	if (!ulp_sc_info) {
+		bnxt_ulp_cntxt_entry_release();
+		goto terminate;
+	}
+
+	sc_ops = ulp_sc_info->sc_ops;
+
+	hz = rte_get_timer_hz();
+	cpms = hz / 1000;
+
+	while (true) {
+		bnxt_ulp_cntxt_entry_release();
+		ctxt = NULL;
+		rte_delay_ms(delay);
+
+		while (!ctxt) {
+			ctxt = bnxt_ulp_cntxt_entry_acquire(arg);
+
+			if (ctxt)
+				break;
+
+			BNXT_DRV_DBG(INFO, "could not get the ulp context lock\n");
+			rte_delay_us_block(1);
+		}
+
+		start = rte_get_timer_cycles();
+		sce = ulp_sc_info->stats_cache_tbl;
+		sce_end = sce + (ulp_sc_info->num_counters + (ulp_sc_info->num_counters / 10));
+
+		while (ulp_sc_info->num_entries && (sce < sce_end)) {
+			data = ulp_sc_info->read_data;
+
+			rc = tfc_mpc_batch_start(&batch_info);
+			if (rc) {
+				PMD_DRV_LOG_LINE(ERR,
+						 "MPC batch start failed rc:%d loop:%d",
+						 rc, loop);
+				break;
+			}
+
+			if (bnxt_ulp_cntxt_acquire_fdb_lock(ctxt))
+				break;
+
+			rc = pthread_mutex_lock(&ulp_sc_info->sc_lock);
+			if (rc) {
+				PMD_DRV_LOG_LINE(ERR,
+						 "Failed to get SC lock, terminating main loop rc:%d loop:%d",
+						 rc, loop);
+				goto terminate;
+			}
+
+			for (batch = 0; (batch < ULP_SC_BATCH_SIZE) && (sce < sce_end);) {
+				if (!(sce->flags & ULP_SC_ENTRY_FLAG_VALID)) {
+					sce++;
+					continue;
+				}
+
+				tfcp = bnxt_ulp_cntxt_tfcp_get(sce->ctxt);
+				if (tfcp == NULL) {
+					bnxt_ulp_cntxt_entry_release();
+					goto terminate;
+				}
+
+
+				/* Store the entry pointer to use for counter update */
+				batch_info.em_hdl[batch_info.count] = (uint64_t)sce;
+
+				rc = sc_ops->ulp_stats_cache_update(tfcp,
+								    sce->dir,
+								    data,
+								    sce->handle,
+								    &words,
+								    &batch_info,
+								    sce->reset);
+				if (rc) {
+					/* Abort this batch */
+					PMD_DRV_LOG_LINE(ERR,
+							 "loop:%d read_counter() failed:%d",
+							 loop, rc);
+					break;
+				}
+
+				if (sce->reset)
+					sce->reset = false;
+
+				/* Next */
+				batch++;
+				sce++;
+				data += ULP_SC_PAGE_SIZE;
+			}
+
+			batch_size = batch_info.count;
+			rc = tfc_mpc_batch_end(tfcp, &batch_info);
+
+			pthread_mutex_unlock(&ulp_sc_info->sc_lock);
+			bnxt_ulp_cntxt_release_fdb_lock(ctxt);
+
+			if (rc) {
+				PMD_DRV_LOG_LINE(ERR,
+						 "MPC batch end failed rc:%d loop:%d",
+						 rc, loop);
+				batch_info.enabled = false;
+				break;
+			}
+
+			/* Process counts */
+			data = ulp_sc_info->read_data;
+
+			for (batch = 0; batch < batch_size; batch++) {
+				/* Check for error in completion */
+				if (batch_info.result[batch]) {
+					PMD_DRV_LOG_LINE(ERR,
+							 "batch:%d result:%d",
+							 batch, batch_info.result[batch]);
+				} else {
+					count = (struct ulp_sc_tfc_stats_cache_entry *)
+						((uintptr_t)batch_info.em_hdl[batch]);
+					memcpy(&count->packet_count, data, ULP_TFC_ACT_WORD_SZ);
+				}
+
+				data += ULP_SC_PAGE_SIZE;
+			}
+		}
+
+		loop++;
+		stop = rte_get_timer_cycles();
+		cycles = stop - start;
+		if (cycles > (hz * ULP_SC_PERIOD_S)) {
+			PMD_DRV_LOG_LINE(ERR,
+					 "Stats collection time exceeded %dmS Cycles:%" PRIu64,
+					 ULP_SC_PERIOD_MS, cycles);
+			delay = ULP_SC_PERIOD_MS;
+		} else {
+			delay = ULP_SC_PERIOD_MS - (cycles / cpms);
+
+			if (delay > ULP_SC_PERIOD_MS) {
+				PMD_DRV_LOG_LINE(ERR,
+						 "Stats collection delay:%dmS exceedes %dmS",
+						 delay, ULP_SC_PERIOD_MS);
+				delay = ULP_SC_PERIOD_MS;
+			}
+		}
+	}
+
+ terminate:
+	return 0;
+}
+
+/*
+ * Check if the alarm thread that walks through the flows is started
+ *
+ * ctxt [in] The ulp context for the flow counter manager
+ *
+ */
+bool ulp_sc_mgr_thread_isstarted(struct bnxt_ulp_context *ctxt)
+{
+	struct bnxt_ulp_sc_info *ulp_sc_info;
+
+	ulp_sc_info = bnxt_ulp_cntxt_ptr2_sc_info_get(ctxt);
+
+	if (ulp_sc_info)
+		return !!(ulp_sc_info->flags & ULP_FLAG_SC_THREAD);
+
+	return false;
+}
+
+/*
+ * Setup the Flow counter timer thread that will fetch/accumulate raw counter
+ * data from the chip's internal flow counters
+ *
+ * ctxt [in] The ulp context for the flow counter manager
+ *
+ */
+int32_t
+ulp_sc_mgr_thread_start(struct bnxt_ulp_context *ctxt)
+{
+	struct bnxt_ulp_sc_info *ulp_sc_info;
+	int rc;
+
+	ulp_sc_info = bnxt_ulp_cntxt_ptr2_sc_info_get(ctxt);
+
+	if (ulp_sc_info && !(ulp_sc_info->flags & ULP_FLAG_SC_THREAD)) {
+		rc = rte_thread_create(&ulp_sc_info->tid,
+				       NULL,
+				       &ulp_stats_cache_main_loop,
+				       (void *)ctxt->cfg_data);
+		if (rc)
+			return rc;
+
+		ulp_sc_info->flags |= ULP_FLAG_SC_THREAD;
+	}
+
+	return 0;
+}
+
+/*
+ * Cancel the alarm handler
+ *
+ * ctxt [in] The ulp context for the flow counter manager
+ *
+ */
+void ulp_sc_mgr_thread_cancel(struct bnxt_ulp_context *ctxt)
+{
+	struct bnxt_ulp_sc_info *ulp_sc_info;
+
+	ulp_sc_info = bnxt_ulp_cntxt_ptr2_sc_info_get(ctxt);
+	if (!ulp_sc_info)
+		return;
+
+	ulp_sc_info->flags &= ~ULP_FLAG_SC_THREAD;
+}
+
+/*
+ * Fill the rte_flow_query_count 'data' argument passed
+ * in the rte_flow_query() with the values obtained and
+ * accumulated locally.
+ *
+ * ctxt [in] The ulp context for the flow counter manager
+ *
+ * flow_id [in] The HW flow ID
+ *
+ * count [out] The rte_flow_query_count 'data' that is set
+ *
+ */
+int ulp_sc_mgr_query_count_get(struct bnxt_ulp_context *ctxt,
+			       uint32_t flow_id,
+			       struct rte_flow_query_count *count)
+{
+	struct ulp_sc_tfc_stats_cache_entry *sce;
+	struct bnxt_ulp_sc_info *ulp_sc_info;
+	int rc = 0;
+
+	ulp_sc_info = bnxt_ulp_cntxt_ptr2_sc_info_get(ctxt);
+	if (!ulp_sc_info)
+		return -ENODEV;
+
+	sce = ulp_sc_info->stats_cache_tbl;
+	sce += flow_id;
+
+	/* If entry is not valid return an error */
+	if (!(sce->flags & ULP_SC_ENTRY_FLAG_VALID))
+		return -EBUSY;
+
+	count->hits = sce->packet_count;
+	count->hits_set = 1;
+	count->bytes = sce->byte_count;
+	count->bytes_set = 1;
+
+	if (count->reset)
+		sce->reset = true;
+
+	return rc;
+}
+
+
+int ulp_sc_mgr_entry_alloc(struct bnxt_ulp_mapper_parms *parms,
+			   uint64_t counter_handle,
+			   struct bnxt_ulp_mapper_tbl_info *tbl)
+{
+	struct ulp_sc_tfc_stats_cache_entry *sce;
+	struct bnxt_ulp_sc_info *ulp_sc_info;
+
+	ulp_sc_info = bnxt_ulp_cntxt_ptr2_sc_info_get(parms->ulp_ctx);
+	if (!ulp_sc_info)
+		return -ENODEV;
+
+	pthread_mutex_lock(&ulp_sc_info->sc_lock);
+
+	sce = ulp_sc_info->stats_cache_tbl;
+	sce += parms->flow_id;
+
+	/* If entry is not free return an error */
+	if (sce->flags & ULP_SC_ENTRY_FLAG_VALID) {
+		pthread_mutex_unlock(&ulp_sc_info->sc_lock);
+		return -EBUSY;
+	}
+
+	memset(sce, 0, sizeof(*sce));
+	sce->ctxt = parms->ulp_ctx;
+	sce->flags |= ULP_SC_ENTRY_FLAG_VALID;
+	sce->handle = counter_handle;
+	sce->dir = tbl->direction;
+	ulp_sc_info->num_entries++;
+	pthread_mutex_unlock(&ulp_sc_info->sc_lock);
+
+	return 0;
+}
+
+void ulp_sc_mgr_entry_free(struct bnxt_ulp_context *ulp,
+			   uint32_t fid)
+{
+	struct ulp_sc_tfc_stats_cache_entry *sce;
+	struct bnxt_ulp_sc_info *ulp_sc_info;
+
+	ulp_sc_info = bnxt_ulp_cntxt_ptr2_sc_info_get(ulp);
+	if (!ulp_sc_info)
+		return;
+
+	pthread_mutex_lock(&ulp_sc_info->sc_lock);
+
+	sce = ulp_sc_info->stats_cache_tbl;
+	sce += fid;
+
+	if (!(sce->flags & ULP_SC_ENTRY_FLAG_VALID)) {
+		pthread_mutex_unlock(&ulp_sc_info->sc_lock);
+		return;
+	}
+
+	sce->flags = 0;
+	ulp_sc_info->num_entries--;
+
+	pthread_mutex_unlock(&ulp_sc_info->sc_lock);
+}

drivers/net/bnxt/tf_ulp/ulp_sc_mgr.h
@@ -1,0 +1,142 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2014-2023 Broadcom
+ * All rights reserved.
+ */
+
+#ifndef _ULP_SC_MGR_H_
+#define _ULP_SC_MGR_H_
+
+#include "pthread.h"
+#include "bnxt_ulp.h"
+#include "ulp_flow_db.h"
+
+#define ULP_FLAG_SC_THREAD			BIT(0)
+
+#define ULP_SC_ENTRY_FLAG_VALID BIT(0)
+
+#define ULP_SC_BATCH_SIZE   64
+#define ULP_SC_PAGE_SIZE  4096
+
+struct ulp_sc_tfc_stats_cache_entry {
+	struct bnxt_ulp_context *ctxt;
+	uint32_t flags;
+	uint64_t timestamp;
+	uint64_t handle;
+	uint8_t dir;
+	uint64_t packet_count;
+	uint64_t byte_count;
+	uint64_t count_fields1;
+	uint64_t count_fields2;
+	bool reset;
+};
+
+struct bnxt_ulp_sc_info {
+	struct ulp_sc_tfc_stats_cache_entry *stats_cache_tbl;
+	uint8_t                 *read_data;
+	uint32_t		flags;
+	uint32_t		num_entries;
+	pthread_mutex_t		sc_lock;
+	uint32_t		num_counters;
+	rte_thread_t            tid;
+	const struct bnxt_ulp_sc_core_ops *sc_ops;
+};
+
+struct bnxt_ulp_sc_core_ops {
+	int32_t
+	(*ulp_stats_cache_update)(struct tfc *tfcp,
+				  int dir,
+				  uint8_t *data,
+				  uint64_t handle,
+				  uint16_t *words,
+				  struct tfc_mpc_batch_info_t *batch_info,
+				  bool reset);
+};
+
+/*
+ * Allocate all resources in the stats cache manager for this ulp context
+ *
+ * ctxt [in] The ulp context for the stats cache manager
+ */
+int32_t
+ulp_sc_mgr_init(struct bnxt_ulp_context *ctxt);
+
+/*
+ * Release all resources in the stats cache manager for this ulp context
+ *
+ * ctxt [in] The ulp context for the stats cache manager
+ */
+int32_t
+ulp_sc_mgr_deinit(struct bnxt_ulp_context *ctxt);
+
+/*
+ * Setup the stats cache timer thread that will fetch/accumulate raw counter
+ * data from the chip's internal stats caches
+ *
+ * ctxt [in] The ulp context for the stats cache manager
+ */
+int32_t
+ulp_sc_mgr_thread_start(struct bnxt_ulp_context *ctxt);
+
+/*
+ * Alarm handler that will issue the TF-Core API to fetch
+ * data from the chip's internal stats caches
+ *
+ * ctxt [in] The ulp context for the stats cache manager
+ */
+void
+ulp_sc_mgr_alarm_cb(void *arg);
+
+/*
+ * Cancel the alarm handler
+ *
+ * ctxt [in] The ulp context for the stats cache manager
+ *
+ */
+void ulp_sc_mgr_thread_cancel(struct bnxt_ulp_context *ctxt);
+
+/*
+ * Check if the thread that walks through the flows is started
+ *
+ * ctxt [in] The ulp context for the stats cache manager
+ *
+ */
+bool ulp_sc_mgr_thread_isstarted(struct bnxt_ulp_context *ctxt);
+
+/*
+ * Get the current counts for the given flow id
+ *
+ * ctxt [in] The ulp context for the stats cache manager
+ * flow_id [in] The flow identifier
+ * count [out] structure in which the updated counts are passed
+ * back to the caller.
+ *
+ */
+int ulp_sc_mgr_query_count_get(struct bnxt_ulp_context *ctxt,
+			       uint32_t flow_id,
+			       struct rte_flow_query_count *count);
+
+/*
+ * Allocate a cache entry for flow
+ *
+ * parms [in] Various fields used to identify the flow
+ * counter_handle [in] This is the action table entry identifier.
+ * tbl [in] Various fields used to identify the flow
+ *
+ */
+int ulp_sc_mgr_entry_alloc(struct bnxt_ulp_mapper_parms *parms,
+			   uint64_t counter_handle,
+			   struct bnxt_ulp_mapper_tbl_info *tbl);
+
+/*
+ * Free cache entry
+ *
+ * ulp [in] The ulp context for the stats cache manager
+ * fid [in] The flow identifier
+ *
+ */
+void ulp_sc_mgr_entry_free(struct bnxt_ulp_context *ulp,
+			   uint32_t fid);
+
+extern const struct bnxt_ulp_sc_core_ops ulp_sc_tfc_core_ops;
+
+#endif /* _ULP_SC_MGR_H_ */

commit 4333a7812f951f4809b691cf8f4c9604e30603e9

net/bnxt/tf_ulp: fix vfr clean up and stats lockup
The representor flows were not being deleted as part of the
vfr clean up. Added code to delete flows related to vfr interface.
Also fixed the stats counter thread lockup.

Fixes: 0513f0af034d ("net/bnxt/tf_ulp: add stats cache for Thor2")
Reviewed-by: Peter Spreadborough <peter.spreadborough@broadcom.com>
Reviewed-by: Shuanglin Wang <shuanglin.wang@broadcom.com>
Signed-off-by: Kishore Padmanabha <kishore.padmanabha@broadcom.com>
Signed-off-by: Sriharsha Basavapatna <sriharsha.basavapatna@broadcom.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/bnxt/tf_ulp/bnxt_ulp.c
@@ -596,6 +596,9 @@ bnxt_ulp_port_deinit(struct bnxt *bp)
 			/* close the session associated with this port */
 			bp->ulp_ctx->ops->ulp_ctx_detach(bp, session);
 		} else {
+			/* Free the ulp context in the context entry list */
+			bnxt_ulp_cntxt_list_del(bp->ulp_ctx);
+
 			/* clean up default flows */
 			bnxt_ulp_destroy_df_rules(bp, true);
 
@@ -660,6 +663,20 @@ bnxt_ulp_cntxt_list_del(struct bnxt_ulp_context *ulp_ctx)
 		}
 	}
 	rte_spinlock_unlock(&bnxt_ulp_ctxt_lock);
+}
+
+int
+bnxt_ulp_cntxt_list_count(void)
+{
+	struct ulp_context_list_entry *entry, *temp;
+	int count_1 = 0;
+
+	rte_spinlock_lock(&bnxt_ulp_ctxt_lock);
+	RTE_TAILQ_FOREACH_SAFE(entry, &ulp_cntx_list, next, temp) {
+		count_1++;
+	}
+	rte_spinlock_unlock(&bnxt_ulp_ctxt_lock);
+	return count_1;
 }
 
 struct bnxt_ulp_context *

drivers/net/bnxt/tf_ulp/bnxt_ulp.h
@@ -297,6 +297,9 @@ bnxt_ulp_cntxt_list_add(struct bnxt_ulp_context *ulp_ctx);
 void
 bnxt_ulp_cntxt_list_del(struct bnxt_ulp_context *ulp_ctx);
 
+int
+bnxt_ulp_cntxt_list_count(void);
+
 struct bnxt_ulp_context *
 bnxt_ulp_cntxt_entry_acquire(void *arg);
 

drivers/net/bnxt/tf_ulp/ulp_sc_mgr.c
@@ -89,12 +89,6 @@ int32_t ulp_sc_mgr_init(struct bnxt_ulp_context *ctxt)
 	ulp_sc_info->sc_ops = sc_ops;
 	ulp_sc_info->flags = 0;
 
-	rc = pthread_mutex_init(&ulp_sc_info->sc_lock, NULL);
-	if (rc) {
-		BNXT_DRV_DBG(ERR, "Failed to initialize sc mutex\n");
-		goto error;
-	}
-
 	/* Add the SC info tbl to the ulp context. */
 	bnxt_ulp_cntxt_ptr2_sc_info_set(ctxt, ulp_sc_info);
 
@@ -109,9 +103,10 @@ int32_t ulp_sc_mgr_init(struct bnxt_ulp_context *ctxt)
 	 * Size is determined by the number of flows + 10% to cover IDs
 	 * used for resources.
 	 */
+	ulp_sc_info->cache_tbl_size = ulp_sc_info->num_counters +
+		(ulp_sc_info->num_counters / 10);
 	stats_cache_tbl_sz = sizeof(struct ulp_sc_tfc_stats_cache_entry) *
-		(ulp_sc_info->num_counters +
-		 (ulp_sc_info->num_counters / 10));
+		ulp_sc_info->cache_tbl_size;
 
 	ulp_sc_info->stats_cache_tbl = rte_zmalloc("ulp_stats_cache_tbl",
 						   stats_cache_tbl_sz, 0);
@@ -153,12 +148,6 @@ ulp_sc_mgr_deinit(struct bnxt_ulp_context *ctxt)
 	if (!ulp_sc_info)
 		return -EINVAL;
 
-	pthread_mutex_lock(&ulp_sc_info->sc_lock);
-
-	ulp_sc_mgr_thread_cancel(ctxt);
-
-	pthread_mutex_destroy(&ulp_sc_info->sc_lock);
-
 	if (ulp_sc_info->stats_cache_tbl)
 		rte_free(ulp_sc_info->stats_cache_tbl);
 
@@ -173,13 +162,13 @@ ulp_sc_mgr_deinit(struct bnxt_ulp_context *ctxt)
 	return 0;
 }
 
-#define ULP_SC_PERIOD_S 1
-#define ULP_SC_PERIOD_MS (ULP_SC_PERIOD_S * 1000)
+#define ULP_SC_PERIOD_US 256
+#define ULP_SC_CTX_DELAY 10000
 
 static uint32_t ulp_stats_cache_main_loop(void *arg)
 {
 	struct ulp_sc_tfc_stats_cache_entry *count;
-	const struct bnxt_ulp_sc_core_ops *sc_ops;
+	const struct bnxt_ulp_sc_core_ops *sc_ops = NULL;
 	struct ulp_sc_tfc_stats_cache_entry *sce;
 	struct ulp_sc_tfc_stats_cache_entry *sce_end;
 	struct tfc_mpc_batch_info_t batch_info;
@@ -188,95 +177,68 @@ static uint32_t ulp_stats_cache_main_loop(void *arg)
 	uint16_t words = (ULP_TFC_CNTR_READ_BYTES + ULP_TFC_ACT_WORD_SZ - 1) / ULP_TFC_ACT_WORD_SZ;
 	uint32_t batch_size;
 	struct tfc *tfcp = NULL;
-	uint32_t batch;
-	uint32_t delay = ULP_SC_PERIOD_MS;
-	uint64_t start;
-	uint64_t stop;
-	uint64_t hz;
+	uint32_t batch, stat_cnt;
 	uint8_t *data;
 	int rc;
-	static uint32_t loop;
-	uint64_t cycles = 0;
-	uint64_t cpms = 0;
-
-	while (!ctxt) {
-		ctxt = bnxt_ulp_cntxt_entry_acquire(arg);
-
-		if (ctxt)
-			break;
-
-		BNXT_DRV_DBG(INFO, "could not get the ulp context lock\n");
-		rte_delay_us_block(1000);
-	}
-
-
-	ulp_sc_info = bnxt_ulp_cntxt_ptr2_sc_info_get(ctxt);
-	if (!ulp_sc_info) {
-		bnxt_ulp_cntxt_entry_release();
-		goto terminate;
-	}
-
-	sc_ops = ulp_sc_info->sc_ops;
-
-	hz = rte_get_timer_hz();
-	cpms = hz / 1000;
 
 	while (true) {
-		bnxt_ulp_cntxt_entry_release();
 		ctxt = NULL;
-		rte_delay_ms(delay);
-
 		while (!ctxt) {
 			ctxt = bnxt_ulp_cntxt_entry_acquire(arg);
 
 			if (ctxt)
 				break;
-
-			BNXT_DRV_DBG(INFO, "could not get the ulp context lock\n");
-			rte_delay_us_block(1);
+			/* If there are no more contexts just exit */
+			if (bnxt_ulp_cntxt_list_count() == 0)
+				goto terminate;
+			rte_delay_us_block(ULP_SC_CTX_DELAY);
 		}
 
-		start = rte_get_timer_cycles();
+		/* get the stats counter info block from ulp context */
+		ulp_sc_info = bnxt_ulp_cntxt_ptr2_sc_info_get(ctxt);
+		if (unlikely(!ulp_sc_info)) {
+			bnxt_ulp_cntxt_entry_release();
+			goto terminate;
+		}
+
 		sce = ulp_sc_info->stats_cache_tbl;
-		sce_end = sce + (ulp_sc_info->num_counters + (ulp_sc_info->num_counters / 10));
+		sce_end = sce + ulp_sc_info->cache_tbl_size;
 
-		while (ulp_sc_info->num_entries && (sce < sce_end)) {
+		if (unlikely(!sc_ops))
+			sc_ops = ulp_sc_info->sc_ops;
+
+		stat_cnt = 0;
+		while (stat_cnt < ulp_sc_info->num_entries && (sce < sce_end)) {
 			data = ulp_sc_info->read_data;
-
-			rc = tfc_mpc_batch_start(&batch_info);
-			if (rc) {
-				PMD_DRV_LOG_LINE(ERR,
-						 "MPC batch start failed rc:%d loop:%d",
-						 rc, loop);
-				break;
-			}
 
 			if (bnxt_ulp_cntxt_acquire_fdb_lock(ctxt))
 				break;
 
-			rc = pthread_mutex_lock(&ulp_sc_info->sc_lock);
-			if (rc) {
+			rc = tfc_mpc_batch_start(&batch_info);
+			if (unlikely(rc)) {
 				PMD_DRV_LOG_LINE(ERR,
-						 "Failed to get SC lock, terminating main loop rc:%d loop:%d",
-						 rc, loop);
-				goto terminate;
+						 "MPC batch start failed rc:%d", rc);
+				bnxt_ulp_cntxt_release_fdb_lock(ctxt);
+				break;
 			}
 
-			for (batch = 0; (batch < ULP_SC_BATCH_SIZE) && (sce < sce_end);) {
+			for (batch = 0; (batch < ULP_SC_BATCH_SIZE) &&
+			     (sce < sce_end);) {
 				if (!(sce->flags & ULP_SC_ENTRY_FLAG_VALID)) {
 					sce++;
 					continue;
 				}
-
+				stat_cnt++;
 				tfcp = bnxt_ulp_cntxt_tfcp_get(sce->ctxt);
-				if (tfcp == NULL) {
+				if (unlikely(!tfcp)) {
+					bnxt_ulp_cntxt_release_fdb_lock(ctxt);
 					bnxt_ulp_cntxt_entry_release();
 					goto terminate;
 				}
 
-
 				/* Store the entry pointer to use for counter update */
-				batch_info.em_hdl[batch_info.count] = (uint64_t)sce;
+				batch_info.em_hdl[batch_info.count] =
+					(uint64_t)sce;
 
 				rc = sc_ops->ulp_stats_cache_update(tfcp,
 								    sce->dir,
@@ -285,11 +247,10 @@ static uint32_t ulp_stats_cache_main_loop(void *arg)
 								    &words,
 								    &batch_info,
 								    sce->reset);
-				if (rc) {
+				if (unlikely(rc)) {
 					/* Abort this batch */
 					PMD_DRV_LOG_LINE(ERR,
-							 "loop:%d read_counter() failed:%d",
-							 loop, rc);
+							 "read_counter() failed:%d", rc);
 					break;
 				}
 
@@ -305,13 +266,10 @@ static uint32_t ulp_stats_cache_main_loop(void *arg)
 			batch_size = batch_info.count;
 			rc = tfc_mpc_batch_end(tfcp, &batch_info);
 
-			pthread_mutex_unlock(&ulp_sc_info->sc_lock);
 			bnxt_ulp_cntxt_release_fdb_lock(ctxt);
 
-			if (rc) {
-				PMD_DRV_LOG_LINE(ERR,
-						 "MPC batch end failed rc:%d loop:%d",
-						 rc, loop);
+			if (unlikely(rc)) {
+				PMD_DRV_LOG_LINE(ERR, "MPC batch end failed rc:%d", rc);
 				batch_info.enabled = false;
 				break;
 			}
@@ -322,8 +280,7 @@ static uint32_t ulp_stats_cache_main_loop(void *arg)
 			for (batch = 0; batch < batch_size; batch++) {
 				/* Check for error in completion */
 				if (batch_info.result[batch]) {
-					PMD_DRV_LOG_LINE(ERR,
-							 "batch:%d result:%d",
+					PMD_DRV_LOG_LINE(ERR, "batch:%d result:%d",
 							 batch, batch_info.result[batch]);
 				} else {
 					count = (struct ulp_sc_tfc_stats_cache_entry *)
@@ -334,28 +291,13 @@ static uint32_t ulp_stats_cache_main_loop(void *arg)
 				data += ULP_SC_PAGE_SIZE;
 			}
 		}
-
-		loop++;
-		stop = rte_get_timer_cycles();
-		cycles = stop - start;
-		if (cycles > (hz * ULP_SC_PERIOD_S)) {
-			PMD_DRV_LOG_LINE(ERR,
-					 "Stats collection time exceeded %dmS Cycles:%" PRIu64,
-					 ULP_SC_PERIOD_MS, cycles);
-			delay = ULP_SC_PERIOD_MS;
-		} else {
-			delay = ULP_SC_PERIOD_MS - (cycles / cpms);
-
-			if (delay > ULP_SC_PERIOD_MS) {
-				PMD_DRV_LOG_LINE(ERR,
-						 "Stats collection delay:%dmS exceedes %dmS",
-						 delay, ULP_SC_PERIOD_MS);
-				delay = ULP_SC_PERIOD_MS;
-			}
-		}
+		bnxt_ulp_cntxt_entry_release();
+		/* Sleep to give any other threads opportunity to access ULP */
+		rte_delay_us_sleep(ULP_SC_PERIOD_US);
 	}
 
  terminate:
+	PMD_DRV_LOG_LINE(DEBUG, "Terminating the stats cachce thread");
 	return 0;
 }
 
@@ -534,14 +476,13 @@ int ulp_sc_mgr_entry_alloc(struct bnxt_ulp_mapper_parms *parms,
 	if (!ulp_sc_info)
 		return -ENODEV;
 
-	pthread_mutex_lock(&ulp_sc_info->sc_lock);
-
 	sce = ulp_sc_info->stats_cache_tbl;
 	sce += parms->flow_id;
 
 	/* If entry is not free return an error */
 	if (sce->flags & ULP_SC_ENTRY_FLAG_VALID) {
-		pthread_mutex_unlock(&ulp_sc_info->sc_lock);
+		BNXT_DRV_DBG(ERR, "Entry is not free, invalid flow id %u\n",
+			     parms->flow_id);
 		return -EBUSY;
 	}
 
@@ -553,8 +494,6 @@ int ulp_sc_mgr_entry_alloc(struct bnxt_ulp_mapper_parms *parms,
 	sce->handle = counter_handle;
 	sce->dir = tbl->direction;
 	ulp_sc_info->num_entries++;
-	pthread_mutex_unlock(&ulp_sc_info->sc_lock);
-
 	return 0;
 }
 
@@ -568,20 +507,17 @@ void ulp_sc_mgr_entry_free(struct bnxt_ulp_context *ulp,
 	if (!ulp_sc_info)
 		return;
 
-	pthread_mutex_lock(&ulp_sc_info->sc_lock);
-
 	sce = ulp_sc_info->stats_cache_tbl;
 	sce += fid;
 
 	if (!(sce->flags & ULP_SC_ENTRY_FLAG_VALID)) {
-		pthread_mutex_unlock(&ulp_sc_info->sc_lock);
+		BNXT_DRV_DBG(ERR, "Entry already free, invalid flow id %u\n",
+			     fid);
 		return;
 	}
 
 	sce->flags = 0;
 	ulp_sc_info->num_entries--;
-
-	pthread_mutex_unlock(&ulp_sc_info->sc_lock);
 }
 
 /*
@@ -606,11 +542,7 @@ void ulp_sc_mgr_set_pc_idx(struct bnxt_ulp_context *ctxt,
 	if (!ulp_sc_info)
 		return;
 
-	pthread_mutex_lock(&ulp_sc_info->sc_lock);
-
 	sce = ulp_sc_info->stats_cache_tbl;
 	sce += flow_id;
 	sce->pc_idx = pc_idx & ULP_SC_PC_IDX_MASK;
-
-	pthread_mutex_unlock(&ulp_sc_info->sc_lock);
 }

drivers/net/bnxt/tf_ulp/ulp_sc_mgr.h
@@ -35,12 +35,12 @@ struct ulp_sc_tfc_stats_cache_entry {
 
 struct bnxt_ulp_sc_info {
 	struct ulp_sc_tfc_stats_cache_entry *stats_cache_tbl;
-	uint8_t                 *read_data;
-	uint32_t		flags;
-	uint32_t		num_entries;
-	pthread_mutex_t		sc_lock;
-	uint32_t		num_counters;
-	rte_thread_t            tid;
+	uint8_t		*read_data;
+	uint32_t	flags;
+	uint32_t	num_entries;
+	uint32_t	num_counters;
+	uint32_t	cache_tbl_size;
+	rte_thread_t	tid;
 	const struct bnxt_ulp_sc_core_ops *sc_ops;
 };
 