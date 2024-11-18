812bd13b7547d1590834935bc4cac81fc7d69d29
Tomasz Duszynski <tduszynski@marvell.com>
2024-11-18 08:37:04 +0100

    pmu: support reading ARM PMU events in runtime
    Add support for reading ARM PMU events in runtime.
    
    Signed-off-by: Tomasz Duszynski <tduszynski@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/pmu/rte_pmu.h
+++ lib/pmu/rte_pmu.h
@@ -37,6 +37,10 @@ extern "C" {
 #include <rte_compat.h>
 #include <rte_lcore.h>
 
+#if defined(RTE_ARCH_ARM64)
+#include "rte_pmu_pmc_arm64.h"
+#endif
+
 /** Maximum number of events in a group */
 #define RTE_MAX_NUM_GROUP_EVENTS 8
 
