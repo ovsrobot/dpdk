255e6daa7522110f121b86eaf505067f780b57ec
Andre Muezerie <andremue@linux.microsoft.com>
2024-12-04 13:41:04 -0800

    drivers/net: eliminate dependency on non-portable __SIZEOF_LONG__
    Macro __SIZEOF_LONG__ is not standardized and MSVC does not define it.
    Therefore the errors below are seen with MSVC:
    
    ../lib/mldev/mldev_utils_scalar.c(465): error C2065:
        '__SIZEOF_LONG__': undeclared identifier
    ../lib/mldev/mldev_utils_scalar.c(478): error C2051:
        case expression not constant
    
    ../lib/mldev/mldev_utils_scalar_bfloat16.c(33): error C2065:
        '__SIZEOF_LONG__': undeclared identifier
    ../lib/mldev/mldev_utils_scalar_bfloat16.c(49): error C2051:
        case expression not constant
    
    Turns out that the places where __SIZEOF_LONG__ is currently
    being used can equally well use sizeof(long) instead.
    
    Signed-off-by: Andre Muezerie <andremue@linux.microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/hns3/hns3_ethdev.h
+++ drivers/net/hns3/hns3_ethdev.h
@@ -952,7 +952,7 @@ static inline struct hns3_vf *HNS3_DEV_HW_TO_VF(struct hns3_hw *hw)
 
 #define BIT_ULL(x) (1ULL << (x))
 
-#define BITS_PER_LONG	(__SIZEOF_LONG__ * 8)
+#define BITS_PER_LONG	(sizeof(long) * 8)
 #define GENMASK(h, l) \
 	(((~0UL) << (l)) & (~0UL >> (BITS_PER_LONG - 1 - (h))))
 
