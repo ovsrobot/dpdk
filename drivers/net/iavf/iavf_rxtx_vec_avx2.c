fb93bbfabe91350b530730b4c3237a42abf80c2f
Bruce Richardson <bruce.richardson@intel.com>
2024-12-11 17:33:09 +0000

    net/_common_intel: add Tx mbuf ring replenish fn
    Move the short function used to place mbufs on the SW Tx ring to common
    code to avoid duplication.
    
    Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/iavf/iavf_rxtx_vec_avx2.c
+++ drivers/net/iavf/iavf_rxtx_vec_avx2.c
@@ -1757,7 +1757,7 @@ iavf_xmit_fixed_burst_vec_avx2(void *tx_queue, struct rte_mbuf **tx_pkts,
 
 	n = (uint16_t)(txq->nb_tx_desc - tx_id);
 	if (nb_commit >= n) {
-		tx_backlog_entry(txep, tx_pkts, n);
+		ci_tx_backlog_entry(txep, tx_pkts, n);
 
 		iavf_vtx(txdp, tx_pkts, n - 1, flags, offload);
 		tx_pkts += (n - 1);
@@ -1775,7 +1775,7 @@ iavf_xmit_fixed_burst_vec_avx2(void *tx_queue, struct rte_mbuf **tx_pkts,
 		txep = &txq->sw_ring[tx_id];
 	}
 
-	tx_backlog_entry(txep, tx_pkts, nb_commit);
+	ci_tx_backlog_entry(txep, tx_pkts, nb_commit);
 
 	iavf_vtx(txdp, tx_pkts, nb_commit, flags, offload);
 
