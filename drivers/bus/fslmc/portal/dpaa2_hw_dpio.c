c8a75c8411c24fe3780933299a8e4c5aa5977f84
Stephen Hemminger <stephen@networkplumber.org>
2024-12-18 09:05:30 -0800

    bus/fslmc: fix use after rte_free
    The cleanup loop would derefence the dpio_dev after freeing.
    Use TAILQ_FOREACH_SAFE to fix that.
    Found by building with sanitizer undefined flag.
    
    Fixes: e55d0494ab98 ("bus/fslmc: support secondary process")
    Cc: shreyansh.jain@nxp.com
    Cc: stable@dpdk.org
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/bus/fslmc/portal/dpaa2_hw_dpio.c
+++ drivers/bus/fslmc/portal/dpaa2_hw_dpio.c
@@ -403,6 +403,7 @@ dpaa2_create_dpio_device(int vdev_fd,
 	struct rte_dpaa2_device *obj)
 {
 	struct dpaa2_dpio_dev *dpio_dev = NULL;
+	struct dpaa2_dpio_dev *dpio_tmp;
 	struct vfio_region_info reg_info = { .argsz = sizeof(reg_info)};
 	struct qbman_swp_desc p_des;
 	struct dpio_attr attr;
@@ -588,7 +589,7 @@ err:
 	rte_free(dpio_dev);
 
 	/* For each element in the list, cleanup */
-	TAILQ_FOREACH(dpio_dev, &dpio_dev_list, next) {
+	RTE_TAILQ_FOREACH_SAFE(dpio_dev, &dpio_dev_list, next, dpio_tmp) {
 		if (dpio_dev->dpio) {
 			dpio_disable(dpio_dev->dpio, CMD_PRI_LOW,
 				dpio_dev->token);
