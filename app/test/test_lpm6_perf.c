28d76d8044b2b2bf9aa4f86e1a70d5902bb24c10
Andre Muezerie <andremue@linux.microsoft.com>
2024-12-18 07:21:26 -0800

    app/test: fix stack overflow in lpm6_perf_autotest
    Test lpm6_perf_autotest was hitting a stack overflow on Windows
    with both MSVC and Clang.
    
    The fix is to move some of the data from the stack to the heap.
    
    Signed-off-by: Andre Muezerie <andremue@linux.microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- app/test/test_lpm6_perf.c
+++ app/test/test_lpm6_perf.c
@@ -10,6 +10,7 @@
 
 #include <rte_cycles.h>
 #include <rte_random.h>
+#include <rte_malloc.h>
 #include <rte_memory.h>
 #include <rte_lpm6.h>
 
@@ -117,8 +118,13 @@ test_lpm6_perf(void)
 	total_time = 0;
 	count = 0;
 
-	struct rte_ipv6_addr ip_batch[NUM_IPS_ENTRIES];
-	int32_t next_hops[NUM_IPS_ENTRIES];
+	struct rte_ipv6_addr *ip_batch = rte_calloc("ip_batch",
+			NUM_IPS_ENTRIES, sizeof(struct rte_ipv6_addr), 0);
+	TEST_LPM_ASSERT(ip_batch != NULL);
+
+	int32_t *next_hops = rte_calloc("next_hops",
+			NUM_IPS_ENTRIES, sizeof(int32_t), 0);
+	TEST_LPM_ASSERT(next_hops != NULL);
 
 	for (i = 0; i < NUM_IPS_ENTRIES; i++)
 		ip_batch[i] = large_ips_table[i].ip;
@@ -152,6 +158,9 @@ test_lpm6_perf(void)
 
 	printf("Average LPM Delete: %g cycles\n",
 			(double)total_time / NUM_ROUTE_ENTRIES);
+
+	rte_free(next_hops);
+	rte_free(ip_batch);
 
 	rte_lpm6_delete_all(lpm);
 	rte_lpm6_free(lpm);
