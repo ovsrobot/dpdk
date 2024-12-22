ovsrobot/dpdk

commit 06c047b680615678bb8773faaec53156ade94770

remove unnecessary null checks
Functions like free, rte_free, and rte_mempool_free
already handle NULL pointer so the checks here are not necessary.

Remove redundant NULL pointer checks before free functions
found by nullfree.cocci

Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>

drivers/net/mlx4/mlx4_mr.c
@@ -471,8 +471,7 @@ mr_free(struct mlx4_mr *mr)
 	DEBUG("freeing MR(%p):", (void *)mr);
 	if (mr->ibv_mr != NULL)
 		claim_zero(mlx4_glue->dereg_mr(mr->ibv_mr));
-	if (mr->ms_bmp != NULL)
-		rte_bitmap_free(mr->ms_bmp);
+	rte_bitmap_free(mr->ms_bmp);
 	rte_free(mr);
 }
 

commit 1a2a7bee15dda4ae1a1cdbc46afd42b43fd5cada

net/mlx4: remove unused rte_bitmap_free()
Depends on 17dd8149d6 ("devtools/cocci,lib/eal: remove unused
rte_bitmap_free()").

Fixes: 06c047b680 ("remove unnecessary null checks")
Signed-off-by: Ariel Otilibili <otilibil@eurecom.fr>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/mlx4/mlx4_mr.c
@@ -471,7 +471,6 @@ mr_free(struct mlx4_mr *mr)
 	DEBUG("freeing MR(%p):", (void *)mr);
 	if (mr->ibv_mr != NULL)
 		claim_zero(mlx4_glue->dereg_mr(mr->ibv_mr));
-	rte_bitmap_free(mr->ms_bmp);
 	rte_free(mr);
 }
 