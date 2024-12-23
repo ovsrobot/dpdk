4c71e9f6851bb9bc0f4f4a208583353a9815a618
Dean Marx <dmarx@iol.unh.edu>
2024-12-23 13:57:39 -0500

    dts: add EAL confidence check suite
    Add a test suite to replace hello_world which simply
    starts and stops a testpmd session. The user can use
    this as a confidence check to verify their configuration.
    
    Signed-off-by: Dean Marx <dmarx@iol.unh.edu>
    Reviewed-by: Paul Szczepanek <paul.szczepanek@arm.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ dts/tests/TestSuite_eal.py
@@ -0,0 +1,27 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2024 University of New Hampshire
+
+"""DPDK EAL confidence check suite.
+
+Starts and stops a testpmd session to verify EAL parameters
+are properly configured.
+"""
+
+from framework.remote_session.testpmd_shell import TestPmdShell
+from framework.test_suite import TestSuite, func_test
+
+
+class TestEal(TestSuite):
+    """EAL test suite. One test case, which starts and stops a testpmd session."""
+
+    @func_test
+    def test_verify_eal(self) -> None:
+        """EAL confidence test.
+
+        Steps:
+            Start testpmd session and check status.
+        Verify:
+            The testpmd session is alive after starting.
+        """
+        with TestPmdShell(node=self.sut_node) as testpmd:
+            testpmd.start()
