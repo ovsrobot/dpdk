b9a7dc8a02fcf28c9ebdb356f3e51f78f6b85417
Ariel Otilibili <otilibil@eurecom.fr>
2025-01-12 20:30:38 +0100

    raw/ifpga: remove check around pthread_mutex_init()
    > pthread_mutex_init always returns 0. The other mutex functions
    > return 0 on success and a non-zero error code on error.
    
    Link: https://man7.org/linux/man-pages/man3/pthread_mutex_lock.3.html
    Bugzilla ID: 1586
    Signed-off-by: Ariel Otilibili <otilibil@eurecom.fr>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/raw/ifpga/base/opae_spi_transaction.c
+++ drivers/raw/ifpga/base/opae_spi_transaction.c
@@ -494,7 +494,6 @@ struct spi_transaction_dev *spi_transaction_init(struct altera_spi_device *dev,
 		int chipselect)
 {
 	struct spi_transaction_dev *spi_tran_dev;
-	int ret;
 
 	spi_tran_dev = opae_malloc(sizeof(struct spi_transaction_dev));
 	if (!spi_tran_dev)
@@ -507,11 +506,7 @@ struct spi_transaction_dev *spi_transaction_init(struct altera_spi_device *dev,
 	if (!spi_tran_dev->buffer)
 		goto err;
 
-	ret = pthread_mutex_init(&spi_tran_dev->lock, NULL);
-	if (ret) {
-		dev_err(spi_tran_dev, "fail to init mutex lock\n");
-		goto err;
-	}
+	pthread_mutex_init(&spi_tran_dev->lock, NULL);
 	if (dev->mutex) {
 		dev_info(NULL, "use multi-process mutex in spi\n");
 		spi_tran_dev->mutex = dev->mutex;
@@ -523,8 +518,8 @@ struct spi_transaction_dev *spi_transaction_init(struct altera_spi_device *dev,
 	return spi_tran_dev;
 
 err:
-	opae_free(spi_tran_dev);
-	return NULL;
+       opae_free(spi_tran_dev);
+       return NULL;
 }
 
 void spi_transaction_remove(struct spi_transaction_dev *dev)
