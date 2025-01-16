ca18ba30fcfc6a661b90376c27281ab42a5f7f6a
Andre Muezerie <andremue@linux.microsoft.com>
2025-01-15 17:55:11 -0800

    drivers/net: add diagnostics macros to make code portable
    It was a common pattern to have "GCC diagnostic ignored" pragmas
    sprinkled over the code and only activate these pragmas for certain
    compilers (gcc and clang). Clang supports GCC's pragma for
    compatibility with existing source code, so #pragma GCC diagnostic
    and #pragma clang diagnostic are synonyms for Clang
    (https://clang.llvm.org/docs/UsersManual.html).
    
    Now that effort is being made to make the code compatible with MSVC
    these expressions would become more complex. It makes sense to hide
    this complexity behind macros. This makes maintenance easier as these
    macros are defined in a single place. As a plus the code becomes
    more readable as well.
    
    Signed-off-by: Andre Muezerie <andremue@linux.microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/iavf/iavf_rxtx_vec_sse.c
+++ drivers/net/iavf/iavf_rxtx_vec_sse.c
@@ -12,10 +12,6 @@
 
 #include <rte_vect.h>
 
-#ifndef __INTEL_COMPILER
-#pragma GCC diagnostic ignored "-Wcast-qual"
-#endif
-
 static inline void
 iavf_rxq_rearm(struct iavf_rx_queue *rxq)
 {
@@ -38,7 +34,7 @@ iavf_rxq_rearm(struct iavf_rx_queue *rxq)
 			dma_addr0 = _mm_setzero_si128();
 			for (i = 0; i < IAVF_VPMD_DESCS_PER_LOOP; i++) {
 				rxp[i] = &rxq->fake_mbuf;
-				_mm_store_si128((__m128i *)&rxdp[i].read,
+				_mm_store_si128(RTE_PTR_DROP_QUALIFIERS(&rxdp[i].read),
 						dma_addr0);
 			}
 		}
@@ -69,8 +65,10 @@ iavf_rxq_rearm(struct iavf_rx_queue *rxq)
 		dma_addr1 = _mm_add_epi64(dma_addr1, hdr_room);
 
 		/* flush desc with pa dma_addr */
-		_mm_store_si128((__m128i *)&rxdp++->read, dma_addr0);
-		_mm_store_si128((__m128i *)&rxdp++->read, dma_addr1);
+		_mm_store_si128(RTE_PTR_DROP_QUALIFIERS
+				(&rxdp++->read), dma_addr0);
+		_mm_store_si128(RTE_PTR_DROP_QUALIFIERS
+				(&rxdp++->read), dma_addr1);
 	}
 
 	rxq->rxrearm_start += rxq->rx_free_thresh;
@@ -578,7 +576,8 @@ _recv_raw_pkts_vec(struct iavf_rx_queue *rxq, struct rte_mbuf **rx_pkts,
 		mbp1 = _mm_loadu_si128((__m128i *)&sw_ring[pos]);
 		/* Read desc statuses backwards to avoid race condition */
 		/* A.1 load desc[3] */
-		descs[3] = _mm_loadu_si128((__m128i *)(rxdp + 3));
+		descs[3] = _mm_loadu_si128(RTE_PTR_DROP_QUALIFIERS
+			(rxdp + 3));
 		rte_compiler_barrier();
 
 		/* B.2 copy 2 64 bit or 4 32 bit mbuf point into rx_pkts */
@@ -590,11 +589,14 @@ _recv_raw_pkts_vec(struct iavf_rx_queue *rxq, struct rte_mbuf **rx_pkts,
 #endif
 
 		/* A.1 load desc[2-0] */
-		descs[2] = _mm_loadu_si128((__m128i *)(rxdp + 2));
+		descs[2] = _mm_loadu_si128(RTE_PTR_DROP_QUALIFIERS
+			(rxdp + 2));
 		rte_compiler_barrier();
-		descs[1] = _mm_loadu_si128((__m128i *)(rxdp + 1));
+		descs[1] = _mm_loadu_si128(RTE_PTR_DROP_QUALIFIERS
+			(rxdp + 1));
 		rte_compiler_barrier();
-		descs[0] = _mm_loadu_si128((__m128i *)(rxdp));
+		descs[0] = _mm_loadu_si128(RTE_PTR_DROP_QUALIFIERS
+			(rxdp));
 
 #if defined(RTE_ARCH_X86_64)
 		/* B.2 copy 2 mbuf point into rx_pkts  */
@@ -783,7 +785,7 @@ _recv_raw_pkts_vec_flex_rxd(struct iavf_rx_queue *rxq,
 	/* Just the act of getting into the function from the application is
 	 * going to cost about 7 cycles
 	 */
-	rxdp = (union iavf_rx_flex_desc *)rxq->rx_ring + rxq->rx_tail;
+	rxdp = (volatile union iavf_rx_flex_desc *)rxq->rx_ring + rxq->rx_tail;
 
 	rte_prefetch0(rxdp);
 
@@ -864,7 +866,7 @@ _recv_raw_pkts_vec_flex_rxd(struct iavf_rx_queue *rxq,
 		mbp1 = _mm_loadu_si128((__m128i *)&sw_ring[pos]);
 		/* Read desc statuses backwards to avoid race condition */
 		/* A.1 load desc[3] */
-		descs[3] = _mm_loadu_si128((__m128i *)(rxdp + 3));
+		descs[3] = _mm_loadu_si128(RTE_PTR_DROP_QUALIFIERS(rxdp + 3));
 		rte_compiler_barrier();
 
 		/* B.2 copy 2 64 bit or 4 32 bit mbuf point into rx_pkts */
@@ -876,11 +878,11 @@ _recv_raw_pkts_vec_flex_rxd(struct iavf_rx_queue *rxq,
 #endif
 
 		/* A.1 load desc[2-0] */
-		descs[2] = _mm_loadu_si128((__m128i *)(rxdp + 2));
+		descs[2] = _mm_loadu_si128(RTE_PTR_DROP_QUALIFIERS(rxdp + 2));
 		rte_compiler_barrier();
-		descs[1] = _mm_loadu_si128((__m128i *)(rxdp + 1));
+		descs[1] = _mm_loadu_si128(RTE_PTR_DROP_QUALIFIERS(rxdp + 1));
 		rte_compiler_barrier();
-		descs[0] = _mm_loadu_si128((__m128i *)(rxdp));
+		descs[0] = _mm_loadu_si128(RTE_PTR_DROP_QUALIFIERS(rxdp));
 
 #if defined(RTE_ARCH_X86_64)
 		/* B.2 copy 2 mbuf point into rx_pkts  */
@@ -927,17 +929,17 @@ _recv_raw_pkts_vec_flex_rxd(struct iavf_rx_queue *rxq,
 			offloads & RTE_ETH_RX_OFFLOAD_TIMESTAMP ||
 			rxq->rx_flags & IAVF_RX_FLAGS_VLAN_TAG_LOC_L2TAG2_2) {
 			/* load bottom half of every 32B desc */
-			descs_bh[3] = _mm_load_si128
-					((void *)(&rxdp[3].wb.status_error1));
+			descs_bh[3] = _mm_load_si128(RTE_PTR_DROP_QUALIFIERS
+				(&rxdp[3].wb.status_error1));
 			rte_compiler_barrier();
-			descs_bh[2] = _mm_load_si128
-					((void *)(&rxdp[2].wb.status_error1));
+			descs_bh[2] = _mm_load_si128(RTE_PTR_DROP_QUALIFIERS
+				(&rxdp[2].wb.status_error1));
 			rte_compiler_barrier();
-			descs_bh[1] = _mm_load_si128
-					((void *)(&rxdp[1].wb.status_error1));
+			descs_bh[1] = _mm_load_si128(RTE_PTR_DROP_QUALIFIERS
+				(&rxdp[1].wb.status_error1));
 			rte_compiler_barrier();
-			descs_bh[0] = _mm_load_si128
-					((void *)(&rxdp[0].wb.status_error1));
+			descs_bh[0] = _mm_load_si128(RTE_PTR_DROP_QUALIFIERS
+				(&rxdp[0].wb.status_error1));
 		}
 
 		if (offloads & RTE_ETH_RX_OFFLOAD_RSS_HASH) {
@@ -1349,7 +1351,7 @@ vtx1(volatile struct iavf_tx_desc *txdp, struct rte_mbuf *pkt, uint64_t flags)
 
 	__m128i descriptor = _mm_set_epi64x(high_qw,
 					    pkt->buf_iova + pkt->data_off);
-	_mm_store_si128((__m128i *)txdp, descriptor);
+	_mm_store_si128(RTE_PTR_DROP_QUALIFIERS(txdp), descriptor);
 }
 
 static inline void
