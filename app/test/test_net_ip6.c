ovsrobot/dpdk

commit 3d6d85f58c1cb88e3906dd3318f232a58be2e10e

net: add utilities for well known IPv6 address types
Add more utilities to work with IPv6 addresses. These functions will be
required in order to help building IPv6 routing applications.

Signed-off-by: Robin Jarry <rjarry@redhat.com>
Acked-by: Stephen Hemminger <stephen@networkplumber.org>

lib/net/rte_ip6.h
@@ -30,6 +30,7 @@
 
 #include <rte_byteorder.h>
 #include <rte_cksum.h>
+#include <rte_ether.h>
 #include <rte_mbuf.h>
 
 #ifdef __cplusplus
@@ -247,6 +248,214 @@ rte_ipv6_addr_is_unspec(const struct rte_ipv6_addr *ip)
 
 /** Loopback IPv6 address as defined in RFC 4291, section 2.5.3. */
 #define RTE_IPV6_ADDR_LOOPBACK RTE_IPV6(0, 0, 0, 0, 0, 0, 0, 1)
+
+/**
+ * Check if an IPv6 address is the loopback address as defined in RFC 4291,
+ * section 2.5.3.
+ *
+ * @param ip
+ *   The address to check.
+ * @return
+ *   @c true if the address is the loopback address (all zeroes except the last bit).
+ */
+static inline bool
+rte_ipv6_addr_is_loopback(const struct rte_ipv6_addr *ip)
+{
+	struct rte_ipv6_addr loopback = RTE_IPV6_ADDR_LOOPBACK;
+	return rte_ipv6_addr_eq(ip, &loopback);
+}
+
+/**
+ * Check if an IPv6 address is link-local as defined in RFC 4291, section 2.5.6.
+ *
+ * @param ip
+ *   The address to check.
+ * @return
+ *   @c true if the address is a link-local address.
+ */
+static inline bool
+rte_ipv6_addr_is_linklocal(const struct rte_ipv6_addr *ip)
+{
+	return ip->a[0] == 0xfe && (ip->a[1] & 0xc0) == 0x80;
+}
+
+/**
+ * Check if an IPv6 address is site-local as defined in RFC 4291, section 2.5.7.
+ *
+ * @param ip
+ *   The address to check.
+ * @return
+ *   @c true if the address is a site-local address.
+ */
+static inline bool
+rte_ipv6_addr_is_sitelocal(const struct rte_ipv6_addr *ip)
+{
+	return ip->a[0] == 0xfe && (ip->a[1] & 0xc0) == 0xc0;
+}
+
+/**
+ * Check if an IPv6 address is an IPv4-compatible address as defined in RFC 4291,
+ * section 2.5.5.1.
+ *
+ * @param ip
+ *   The address to check.
+ * @return
+ *   @c true if the address is an IPv4-compatible address.
+ */
+static inline bool
+rte_ipv6_addr_is_v4compat(const struct rte_ipv6_addr *ip)
+{
+	const struct rte_ipv6_addr unspec = RTE_IPV6_ADDR_UNSPEC;
+	return rte_ipv6_addr_eq_prefix(ip, &unspec, 32) && !rte_ipv6_addr_is_loopback(ip);
+}
+
+#define RTE_IPV6_ADDR_PREFIX_V4MAPPED RTE_IPV6(0, 0, 0, 0, 0, 0xffff, 0, 0)
+
+/**
+ * Check if an IPv6 address is an IPv4-mapped address as defined in RFC 4291,
+ * section 2.5.5.2.
+ *
+ * @param ip
+ *   The address to check.
+ * @return
+ *   @c true if the address is an IPv4-mapped address.
+ */
+static inline bool
+rte_ipv6_addr_is_v4mapped(const struct rte_ipv6_addr *ip)
+{
+	const struct rte_ipv6_addr prefix = RTE_IPV6_ADDR_PREFIX_V4MAPPED;
+	return rte_ipv6_addr_eq_prefix(ip, &prefix, 32);
+}
+
+/**
+ * Check if an IPv6 address is multicast as defined in RFC 4291, section 2.7.
+ *
+ * @param ip
+ *   The address to check.
+ * @return
+ *   @c true if the address is multicast.
+ */
+static inline bool
+rte_ipv6_addr_is_mcast(const struct rte_ipv6_addr *ip)
+{
+	return ip->a[0] == 0xff;
+}
+
+/**
+ * IPv6 multicast scope values as defined in RFC 4291, section 2.7.
+ */
+enum rte_ipv6_mc_scope {
+	/** Invalid multicast scope. */
+	RTE_IPV6_MC_SCOPE_NONE = 0x00,
+	/** Interface-local multicast scope. */
+	RTE_IPV6_MC_SCOPE_IFACELOCAL = 0x01,
+	/** Link-local multicast scope. */
+	RTE_IPV6_MC_SCOPE_LINKLOCAL = 0x02,
+	/** Site-local multicast scope. */
+	RTE_IPV6_MC_SCOPE_SITELOCAL = 0x05,
+	/** Organizational-local multicast scope. */
+	RTE_IPV6_MC_SCOPE_ORGLOCAL = 0x08,
+	/** Global multicast scope. */
+	RTE_IPV6_MC_SCOPE_GLOBAL = 0x0e,
+} __rte_packed;
+
+/**
+ * Extract the IPv6 multicast scope value as defined in RFC 4291, section 2.7.
+ *
+ * @param ip
+ *   The address from which to get the multicast scope.
+ * @return
+ *   The multicast scope of the address, or #RTE_IPV6_MC_SCOPE_NONE if the
+ *   address is not multicast.
+ */
+static inline enum rte_ipv6_mc_scope
+rte_ipv6_mc_scope(const struct rte_ipv6_addr *ip)
+{
+	if (!rte_ipv6_addr_is_mcast(ip))
+		return RTE_IPV6_MC_SCOPE_NONE;
+	return (enum rte_ipv6_mc_scope)(ip->a[1] & 0x0f);
+}
+
+/** @name Well known multicast addresses */
+/**@{*/
+/** Interface-local all-nodes multicast address as defined in RFC 4291, section 2.7.1. */
+#define RTE_IPV6_ADDR_ALLNODES_IFACE_LOCAL RTE_IPV6(0xff01, 0, 0, 0, 0, 0, 0, 1)
+/** Link-local all-nodes multicast address as defined in RFC 4291, section 2.7.1. */
+#define RTE_IPV6_ADDR_ALLNODES_LINK_LOCAL RTE_IPV6(0xff02, 0, 0, 0, 0, 0, 0, 1)
+/** Interface-local all-routers multicast address as defined in RFC 4291, section 2.7.1. */
+#define RTE_IPV6_ADDR_ALLROUTERS_IFACE_LOCAL RTE_IPV6(0xff01, 0, 0, 0, 0, 0, 0, 2)
+/** Link-local all-routers multicast address as defined in RFC 4291, section 2.7.1. */
+#define RTE_IPV6_ADDR_ALLROUTERS_LINK_LOCAL RTE_IPV6(0xff02, 0, 0, 0, 0, 0, 0, 2)
+/** Site-local all-routers multicast address as defined in RFC 4291, section 2.7.1. */
+#define RTE_IPV6_ADDR_ALLROUTERS_SITE_LOCAL RTE_IPV6(0xff05, 0, 0, 0, 0, 0, 0, 2)
+/**@}*/
+
+/*
+ * Generate a link-local IPv6 address from an Ethernet address as specified in
+ * RFC 2464, section 5.
+ *
+ * @param[out] ip
+ *   The link-local IPv6 address to generate.
+ * @param[in] mac
+ *   An Ethernet address.
+ */
+static inline void
+rte_ipv6_llocal_from_ethernet(struct rte_ipv6_addr *ip, const struct rte_ether_addr *mac)
+{
+	ip->a[0] = 0xfe;
+	ip->a[1] = 0x80;
+	memset(&ip->a[2], 0, 6);
+	ip->a[8] = mac->addr_bytes[0];
+	ip->a[9] = mac->addr_bytes[1];
+	ip->a[10] = mac->addr_bytes[2];
+	ip->a[11] = 0xff;
+	ip->a[12] = 0xfe;
+	ip->a[13] = mac->addr_bytes[3];
+	ip->a[14] = mac->addr_bytes[4];
+	ip->a[15] = mac->addr_bytes[5];
+}
+
+/**
+ * Convert a unicast or anycast IPv6 address to a solicited-node multicast
+ * address as defined in RFC 4291, section 2.7.1.
+ *
+ * @param[out] sol
+ *   The IPv6 solicited-node multicast address to generate.
+ * @param[in] ip
+ *   A unicast or anycast address.
+ */
+static inline void
+rte_ipv6_solnode_from_addr(struct rte_ipv6_addr *sol, const struct rte_ipv6_addr *ip)
+{
+	sol->a[0] = 0xff;
+	sol->a[1] = 0x02;
+	memset(&sol->a[2], 0, 9);
+	sol->a[11] = 0x01;
+	sol->a[12] = 0xff;
+	sol->a[13] = ip->a[13];
+	sol->a[14] = ip->a[14];
+	sol->a[15] = ip->a[15];
+}
+
+/**
+ * Generate a multicast Ethernet address from a multicast IPv6 address as defined
+ * in RFC 2464, section 7.
+ *
+ * @param[out] mac
+ *   The multicast Ethernet address to generate.
+ * @param[in] ip
+ *   A multicast IPv6 address.
+ */
+static inline void
+rte_ether_mcast_from_ipv6(struct rte_ether_addr *mac, const struct rte_ipv6_addr *ip)
+{
+	mac->addr_bytes[0] = 0x33;
+	mac->addr_bytes[1] = 0x33;
+	mac->addr_bytes[2] = ip->a[12];
+	mac->addr_bytes[3] = ip->a[13];
+	mac->addr_bytes[4] = ip->a[14];
+	mac->addr_bytes[5] = ip->a[15];
+}
 
 /**
  * IPv6 Header

app/test/test_net_ip6.c
@@ -81,20 +81,85 @@ static int
 test_ipv6_addr_kind(void)
 {
 	TEST_ASSERT(rte_ipv6_addr_is_unspec(&zero_addr), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_linklocal(&zero_addr), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_loopback(&zero_addr), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_mcast(&zero_addr), "");
 
 	const struct rte_ipv6_addr ucast =
 		RTE_IPV6(0x2a01, 0xcb00, 0x0254, 0x3300, 0x6239, 0xe1f4, 0x7a0b, 0x2371);
 	TEST_ASSERT(!rte_ipv6_addr_is_unspec(&ucast), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_linklocal(&ucast), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_loopback(&ucast), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_mcast(&ucast), "");
 
 	const struct rte_ipv6_addr mcast = RTE_IPV6(0xff01, 0, 0, 0, 0, 0, 0, 1);
 	TEST_ASSERT(!rte_ipv6_addr_is_unspec(&mcast), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_linklocal(&mcast), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_loopback(&mcast), "");
+	TEST_ASSERT(rte_ipv6_addr_is_mcast(&mcast), "");
 
 	const struct rte_ipv6_addr lo = RTE_IPV6_ADDR_LOOPBACK;
 	TEST_ASSERT(!rte_ipv6_addr_is_unspec(&lo), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_linklocal(&lo), "");
+	TEST_ASSERT(rte_ipv6_addr_is_loopback(&lo), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_mcast(&lo), "");
 
 	const struct rte_ipv6_addr local =
 		RTE_IPV6(0xfe80, 0, 0, 0, 0x5a84, 0xc52c, 0x6aef, 0x4639);
 	TEST_ASSERT(!rte_ipv6_addr_is_unspec(&local), "");
+	TEST_ASSERT(rte_ipv6_addr_is_linklocal(&local), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_loopback(&local), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_mcast(&local), "");
+
+	return TEST_SUCCESS;
+}
+
+static int
+test_ipv6_llocal_from_ethernet(void)
+{
+	const struct rte_ether_addr local_mac = {{0x04, 0x7b, 0xcb, 0x5c, 0x08, 0x44}};
+	const struct rte_ipv6_addr local_ip =
+		RTE_IPV6(0xfe80, 0, 0, 0, 0x047b, 0xcbff, 0xfe5c, 0x0844);
+	struct rte_ipv6_addr ip;
+
+	rte_ipv6_llocal_from_ethernet(&ip, &local_mac);
+	TEST_ASSERT(rte_ipv6_addr_eq(&ip, &local_ip), "");
+
+	return TEST_SUCCESS;
+}
+
+static int
+test_ipv6_solnode_from_addr(void)
+{
+	struct rte_ipv6_addr sol;
+
+	const struct rte_ipv6_addr llocal =
+		RTE_IPV6(0xfe80, 0, 0, 0, 0x047b, 0xcbff, 0xfe5c, 0x0844);
+	const struct rte_ipv6_addr llocal_sol =
+		RTE_IPV6(0xff02, 0, 0, 0, 0, 0x0001, 0xff5c, 0x0844);
+	rte_ipv6_solnode_from_addr(&sol, &llocal);
+	TEST_ASSERT(rte_ipv6_addr_eq(&sol, &llocal_sol), "");
+
+	const struct rte_ipv6_addr ucast =
+		RTE_IPV6(0x2a01, 0xcb00, 0x0254, 0x3300, 0x1b9f, 0x8071, 0x67cd, 0xbf20);
+	const struct rte_ipv6_addr ucast_sol =
+		RTE_IPV6(0xff02, 0, 0, 0, 0, 0x0001, 0xffcd, 0xbf20);
+	rte_ipv6_solnode_from_addr(&sol, &ucast);
+	TEST_ASSERT(rte_ipv6_addr_eq(&sol, &ucast_sol), "");
+
+	return TEST_SUCCESS;
+}
+
+static int
+test_ether_mcast_from_ipv6(void)
+{
+	const struct rte_ether_addr mcast_mac = {{0x33, 0x33, 0xd3, 0x00, 0x02, 0x01}};
+	const struct rte_ipv6_addr mcast_ip =
+		RTE_IPV6(0xff02, 0, 0, 0x0201, 0, 0, 0xd300, 0x0201);
+	struct rte_ether_addr mac;
+
+	rte_ether_mcast_from_ipv6(&mac, &mcast_ip);
+	TEST_ASSERT(rte_is_same_ether_addr(&mac, &mcast_mac), "");
 
 	return TEST_SUCCESS;
 }
@@ -105,6 +170,9 @@ test_net_ipv6(void)
 	TEST_ASSERT_SUCCESS(test_ipv6_addr_mask(), "");
 	TEST_ASSERT_SUCCESS(test_ipv6_addr_eq_prefix(), "");
 	TEST_ASSERT_SUCCESS(test_ipv6_addr_kind(), "");
+	TEST_ASSERT_SUCCESS(test_ipv6_llocal_from_ethernet(), "");
+	TEST_ASSERT_SUCCESS(test_ipv6_solnode_from_addr(), "");
+	TEST_ASSERT_SUCCESS(test_ether_mcast_from_ipv6(), "");
 	return TEST_SUCCESS;
 }
 

commit 1e587924598d88772d70ade025c78a9d1bf56ad7

net: fix IPv4 compat and mapped checks
The RFC states that the first 96 bits should be compared, not the first
32 bits.

Add unit tests to ensure this works.

Cc: stable@dpdk.org
Fixes: 3d6d85f58c1c ("net: add utilities for well known IPv6 address types")
Reported-by: Junxiao Shi <sunnylandh@gmail.com>
Closes: https://bugs.dpdk.org/show_bug.cgi?id=1664
Signed-off-by: Robin Jarry <rjarry@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/net/rte_ip6.h
@@ -307,7 +307,7 @@ static inline bool
 rte_ipv6_addr_is_v4compat(const struct rte_ipv6_addr *ip)
 {
 	const struct rte_ipv6_addr unspec = RTE_IPV6_ADDR_UNSPEC;
-	return rte_ipv6_addr_eq_prefix(ip, &unspec, 32) && !rte_ipv6_addr_is_loopback(ip);
+	return rte_ipv6_addr_eq_prefix(ip, &unspec, 96) && !rte_ipv6_addr_is_loopback(ip);
 }
 
 #define RTE_IPV6_ADDR_PREFIX_V4MAPPED RTE_IPV6(0, 0, 0, 0, 0, 0xffff, 0, 0)
@@ -325,7 +325,7 @@ static inline bool
 rte_ipv6_addr_is_v4mapped(const struct rte_ipv6_addr *ip)
 {
 	const struct rte_ipv6_addr prefix = RTE_IPV6_ADDR_PREFIX_V4MAPPED;
-	return rte_ipv6_addr_eq_prefix(ip, &prefix, 32);
+	return rte_ipv6_addr_eq_prefix(ip, &prefix, 96);
 }
 
 /**

app/test/test_net_ip6.c
@@ -99,6 +99,8 @@ test_ipv6_addr_kind(void)
 	TEST_ASSERT(!rte_ipv6_addr_is_linklocal(&zero_addr), "");
 	TEST_ASSERT(!rte_ipv6_addr_is_loopback(&zero_addr), "");
 	TEST_ASSERT(!rte_ipv6_addr_is_mcast(&zero_addr), "");
+	TEST_ASSERT(rte_ipv6_addr_is_v4compat(&zero_addr), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_v4mapped(&zero_addr), "");
 
 	const struct rte_ipv6_addr ucast =
 		RTE_IPV6(0x2a01, 0xcb00, 0x0254, 0x3300, 0x6239, 0xe1f4, 0x7a0b, 0x2371);
@@ -106,18 +108,24 @@ test_ipv6_addr_kind(void)
 	TEST_ASSERT(!rte_ipv6_addr_is_linklocal(&ucast), "");
 	TEST_ASSERT(!rte_ipv6_addr_is_loopback(&ucast), "");
 	TEST_ASSERT(!rte_ipv6_addr_is_mcast(&ucast), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_v4compat(&ucast), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_v4mapped(&ucast), "");
 
 	const struct rte_ipv6_addr mcast = RTE_IPV6(0xff01, 0, 0, 0, 0, 0, 0, 1);
 	TEST_ASSERT(!rte_ipv6_addr_is_unspec(&mcast), "");
 	TEST_ASSERT(!rte_ipv6_addr_is_linklocal(&mcast), "");
 	TEST_ASSERT(!rte_ipv6_addr_is_loopback(&mcast), "");
 	TEST_ASSERT(rte_ipv6_addr_is_mcast(&mcast), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_v4compat(&mcast), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_v4mapped(&mcast), "");
 
 	const struct rte_ipv6_addr lo = RTE_IPV6_ADDR_LOOPBACK;
 	TEST_ASSERT(!rte_ipv6_addr_is_unspec(&lo), "");
 	TEST_ASSERT(!rte_ipv6_addr_is_linklocal(&lo), "");
 	TEST_ASSERT(rte_ipv6_addr_is_loopback(&lo), "");
 	TEST_ASSERT(!rte_ipv6_addr_is_mcast(&lo), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_v4compat(&lo), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_v4mapped(&lo), "");
 
 	const struct rte_ipv6_addr local =
 		RTE_IPV6(0xfe80, 0, 0, 0, 0x5a84, 0xc52c, 0x6aef, 0x4639);
@@ -125,6 +133,24 @@ test_ipv6_addr_kind(void)
 	TEST_ASSERT(rte_ipv6_addr_is_linklocal(&local), "");
 	TEST_ASSERT(!rte_ipv6_addr_is_loopback(&local), "");
 	TEST_ASSERT(!rte_ipv6_addr_is_mcast(&local), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_v4compat(&local), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_v4mapped(&local), "");
+
+	const struct rte_ipv6_addr v4compat = RTE_IPV6(0, 0, 0, 0, 0, 0, 0xc0a8, 0x0001);
+	TEST_ASSERT(!rte_ipv6_addr_is_unspec(&v4compat), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_linklocal(&v4compat), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_loopback(&v4compat), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_mcast(&v4compat), "");
+	TEST_ASSERT(rte_ipv6_addr_is_v4compat(&v4compat), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_v4mapped(&v4compat), "");
+
+	const struct rte_ipv6_addr v4mapped = RTE_IPV6(0, 0, 0, 0, 0, 0xffff, 0xc0a8, 0x0001);
+	TEST_ASSERT(!rte_ipv6_addr_is_unspec(&v4mapped), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_linklocal(&v4mapped), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_loopback(&v4mapped), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_mcast(&v4mapped), "");
+	TEST_ASSERT(!rte_ipv6_addr_is_v4compat(&v4mapped), "");
+	TEST_ASSERT(rte_ipv6_addr_is_v4mapped(&v4mapped), "");
 
 	return TEST_SUCCESS;
 }