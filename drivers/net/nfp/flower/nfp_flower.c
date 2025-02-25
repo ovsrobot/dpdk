ovsrobot/dpdk

commit 636e133ec8913d8a5e964289501060b97e7d4053

net/nfp: update Tx and Rx for multiple PF
Update the Tx function and Rx function for multiple PF and simplify the
Rx function to enhance the performance.
In the RX function, the packets are processed according the firmware
type.

Signed-off-by: Peng Zhang <peng.zhang@corigine.com>
Reviewed-by: Chaoyong He <chaoyong.he@corigine.com>
Reviewed-by: Long Wu <long.wu@corigine.com>

drivers/net/nfp/flower/nfp_flower.c
@@ -215,6 +215,76 @@ nfp_flower_pf_xmit_pkts(void *tx_queue,
 	return app_fw_flower->nfd_func.pf_xmit_t(tx_queue, tx_pkts, nb_pkts);
 }
 
+uint16_t
+nfp_flower_multiple_pf_recv_pkts(void *rx_queue,
+		struct rte_mbuf **rx_pkts,
+		uint16_t nb_pkts)
+{
+	int i;
+	uint16_t recv;
+	uint32_t data_len;
+	struct nfp_net_rxq *rxq;
+	struct rte_eth_dev *repr_dev;
+	struct nfp_flower_representor *repr;
+
+	recv = nfp_net_recv_pkts(rx_queue, rx_pkts, nb_pkts);
+	if (recv != 0) {
+		/* Grab a handle to the representor struct */
+		rxq = rx_queue;
+		repr_dev = &rte_eth_devices[rxq->port_id];
+		repr = repr_dev->data->dev_private;
+
+		data_len = 0;
+		for (i = 0; i < recv; i++)
+			data_len += rx_pkts[i]->data_len;
+
+		repr->repr_stats.ipackets += recv;
+		repr->repr_stats.q_ipackets[rxq->qidx] += recv;
+		repr->repr_stats.q_ibytes[rxq->qidx] += data_len;
+	}
+
+	return recv;
+}
+
+uint16_t
+nfp_flower_multiple_pf_xmit_pkts(void *tx_queue,
+		struct rte_mbuf **tx_pkts,
+		uint16_t nb_pkts)
+{
+	int i;
+	uint16_t sent;
+	uint32_t data_len;
+	struct nfp_net_txq *txq;
+	struct rte_eth_dev *repr_dev;
+	struct nfp_flower_representor *repr;
+
+	txq = tx_queue;
+	if (unlikely(txq == NULL)) {
+		PMD_TX_LOG(ERR, "TX Bad queue.");
+		return 0;
+	}
+
+	/* Grab a handle to the representor struct */
+	repr_dev = &rte_eth_devices[txq->port_id];
+	repr = repr_dev->data->dev_private;
+	for (i = 0; i < nb_pkts; i++)
+		nfp_flower_pkt_add_metadata(repr->app_fw_flower,
+				tx_pkts[i], repr->port_id);
+
+	sent = nfp_flower_pf_xmit_pkts(tx_queue, tx_pkts, nb_pkts);
+	if (sent != 0) {
+		data_len = 0;
+		for (i = 0; i < sent; i++)
+			data_len += tx_pkts[i]->data_len;
+
+		repr->repr_stats.opackets += sent;
+		repr->repr_stats.q_opackets[txq->qidx] += sent;
+		repr->repr_stats.q_obytes[txq->qidx] += data_len;
+	}
+
+	return sent;
+}
+
 static int
 nfp_flower_init_vnic_common(struct nfp_net_hw_priv *hw_priv,
 		struct nfp_net_hw *hw,

drivers/net/nfp/flower/nfp_flower_representor.c
@@ -815,8 +815,8 @@ nfp_flower_multiple_pf_repr_init(struct rte_eth_dev *eth_dev,
 	hw_priv = repr_init->hw_priv;
 
 	eth_dev->dev_ops = &nfp_flower_multiple_pf_repr_dev_ops;
-	eth_dev->rx_pkt_burst = nfp_net_recv_pkts;
-	eth_dev->tx_pkt_burst = nfp_flower_pf_xmit_pkts;
+	eth_dev->rx_pkt_burst = nfp_flower_multiple_pf_recv_pkts;
+	eth_dev->tx_pkt_burst = nfp_flower_multiple_pf_xmit_pkts;
 	eth_dev->data->dev_flags |= RTE_ETH_DEV_REPRESENTOR |
 			RTE_ETH_DEV_AUTOFILL_QUEUE_XSTATS;
 

drivers/net/nfp/flower/nfp_flower.h
@@ -115,6 +115,12 @@ bool nfp_flower_pf_dispatch_pkts(struct nfp_net_rxq *rxq,
 		uint32_t port_id);
 uint16_t nfp_flower_pf_xmit_pkts(void *tx_queue, struct rte_mbuf **tx_pkts,
 		uint16_t nb_pkts);
+uint16_t nfp_flower_multiple_pf_xmit_pkts(void *tx_queue,
+		struct rte_mbuf **tx_pkts,
+		uint16_t nb_pkts);
+uint16_t nfp_flower_multiple_pf_recv_pkts(void *rx_queue,
+		struct rte_mbuf **tx_pkts,
+		uint16_t nb_pkts);
 int nfp_flower_pf_start(struct rte_eth_dev *dev);
 int nfp_flower_pf_stop(struct rte_eth_dev *dev);
 uint32_t nfp_flower_pkt_add_metadata(struct nfp_app_fw_flower *app_fw_flower,

drivers/net/nfp/flower/nfp_flower_cmsg.h
@@ -1045,4 +1045,11 @@ int nfp_flower_cmsg_qos_delete(struct nfp_app_fw_flower *app_fw_flower,
 int nfp_flower_cmsg_qos_stats(struct nfp_app_fw_flower *app_fw_flower,
 		struct nfp_cfg_head *head);
 
+static inline bool
+nfp_flower_port_is_phy_port(uint32_t port_id)
+{
+	return (NFP_FLOWER_CMSG_PORT_TYPE(port_id) ==
+			NFP_FLOWER_CMSG_PORT_TYPE_PHYS_PORT);
+}
+
 #endif /* __NFP_CMSG_H__ */

drivers/net/nfp/nfp_ethdev.c
@@ -2536,6 +2536,12 @@ nfp_pf_init(struct rte_pci_device *pci_dev)
 
 	hw_priv->is_pf = true;
 
+	if (!nfp_net_recv_pkt_meta_check_register(hw_priv)) {
+		PMD_INIT_LOG(ERR, "PF register meta check function failed.");
+		ret = -EIO;
+		goto hw_priv_free;
+	}
+
 	/*
 	 * PF initialization has been done at this point. Call app specific
 	 * init code now.

drivers/net/nfp/nfp_ethdev_vf.c
@@ -316,6 +316,12 @@ nfp_netvf_init(struct rte_eth_dev *eth_dev)
 	hw_priv->dev_info = dev_info;
 	hw_priv->pf_dev = pf_dev;
 
+	if (!nfp_net_recv_pkt_meta_check_register(hw_priv)) {
+		PMD_INIT_LOG(ERR, "VF register meta check function failed.");
+		err = -EINVAL;
+		goto hw_priv_free;
+	}
+
 	eth_dev->process_private = hw_priv;
 
 	/* For secondary processes, the primary has done all the work */

drivers/net/nfp/nfp_net_common.c
@@ -9,6 +9,7 @@
 
 #include <rte_alarm.h>
 
+#include "flower/nfp_flower_cmsg.h"
 #include "flower/nfp_flower_representor.h"
 #include "nfd3/nfp_nfd3.h"
 #include "nfdk/nfp_nfdk.h"
@@ -2876,4 +2877,51 @@ nfp_net_vf_config_app_init(struct nfp_net_hw *net_hw,
 	}
 
 	return 0;
+}
+
+static inline bool
+nfp_net_meta_has_no_port_type(__rte_unused struct nfp_net_meta_parsed *meta)
+{
+	return true;
+}
+
+static inline bool
+nfp_net_meta_is_not_pf_port(__rte_unused struct nfp_net_meta_parsed *meta)
+{
+	return false;
+}
+
+static inline bool
+nfp_net_meta_is_pf_port(struct nfp_net_meta_parsed *meta)
+{
+	return nfp_flower_port_is_phy_port(meta->port_id);
+}
+
+bool
+nfp_net_recv_pkt_meta_check_register(struct nfp_net_hw_priv *hw_priv)
+{
+	struct nfp_pf_dev *pf_dev;
+
+	pf_dev = hw_priv->pf_dev;
+	if (!hw_priv->is_pf) {
+		pf_dev->recv_pkt_meta_check_t = nfp_net_meta_has_no_port_type;
+		return true;
+	}
+
+	switch (pf_dev->app_fw_id) {
+	case NFP_APP_FW_CORE_NIC:
+		pf_dev->recv_pkt_meta_check_t = nfp_net_meta_has_no_port_type;
+		break;
+	case NFP_APP_FW_FLOWER_NIC:
+		if (pf_dev->multi_pf.enabled)
+			pf_dev->recv_pkt_meta_check_t = nfp_net_meta_is_pf_port;
+		else
+			pf_dev->recv_pkt_meta_check_t = nfp_net_meta_is_not_pf_port;
+		break;
+	default:
+		PMD_INIT_LOG(ERR, "Unsupported Firmware loaded.");
+		return false;
+	}
+
+	return true;
 }

drivers/net/nfp/nfp_net_common.h
@@ -169,6 +169,9 @@ struct nfp_pf_dev {
 
 	/** Record the speed uptade */
 	bool speed_updated;
+
+	/** Function pointer used to check the metadata of recv pkts. */
+	bool (*recv_pkt_meta_check_t)(struct nfp_net_meta_parsed *meta);
 };
 
 #define NFP_NET_ETH_FLOW_LIMIT    8
@@ -389,6 +392,7 @@ bool nfp_net_version_check(struct nfp_hw *hw,
 void nfp_net_ctrl_bar_size_set(struct nfp_pf_dev *pf_dev);
 void nfp_net_notify_port_speed(struct nfp_net_hw *hw,
 		struct rte_eth_link *link);
+bool nfp_net_recv_pkt_meta_check_register(struct nfp_net_hw_priv *hw_priv);
 
 #define NFP_PRIV_TO_APP_FW_NIC(app_fw_priv)\
 	((struct nfp_app_fw_nic *)app_fw_priv)

commit 44b82b4b5d7ae190d49e2d3920ca50f79bba04bc

net/nfp: fix get represetor wrong port stats
The 'ipackets'/'opackets' are used to record the number
of packets on represetor port received/sent. But the
code does not consider concurrent calculation of
'ipackets'/'opackets'. If multiple queues are calculated
'ipackets'/'opackets' simultaneously, it will result in
incorrect results.

The previous logic has recorded the number of packets on
each queue, therefore driver only needs to add the data of
all queues to obtain the data of the representor port.

Based on this, modify code to fix the issue.

Fixes: 636e133ec891 ("net/nfp: update Tx and Rx for multiple PF")
Fixes: 82a2c286f35a ("net/nfp: support xstats for flower firmware")
Cc: peng.zhang@corigine.com
Cc: chaoyong.he@corigine.com
Cc: stable@dpdk.org

Signed-off-by: Long Wu <long.wu@corigine.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/nfp/flower/nfp_flower.c
@@ -238,7 +238,6 @@ nfp_flower_multiple_pf_recv_pkts(void *rx_queue,
 		for (i = 0; i < recv; i++)
 			data_len += rx_pkts[i]->data_len;
 
-		repr->repr_stats.ipackets += recv;
 		repr->repr_stats.q_ipackets[rxq->qidx] += recv;
 		repr->repr_stats.q_ibytes[rxq->qidx] += data_len;
 	}
@@ -277,7 +276,6 @@ nfp_flower_multiple_pf_xmit_pkts(void *tx_queue,
 		for (i = 0; i < sent; i++)
 			data_len += tx_pkts[i]->data_len;
 
-		repr->repr_stats.opackets += sent;
 		repr->repr_stats.q_opackets[txq->qidx] += sent;
 		repr->repr_stats.q_obytes[txq->qidx] += data_len;
 	}

drivers/net/nfp/flower/nfp_flower_representor.c
@@ -315,9 +315,25 @@ static int
 nfp_flower_repr_stats_get(struct rte_eth_dev *ethdev,
 		struct rte_eth_stats *stats)
 {
+	uint16_t i;
 	struct nfp_flower_representor *repr;
 
 	repr = ethdev->data->dev_private;
+
+	repr->repr_stats.ipackets = 0;
+	repr->repr_stats.ibytes = 0;
+	for (i = 0; i < ethdev->data->nb_rx_queues; i++) {
+		repr->repr_stats.ipackets += repr->repr_stats.q_ipackets[i];
+		repr->repr_stats.ibytes += repr->repr_stats.q_ibytes[i];
+	}
+
+	repr->repr_stats.opackets = 0;
+	repr->repr_stats.obytes = 0;
+	for (i = 0; i < ethdev->data->nb_tx_queues; i++) {
+		repr->repr_stats.opackets += repr->repr_stats.q_opackets[i];
+		repr->repr_stats.obytes += repr->repr_stats.q_obytes[i];
+	}
+
 	rte_memcpy(stats, &repr->repr_stats, sizeof(struct rte_eth_stats));
 
 	return 0;
@@ -385,7 +401,6 @@ nfp_flower_repr_rx_burst(void *rx_queue,
 		for (i = 0; i < total_dequeue; i++)
 			data_len += rx_pkts[i]->data_len;
 
-		repr->repr_stats.ipackets += total_dequeue;
 		repr->repr_stats.q_ipackets[rxq->qidx] += total_dequeue;
 		repr->repr_stats.q_ibytes[rxq->qidx] += data_len;
 	}
@@ -434,7 +449,6 @@ nfp_flower_repr_tx_burst(void *tx_queue,
 		for (i = 0; i < sent; i++)
 			data_len += tx_pkts[i]->data_len;
 
-		repr->repr_stats.opackets += sent;
 		repr->repr_stats.q_opackets[txq->qidx] += sent;
 		repr->repr_stats.q_obytes[txq->qidx] += data_len;
 	}