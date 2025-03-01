ovsrobot/dpdk

commit fabc9e1322e26b41ab74a8e35dcb477c702c3f6d

net/ice: fix flows handling
Currently ICE PMD uses group attribute to select the appropriate HW engine
to offload the flow. This behavior violates the rte_flow API, existing
documentation/examples, and reveals hardware specific details.

This patch eliminates the use of the group attribute and runs each engine
parser in the order they work in the HW pipeline.

Fixes: 9c5f0070fa3f ("net/ice: map group to pipeline stage")
Cc: stable@dpdk.org

Signed-off-by: Vladimir Medvedkin <vladimir.medvedkin@intel.com>
Acked-by: Bruce Richardson <bruce.richardson@intel.com>

drivers/net/intel/ice/ice_generic_flow.c
@@ -20,6 +20,8 @@
 
 #define ICE_FLOW_ENGINE_DISABLED(mask, type) ((mask) & BIT(type))
 
+#define ICE_FLOW_ENGINE_NB	3
+
 static struct ice_engine_list engine_list =
 		TAILQ_HEAD_INITIALIZER(engine_list);
 
@@ -2295,21 +2297,23 @@ ice_flow_process_filter(struct rte_eth_dev *dev,
 		return 0;
 	}
 
-	parser = get_flow_parser(attr->group);
-	if (parser == NULL) {
-		rte_flow_error_set(error, EINVAL,
-				   RTE_FLOW_ERROR_TYPE_ATTR,
-				   NULL, "NULL attribute.");
-		return -rte_errno;
+	for (int i = 0; i < ICE_FLOW_ENGINE_NB; i++) {
+		parser = get_flow_parser(i);
+		if (parser == NULL) {
+			rte_flow_error_set(error, EINVAL,
+					RTE_FLOW_ERROR_TYPE_ATTR,
+					NULL, "NULL attribute.");
+			return -rte_errno;
+		}
+
+		if (ice_parse_engine(ad, flow, parser, attr->priority,
+				pattern, actions, error)) {
+			*engine = parser->engine;
+			return 0;
+		}
 	}
 
-	if (ice_parse_engine(ad, flow, parser, attr->priority,
-			     pattern, actions, error)) {
-		*engine = parser->engine;
-		return 0;
-	} else {
-		return -rte_errno;
-	}
+	return -rte_errno;
 }
 
 static int

commit 2a5b3dc1716ad494923d321489ae25f2836631f9

net/ice: fix flow engines order
Reorder flow engine evaluation for RTE flow rule installation.
Currently, the driver evaluates rules with engines in the order they are
executed in the hardware. However, in this situation, some wildcarded flows
that are also supported by the ACL engine are installed as switch engine
rules, which scales poorly with each distinct wildcard mask configuration.

Fixes: fabc9e1322e2 ("net/ice: fix flows handling")
Cc: stable@dpdk.org

Signed-off-by: Vladimir Medvedkin <vladimir.medvedkin@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/intel/ice/ice_generic_flow.c
@@ -2239,11 +2239,11 @@ static struct ice_flow_parser *get_flow_parser(uint32_t group)
 {
 	switch (group) {
 	case 0:
-		return &ice_switch_parser;
+		return &ice_fdir_parser;
 	case 1:
 		return &ice_acl_parser;
 	case 2:
-		return &ice_fdir_parser;
+		return &ice_switch_parser;
 	default:
 		return NULL;
 	}
@@ -2298,6 +2298,13 @@ ice_flow_process_filter(struct rte_eth_dev *dev,
 	}
 
 	for (int i = 0; i < ICE_FLOW_ENGINE_NB; i++) {
+		/**
+		 * Evaluate parsers in the following order:
+		 * FDIR - for exact match rules
+		 * ACL - for some subset of wildcard matching rules
+		 * Switch - for the rest. This engine is placed after ACL
+		 * because it scales worse than ACL for different wildcard masks.
+		 **/
 		parser = get_flow_parser(i);
 		if (parser == NULL) {
 			rte_flow_error_set(error, EINVAL,