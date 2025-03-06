ovsrobot/dpdk

commit dd88f51a57255e93640cc880fd1e09ed6ffe1912

devtools: forbid DPDK API in cnxk base driver
cnxk base code is shared between different driver environments.

Forbid the direct usage of rte_ symbols instead use plt_
symbol as alternatives.

roc_platform.[ch] files abstract the difference of driver
environment, hence skip those files for rules check.

Suggested-by: David Marchand <david.marchand@redhat.com>
Signed-off-by: Jerin Jacob <jerinj@marvell.com>

devtools/checkpatches.sh
@@ -175,6 +175,15 @@ check_forbidden_additions() { # <patch>
 		-f $(dirname $(readlink -f $0))/check-forbidden-tokens.awk \
 		"$1" || res=1
 
+	# forbid rte_ symbols in cnxk base driver
+	awk -v FOLDERS='drivers/common/cnxk/roc_*' \
+		-v SKIP_FILES='roc_platform*' \
+		-v EXPRESSIONS="rte_ RTE_" \
+		-v RET_ON_FAIL=1 \
+		-v MESSAGE='Use plt_ symbols instead of rte_ API in cnxk base driver' \
+		-f $(dirname $(readlink -f $0))/check-forbidden-tokens.awk \
+		"$1" || res=1
+
 	# forbid inclusion of driver specific headers in apps and examples
 	awk -v FOLDERS='app examples' \
 		-v EXPRESSIONS='include.*_driver\\.h include.*_pmd\\.h' \

commit b7c8f87fd226954a01c43532e36fa71b91503553

devtools: fix regex of skip files
SKIP_FILES should include the path of the file along with the name.
Update the regex to match the entire path.

Fixes: dd88f51a5725 ("devtools: forbid DPDK API in cnxk base driver")
Fixes: 2b843cac232e ("drivers: use per line logging in helpers")

Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

devtools/checkpatches.sh
@@ -63,7 +63,7 @@ check_forbidden_additions() { # <patch>
 
 	# refrain from new calls to RTE_LOG in drivers (but leave some leeway for base drivers)
 	awk -v FOLDERS="drivers" \
-		-v SKIP_FILES='osdep.h$' \
+		-v SKIP_FILES='.*osdep.h$' \
 		-v EXPRESSIONS="RTE_LOG\\\( RTE_LOG_DP\\\( rte_log\\\(" \
 		-v RET_ON_FAIL=1 \
 		-v MESSAGE='Prefer RTE_LOG_LINE/RTE_LOG_DP_LINE' \
@@ -236,7 +236,7 @@ check_forbidden_additions() { # <patch>
 
 	# forbid rte_ symbols in cnxk base driver
 	awk -v FOLDERS='drivers/common/cnxk/roc_*' \
-		-v SKIP_FILES='roc_platform*' \
+		-v SKIP_FILES='.*roc_platform.*' \
 		-v EXPRESSIONS="rte_ RTE_" \
 		-v RET_ON_FAIL=1 \
 		-v MESSAGE='Use plt_ symbols instead of rte_ API in cnxk base driver' \