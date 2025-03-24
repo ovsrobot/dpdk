f7ecc56790b4d6847d176903785dfb27521e29d7
Bruce Richardson <bruce.richardson@intel.com>
2025-03-24 17:30:28 +0000

    eal: convert core masks and lists to core sets
    Rather than directly parsing and working with the core mask and core
    list parameters, convert them into core maps, and centralise all core
    parsing there. This allows future work to adjust the mappings of cores
    when generating that mapping parameter.
    
    Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/include/rte_eal.h
+++ lib/eal/include/rte_eal.h
@@ -493,22 +493,20 @@ rte_eal_get_runtime_dir(void);
 /**
  * Convert a string describing a mask of core ids into an array of core ids.
  *
- * On success, the passed array is filled with the orders of the core ids
- * present in the mask (-1 indicating that a core id is absent).
- * For example, passing a 0xa coremask results in cores[1] = 0, cores[3] = 1,
- * and the rest of the array is set to -1.
+ * On success, the passed array is filled with the core ids present in the mask.
  *
  * @param coremask
  *   A string describing a mask of core ids.
  * @param cores
- *   An array where to store the core ids orders.
+ *   The output array to store the core ids.
  *   This array must be at least RTE_MAX_LCORE large.
  * @return
- *   0 on success, -1 if the string content was invalid.
+ *  The number of cores in the coremask, and in the returned "cores" array,
+ *  -1 if the string content was invalid.
  */
 __rte_internal
 int
-rte_eal_parse_coremask(const char *coremask, int *cores);
+rte_eal_expand_coremask(const char *coremask, int *cores);
 
 #ifdef __cplusplus
 }
