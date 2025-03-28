32e73554b137d58333ccf100efc3b6efff950371
David Marchand <david.marchand@redhat.com>
2025-03-28 11:52:42 +0100

    build: mark exported symbols
    Annotate symbols with newly introduced export macros.
    
    For code not compiled via drivers/meson.build (think base drivers code),
    the exported symbols are added in some new .c listed in the sources so they
    get caught by drivers/meson.build.
    
    Signed-off-by: David Marchand <david.marchand@redhat.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/intel/iavf/iavf_symbols.c
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2025 Red Hat, Inc.
+ */
+
+#include <eal_symbol_exports.h>
+
+RTE_EXPORT_INTERNAL_SYMBOL(iavf_init_adminq)
+RTE_EXPORT_INTERNAL_SYMBOL(iavf_shutdown_adminq)
+RTE_EXPORT_INTERNAL_SYMBOL(iavf_clean_arq_element)
+RTE_EXPORT_INTERNAL_SYMBOL(iavf_set_mac_type)
+RTE_EXPORT_INTERNAL_SYMBOL(iavf_aq_send_msg_to_pf)
+RTE_EXPORT_INTERNAL_SYMBOL(iavf_vf_parse_hw_config)
+RTE_EXPORT_INTERNAL_SYMBOL(iavf_vf_reset)
--- lib/eal/arm/rte_hypervisor.c
+++ lib/eal/arm/rte_hypervisor.c
@@ -2,8 +2,10 @@
  * Copyright 2017 Mellanox Technologies, Ltd
  */
 
+#include <eal_symbol_exports.h>
 #include "rte_hypervisor.h"
 
+RTE_EXPORT_SYMBOL(rte_hypervisor_get)
 enum rte_hypervisor
 rte_hypervisor_get(void)
 {
