9a2047458c538b90455385a9d201d185f62db17c
liwencheng <liwencheng@phytium.com.cn>
2025-04-02 06:59:12 +0000

    net/macb: add NEON vectorized Rx/Tx
    To optimize Rx/Tx burst process,
    add NEON vector instructions on arm architecture.
    
    v4:
    * Support hardware Rx checksum offload.
    * Fixed some code style issues.
    
    Signed-off-by: liwencheng <liwencheng@phytium.com.cn>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/macb/macb_rxtx.c
+++ drivers/net/macb/macb_rxtx.c
@@ -1366,6 +1366,8 @@ int __rte_cold eth_macb_rx_init(struct rte_eth_dev *dev)
 	return 0;
 }
 
+/* Stubs needed for linkage when RTE_ARCH_ARM64 is not set. */
+#if !defined(RTE_ARCH_ARM64)
 uint16_t
 eth_macb_recv_pkts_vec(void __rte_unused *rx_queue,
 		       struct rte_mbuf __rte_unused **rx_pkts,
@@ -1389,3 +1391,4 @@ eth_macb_xmit_pkts_vec(void __rte_unused *tx_queue,
 {
 	return 0;
 }
+#endif
