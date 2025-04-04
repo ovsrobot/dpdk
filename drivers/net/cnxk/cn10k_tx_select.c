3f76053b7eb47c744f480ee166227c84d0cc404c
Andre Muezerie <andremue@linux.microsoft.com>
2025-04-03 18:47:49 -0700

    drivers: remove __rte_used from functions for compatibility with MSVC
    With gcc, the macro __rte_used translates to __attribute__((used)).
    MSVC has something to the same effect, but harder to use and with some
    limitations (one being that it cannot be used with "static"). Therefore,
    it makes sense to avoid __rte_used in some cases.
    
    The functions modified in this patch don't really need to use __rte_used.
    Instead, these functions can be involved in same ifdefs used in the
    callers. That way, they are only defined when needed (when
    someone is actually calling the function). Doing so makes the code
    compatible with MSVC and avoids compiler warnings about functions being
    defined but not used.
    
    Signed-off-by: Andre Muezerie <andremue@linux.microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/cnxk/cn10k_tx_select.c
+++ drivers/net/cnxk/cn10k_tx_select.c
@@ -5,7 +5,9 @@
 #include "cn10k_ethdev.h"
 #include "cn10k_tx.h"
 
-static __rte_used inline void
+#if defined(RTE_ARCH_ARM64)
+#if !defined(CNXK_DIS_TMPLT_FUNC)
+static inline void
 pick_tx_func(struct rte_eth_dev *eth_dev,
 	     const eth_tx_burst_t tx_burst[NIX_TX_OFFLOAD_MAX])
 {
@@ -19,8 +21,8 @@ pick_tx_func(struct rte_eth_dev *eth_dev,
 		rte_eth_fp_ops[eth_dev->data->port_id].tx_pkt_burst =
 			eth_dev->tx_pkt_burst;
 }
+#endif
 
-#if defined(RTE_ARCH_ARM64)
 static int
 cn10k_nix_tx_queue_count(void *tx_queue)
 {
