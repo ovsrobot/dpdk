3d56a6e6c8533a32d011db28c3103542c77f463b
Ankur Dwivedi <adwivedi@marvell.com>
2025-04-15 17:40:51 +0530

    node: add next node in packet classification
    The ip6_lookup_fib is added as next node in packet classification.
    
    Signed-off-by: Ankur Dwivedi <adwivedi@marvell.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/node/pkt_cls.c
+++ lib/node/pkt_cls.c
@@ -233,6 +233,7 @@ struct rte_node_register pkt_cls_node = {
 		[PKT_CLS_NEXT_IP4_LOOKUP] = "ip4_lookup",
 		[PKT_CLS_NEXT_IP6_LOOKUP] = "ip6_lookup",
 		[PKT_CLS_NEXT_IP4_LOOKUP_FIB] = "ip4_lookup_fib",
+		[PKT_CLS_NEXT_IP6_LOOKUP_FIB] = "ip6_lookup_fib",
 	},
 };
 RTE_NODE_REGISTER(pkt_cls_node);
