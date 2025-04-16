08018a589aa60a2cc939383b9501d7e8ea7d8cfa
Andre Muezerie <andremue@linux.microsoft.com>
2025-04-16 14:44:56 -0700

    buildtools: use macro to embed information in binaries
    The archiver tool from the MSVC toolset is lib.exe. It has different
    parameters then it's GNU counterpart "ar".
    
    buildtools\meson.build was updated to use lib.exe when MSVC compiler is
    used. This is to allow the code to be built without requiring GNU "ar"
    to be installed in that scenario.
    
    Script gen-pmdinfo-cfile.py was updated to use the correct parameters
    when using lib.exe.
    
    Signed-off-by: Andre Muezerie <andremue@linux.microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- buildtools/gen-pmdinfo-cfile.py
+++ buildtools/gen-pmdinfo-cfile.py
@@ -7,15 +7,29 @@ import subprocess
 import sys
 import tempfile
 
-_, tmp_root, ar, archive, output, *pmdinfogen = sys.argv
+_, tmp_root, archiver, archive, output, *pmdinfogen = sys.argv
 with tempfile.TemporaryDirectory(dir=tmp_root) as temp:
     paths = []
-    for name in subprocess.run([ar, "t", archive], stdout=subprocess.PIPE,
-                               check=True).stdout.decode().splitlines():
+    if archiver == "lib":
+        archiver_options = ["/LIST", "/NOLOGO"]
+    else:
+        archiver_options = ["t"]
+    for name in (
+        subprocess.run(
+            [archiver] + archiver_options + [archive],
+            stdout=subprocess.PIPE,
+            check=True,
+        )
+        .stdout.decode()
+        .splitlines()
+    ):
         if os.path.exists(name):
             paths.append(name)
         else:
-            subprocess.run([ar, "x", os.path.abspath(archive), name],
-                           check=True, cwd=temp)
+            if archiver == "lib":
+                run_args = [archiver, f"/EXTRACT:{name}", os.path.abspath(archive)]
+            else:
+                run_args = [archiver, "x", os.path.abspath(archive), name]
+            subprocess.run(run_args, check=True, cwd=temp)
             paths.append(os.path.join(temp, name))
     subprocess.run(pmdinfogen + paths + [output], check=True)
