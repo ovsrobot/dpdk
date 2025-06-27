ovsrobot/dpdk

commit c81daae2383ac655fe503e7da4767959ccc38ab7

net/ixgbe: fix port mask default value in filter
The fdir filter should mask L4 protocol in order to process raw IP
packets.  However, the initial value of port mask is 0xFF and the filter
will not process raw IP packets if the flow rule does not change it.

Fixed by setting the default value to 0.

Fixes: 444505f933f1 ("net/ixgbe: fix IPv6 mask in flow director")
Cc: stable@dpdk.org

Signed-off-by: Yuan Wang <yuanx.wang@intel.com>
Tested-by: Hailin Xu <hailinx.xu@intel.com>

drivers/net/intel/ixgbe/ixgbe_flow.c
@@ -1644,6 +1644,8 @@ ixgbe_parse_fdir_filter_normal(struct rte_eth_dev *dev,
 	memset(&rule->mask, 0xFF, sizeof(struct ixgbe_hw_fdir_mask));
 	rule->mask.vlan_tci_mask = 0;
 	rule->mask.flex_bytes_mask = 0;
+	rule->mask.dst_port_mask = 0;
+	rule->mask.src_port_mask = 0;
 
 	/**
 	 * The first not void item should be

commit 1bbb3ad37fa067565fee18170ff84ec124c9d6e9

net/ixgbe: fix sctp mask in flow director
Since the default value of the port mask is set to 0, the port mask does
not change in some cases when creating SCTP flow rules, which results in
incorrect L4P register configuration.

This patch fixes this issue by setting the mask to 0xffff in these cases.

Fixes: c81daae2383a (net/ixgbe: fix port mask default value in filter)
Cc: stable@dpdk.org

Signed-off-by: Yuan Wang <yuanx.wang@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/intel/ixgbe/ixgbe_flow.c
@@ -2161,6 +2161,8 @@ ixgbe_parse_fdir_filter_normal(struct rte_eth_dev *dev,
 					item, "Not supported by fdir filter");
 				return -rte_errno;
 			}
+			rule->mask.src_port_mask = 0xffff;
+			rule->mask.dst_port_mask = 0xffff;
 		}
 
 		item = next_no_fuzzy_pattern(pattern, item);