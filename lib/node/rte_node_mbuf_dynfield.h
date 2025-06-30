ovsrobot/dpdk

commit 746e8736da70f49aa91a777446c62193c127aa2a

node: add global mbuf dynfield
This patch defines rte_node specific dynamic field structure
(rte_node_mbuf_dynfield_t)

rte_node_mbuf_dynfield_t structure holds two types of fields
- Persistent data fields which are preserved across graph walk.
  Currently size of persistent data fields is zero.
- Overloadable data fields which are used by any two adjacent nodes.
  Same fields can be repurposed by any other adjacent nodes

This dynfield can be also be used by out-of-tree nodes.

Signed-off-by: Nitin Saxena <nsaxena@marvell.com>
Acked-by: Pavan Nikhilesh <pbhagavatula@marvell.com>

lib/node/rte_node_mbuf_dynfield.h
@@ -1,0 +1,147 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(C) 2025 Marvell International Ltd.
+ */
+
+#ifndef _RTE_GRAPH_MBUF_DYNFIELD_H_
+#define _RTE_GRAPH_MBUF_DYNFIELD_H_
+
+#include <rte_common.h>
+#include <rte_mbuf.h>
+#include <rte_mbuf_dyn.h>
+
+/**
+ * @file: rte_node_mbuf_dynfield.h
+ *
+ * @warning
+ * @b EXPERIMENTAL: this API may change without prior notice.
+ *
+ * Defines rte_node specific mbuf dynamic field region [rte_node_mbuf_dynfield_t]
+ * which can be used by both DPDK built-in and out-of-tree nodes
+ * for storing per-mbuf fields for graph walk.
+ */
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef RTE_NODE_MBUF_PERSISTENT_FIELDS_SIZE
+/** Size of persistent mbuf fields */
+#define RTE_NODE_MBUF_PERSISTENT_FIELDS_SIZE          (0)
+#endif /* !RTE_NODE_MBUF_PERSISTENT_FIELDS_SIZE */
+
+#ifndef RTE_NODE_MBUF_OVERLOADABLE_FIELDS_SIZE
+/** Size of overloadable mbuf fields */
+#define RTE_NODE_MBUF_OVERLOADABLE_FIELDS_SIZE        (8)
+#endif /* !RTE_NODE_MBUF_OVERLOADABLE_FIELDS_SIZE */
+
+/** Size of node mbuf dynamic field */
+#define RTE_NODE_MBUF_DYNFIELD_SIZE     \
+	(RTE_NODE_MBUF_PERSISTENT_FIELDS_SIZE + RTE_NODE_MBUF_OVERLOADABLE_FIELDS_SIZE)
+
+/**
+ * Node mbuf overloadable data.
+ *
+ * Out-of-tree nodes can repurpose overloadable fields via
+ * rte_node_mbuf_overload_fields_get(mbuf). Overloadable fields are not
+ * preserved and typically can be used with-in two adjacent nodes in the graph.
+ */
+typedef struct rte_node_mbuf_overload_fields {
+	union {
+		uint8_t data[RTE_NODE_MBUF_OVERLOADABLE_FIELDS_SIZE];
+	};
+} rte_node_mbuf_overload_fields_t;
+
+/**
+ * rte_node specific mbuf dynamic field structure [rte_node_mbuf_dynfield_t]
+ *
+ * It holds two types of fields:
+ * 1. Persistent fields: Fields which are preserved across nodes during graph walk.
+ *    - Eg: rx/tx interface etc
+ * 2. Overloadable fields: Fields which can be repurposed by two adjacent nodes.
+ */
+typedef struct rte_node_mbuf_dynfield {
+	/**
+	 * Persistent mbuf region across nodes in graph walk
+	 *
+	 * These fields are preserved across graph walk and can be accessed by
+	 * rte_node_mbuf_dynfield_get() in fast path.
+	 */
+	union {
+		uint8_t persistent_data[RTE_NODE_MBUF_PERSISTENT_FIELDS_SIZE];
+	};
+	/**
+	 * Overloadable mbuf fields across graph walk. Fields which can change.
+	 *
+	 * Two adjacent nodes (producer, consumer) can use this memory region for
+	 * sharing per-mbuf specific information. Once mbuf leaves a consumer node,
+	 * this region can be repurposed by another sets of [producer, consumer] node.
+	 *
+	 * In fast path, this region can be accessed by rte_node_mbuf_overload_fields_get().
+	 */
+	rte_node_mbuf_overload_fields_t overloadable_data;
+} rte_node_mbuf_dynfield_t;
+
+/**
+ * For a given mbuf and dynamic offset, return pointer to rte_node_mbuf_dynfield_t *
+ *
+ * @param m
+ *   Mbuf
+ * @param offset
+ *   Dynamic offset returned by @ref rte_node_mbuf_dynfield_register()
+ * @return
+ *  Pointer to node specific mbuf dynamic field structure
+ */
+__rte_experimental
+static __rte_always_inline rte_node_mbuf_dynfield_t *
+rte_node_mbuf_dynfield_get(struct rte_mbuf *m, const int offset)
+{
+	return RTE_MBUF_DYNFIELD(m, offset, struct rte_node_mbuf_dynfield *);
+}
+
+/**
+ * For a given mbuf and dynamic offset, return pointer to overloadable fields.
+ * Nodes can typecast returned pointer to reuse for their own purpose.
+ *
+ * @param m
+ *   Mbuf
+ * @param offset
+ *   Dynamic offset returned by @ref rte_node_mbuf_dynfield_register()
+ * @return
+ * Pointer to node mbuf overloadable fields
+ */
+__rte_experimental
+static __rte_always_inline rte_node_mbuf_overload_fields_t *
+rte_node_mbuf_overload_fields_get(struct rte_mbuf *m, const int offset)
+{
+	rte_node_mbuf_dynfield_t *f = NULL;
+
+	f = RTE_MBUF_DYNFIELD(m, offset, rte_node_mbuf_dynfield_t *);
+
+	return &(f->overloadable_data);
+}
+
+/**
+ *  Register rte_node specific common mbuf dynamic field region. Can be called
+ *  in rte_node_register()->init() function to save offset in node->ctx
+ *
+ *  In process() function, node->ctx can be passed to
+ *  - rte_node_mbuf_dynfield_get(mbuf, offset)
+ *  - rte_node_mbuf_overload_fields_get(mbuf, offset)
+ *
+ *  Can be called multiple times by any number of nodes in init() function.
+ *  - Very first call registers dynamic field and returns offset.
+ *  - Subsequent calls return same offset.
+ *
+ *  @return
+ *   <0 on error: rte_errno set to one of:
+ *    - ENOMEM - no memory
+ *   >=0 on success: dynamic field offset
+ */
+__rte_experimental
+int rte_node_mbuf_dynfield_register(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _RTE_GRAPH_MBUF_DYNFIELD_H_ */

lib/node/meson.build
@@ -23,6 +23,7 @@ sources = files(
         'kernel_rx.c',
         'kernel_tx.c',
         'log.c',
+        'node_mbuf_dynfield.c',
         'null.c',
         'pkt_cls.c',
         'pkt_drop.c',
@@ -32,6 +33,7 @@ headers = files(
         'rte_node_eth_api.h',
         'rte_node_ip4_api.h',
         'rte_node_ip6_api.h',
+        'rte_node_mbuf_dynfield.h',
         'rte_node_pkt_cls_api.h',
         'rte_node_udp4_input_api.h',
 )

lib/node/node_mbuf_dynfield.c
@@ -1,0 +1,60 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(C) 2025 Marvell International Ltd.
+ */
+
+#include <rte_memzone.h>
+#include <rte_node_mbuf_dynfield.h>
+#include <node_private.h>
+#include <eal_export.h>
+#include <rte_errno.h>
+
+#define NODE_MBUF_DYNFIELD_MEMZONE_NAME "__rte_node_mbuf_dynfield"
+
+struct node_mbuf_dynfield_mz {
+	int dynfield_offset;
+};
+
+static const struct rte_mbuf_dynfield node_mbuf_dynfield_desc = {
+	.name = "rte_node_mbuf_dynfield",
+	.size = sizeof(rte_node_mbuf_dynfield_t),
+	.align = alignof(rte_node_mbuf_dynfield_t),
+};
+
+RTE_EXPORT_EXPERIMENTAL_SYMBOL(rte_node_mbuf_dynfield_register, 25.07);
+int rte_node_mbuf_dynfield_register(void)
+{
+	struct node_mbuf_dynfield_mz *f = NULL;
+	const struct rte_memzone *mz = NULL;
+	int dyn_offset;
+
+	RTE_BUILD_BUG_ON(sizeof(rte_node_mbuf_dynfield_t) < RTE_NODE_MBUF_DYNFIELD_SIZE);
+	RTE_BUILD_BUG_ON(sizeof(rte_node_mbuf_overload_fields_t) <
+			 RTE_NODE_MBUF_OVERLOADABLE_FIELDS_SIZE);
+
+	mz = rte_memzone_lookup(NODE_MBUF_DYNFIELD_MEMZONE_NAME);
+
+	if (!mz) {
+		mz = rte_memzone_reserve_aligned(NODE_MBUF_DYNFIELD_MEMZONE_NAME,
+						 sizeof(struct node_mbuf_dynfield_mz),
+						 SOCKET_ID_ANY, 0,
+						 RTE_CACHE_LINE_SIZE);
+		if (!mz) {
+			node_err("node_mbuf_dyn", "rte_memzone_reserve_aligned failed");
+			rte_errno = ENOMEM;
+			return -1;
+		}
+		dyn_offset = rte_mbuf_dynfield_register(&node_mbuf_dynfield_desc);
+		if (dyn_offset < 0) {
+			node_err("node_mbuf_dyn", "rte_mbuf_dynfield_register failed");
+			return dyn_offset;
+		}
+		f = (struct node_mbuf_dynfield_mz *)mz->addr;
+		f->dynfield_offset = dyn_offset;
+
+		node_dbg("node_mbuf_dyn", "memzone: %s of size: %zu at offset : %d",
+			 mz->name, mz->len, f->dynfield_offset);
+	} else {
+		f = (struct node_mbuf_dynfield_mz *)mz->addr;
+	}
+	return f->dynfield_offset;
+}

commit 64d04668191173c220914299069bd407d903d38c

node: fix C++ compatibility errors (option 1)
C++ does not allow zero-sized unions - they end up being of size 1-byte,
which leads to C/C++ compatibility issues, flagged by the compiler.

  lib/node/rte_node_mbuf_dynfield.h:78:2: error: union has size 0 in C, size 1 in C++ [-Werror,-Wextern-c-compat]
   78 |         union {
	         |         ^
		 1 error generated.

Fix the error by defaulting to a having a 1-byte sized union rather than
zero-sized.

Fixes: 746e8736da70 ("node: add global mbuf dynfield")

Signed-off-by: Bruce Richardson <bruce.richardson@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/node/rte_node_mbuf_dynfield.h
@@ -26,12 +26,13 @@ extern "C" {
 
 #ifndef RTE_NODE_MBUF_PERSISTENT_FIELDS_SIZE
 /** Size of persistent mbuf fields */
-#define RTE_NODE_MBUF_PERSISTENT_FIELDS_SIZE          (0)
+#define RTE_NODE_MBUF_PERSISTENT_FIELDS_SIZE      (1)  /* Must be non-zero to avoid a zero-sized
+                                                        * union, which causes C++ compat issues*/
 #endif /* !RTE_NODE_MBUF_PERSISTENT_FIELDS_SIZE */
 
 #ifndef RTE_NODE_MBUF_OVERLOADABLE_FIELDS_SIZE
 /** Size of overloadable mbuf fields */
-#define RTE_NODE_MBUF_OVERLOADABLE_FIELDS_SIZE        (8)
+#define RTE_NODE_MBUF_OVERLOADABLE_FIELDS_SIZE   (8)
 #endif /* !RTE_NODE_MBUF_OVERLOADABLE_FIELDS_SIZE */
 
 /** Size of node mbuf dynamic field */