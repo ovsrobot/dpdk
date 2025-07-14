e23b515d3a987f3ed06a21b8d052285ac4db5c5d
Jie Liu <liujie5@linkdatatechnology.com>
2025-07-13 23:54:34 -0400

    net/sxe: add ethdev probe and remove
    Add basic modules: logs、 hardware communication、common components
    and support basic PCIe ethdev probe and remove.
    
    Signed-off-by: Jie Liu <liujie5@linkdatatechnology.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/sxe/sxe_drv_type.h
@@ -0,0 +1,19 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (C), 2022, Linkdata Technology Co., Ltd.
+ */
+
+#ifndef __SXE_DRV_TYPEDEF_H__
+#define __SXE_DRV_TYPEDEF_H__
+
+#include "sxe_types.h"
+#ifndef bool
+#define bool _Bool
+#endif
+
+typedef u8 U8;
+typedef u16 U16;
+typedef u32 U32;
+typedef u64 U64;
+typedef bool BOOL;
+
+#endif
