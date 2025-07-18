ovsrobot/dpdk

commit c81daae2383ac655fe503e7da4767959ccc38ab7

net/ixgbe: fix port mask default value in filter
The fdir filter should mask L4 protocol in order to process raw IP
packets.  However, the initial value of port mask is 0xFF and the filter
will not process raw IP packets if the flow rule does not change it.

Fixed by setting the default value to 0.

Fixes: 444505f933f1 ("net/ixgbe: fix IPv6 mask in flow director")
Cc: stable@dpdk.org

Signed-off-by: Yuan Wang <yuanx.wang@intel.com>
Tested-by: Hailin Xu <hailinx.xu@intel.com>

drivers/net/intel/ixgbe/ixgbe_flow.c
@@ -1644,6 +1644,8 @@ ixgbe_parse_fdir_filter_normal(struct rte_eth_dev *dev,
 	memset(&rule->mask, 0xFF, sizeof(struct ixgbe_hw_fdir_mask));
 	rule->mask.vlan_tci_mask = 0;
 	rule->mask.flex_bytes_mask = 0;
+	rule->mask.dst_port_mask = 0;
+	rule->mask.src_port_mask = 0;
 
 	/**
 	 * The first not void item should be

commit eaf86af25f5fea72cfa357eefc1db64f23f3710a

net/ixgbe: fix port mask default value on E610
Modification of default values should be limited to E610,
as 82599 handles raw IP well.

Fixes: c81daae2383a (net/ixgbe: fix port mask default value in filter)
Cc: stable@dpdk.org

Signed-off-by: Yuan Wang <yuanx.wang@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/intel/ixgbe/ixgbe_flow.c
@@ -1644,8 +1644,10 @@ ixgbe_parse_fdir_filter_normal(struct rte_eth_dev *dev,
 	memset(&rule->mask, 0xFF, sizeof(struct ixgbe_hw_fdir_mask));
 	rule->mask.vlan_tci_mask = 0;
 	rule->mask.flex_bytes_mask = 0;
-	rule->mask.dst_port_mask = 0;
-	rule->mask.src_port_mask = 0;
+	if (hw->mac.type == ixgbe_mac_E610) {
+		rule->mask.dst_port_mask = 0;
+		rule->mask.src_port_mask = 0;
+	}
 
 	/**
 	 * The first not void item should be