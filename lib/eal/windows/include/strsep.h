552b56ce6e001ab01a995d9ec39c9b664278ed84
Andre Muezerie <andremue@linux.microsoft.com>
2025-07-21 13:39:02 -0700

    eal: add strsep() for Windows builds
    Function strsep() is used by DPDK code but is not available on Windows.
    
    This patch adds an implementation from NetBSD, unblocking code depending
    on it from being compiled on Windows.
    
    Signed-off-by: Andre Muezerie <andremue@linux.microsoft.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ lib/eal/windows/include/strsep.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 1990, 1993
+ * The Regents of the University of California.  All rights reserved.
+ */
+
+#ifndef _STRSEP_
+#define _STRSEP_
+
+char *
+strsep(char **stringp, const char *delim);
+
+#endif
