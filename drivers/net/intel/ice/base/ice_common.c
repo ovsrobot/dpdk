7ab3b51faf51978d81896a2e50f58ccf1d1dcb63
Vladimir Medvedkin <vladimir.medvedkin@intel.com>
2025-08-11 13:42:56 +0000

    net/ice/base: add utility functions
    Add the following function related to DCB:
    
    * Get number of Congestion Domains per port
    * Resolve Traffic Class(TC) by 802.1p VLAN User Priority(UP)
    * Get absolute index of the Congestion Domain by the TC
    
    Signed-off-by: Vladimir Medvedkin <vladimir.medvedkin@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/intel/ice/base/ice_common.c
+++ drivers/net/intel/ice/base/ice_common.c
@@ -6579,3 +6579,31 @@ bool ice_is_fw_auto_drop_supported(struct ice_hw *hw)
 		return true;
 	return false;
 }
+
+enum ice_cgd_per_port
+ice_get_port_max_cgd(struct ice_hw *hw)
+{
+#define ICE_8_PORTS_LINK_TOPO	0x2
+	u32 link_topo = LE32_TO_CPU(rd32(hw, GLGEN_MAC_LINK_TOPO)) &
+		GLGEN_MAC_LINK_TOPO_LINK_TOPO_M;
+
+	return (link_topo == ICE_8_PORTS_LINK_TOPO) ? ICE_4_CGD_PER_PORT : ICE_8_CGD_PER_PORT;
+}
+
+u8
+ice_get_tc_by_up(struct ice_hw *hw, u8 up)
+{
+	struct ice_port_info *port_info = hw->port_info;
+	struct ice_qos_cfg *qos_cfg = &port_info->qos_cfg;
+	struct ice_dcbx_cfg *local_dcb_conf = &qos_cfg->local_dcbx_cfg;
+
+	up &= (ICE_MAX_TRAFFIC_CLASS - 1);
+
+	return local_dcb_conf->etscfg.prio_table[up];
+}
+
+int
+ice_get_cgd_idx(struct ice_hw *hw, u8 tc)
+{
+	return hw->port_info->lport * ice_get_port_max_cgd(hw) + tc;
+}
