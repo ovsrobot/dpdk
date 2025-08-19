e0c5a0d69d606bc66ca197778d24ccefa947e8cd
Dimon Zhao <dimon.zhao@nebula-matrix.com>
2025-08-19 03:22:32 -0700

    net/nbl: add nbl device rxtx queue setup and release ops
    Implement NBL device Rx and Tx queue setup & release functions
    
    Signed-off-by: Dimon Zhao <dimon.zhao@nebula-matrix.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/nbl/nbl_hw/nbl_resource.h
+++ drivers/net/nbl/nbl_hw/nbl_resource.h
@@ -16,12 +16,102 @@
 #define NBL_RES_MGT_TO_CHAN_OPS(res_mgt)	(NBL_RES_MGT_TO_CHAN_OPS_TBL(res_mgt)->ops)
 #define NBL_RES_MGT_TO_CHAN_PRIV(res_mgt)	(NBL_RES_MGT_TO_CHAN_OPS_TBL(res_mgt)->priv)
 #define NBL_RES_MGT_TO_ETH_DEV(res_mgt)		((res_mgt)->eth_dev)
+#define NBL_RES_MGT_TO_COMMON(res_mgt)		((res_mgt)->common)
 #define NBL_RES_MGT_TO_TXRX_MGT(res_mgt)	((res_mgt)->txrx_mgt)
+#define NBL_RES_MGT_TO_TX_RING(res_mgt, index)	\
+	(NBL_RES_MGT_TO_TXRX_MGT(res_mgt)->tx_rings[(index)])
+#define NBL_RES_MGT_TO_RX_RING(res_mgt, index)	\
+	(NBL_RES_MGT_TO_TXRX_MGT(res_mgt)->rx_rings[(index)])
+
+struct nbl_packed_desc {
+	rte_le64_t addr;
+	rte_le32_t len;
+	rte_le16_t id;
+	rte_le16_t flags;
+};
+
+struct nbl_tx_entry {
+	struct rte_mbuf *mbuf;
+	uint16_t first_id;
+};
+
+struct nbl_rx_entry {
+	struct rte_mbuf *mbuf;
+};
 
 struct nbl_res_tx_ring {
+	volatile struct nbl_packed_desc *desc;
+	struct nbl_tx_entry *tx_entry;
+	const struct rte_memzone *net_hdr_mz;
+	volatile uint8_t *notify;
+	const struct rte_eth_dev *eth_dev;
+	struct nbl_common_info *common;
+	u64 default_hdr[2];
+
+	enum nbl_product_type product;
+	int dma_limit_msb;
+	bool dma_set_msb;
+	u16 nb_desc;
+	u16 next_to_clean;
+	u16 next_to_use;
+
+	u16 avail_used_flags;
+	bool used_wrap_counter;
+	u16 notify_qid;
+	u16 exthdr_len;
+
+	u16 vlan_proto;
+	u16 vlan_tci;
+	u16 lag_id;
+	u16 vq_free_cnt;
+	/* Start freeing TX buffers if there are less free descriptors than this value */
+	u16 tx_free_thresh;
+	/* Number of Tx descriptors to use before RS bit is set */
+	u16 tx_rs_thresh;
+
+	unsigned int size;
+
+	u16 queue_id;
+
+	u64 offloads;
+	u64 ring_phys_addr;
+
+	u16 (*prep_tx_ehdr)(void *priv, struct rte_mbuf *mbuf);
 };
 
 struct nbl_res_rx_ring {
+	volatile struct nbl_packed_desc *desc;
+	struct nbl_rx_entry *rx_entry;
+	struct rte_mempool *mempool;
+	volatile uint8_t *notify;
+	const struct rte_eth_dev *eth_dev;
+	struct nbl_common_info *common;
+	uint64_t mbuf_initializer; /**< value to init mbufs */
+	struct rte_mbuf fake_mbuf;
+
+	enum nbl_product_type product;
+	int dma_limit_msb;
+	unsigned int size;
+	bool dma_set_msb;
+	u16 nb_desc;
+	u16 next_to_clean;
+	u16 next_to_use;
+
+	u16 avail_used_flags;
+	bool used_wrap_counter;
+	u16 notify_qid;
+	u16 exthdr_len;
+
+	u16 vlan_proto;
+	u16 vlan_tci;
+	u16 vq_free_cnt;
+	u16 port_id;
+
+	u16 queue_id;
+	u16 buf_length;
+
+	u64 offloads;
+	u64 ring_phys_addr;
 };
 
 struct nbl_txrx_mgt {
@@ -33,11 +123,20 @@ struct nbl_txrx_mgt {
 	u8 rx_ring_num;
 };
 
+struct nbl_res_info {
+	u16 base_qid;
+	u16 lcore_max;
+	u16 *pf_qid_to_lcore_id;
+	rte_atomic16_t tx_current_queue;
+};
+
 struct nbl_resource_mgt {
 	const struct rte_eth_dev *eth_dev;
 	struct nbl_channel_ops_tbl *chan_ops_tbl;
 	struct nbl_hw_ops_tbl *hw_ops_tbl;
 	struct nbl_txrx_mgt *txrx_mgt;
+	struct nbl_common_info *common;
+	struct nbl_res_info res_info;
 };
 
 struct nbl_resource_mgt_leonis {
