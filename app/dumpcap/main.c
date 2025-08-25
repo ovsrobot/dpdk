b41524ea626e02a28859ac10933780f71744d27a
Stephen Hemminger <stephen@networkplumber.org>
2025-08-24 20:38:21 -0700

    apps: use rte_bsd_queue.h
    Use DPDK not system version.
    Not really a requirement but better to avoid any possible conflict.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- app/dumpcap/main.c
+++ app/dumpcap/main.c
@@ -17,12 +17,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <sys/queue.h>
 #include <sys/types.h>
 #include <sys/utsname.h>
 #include <time.h>
 #include <unistd.h>
 
+#include <rte_bsd_queue.h>
 #include <rte_alarm.h>
 #include <rte_bitops.h>
 #include <rte_bpf.h>
