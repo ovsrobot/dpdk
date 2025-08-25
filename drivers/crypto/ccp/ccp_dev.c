a0082b8916956608d6eedb88b00c7fb5b3cc43a8
Stephen Hemminger <stephen@networkplumber.org>
2025-08-24 20:38:54 -0700

    drivers/crypto: remove use of sys_queue.h
    The system version of queue.h should not be used.
    If necessary use rte_bsd_queue.h
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/crypto/ccp/ccp_dev.c
+++ drivers/crypto/ccp/ccp_dev.c
@@ -7,11 +7,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <sys/mman.h>
-#include <sys/queue.h>
 #include <sys/types.h>
 #include <sys/file.h>
 #include <unistd.h>
 
+#include <rte_bsd_queue.h>
 #include <rte_hexdump.h>
 #include <rte_memzone.h>
 #include <rte_malloc.h>
