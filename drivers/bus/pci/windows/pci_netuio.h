2404407985d92c517e13eb2071cd3239d18e2da6
Thomas Monjalon <thomas@monjalon.net>
2025-09-08 23:17:49 +0200

    bus/pci: fix build with MinGW 13
    After an upgrade to MinGW version 13, some compilation errors appear:
    
    drivers/bus/pci/windows/pci.c:362:58:
    	error: 'GUID_DEVCLASS_NETUIO' undeclared
    drivers/bus/pci/windows/pci_netuio.c:57:39:
    	error: 'GUID_DEVINTERFACE_NETUIO' undeclared
    
    The cause is MinGW has set NTDDI_VERSION to the highest version
    without defining the expected NETUIO constants.
    
    It is safer to not rely on Windows headers version,
    and instead define what is not already defined,
    after including Windows headers.
    
    Fixes: 6605c7f02e24 ("bus/pci: fix build with Windows SDK >= 10.0.20253")
    Cc: stable@dpdk.org
    
    Signed-off-by: Thomas Monjalon <thomas@monjalon.net>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/bus/pci/windows/pci_netuio.h
+++ drivers/bus/pci/windows/pci_netuio.h
@@ -5,12 +5,19 @@
 #ifndef _PCI_NETUIO_H_
 #define _PCI_NETUIO_H_
 
-#if !defined(NTDDI_WIN10_FE) || NTDDI_VERSION < NTDDI_WIN10_FE
-/* GUID definition for device class netUIO */
+/* GUID_DEVCLASS_NETUIO should be defined in devguid.h */
+#ifndef GUID_DEVCLASS_NETUIO
 DEFINE_GUID(GUID_DEVCLASS_NETUIO, 0x78912bc1, 0xcb8e, 0x4b28,
 	0xa3, 0x29, 0xf3, 0x22, 0xeb, 0xad, 0xbe, 0x0f);
+#endif
 
-/* GUID definition for the netuio device interface */
+/* GUID_DEVINTERFACE_NETUIO should be defined in ndisguid.h */
+#ifdef __MINGW32__
+#include <ddk/ndisguid.h>
+#else
+#include <ndisguid.h>
+#endif
+#ifndef GUID_DEVINTERFACE_NETUIO
 DEFINE_GUID(GUID_DEVINTERFACE_NETUIO, 0x08336f60, 0x0679, 0x4c6c,
 	0x85, 0xd2, 0xae, 0x7c, 0xed, 0x65, 0xff, 0xf7);
 #endif
