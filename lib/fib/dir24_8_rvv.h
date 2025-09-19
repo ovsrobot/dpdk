5c5d6180e7f77c080ac19f36ab3fabf57d50c86b
Sun Yuechi <sunyuechi@iscas.ac.cn>
2025-09-20 00:33:56 +0800

    lib/fib: R-V V rte_fib_lookup_bulk
    Implement rte_fib_lookup_bulk function for RISC-V architecture using RISC-V
    Vector Extension instruction set
    
    Signed-off-by: Sun Yuechi <sunyuechi@iscas.ac.cn>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ lib/fib/dir24_8_rvv.h
@@ -0,0 +1,26 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright (c) 2025 Institute of Software Chinese Academy of Sciences (ISCAS).
+ */
+
+#ifndef _DIR248_RVV_H_
+#define _DIR248_RVV_H_
+
+#include "rte_cpuflags.h"
+
+void
+rte_dir24_8_vec_lookup_bulk_1b(void *p, const uint32_t *ips,
+	uint64_t *next_hops, const unsigned int n);
+
+void
+rte_dir24_8_vec_lookup_bulk_2b(void *p, const uint32_t *ips,
+	uint64_t *next_hops, const unsigned int n);
+
+void
+rte_dir24_8_vec_lookup_bulk_4b(void *p, const uint32_t *ips,
+	uint64_t *next_hops, const unsigned int n);
+
+void
+rte_dir24_8_vec_lookup_bulk_8b(void *p, const uint32_t *ips,
+	uint64_t *next_hops, const unsigned int n);
+
+#endif /* _DIR248_RVV_H_ */
