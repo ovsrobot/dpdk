d05892f710c3016a8f5e52111b76bd0e6ada274a
Ciara Loftus <ciara.loftus@intel.com>
2025-10-03 10:23:58 +0000

    net/iavf: support VF initiated resets
    Introduce a function that allows a VF to request the PF to reset the VF
    and then subsequently reinitialise and restart the VF transparently.
    This is useful for example when the application detects that one of the
    queues have hung or any event where a reset is required and the PF is
    unlikely to trigger it.
    
    This commit leverages the existing graceful reset recovery path that was
    introduced in commit 3e6a5d2d310a ("net/iavf: add devargs to enable VF
    auto-reset").
    
    Signed-off-by: Ciara Loftus <ciara.loftus@intel.com>
    Signed-off-by: Timothy Miskell <timothy.miskell@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ drivers/net/intel/iavf/rte_pmd_iavf.c
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2010-2025 Intel Corporation
+ */
+
+#include <eal_export.h>
+
+#include "iavf.h"
+#include "rte_pmd_iavf.h"
+
+RTE_EXPORT_EXPERIMENTAL_SYMBOL(rte_pmd_iavf_restore, 25.11)
+int
+rte_pmd_iavf_restore(uint16_t port)
+{
+	struct rte_eth_dev *dev;
+	int ret;
+
+	RTE_ETH_VALID_PORTID_OR_ERR_RET(port, -ENODEV);
+
+	dev = &rte_eth_devices[port];
+
+	if (!is_iavf_supported(dev)) {
+		PMD_DRV_LOG(ERR, "Cannot restore VF, port %u is not an IAVF device.", port);
+		return -ENOTSUP;
+	}
+
+	ret = iavf_request_restore(dev);
+	if (ret) {
+		PMD_DRV_LOG(ERR, "Request to restore VF failed for port %u", port);
+		return -1;
+	}
+
+	return 0;
+}
