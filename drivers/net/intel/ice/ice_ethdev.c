ovsrobot/dpdk

commit af05f9e37153c53335938bcde2978a359c5f4efa

net/ice: count drop-all filter in Rx stats
Packets dropped in the receive direction are counted as
"rx_unknown_protocol" (GLSWID_RUPP), and these packets need to be added
to the "ipackets". Similarly, the GLV_GORCH/GLV_GORCL counters need to
be added to rx_bytes. For the latter change, rather than adding the
counters we can just read the stats from PF, rather than VSI, since they
are already counted there.

Signed-off-by: Jeff Shaw <jeffrey.b.shaw@intel.com>
Signed-off-by: Shaiq Wani <shaiq.wani@intel.com>
Signed-off-by: Anatoly Burakov <anatoly.burakov@intel.com>
Acked-by: Bruce Richardson <bruce.richardson@intel.com>

drivers/net/intel/ice/ice_ethdev.c
@@ -6308,12 +6308,13 @@ ice_stats_get(struct rte_eth_dev *dev, struct rte_eth_stats *stats)
 
 	stats->ipackets = pf->main_vsi->eth_stats.rx_unicast +
 			  pf->main_vsi->eth_stats.rx_multicast +
+			  ns->eth.rx_unknown_protocol +
 			  pf->main_vsi->eth_stats.rx_broadcast -
 			  pf->main_vsi->eth_stats.rx_discards;
 	stats->opackets = ns->eth.tx_unicast +
 			  ns->eth.tx_multicast +
 			  ns->eth.tx_broadcast;
-	stats->ibytes   = pf->main_vsi->eth_stats.rx_bytes;
+	stats->ibytes   = ns->eth.rx_bytes;
 	stats->obytes   = ns->eth.tx_bytes;
 	stats->oerrors  = ns->eth.tx_errors +
 			  pf->main_vsi->eth_stats.tx_errors;

commit e14e9572d8ccd02645ad69e3bf5bcd68c5bfae1e

net/ice: revert count drop-all filter in Rx stats
This reverts commit af05f9e37153c53335938bcde2978a359c5f4efa

This patch caused testpmd to count packets not destined for our MAC
even when promiscuous mode is off.This behaviour is inconsistent with
other drivers and leads to incorrect Rx statistics.

Fixes: af05f9e ("net/ice: count drop-all filter in Rx stats")
Cc: stable@dpdk.org

Signed-off-by: Shaiq Wani <shaiq.wani@intel.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/intel/ice/ice_ethdev.c
@@ -6353,13 +6353,12 @@ ice_stats_get(struct rte_eth_dev *dev, struct rte_eth_stats *stats)
 
 	stats->ipackets = pf->main_vsi->eth_stats.rx_unicast +
 			  pf->main_vsi->eth_stats.rx_multicast +
-			  ns->eth.rx_unknown_protocol +
 			  pf->main_vsi->eth_stats.rx_broadcast -
 			  pf->main_vsi->eth_stats.rx_discards;
 	stats->opackets = ns->eth.tx_unicast +
 			  ns->eth.tx_multicast +
 			  ns->eth.tx_broadcast;
-	stats->ibytes   = ns->eth.rx_bytes;
+	stats->ibytes   = pf->main_vsi->eth_stats.rx_bytes;
 	stats->obytes   = ns->eth.tx_bytes;
 	stats->oerrors  = ns->eth.tx_errors +
 			  pf->main_vsi->eth_stats.tx_errors;