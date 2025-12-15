ovsrobot/dpdk

commit eba657ae3c3c4e2f210229f6e7f2e6af4215ceee

dts: move testpmd module from framework to API directory
Testpmd moved into new API directory.
Capabilities converted into vanilla enum and moved to API.
Removed some circular dependencies and incorrect imports.

Changed tests to import the new API modules,
use the new capability decorators
and use the new TestPmd name.

Signed-off-by: Paul Szczepanek <paul.szczepanek@arm.com>
Reviewed-by: Luca Vizzarro <luca.vizzarro@arm.com>
Reviewed-by: Patrick Robb <probb@iol.unh.edu>

dts/api/capabilities.py
@@ -1,0 +1,180 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2024 PANTHEON.tech s.r.o.
+# Copyright(c) 2025 Arm Limited
+
+"""Testbed capabilities.
+
+This module provides a protocol that defines the common attributes of test cases and suites
+and support for test environment capabilities.
+
+Many test cases are testing features not available on all hardware.
+On the other hand, some test cases or suites may not need the most complex topology available.
+
+The module allows developers to mark test cases or suites to require certain hardware capabilities
+or a particular topology.
+
+There are differences between hardware and topology capabilities:
+
+    * Hardware capabilities are assumed to not be required when not specified.
+    * However, some topology is always available, so each test case or suite is assigned
+      a default topology if no topology is specified in the decorator.
+
+Examples:
+    .. code:: python
+
+        from framework.test_suite import TestSuite, func_test
+        from framework.testbed_model.capability import LinkTopology, requires_link_topology
+        # The whole test suite (each test case within) doesn't require any links.
+        @requires_link_topology(LinkTopology.NO_LINK)
+        @func_test
+        class TestHelloWorld(TestSuite):
+            def hello_world_single_core(self):
+            ...
+
+    .. code:: python
+
+        from framework.test_suite import TestSuite, func_test
+        from framework.testbed_model.capability import NicCapability, requires_nic_capability
+        class TestPmdBufferScatter(TestSuite):
+            # only the test case requires the SCATTERED_RX_ENABLED capability
+            # other test cases may not require it
+            @requires_nic_capability(NicCapability.SCATTERED_RX_ENABLED)
+            @func_test
+            def test_scatter_mbuf_2048(self):
+"""
+
+from enum import IntEnum, auto
+from typing import TYPE_CHECKING, Callable
+
+if TYPE_CHECKING:
+    from framework.test_suite import TestProtocol
+
+
+class LinkTopology(IntEnum):
+    """Supported topology types."""
+
+    #: A topology with no Traffic Generator.
+    NO_LINK = 0
+    #: A topology with one physical link between the SUT node and the TG node.
+    ONE_LINK = auto()
+    #: A topology with two physical links between the Sut node and the TG node.
+    TWO_LINKS = auto()
+
+    @classmethod
+    def default(cls) -> "LinkTopology":
+        """The default topology required by test cases if not specified otherwise."""
+        return cls.TWO_LINKS
+
+
+class NicCapability(IntEnum):
+    """DPDK NIC capabilities.
+
+    The capabilities are used to mark test cases or suites that require a specific
+    DPDK NIC capability to run. The capabilities are used by the test framework to
+    determine whether a test case or suite can be run on the current testbed.
+    """
+
+    #: Scattered packets Rx enabled.
+    SCATTERED_RX_ENABLED = 0
+    #: Device supports VLAN stripping.
+    RX_OFFLOAD_VLAN_STRIP = auto()
+    #: Device supports L3 checksum offload.
+    RX_OFFLOAD_IPV4_CKSUM = auto()
+    #: Device supports L4 checksum offload.
+    RX_OFFLOAD_UDP_CKSUM = auto()
+    #: Device supports L4 checksum offload.
+    RX_OFFLOAD_TCP_CKSUM = auto()
+    #: Device supports Large Receive Offload.
+    RX_OFFLOAD_TCP_LRO = auto()
+    #: Device supports QinQ (queue in queue) offload.
+    RX_OFFLOAD_QINQ_STRIP = auto()
+    #: Device supports inner packet L3 checksum.
+    RX_OFFLOAD_OUTER_IPV4_CKSUM = auto()
+    #: Device supports MACsec.
+    RX_OFFLOAD_MACSEC_STRIP = auto()
+    #: Device supports filtering of a VLAN Tag identifier.
+    RX_OFFLOAD_VLAN_FILTER = auto()
+    #: Device supports VLAN offload.
+    RX_OFFLOAD_VLAN_EXTEND = auto()
+    #: Device supports receiving segmented mbufs.
+    RX_OFFLOAD_SCATTER = auto()
+    #: Device supports Timestamp.
+    RX_OFFLOAD_TIMESTAMP = auto()
+    #: Device supports crypto processing while packet is received in NIC.
+    RX_OFFLOAD_SECURITY = auto()
+    #: Device supports CRC stripping.
+    RX_OFFLOAD_KEEP_CRC = auto()
+    #: Device supports L4 checksum offload.
+    RX_OFFLOAD_SCTP_CKSUM = auto()
+    #: Device supports inner packet L4 checksum.
+    RX_OFFLOAD_OUTER_UDP_CKSUM = auto()
+    #: Device supports RSS hashing.
+    RX_OFFLOAD_RSS_HASH = auto()
+    #: Device supports scatter Rx packets to segmented mbufs.
+    RX_OFFLOAD_BUFFER_SPLIT = auto()
+    #: Device supports all checksum capabilities.
+    RX_OFFLOAD_CHECKSUM = auto()
+    #: Device supports all VLAN capabilities.
+    RX_OFFLOAD_VLAN = auto()
+    #: Device supports Rx queue setup after device started.
+    RUNTIME_RX_QUEUE_SETUP = auto()
+    #: Device supports Tx queue setup after device started.
+    RUNTIME_TX_QUEUE_SETUP = auto()
+    #: Device supports shared Rx queue among ports within Rx domain and switch domain.
+    RXQ_SHARE = auto()
+    #: Device supports keeping flow rules across restart.
+    FLOW_RULE_KEEP = auto()
+    #: Device supports keeping shared flow objects across restart.
+    FLOW_SHARED_OBJECT_KEEP = auto()
+    #: Device supports multicast address filtering.
+    MCAST_FILTERING = auto()
+    #: Device supports flow ctrl.
+    FLOW_CTRL = auto()
+    #: Device is running on a physical function.
+    PHYSICAL_FUNCTION = auto()
+
+
+def requires_link_topology(
+    link_topology: LinkTopology,
+) -> Callable[[type["TestProtocol"]], type["TestProtocol"]]:
+    """Decorator to set required topology type for a test case or test suite.
+
+    Args:
+        link_topology: The topology type the test suite or case requires.
+
+    Returns:
+        The decorated test case or test suite.
+    """
+    from framework.testbed_model.capability import TopologyCapability
+
+    def add_required_topology(
+        test_case_or_suite: type["TestProtocol"],
+    ) -> type["TestProtocol"]:
+        topology_capability = TopologyCapability.get_unique(link_topology)
+        topology_capability.set_required(test_case_or_suite)
+        return test_case_or_suite
+
+    return add_required_topology
+
+
+def requires_nic_capability(
+    nic_capability: NicCapability,
+) -> Callable[[type["TestProtocol"]], type["TestProtocol"]]:
+    """Decorator to add a single required NIC capability to a test case or test suite.
+
+    Args:
+        nic_capability: The NIC capability that is required by the test case or test suite.
+
+    Returns:
+        The decorated test case or test suite.
+    """
+    from framework.testbed_model.capability import DecoratedNicCapability
+
+    def add_required_capability(
+        test_case_or_suite: type["TestProtocol"],
+    ) -> type["TestProtocol"]:
+        decorated = DecoratedNicCapability.get_unique(nic_capability)
+        decorated.add_to_required(test_case_or_suite)
+        return test_case_or_suite
+
+    return add_required_capability

dts/api/__init__.py
@@ -1,0 +1,14 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2025 Arm Limited
+
+"""DTS API.
+
+This package exposes public API modules for test writers.
+
+All modules in this package are considered stable.
+Do not use modules from the framework directory in your tests.
+Any missing functionality should be added to the public API.
+
+Private methods and members are prefixed with an underscore
+and should not be used outside of the framework.
+"""

dts/api/testpmd/config.py
@@ -1,7 +1,12 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright(c) 2024 Arm Limited
 
-"""Module containing all the TestPmd-related parameter classes."""
+"""Module containing all classes needed to configure :class:`TestPmd`.
+
+This module defines the :class:`TestPmdParams` class which is used to configure the
+TestPmd shell. It also includes various data classes and enums that are used
+to represent different configurations and settings.
+"""
 
 from dataclasses import dataclass, field
 from enum import EnumMeta, Flag, auto, unique
@@ -146,7 +151,7 @@ class RSSSetting(EnumMeta):
 
 
 class SimpleForwardingModes(StrEnum):
-    r"""The supported packet forwarding modes for :class:`~TestPmdShell`\s."""
+    r"""The supported packet forwarding modes for :class:`~TestPmd`\s."""
 
     #:
     io = auto()

dts/framework/config/__init__.py
@@ -28,7 +28,6 @@ Nearly all of them are frozen:
       and makes it thread safe should we ever want to move in that direction.
 """
 
-import os
 from pathlib import Path
 from typing import TYPE_CHECKING, Annotated, Any, Literal, TypeVar, cast
 
@@ -43,7 +42,7 @@ from .node import NodeConfiguration
 from .test_run import TestRunConfiguration, create_test_suites_config_model
 
 # Import only if type checking or building docs, to prevent circular imports.
-if TYPE_CHECKING or os.environ.get("DTS_DOC_BUILD"):
+if TYPE_CHECKING:
     from framework.test_suite import BaseConfig
 
 NodesConfig = Annotated[list[NodeConfiguration], Field(min_length=1)]

dts/framework/params/eal.py
@@ -4,15 +4,17 @@
 """Module representing the DPDK EAL-related parameters."""
 
 from dataclasses import dataclass, field
-from typing import Literal
+from typing import TYPE_CHECKING, Literal
 
 from framework.params import Params, Switch
 from framework.testbed_model.cpu import LogicalCoreList
-from framework.testbed_model.port import Port
 from framework.testbed_model.virtual_device import VirtualDevice
 
+if TYPE_CHECKING:
+    from framework.testbed_model.port import Port
 
-def _port_to_pci(port: Port) -> str:
+
+def _port_to_pci(port: "Port") -> str:
     return port.pci
 
 
@@ -42,11 +44,11 @@ class EalParams(Params):
     vdevs: list[VirtualDevice] | None = field(
         default=None, metadata=Params.multiple() | Params.long("vdev")
     )
-    allowed_ports: list[Port] | None = field(
+    allowed_ports: list["Port"] | None = field(
         default=None,
         metadata=Params.convert_value(_port_to_pci) | Params.multiple() | Params.short("a"),
     )
-    blocked_ports: list[Port] | None = field(
+    blocked_ports: list["Port"] | None = field(
         default=None,
         metadata=Params.convert_value(_port_to_pci) | Params.multiple() | Params.short("b"),
     )

dts/framework/params/types.py
@@ -15,8 +15,7 @@ Example:
 from pathlib import PurePath
 from typing import TypedDict
 
-from framework.params import Switch, YesNoSwitch
-from framework.params.testpmd import (
+from api.testpmd.config import (
     AnonMempoolAllocationMode,
     EthPeer,
     Event,
@@ -37,6 +36,7 @@ from framework.params.testpmd import (
     TXRingParams,
     TxUDPPortPair,
 )
+from framework.params import Switch, YesNoSwitch
 from framework.testbed_model.cpu import LogicalCoreList
 from framework.testbed_model.port import Port
 from framework.testbed_model.virtual_device import VirtualDevice

commit a381e8f95513d96ee61b234cd8c7a1a35cd2dbbd

dts: show missing NIC capabilities in DTS logs
When a test was skipped due to a missing NIC capability, the logs should
print the name of the missing capability. This has since changed to show
only the integer representation of the missing capability. This patch
restores the previous behavior of printing the name of the missing
capability to the user.

Bugzilla ID: 1803
Fixes: eba657ae3c3c ("dts: move testpmd module from framework to API
directory")

Signed-off-by: Andrew Bailey <abailey@iol.unh.edu>
Signed-off-by: 0-day Robot <robot@bytheb.org>

dts/api/capabilities.py
@@ -219,6 +219,10 @@ class NicCapability(IntEnum):
     PORT_TX_OFFLOAD_SEND_ON_TIMESTAMP = auto()
     QUEUE_TX_OFFLOAD_SEND_ON_TIMESTAMP = auto()
 
+    def __str__(self):
+        """Override the default string representation to return the name of the capability."""
+        return self.name
+
 
 def requires_link_topology(
     link_topology: LinkTopology,