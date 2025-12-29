ovsrobot/dpdk

commit b526903f308ed5a8bac5ec0878e74cfb1669122d

net/sfc: offer support for 200G link ability on new adaptors
New adaptors (for instance, Medford4) can support 200G speed.

Signed-off-by: Ivan Malov <ivan.malov@arknetworks.am>
Reviewed-by: Andy Moreton <andy.moreton@amd.com>
Reviewed-by: Pieter Jansen Van Vuuren <pieter.jansen-van-vuuren@amd.com>

drivers/net/sfc/sfc_ethdev.c
@@ -117,6 +117,8 @@ sfc_dev_infos_get(struct rte_eth_dev *dev, struct rte_eth_dev_info *dev_info)
 		dev_info->speed_capa |= RTE_ETH_LINK_SPEED_50G;
 	if (sa->port.phy_adv_cap_mask & (1u << EFX_PHY_CAP_100000FDX))
 		dev_info->speed_capa |= RTE_ETH_LINK_SPEED_100G;
+	if (sa->port.phy_adv_cap_mask & (1u << EFX_PHY_CAP_200000FDX))
+		dev_info->speed_capa |= RTE_ETH_LINK_SPEED_200G;
 
 	dev_info->max_rx_queues = sa->rxq_max;
 	dev_info->max_tx_queues = sa->txq_max;
@@ -2432,6 +2434,19 @@ sfc_fec_get_capa_speed_to_fec(uint32_t supported_caps,
 	if (supported_caps & (1u << EFX_PHY_CAP_100000FDX)) {
 		if (speed_fec_capa != NULL) {
 			speed_fec_capa[num].speed = RTE_ETH_SPEED_NUM_100G;
+			speed_fec_capa[num].capa =
+				RTE_ETH_FEC_MODE_CAPA_MASK(NOFEC) |
+				RTE_ETH_FEC_MODE_CAPA_MASK(AUTO);
+			if (rs) {
+				speed_fec_capa[num].capa |=
+					RTE_ETH_FEC_MODE_CAPA_MASK(RS);
+			}
+		}
+		num++;
+	}
+	if (supported_caps & (1u << EFX_PHY_CAP_200000FDX)) {
+		if (speed_fec_capa != NULL) {
+			speed_fec_capa[num].speed = RTE_ETH_SPEED_NUM_200G;
 			speed_fec_capa[num].capa =
 				RTE_ETH_FEC_MODE_CAPA_MASK(NOFEC) |
 				RTE_ETH_FEC_MODE_CAPA_MASK(AUTO);

drivers/net/sfc/sfc.c
@@ -126,7 +126,8 @@ sfc_phy_cap_from_link_speeds(uint32_t speeds)
 				(1 << EFX_PHY_CAP_25000FDX) |
 				(1 << EFX_PHY_CAP_40000FDX) |
 				(1 << EFX_PHY_CAP_50000FDX) |
-				(1 << EFX_PHY_CAP_100000FDX);
+				(1 << EFX_PHY_CAP_100000FDX) |
+				(1 << EFX_PHY_CAP_200000FDX);
 	}
 	if (speeds & RTE_ETH_LINK_SPEED_1G)
 		phy_caps |= (1 << EFX_PHY_CAP_1000FDX);
@@ -140,6 +141,8 @@ sfc_phy_cap_from_link_speeds(uint32_t speeds)
 		phy_caps |= (1 << EFX_PHY_CAP_50000FDX);
 	if (speeds & RTE_ETH_LINK_SPEED_100G)
 		phy_caps |= (1 << EFX_PHY_CAP_100000FDX);
+	if (speeds & RTE_ETH_LINK_SPEED_200G)
+		phy_caps |= (1 << EFX_PHY_CAP_200000FDX);
 
 	return phy_caps;
 }

drivers/net/sfc/sfc_port.c
@@ -144,6 +144,8 @@ sfc_port_init_dev_link(struct sfc_adapter *sa)
 static efx_link_mode_t
 sfc_port_phy_caps_to_max_link_speed(uint32_t phy_caps)
 {
+	if (phy_caps & (1u << EFX_PHY_CAP_200000FDX))
+		return EFX_LINK_200000FDX;
 	if (phy_caps & (1u << EFX_PHY_CAP_100000FDX))
 		return EFX_LINK_100000FDX;
 	if (phy_caps & (1u << EFX_PHY_CAP_50000FDX))
@@ -647,6 +649,10 @@ sfc_port_link_mode_to_info(efx_link_mode_t link_mode,
 		break;
 	case EFX_LINK_100000FDX:
 		link_info->link_speed  = RTE_ETH_SPEED_NUM_100G;
+		link_info->link_duplex = RTE_ETH_LINK_FULL_DUPLEX;
+		break;
+	case EFX_LINK_200000FDX:
+		link_info->link_speed  = RTE_ETH_SPEED_NUM_200G;
 		link_info->link_duplex = RTE_ETH_LINK_FULL_DUPLEX;
 		break;
 	default:

commit a5b545090a8a55c33ea4ace6b8202aa4d91e1133

net/sfc: drop AUTO from FEC capabilities and fix the comment
AUTO is not a capability. It is rather a special value. Do not indicate it.
Also, improve the comment to explain this change and the overall principle.

Fixes: b526903f308e ("net/sfc: offer support for 200G link ability on new adaptors")
Fixes: 5efa8fc1cfc0 ("net/sfc: support FEC feature")
Cc: stable@dpdk.org

Signed-off-by: Ivan Malov <ivan.malov@arknetworks.am>
Reviewed-by: Andy Moreton <andy.moreton@amd.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/net/sfc/sfc_ethdev.c
@@ -2517,18 +2517,20 @@ sfc_fec_get_capa_speed_to_fec(uint32_t supported_caps,
 		rs = true;
 
 	/*
-	 * NOFEC and AUTO FEC modes are always supported.
-	 * FW does not provide information about the supported
-	 * FEC modes per the link speed.
-	 * Supported FEC depends on supported link speeds and
-	 * supported FEC modes by a device.
+	 * NOFEC can always be requested, but if the link technology for the
+	 * intended speed mandates FEC, doing so will not bring the link up.
+	 *
+	 * FW cannot report supported FEC modes per speed/link technology,
+	 * so use best-effort approximation to fill in the capabilities.
+	 *
+	 * Do not indicate 'AUTO'. This bit is reserved for user
+	 * input. It has nothing to do with capability reporting.
 	 */
 	if (supported_caps & (1u << EFX_PHY_CAP_10000FDX)) {
 		if (speed_fec_capa != NULL) {
 			speed_fec_capa[num].speed = RTE_ETH_SPEED_NUM_10G;
 			speed_fec_capa[num].capa =
-				RTE_ETH_FEC_MODE_CAPA_MASK(NOFEC) |
-				RTE_ETH_FEC_MODE_CAPA_MASK(AUTO);
+				RTE_ETH_FEC_MODE_CAPA_MASK(NOFEC);
 			if (baser) {
 				speed_fec_capa[num].capa |=
 					RTE_ETH_FEC_MODE_CAPA_MASK(BASER);
@@ -2540,8 +2542,7 @@ sfc_fec_get_capa_speed_to_fec(uint32_t supported_caps,
 		if (speed_fec_capa != NULL) {
 			speed_fec_capa[num].speed = RTE_ETH_SPEED_NUM_25G;
 			speed_fec_capa[num].capa =
-				RTE_ETH_FEC_MODE_CAPA_MASK(NOFEC) |
-				RTE_ETH_FEC_MODE_CAPA_MASK(AUTO);
+				RTE_ETH_FEC_MODE_CAPA_MASK(NOFEC);
 			if (baser) {
 				speed_fec_capa[num].capa |=
 					RTE_ETH_FEC_MODE_CAPA_MASK(BASER);
@@ -2557,8 +2558,7 @@ sfc_fec_get_capa_speed_to_fec(uint32_t supported_caps,
 		if (speed_fec_capa != NULL) {
 			speed_fec_capa[num].speed = RTE_ETH_SPEED_NUM_40G;
 			speed_fec_capa[num].capa =
-				RTE_ETH_FEC_MODE_CAPA_MASK(NOFEC) |
-				RTE_ETH_FEC_MODE_CAPA_MASK(AUTO);
+				RTE_ETH_FEC_MODE_CAPA_MASK(NOFEC);
 			if (baser) {
 				speed_fec_capa[num].capa |=
 					RTE_ETH_FEC_MODE_CAPA_MASK(BASER);
@@ -2570,8 +2570,7 @@ sfc_fec_get_capa_speed_to_fec(uint32_t supported_caps,
 		if (speed_fec_capa != NULL) {
 			speed_fec_capa[num].speed = RTE_ETH_SPEED_NUM_50G;
 			speed_fec_capa[num].capa =
-				RTE_ETH_FEC_MODE_CAPA_MASK(NOFEC) |
-				RTE_ETH_FEC_MODE_CAPA_MASK(AUTO);
+				RTE_ETH_FEC_MODE_CAPA_MASK(NOFEC);
 			if (baser) {
 				speed_fec_capa[num].capa |=
 					RTE_ETH_FEC_MODE_CAPA_MASK(BASER);
@@ -2587,8 +2586,7 @@ sfc_fec_get_capa_speed_to_fec(uint32_t supported_caps,
 		if (speed_fec_capa != NULL) {
 			speed_fec_capa[num].speed = RTE_ETH_SPEED_NUM_100G;
 			speed_fec_capa[num].capa =
-				RTE_ETH_FEC_MODE_CAPA_MASK(NOFEC) |
-				RTE_ETH_FEC_MODE_CAPA_MASK(AUTO);
+				RTE_ETH_FEC_MODE_CAPA_MASK(NOFEC);
 			if (rs) {
 				speed_fec_capa[num].capa |=
 					RTE_ETH_FEC_MODE_CAPA_MASK(RS);
@@ -2600,8 +2598,7 @@ sfc_fec_get_capa_speed_to_fec(uint32_t supported_caps,
 		if (speed_fec_capa != NULL) {
 			speed_fec_capa[num].speed = RTE_ETH_SPEED_NUM_200G;
 			speed_fec_capa[num].capa =
-				RTE_ETH_FEC_MODE_CAPA_MASK(NOFEC) |
-				RTE_ETH_FEC_MODE_CAPA_MASK(AUTO);
+				RTE_ETH_FEC_MODE_CAPA_MASK(NOFEC);
 			if (rs) {
 				speed_fec_capa[num].capa |=
 					RTE_ETH_FEC_MODE_CAPA_MASK(RS);