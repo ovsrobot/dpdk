d4b2737aa1c9ff93c669bba558bde0f4d90fc5d5
Stephen Hemminger <stephen@networkplumber.org>
2026-01-18 08:58:39 -0800

    net/pcap: support setting MTU
    If PCAP PMD is used in single interface mode, it can pass
    the MTU setup to the device. Only implemented on
    Linux and FreeBSD.
    
    Signed-off-by: Stephen Hemminger <stephen@networkplumber.org>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/pcap/pcap_osdep_freebsd.c
+++ drivers/net/pcap/pcap_osdep_freebsd.c
@@ -4,9 +4,12 @@
  * All rights reserved.
  */
 
+#include <unistd.h>
 #include <net/if.h>
 #include <net/if_dl.h>
 #include <sys/sysctl.h>
+#include <sys/ioctl.h>
+#include <sys/sockio.h>
 
 #include "pcap_osdep.h"
 
@@ -53,4 +56,27 @@ osdep_iface_mac_get(const char *if_name, struct rte_ether_addr *mac)
 
 	free(buf);
 	return 0;
+}
+
+int
+osdep_iface_mtu_set(int ifindex, uint16_t mtu)
+{
+	struct ifreq ifr = { 0 };
+	char ifname[IFNAMSIZ];
+	int s, ret;
+
+	if (if_indextoname(ifindex, ifname) == NULL)
+		return -errno;
+
+	s = socket(AF_INET, SOCK_DGRAM, 0);
+	if (s < 0)
+		return -errno;
+
+	strlcpy(ifr.ifr_name, ifname, IFNAMSIZ);
+	ifr.ifr_mtu = mtu;
+
+	ret = ioctl(s, SIOCSIFMTU, &ifr);
+	close(s);
+
+	return (ret < 0) ? -errno : 0;
 }
