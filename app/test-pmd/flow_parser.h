53d56e44a1dea52a403786b24277149838d87697
Lukas Sismis <sismis@dyna-nic.com>
2026-01-21 00:24:21 +0100

    app/testpmd: use shared flow parser library
    Switch testpmd to use the shared librte_flow_parser library instead
    of the embedded cmdline_flow.c implementation.
    
    Changes:
    - Remove app/test-pmd/cmdline_flow.c (moved to library)
    - Add flow_parser.c: Implements flow operation callbacks that bridge
      the parser library to testpmd's port/flow management functions
    - Add flow_parser_cli.c: Hosts cmdline integration for 'flow' and
      'set raw_encap/decap' commands
    - Update cmdline.c to register flow commands with the parser
    - Update meson.build to link against flow_parser library
    
    The testpmd flow command syntax remains unchanged. All existing
    flow commands continue to work identically.
    
    Signed-off-by: Lukas Sismis <sismis@dyna-nic.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ app/test-pmd/flow_parser.h
@@ -0,0 +1,8 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+
+#ifndef _TESTPMD_FLOW_PARSER_H_
+#define _TESTPMD_FLOW_PARSER_H_
+
+#include <rte_flow_parser_internal.h>
+
+#endif /* _TESTPMD_FLOW_PARSER_H_ */
