0afedc1db0bcbddff260bcd75fb9f452fd5b963a
Thomas Monjalon <thomas@monjalon.net>
2026-01-28 17:20:52 +0100

    license: standardize SPDX tag
    In the Linux Foundation SPDX recommendation,
    there is no need of parentheses when 2 licenses are possible:
    	https://spdx.dev/learn/handling-license-info/
    
    The useless parentheses and spaces are removed,
    the keyword "OR" is uppercased, and the comment starts with /*
    
    The script check-spdx-tag.sh is updated to detect some syntax errors.
    
    Signed-off-by: Thomas Monjalon <thomas@monjalon.net>
    Acked-by: Bruce Richardson <bruce.richardson@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- kernel/linux/uapi/linux/vduse.h
+++ kernel/linux/uapi/linux/vduse.h
@@ -1,4 +1,5 @@
-/* SPDX-License-Identifier: ((GPL-2.0 WITH Linux-syscall-note) OR BSD-3-Clause) */
+/* SPDX-License-Identifier: (GPL-2.0 WITH Linux-syscall-note) OR BSD-3-Clause */
+
 #ifndef _VDUSE_H_
 #define _VDUSE_H_
 
