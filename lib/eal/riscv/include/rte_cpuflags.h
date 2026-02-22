249c4d6ac15f19d40c0b0c49c925d10799157640
Daniel Gregory <daniel.gregory@bytedance.com>
2026-02-22 16:29:55 +0100

    config/riscv: detect presence of Zbc extension
    The RISC-V Zbc extension adds carry-less multiply instructions we can
    use to implement more efficient CRC hashing algorithms.
    
    The RISC-V C api defines Zbc intrinsics we can use rather than inline
    assembly on newer versions of GCC (14.1.0+) and Clang (18.1.0+). We can
    detect whether these are supported on the compiler and -march we're
    building with by using the architecture extension macros in combination
    with checking compiler version.
    
    The Linux kernel exposes a RISC-V hardware probing syscall for getting
    information about the system at run-time including which extensions are
    available. We detect whether this interface is present by looking for
    the <asm/hwprobe.h> header, as it's only present in newer kernels
    (v6.4+). Furthermore, support for detecting certain extensions,
    including Zbc, wasn't present until versions after this, so we need to
    check the constants this header exports.
    
    The kernel exposes bitmasks for each extension supported by the probing
    interface, rather than the bit index that is set if that extensions is
    present, so modify the existing cpu flag HWCAP table entries to line up
    with this. The values returned by the interface are 64-bits long, so
    grow the hwcap registers array to be able to hold them.
    
    If the Zbc extension and intrinsics are both present and we can detect
    the Zbc extension at runtime, we define a flag, RTE_RISCV_FEATURE_ZBC.
    
    Signed-off-by: Daniel Gregory <daniel.gregory@bytedance.com>
    Signed-off-by: Daniel Gregory <code@danielg0.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- lib/eal/riscv/include/rte_cpuflags.h
+++ lib/eal/riscv/include/rte_cpuflags.h
@@ -38,6 +38,8 @@ enum rte_cpu_flag_t {
 	RTE_CPUFLAG_RISCV_ISA_X, /* Non-standard extension present */
 	RTE_CPUFLAG_RISCV_ISA_Y, /* Reserved */
 	RTE_CPUFLAG_RISCV_ISA_Z, /* Reserved */
+
+	RTE_CPUFLAG_RISCV_EXT_ZBC, /* Carry-less multiplication */
 };
 
 #include "generic/rte_cpuflags.h"
