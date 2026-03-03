a22f0650885970f132487ce748878ed8420461b9
Jie Liu <liujie5@linkdatatechnology.com>
2026-03-03 17:10:48 +0800

    net/sxe: add link and mac layer operations
    Add link and mac layer operations.
    
    Signed-off-by: Jie Liu <liujie5@linkdatatechnology.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/sxe/pf/sxe.h
+++ drivers/net/sxe/pf/sxe.h
@@ -6,16 +6,18 @@
 
 #include <rte_pci.h>
 #include <rte_time.h>
-
 #include <stdbool.h>
 #include "sxe_types.h"
 #include "sxe_filter.h"
 #include "sxe_irq.h"
 #include "sxe_stats.h"
 #include "sxe_phy.h"
+#include "sxe_vf.h"
+#include "sxe_dcb.h"
 #include "sxe_hw.h"
 
 struct sxe_hw;
+struct sxe_vlan_context;
 
 #define SXE_LPBK_DISABLED   0x0
 #define SXE_LPBK_ENABLED	0x1
@@ -41,13 +43,45 @@ struct sxe_hw;
 #define rte_sxe_prefetch(p)   do {} while (0)
 #endif
 
+struct sxe_ptp_context {
+	struct rte_timecounter	  systime_tc;
+	struct rte_timecounter	  rx_tstamp_tc;
+	struct rte_timecounter	  tx_tstamp_tc;
+	u32 tx_hwtstamp_sec;
+	u32 tx_hwtstamp_nsec;
+};
+
 struct sxe_adapter {
 	struct sxe_hw hw;
 
 	struct sxe_irq_context irq_ctxt;
 
+	struct sxe_vlan_context vlan_ctxt;
+	struct sxe_mac_filter_context mac_filter_ctxt;
+#ifdef RTE_ADAPTER_HAVE_FNAV_CONF
+	struct rte_eth_fdir_conf fnav_conf;
+#endif
+	struct sxe_ptp_context ptp_ctxt;
+	struct sxe_phy_context phy_ctxt;
+	struct sxe_virtual_context vt_ctxt;
+
+	struct sxe_stats_info stats_info;
+	struct sxe_dcb_context dcb_ctxt;
+
 	bool rx_batch_alloc_allowed;
+#if defined SXE_DPDK_L4_FEATURES && defined SXE_DPDK_SIMD
+	bool rx_vec_allowed;
+#endif
 	s8 name[PCI_PRI_STR_SIZE + 1];
+
+	u32 mtu;
+
+	bool rss_reta_updated;
+
+	RTE_ATOMIC(bool) link_thread_running;
+	RTE_ATOMIC(bool) is_stopping;
+	rte_thread_t link_thread_tid;
+	bool is_stopped;
 };
 
 s32 sxe_hw_reset(struct sxe_hw *hw);
