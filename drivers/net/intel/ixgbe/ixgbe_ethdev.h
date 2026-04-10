07af9bcfea6d92794da66f362cf8b3691e6cc85d
Anatoly Burakov <anatoly.burakov@intel.com>
2026-04-10 14:12:56 +0100

    net/ixgbe: store max VFs in adapter
    Currently, rte_flow-related checks use `rte_eth_dev` to check for max VFs.
    With coming rework of flows, the aim is to make the code as multiprocess
    agnostic as possible, and `rte_eth_dev` pointer is process-local.
    
    To support this in VF-related checks, cache max_vfs in ixgbe adapter
    during device init and read it in the rte_flow check paths to avoid direct
    dependency on `rte_eth_dev`.
    
    Signed-off-by: Anatoly Burakov <anatoly.burakov@intel.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/net/intel/ixgbe/ixgbe_ethdev.h
+++ drivers/net/intel/ixgbe/ixgbe_ethdev.h
@@ -491,6 +491,7 @@ struct ixgbe_adapter {
 
 	/* Used for limiting SDP3 TX_DISABLE checks */
 	uint8_t sdp3_no_tx_disable;
+	uint16_t max_vfs;
 
 	/* Used for VF link sync with PF's physical and logical (by checking
 	 * mailbox status) link status.
@@ -514,6 +515,9 @@ uint16_t ixgbe_vf_representor_tx_burst(void *tx_queue, struct rte_mbuf **tx_pkts
 
 #define IXGBE_DEV_FDIR_CONF(dev) \
 	(&((struct ixgbe_adapter *)(dev)->data->dev_private)->fdir_conf)
+
+#define IXGBE_DEV_PRIVATE_TO_ADAPTER(adapter) \
+	((struct ixgbe_adapter *)adapter)
 
 #define IXGBE_DEV_PRIVATE_TO_HW(adapter)\
 	(&((struct ixgbe_adapter *)adapter)->hw)
