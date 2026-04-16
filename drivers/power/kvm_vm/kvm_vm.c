ovsrobot/dpdk

commit 6f987b594fa6751b49769755fe1d1bf9f9d15ac4

power: refactor core power management
This patch introduces a comprehensive refactor to the core power
management library. The primary focus is on improving modularity
and organization by relocating specific driver implementations
from the 'lib/power' directory to dedicated directories within
'drivers/power/*'. The adjustment of meson.build files
enables the selective activation of individual drivers.

These changes contribute to a significant enhancement in code
organization, providing a clearer structure for driver implementations.
The refactor aims to improve overall code clarity and boost
maintainability. Additionally, it establishes a foundation for
future development, allowing for more focused work on individual
drivers and seamless integration of forthcoming enhancements.

Signed-off-by: Sivaprasad Tummala <sivaprasad.tummala@amd.com>
Acked-by: Huisong Li <lihuisong@huawei.com>

drivers/power/kvm_vm/kvm_vm.c
@@ -9,7 +9,7 @@
 #include "rte_power_guest_channel.h"
 #include "guest_channel.h"
 #include "power_common.h"
-#include "power_kvm_vm.h"
+#include "kvm_vm.h"
 
 #define FD_PATH "/dev/virtio-ports/virtio.serial.port.poweragent"
 
@@ -137,3 +137,23 @@ int power_kvm_vm_get_capabilities(__rte_unused unsigned int lcore_id,
 	POWER_LOG(ERR, "rte_power_get_capabilities is not implemented for Virtual Machine Power Management");
 	return -ENOTSUP;
 }
+
+static struct rte_power_cpufreq_ops kvm_vm_ops = {
+	.name = "kvm-vm",
+	.init = power_kvm_vm_init,
+	.exit = power_kvm_vm_exit,
+	.check_env_support = power_kvm_vm_check_supported,
+	.get_avail_freqs = power_kvm_vm_freqs,
+	.get_freq = power_kvm_vm_get_freq,
+	.set_freq = power_kvm_vm_set_freq,
+	.freq_down = power_kvm_vm_freq_down,
+	.freq_up = power_kvm_vm_freq_up,
+	.freq_max = power_kvm_vm_freq_max,
+	.freq_min = power_kvm_vm_freq_min,
+	.turbo_status = power_kvm_vm_turbo_status,
+	.enable_turbo = power_kvm_vm_enable_turbo,
+	.disable_turbo = power_kvm_vm_disable_turbo,
+	.get_caps = power_kvm_vm_get_capabilities
+};
+
+RTE_POWER_REGISTER_CPUFREQ_OPS(kvm_vm_ops);

MAINTAINERS
@@ -1768,6 +1768,7 @@ M: David Hunt <david.hunt@intel.com>
 M: Sivaprasad Tummala <sivaprasad.tummala@amd.com>
 F: lib/power/
 F: doc/guides/prog_guide/power_man.rst
+F: drivers/power/
 F: app/test/test_power*
 F: examples/l3fwd-power/
 F: doc/guides/sample_app_ug/l3_forward_power_man.rst

drivers/meson.build
@@ -29,6 +29,7 @@ subdirs = [
         'event',          # depends on common, bus, mempool and net.
         'baseband',       # depends on common and bus.
         'gpu',            # depends on common and bus.
+        'power',
 ]
 
 if meson.is_cross_build()

drivers/power/acpi/acpi_cpufreq.c
@@ -10,7 +10,7 @@
 #include <rte_stdatomic.h>
 #include <rte_string_fns.h>
 
-#include "power_acpi_cpufreq.h"
+#include "acpi_cpufreq.h"
 #include "power_common.h"
 
 #define STR_SIZE     1024
@@ -587,3 +587,23 @@ int power_acpi_get_capabilities(unsigned int lcore_id,
 
 	return 0;
 }
+
+static struct rte_power_cpufreq_ops acpi_ops = {
+	.name = "acpi",
+	.init = power_acpi_cpufreq_init,
+	.exit = power_acpi_cpufreq_exit,
+	.check_env_support = power_acpi_cpufreq_check_supported,
+	.get_avail_freqs = power_acpi_cpufreq_freqs,
+	.get_freq = power_acpi_cpufreq_get_freq,
+	.set_freq = power_acpi_cpufreq_set_freq,
+	.freq_down = power_acpi_cpufreq_freq_down,
+	.freq_up = power_acpi_cpufreq_freq_up,
+	.freq_max = power_acpi_cpufreq_freq_max,
+	.freq_min = power_acpi_cpufreq_freq_min,
+	.turbo_status = power_acpi_turbo_status,
+	.enable_turbo = power_acpi_enable_turbo,
+	.disable_turbo = power_acpi_disable_turbo,
+	.get_caps = power_acpi_get_capabilities
+};
+
+RTE_POWER_REGISTER_CPUFREQ_OPS(acpi_ops);

drivers/power/acpi/acpi_cpufreq.h
@@ -2,15 +2,15 @@
  * Copyright(c) 2010-2014 Intel Corporation
  */
 
-#ifndef _POWER_ACPI_CPUFREQ_H
-#define _POWER_ACPI_CPUFREQ_H
+#ifndef ACPI_CPUFREQ_H
+#define ACPI_CPUFREQ_H
 
 /**
  * @file
- * RTE Power Management via userspace ACPI cpufreq
+ * Power Management via userspace ACPI cpufreq
  */
 
-#include "rte_power.h"
+#include "power_cpufreq.h"
 
 /**
  * Check if ACPI power management is supported.
@@ -214,4 +214,4 @@ int power_acpi_disable_turbo(unsigned int lcore_id);
 int power_acpi_get_capabilities(unsigned int lcore_id,
 		struct rte_power_core_capabilities *caps);
 
-#endif
+#endif /* ACPI_CPUFREQ_H */

drivers/power/acpi/meson.build
@@ -1,0 +1,10 @@
+# SPDX-License-Identifier: BSD-3-Clause
+# Copyright(c) 2024 Advanced Micro Devices, Inc.
+
+if not is_linux
+    build = false
+    reason = 'only supported on Linux'
+endif
+sources = files('acpi_cpufreq.c')
+
+deps += ['power']

drivers/power/amd_pstate/amd_pstate_cpufreq.c
@@ -1,6 +1,7 @@
 /* SPDX-License-Identifier: BSD-3-Clause
  * Copyright(c) 2010-2021 Intel Corporation
  * Copyright(c) 2021 Arm Limited
+ * Copyright(c) 2024 Advanced Micro Devices, Inc.
  */
 
 #include <stdlib.h>
@@ -8,17 +9,12 @@
 #include <rte_memcpy.h>
 #include <rte_stdatomic.h>
 
-#include "power_cppc_cpufreq.h"
+#include "amd_pstate_cpufreq.h"
 #include "power_common.h"
 
-/* macros used for rounding frequency to nearest 100000 */
-#define FREQ_ROUNDING_DELTA 50000
-#define ROUND_FREQ_TO_N_100000 100000
-
-/* the unit of highest_perf and nominal_perf differs on different arm platforms.
- * For highest_perf, it maybe 300 or 3000000, both means 3.0GHz.
- */
-#define UNIT_DIFF 10000
+/* macros used for rounding frequency to nearest 1000 */
+#define FREQ_ROUNDING_DELTA 500
+#define ROUND_FREQ_TO_N_1000 1000
 
 #define POWER_CONVERT_TO_DECIMAL 10
 
@@ -33,11 +29,11 @@
 		"/sys/devices/system/cpu/cpu%u/acpi_cppc/highest_perf"
 #define POWER_SYSFILE_NOMINAL_PERF \
 		"/sys/devices/system/cpu/cpu%u/acpi_cppc/nominal_perf"
-#define POWER_SYSFILE_SYS_MAX \
-		"/sys/devices/system/cpu/cpu%u/cpufreq/cpuinfo_max_freq"
+#define POWER_SYSFILE_NOMINAL_FREQ \
+		"/sys/devices/system/cpu/cpu%u/acpi_cppc/nominal_freq"
 
-#define POWER_CPPC_DRIVER "cppc_cpufreq"
-#define BUS_FREQ     100000
+#define POWER_AMD_PSTATE_DRIVER "amd-pstate"
+#define BUS_FREQ     1000	/* khz */
 
 enum power_state {
 	POWER_IDLE = 0,
@@ -49,12 +45,13 @@ enum power_state {
 /**
  * Power info per lcore.
  */
-struct __rte_cache_aligned cppc_power_info {
-	unsigned int lcore_id;               /**< Logical core id */
+struct __rte_cache_aligned amd_pstate_power_info {
+	uint32_t lcore_id;                   /**< Logical core id */
 	RTE_ATOMIC(uint32_t) state;          /**< Power in use state */
 	FILE *f;                             /**< FD of scaling_setspeed */
-	char governor_ori[32];               /**< Original governor name */
+	char governor_ori[28];               /**< Original governor name */
 	uint32_t curr_idx;                   /**< Freq index in freqs array */
+	uint32_t nom_idx;                    /**< Nominal index in freqs array */
 	uint32_t highest_perf;		     /**< system wide max freq */
 	uint32_t nominal_perf;		     /**< system wide nominal freq */
 	uint16_t turbo_available;            /**< Turbo Boost available */
@@ -63,14 +60,14 @@ struct __rte_cache_aligned cppc_power_info {
 	uint32_t freqs[RTE_MAX_LCORE_FREQS]; /**< Frequency array */
 };
 
-static struct cppc_power_info lcore_power_info[RTE_MAX_LCORE];
+static struct amd_pstate_power_info lcore_power_info[RTE_MAX_LCORE];
 
 /**
  * It is to set specific freq for specific logical core, according to the index
  * of supported frequencies.
  */
 static int
-set_freq_internal(struct cppc_power_info *pi, uint32_t idx)
+set_freq_internal(struct amd_pstate_power_info *pi, uint32_t idx)
 {
 	if (idx >= RTE_MAX_LCORE_FREQS || idx >= pi->nb_freqs) {
 		POWER_LOG(ERR, "Invalid frequency index %u, which "
@@ -106,18 +103,18 @@ set_freq_internal(struct cppc_power_info *pi, uint32_t idx)
  * governor will be saved for rolling back.
  */
 static int
-power_set_governor_userspace(struct cppc_power_info *pi)
+power_set_governor_userspace(struct amd_pstate_power_info *pi)
 {
 	return power_set_governor(pi->lcore_id, POWER_GOVERNOR_USERSPACE,
 			pi->governor_ori, sizeof(pi->governor_ori));
 }
 
 static int
-power_check_turbo(struct cppc_power_info *pi)
+power_check_turbo(struct amd_pstate_power_info *pi)
 {
-	FILE *f_nom = NULL, *f_max = NULL, *f_cmax = NULL;
+	FILE *f_nom = NULL, *f_max = NULL;
 	int ret = -1;
-	uint32_t nominal_perf = 0, highest_perf = 0, cpuinfo_max_freq = 0;
+	uint32_t nominal_perf = 0, highest_perf = 0;
 
 	open_core_sysfs_file(&f_max, "r", POWER_SYSFILE_HIGHEST_PERF,
 			pi->lcore_id);
@@ -135,14 +132,6 @@ power_check_turbo(struct cppc_power_info *pi)
 		goto err;
 	}
 
-	open_core_sysfs_file(&f_cmax, "r", POWER_SYSFILE_SYS_MAX,
-			pi->lcore_id);
-	if (f_cmax == NULL) {
-		POWER_LOG(ERR, "failed to open %s",
-				POWER_SYSFILE_SYS_MAX);
-		goto err;
-	}
-
 	ret = read_core_sysfs_u32(f_max, &highest_perf);
 	if (ret < 0) {
 		POWER_LOG(ERR, "Failed to read %s",
@@ -157,18 +146,10 @@ power_check_turbo(struct cppc_power_info *pi)
 		goto err;
 	}
 
-	ret = read_core_sysfs_u32(f_cmax, &cpuinfo_max_freq);
-	if (ret < 0) {
-		POWER_LOG(ERR, "Failed to read %s",
-				POWER_SYSFILE_SYS_MAX);
-		goto err;
-	}
-
 	pi->highest_perf = highest_perf;
 	pi->nominal_perf = nominal_perf;
 
-	if ((highest_perf > nominal_perf) && ((cpuinfo_max_freq == highest_perf)
-			|| cpuinfo_max_freq == highest_perf * UNIT_DIFF)) {
+	if (highest_perf > nominal_perf) {
 		pi->turbo_available = 1;
 		pi->turbo_enable = 1;
 		ret = 0;
@@ -188,8 +169,6 @@ err:
 		fclose(f_max);
 	if (f_nom != NULL)
 		fclose(f_nom);
-	if (f_cmax != NULL)
-		fclose(f_cmax);
 
 	return ret;
 }
@@ -199,12 +178,14 @@ err:
  * sys file.
  */
 static int
-power_get_available_freqs(struct cppc_power_info *pi)
+power_get_available_freqs(struct amd_pstate_power_info *pi)
 {
-	FILE *f_min = NULL, *f_max = NULL;
-	int ret = -1;
-	uint32_t scaling_min_freq = 0, scaling_max_freq = 0, nominal_perf = 0;
-	uint32_t i, num_freqs = 0;
+	FILE *f_min = NULL, *f_max = NULL, *f_nom = NULL;
+	int ret = -1, nominal_idx = -1;
+	uint32_t scaling_min_freq = 0, scaling_max_freq = 0;
+	uint32_t i, num_freqs = RTE_MAX_LCORE_FREQS;
+	uint32_t nominal_freq = 0, scaling_freq = 0;
+	uint32_t freq_calc = 0;
 
 	open_core_sysfs_file(&f_max, "r", POWER_SYSFILE_SCALING_MAX_FREQ,
 			pi->lcore_id);
@@ -222,6 +203,14 @@ power_get_available_freqs(struct cppc_power_info *pi)
 		goto out;
 	}
 
+	open_core_sysfs_file(&f_nom, "r", POWER_SYSFILE_NOMINAL_FREQ,
+			pi->lcore_id);
+	if (f_nom == NULL) {
+		POWER_LOG(ERR, "failed to open %s",
+				POWER_SYSFILE_NOMINAL_FREQ);
+		goto out;
+	}
+
 	ret = read_core_sysfs_u32(f_max, &scaling_max_freq);
 	if (ret < 0) {
 		POWER_LOG(ERR, "Failed to read %s",
@@ -236,31 +225,54 @@ power_get_available_freqs(struct cppc_power_info *pi)
 		goto out;
 	}
 
+	ret = read_core_sysfs_u32(f_nom, &nominal_freq);
+	if (ret < 0) {
+		POWER_LOG(ERR, "Failed to read %s",
+				POWER_SYSFILE_NOMINAL_FREQ);
+		goto out;
+	}
+
 	power_check_turbo(pi);
 
-	if (scaling_max_freq < scaling_min_freq)
+	if (scaling_max_freq < scaling_min_freq) {
+		POWER_LOG(ERR, "scaling min freq exceeds max freq, "
+			"not expected! Check system power policy");
 		goto out;
+	} else if (scaling_max_freq == scaling_min_freq) {
+		num_freqs = 1;
+	}
 
-	/* If turbo is available then there is one extra freq bucket
-	 * to store the sys max freq which value is scaling_max_freq
-	 */
-	nominal_perf = (pi->nominal_perf < UNIT_DIFF) ?
-			pi->nominal_perf * UNIT_DIFF : pi->nominal_perf;
-	num_freqs = (nominal_perf - scaling_min_freq) / BUS_FREQ + 1 +
-		pi->turbo_available;
-	if (num_freqs >= RTE_MAX_LCORE_FREQS) {
-		POWER_LOG(ERR, "Too many available frequencies: %d",
-				num_freqs);
-		goto out;
+	if (num_freqs > 1) {
+		scaling_freq = (scaling_max_freq - scaling_min_freq);
+		scaling_freq <<= 10;
+		scaling_freq /= (num_freqs - 1);
+		scaling_freq >>= 10;
+	} else {
+		scaling_freq = 0;
 	}
 
 	/* Generate the freq bucket array. */
 	for (i = 0, pi->nb_freqs = 0; i < num_freqs; i++) {
-		if ((i == 0) && pi->turbo_available)
-			pi->freqs[pi->nb_freqs++] = scaling_max_freq;
-		else
-			pi->freqs[pi->nb_freqs++] =
-			nominal_perf - (i - pi->turbo_available) * BUS_FREQ;
+		freq_calc = scaling_max_freq - (i * scaling_freq);
+		/* convert the frequency to nearest 1000 value
+		 * Ex: if freq=1396789 then freq_conv=1397000
+		 * Ex: if freq=800030 then freq_conv=800000
+		 */
+		freq_calc = (freq_calc + FREQ_ROUNDING_DELTA)
+					/ ROUND_FREQ_TO_N_1000;
+		freq_calc = freq_calc * ROUND_FREQ_TO_N_1000;
+
+		/* update the frequency table only if required */
+		if ((pi->nb_freqs == 0) ||
+				pi->freqs[pi->nb_freqs-1] != freq_calc) {
+			pi->freqs[pi->nb_freqs++] = freq_calc;
+		}
+		if (nominal_idx == -1) {
+			if ((nominal_freq * BUS_FREQ) >= freq_calc) {
+				pi->nom_idx = pi->nb_freqs - 1;
+				nominal_idx = pi->nom_idx;
+			}
+		}
 	}
 
 	ret = 0;
@@ -273,6 +285,8 @@ out:
 		fclose(f_min);
 	if (f_max != NULL)
 		fclose(f_max);
+	if (f_nom != NULL)
+		fclose(f_nom);
 
 	return ret;
 }
@@ -281,7 +295,7 @@ out:
  * It is to fopen the sys file for the future setting the lcore frequency.
  */
 static int
-power_init_for_setting_freq(struct cppc_power_info *pi)
+power_init_for_setting_freq(struct amd_pstate_power_info *pi)
 {
 	FILE *f = NULL;
 	char buf[BUFSIZ];
@@ -304,14 +318,14 @@ power_init_for_setting_freq(struct cppc_power_info *pi)
 
 	freq = strtoul(buf, NULL, POWER_CONVERT_TO_DECIMAL);
 
-	/* convert the frequency to nearest 100000 value
-	 * Ex: if freq=1396789 then freq_conv=1400000
+	/* convert the frequency to nearest 1000 value
+	 * Ex: if freq=1396789 then freq_conv=1397000
 	 * Ex: if freq=800030 then freq_conv=800000
 	 */
 	unsigned int freq_conv = 0;
 	freq_conv = (freq + FREQ_ROUNDING_DELTA)
-				/ ROUND_FREQ_TO_N_100000;
-	freq_conv = freq_conv * ROUND_FREQ_TO_N_100000;
+				/ ROUND_FREQ_TO_N_1000;
+	freq_conv = freq_conv * ROUND_FREQ_TO_N_1000;
 
 	for (i = 0; i < pi->nb_freqs; i++) {
 		if (freq_conv == pi->freqs[i]) {
@@ -329,20 +343,20 @@ err:
 }
 
 int
-power_cppc_cpufreq_check_supported(void)
+power_amd_pstate_cpufreq_check_supported(void)
 {
-	return cpufreq_check_scaling_driver(POWER_CPPC_DRIVER);
+	return cpufreq_check_scaling_driver(POWER_AMD_PSTATE_DRIVER);
 }
 
 int
-power_cppc_cpufreq_init(unsigned int lcore_id)
+power_amd_pstate_cpufreq_init(unsigned int lcore_id)
 {
-	struct cppc_power_info *pi;
+	struct amd_pstate_power_info *pi;
 	uint32_t exp_state;
 
-	if (!power_cppc_cpufreq_check_supported()) {
+	if (!power_amd_pstate_cpufreq_check_supported()) {
 		POWER_LOG(ERR, "%s driver is not supported",
-				POWER_CPPC_DRIVER);
+				POWER_AMD_PSTATE_DRIVER);
 		return -1;
 	}
 
@@ -360,8 +374,8 @@ power_cppc_cpufreq_init(unsigned int lcore_id)
 	 * ordering below as lock to make sure the frequency operations
 	 * in the critical section are done under the correct state.
 	 */
-	if (!rte_atomic_compare_exchange_strong_explicit(&(pi->state), &exp_state,
-					POWER_ONGOING,
+	if (!rte_atomic_compare_exchange_strong_explicit(&(pi->state),
+					&exp_state, POWER_ONGOING,
 					rte_memory_order_acquire, rte_memory_order_relaxed)) {
 		POWER_LOG(INFO, "Power management of lcore %u is "
 				"in use", lcore_id);
@@ -395,7 +409,7 @@ power_cppc_cpufreq_init(unsigned int lcore_id)
 	}
 
 	/* Set freq to max by default */
-	if (power_cppc_cpufreq_freq_max(lcore_id) < 0) {
+	if (power_amd_pstate_cpufreq_freq_max(lcore_id) < 0) {
 		POWER_LOG(ERR, "Cannot set frequency of lcore %u "
 				"to max", lcore_id);
 		goto fail;
@@ -418,15 +432,15 @@ fail:
  * needed by writing the sys file.
  */
 static int
-power_set_governor_original(struct cppc_power_info *pi)
+power_set_governor_original(struct amd_pstate_power_info *pi)
 {
 	return power_set_governor(pi->lcore_id, pi->governor_ori, NULL, 0);
 }
 
 int
-power_cppc_cpufreq_exit(unsigned int lcore_id)
+power_amd_pstate_cpufreq_exit(unsigned int lcore_id)
 {
-	struct cppc_power_info *pi;
+	struct amd_pstate_power_info *pi;
 	uint32_t exp_state;
 
 	if (lcore_id >= RTE_MAX_LCORE) {
@@ -442,8 +456,8 @@ power_cppc_cpufreq_exit(unsigned int lcore_id)
 	 * ordering below as lock to make sure the frequency operations
 	 * in the critical section are done under the correct state.
 	 */
-	if (!rte_atomic_compare_exchange_strong_explicit(&(pi->state), &exp_state,
-					POWER_ONGOING,
+	if (!rte_atomic_compare_exchange_strong_explicit(&(pi->state),
+					&exp_state, POWER_ONGOING,
 					rte_memory_order_acquire, rte_memory_order_relaxed)) {
 		POWER_LOG(INFO, "Power management of lcore %u is "
 				"not used", lcore_id);
@@ -475,9 +489,9 @@ fail:
 }
 
 uint32_t
-power_cppc_cpufreq_freqs(unsigned int lcore_id, uint32_t *freqs, uint32_t num)
+power_amd_pstate_cpufreq_freqs(unsigned int lcore_id, uint32_t *freqs, uint32_t num)
 {
-	struct cppc_power_info *pi;
+	struct amd_pstate_power_info *pi;
 
 	if (lcore_id >= RTE_MAX_LCORE) {
 		POWER_LOG(ERR, "Invalid lcore ID");
@@ -500,7 +514,7 @@ power_cppc_cpufreq_freqs(unsigned int lcore_id, uint32_t *freqs, uint32_t num)
 }
 
 uint32_t
-power_cppc_cpufreq_get_freq(unsigned int lcore_id)
+power_amd_pstate_cpufreq_get_freq(unsigned int lcore_id)
 {
 	if (lcore_id >= RTE_MAX_LCORE) {
 		POWER_LOG(ERR, "Invalid lcore ID");
@@ -511,7 +525,7 @@ power_cppc_cpufreq_get_freq(unsigned int lcore_id)
 }
 
 int
-power_cppc_cpufreq_set_freq(unsigned int lcore_id, uint32_t index)
+power_amd_pstate_cpufreq_set_freq(unsigned int lcore_id, uint32_t index)
 {
 	if (lcore_id >= RTE_MAX_LCORE) {
 		POWER_LOG(ERR, "Invalid lcore ID");
@@ -522,9 +536,9 @@ power_cppc_cpufreq_set_freq(unsigned int lcore_id, uint32_t index)
 }
 
 int
-power_cppc_cpufreq_freq_down(unsigned int lcore_id)
+power_amd_pstate_cpufreq_freq_down(unsigned int lcore_id)
 {
-	struct cppc_power_info *pi;
+	struct amd_pstate_power_info *pi;
 
 	if (lcore_id >= RTE_MAX_LCORE) {
 		POWER_LOG(ERR, "Invalid lcore ID");
@@ -540,9 +554,9 @@ power_cppc_cpufreq_freq_down(unsigned int lcore_id)
 }
 
 int
-power_cppc_cpufreq_freq_up(unsigned int lcore_id)
+power_amd_pstate_cpufreq_freq_up(unsigned int lcore_id)
 {
-	struct cppc_power_info *pi;
+	struct amd_pstate_power_info *pi;
 
 	if (lcore_id >= RTE_MAX_LCORE) {
 		POWER_LOG(ERR, "Invalid lcore ID");
@@ -550,7 +564,7 @@ power_cppc_cpufreq_freq_up(unsigned int lcore_id)
 	}
 
 	pi = &lcore_power_info[lcore_id];
-	if (pi->curr_idx == 0 || (pi->curr_idx == 1 &&
+	if (pi->curr_idx == 0 || (pi->curr_idx == pi->nom_idx &&
 		pi->turbo_available && !pi->turbo_enable))
 		return 0;
 
@@ -559,7 +573,7 @@ power_cppc_cpufreq_freq_up(unsigned int lcore_id)
 }
 
 int
-power_cppc_cpufreq_freq_max(unsigned int lcore_id)
+power_amd_pstate_cpufreq_freq_max(unsigned int lcore_id)
 {
 	if (lcore_id >= RTE_MAX_LCORE) {
 		POWER_LOG(ERR, "Invalid lcore ID");
@@ -575,15 +589,16 @@ power_cppc_cpufreq_freq_max(unsigned int lcore_id)
 		else
 			/* Set to max non-turbo */
 			return set_freq_internal(
-				&lcore_power_info[lcore_id], 1);
+				&lcore_power_info[lcore_id],
+				lcore_power_info[lcore_id].nom_idx);
 	} else
 		return set_freq_internal(&lcore_power_info[lcore_id], 0);
 }
 
 int
-power_cppc_cpufreq_freq_min(unsigned int lcore_id)
+power_amd_pstate_cpufreq_freq_min(unsigned int lcore_id)
 {
-	struct cppc_power_info *pi;
+	struct amd_pstate_power_info *pi;
 
 	if (lcore_id >= RTE_MAX_LCORE) {
 		POWER_LOG(ERR, "Invalid lcore ID");
@@ -597,9 +612,9 @@ power_cppc_cpufreq_freq_min(unsigned int lcore_id)
 }
 
 int
-power_cppc_turbo_status(unsigned int lcore_id)
+power_amd_pstate_turbo_status(unsigned int lcore_id)
 {
-	struct cppc_power_info *pi;
+	struct amd_pstate_power_info *pi;
 
 	if (lcore_id >= RTE_MAX_LCORE) {
 		POWER_LOG(ERR, "Invalid lcore ID");
@@ -612,9 +627,9 @@ power_cppc_turbo_status(unsigned int lcore_id)
 }
 
 int
-power_cppc_enable_turbo(unsigned int lcore_id)
+power_amd_pstate_enable_turbo(unsigned int lcore_id)
 {
-	struct cppc_power_info *pi;
+	struct amd_pstate_power_info *pi;
 
 	if (lcore_id >= RTE_MAX_LCORE) {
 		POWER_LOG(ERR, "Invalid lcore ID");
@@ -637,7 +652,7 @@ power_cppc_enable_turbo(unsigned int lcore_id)
 	 * if ((pi->turbo_available) && (pi->curr_idx <= 1))
 	 */
 	/* Max may have changed, so call to max function */
-	if (power_cppc_cpufreq_freq_max(lcore_id) < 0) {
+	if (power_amd_pstate_cpufreq_freq_max(lcore_id) < 0) {
 		POWER_LOG(ERR,
 			"Failed to set frequency of lcore %u to max",
 			lcore_id);
@@ -648,9 +663,9 @@ power_cppc_enable_turbo(unsigned int lcore_id)
 }
 
 int
-power_cppc_disable_turbo(unsigned int lcore_id)
+power_amd_pstate_disable_turbo(unsigned int lcore_id)
 {
-	struct cppc_power_info *pi;
+	struct amd_pstate_power_info *pi;
 
 	if (lcore_id >= RTE_MAX_LCORE) {
 		POWER_LOG(ERR, "Invalid lcore ID");
@@ -661,9 +676,9 @@ power_cppc_disable_turbo(unsigned int lcore_id)
 
 	pi->turbo_enable = 0;
 
-	if ((pi->turbo_available) && (pi->curr_idx <= 1)) {
+	if ((pi->turbo_available) && (pi->curr_idx <= pi->nom_idx)) {
 		/* Try to set freq to max by default coming out of turbo */
-		if (power_cppc_cpufreq_freq_max(lcore_id) < 0) {
+		if (power_amd_pstate_cpufreq_freq_max(lcore_id) < 0) {
 			POWER_LOG(ERR,
 				"Failed to set frequency of lcore %u to max",
 				lcore_id);
@@ -675,10 +690,10 @@ power_cppc_disable_turbo(unsigned int lcore_id)
 }
 
 int
-power_cppc_get_capabilities(unsigned int lcore_id,
+power_amd_pstate_get_capabilities(unsigned int lcore_id,
 		struct rte_power_core_capabilities *caps)
 {
-	struct cppc_power_info *pi;
+	struct amd_pstate_power_info *pi;
 
 	if (lcore_id >= RTE_MAX_LCORE) {
 		POWER_LOG(ERR, "Invalid lcore ID");
@@ -695,3 +710,23 @@ power_cppc_get_capabilities(unsigned int lcore_id,
 
 	return 0;
 }
+
+static struct rte_power_cpufreq_ops amd_pstate_ops = {
+	.name = "amd-pstate",
+	.init = power_amd_pstate_cpufreq_init,
+	.exit = power_amd_pstate_cpufreq_exit,
+	.check_env_support = power_amd_pstate_cpufreq_check_supported,
+	.get_avail_freqs = power_amd_pstate_cpufreq_freqs,
+	.get_freq = power_amd_pstate_cpufreq_get_freq,
+	.set_freq = power_amd_pstate_cpufreq_set_freq,
+	.freq_down = power_amd_pstate_cpufreq_freq_down,
+	.freq_up = power_amd_pstate_cpufreq_freq_up,
+	.freq_max = power_amd_pstate_cpufreq_freq_max,
+	.freq_min = power_amd_pstate_cpufreq_freq_min,
+	.turbo_status = power_amd_pstate_turbo_status,
+	.enable_turbo = power_amd_pstate_enable_turbo,
+	.disable_turbo = power_amd_pstate_disable_turbo,
+	.get_caps = power_amd_pstate_get_capabilities
+};
+
+RTE_POWER_REGISTER_CPUFREQ_OPS(amd_pstate_ops);

drivers/power/amd_pstate/amd_pstate_cpufreq.h
@@ -1,27 +1,28 @@
 /* SPDX-License-Identifier: BSD-3-Clause
  * Copyright(c) 2010-2021 Intel Corporation
  * Copyright(c) 2021 Arm Limited
+ * Copyright(c) 2024 Advanced Micro Devices, Inc.
  */
 
-#ifndef _POWER_CPPC_CPUFREQ_H
-#define _POWER_CPPC_CPUFREQ_H
+#ifndef AMD_PSTATE_CPUFREQ_H
+#define AMD_PSTATE_CPUFREQ_H
 
 /**
  * @file
- * RTE Power Management via userspace CPPC cpufreq
+ * Power Management via userspace AMD pstate cpufreq
  */
 
-#include "rte_power.h"
+#include "power_cpufreq.h"
 
 /**
- * Check if CPPC power management is supported.
+ * Check if amd p-state power management is supported.
  *
  * @return
  *   - 1 if supported
  *   - 0 if unsupported
  *   - -1 if error, with rte_errno indicating reason for error.
  */
-int power_cppc_cpufreq_check_supported(void);
+int power_amd_pstate_cpufreq_check_supported(void);
 
 /**
  * Initialize power management for a specific lcore. It will check and set the
@@ -35,7 +36,7 @@ int power_cppc_cpufreq_check_supported(void);
  *  - 0 on success.
  *  - Negative on error.
  */
-int power_cppc_cpufreq_init(unsigned int lcore_id);
+int power_amd_pstate_cpufreq_init(unsigned int lcore_id);
 
 /**
  * Exit power management on a specific lcore. It will set the governor to which
@@ -48,7 +49,7 @@ int power_cppc_cpufreq_init(unsigned int lcore_id);
  *  - 0 on success.
  *  - Negative on error.
  */
-int power_cppc_cpufreq_exit(unsigned int lcore_id);
+int power_amd_pstate_cpufreq_exit(unsigned int lcore_id);
 
 /**
  * Get the available frequencies of a specific lcore. The return value will be
@@ -67,7 +68,7 @@ int power_cppc_cpufreq_exit(unsigned int lcore_id);
  * @return
  *  The number of available frequencies.
  */
-uint32_t power_cppc_cpufreq_freqs(unsigned int lcore_id, uint32_t *freqs,
+uint32_t power_amd_pstate_cpufreq_freqs(unsigned int lcore_id, uint32_t *freqs,
 		uint32_t num);
 
 /**
@@ -81,7 +82,7 @@ uint32_t power_cppc_cpufreq_freqs(unsigned int lcore_id, uint32_t *freqs,
  * @return
  *  The current index of available frequencies.
  */
-uint32_t power_cppc_cpufreq_get_freq(unsigned int lcore_id);
+uint32_t power_amd_pstate_cpufreq_get_freq(unsigned int lcore_id);
 
 /**
  * Set the new frequency for a specific lcore by indicating the index of
@@ -98,7 +99,7 @@ uint32_t power_cppc_cpufreq_get_freq(unsigned int lcore_id);
  *  - 0 on success without frequency changed.
  *  - Negative on error.
  */
-int power_cppc_cpufreq_set_freq(unsigned int lcore_id, uint32_t index);
+int power_amd_pstate_cpufreq_set_freq(unsigned int lcore_id, uint32_t index);
 
 /**
  * Scale up the frequency of a specific lcore according to the available
@@ -113,7 +114,7 @@ int power_cppc_cpufreq_set_freq(unsigned int lcore_id, uint32_t index);
  *  - 0 on success without frequency changed.
  *  - Negative on error.
  */
-int power_cppc_cpufreq_freq_up(unsigned int lcore_id);
+int power_amd_pstate_cpufreq_freq_up(unsigned int lcore_id);
 
 /**
  * Scale down the frequency of a specific lcore according to the available
@@ -128,7 +129,7 @@ int power_cppc_cpufreq_freq_up(unsigned int lcore_id);
  *  - 0 on success without frequency changed.
  *  - Negative on error.
  */
-int power_cppc_cpufreq_freq_down(unsigned int lcore_id);
+int power_amd_pstate_cpufreq_freq_down(unsigned int lcore_id);
 
 /**
  * Scale up the frequency of a specific lcore to the highest according to the
@@ -143,7 +144,7 @@ int power_cppc_cpufreq_freq_down(unsigned int lcore_id);
  *  - 0 on success without frequency changed.
  *  - Negative on error.
  */
-int power_cppc_cpufreq_freq_max(unsigned int lcore_id);
+int power_amd_pstate_cpufreq_freq_max(unsigned int lcore_id);
 
 /**
  * Scale down the frequency of a specific lcore to the lowest according to the
@@ -158,7 +159,7 @@ int power_cppc_cpufreq_freq_max(unsigned int lcore_id);
  *  - 0 on success without frequency changed.
  *  - Negative on error.
  */
-int power_cppc_cpufreq_freq_min(unsigned int lcore_id);
+int power_amd_pstate_cpufreq_freq_min(unsigned int lcore_id);
 
 /**
  * Get the turbo status of a specific lcore.
@@ -172,7 +173,7 @@ int power_cppc_cpufreq_freq_min(unsigned int lcore_id);
  *  - 0 Turbo Boost is disabled on this lcore.
  *  - Negative on error.
  */
-int power_cppc_turbo_status(unsigned int lcore_id);
+int power_amd_pstate_turbo_status(unsigned int lcore_id);
 
 /**
  * Enable Turbo Boost on a specific lcore.
@@ -185,7 +186,7 @@ int power_cppc_turbo_status(unsigned int lcore_id);
  *  - 0 Turbo Boost is enabled successfully on this lcore.
  *  - Negative on error.
  */
-int power_cppc_enable_turbo(unsigned int lcore_id);
+int power_amd_pstate_enable_turbo(unsigned int lcore_id);
 
 /**
  * Disable Turbo Boost on a specific lcore.
@@ -198,7 +199,7 @@ int power_cppc_enable_turbo(unsigned int lcore_id);
  *  - 0 Turbo Boost disabled successfully on this lcore.
  *  - Negative on error.
  */
-int power_cppc_disable_turbo(unsigned int lcore_id);
+int power_amd_pstate_disable_turbo(unsigned int lcore_id);
 
 /**
  * Returns power capabilities for a specific lcore.
@@ -212,7 +213,7 @@ int power_cppc_disable_turbo(unsigned int lcore_id);
  *  - 0 on success.
  *  - Negative on error.
  */
-int power_cppc_get_capabilities(unsigned int lcore_id,
+int power_amd_pstate_get_capabilities(unsigned int lcore_id,
 		struct rte_power_core_capabilities *caps);
 
-#endif /* _POWER_CPPC_CPUFREQ_H */
+#endif /* AMD_PSTATE_CPUFREQ_H */

commit b60b60aa0b9482c158c9909fe4c17a77f4631023

power/kvm_vm: enforce enabled lcore ID check
The locre ID in cpufreq power must be enabled core in application.
Use rte_lcore_is_enabled to verify lcore_id.

Fixes: 6f987b594fa6 ("power: refactor core power management")
Cc: stable@dpdk.org

Signed-off-by: Huisong Li <lihuisong@huawei.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

drivers/power/kvm_vm/kvm_vm.c
@@ -24,11 +24,11 @@ power_kvm_vm_check_supported(void)
 int
 power_kvm_vm_init(unsigned int lcore_id)
 {
-	if (lcore_id >= RTE_MAX_LCORE) {
-		POWER_LOG(ERR, "Core(%u) is out of range 0...%d",
-				lcore_id, RTE_MAX_LCORE-1);
+	if (!rte_lcore_is_enabled(lcore_id)) {
+		POWER_LOG(ERR, "lcore id %u is not enabled", lcore_id);
 		return -1;
 	}
+
 	pkt[lcore_id].command = RTE_POWER_CPU_POWER;
 	pkt[lcore_id].resource_id = lcore_id;
 	return guest_channel_host_connect(FD_PATH, lcore_id);
@@ -73,11 +73,11 @@ send_msg(unsigned int lcore_id, uint32_t scale_direction)
 {
 	int ret;
 
-	if (lcore_id >= RTE_MAX_LCORE) {
-		POWER_LOG(ERR, "Core(%u) is out of range 0...%d",
-				lcore_id, RTE_MAX_LCORE-1);
+	if (!rte_lcore_is_enabled(lcore_id)) {
+		POWER_LOG(ERR, "lcore id %u is not enabled", lcore_id);
 		return -1;
 	}
+
 	pkt[lcore_id].unit = scale_direction;
 	ret = guest_channel_send_msg(&pkt[lcore_id], lcore_id);
 	if (ret == 0)