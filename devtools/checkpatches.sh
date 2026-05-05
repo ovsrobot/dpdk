ovsrobot/dpdk

commit cfe3aeb170b2f6277e6f96173599da51eab0654f

remove experimental tags from all symbol definitions
We had some inconsistencies between functions prototypes and actual
definitions.
Let's avoid this by only adding the experimental tag to the prototypes.
Tests with gcc and clang show it is enough.

git grep -l __rte_experimental |grep \.c$ |while read file; do
	sed -i -e '/^__rte_experimental$/d' $file;
	sed -i -e 's/  *__rte_experimental//' $file;
	sed -i -e 's/__rte_experimental  *//' $file;
done

Signed-off-by: David Marchand <david.marchand@redhat.com>
Acked-by: Adrien Mazarguil <adrien.mazarguil@6wind.com>
Acked-by: Neil Horman <nhorman@tuxdriver.com>

devtools/checkpatches.sh
@@ -79,6 +79,31 @@ check_forbidden_additions() { # <patch>
 	return $res
 }
 
+check_experimental_tags() { # <patch>
+	res=0
+
+	cat "$1" |awk '
+	BEGIN {
+		current_file = "";
+		ret = 0;
+	}
+	/^+++ b\// {
+		current_file = $2;
+	}
+	/^+.*__rte_experimental/ {
+		if (current_file ~ ".c$" ) {
+			print "Please only put __rte_experimental tags in " \
+				"headers ("current_file")";
+			ret = 1;
+		}
+	}
+	END {
+		exit ret;
+	}' || res=1
+
+	return $res
+}
+
 number=0
 range='origin/master..'
 quiet=false
@@ -145,6 +170,14 @@ check () { # <patch> <commit> <title>
 
 	! $verbose || printf '\nChecking forbidden tokens additions:\n'
 	report=$(check_forbidden_additions "$tmpinput")
+	if [ $? -ne 0 ] ; then
+		$headline_printed || print_headline "$3"
+		printf '%s\n' "$report"
+		ret=1
+	fi
+
+	! $verbose || printf '\nChecking __rte_experimental tags:\n'
+	report=$(check_experimental_tags "$tmpinput")
 	if [ $? -ne 0 ] ; then
 		$headline_printed || print_headline "$3"
 		printf '%s\n' "$report"

drivers/net/dpaa2/dpaa2_ethdev.c
@@ -102,7 +102,7 @@ static int dpaa2_dev_mtu_set(struct rte_eth_dev *dev, uint16_t mtu);
 
 int dpaa2_logtype_pmd;
 
-__rte_experimental void
+void
 rte_pmd_dpaa2_set_timestamp(enum pmd_dpaa2_ts enable)
 {
 	dpaa2_enable_ts = enable;

drivers/net/ixgbe/rte_pmd_ixgbe.c
@@ -882,7 +882,7 @@ rte_pmd_ixgbe_set_tc_bw_alloc(uint16_t port,
 	return 0;
 }
 
-int __rte_experimental
+int
 rte_pmd_ixgbe_upd_fctrl_sbp(uint16_t port, int enable)
 {
 	struct ixgbe_hw *hw;
@@ -1096,7 +1096,7 @@ STATIC void rte_pmd_ixgbe_release_swfw(struct ixgbe_hw *hw, u32 mask)
 	ixgbe_release_swfw_semaphore(hw, mask);
 }
 
-int __rte_experimental
+int
 rte_pmd_ixgbe_mdio_lock(uint16_t port)
 {
 	struct ixgbe_hw *hw;
@@ -1123,7 +1123,7 @@ rte_pmd_ixgbe_mdio_lock(uint16_t port)
 	return IXGBE_SUCCESS;
 }
 
-int __rte_experimental
+int
 rte_pmd_ixgbe_mdio_unlock(uint16_t port)
 {
 	struct rte_eth_dev *dev;
@@ -1150,7 +1150,7 @@ rte_pmd_ixgbe_mdio_unlock(uint16_t port)
 	return IXGBE_SUCCESS;
 }
 
-int __rte_experimental
+int
 rte_pmd_ixgbe_mdio_unlocked_read(uint16_t port, uint32_t reg_addr,
 				 uint32_t dev_type, uint16_t *phy_data)
 {
@@ -1197,7 +1197,7 @@ rte_pmd_ixgbe_mdio_unlocked_read(uint16_t port, uint32_t reg_addr,
 	return 0;
 }
 
-int __rte_experimental
+int
 rte_pmd_ixgbe_mdio_unlocked_write(uint16_t port, uint32_t reg_addr,
 				  uint32_t dev_type, uint16_t phy_data)
 {

lib/librte_bbdev/rte_bbdev.c
@@ -173,7 +173,7 @@ find_free_dev_id(void)
 	return RTE_BBDEV_MAX_DEVS;
 }
 
-struct rte_bbdev * __rte_experimental
+struct rte_bbdev *
 rte_bbdev_allocate(const char *name)
 {
 	int ret;
@@ -232,7 +232,7 @@ rte_bbdev_allocate(const char *name)
 	return bbdev;
 }
 
-int __rte_experimental
+int
 rte_bbdev_release(struct rte_bbdev *bbdev)
 {
 	uint16_t dev_id;
@@ -266,7 +266,7 @@ rte_bbdev_release(struct rte_bbdev *bbdev)
 	return 0;
 }
 
-struct rte_bbdev * __rte_experimental
+struct rte_bbdev *
 rte_bbdev_get_named_dev(const char *name)
 {
 	unsigned int i;
@@ -286,13 +286,13 @@ rte_bbdev_get_named_dev(const char *name)
 	return NULL;
 }
 
-uint16_t __rte_experimental
+uint16_t
 rte_bbdev_count(void)
 {
 	return num_devs;
 }
 
-bool __rte_experimental
+bool
 rte_bbdev_is_valid(uint16_t dev_id)
 {
 	if ((dev_id < RTE_BBDEV_MAX_DEVS) &&
@@ -301,7 +301,7 @@ rte_bbdev_is_valid(uint16_t dev_id)
 	return false;
 }
 
-uint16_t __rte_experimental
+uint16_t
 rte_bbdev_find_next(uint16_t dev_id)
 {
 	dev_id++;
@@ -311,7 +311,7 @@ rte_bbdev_find_next(uint16_t dev_id)
 	return dev_id;
 }
 
-int __rte_experimental
+int
 rte_bbdev_setup_queues(uint16_t dev_id, uint16_t num_queues, int socket_id)
 {
 	unsigned int i;
@@ -401,7 +401,7 @@ error:
 	return ret;
 }
 
-int __rte_experimental
+int
 rte_bbdev_intr_enable(uint16_t dev_id)
 {
 	int ret;
@@ -433,7 +433,7 @@ rte_bbdev_intr_enable(uint16_t dev_id)
 	return -ENOTSUP;
 }
 
-int __rte_experimental
+int
 rte_bbdev_queue_configure(uint16_t dev_id, uint16_t queue_id,
 		const struct rte_bbdev_queue_conf *conf)
 {
@@ -551,7 +551,7 @@ rte_bbdev_queue_configure(uint16_t dev_id, uint16_t queue_id,
 	return 0;
 }
 
-int __rte_experimental
+int
 rte_bbdev_start(uint16_t dev_id)
 {
 	int i;
@@ -583,7 +583,7 @@ rte_bbdev_start(uint16_t dev_id)
 	return 0;
 }
 
-int __rte_experimental
+int
 rte_bbdev_stop(uint16_t dev_id)
 {
 	struct rte_bbdev *dev = get_dev(dev_id);
@@ -604,7 +604,7 @@ rte_bbdev_stop(uint16_t dev_id)
 	return 0;
 }
 
-int __rte_experimental
+int
 rte_bbdev_close(uint16_t dev_id)
 {
 	int ret;
@@ -649,7 +649,7 @@ rte_bbdev_close(uint16_t dev_id)
 	return 0;
 }
 
-int __rte_experimental
+int
 rte_bbdev_queue_start(uint16_t dev_id, uint16_t queue_id)
 {
 	struct rte_bbdev *dev = get_dev(dev_id);
@@ -679,7 +679,7 @@ rte_bbdev_queue_start(uint16_t dev_id, uint16_t queue_id)
 	return 0;
 }
 
-int __rte_experimental
+int
 rte_bbdev_queue_stop(uint16_t dev_id, uint16_t queue_id)
 {
 	struct rte_bbdev *dev = get_dev(dev_id);
@@ -739,7 +739,7 @@ reset_stats_in_queues(struct rte_bbdev *dev)
 	rte_bbdev_log_debug("Reset stats on %u", dev->data->dev_id);
 }
 
-int __rte_experimental
+int
 rte_bbdev_stats_get(uint16_t dev_id, struct rte_bbdev_stats *stats)
 {
 	struct rte_bbdev *dev = get_dev(dev_id);
@@ -762,7 +762,7 @@ rte_bbdev_stats_get(uint16_t dev_id, struct rte_bbdev_stats *stats)
 	return 0;
 }
 
-int __rte_experimental
+int
 rte_bbdev_stats_reset(uint16_t dev_id)
 {
 	struct rte_bbdev *dev = get_dev(dev_id);
@@ -779,7 +779,7 @@ rte_bbdev_stats_reset(uint16_t dev_id)
 	return 0;
 }
 
-int __rte_experimental
+int
 rte_bbdev_info_get(uint16_t dev_id, struct rte_bbdev_info *dev_info)
 {
 	struct rte_bbdev *dev = get_dev(dev_id);
@@ -807,7 +807,7 @@ rte_bbdev_info_get(uint16_t dev_id, struct rte_bbdev_info *dev_info)
 	return 0;
 }
 
-int __rte_experimental
+int
 rte_bbdev_queue_info_get(uint16_t dev_id, uint16_t queue_id,
 		struct rte_bbdev_queue_info *queue_info)
 {
@@ -872,7 +872,7 @@ bbdev_op_init(struct rte_mempool *mempool, void *arg, void *element,
 	}
 }
 
-struct rte_mempool * __rte_experimental
+struct rte_mempool *
 rte_bbdev_op_pool_create(const char *name, enum rte_bbdev_op_type type,
 		unsigned int num_elements, unsigned int cache_size,
 		int socket_id)
@@ -919,7 +919,7 @@ rte_bbdev_op_pool_create(const char *name, enum rte_bbdev_op_type type,
 	return mp;
 }
 
-int __rte_experimental
+int
 rte_bbdev_callback_register(uint16_t dev_id, enum rte_bbdev_event_type event,
 		rte_bbdev_cb_fn cb_fn, void *cb_arg)
 {
@@ -964,7 +964,7 @@ rte_bbdev_callback_register(uint16_t dev_id, enum rte_bbdev_event_type event,
 	return (user_cb == NULL) ? -ENOMEM : 0;
 }
 
-int __rte_experimental
+int
 rte_bbdev_callback_unregister(uint16_t dev_id, enum rte_bbdev_event_type event,
 		rte_bbdev_cb_fn cb_fn, void *cb_arg)
 {
@@ -1009,7 +1009,7 @@ rte_bbdev_callback_unregister(uint16_t dev_id, enum rte_bbdev_event_type event,
 	return ret;
 }
 
-void __rte_experimental
+void
 rte_bbdev_pmd_callback_process(struct rte_bbdev *dev,
 	enum rte_bbdev_event_type event, void *ret_param)
 {
@@ -1051,7 +1051,7 @@ rte_bbdev_pmd_callback_process(struct rte_bbdev *dev,
 	rte_spinlock_unlock(&rte_bbdev_cb_lock);
 }
 
-int __rte_experimental
+int
 rte_bbdev_queue_intr_enable(uint16_t dev_id, uint16_t queue_id)
 {
 	struct rte_bbdev *dev = get_dev(dev_id);
@@ -1062,7 +1062,7 @@ rte_bbdev_queue_intr_enable(uint16_t dev_id, uint16_t queue_id)
 	return dev->dev_ops->queue_intr_enable(dev, queue_id);
 }
 
-int __rte_experimental
+int
 rte_bbdev_queue_intr_disable(uint16_t dev_id, uint16_t queue_id)
 {
 	struct rte_bbdev *dev = get_dev(dev_id);
@@ -1073,7 +1073,7 @@ rte_bbdev_queue_intr_disable(uint16_t dev_id, uint16_t queue_id)
 	return dev->dev_ops->queue_intr_disable(dev, queue_id);
 }
 
-int __rte_experimental
+int
 rte_bbdev_queue_intr_ctl(uint16_t dev_id, uint16_t queue_id, int epfd, int op,
 		void *data)
 {
@@ -1110,7 +1110,7 @@ rte_bbdev_queue_intr_ctl(uint16_t dev_id, uint16_t queue_id, int epfd, int op,
 }
 
 
-const char * __rte_experimental
+const char *
 rte_bbdev_op_type_str(enum rte_bbdev_op_type op_type)
 {
 	static const char * const op_types[] = {

lib/librte_bpf/bpf.c
@@ -16,7 +16,7 @@
 
 int rte_bpf_logtype;
 
-__rte_experimental void
+void
 rte_bpf_destroy(struct rte_bpf *bpf)
 {
 	if (bpf != NULL) {
@@ -26,7 +26,7 @@ rte_bpf_destroy(struct rte_bpf *bpf)
 	}
 }
 
-__rte_experimental int
+int
 rte_bpf_get_jit(const struct rte_bpf *bpf, struct rte_bpf_jit *jit)
 {
 	if (bpf == NULL || jit == NULL)

lib/librte_bpf/bpf_exec.c
@@ -424,7 +424,7 @@ bpf_exec(const struct rte_bpf *bpf, uint64_t reg[EBPF_REG_NUM])
 	return 0;
 }
 
-__rte_experimental uint32_t
+uint32_t
 rte_bpf_exec_burst(const struct rte_bpf *bpf, void *ctx[], uint64_t rc[],
 	uint32_t num)
 {
@@ -443,7 +443,7 @@ rte_bpf_exec_burst(const struct rte_bpf *bpf, void *ctx[], uint64_t rc[],
 	return i;
 }
 
-__rte_experimental uint64_t
+uint64_t
 rte_bpf_exec(const struct rte_bpf *bpf, void *ctx)
 {
 	uint64_t rc;

lib/librte_bpf/bpf_load.c
@@ -86,7 +86,7 @@ bpf_check_xsym(const struct rte_bpf_xsym *xsym)
 	return 0;
 }
 
-__rte_experimental struct rte_bpf *
+struct rte_bpf *
 rte_bpf_load(const struct rte_bpf_prm *prm)
 {
 	struct rte_bpf *bpf;
@@ -132,7 +132,7 @@ rte_bpf_load(const struct rte_bpf_prm *prm)
 }
 
 #ifndef RTE_LIBRTE_BPF_ELF
-__rte_experimental struct rte_bpf *
+struct rte_bpf *
 rte_bpf_elf_load(const struct rte_bpf_prm *prm, const char *fname,
 	const char *sname)
 {

lib/librte_bpf/bpf_load_elf.c
@@ -294,7 +294,7 @@ bpf_load_elf(const struct rte_bpf_prm *prm, int32_t fd, const char *section)
 	return bpf;
 }
 
-__rte_experimental struct rte_bpf *
+struct rte_bpf *
 rte_bpf_elf_load(const struct rte_bpf_prm *prm, const char *fname,
 	const char *sname)
 {

commit 1befa4b916b45613563475fd6ebacc19d5707405

devtools: fix regex matching literal plus in patches
In Extended Regular Expressions (ERE) as used in awk,
'+' is a quantifier, not a literal character.
The pattern /^+/ matches the start of any line
instead of only lines beginning with a literal '+'.
As a result, check_experimental_tags and check_internal_tags
were matching context and removed lines in diffs, causing false positives.

Use [+] character class to unambiguously match a literal '+'.

Fixes: cfe3aeb170b2 ("remove experimental tags from all symbol definitions")
Fixes: fba5af82adc8 ("eal: add internal ABI tag definition")
Cc: stable@dpdk.org

Signed-off-by: Thomas Monjalon <thomas@monjalon.net>
Signed-off-by: 0-day Robot <robot@bytheb.org>

devtools/checkpatches.sh
@@ -321,10 +321,10 @@ check_experimental_tags() { # <patch>
 		current_file = "";
 		ret = 0;
 	}
-	/^+++ b\// {
+	/^[+][+][+] b\// {
 		current_file = $2;
 	}
-	/^+.*__rte_experimental/ {
+	/^[+].*__rte_experimental/ {
 		if (current_file ~ ".c$" ) {
 			print "Please only put __rte_experimental tags in " \
 				"headers ("current_file")";
@@ -351,10 +351,10 @@ check_internal_tags() { # <patch>
 		current_file = "";
 		ret = 0;
 	}
-	/^+++ b\// {
+	/^[+][+][+] b\// {
 		current_file = $2;
 	}
-	/^+.*__rte_internal/ {
+	/^[+].*__rte_internal/ {
 		if (current_file ~ ".c$" ) {
 			print "Please only put __rte_internal tags in " \
 				"headers ("current_file")";