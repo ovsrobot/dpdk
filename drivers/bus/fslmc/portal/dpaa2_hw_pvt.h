85f4b6b1ea056c112db05a7e7be04944c7fb58d4
Maxime Leroy <maxime@leroys.fr>
2026-06-11 17:49:18 +0200

    bus/fslmc: move DPCON management from event driver to bus
    The DPCON allocation helpers (rte_dpaa2_alloc_dpcon_dev /
    rte_dpaa2_free_dpcon_dev) lived in the event driver, but a notification
    channel is a generic QBMan resource. Move dpaa2_hw_dpcon.c to the fslmc
    bus and export the helpers as internal symbols so both the event PMD and
    the net driver's rx-queue interrupt path can draw channels from the same
    pool. No functional change.
    
    Signed-off-by: Maxime Leroy <maxime@leroys.fr>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

--- drivers/bus/fslmc/portal/dpaa2_hw_pvt.h
+++ drivers/bus/fslmc/portal/dpaa2_hw_pvt.h
@@ -274,6 +274,14 @@ struct dpaa2_dpcon_dev {
 	uint8_t channel_index;
 };
 
+/* DPCON channel allocation -- managed by the fslmc bus so both the net
+ * NAPI/DQRR rx path and the event PMD can grab channels.
+ */
+__rte_internal
+struct dpaa2_dpcon_dev *rte_dpaa2_alloc_dpcon_dev(void);
+__rte_internal
+void rte_dpaa2_free_dpcon_dev(struct dpaa2_dpcon_dev *dpcon);
+
 /* Refer to Table 7-3 in SEC BG */
 #define QBMAN_FLE_WORD4_FMT_SBF 0x0    /* Single buffer frame */
 #define QBMAN_FLE_WORD4_FMT_SGE 0x2 /* Scatter gather frame */
