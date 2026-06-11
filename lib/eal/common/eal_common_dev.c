ovsrobot/dpdk

commit 76622feba9e67b37081a8f5aaf5be7e130e7fa5e

bus: refactor device probe
Introduce a new rte_bus_probe_device_t operation with signature
(struct rte_driver *drv, struct rte_device *dev).

Replace the existing .plug field in the struct rte_bus with .probe_device.

Update all in-tree buses to use .probe_device instead of .plug.
Each bus probe() function now calls rte_bus_find_driver() (which uses the
match operation added in previous commit) and passes the found driver
to bus.probe_device(driver, device).

Signed-off-by: David Marchand <david.marchand@redhat.com>
Acked-by: Bruce Richardson <bruce.richardson@intel.com>
Acked-by: Chengwen Feng <fengchengwen@huawei.com>

lib/eal/common/eal_common_dev.c
@@ -179,6 +179,7 @@ int
 local_dev_probe(const char *devargs, struct rte_device **new_dev)
 {
 	struct rte_device *dev;
+	struct rte_driver *drv;
 	struct rte_devargs *da;
 	int ret;
 
@@ -191,7 +192,7 @@ local_dev_probe(const char *devargs, struct rte_device **new_dev)
 	if (ret)
 		goto err_devarg;
 
-	if (da->bus->plug == NULL) {
+	if (da->bus->probe_device == NULL) {
 		EAL_LOG(ERR, "Function plug not supported by bus (%s)",
 			da->bus->name);
 		ret = -ENOTSUP;
@@ -219,9 +220,16 @@ local_dev_probe(const char *devargs, struct rte_device **new_dev)
 	 * those devargs shouldn't be removed manually anymore.
 	 */
 
-	ret = dev->bus->plug(dev);
-	if (ret > 0)
+	drv = NULL;
+next_driver:
+	drv = rte_bus_find_driver(dev->bus, drv, dev);
+	if (drv == NULL) {
 		ret = -ENOTSUP;
+	} else {
+		ret = dev->bus->probe_device(drv, dev);
+		if (ret > 0)
+			goto next_driver;
+	}
 
 	if (ret && !rte_dev_is_probed(dev)) { /* if hasn't ever succeeded */
 		EAL_LOG(ERR, "Driver cannot attach the device (%s)",

drivers/bus/auxiliary/auxiliary_common.c
@@ -74,61 +74,60 @@ auxiliary_bus_match(const struct rte_driver *drv, const struct rte_device *dev)
  * Call the probe() function of the driver.
  */
 static int
-rte_auxiliary_probe_one_driver(struct rte_auxiliary_driver *drv,
-			       struct rte_auxiliary_device *dev)
+auxiliary_probe_device(struct rte_driver *drv, struct rte_device *dev)
 {
+	struct rte_auxiliary_device *aux_dev = RTE_BUS_DEVICE(dev, *aux_dev);
+	struct rte_auxiliary_driver *aux_drv = RTE_BUS_DRIVER(drv, *aux_drv);
 	enum rte_iova_mode iova_mode;
 	int ret;
 
-	/* Check if driver supports it. */
-	if (!auxiliary_bus_match(&drv->driver, &dev->device))
-		/* Match of device and driver failed */
-		return 1;
+	if (!auxiliary_dev_exists(dev->name))
+		return -ENOENT;
 
 	/* No initialization when marked as blocked, return without error. */
-	if (dev->device.devargs != NULL &&
-	    dev->device.devargs->policy == RTE_DEV_BLOCKED) {
+	if (aux_dev->device.devargs != NULL &&
+	    aux_dev->device.devargs->policy == RTE_DEV_BLOCKED) {
 		AUXILIARY_LOG(INFO, "Device is blocked, not initializing");
 		return -1;
 	}
 
-	if (dev->device.numa_node < 0 && rte_socket_count() > 1)
-		AUXILIARY_LOG(INFO, "Device %s is not NUMA-aware", dev->name);
+	if (aux_dev->device.numa_node < 0 && rte_socket_count() > 1)
+		AUXILIARY_LOG(INFO, "Device %s is not NUMA-aware", aux_dev->name);
 
-	if (rte_dev_is_probed(&dev->device)) {
+	if (rte_dev_is_probed(&aux_dev->device)) {
 		AUXILIARY_LOG(DEBUG, "Device %s is already probed on auxiliary bus",
-			dev->device.name);
+			aux_dev->device.name);
 		return -EEXIST;
 	}
 
 	iova_mode = rte_eal_iova_mode();
-	if ((drv->drv_flags & RTE_AUXILIARY_DRV_NEED_IOVA_AS_VA) > 0 &&
+	if ((aux_drv->drv_flags & RTE_AUXILIARY_DRV_NEED_IOVA_AS_VA) > 0 &&
 	    iova_mode != RTE_IOVA_VA) {
 		AUXILIARY_LOG(ERR, "Driver %s expecting VA IOVA mode but current mode is PA, not initializing",
-			      drv->driver.name);
+			      aux_drv->driver.name);
 		return -EINVAL;
 	}
 
 	/* Allocate interrupt instance */
-	dev->intr_handle =
+	aux_dev->intr_handle =
 		rte_intr_instance_alloc(RTE_INTR_INSTANCE_F_PRIVATE);
-	if (dev->intr_handle == NULL) {
+	if (aux_dev->intr_handle == NULL) {
 		AUXILIARY_LOG(ERR, "Could not allocate interrupt instance for device %s",
-			dev->name);
+			aux_dev->name);
 		return -ENOMEM;
 	}
 
-	dev->driver = drv;
+	aux_dev->driver = aux_drv;
 
 	AUXILIARY_LOG(INFO, "Probe auxiliary driver: %s device: %s (NUMA node %i)",
-		      drv->driver.name, dev->name, dev->device.numa_node);
-	ret = drv->probe(drv, dev);
+		      aux_drv->driver.name, aux_dev->name, aux_dev->device.numa_node);
+	ret = aux_drv->probe(aux_drv, aux_dev);
 	if (ret != 0) {
-		dev->driver = NULL;
-		rte_intr_instance_free(dev->intr_handle);
-		dev->intr_handle = NULL;
+		aux_dev->driver = NULL;
+		rte_intr_instance_free(aux_dev->intr_handle);
+		aux_dev->intr_handle = NULL;
 	} else {
-		dev->device.driver = &drv->driver;
+		aux_dev->device.driver = &aux_drv->driver;
 	}
 
 	return ret;
@@ -160,33 +159,6 @@ rte_auxiliary_driver_remove_dev(struct rte_auxiliary_device *dev)
 }
 
 /*
- * Call the probe() function of all registered drivers for the given device.
- * Return < 0 if initialization failed.
- * Return 1 if no driver is found for this device.
- */
-static int
-auxiliary_probe_all_drivers(struct rte_auxiliary_device *dev)
-{
-	struct rte_auxiliary_driver *drv;
-	int rc;
-
-	RTE_BUS_FOREACH_DRV(drv, &auxiliary_bus.bus) {
-		if (!drv->match(dev->name))
-			continue;
-
-		rc = rte_auxiliary_probe_one_driver(drv, dev);
-		if (rc < 0)
-			/* negative value is an error */
-			return rc;
-		if (rc > 0)
-			/* positive value means driver doesn't support it */
-			continue;
-		return 0;
-	}
-	return 1;
-}
-
-/*
  * Scan the content of the auxiliary bus, and call the probe function for
  * all registered drivers to try to probe discovered devices.
  */
@@ -195,12 +167,19 @@ auxiliary_probe(void)
 {
 	struct rte_auxiliary_device *dev = NULL;
 	size_t probed = 0, failed = 0;
-	int ret = 0;
 
 	RTE_BUS_FOREACH_DEV(dev, &auxiliary_bus.bus) {
+		struct rte_driver *drv = NULL;
+		int ret;
+
 		probed++;
 
-		ret = auxiliary_probe_all_drivers(dev);
+next_driver:
+		drv = rte_bus_find_driver(&auxiliary_bus.bus, drv, &dev->device);
+		if (drv == NULL)
+			continue;
+
+		ret = auxiliary_bus.bus.probe_device(drv, &dev->device);
 		if (ret < 0) {
 			if (ret != -EEXIST) {
 				AUXILIARY_LOG(ERR, "Requested device %s cannot be used",
@@ -208,7 +187,8 @@ auxiliary_probe(void)
 				rte_errno = errno;
 				failed++;
 			}
-			ret = 0;
+		} else if (ret > 0) {
+			goto next_driver;
 		}
 	}
 
@@ -248,14 +228,6 @@ void
 rte_auxiliary_unregister(struct rte_auxiliary_driver *driver)
 {
 	rte_bus_remove_driver(&auxiliary_bus.bus, &driver->driver);
-}
-
-static int
-auxiliary_plug(struct rte_device *dev)
-{
-	if (!auxiliary_dev_exists(dev->name))
-		return -ENOENT;
-	return auxiliary_probe_all_drivers(RTE_BUS_DEVICE(dev, struct rte_auxiliary_device));
 }
 
 static int
@@ -340,7 +312,7 @@ struct rte_auxiliary_bus auxiliary_bus = {
 		.cleanup = auxiliary_cleanup,
 		.find_device = rte_bus_generic_find_device,
 		.match = auxiliary_bus_match,
-		.plug = auxiliary_plug,
+		.probe_device = auxiliary_probe_device,
 		.unplug = auxiliary_unplug,
 		.parse = auxiliary_parse,
 		.dma_map = auxiliary_dma_map,

drivers/bus/cdx/cdx.c
@@ -308,29 +308,23 @@ cdx_bus_match(const struct rte_driver *drv, const struct rte_device *dev)
  * driver.
  */
 static int
-cdx_probe_one_driver(struct rte_cdx_driver *dr,
-		struct rte_cdx_device *dev)
+cdx_probe_device(struct rte_driver *drv, struct rte_device *dev)
 {
-	const char *dev_name = dev->name;
-	bool already_probed;
+	struct rte_cdx_device *cdx_dev = RTE_BUS_DEVICE(dev, *cdx_dev);
+	struct rte_cdx_driver *cdx_drv = RTE_BUS_DRIVER(drv, *cdx_drv);
+	const char *dev_name = cdx_dev->name;
 	int ret;
 
-	/* The device is not blocked; Check if driver supports it */
-	if (!cdx_bus_match(&dr->driver, &dev->device))
-		/* Match of device and driver failed */
-		return 1;
-
-	already_probed = rte_dev_is_probed(&dev->device);
-	if (already_probed) {
+	if (rte_dev_is_probed(&cdx_dev->device)) {
 		CDX_BUS_INFO("Device %s is already probed", dev_name);
 		return -EEXIST;
 	}
 
 	CDX_BUS_DEBUG("  probe device %s using driver: %s", dev_name,
-		dr->driver.name);
+		cdx_drv->driver.name);
 
-	if (dr->drv_flags & RTE_CDX_DRV_NEED_MAPPING) {
-		ret = cdx_vfio_map_resource(dev);
+	if (cdx_drv->drv_flags & RTE_CDX_DRV_NEED_MAPPING) {
+		ret = cdx_vfio_map_resource(cdx_dev);
 		if (ret != 0) {
 			CDX_BUS_ERR("CDX map device failed: %d", ret);
 			goto error_map_device;
@@ -338,48 +332,24 @@ cdx_probe_one_driver(struct rte_cdx_driver *dr,
 	}
 
 	/* call the driver probe() function */
-	ret = dr->probe(dr, dev);
+	ret = cdx_drv->probe(cdx_drv, cdx_dev);
 	if (ret) {
 		CDX_BUS_ERR("Probe CDX driver: %s device: %s failed: %d",
-			dr->driver.name, dev_name, ret);
+			cdx_drv->driver.name, dev_name, ret);
 		goto error_probe;
 	} else {
-		dev->device.driver = &dr->driver;
+		cdx_dev->device.driver = &cdx_drv->driver;
 	}
-	dev->driver = dr;
+	cdx_dev->driver = cdx_drv;
 
 	return ret;
 
 error_probe:
-	cdx_vfio_unmap_resource(dev);
-	rte_intr_instance_free(dev->intr_handle);
-	dev->intr_handle = NULL;
+	cdx_vfio_unmap_resource(cdx_dev);
+	rte_intr_instance_free(cdx_dev->intr_handle);
+	cdx_dev->intr_handle = NULL;
 error_map_device:
 	return ret;
-}
-
-/*
- * If vendor/device ID match, call the probe() function of all
- * registered driver for the given device. Return < 0 if initialization
- * failed, return 1 if no driver is found for this device.
- */
-static int
-cdx_probe_all_drivers(struct rte_cdx_device *dev)
-{
-	struct rte_cdx_driver *dr = NULL;
-	int rc = 0;
-
-	RTE_BUS_FOREACH_DRV(dr, &rte_cdx_bus.bus) {
-		rc = cdx_probe_one_driver(dr, dev);
-		if (rc < 0)
-			/* negative value is an error */
-			return rc;
-		if (rc > 0)
-			/* positive value means driver doesn't support it */
-			continue;
-		return 0;
-	}
-	return 1;
 }
 
 /*
@@ -392,17 +362,26 @@ cdx_probe(void)
 {
 	struct rte_cdx_device *dev = NULL;
 	size_t probed = 0, failed = 0;
-	int ret = 0;
 
 	RTE_BUS_FOREACH_DEV(dev, &rte_cdx_bus.bus) {
+		struct rte_driver *drv = NULL;
+		int ret;
+
 		probed++;
 
-		ret = cdx_probe_all_drivers(dev);
+next_driver:
+		drv = rte_bus_find_driver(&rte_cdx_bus.bus, drv, &dev->device);
+		if (drv == NULL)
+			continue;
+
+		ret = rte_cdx_bus.bus.probe_device(drv, &dev->device);
 		if (ret < 0) {
 			CDX_BUS_ERR("Requested device %s cannot be used",
 				dev->name);
 			rte_errno = errno;
 			failed++;
+		} else if (ret > 0) {
+			goto next_driver;
 		}
 	}
 
@@ -471,12 +450,6 @@ cdx_detach_dev(struct rte_cdx_device *dev)
 }
 
 static int
-cdx_plug(struct rte_device *dev)
-{
-	return cdx_probe_all_drivers(RTE_BUS_DEVICE(dev, struct rte_cdx_device));
-}
-
-static int
 cdx_unplug(struct rte_device *dev)
 {
 	struct rte_cdx_device *cdx_dev = RTE_BUS_DEVICE(dev, *cdx_dev);
@@ -524,7 +497,7 @@ struct rte_cdx_bus rte_cdx_bus = {
 		.probe = cdx_probe,
 		.find_device = rte_bus_generic_find_device,
 		.match = cdx_bus_match,
-		.plug = cdx_plug,
+		.probe_device = cdx_probe_device,
 		.unplug = cdx_unplug,
 		.parse = cdx_parse,
 		.dma_map = cdx_dma_map,

drivers/bus/dpaa/dpaa_bus.c
@@ -706,7 +706,6 @@ rte_dpaa_bus_probe(void)
 {
 	int ret = -1;
 	struct rte_dpaa_device *dev;
-	struct rte_dpaa_driver *drv;
 	FILE *svr_file = NULL;
 	uint32_t svr_ver;
 	static int process_once;
@@ -789,25 +788,18 @@ rte_dpaa_bus_probe(void)
 
 	/* For each registered driver, and device, call the driver->probe */
 	RTE_BUS_FOREACH_DEV(dev, &rte_dpaa_bus.bus) {
-		RTE_BUS_FOREACH_DRV(drv, &rte_dpaa_bus.bus) {
-			if (!dpaa_bus_match(&drv->driver, &dev->device))
-				continue;
+		struct rte_driver *driver = NULL;
 
-			if (rte_dev_is_probed(&dev->device))
-				continue;
+next_driver:
+		driver = rte_bus_find_driver(&rte_dpaa_bus.bus, driver, &dev->device);
+		if (driver == NULL)
+			continue;
 
-			if (rte_bus_device_is_ignored(&rte_dpaa_bus.bus, dev->name))
-				continue;
-
-			ret = drv->probe(drv, dev);
-			if (ret) {
-				DPAA_BUS_ERR("unable to probe: %s", dev->name);
-			} else {
-				dev->driver = drv;
-				dev->device.driver = &drv->driver;
-			}
-			break;
-		}
+		ret = rte_dpaa_bus.bus.probe_device(driver, &dev->device);
+		if (ret < 0)
+			DPAA_BUS_ERR("Failed to probe device %s", dev->name);
+		else if (ret > 0)
+			goto next_driver;
 	}
 	dpaa_bus_global_init = 1;
 
@@ -828,17 +820,27 @@ rte_dpaa_get_iommu_class(void)
 }
 
 static int
-dpaa_bus_plug(struct rte_device *dev __rte_unused)
+dpaa_bus_probe_device(struct rte_driver *drv, struct rte_device *dev)
 {
-	/* No operation is performed while plugging the device */
-	return 0;
-}
+	struct rte_dpaa_device *dpaa_dev = RTE_BUS_DEVICE(dev, *dpaa_dev);
+	struct rte_dpaa_driver *dpaa_drv = RTE_BUS_DRIVER(drv, *dpaa_drv);
+	int ret;
 
-static int
-dpaa_bus_unplug(struct rte_device *dev __rte_unused)
-{
-	/* No operation is performed while unplugging the device */
-	return 0;
+	if (rte_dev_is_probed(&dpaa_dev->device))
+		return 0;
+
+	if (rte_bus_device_is_ignored(&rte_dpaa_bus.bus, dpaa_dev->name))
+		return 0;
+
+	ret = dpaa_drv->probe(dpaa_drv, dpaa_dev);
+	if (ret != 0) {
+		DPAA_BUS_ERR("unable to probe: %s", dpaa_dev->name);
+	} else {
+		dpaa_dev->driver = dpaa_drv;
+		dpaa_dev->device.driver = &dpaa_drv->driver;
+	}
+
+	return ret;
 }
 
 static int
@@ -899,8 +901,7 @@ static struct rte_dpaa_bus rte_dpaa_bus = {
 		.find_device = rte_bus_generic_find_device,
 		.get_iommu_class = rte_dpaa_get_iommu_class,
 		.match = dpaa_bus_match,
-		.plug = dpaa_bus_plug,
-		.unplug = dpaa_bus_unplug,
+		.probe_device = dpaa_bus_probe_device,
 		.dev_iterate = rte_bus_generic_dev_iterate,
 		.cleanup = dpaa_bus_cleanup,
 	},

drivers/bus/fslmc/fslmc_bus.c
@@ -411,7 +411,6 @@ rte_fslmc_probe(void)
 	int ret = 0;
 
 	struct rte_dpaa2_device *dev;
-	struct rte_dpaa2_driver *drv;
 
 	static const struct rte_mbuf_dynfield dpaa2_seqn_dynfield_desc = {
 		.name = DPAA2_SEQN_DYNFIELD_NAME,
@@ -456,25 +455,21 @@ rte_fslmc_probe(void)
 	}
 
 	RTE_BUS_FOREACH_DEV(dev, &rte_fslmc_bus.bus) {
-		RTE_BUS_FOREACH_DRV(drv, &rte_fslmc_bus.bus) {
-			if (!fslmc_bus_match(&drv->driver, &dev->device))
-				continue;
+		struct rte_driver *driver = NULL;
 
-			if (rte_dev_is_probed(&dev->device))
-				continue;
+		if (rte_bus_device_is_ignored(&rte_fslmc_bus.bus, dev->device.name))
+			continue;
 
-			if (rte_bus_device_is_ignored(&rte_fslmc_bus.bus, dev->device.name))
-				continue;
+next_driver:
+		driver = rte_bus_find_driver(&rte_fslmc_bus.bus, driver, &dev->device);
+		if (driver == NULL)
+			continue;
 
-			ret = drv->probe(drv, dev);
-			if (ret) {
-				DPAA2_BUS_ERR("Unable to probe");
-			} else {
-				dev->driver = drv;
-				dev->device.driver = &drv->driver;
-			}
-			break;
-		}
+		ret = rte_fslmc_bus.bus.probe_device(driver, &dev->device);
+		if (ret < 0)
+			DPAA2_BUS_ERR("Failed to probe device %s", dev->device.name);
+		else if (ret > 0)
+			goto next_driver;
 	}
 
 	return 0;
@@ -540,35 +535,29 @@ rte_dpaa2_get_iommu_class(void)
 }
 
 static int
-fslmc_bus_plug(struct rte_device *rte_dev)
+fslmc_bus_probe_device(struct rte_driver *driver, struct rte_device *rte_dev)
 {
-	int ret = 0;
 	struct rte_dpaa2_device *dev = RTE_BUS_DEVICE(rte_dev, *dev);
-	struct rte_dpaa2_driver *drv;
+	struct rte_dpaa2_driver *drv = RTE_BUS_DRIVER(driver, *drv);
+	int ret = 0;
 
-	RTE_BUS_FOREACH_DRV(drv, &rte_fslmc_bus.bus) {
-		if (!fslmc_bus_match(&drv->driver, &dev->device))
-			continue;
+	if (rte_dev_is_probed(&dev->device))
+		return 0;
 
-		if (rte_dev_is_probed(&dev->device))
-			continue;
+	if (dev->device.devargs &&
+	    dev->device.devargs->policy == RTE_DEV_BLOCKED) {
+		DPAA2_BUS_DEBUG("%s Blocked, skipping",
+			      dev->device.name);
+		return 0;
+	}
 
-		if (dev->device.devargs &&
-		    dev->device.devargs->policy == RTE_DEV_BLOCKED) {
-			DPAA2_BUS_DEBUG("%s Blocked, skipping",
-				      dev->device.name);
-			continue;
-		}
-
-		ret = drv->probe(drv, dev);
-		if (ret) {
-			DPAA2_BUS_ERR("Unable to probe");
-		} else {
-			dev->driver = drv;
-			dev->device.driver = &drv->driver;
-			DPAA2_BUS_INFO("%s Plugged",  dev->device.name);
-		}
-		break;
+	ret = drv->probe(drv, dev);
+	if (ret != 0) {
+		DPAA2_BUS_ERR("Unable to probe");
+	} else {
+		dev->driver = drv;
+		dev->device.driver = &drv->driver;
+		DPAA2_BUS_INFO("%s Plugged",  dev->device.name);
 	}
 
 	return ret;
@@ -601,7 +590,7 @@ struct rte_fslmc_bus rte_fslmc_bus = {
 		.find_device = rte_bus_generic_find_device,
 		.get_iommu_class = rte_dpaa2_get_iommu_class,
 		.match = fslmc_bus_match,
-		.plug = fslmc_bus_plug,
+		.probe_device = fslmc_bus_probe_device,
 		.unplug = fslmc_bus_unplug,
 		.dev_iterate = rte_bus_generic_dev_iterate,
 	},

drivers/bus/ifpga/ifpga_bus.c
@@ -268,33 +268,11 @@ ifpga_bus_match(const struct rte_driver *drv, const struct rte_device *dev)
 }
 
 static int
-ifpga_probe_one_driver(struct rte_afu_driver *drv,
-			struct rte_afu_device *afu_dev)
+ifpga_probe_device(struct rte_driver *drv, struct rte_device *dev)
 {
+	struct rte_afu_device *afu_dev = RTE_BUS_DEVICE(dev, *afu_dev);
+	struct rte_afu_driver *afu_drv = RTE_BUS_DRIVER(drv, *afu_drv);
 	int ret;
-
-	if (!ifpga_bus_match(&drv->driver, &afu_dev->device))
-		/* Match of device and driver failed */
-		return 1;
-
-	/* reference driver structure */
-	afu_dev->driver = drv;
-
-	/* call the driver probe() function */
-	ret = drv->probe(afu_dev);
-	if (ret)
-		afu_dev->driver = NULL;
-	else
-		afu_dev->device.driver = &drv->driver;
-
-	return ret;
-}
-
-static int
-ifpga_probe_all_drivers(struct rte_afu_device *afu_dev)
-{
-	struct rte_afu_driver *drv = NULL;
-	int ret = 0;
 
 	/* Check if a driver is already loaded */
 	if (rte_dev_is_probed(&afu_dev->device)) {
@@ -303,20 +281,17 @@ ifpga_probe_all_drivers(struct rte_afu_device *afu_dev)
 		return -EEXIST;
 	}
 
-	RTE_BUS_FOREACH_DRV(drv, &rte_ifpga_bus) {
-		ret = ifpga_probe_one_driver(drv, afu_dev);
-		if (ret < 0)
-			/* negative value is an error */
-			return ret;
-		if (ret > 0)
-			/* positive value means driver doesn't support it */
-			continue;
-		return 0;
-	}
-	if ((ret > 0) && (afu_dev->driver == NULL))
-		return 0;
+	/* reference driver structure */
+	afu_dev->driver = afu_drv;
+
+	/* call the driver probe() function */
+	ret = afu_drv->probe(afu_dev);
+	if (ret)
+		afu_dev->driver = NULL;
 	else
-		return ret;
+		afu_dev->device.driver = &afu_drv->driver;
+
+	return ret;
 }
 
 /*
@@ -331,12 +306,21 @@ ifpga_probe(void)
 	int ret = 0;
 
 	RTE_BUS_FOREACH_DEV(afu_dev, &rte_ifpga_bus) {
-		ret = ifpga_probe_all_drivers(afu_dev);
+		struct rte_driver *drv = NULL;
+
+next_driver:
+		drv = rte_bus_find_driver(&rte_ifpga_bus, drv, &afu_dev->device);
+		if (drv == NULL)
+			continue;
+
+		ret = rte_ifpga_bus.probe_device(drv, &afu_dev->device);
 		if (ret == -EEXIST)
 			continue;
 		if (ret < 0)
 			IFPGA_BUS_ERR("failed to initialize %s device",
 				rte_ifpga_device_name(afu_dev));
+		else if (ret > 0)
+			goto next_driver;
 	}
 
 	return ret;
@@ -377,12 +361,6 @@ free:
 	}
 
 	return error;
-}
-
-static int
-ifpga_plug(struct rte_device *dev)
-{
-	return ifpga_probe_all_drivers(RTE_BUS_DEVICE(dev, struct rte_afu_device));
 }
 
 static int
@@ -454,7 +432,7 @@ static struct rte_bus rte_ifpga_bus = {
 	.cleanup     = ifpga_cleanup,
 	.find_device = rte_bus_generic_find_device,
 	.match       = ifpga_bus_match,
-	.plug        = ifpga_plug,
+	.probe_device = ifpga_probe_device,
 	.unplug      = ifpga_unplug,
 	.parse       = ifpga_parse,
 };

drivers/bus/pci/pci_common.c
@@ -181,51 +181,42 @@ pci_bus_match(const struct rte_driver *drv, const struct rte_device *dev)
  * driver.
  */
 static int
-rte_pci_probe_one_driver(struct rte_pci_driver *dr,
-			 struct rte_pci_device *dev)
+pci_probe_device(struct rte_driver *drv, struct rte_device *dev)
 {
-	int ret;
+	struct rte_pci_device *pci_dev = RTE_BUS_DEVICE(dev, *pci_dev);
+	struct rte_pci_driver *pci_drv = RTE_BUS_DRIVER(drv, *pci_drv);
+	struct rte_pci_addr *loc = &pci_dev->addr;
 	bool already_probed;
-	struct rte_pci_addr *loc;
-
-	if ((dr == NULL) || (dev == NULL))
-		return -EINVAL;
-
-	loc = &dev->addr;
-
-	/* The device is not blocked; Check if driver supports it */
-	if (!pci_bus_match(&dr->driver, &dev->device))
-		/* Match of device and driver failed */
-		return 1;
+	int ret;
 
 	PCI_LOG(DEBUG, "PCI device "PCI_PRI_FMT" on NUMA socket %i",
 		loc->domain, loc->bus, loc->devid, loc->function,
-		dev->device.numa_node);
+		pci_dev->device.numa_node);
 
 	/* no initialization when marked as blocked, return without error */
-	if (dev->device.devargs != NULL &&
-		dev->device.devargs->policy == RTE_DEV_BLOCKED) {
+	if (pci_dev->device.devargs != NULL &&
+		pci_dev->device.devargs->policy == RTE_DEV_BLOCKED) {
 		PCI_LOG(INFO, "  Device is blocked, not initializing");
 		return 1;
 	}
 
-	if (dev->device.numa_node < 0 && rte_socket_count() > 1)
-		PCI_LOG(INFO, "Device %s is not NUMA-aware", dev->name);
+	if (pci_dev->device.numa_node < 0 && rte_socket_count() > 1)
+		PCI_LOG(INFO, "Device %s is not NUMA-aware", pci_dev->name);
 
-	already_probed = rte_dev_is_probed(&dev->device);
-	if (already_probed && !(dr->drv_flags & RTE_PCI_DRV_PROBE_AGAIN)) {
-		PCI_LOG(DEBUG, "Device %s is already probed", dev->device.name);
+	already_probed = rte_dev_is_probed(&pci_dev->device);
+	if (already_probed && !(pci_drv->drv_flags & RTE_PCI_DRV_PROBE_AGAIN)) {
+		PCI_LOG(DEBUG, "Device %s is already probed", pci_dev->device.name);
 		return -EEXIST;
 	}
 
-	PCI_LOG(DEBUG, "  probe driver: %x:%x %s", dev->id.vendor_id,
-		dev->id.device_id, dr->driver.name);
+	PCI_LOG(DEBUG, "  probe driver: %x:%x %s", pci_dev->id.vendor_id,
+		pci_dev->id.device_id, pci_drv->driver.name);
 
 	if (!already_probed) {
 		enum rte_iova_mode dev_iova_mode;
 		enum rte_iova_mode iova_mode;
 
-		dev_iova_mode = pci_device_iova_mode(dr, dev);
+		dev_iova_mode = pci_device_iova_mode(pci_drv, pci_dev);
 		iova_mode = rte_eal_iova_mode();
 		if (dev_iova_mode != RTE_IOVA_DC &&
 		    dev_iova_mode != iova_mode) {
@@ -236,21 +227,21 @@ rte_pci_probe_one_driver(struct rte_pci_driver *dr,
 		}
 
 		/* Allocate interrupt instance for pci device */
-		dev->intr_handle =
+		pci_dev->intr_handle =
 			rte_intr_instance_alloc(RTE_INTR_INSTANCE_F_PRIVATE);
-		if (dev->intr_handle == NULL) {
+		if (pci_dev->intr_handle == NULL) {
 			PCI_LOG(ERR, "Failed to create interrupt instance for %s",
-				dev->device.name);
+				pci_dev->device.name);
 			return -ENOMEM;
 		}
 
-		dev->vfio_req_intr_handle =
+		pci_dev->vfio_req_intr_handle =
 			rte_intr_instance_alloc(RTE_INTR_INSTANCE_F_PRIVATE);
-		if (dev->vfio_req_intr_handle == NULL) {
-			rte_intr_instance_free(dev->intr_handle);
-			dev->intr_handle = NULL;
+		if (pci_dev->vfio_req_intr_handle == NULL) {
+			rte_intr_instance_free(pci_dev->intr_handle);
+			pci_dev->intr_handle = NULL;
 			PCI_LOG(ERR, "Failed to create vfio req interrupt instance for %s",
-				dev->device.name);
+				pci_dev->device.name);
 			return -ENOMEM;
 		}
 
@@ -259,43 +250,43 @@ rte_pci_probe_one_driver(struct rte_pci_driver *dr,
 		 * This needs to be before rte_pci_map_device(), as it enables
 		 * to use driver flags for adjusting configuration.
 		 */
-		dev->driver = dr;
-		if (dev->driver->drv_flags & RTE_PCI_DRV_NEED_MAPPING) {
-			ret = rte_pci_map_device(dev);
+		pci_dev->driver = pci_drv;
+		if (pci_dev->driver->drv_flags & RTE_PCI_DRV_NEED_MAPPING) {
+			ret = rte_pci_map_device(pci_dev);
 			if (ret != 0) {
-				dev->driver = NULL;
-				rte_intr_instance_free(dev->vfio_req_intr_handle);
-				dev->vfio_req_intr_handle = NULL;
-				rte_intr_instance_free(dev->intr_handle);
-				dev->intr_handle = NULL;
+				pci_dev->driver = NULL;
+				rte_intr_instance_free(pci_dev->vfio_req_intr_handle);
+				pci_dev->vfio_req_intr_handle = NULL;
+				rte_intr_instance_free(pci_dev->intr_handle);
+				pci_dev->intr_handle = NULL;
 				return ret;
 			}
 		}
 	}
 
 	PCI_LOG(INFO, "Probe PCI driver: %s (%x:%04x) device: "PCI_PRI_FMT" (socket %i)",
-		dr->driver.name, dev->id.vendor_id, dev->id.device_id,
+		pci_drv->driver.name, pci_dev->id.vendor_id, pci_dev->id.device_id,
 		loc->domain, loc->bus, loc->devid, loc->function,
-		dev->device.numa_node);
+		pci_dev->device.numa_node);
 	/* call the driver probe() function */
-	ret = dr->probe(dr, dev);
+	ret = pci_drv->probe(pci_drv, pci_dev);
 	if (already_probed)
 		return ret; /* no rollback if already succeeded earlier */
 	if (ret) {
-		dev->driver = NULL;
-		if ((dr->drv_flags & RTE_PCI_DRV_NEED_MAPPING) &&
+		pci_dev->driver = NULL;
+		if ((pci_drv->drv_flags & RTE_PCI_DRV_NEED_MAPPING) &&
 			/* Don't unmap if device is unsupported and
 			 * driver needs mapped resources.
 			 */
 			!(ret > 0 &&
-				(dr->drv_flags & RTE_PCI_DRV_KEEP_MAPPED_RES)))
-			rte_pci_unmap_device(dev);
-		rte_intr_instance_free(dev->vfio_req_intr_handle);
-		dev->vfio_req_intr_handle = NULL;
-		rte_intr_instance_free(dev->intr_handle);
-		dev->intr_handle = NULL;
+				(pci_drv->drv_flags & RTE_PCI_DRV_KEEP_MAPPED_RES)))
+			rte_pci_unmap_device(pci_dev);
+		rte_intr_instance_free(pci_dev->vfio_req_intr_handle);
+		pci_dev->vfio_req_intr_handle = NULL;
+		rte_intr_instance_free(pci_dev->intr_handle);
+		pci_dev->intr_handle = NULL;
 	} else {
-		dev->device.driver = &dr->driver;
+		pci_dev->device.driver = &pci_drv->driver;
 	}
 
 	return ret;
@@ -344,33 +335,6 @@ rte_pci_detach_dev(struct rte_pci_device *dev)
 }
 
 /*
- * If vendor/device ID match, call the probe() function of all
- * registered driver for the given device. Return < 0 if initialization
- * failed, return 1 if no driver is found for this device.
- */
-static int
-pci_probe_all_drivers(struct rte_pci_device *dev)
-{
-	struct rte_pci_driver *dr = NULL;
-	int rc = 0;
-
-	if (dev == NULL)
-		return -EINVAL;
-
-	RTE_BUS_FOREACH_DRV(dr, &rte_pci_bus.bus) {
-		rc = rte_pci_probe_one_driver(dr, dev);
-		if (rc < 0)
-			/* negative value is an error */
-			return rc;
-		if (rc > 0)
-			/* positive value means driver doesn't support it */
-			continue;
-		return 0;
-	}
-	return 1;
-}
-
-/*
  * Scan the content of the PCI bus, and call the probe() function for
  * all registered drivers that have a matching entry in its id_table
  * for discovered devices.
@@ -380,12 +344,19 @@ pci_probe(void)
 {
 	struct rte_pci_device *dev = NULL;
 	size_t probed = 0, failed = 0;
-	int ret = 0;
 
 	RTE_BUS_FOREACH_DEV(dev, &rte_pci_bus.bus) {
+		struct rte_driver *drv = NULL;
+		int ret;
+
 		probed++;
 
-		ret = pci_probe_all_drivers(dev);
+next_driver:
+		drv = rte_bus_find_driver(&rte_pci_bus.bus, drv, &dev->device);
+		if (drv == NULL)
+			continue;
+
+		ret = rte_pci_bus.bus.probe_device(drv, &dev->device);
 		if (ret < 0) {
 			if (ret != -EEXIST) {
 				PCI_LOG(ERR, "Requested device " PCI_PRI_FMT " cannot be used",
@@ -395,6 +366,8 @@ pci_probe(void)
 				failed++;
 			}
 			ret = 0;
+		} else if (ret > 0) {
+			goto next_driver;
 		}
 	}
 
@@ -593,12 +566,6 @@ pci_sigbus_handler(const void *failure_addr)
 		}
 	}
 	return ret;
-}
-
-static int
-pci_plug(struct rte_device *dev)
-{
-	return pci_probe_all_drivers(RTE_BUS_DEVICE(dev, struct rte_pci_device));
 }
 
 static int
@@ -861,7 +828,7 @@ struct rte_pci_bus rte_pci_bus = {
 		.cleanup = pci_cleanup,
 		.find_device = rte_bus_generic_find_device,
 		.match = pci_bus_match,
-		.plug = pci_plug,
+		.probe_device = pci_probe_device,
 		.unplug = pci_unplug,
 		.parse = pci_parse,
 		.dev_compare = pci_dev_compare,

commit d89d694afc1fb39ef2861bfec954254c8e0c4114

bus: fix reference to plug callback
Update documentation and some log following the callback rename.

Fixes: 76622feba9e6 ("bus: refactor device probe")

Signed-off-by: David Marchand <david.marchand@redhat.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/eal/common/eal_common_dev.c
@@ -193,7 +193,7 @@ local_dev_probe(const char *devargs, struct rte_device **new_dev)
 		goto err_devarg;
 
 	if (da->bus->probe_device == NULL) {
-		EAL_LOG(ERR, "Function plug not supported by bus (%s)",
+		EAL_LOG(ERR, "Function probe_device not supported by bus (%s)",
 			da->bus->name);
 		ret = -ENOTSUP;
 		goto err_devarg;