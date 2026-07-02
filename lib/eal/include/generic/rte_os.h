2919bb044ac1e956ba4d0660c88dab24caf645d0
Marat Khalili <marat.khalili@huawei.com>
2026-07-02 21:00:44 +0100

    doc: document rte_os.h
    File rte_os.h did not previously have a generic version to generate
    documentation from, but its OS-specific version was installed. Since it
    is now verified that documentation is generated for all public headers,
    add a generic stub containing description of this file.
    
    Signed-off-by: Marat Khalili <marat.khalili@huawei.com>
    Signed-off-by: 0-day Robot <robot@bytheb.org>

---
+++ lib/eal/include/generic/rte_os.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2010-2019 Intel Corporation
+ */
+
+#ifndef _RTE_OS_H_
+#define _RTE_OS_H_
+
+/**
+ * This header should contain any definition
+ * which is not supported natively or named differently in the local OS.
+ */
+
+#endif /* _RTE_OS_H_ */
