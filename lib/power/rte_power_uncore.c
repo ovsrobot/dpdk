ovsrobot/dpdk

commit 3b3af56d3c9c472f9ba0ee1078d714919cc2b6b4

power: fix uncore configuration
Add uncore auto-detect environment.

Update the l3fwd-power app to configure the uncore env before invoking
any uncore API. With auto-detection in 'rte_power_uncore_init()' it is
too late because other APIs already called.

Bugzilla ID: 1304
Fixes: ac1edcb6621a ("power: refactor uncore power management API")

Signed-off-by: Sivaprasad Tummala <sivaprasad.tummala@amd.com>
Tested-by: Karen Kelly <karen.kelly@intel.com>
Acked-by: David Hunt <david.hunt@intel.com>

lib/power/rte_power_uncore.c
@@ -105,6 +105,13 @@ rte_power_set_uncore_env(enum rte_uncore_power_mgmt_env env)
 		return -1;
 	}
 
+	if (env == RTE_UNCORE_PM_ENV_AUTO_DETECT)
+		/* Currently only intel_uncore is supported.
+		 * This will be extended with auto-detection support
+		 * for multiple uncore implementations.
+		 */
+		env = RTE_UNCORE_PM_ENV_INTEL_UNCORE;
+
 	ret = 0;
 	if (env == RTE_UNCORE_PM_ENV_INTEL_UNCORE) {
 		rte_power_get_uncore_freq = power_get_intel_uncore_freq;

.mailmap
@@ -723,6 +723,7 @@ Kamil Chalupnik <kamilx.chalupnik@intel.com>
 Kamil Rytarowski <kamil.rytarowski@caviumnetworks.com>
 Kamil Vojanec <xvojan00@stud.fit.vutbr.cz>
 Kanaka Durga Kotamarthy <kkotamarthy@marvell.com>
+Karen Kelly <karen.kelly@intel.com>
 Karen Sornek <karen.sornek@intel.com>
 Karla Saur <karla.saur@intel.com>
 Karl Bonde Torp <k.torp@samsung.com>

examples/l3fwd-power/main.c
@@ -1561,6 +1561,12 @@ parse_uncore_options(enum uncore_choice choice, const char *argument)
 {
 	unsigned int die, pkg, max_pkg, max_die;
 	int ret = 0;
+	ret = rte_power_set_uncore_env(RTE_UNCORE_PM_ENV_AUTO_DETECT);
+	if (ret < 0) {
+		RTE_LOG(INFO, L3FWD_POWER, "Failed to set uncore env\n");
+		return ret;
+	}
+
 	max_pkg = rte_power_uncore_get_num_pkgs();
 	if (max_pkg == 0)
 		return -1;

lib/power/rte_power_uncore.h
@@ -21,15 +21,16 @@ extern "C" {
 /* Uncore Power Management Environment */
 enum rte_uncore_power_mgmt_env {
 	RTE_UNCORE_PM_ENV_NOT_SET,
+	RTE_UNCORE_PM_ENV_AUTO_DETECT,
 	RTE_UNCORE_PM_ENV_INTEL_UNCORE,
 	RTE_UNCORE_PM_ENV_AMD_HSMP
 };
 
 /**
- * Set the default uncore power management implementation. If this is not called prior
- * to rte_power_uncore_init(), then auto-detect of the environment will take place.
- * It is thread safe. New env can be set only in uninitialized state
- * (thus rte_power_unset_uncore_env must be called if different env was already set).
+ * Set the default uncore power management implementation.
+ * This has to be called prior to calling any other rte_power_uncore_*() API.
+ * It is thread safe. New env can be set only in uninitialized state.
+ * rte_power_unset_uncore_env must be called if different env was already set.
  *
  * @param env
  *  env. The environment in which to initialise Uncore Power Management for.

commit 6063a9d90b00d557f060c2625feed4048b2d4d9d

power: support automatic detection of uncore driver
Currently, uncore driver library just supports intel_uncore
driver as default driver when user use AUTO_DETECT, please see
the commit 3b3af56d3c9c ("power: fix uncore configuration").
This is not good to use for application. So this patch support
probing for uncore drivers.

Fixes: 3b3af56d3c9c ("power: fix uncore configuration")
Cc: stable@dpdk.org

Signed-off-by: Huisong Li <lihuisong@huawei.com>
Signed-off-by: 0-day Robot <robot@bytheb.org>

lib/power/rte_power_uncore.c
@@ -2,6 +2,7 @@
  * Copyright(c) 2010-2014 Intel Corporation
  * Copyright(c) 2023 AMD Corporation
  */
+#include <errno.h>
 
 #include <eal_export.h>
 #include <rte_spinlock.h>
@@ -46,6 +47,39 @@ rte_power_register_uncore_ops(struct rte_power_uncore_ops *driver_ops)
 	return 0;
 }
 
+static uint32_t power_uncore_driver_name2env(char *name)
+{
+	for (uint32_t i = 0; i < RTE_DIM(uncore_env_str); i++) {
+		if (!strcmp(name, uncore_env_str[i]))
+			return i;
+	}
+
+	return UINT32_MAX;
+}
+
+static int power_uncore_probe_driver(void)
+{
+	struct rte_power_uncore_ops *ops;
+	int ret;
+
+	global_uncore_ops = NULL;
+	/* Use package-0 and die-0 to probe uncore driver. */
+	RTE_TAILQ_FOREACH(ops, &uncore_ops_list, next) {
+		ret = ops->init(0, 0);
+		if (ret == 0) {
+			uint32_t env = power_uncore_driver_name2env(ops->name);
+			if (env == UINT32_MAX)
+				continue;
+			global_uncore_env = env;
+			global_uncore_ops = ops;
+			ops->exit(0, 0);
+			break;
+		}
+	}
+
+	return global_uncore_ops ? 0 : -ENODEV;
+}
+
 RTE_EXPORT_EXPERIMENTAL_SYMBOL(rte_power_set_uncore_env, 23.11)
 int
 rte_power_set_uncore_env(enum rte_uncore_power_mgmt_env env)
@@ -60,12 +94,12 @@ rte_power_set_uncore_env(enum rte_uncore_power_mgmt_env env)
 		goto out;
 	}
 
-	if (env == RTE_UNCORE_PM_ENV_AUTO_DETECT)
-		/* Currently only intel_uncore is supported.
-		 * This will be extended with auto-detection support
-		 * for multiple uncore implementations.
-		 */
-		env = RTE_UNCORE_PM_ENV_INTEL_UNCORE;
+	if (env == RTE_UNCORE_PM_ENV_AUTO_DETECT) {
+		ret = power_uncore_probe_driver();
+		if (ret != 0)
+			POWER_LOG(ERR, "Probe uncore driver failed, ret = %d", ret);
+		goto out;
+	}
 
 	if (env < RTE_DIM(uncore_env_str)) {
 		RTE_TAILQ_FOREACH(ops, &uncore_ops_list, next)